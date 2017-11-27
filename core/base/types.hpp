002cf3dfef4ca4bb7146c54a4be3d87e4e35fbf9
Goran Flegar <flegar.goran@gmail.com>
2017-11-27 15:17:23 +0100

    Add initial LinOp implementation

--- core/base/types.hpp
+++ core/base/types.hpp
@@ -13,8 +13,21 @@ namespace gko {
  * Integral type used for allocation quantities.
  */
 using size_type = std::size_t;
+
+/**
+ * 32-bit signed integral type.
+ */
 using int32 = std::int32_t;
+
+/**
+ * 64-bit signed integral type.
+ */
 using int64 = std::int64_t;
+
+/**
+ * The most precise floating-point type.
+ */
+using full_precision = double;
 
 
 }  // namespace gko
