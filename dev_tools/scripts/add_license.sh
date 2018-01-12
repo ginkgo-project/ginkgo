3bbf93de1fd935d4b0daa94a5f10b87b2aec5dc0
Terry Cojean <terry.cojean@kit.edu>
2018-01-12 15:42:42 +0100

    Fix the license formatting and add it to forgotten files.

--- dev_tools/scripts/add_license.sh
+++ dev_tools/scripts/add_license.sh
@@ -6,14 +6,26 @@ THIS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" &>/dev/null && pwd )
 GINKGO_ROOT_DIR="${THIS_DIR}/../.."
 LICENSE_FILE="${GINKGO_ROOT_DIR}/LICENSE"
 PATTERNS="cpp|hpp|cuh|cu"
+EXCLUDED_DIRECTORIES="devel_tools|build|third_party"
+GINKGO_LICENSE_BEACON="******************************<GINKGO LICENSE>******************************"
 
-echo -e "/*\n" "$(cat ${LICENSE_FILE} | sed -e 's/^/ \* /')" "\n*/\n" > commented_license.tmp
+echo -e "/*${GINKGO_LICENSE_BEACON}\n$(cat ${LICENSE_FILE})\n${GINKGO_LICENSE_BEACON}*/\n" > commented_license.tmp
 
-for i in ${GINKGO_ROOT_DIR}/**/*.@(${PATTERNS})
+for i in ${GINKGO_ROOT_DIR}/!(${EXCLUDED_DIRECTORIES})/**/*.@(${PATTERNS})
 do
-    if ! grep -q Copyright $i
+    if ! grep -q "${GINKGO_LICENSE_BEACON}" $i
     then
         cat commented_license.tmp $i >$i.new && mv $i.new $i
+    else
+        beginning=$(grep -n "/\*${GINKGO_LICENSE_BEACON}" $i | cut -d":" -f1)
+        end=$(grep -n "${GINKGO_LICENSE_BEACON}.*/" $i | cut -d":" -f1)
+        end=$((end+1))
+        diff -u <(sed -n "${beginning},${end}p" $i) commented_license.tmp > diff.patch
+        if [ "$(cat diff.patch)" != "" ]
+        then
+            patch $i diff.patch
+            rm diff.patch
+        fi
     fi
 done
 
