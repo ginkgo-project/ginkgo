df82acdae6fe035034e67f85afd4e71d965a6554
Terry Cojean <terry.cojean@kit.edu>
2018-01-15 09:23:41 +0100

    Factorize several parts of @hantz's solver generator script.

--- dev_tools/scripts/create_new_solver.sh
+++ dev_tools/scripts/create_new_solver.sh
@@ -37,12 +37,61 @@ done
 
 
 # Important script variable
-execute=1
 TMPDIR="./tmp_$(date +%s)"
 THIS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" &>/dev/null && pwd )
 GINKGO_ROOT_DIR="${THIS_DIR}/../.."
+solvername=${solvername,,}
 Solvername=${solvername^}
 SOLVERNAME=${solvername^^}
+
+TEMPLATE_FILES=(
+    "solver.cpp"
+    "solver.hpp"
+    "solver_kernels.hpp"
+    "solver_reference_kernels.cpp"
+    "solver_cpu_kernels.cpp"
+    "solver_gpu_kernels.cu"
+    "solver_core_test.cpp"
+    "solver_reference_test.cpp"
+    "solver_cpu_test.cpp"
+    "solver_gpu_test.cpp"
+)
+TEMPLATE_FILES_LOCATIONS=(
+    "core/solver"
+    "core/solver"
+    "core/solver"
+    "reference/solver"
+    "cpu/solver"
+    "gpu/solver"
+    "core/test/solver"
+    "reference/test/solver"
+    "cpu/test/solver"
+    "gpu/test/solver"
+)
+TEMPLATE_FILES_TYPES=(
+    "solver file"
+    "class header"
+    "kernel header"
+    "kernel file"
+    "kernel file"
+    "kernel file"
+    "unit tests for ${solvername} solver"
+    "unit tests for ${solvername} reference kernels"
+    "unit tests for ${solvername} CPU kernels"
+    "unit tests for ${solvername} GPU kernels"
+)
+TEMPLATE_FILES_DESCRIPTIONS=(
+    "This is where the ${solvername} algorithm needs to be implemented."
+    "This is where the ${solvername} class functions need to be implemented."
+    "This is where the algorithm-specific kernels need to be added."
+    "Reference kernels for ${solvername} need to be implemented here."
+    "CPU kernels for ${solvername} need to be implemented here."
+    "GPU kernels for ${solvername} need to be implemented here."
+    ""
+    ""
+    ""
+    ""
+)
 
 mkdir ${TMPDIR}
 
@@ -55,179 +104,66 @@ fi
 # create folder for temporary files
 
 # copy files needed into temporary folder
-cp ${THIS_DIR}/../templates/solver.cpp ${TMPDIR}/.
-cp ${THIS_DIR}/../templates/solver.hpp ${TMPDIR}/.
-cp ${THIS_DIR}/../templates/solver_kernels.hpp ${TMPDIR}/.
-cp ${THIS_DIR}/../templates/solver_reference_kernels.cpp ${TMPDIR}/.
-cp ${THIS_DIR}/../templates/solver_cpu_kernels.cpp ${TMPDIR}/.
-cp ${THIS_DIR}/../templates/solver_gpu_kernels.cu ${TMPDIR}/.
-
-cp ${THIS_DIR}/../templates/solver_core_test.cpp ${TMPDIR}/.
-cp ${THIS_DIR}/../templates/solver_reference_test.cpp ${TMPDIR}/.
-cp ${THIS_DIR}/../templates/solver_cpu_test.cpp ${TMPDIR}/.
-cp ${THIS_DIR}/../templates/solver_gpu_test.cpp ${TMPDIR}/.
+for i in "${TEMPLATE_FILES[@]}"
+do
+    cp ${THIS_DIR}/../templates/$i ${TMPDIR}/.
+done
 
 # search and replace keywords with new solver name
 perl -pi -e "s/XXSOLVERXX/$SOLVERNAME/g" ${TMPDIR}/*
 perl -pi -e "s/Xxsolverxx/$Solvername/g" ${TMPDIR}/*
 perl -pi -e "s/xxsolverxx/$solvername/g" ${TMPDIR}/*
-echo ""
-echo "Create temporary files:"
-echo ""
-ls ${TMPDIR}/solver.cpp
-ls ${TMPDIR}/solver.hpp
-ls ${TMPDIR}/solver_kernels.hpp
-ls ${TMPDIR}/solver_reference_kernels.cpp
-ls ${TMPDIR}/solver_cpu_kernels.cpp
-ls ${TMPDIR}/solver_gpu_kernels.cu
-echo ""
-ls ${TMPDIR}/solver_core_test.cpp
-ls ${TMPDIR}/solver_reference_test.cpp
-ls ${TMPDIR}/solver_cpu_test.cpp
-ls ${TMPDIR}/solver_gpu_test.cpp
-echo ""
+echo -e "\nCreating temporary files:"
+for i in "${TEMPLATE_FILES[@]}"
+do
+    ls ${TMPDIR}/$i
+done
 
 if [ $execute == 1 ]
 then
-    echo "renaming and distributing files"
+    echo -e "\nRenaming and distributing files"
     # rename and distribute the files to the right location
     # for each file, make sure it does not exist yet
-    
-    # first the kernel and header files
-    if [ ! -f ${GINKGO_ROOT_DIR}/core/solver/${solvername}.cpp ]; then
-        cp ${TMPDIR}/solver.cpp ${GINKGO_ROOT_DIR}/core/solver/${solvername}.cpp
-    else
-        echo "Error: file ${GINKGO_ROOT_DIR}/core/solver/${solvername}.cpp exists"
-        echo "Remove file first if you want to replace it."
-        read -p ""
-    fi
-    if [ ! -f ${GINKGO_ROOT_DIR}/core/solver/${solvername}.hpp ]; then
-        cp ${TMPDIR}/solver.hpp ${GINKGO_ROOT_DIR}/core/solver/${solvername}.hpp
-    else
-        echo "Error: file ${GINKGO_ROOT_DIR}/core/solver/${solvername}.hpp exists"
-        echo "Remove file first if you want to replace it."
-        read -p ""
-    fi
-    if [ ! -f ${GINKGO_ROOT_DIR}/core/solver/${solvername}_kernels.hpp ]; then
-        cp ${TMPDIR}/solver_kernels.hpp ${GINKGO_ROOT_DIR}/core/solver/${solvername}_kernels.hpp
-    else
-        echo "Error: file ${GINKGO_ROOT_DIR}/core/solver/${solvername}_kernels.hpp exists"
-        echo "Remove file first if you want to replace it."
-        read -p ""
-    fi
-    if [ ! -f ${GINKGO_ROOT_DIR}/reference/solver/${solvername}_kernels.cpp ]; then
-        cp ${TMPDIR}/solver_reference_kernels.cpp ${GINKGO_ROOT_DIR}/reference/solver/${solvername}_kernels.cpp
-    else
-        echo "Error: file ${GINKGO_ROOT_DIR}/reference/solver/${solvername}_kernels.cpp exists"
-        echo "Remove file first if you want to replace it."
-        read -p ""
-    fi
-    if [ ! -f ${GINKGO_ROOT_DIR}/cpu/solver/${solvername}_kernels.cpp ]; then
-        cp ${TMPDIR}/solver_cpu_kernels.cpp ${GINKGO_ROOT_DIR}/cpu/solver/${solvername}_kernels.cpp
-    else
-        echo "Error: file ${GINKGO_ROOT_DIR}/cpu/solver/${solvername}_kernels.cpp exists"
-        echo "Remove file first if you want to replace it."
-        read -p ""
-    fi
-    if [ ! -f ${GINKGO_ROOT_DIR}/gpu/solver/${solvername}_kernels.cu ]; then
-        cp ${TMPDIR}/solver_gpu_kernels.cu ${GINKGO_ROOT_DIR}/gpu/solver/${solvername}_kernels.cu
-    else
-        echo "Error: file ${GINKGO_ROOT_DIR}/gpu/solver/${solvername}_kernels.cu exists"
-        echo "Remove file first if you want to replace it."
-        read -p ""
-    fi
-#    cp -n ${TMPDIR}/solver.cpp ${GINKGO_ROOT_DIR}/core/solver/${solvername}.cpp
-#    cp -n ${TMPDIR}/solver.hpp ${GINKGO_ROOT_DIR}/core/solver/${solvername}.hpp
-#    cp -n ${TMPDIR}/solver_kernels.hpp ${GINKGO_ROOT_DIR}/core/solver/${solvername}_kernels.hpp
-#    cp -n ${TMPDIR}/solver_reference_kernels.cpp ${GINKGO_ROOT_DIR}/reference/solver/${solvername}_kernels.cpp
-#    cp -n ${TMPDIR}/solver_cpu_kernels.cpp ${GINKGO_ROOT_DIR}/cpu/solver/${solvername}_kernels.cpp
-#    cp -n ${TMPDIR}/solver_gpu_kernels.cu ${GINKGO_ROOT_DIR}/gpu/solver/${solvername}_kernels.cu
-    
-    # now the unit tests
-    if [ ! -f ${GINKGO_ROOT_DIR}/core/test/solver/${solvername}.cpp ]; then
-        cp ${TMPDIR}/solver_core_test.cpp ${GINKGO_ROOT_DIR}/core/test/solver/${solvername}.cpp
-    else
-        echo "Error: file ${GINKGO_ROOT_DIR}/core/test/solver/${solvername}.cpp exists"
-        echo "Remove file first if you want to replace it."
-        read -p ""
-    fi
-    if [ ! -f ${GINKGO_ROOT_DIR}/reference/test/solver/${solvername}_kernels.cpp ]; then
-        cp ${TMPDIR}/solver_reference_test.cpp ${GINKGO_ROOT_DIR}/reference/test/solver/${solvername}_kernels.cpp
-    else
-        echo "Error: file ${GINKGO_ROOT_DIR}/reference/test/solver/${solvername}_kernels.cpp"
-        echo "Remove file first if you want to replace it."
-        read -p ""
-    fi
-    if [ ! -f ${GINKGO_ROOT_DIR}/cpu/test/solver/${solvername}_kernels.cpp ]; then
-        cp ${TMPDIR}/solver_cpu_test.cpp ${GINKGO_ROOT_DIR}/cpu/test/solver/${solvername}_kernels.cpp
-    else
-        echo "Error: file ${GINKGO_ROOT_DIR}/cpu/test/solver/${solvername}_kernels.cpp"
-        echo "Remove file first if you want to replace it."
-        read -p ""
-    fi
-    if [ ! -f ${GINKGO_ROOT_DIR}/gpu/test/solver/${solvername}_kernels.cpp ]; then
-        cp ${TMPDIR}/solver_gpu_test.cpp ${GINKGO_ROOT_DIR}/gpu/test/solver/${solvername}_kernels.cpp
-    else
-        echo "Error: file ${GINKGO_ROOT_DIR}/gpu/test/solver/${solvername}_kernels.cpp"
-        echo "Remove file first if you want to replace it."
-        read -p ""
-    fi
-#    cp -n ${TMPDIR}/solver_core_test.cpp ${GINKGO_ROOT_DIR}/core/test/solver/${solvername}.cpp
-#    cp -n ${TMPDIR}/solver_reference_test.cpp ${GINKGO_ROOT_DIR}/reference/test/solver/${solvername}_kernels.cpp
-#    cp -n ${TMPDIR}/solver_cpu_test.cpp ${GINKGO_ROOT_DIR}/cpu/test/solver/${solvername}_kernels.cpp
-#    cp -n ${TMPDIR}/solver_gpu_test.cpp ${GINKGO_ROOT_DIR}/gpu/test/solver/${solvername}_kernels.cpp
-    
-    
-    # clean up temporary folder
+    for (( i=1; i<${#TEMPLATE_FILES[@]}+1; i++ ))
+    do
+        filename=$(echo ${TEMPLATE_FILES[$i-1]} | sed "s/solver/${solvername}/")
+        destpath=${TEMPLATE_FILES_LOCATIONS[$i-1]}/$filename
+        if [ ! -f ${GINKGO_ROOT_DIR}/$destpath ]; then
+            cp ${TMPDIR}/${TEMPLATE_FILES[$i-1]} ${GINKGO_ROOT_DIR}/$destpath
+        else
+            echo -e "Error: file ${GINKGO_ROOT_DIR}/$destpath exists"
+            echo -e "Remove file first if you want to replace it."
+            read -p ""
+        fi
+    done
+
+
+    echo -e "cleaning up temporary files."
+    rm -rf ${TMPDIR}
 else
-    echo ""
+    echo -e "\nNo file was copied because --dry-run was used"
+    echo -e "You can inspect the generated solver files in ${TMPDIR}."
 fi
-
-
 
 if [ -f todo_${solvername}.txt ]; then
     rm todo_${solvername}.txt
 fi
 
-echo "Summary:"                                                                 | tee -a todo_${solvername}.txt
-echo "Created solver file"                                                      | tee -a todo_${solvername}.txt
-echo "ginkgo/core/solver/${solvername}.cpp"                                        | tee -a todo_${solvername}.txt
-echo "    This is where the ${solvername} algorithm needs to be implemented."      | tee -a todo_${solvername}.txt
-echo ""                                                                         | tee -a todo_${solvername}.txt
-echo "Created class header"                                                     | tee -a todo_${solvername}.txt
-echo "ginkgo/core/solver/${solvername}.hpp"                                        | tee -a todo_${solvername}.txt
-echo "    This is where the ${solvername} class functions need to be implemented." | tee -a todo_${solvername}.txt
-echo ""                                                                         | tee -a todo_${solvername}.txt
-echo "Created kernel header"                                                    | tee -a todo_${solvername}.txt
-echo "ginkgo/core/solver/${solvername}_kernels.hpp"                                | tee -a todo_${solvername}.txt
-echo "    This is where the algorithm-specific kernels need to be added."       | tee -a todo_${solvername}.txt
-echo ""                                                                         | tee -a todo_${solvername}.txt
-echo ""                                                                         | tee -a todo_${solvername}.txt
-echo "Created kernel file"                                                      | tee -a todo_${solvername}.txt
-echo "ginkgo/reference/solver/${solvername}_kernels.cpp"                           | tee -a todo_${solvername}.txt
-echo "    Reference kernels for ${solvername} need to be implemented here."        | tee -a todo_${solvername}.txt
-echo ""                                                                         | tee -a todo_${solvername}.txt
-echo "Created kernel file"                                                      | tee -a todo_${solvername}.txt
-echo "ginkgo/cpu/solver/${solvername}_kernels.cpp"                                 | tee -a todo_${solvername}.txt
-echo "    CPU kernels for ${solvername} need to be implemented here."              | tee -a todo_${solvername}.txt
-echo ""                                                                         | tee -a todo_${solvername}.txt
-echo "Created kernel file"                                                      | tee -a todo_${solvername}.txt
-echo "ginkgo/gpu/solver/${solvername}_kernels.cu"                                  | tee -a todo_${solvername}.txt
-echo "    GPU kernels for ${solvername} need to be implemented here."              | tee -a todo_${solvername}.txt
-echo ""                                                                         | tee -a todo_${solvername}.txt
-echo ""                                                                         | tee -a todo_${solvername}.txt
-echo "Created unit tests for ${solvername} solver in:"                             | tee -a todo_${solvername}.txt
-echo "ginkgo/core/test/solver/${solvername}.cpp"                                   | tee -a todo_${solvername}.txt
-echo ""                                                                         | tee -a todo_${solvername}.txt
-echo "Created unit tests for ${solvername} reference kernels in"                   | tee -a todo_${solvername}.txt
-echo "ginkgo/reference/test/solver/${solvername}_kernels.cpp"                      | tee -a todo_${solvername}.txt
-echo ""                                                                         | tee -a todo_${solvername}.txt
-echo "Created unit tests for ${solvername} CPU kernels in"                         | tee -a todo_${solvername}.txt
-echo "ginkgo/cpu/test/solver/${solvername}_kernels.cpp"                            | tee -a todo_${solvername}.txt
-echo ""                                                                         | tee -a todo_${solvername}.txt
-echo "Created unit tests for ${solvername} GPU kernels in"                         | tee -a todo_${solvername}.txt
-echo "ginkgo/gpu/test/solver/${solvername}_kernels.cpp"                            | tee -a todo_${solvername}.txt
-echo ""                                                                         | tee -a todo_${solvername}.txt
+echo -e "\nSummary:"                                                                 | tee -a todo_${solvername}.txt
+for (( i=1; i<${#TEMPLATE_FILES[@]}+1; i++ ))
+do
+    filename=$(echo ${TEMPLATE_FILES[$i-1]} | sed "s/solver/${solvername}/")
+    destpath=${TEMPLATE_FILES_LOCATIONS[$i-1]}/$filename
+
+    echo "Created ${TEMPLATE_FILES_TYPES[$i-1]}"         | tee -a todo_${solvername}.txt
+    echo "$destpath"                                     | tee -a todo_${solvername}.txt
+    if [ "${TEMPLATE_FILES_DESCRIPTIONS[$i-1]}" != "" ]
+    then
+        echo -e "\t${TEMPLATE_FILES_DESCRIPTIONS[$i-1]}" | tee -a todo_${solvername}.txt
+    fi
+    echo ""                                              | tee -a todo_${solvername}.txt
+done
+
 echo "All tests have to be modified to the specific solver characteristics."    | tee -a todo_${solvername}.txt
 echo ""                                                                         | tee -a todo_${solvername}.txt
 echo ""                                                                         | tee -a todo_${solvername}.txt
@@ -266,6 +202,3 @@ echo ""                                                                         
 echo ""                                                                         | tee -a todo_${solvername}.txt
 echo "A summary of the required next steps has been written to:"
 echo "todo_${solvername}.txt"
-
-echo -e "cleaning up temporary files."
-rm -rf ${TMPDIR}
