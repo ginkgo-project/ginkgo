a2c4f54a9f8e66a46439d4c05dfc2e684700734d
Pratik Nayak <pratik.nayak@kit.edu>
2018-01-16 15:36:01 +0100

    backup

--- core/solver/cg.cpp
+++ core/solver/cg.cpp
@@ -121,14 +121,14 @@ void Cg<ValueType>::apply(const LinOp *b, LinOp *x) const
     auto p = Vector::create_with_config_of(dense_b);
     auto q = Vector::create_with_config_of(dense_b);
 
-    auto alpha = Vector::create(exec, 1, dense_b->get_num_cols(), 1);
+    auto alpha = Vector::create(exec, dense_b->get_num_cols(), 1, 1);
     auto beta = Vector::create_with_config_of(alpha.get());
     auto prev_rho = Vector::create_with_config_of(alpha.get());
     auto rho = Vector::create_with_config_of(alpha.get());
     auto tau = Vector::create_with_config_of(alpha.get());
 
     auto master_tau =
-        Vector::create(exec->get_master(), 1, dense_b->get_num_cols(), 1);
+        Vector::create(exec->get_master(), dense_b->get_num_cols(), 1, 1);
     auto starting_tau = Vector::create_with_config_of(master_tau.get());
 
     // TODO: replace this with automatic merged kernel generator
