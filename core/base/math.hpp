12bd9dc032d7f0cdfdf58133876c0a2a95ca0d1b
Pratik Nayak <pratik.nayak@kit.edu>
2018-01-25 15:48:04 +0100

    re-fix

--- core/base/math.hpp
+++ core/base/math.hpp
@@ -162,7 +162,8 @@ GKO_INLINE GKO_ATTRIBUTES constexpr bool is_complex()
 template <typename T>
 GKO_INLINE GKO_ATTRIBUTES constexpr remove_complex<T> squared_norm(const T &x)
 {
-    return std::real(gko::conj(x) * x);
+    using std::real;
+    return real(gko::conj(x) * x);
 }
 
 }  // namespace gko
