008ffd2ece0a3edc096754a53d0281ad066b5949
Goran Flegar <flegar.goran@gmail.com>
2018-01-29 23:22:51 +0100

    Add Identity matrix

---
+++ core/matrix/identity.hpp
@@ -0,0 +1,100 @@
+/*******************************<GINKGO LICENSE>******************************
+Copyright 2017-2018
+
+Karlsruhe Institute of Technology
+Universitat Jaume I
+University of Tennessee
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+   may be used to endorse or promote products derived from this software
+   without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+******************************<GINKGO LICENSE>*******************************/
+
+#ifndef GKO_CORE_MATRIX_IDENTITY_HPP_
+#define GKO_CORE_MATRIX_IDENTITY_HPP_
+
+
+#include "core/base/lin_op.hpp"
+
+
+namespace gko {
+namespace matrix {
+
+
+/**
+ * This class is a utility which efficiently implements the identity matrix (a
+ * linear operator which maps each vector to itself).
+ *
+ * Thus, objects of the Identity class always represent a square matrix, and
+ * don't require any storage for their values. The apply method is implemented
+ * as a simple copy (or a linear combination).
+ *
+ * @note This class is useful when composing it with other operators. For
+ *       example, it can be used instead of a preconditioner in Krylov solvers,
+ *       if one wants to run a "plain" solver, without using a preconditioner.
+ *
+ * @tparam ValueType  precision of matrix elements
+ */
+template <typename ValueType = default_precision>
+class Identity : public BasicLinOp<Identity<ValueType>> {
+    friend class BasicLinOp<Identity>;
+
+public:
+    using BasicLinOp<Identity>::create;
+    using BasicLinOp<Identity>::convert_to;
+    using BasicLinOp<Identity>::move_to;
+
+    using value_type = ValueType;
+
+    void apply(const LinOp *b, LinOp *x) const override;
+
+    void apply(const LinOp *alpha, const LinOp *b, const LinOp *beta,
+               LinOp *x) const override;
+
+protected:
+    /**
+     * Creates an empty Identity matrix.
+     *
+     * @param exec  Executor associated to the matrix
+     */
+    explicit Identity(std::shared_ptr<const Executor> exec)
+        : BasicLinOp<Identity>(exec, 0, 0, 0)
+    {}
+
+    /**
+     * Creates an Identity matrix of the specified size.
+     *
+     * @param dimension  size of the matrix
+     */
+    Identity(std::shared_ptr<const Executor> exec, size_type dimension)
+        : BasicLinOp<Identity>(exec, dimension, dimension, 0)
+    {}
+};
+
+
+}  // namespace matrix
+}  // namespace gko
+
+
+#endif  // GKO_CORE_MATRIX_IDENTITY_HPP_
