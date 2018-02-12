97b9bac7917c7af86f162a695889551dc2949028
Pratik Nayak <pratik.nayak@kit.edu>
2018-02-12 11:22:08 +0100

    Improve the reference test

--- reference/test/matrix/csr_kernels.cpp
+++ reference/test/matrix/csr_kernels.cpp
@@ -190,62 +190,19 @@ TEST_F(Csr, MovesToDense)
 
 TEST_F(Csr, SquareMtxIsTransposable)
 {
-    auto mtx2 = gko::matrix::Csr<>::create(mtx->get_executor(), 3, 3, 6);
-    Mtx::value_type *v_orig = mtx2->get_values();
-    Mtx::index_type *c_orig = mtx2->get_col_idxs();
-    Mtx::index_type *r_orig = mtx2->get_row_ptrs();
-    /*
-     * 1   3   2
-     * 0   5   0
-     * 0  1.5  0
-     */
-    r_orig[0] = 0;
-    r_orig[1] = 3;
-    r_orig[2] = 4;
-    r_orig[3] = 6;
-    c_orig[0] = 0;
-    c_orig[1] = 1;
-    c_orig[2] = 2;
-    c_orig[3] = 1;
-    c_orig[4] = 1;
-    c_orig[5] = 2;
-    v_orig[0] = 1.0;
-    v_orig[1] = 3.0;
-    v_orig[2] = 2.0;
-    v_orig[3] = 5.0;
-    v_orig[4] = 1.5;
-    v_orig[5] = 2.0;
+    auto mtx2 = gko::initialize<gko::matrix::Csr<>>(
+        //clang-format off
+        {{1.0, 3.0, 2.0}, {0.0, 5.0, 0.0}, {0.0, 1.5, 2.0}}, exec);
+    //clang-format on
 
     auto trans = mtx2->transpose();
     auto trans_as_csr = static_cast<gko::matrix::Csr<> *>(trans.get());
 
-    ASSERT_EQ(trans_as_csr->get_num_rows(), 3);
-    ASSERT_EQ(trans_as_csr->get_num_cols(), 3);
-    ASSERT_EQ(trans_as_csr->get_num_stored_elements(), 6);
-    Mtx::value_type *v = trans_as_csr->get_values();
-    Mtx::index_type *c = trans_as_csr->get_col_idxs();
-    Mtx::index_type *r = trans_as_csr->get_row_ptrs();
-    /*
-     * 1   2   0
-     * 3   5  1.5
-     * 0   0   0
-     */
-    EXPECT_EQ(r[0], 0);
-    EXPECT_EQ(r[1], 1);
-    EXPECT_EQ(r[2], 4);
-    EXPECT_EQ(r[3], 6);
-    EXPECT_EQ(c[0], 0);
-    EXPECT_EQ(c[1], 0);
-    EXPECT_EQ(c[2], 1);
-    EXPECT_EQ(c[3], 2);
-    EXPECT_EQ(c[4], 0);
-    EXPECT_EQ(c[5], 2);
-    EXPECT_EQ(v[0], 1.0);
-    EXPECT_EQ(v[1], 3.0);
-    EXPECT_EQ(v[2], 5.0);
-    EXPECT_EQ(v[3], 1.5);
-    EXPECT_EQ(v[4], 2.0);
-    EXPECT_EQ(v[5], 2.0);
+    ASSERT_MTX_NEAR(trans_as_csr,
+                    //clang-format off
+                    l({{1.0, 0.0, 0.0}, {3.0, 5.0, 1.5}, {2.0, 0.0, 2.0}}),
+                    0.0);
+    //clang-format on
 }
 
 
@@ -254,92 +211,34 @@ TEST_F(Csr, NonSquareMtxIsTransposable)
     auto trans = mtx->transpose();
     auto trans_as_csr = static_cast<gko::matrix::Csr<> *>(trans.get());
 
-    ASSERT_EQ(trans_as_csr->get_num_rows(), 3);
-    ASSERT_EQ(trans_as_csr->get_num_cols(), 2);
-    ASSERT_EQ(trans_as_csr->get_num_stored_elements(), 4);
-    Mtx::value_type *v = trans_as_csr->get_values();
-    Mtx::index_type *c = trans_as_csr->get_col_idxs();
-    Mtx::index_type *r = trans_as_csr->get_row_ptrs();
-    /*
-     * 1   0
-     * 3   5
-     * 2   0
-     */
-    EXPECT_EQ(r[0], 0);
-    EXPECT_EQ(r[1], 1);
-    EXPECT_EQ(r[2], 3);
-    EXPECT_EQ(r[3], 4);
-    EXPECT_EQ(c[0], 0);
-    EXPECT_EQ(c[1], 0);
-    EXPECT_EQ(c[2], 1);
-    EXPECT_EQ(c[3], 0);
-    EXPECT_EQ(v[0], 1.0);
-    EXPECT_EQ(v[1], 3.0);
-    EXPECT_EQ(v[2], 5.0);
-    EXPECT_EQ(v[3], 2.0);
+    ASSERT_MTX_NEAR(trans_as_csr,
+                    //clang-format off
+                    l({{1.0, 0.0}, {3.0, 5.0}, {2.0, 0.0}}), 0.0);
+    //clang-format on
 }
 
 
 TEST_F(Csr, MtxIsConjugateTransposable)
 {
-    auto mtx2 = gko::matrix::Csr<std::complex<double>>::create(
-        mtx->get_executor(), 3, 3, 6);
-    ComplexMtx::value_type *v_orig = mtx2->get_values();
-    ComplexMtx::index_type *c_orig = mtx2->get_col_idxs();
-    ComplexMtx::index_type *r_orig = mtx2->get_row_ptrs();
-    /*
-     * 1+2i   3    2
-     * 0   5-3.5i  0
-     * 0     1.5i  2
-     */
-    r_orig[0] = 0;
-    r_orig[1] = 3;
-    r_orig[2] = 4;
-    r_orig[3] = 6;
-    c_orig[0] = 0;
-    c_orig[1] = 1;
-    c_orig[2] = 2;
-    c_orig[3] = 1;
-    c_orig[4] = 1;
-    c_orig[5] = 2;
-    v_orig[0] = 1.0 + 2.0 * i;
-    v_orig[1] = 3.0;
-    v_orig[2] = 2.0;
-    v_orig[3] = 5.0 - 3.5 * i;
-    v_orig[4] = 1.5 * i;
-    v_orig[5] = 2.0;
+    auto mtx2 = gko::initialize<gko::matrix::Csr<std::complex<double>>>(
+        //clang-format off
+        {{1.0 + 2.0 * i, 3.0 + 0.0 * i, 2.0 + 0.0 * i},
+         {0.0 + 0.0 * i, 5.0 - 3.5 * i, 0.0 + 0.0 * i},
+         {0.0 + 0.0 * i, 0.0 + 1.5 * i, 2.0 + 0.0 * i}},
+        exec);
+    //clang-format on
 
     auto trans = mtx2->conj_transpose();
     auto trans_as_csr =
         static_cast<gko::matrix::Csr<std::complex<double>> *>(trans.get());
 
-    ASSERT_EQ(trans_as_csr->get_num_rows(), 3);
-    ASSERT_EQ(trans_as_csr->get_num_cols(), 3);
-    ASSERT_EQ(trans_as_csr->get_num_stored_elements(), 6);
-    ComplexMtx::value_type *v = trans_as_csr->get_values();
-    ComplexMtx::index_type *c = trans_as_csr->get_col_idxs();
-    ComplexMtx::index_type *r = trans_as_csr->get_row_ptrs();
-    /*
-     * 1-2i   0      0
-     * 3   5+3.5i  -1.5i
-     * 2      0      2
-     */
-    EXPECT_EQ(r[0], 0);
-    EXPECT_EQ(r[1], 1);
-    EXPECT_EQ(r[2], 4);
-    EXPECT_EQ(r[3], 6);
-    EXPECT_EQ(c[0], 0);
-    EXPECT_EQ(c[1], 0);
-    EXPECT_EQ(c[2], 1);
-    EXPECT_EQ(c[3], 2);
-    EXPECT_EQ(c[4], 0);
-    EXPECT_EQ(c[5], 2);
-    EXPECT_EQ(v[0], 1.0 - 2.0 * i);
-    EXPECT_EQ(v[1], 3.0);
-    EXPECT_EQ(v[2], 5.0 + 3.5 * i);
-    EXPECT_EQ(v[3], -1.5 * i);
-    EXPECT_EQ(v[4], 2.0);
-    EXPECT_EQ(v[5], 2.0);
+    ASSERT_MTX_NEAR(trans_as_csr,
+                    //clang-format off
+                    l({{1.0 - 2.0 * i, 0.0 + 0.0 * i, 0.0 + 0.0 * i},
+                       {3.0 + 0.0 * i, 5.0 + 3.5 * i, 0.0 - 1.5 * i},
+                       {2.0 + 0.0 * i, 0.0 + 0.0 * i, 2.0 + 0.0 * i}}),
+                    0.0);
+    //clang-format on
 }
 
 
