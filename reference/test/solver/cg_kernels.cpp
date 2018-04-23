50058c256cbebb260fc721000305f771271254f7
Thomas Grützmacher <thogru.kit@gmx.de>
2018-04-23 18:57:32 +0200

    Added tests for the remaining krylov solver. Unfortunately, they do not detect divergence and (all) need to be changed.

--- reference/test/solver/cg_kernels.cpp
+++ reference/test/solver/cg_kernels.cpp
@@ -53,12 +53,23 @@ protected:
         : exec(gko::ReferenceExecutor::create()),
           mtx(gko::initialize<Mtx>(
               {{2, -1.0, 0.0}, {-1.0, 2, -1.0}, {0.0, -1.0, 2}}, exec)),
-          cg_factory(gko::solver::CgFactory<>::create(exec, 4, 1e-15))
+          cg_factory(gko::solver::CgFactory<>::create(exec, 4, 1e-15)),
+          mtx_big(gko::initialize<Mtx>(
+              {{8828.0, 2673.0, 4150.0, -3139.5, 3829.5, 5856.0},
+               {2673.0, 10765.5, 1805.0, 73.0, 1966.0, 3919.5},
+               {4150.0, 1805.0, 6472.5, 2656.0, 2409.5, 3836.5},
+               {-3139.5, 73.0, 2656.0, 6048.0, 665.0, -132.0},
+               {3829.5, 1966.0, 2409.5, 665.0, 4240.5, 4373.5},
+               {5856.0, 3919.5, 3836.5, -132.0, 4373.5, 5678.0}},
+              exec)),
+          cg_factory_big(gko::solver::CgFactory<>::create(exec, 100, 1e-15))
     {}
 
     std::shared_ptr<const gko::Executor> exec;
     std::shared_ptr<Mtx> mtx;
+    std::shared_ptr<Mtx> mtx_big;
     std::unique_ptr<gko::solver::CgFactory<>> cg_factory;
+    std::unique_ptr<gko::solver::CgFactory<>> cg_factory_big;
 };
 
 
@@ -111,6 +122,105 @@ TEST_F(Cg, SolvesMultipleStencilSystemsUsingAdvancedApply)
     solver->apply(alpha.get(), b.get(), beta.get(), x.get());
 
     ASSERT_MTX_NEAR(x, l({{1.5, 1.0}, {5.0, 0.0}, {2.0, -1.0}}), 1e-14);
+}
+
+TEST_F(Cg, SolvesBigDenseSystemForDivergenceCheck1)
+{
+    auto solver = cg_factory_big->generate(mtx_big);
+    auto b = gko::initialize<Mtx>(
+        {1300083.0, 1018120.5, 906410.0, -42679.5, 846779.5, 1176858.5}, exec);
+    auto x = gko::initialize<Mtx>({0.0, 0.0, 0.0, 0.0, 0.0, 0.0}, exec);
+
+    solver->apply(b.get(), x.get());
+
+    ASSERT_MTX_NEAR(x, l({81.0, 55.0, 45.0, 5.0, 85.0, -10.0}), 1e-10);
+}
+
+
+TEST_F(Cg, SolvesBigDenseSystemForDivergenceCheck2)
+{
+    auto solver = cg_factory_big->generate(mtx_big);
+    auto b = gko::initialize<Mtx>(
+        {886630.5, -172578.0, 684522.0, -65310.5, 455487.5, 607436.0}, exec);
+    auto x = gko::initialize<Mtx>({0.0, 0.0, 0.0, 0.0, 0.0, 0.0}, exec);
+
+    solver->apply(b.get(), x.get());
+
+    ASSERT_MTX_NEAR(x, l({33.0, -56.0, 81.0, -30.0, 21.0, 40.0}), 1e-10);
+}
+
+
+double infNorm(gko::matrix::Dense<> *mat, size_t col = 0)
+{
+    using std::abs;
+    double norm = 0.0;
+    for (size_t i = 0; i < mat->get_num_rows(); ++i) {
+        double absEntry = abs(mat->at(i, col));
+        if (norm < absEntry) norm = absEntry;
+    }
+    return norm;
+}
+
+
+TEST_F(Cg, SolvesMultipleDenseSystemForDivergenceCheck)
+{
+    auto solver = cg_factory_big->generate(mtx_big);
+    auto b1 = gko::initialize<Mtx>(
+        {1300083.0, 1018120.5, 906410.0, -42679.5, 846779.5, 1176858.5}, exec);
+    auto b2 = gko::initialize<Mtx>(
+        {886630.5, -172578.0, 684522.0, -65310.5, 455487.5, 607436.0}, exec);
+
+    auto x1 = gko::initialize<Mtx>({0.0, 0.0, 0.0, 0.0, 0.0, 0.0}, exec);
+    auto x2 = gko::initialize<Mtx>({0.0, 0.0, 0.0, 0.0, 0.0, 0.0}, exec);
+
+    auto bc = Mtx::create(exec, mtx_big->get_num_rows(), 2);
+    auto xc = Mtx::create(exec, mtx_big->get_num_cols(), 2);
+    for (size_t i = 0; i < bc->get_num_rows(); ++i) {
+        bc->at(i, 0) = b1->at(i);
+        bc->at(i, 1) = b2->at(i);
+
+        xc->at(i, 0) = x1->at(i);
+        xc->at(i, 1) = x2->at(i);
+    }
+
+    solver->apply(b1.get(), x1.get());
+    solver->apply(b2.get(), x2.get());
+    solver->apply(bc.get(), xc.get());
+    auto mergedRes = Mtx::create(exec, b1->get_num_rows(), 2);
+    for (size_t i = 0; i < mergedRes->get_num_rows(); ++i) {
+        mergedRes->at(i, 0) = x1->at(i);
+        mergedRes->at(i, 1) = x2->at(i);
+    }
+
+    auto alpha = gko::initialize<Mtx>({1.0}, exec);
+    auto beta = gko::initialize<Mtx>({-1.0}, exec);
+
+    auto residual1 = Mtx::create(exec, b1->get_num_rows(), b1->get_num_cols());
+    residual1->copy_from(b1.get());
+    auto residual2 = Mtx::create(exec, b2->get_num_rows(), b2->get_num_cols());
+    residual2->copy_from(b2.get());
+    auto residualC = Mtx::create(exec, bc->get_num_rows(), bc->get_num_cols());
+    residualC->copy_from(bc.get());
+
+    mtx_big->apply(alpha.get(), x1.get(), beta.get(), residual1.get());
+    mtx_big->apply(alpha.get(), x2.get(), beta.get(), residual2.get());
+    mtx_big->apply(alpha.get(), xc.get(), beta.get(), residualC.get());
+
+    double normS1 = infNorm(residual1.get());
+    double normS2 = infNorm(residual2.get());
+    double normC1 = infNorm(residualC.get(), 0);
+    double normC2 = infNorm(residualC.get(), 1);
+    double normB1 = infNorm(b1.get());
+    double normB2 = infNorm(b2.get());
+
+    // make sure that all combined solutions are as good or better than the
+    // single solutions
+    ASSERT_LE(normC1 / normB1, normS1 / normB1 + 1e-14);
+    ASSERT_LE(normC2 / normB2, normS2 / normB2 + 1e-14);
+
+    // Not sure if this is necessary, the assertions above should cover what is
+    // needed.
+    ASSERT_MTX_NEAR(xc, mergedRes, 1e-14);
 }
 
 
