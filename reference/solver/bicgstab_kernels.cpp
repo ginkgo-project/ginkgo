5532e1e5367b84543e7973297aad80165520db19
Terry Cojean <terry.cojean@kit.edu>
2018-05-04 12:09:18 +0200

    Remove unneeded test_convergence kernel code in bicgstab solver.

--- reference/solver/bicgstab_kernels.cpp
+++ reference/solver/bicgstab_kernels.cpp
@@ -83,33 +83,6 @@ void initialize(std::shared_ptr<const ReferenceExecutor> exec,
 
 GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE(GKO_DECLARE_BICGSTAB_INITIALIZE_KERNEL);
 
-template <typename ValueType>
-void test_convergence(std::shared_ptr<const ReferenceExecutor> exec,
-                      const matrix::Dense<ValueType> *tau,
-                      const matrix::Dense<ValueType> *orig_tau,
-                      remove_complex<ValueType> rel_residual_goal,
-                      uint8 stoppingId, bool setFinalized,
-                      Array<stopping_status> *stop_status, bool *all_converged,
-                      bool *one_changed)
-{
-    *all_converged = true;
-    for (size_type i = 0; i < tau->get_size().num_cols; ++i) {
-        if (abs(tau->at(i)) < rel_residual_goal * abs(orig_tau->at(i))) {
-            stop_status->get_data()[i].converge(stoppingId, setFinalized);
-            *one_changed = true;
-        }
-    }
-    for (size_type i = 0; i < stop_status->get_num_elems(); ++i) {
-        if (!stop_status->get_const_data()[i].has_stopped()) {
-            *all_converged = false;
-            break;
-        }
-    }
-}
-
-GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE(
-    GKO_DECLARE_BICGSTAB_TEST_CONVERGENCE_KERNEL);
-
 
 template <typename ValueType>
 void step_1(std::shared_ptr<const ReferenceExecutor> exec,
