1141d3bbcb275858dcd88849d5e6d54b07fd7522
Terry Cojean <terry.cojean@kit.edu>
2018-05-07 17:17:11 +0200

    Use constexpr in the .cpp file also.

--- core/log/logger.cpp
+++ core/log/logger.cpp
@@ -40,9 +40,9 @@ namespace log {
 
 
 const Logger::mask_type Logger::all_events_mask = ~Logger::mask_type{};
-const Logger::mask_type Logger::iteration_complete_mask;
-const Logger::mask_type Logger::apply_mask;
-const Logger::mask_type Logger::converged_mask;
+constexpr Logger::mask_type Logger::iteration_complete_mask;
+constexpr Logger::mask_type Logger::apply_mask;
+constexpr Logger::mask_type Logger::converged_mask;
 
 
 }  // namespace log
