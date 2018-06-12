99484d1a3a9ba88810b2335a1abb93134c37bc77
Tobias Ribizel <tobias.ribizel@googlemail.com>
2018-06-12 14:02:04 +0200

    restructure doxygen configuration files
    * Only two levels of configuration: general and version-specific
      (no more output-specific configuration file)
    * Use explicit pages directory
    * Shorter PDF generated in LaTeX batch mode

--- doc/helpers.cmake
+++ doc/helpers.cmake
@@ -6,19 +6,18 @@ function(configure_to_string in variable)
   set(${variable} "${str_conf}" PARENT_SCOPE)
 endfunction()
 
-# writes the concatenated configured files <in1,2,3>
+# writes the concatenated configured files <in1,2>
 # in <base_in> into <out>
-function(doc_conf_concat base_in in1 in2 in3 out)
+function(doc_conf_concat base_in in1 in2 out)
   configure_to_string("${base_in}/${in1}" s1)
   configure_to_string("${base_in}/${in2}" s2)
-  configure_to_string("${base_in}/${in3}" s3)
-  string(CONCAT so "${s1}" "${s2}" "${s3}")
+  string(CONCAT so "${s1}" "\n" "${s2}")
   file(WRITE "${out}" "${so}")
 endfunction()
 
 # adds a pdflatex build step
 function(doc_pdf name path)
-  add_custom_command(TARGET ${name} POST_BUILD
+  add_custom_command(TARGET "${name}" POST_BUILD
     COMMAND make
     COMMAND "${CMAKE_COMMAND}" -E copy refman.pdf
     "${CMAKE_CURRENT_BINARY_DIR}/${name}.pdf"
@@ -30,13 +29,13 @@ endfunction()
 
 # generates the documentation named <name> with the additional
 # config file <in> in <pdf/html> format
-function(doc_gen name in pdf)
-  set(DIR_PAGES "${CMAKE_CURRENT_SOURCE_DIR}/pages")
+function(doc_gen name in pdf mainpage)
   set(DIR_SCRIPT "${CMAKE_CURRENT_SOURCE_DIR}/scripts")
   set(DIR_BASE "${CMAKE_CURRENT_SOURCE_DIR}/..")
   set(DIR_OUT "${CMAKE_CURRENT_BINARY_DIR}/${name}")
+  set(MAINPAGE "${CMAKE_CURRENT_SOURCE_DIR}/pages/${mainpage}")
   set(doxyfile "${CMAKE_CURRENT_BINARY_DIR}/Doxyfile-${name}")
-  add_custom_target(${name} ALL
+  add_custom_target("${name}" ALL
     #DEPEND "${doxyfile}.stamp" Doxyfile.in ${in} ${in2}
     COMMAND "${DOXYGEN_EXECUTABLE}" "${doxyfile}"
     #COMMAND "${CMAKE_COMMAND}" cmake -E touch "${doxyfile}.stamp"
@@ -44,12 +43,9 @@ function(doc_gen name in pdf)
     VERBATIM
     )
   if(pdf)
-    set(in2 "Doxyfile-pdf.in")
-    doc_pdf(${name} "${DIR_OUT}")
-  else()
-    set(in2 "Doxyfile-html.in")
+    doc_pdf("${name}" "${DIR_OUT}")
   endif()
   doc_conf_concat("${CMAKE_CURRENT_SOURCE_DIR}/conf"
-    Doxyfile.in ${in2} ${in} "${doxyfile}"
+    Doxyfile.in "${in}" "${doxyfile}"
     )
 endfunction()
