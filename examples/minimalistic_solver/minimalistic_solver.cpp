baf6cc72696bd52a021816112e0b5248e716bb45
Goran Flegar <flegar.goran@gmail.com>
2018-08-08 19:06:22 +0200

    Add a minimal example for solving a system

---
+++ examples/minimalistic_solver/minimalistic_solver.cpp
@@ -0,0 +1,112 @@
+/*******************************<GINKGO LICENSE>******************************
+Copyright 2017-2018
+
+Karlsruhe Institute of Technology
+Universitat Jaume I
+University of Tennessee
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+   may be used to endorse or promote products derived from this software
+   without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+******************************<GINKGO LICENSE>*******************************/
+
+/*****************************<COMPILATION>***********************************
+The easiest way to build the example solver is to use the script provided:
+./build.sh <PATH_TO_GINKGO_BUILD_DIR>
+
+Ginkgo should be compiled with `-DBUILD_REFERENCE=on` option.
+
+Alternatively, you can setup the configuration manually:
+
+Go to the <PATH_TO_GINKGO_BUILD_DIR> directory and copy the shared
+libraries located in the following subdirectories:
+
+    + core/
+    + core/device_hooks/
+    + reference/
+    + omp/
+    + cuda/
+
+to this directory.
+
+Then compile the file with the following command line:
+
+c++ -std=c++11 -o minimalistic_solver minimalistic_solver.cpp -I../.. \
+    -L. -lginkgo -lginkgo_reference -lginkgo_omp -lginkgo_cuda
+
+(if ginkgo was built in debug mode, append 'd' to every library name)
+
+Now you should be able to run the program using:
+
+env LD_LIBRARY_PATH=.:${LD_LIBRARY_PATH} ./minimalistic_solver
+
+*****************************<COMPILATION>**********************************/
+
+/*****************************<DECSRIPTION>***********************************
+This is a minimal example that solves a system with Ginkgo. The matrix, right
+hand side and initial guess are read from standard input, and the result is
+written to standard output. The system matrix is stored in CSR format, and the
+system solved using the CG method, preconditioned with the Block-Jacobi
+preconditioner. All computations are done on the GPU.
+
+The easiest way to use the example data from the data/ folder is to concatenate
+the matrix, the right hand side, and the initial solution (in that exact order),
+and pipe the result to the minimalistic_solver executable:
+
+cat data/A.mtx data/b.mtx data/x0.mtx | ./minimalistic_solver
+
+*****************************<DECSRIPTION>**********************************/
+
+#include <ginkgo.hpp>
+#include <iostream>
+
+int main()
+{
+    // Instantiate a CUDA executor
+    auto exec = gko::CudaExecutor::create(0, gko::OmpExecutor::create());
+    // Read data
+    auto A = gko::read<gko::matrix::Csr<>>(std::cin, exec);
+    auto b = gko::read<gko::matrix::Dense<>>(std::cin, exec);
+    auto x = gko::read<gko::matrix::Dense<>>(std::cin, exec);
+    // Create the solver
+    auto solver =
+        gko::solver::Cg<>::Factory::create()
+            .with_preconditioner(
+                gko::preconditioner::BlockJacobiFactory<>::create(exec, 32))
+            .with_criterion(
+                gko::stop::Combined::Factory::create()
+                    .with_criteria(
+                        gko::stop::Iteration::Factory::create()
+                            .with_max_iters(20u)
+                            .on_executor(exec),
+                        gko::stop::ResidualNormReduction<>::Factory::create()
+                            .with_reduction_factor(1e-15)
+                            .on_executor(exec))
+                    .on_executor(exec))
+            .on_executor(exec);
+    // Solve system
+    solver->generate(give(A))->apply(lend(b), lend(x));
+    // Write result
+    write(std::cout, lend(x));
+}
