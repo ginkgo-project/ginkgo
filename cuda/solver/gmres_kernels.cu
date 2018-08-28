54bd6a51f5500b48586c81fe3f1aa8b49ad73017
yanjen <yanjen224@gmail.com>
2018-08-28 16:26:20 +0200

    add initialize 1 cuda kernel

--- cuda/solver/gmres_kernels.cu
+++ cuda/solver/gmres_kernels.cu
@@ -40,6 +40,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include "cuda/base/math.hpp"
 #include "cuda/base/types.hpp"
+#include "cuda/components/reduction.cuh"
 
 
 namespace gko {
@@ -48,16 +49,58 @@ namespace cuda {
 namespace gmres {
 
 
+constexpr int default_block_size = 512;
+
+
 template <typename ValueType>
-void initialize_1(std::shared_ptr<const CudaExecutor> exec,
-                  const matrix::Dense<ValueType> *b,
-                  matrix::Dense<ValueType> *b_norm,
-                  matrix::Dense<ValueType> *residual,
-                  matrix::Dense<ValueType> *givens_sin,
-                  matrix::Dense<ValueType> *givens_cos,
-                  Array<stopping_status> *stop_status, const int krylov_dim)
+__global__ __launch_bounds__(default_block_size) void initialize_1_kernel(
+    size_type num_rows, size_type num_cols, size_type stride,
+    size_type krylov_dim, const ValueType *__restrict__ b,
+    ValueType *__restrict__ b_norm, ValueType *__restrict__ residual,
+    ValueType *__restrict__ givens_sin, ValueType *__restrict__ givens_cos,
+    stopping_status *__restrict__ stop_status)
 {
-    NOT_IMPLEMENTED;
+    const auto idx =
+        static_cast<size_type>(blockDim.x) * blockIdx.x + threadIdx.x;
+
+    if (idx < num_cols) {
+        b_norm[idx] = zero<ValueType>();
+        // TODO: implement reduction for b_norm
+        for (size_type i = 0; i < num_rows; ++i) {
+            b_norm[idx] += b[i * stride + idx] * b[i * stride + idx];
+        }
+        b_norm[idx] = sqrt(b_norm[idx]);
+        stop_status[idx].reset();
+    }
+
+    if (idx < num_rows * stride) {
+        residual[idx] = b[idx];
+    }
+
+    if (idx < krylov_dim * num_cols) {
+        givens_sin[idx] = zero<ValueType>();
+        givens_cos[idx] = zero<ValueType>();
+    }
+}
+
+
+template <typename ValueType>
+void initialize_1(
+    std::shared_ptr<const CudaExecutor> exec, const matrix::Dense<ValueType> *b,
+    matrix::Dense<ValueType> *b_norm, matrix::Dense<ValueType> *residual,
+    matrix::Dense<ValueType> *givens_sin, matrix::Dense<ValueType> *givens_cos,
+    Array<stopping_status> *stop_status, const size_type krylov_dim)
+{
+    const dim3 block_size(default_block_size, 1, 1);
+    const dim3 grid_size(ceildiv(b->get_size()[0], default_block_size),
+                         b->get_size()[1], 1);
+    initialize_1_kernel<<<grid_size, block_size, 0, 0>>>(
+        b->get_size()[0], b->get_size()[1], b->get_stride(), krylov_dim,
+        as_cuda_type(b->get_const_values()), as_cuda_type(b_norm->get_values()),
+        as_cuda_type(residual->get_values()),
+        as_cuda_type(givens_sin->get_values()),
+        as_cuda_type(givens_cos->get_values()),
+        as_cuda_type(stop_status->get_data()));
 }
 
 GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE(GKO_DECLARE_GMRES_INITIALIZE_1_KERNEL);
@@ -69,7 +112,7 @@ void initialize_2(std::shared_ptr<const CudaExecutor> exec,
                   matrix::Dense<ValueType> *residual_norm,
                   matrix::Dense<ValueType> *residual_norms,
                   matrix::Dense<ValueType> *krylov_bases,
-                  Array<size_type> *final_iter_nums, const int krylov_dim)
+                  Array<size_type> *final_iter_nums, const size_type krylov_dim)
 {
     NOT_IMPLEMENTED;
 }
