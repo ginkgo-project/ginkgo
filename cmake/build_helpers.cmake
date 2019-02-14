e228e2bf58d98bcea2086a97fd7ec2349b313da2
tcojean <35342290+tcojean@users.noreply.github.com>
2019-02-14 15:18:24 +0100

    Change alignment of build_helpers.cmake

--- cmake/build_helpers.cmake
+++ cmake/build_helpers.cmake
@@ -2,10 +2,10 @@ function(ginkgo_default_includes name)
     # set include path depending on used interface
     target_include_directories("${name}"
         PUBLIC
-				$<BUILD_INTERFACE:${Ginkgo_BINARY_DIR}/include>
-				$<BUILD_INTERFACE:${Ginkgo_SOURCE_DIR}/include>
-				$<BUILD_INTERFACE:${Ginkgo_SOURCE_DIR}>
-        $<INSTALL_INTERFACE:include>
+            $<BUILD_INTERFACE:${Ginkgo_BINARY_DIR}/include>
+            $<BUILD_INTERFACE:${Ginkgo_SOURCE_DIR}/include>
+            $<BUILD_INTERFACE:${Ginkgo_SOURCE_DIR}>
+            $<INSTALL_INTERFACE:include>
         )
 endfunction()
 
