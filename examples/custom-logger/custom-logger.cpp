03907311b85f69a259e814fc7430314fa45b7b7b
Terry Cojean <terry.cojean@kit.edu>
2019-04-15 16:58:49 +0200

    Add a new example called `custom-logger`.
    The purpose of this example is to show the users how to customize Ginkgo by
    adding a new logger, which is useful and more efficient for application specific
    problems. This is also one of the most basic (and simple) ways of customizing
    Ginkgo, therefore this is a good entry level example.
    
    This example simply prints a table of the recurrent residual norm against the
    real residual norm.
    
    This example is documented as much as well as the `simple-solver` example for
    the user's convenience.

--- examples/custom-logger/custom-logger.cpp
+++ examples/custom-logger/custom-logger.cpp
@@ -37,10 +37,144 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 // Add the fstream header to read from data from files.
 #include <fstream>
+// Add the C++ iomanip header to prettify the output.
+#include <iomanip>
 // Add the C++ iostream header to output information to the console.
 #include <iostream>
 // Add the string manipulation header to handle strings.
 #include <string>
+// Add the vector header for storing the logger's data
+#include <vector>
+
+// Utility function which gets the scalar value of a Ginkgo gko::matrix::Dense
+// matrix representing the norm of a vector.
+template <typename ValueType>
+double get_norm(const gko::matrix::Dense<ValueType> *norm)
+{
+    // Put the value on CPU thanks to the master executor
+    auto cpu_norm = clone(norm->get_executor()->get_master(), norm);
+    // Return the scalar value contained at position (0, 0)
+    return cpu_norm->at(0, 0);
+}
+
+// Utility function which computes the norm of a Ginkgo gko::matrix::Dense
+// vector.
+template <typename ValueType>
+double compute_norm(const gko::matrix::Dense<ValueType> *b)
+{
+    // Get the executor of the vector
+    auto exec = b->get_executor();
+    // Initialize a result scalar containing the value 0.0.
+    auto b_norm = gko::initialize<gko::matrix::Dense<ValueType>>({0.0}, exec);
+    // Use the dense `compute_norm2` function to compute the norm.
+    b->compute_norm2(lend(b_norm));
+    // Use the other utility function to return the norm contained in `b_norm``
+    return get_norm(lend(b_norm));
+}
+
+// Custom logger class which intercepts the residual norm scalar and solution
+// vector in order to print a table of real vs recurrent (internal to the
+// solvers) residual norms.
+template <typename ValueType>
+struct ResidualLogger : gko::log::Logger {
+    // Output the logger's data in a table format
+    void write() const
+    {
+        // Print a header for the table
+        std::cout << "Recurrent vs real residual norm:" << std::endl;
+        std::cout << '|' << std::setw(10) << "Iteration" << '|' << std::setw(25)
+                  << "Recurrent Residual Norm" << '|' << std::setw(25)
+                  << "Real Residual Norm" << '|' << std::endl;
+        // Print a separation line. Note that for creating `10` caracters
+        // `std::setw()` should be set to `11`.
+        std::cout << '|' << std::setfill('-') << std::setw(11) << '|'
+                  << std::setw(26) << '|' << std::setw(26) << '|'
+                  << std::setfill(' ') << std::endl;
+        // Print the data one by one in the form
+        for (std::size_t i = 0; i < iterations.size(); i++) {
+            std::cout << std::scientific << '|' << std::setw(10)
+                      << iterations[i] << '|' << std::setw(25)
+                      << recurrent_norms[i] << '|' << std::setw(25)
+                      << real_norms[i] << '|' << std::defaultfloat << std::endl;
+        }
+        // Print a separation line
+        std::cout << '|' << std::setfill('-') << std::setw(11) << '|'
+                  << std::setw(26) << '|' << std::setw(26) << '|'
+                  << std::setfill(' ') << std::endl;
+    }
+
+    using gko_dense = gko::matrix::Dense<ValueType>;
+
+    // Customize the logging hook which is called everytime an iteration is
+    // completed
+    void on_iteration_complete(const gko::LinOp *,
+                               const gko::size_type &iteration,
+                               const gko::LinOp *residual,
+                               const gko::LinOp *solution,
+                               const gko::LinOp *residual_norm) const override
+    {
+        // If the solver shares a residual norm, log its value
+        if (residual_norm) {
+            auto dense_norm = gko::as<gko_dense>(residual_norm);
+            // Add the norm to the `recurrent_norms` vector
+            recurrent_norms.push_back(get_norm(dense_norm));
+            // Otherwise, use the recurrent residual vector
+        } else {
+            auto dense_residual = gko::as<gko_dense>(residual);
+            // Compute the residual vector's norm
+            auto norm = compute_norm(gko::lend(dense_residual));
+            // Add the computed norm to the `recurrent_norms` vector
+            recurrent_norms.push_back(norm);
+        }
+
+        // If the solver shares the current solution vector
+        if (solution) {
+            // Store the matrix's executor
+            auto exec = matrix->get_executor();
+            // Create a scalar containing the value 1.0
+            auto one = gko::initialize<gko_dense>({1.0}, exec);
+            // Create a scalar containing the value -1.0
+            auto neg_one = gko::initialize<gko_dense>({-1.0}, exec);
+            // Instantiate a temporary result variable
+            auto res = gko::clone(b);
+            // Compute the real residual vector by calling apply on the system
+            // matrix
+            matrix->apply(gko::lend(one), gko::lend(solution),
+                          gko::lend(neg_one), gko::lend(res));
+
+            // Compute the norm of the residual vector and add it to the
+            // `real_norms` vector
+            real_norms.push_back(compute_norm(gko::lend(res)));
+        } else {
+            // Add to the `real_norms` vector the value -1.0 if it could not be
+            // computed
+            real_norms.push_back(-1.0);
+        }
+
+        // Add the current iteration number to the `iterations` vector
+        iterations.push_back(iteration);
+    }
+
+    // Construct the logger and store the system matrix and b vectors
+    ResidualLogger(std::shared_ptr<const gko::Executor> exec,
+                   const gko::LinOp *matrix, const gko_dense *b)
+        : gko::log::Logger(exec, gko::log::Logger::iteration_complete_mask),
+          matrix{matrix},
+          b{b}
+    {}
+
+private:
+    // Pointer to the system matrix
+    const gko::LinOp *matrix;
+    // Pointer to the right hand sides
+    const gko_dense *b;
+    // Vector which stores all the recurrent residual norms
+    mutable std::vector<ValueType> recurrent_norms{};
+    // Vector which stores all the real residual norms
+    mutable std::vector<ValueType> real_norms{};
+    // Vector which stores all the iteration numbers
+    mutable std::vector<std::size_t> iterations{};
+};
 
 
 int main(int argc, char *argv[])
@@ -113,15 +247,25 @@ int main(int argc, char *argv[])
                     .with_reduction_factor(1e-15)
                     .on(exec))
             .on(exec);
+
+    // Instantiate a ResidualLogger logger.
+    auto logger = std::make_shared<ResidualLogger<double>>(exec, gko::lend(A),
+                                                           gko::lend(b));
+
+    // Add the previously created logger to the solver factory. The logger will
+    // be automatically propagated to all solvers created from this factory.
+    solver_gen->add_logger(logger);
+
     // Generate the solver from the matrix. The solver factory built in the
     // previous step takes a "matrix"(a gko::LinOp to be more general) as an
-    // input. In this case we provide it with a full matrix that we previously
-    // read, but as the solver only effectively uses the apply() method within
-    // the provided "matrix" object, you can effectively create a gko::LinOp
-    // class with your own apply implementation to accomplish more tasks. We
-    // will see an example of how this can be done in the custom-matrix-format
-    // example
+    // input. In this case we provide it with a full matrix that we
+    // previously read, but as the solver only effectively uses the apply()
+    // method within the provided "matrix" object, you can effectively
+    // create a gko::LinOp class with your own apply implementation to
+    // accomplish more tasks. We will see an example of how this can be done
+    // in the custom-matrix-format example
     auto solver = solver_gen->generate(A);
+
 
     // Finally, solve the system. The solver, being a gko::LinOp, can be applied
     // to a right hand side, b to
@@ -131,6 +275,9 @@ int main(int argc, char *argv[])
     // Print the solution to the command line.
     std::cout << "Solution (x): \n";
     write(std::cout, lend(x));
+
+    // Print the table of the residuals obtained from the logger
+    logger->write();
 
     // To measure if your solution has actually converged, you can measure the
     // error of the solution.
