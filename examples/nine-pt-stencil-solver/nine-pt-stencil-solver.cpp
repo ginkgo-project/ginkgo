fc6f2300e434a182e81527bb74b416715f3f356c
Thomas Grützmacher <thomas.gruetzmacher@kit.edu>
2019-04-26 13:26:41 +0200

    Update examples/nine-pt-stencil-solver/nine-pt-stencil-solver.cpp
    Co-Authored-By: keldu <claudius.holeksa@gmail.com>

--- examples/nine-pt-stencil-solver/nine-pt-stencil-solver.cpp
+++ examples/nine-pt-stencil-solver/nine-pt-stencil-solver.cpp
@@ -51,7 +51,7 @@ void generate_stencil_matrix(int dp, int *row_ptrs, int *col_idxs,
                              double *values)
 {
     int pos = 0;
-    size_t dp_2 = dp * dp;
+    const size_t dp_2 = dp * dp;
     row_ptrs[0] = pos;
     for (int k = 0; k < dp; ++k) {
         for (int i = 0; i < dp; ++i) {
