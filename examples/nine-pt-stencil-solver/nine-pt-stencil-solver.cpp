9ada3be376c607bc02178457fcbe42810e999daa
keldu.magnus <git@keldu.de>
2019-04-26 13:13:36 +0200

    no empty doc field

--- examples/nine-pt-stencil-solver/nine-pt-stencil-solver.cpp
+++ examples/nine-pt-stencil-solver/nine-pt-stencil-solver.cpp
@@ -30,10 +30,6 @@ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ******************************<GINKGO LICENSE>*******************************/
 
-/******************************<DESCRIPTION>*********************************
-
-*******************************<DESCRIPTION>*********************************/
-
 #include <array>
 #include <chrono>
 #include <ginkgo/ginkgo.hpp>
