2353b4bb6532d9a49fdfa97fcdc411e42721b172
Hartwig Anzt <hanzt@icl.utk.edu>
2019-06-18 22:52:23 +0200

    typo

--- omp/factorization/par_ilu_kernels.cpp
+++ omp/factorization/par_ilu_kernels.cpp
@@ -149,7 +149,7 @@ void compute_l_u_factors(std::shared_ptr<const OmpExecutor> exec,
     auto vals_l = l_factor->get_values();
     auto vals_u = u_factor->get_values();
     for (size_type iter = 0; iter < iterations; ++iter) {
-        // all elments in the incomplete factors are updated in parallel
+        // all elements in the incomplete factors are updated in parallel
 #pragma omp parallel for
         for (size_type el = 0; el < system_matrix->get_num_stored_elements();
              ++el) {
