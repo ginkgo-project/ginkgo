0ac57a8ee17de638421c7f0a92142cbceabae637
Thomas Grützmacher <thomas.gruetzmacher@kit.edu>
2019-07-02 19:43:52 +0200

    Started implementing CUDA kernels for ParILU

--- cuda/factorization/par_ilu_kernels.cu
+++ cuda/factorization/par_ilu_kernels.cu
@@ -33,8 +33,14 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "core/factorization/par_ilu_kernels.hpp"
 
 
+#include <ginkgo/core/base/array.hpp>
 #include <ginkgo/core/base/exception_helpers.hpp>
 #include <ginkgo/core/matrix/coo.hpp>
+
+
+#include "cuda/base/math.hpp"
+#include "cuda/base/types.hpp"
+#include "cuda/components/prefix_sum.cuh"
 
 
 namespace gko {
@@ -48,11 +54,77 @@ namespace cuda {
 namespace par_ilu_factorization {
 
 
+constexpr int default_block_size{512};
+
+
+namespace kernel {
+
+
+template <typename ValueType, typename IndexType>
+__global__ __launch_bounds__(default_block_size) void count_nnz_per_l_u_row(
+    size_type num_rows, const IndexType *__restrict__ row_ptrs,
+    const IndexType *__restrict__ col_idxs,
+    const ValueType *__restrict__ values,
+    IndexType *__restrict__ l_nnz_previous_row,
+    IndexType *__restrict__ u_nnz_previous_row)
+{
+    const auto global_id = blockDim.x * blockIdx.x + threadIdx.x;
+    const auto row = global_id;
+    if (row == 0) {
+        l_nnz_previous_row[0] = 0;
+        u_nnz_previous_row[0] = 0;
+    }
+    if (row < num_rows) {
+        IndexType l_row_nnz{};
+        IndexType u_row_nnz{};
+        for (auto idx = row_ptrs[row]; idx < row_ptrs[row + 1]; ++idx) {
+            auto col = col_idxs[idx];
+            l_row_nnz += (col <= row) ? 1 : 0;
+            u_row_nnz += (row <= col) ? 1 : 0;
+        }
+        l_nnz_previous_row[row + 1] = l_row_nnz;
+        u_nnz_previous_row[row + 1] = u_row_nnz;
+    }
+}
+
+
+}  // namespace kernel
+
+
 template <typename ValueType, typename IndexType>
 void initialize_row_ptrs_l_u(
     std::shared_ptr<const CudaExecutor> exec,
     const matrix::Csr<ValueType, IndexType> *system_matrix,
-    IndexType *l_row_ptrs, IndexType *u_row_ptrs) GKO_NOT_IMPLEMENTED;
+    IndexType *l_row_ptrs, IndexType *u_row_ptrs)
+{
+    const size_type num_rows{system_matrix->get_size()[0]};
+    const auto row_ptrs = system_matrix->get_const_row_ptrs();
+    const auto col_idxs = system_matrix->get_const_col_idxs();
+    const auto values = system_matrix->get_const_values();
+
+    const dim3 block_size{default_block_size, 1, 1};
+    const dim3 grid_dim{static_cast<uint32>(ceildiv(
+                            num_rows, static_cast<size_type>(block_size.x))),
+                        1, 1};
+
+    kernel::count_nnz_per_l_u_row<<<grid_dim, block_size, 0, 0>>>(
+        num_rows, as_cuda_type(row_ptrs), as_cuda_type(col_idxs),
+        as_cuda_type(values), as_cuda_type(l_row_ptrs),
+        as_cuda_type(u_row_ptrs));
+
+    Array<IndexType> block_sum(exec, grid_dim.x);
+    auto block_sum_ptr = block_sum.get_data();
+
+    start_prefix_sum<default_block_size><<<grid_dim, block_size>>>(
+        num_rows, as_cuda_type(l_row_ptrs), as_cuda_type(block_sum_ptr));
+    finalize_prefix_sum<default_block_size><<<grid_dim, block_size>>>(
+        num_rows, as_cuda_type(l_row_ptrs), as_cuda_type(block_sum_ptr));
+
+    start_prefix_sum<default_block_size><<<grid_dim, block_size>>>(
+        num_rows, as_cuda_type(u_row_ptrs), as_cuda_type(block_sum_ptr));
+    finalize_prefix_sum<default_block_size><<<grid_dim, block_size>>>(
+        num_rows, as_cuda_type(u_row_ptrs), as_cuda_type(block_sum_ptr));
+}
 
 GKO_INSTANTIATE_FOR_EACH_VALUE_AND_INDEX_TYPE(
     GKO_DECLARE_PAR_ILU_INITIALIZE_ROW_PTRS_L_U_KERNEL);
