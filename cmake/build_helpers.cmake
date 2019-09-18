c45ef0fdebb06dde4c1e70da49d0abcc716b636e
Yuhsiang M. Tsai <yhmtsai@gmail.com>
2019-09-18 14:21:17 +0200

    add windows shared custom dir and check the environment path

--- cmake/build_helpers.cmake
+++ cmake/build_helpers.cmake
@@ -17,9 +17,39 @@ function(ginkgo_compile_features name)
     if(GINKGO_WITH_IWYU AND GINKGO_IWYU_PATH)
         set_property(TARGET "${name}" PROPERTY CXX_INCLUDE_WHAT_YOU_USE ${GINKGO_IWYU_PATH})
     endif()
+    if(GINKGO_CHANGED_SHARED_LIBRARY)
+        # Put all shared libraries and corresponding imported libraries into the specficed path
+        set_property(TARGET "${name}" PROPERTY
+            RUNTIME_OUTPUT_DIRECTORY "${GINKGO_WINDOWS_SHARED_LIBRARY_PATH}")
+        set_property(TARGET "${name}" PROPERTY
+            ARCHIVE_OUTPUT_DIRECTORY "${GINKGO_WINDOWS_SHARED_LIBRARY_PATH}")
+        if(GINKGO_CHECK_PATH)
+            ginkgo_check_shared_library("${CMAKE_SHARED_LIBRARY_PREFIX}${name}${CMAKE_SHARED_LIBRARY_SUFFIX}")
+        endif()
+    endif()
 endfunction()
 
-macro(ginkgo_prepare_windows_path)
-    list(APPEND GINKGO_WINDOWS_PATH ${CMAKE_CURRENT_BINARY_DIR})
-    set(GINKGO_WINDOWS_PATH ${GINKGO_WINDOWS_PATH} PARENT_SCOPE)
-endmacro()
+function(ginkgo_check_shared_library name)
+    set(PATH_LIST $ENV{PATH})
+    set(PASSED_TEST FALSE)
+    foreach(ITEM ${PATH_LIST})
+        string(REPLACE "\\" "/" ITEM "${ITEM}")
+        if("${ITEM}" STREQUAL "${GINKGO_WINDOWS_SHARED_LIBRARY_PATH}")
+            set(PASSED_TEST TRUE)
+            break()
+        else()
+            # If any path before this build, the path must not contain the ginkgo shared library
+            set(EXISTED_DLL "EXISTED_DLL-NOTFOUND")
+            find_file(EXISTED_DLL "${name}" PATHS "${ITEM}" NO_DEFAULT_PATH)
+            if(NOT "${EXISTED_DLL}" STREQUAL "EXISTED_DLL-NOTFOUND")
+                message(FATAL_ERROR "Detect ${name} in ${ITEM} eariler than this build. "
+                    "Please add ${GINKGO_WINDOWS_SHARED_LIBRARY_PATH} before other ginkgo path.")
+            endif()
+        endif()
+    endforeach(ITEM)
+    if(NOT PASSED_TEST)
+        # Do not find this build in environment PATH
+        message(FATAL_ERROR "Do not find this build in environment PATH. "
+            "Please add ${GINKGO_WINDOWS_SHARED_LIBRARY_PATH} into environment PATH.")
+    endif()
+endfunction()
