5cb1443a3dace3a78cd46d82b256bd770cb8f8d4
Terry Cojean <terry.cojean@kit.edu>
2019-10-11 17:51:48 +0200

    Upgrade the containers to the correct new setup.
    This fixes the documentation and provides support for llvm 7, adds cuda 10.1
    and provides all makes the containers ready for HIP.

--- dev_tools/containers/ginkgo-nocuda-base.py
+++ dev_tools/containers/ginkgo-nocuda-base.py
@@ -7,29 +7,34 @@ Contents:
     OpenMP latest apt version for Clang+OpenMP
     Python 2 and 3 (upstream)
     cmake (upstream)
-    build-essential, git, openssh, doxygen, curl, valgrind latest apt version
-    jq, graphviz, ghostscript, texlive, texlive-latex-extra, latest apt version
-    texlive-science, texlive-fonts-extra, texlive-publishers latest apt version
+    build-essential, git, openssh, curl, valgrind latest apt version
+    jq, graphviz, ghostscript, latest apt version
+    bison, flex latest apt version, required for doxygen compilation
+    doxygen: install the latest release
+    texlive: install the latest release
     clang-tidy, iwyu: latest apt version
     hwloc, libhwloc-dev, pkg-config latest apt version
     papi: adds package libpfm4, and copy precompiled papi headers and files
           from a directory called 'papi'
+    gpg-agent: latest apt version, for adding custom keys
 """
 # pylint: disable=invalid-name, undefined-variable, used-before-assignment
 
 import os
 
 Stage0.baseimage('ubuntu:18.04')
-
+release_name = 'bionic'
 
 # Setup extra tools
 Stage0 += python()
 Stage0 += cmake(eula=True)
-Stage0 += apt_get(ospackages=['build-essential', 'git', 'openssh-client', 'doxygen', 'curl', 'valgrind'])
-Stage0 += apt_get(ospackages=['jq', 'graphviz', 'ghostscript', 'texlive', 'texlive-latex-extra'])
-Stage0 += apt_get(ospackages=['texlive-science', 'texlive-fonts-extra', 'texlive-publishers'])
+Stage0 += apt_get(ospackages=['build-essential', 'git', 'openssh-client', 'curl', 'valgrind'])
+Stage0 += apt_get(ospackages=['jq', 'graphviz', 'ghostscript'])
 Stage0 += apt_get(ospackages=['clang-tidy', 'iwyu'])
 Stage0 += apt_get(ospackages=['hwloc', 'libhwloc-dev', 'pkg-config'])
+Stage0 += apt_get(ospackages=['gpg-agent'])
+Stage0 += apt_get(ospackages=['ca-certificates']) # weird github certificates problem
+Stage0 += apt_get(ospackages=['bison', 'flex'])
 
 # GNU compilers
 gnu_version = USERARG.get('gnu', '8')
@@ -37,8 +42,33 @@ Stage0 += gnu(version=gnu_version, extra_repository=True)
 
 # Clang compilers
 llvm_version = USERARG.get('llvm', '7')
-Stage0 += llvm(version=llvm_version, extra_repository=True)
-Stage0 += apt_get(ospackages=['libomp-dev']) #required for openmp+clang
+clang_ver = 'clang-{}'.format(llvm_version)
+repo_ver = ['deb http://apt.llvm.org/{}/ llvm-toolchain-{}-{} main'.format(release_name, release_name, llvm_version)]
+Stage0 += apt_get(ospackages=[clang_ver, 'libomp-dev'], repositories=repo_ver, keys=['https://apt.llvm.org/llvm-snapshot.gpg.key'])
+clang_update = 'update-alternatives --install /usr/bin/clang clang /usr/bin/clang-{} 90'.format(llvm_version)
+clangpp_update = 'update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang-{} 90'.format(llvm_version)
+Stage0 += shell(commands=[clang_update, clangpp_update])
+
+# Doxygen
+Stage0 += shell(commands=['cd /var/tmp', 'git clone https://github.com/doxygen/doxygen'])
+Stage0 += shell(commands=['cd /var/tmp/doxygen', 'git checkout Release_1_8_16',
+                          'mkdir build', 'cd build',
+                          'cmake ..', 'make -j10', 'make install'])
+Stage0 += shell(commands=['cd /var/tmp', 'rm -rf doxygen'])
+
+# Texlive
+if os.path.isdir('texlive/'):
+    Stage0 += copy(src='texlive/texlive.profile', dest='/var/tmp')
+    Stage0 += shell(commands=['cd /var/tmp', 'wget '
+                              'http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz',
+                              'tar -xvf install-tl-unx.tar.gz', 'cd install-tl-2*',
+                              './install-tl --profile=../texlive.profile'])
+    Stage0 += shell(commands=['cd /var/tmp', 'rm -rf install-tl*'])
+    Stage0 += shell(commands=['tlmgr install mathtools float xcolor varwidth '
+                              'fancyvrb multirow hanging adjustbox xkeyval '
+                              'collectbox stackengine etoolbox listofitems ulem '
+                              'wasysym sectsty tocloft newunicodechar caption etoc '
+                              'pgf ec helvetic courier wasy'])
 
 # Copy PAPI libs
 add_papi = USERARG.get('papi', 'False')
---
+++ dev_tools/containers/texlive/texlive.profile
@@ -0,0 +1,30 @@
+# texlive.profile written on Fri Oct 11 15:47:26 2019 UTC
+# It will NOT be updated and reflects only the
+# installation profile at installation time.
+selected_scheme scheme-basic
+TEXDIR /usr/local/texlive/2019
+TEXMFCONFIG ~/.texlive2019/texmf-config
+TEXMFHOME ~/texmf
+TEXMFLOCAL /usr/local/texlive/texmf-local
+TEXMFSYSCONFIG /usr/local/texlive/2019/texmf-config
+TEXMFSYSVAR /usr/local/texlive/2019/texmf-var
+TEXMFVAR ~/.texlive2019/texmf-var
+binary_x86_64-linux 1
+instopt_adjustpath 1
+instopt_adjustrepo 1
+instopt_letter 0
+instopt_portable 0
+instopt_write18_restricted 1
+tlpdbopt_autobackup 1
+tlpdbopt_backupdir tlpkg/backups
+tlpdbopt_create_formats 1
+tlpdbopt_desktop_integration 1
+tlpdbopt_file_assocs 1
+tlpdbopt_generate_updmap 0
+tlpdbopt_install_docfiles 1
+tlpdbopt_install_srcfiles 1
+tlpdbopt_post_code 1
+tlpdbopt_sys_bin /usr/bin
+tlpdbopt_sys_info /usr/info
+tlpdbopt_sys_man /usr/man
+tlpdbopt_w32_multi_user 1
