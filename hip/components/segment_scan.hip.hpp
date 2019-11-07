74665aaf6102d322ca79316d2d2630158d26a9e9
Yuhsiang M. Tsai <yhmtsai@gmail.com>
2019-11-07 20:58:58 +0100

    add common to handle duplicated codes in hip/cuda

--- hip/components/segment_scan.hip.hpp
+++ hip/components/segment_scan.hip.hpp
@@ -45,36 +45,7 @@ namespace kernels {
 namespace hip {
 
 
-/**
- * @internal
- *
- * Compute a segement scan using add operation (+) of a subwarp. Each segment
- * performs suffix sum. Works on the source array and returns whether the thread
- * is the first element of its segment with same `ind`.
- */
-template <size_type subwarp_size, typename ValueType, typename IndexType>
-__device__ __forceinline__ bool segment_scan(
-    const group::thread_block_tile<subwarp_size> &group, const IndexType ind,
-    ValueType *__restrict__ val)
-{
-    bool head = true;
-#pragma unroll
-    for (int i = 1; i < subwarp_size; i <<= 1) {
-        const IndexType add_ind = group.shfl_up(ind, i);
-        ValueType add_val = zero<ValueType>();
-        if (add_ind == ind && threadIdx.x >= i) {
-            add_val = *val;
-            if (i == 1) {
-                head = false;
-            }
-        }
-        add_val = group.shfl_down(add_val, i);
-        if (threadIdx.x < subwarp_size - i) {
-            *val += add_val;
-        }
-    }
-    return head;
-}
+#include "common/components/segment_scan.hpp.inc"
 
 
 }  // namespace hip
