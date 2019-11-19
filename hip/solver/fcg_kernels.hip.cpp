0c9306839b33c8dab2e5536da5e5c3712b322c6e
Yuhsiang M. Tsai <yhmtsai@gmail.com>
2019-11-19 17:07:00 +0100

    add hip files

--- hip/solver/fcg_kernels.hip.cpp
+++ hip/solver/fcg_kernels.hip.cpp
@@ -33,6 +33,9 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "core/solver/fcg_kernels.hpp"
 
 
+#include <hip/hip_runtime.h>
+
+
 #include <ginkgo/core/base/exception_helpers.hpp>
 #include <ginkgo/core/base/math.hpp>
 
@@ -52,6 +55,12 @@ namespace hip {
 namespace fcg {
 
 
+constexpr int default_block_size = 512;
+
+
+#include "common/solver/fcg_kernels.hpp.inc"
+
+
 template <typename ValueType>
 void initialize(std::shared_ptr<const HipExecutor> exec,
                 const matrix::Dense<ValueType> *b, matrix::Dense<ValueType> *r,
@@ -59,7 +68,21 @@ void initialize(std::shared_ptr<const HipExecutor> exec,
                 matrix::Dense<ValueType> *q, matrix::Dense<ValueType> *t,
                 matrix::Dense<ValueType> *prev_rho,
                 matrix::Dense<ValueType> *rho, matrix::Dense<ValueType> *rho_t,
-                Array<stopping_status> *stop_status) GKO_NOT_IMPLEMENTED;
+                Array<stopping_status> *stop_status)
+{
+    const dim3 block_size(default_block_size, 1, 1);
+    const dim3 grid_size(
+        ceildiv(b->get_size()[0] * b->get_stride(), block_size.x), 1, 1);
+
+    hipLaunchKernelGGL(
+        initialize_kernel, dim3(grid_size), dim3(block_size), 0, 0,
+        b->get_size()[0], b->get_size()[1], b->get_stride(),
+        as_hip_type(b->get_const_values()), as_hip_type(r->get_values()),
+        as_hip_type(z->get_values()), as_hip_type(p->get_values()),
+        as_hip_type(q->get_values()), as_hip_type(t->get_values()),
+        as_hip_type(prev_rho->get_values()), as_hip_type(rho->get_values()),
+        as_hip_type(rho_t->get_values()), as_hip_type(stop_status->get_data()));
+}
 
 GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE(GKO_DECLARE_FCG_INITIALIZE_KERNEL);
 
@@ -69,7 +92,20 @@ void step_1(std::shared_ptr<const HipExecutor> exec,
             matrix::Dense<ValueType> *p, const matrix::Dense<ValueType> *z,
             const matrix::Dense<ValueType> *rho_t,
             const matrix::Dense<ValueType> *prev_rho,
-            const Array<stopping_status> *stop_status) GKO_NOT_IMPLEMENTED;
+            const Array<stopping_status> *stop_status)
+{
+    const dim3 block_size(default_block_size, 1, 1);
+    const dim3 grid_size(
+        ceildiv(p->get_size()[0] * p->get_stride(), block_size.x), 1, 1);
+
+    hipLaunchKernelGGL(step_1_kernel, dim3(grid_size), dim3(block_size), 0, 0,
+                       p->get_size()[0], p->get_size()[1], p->get_stride(),
+                       as_hip_type(p->get_values()),
+                       as_hip_type(z->get_const_values()),
+                       as_hip_type(rho_t->get_const_values()),
+                       as_hip_type(prev_rho->get_const_values()),
+                       as_hip_type(stop_status->get_const_data()));
+}
 
 GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE(GKO_DECLARE_FCG_STEP_1_KERNEL);
 
@@ -81,7 +117,22 @@ void step_2(std::shared_ptr<const HipExecutor> exec,
             const matrix::Dense<ValueType> *q,
             const matrix::Dense<ValueType> *beta,
             const matrix::Dense<ValueType> *rho,
-            const Array<stopping_status> *stop_status) GKO_NOT_IMPLEMENTED;
+            const Array<stopping_status> *stop_status)
+{
+    const dim3 block_size(default_block_size, 1, 1);
+    const dim3 grid_size(
+        ceildiv(p->get_size()[0] * p->get_stride(), block_size.x), 1, 1);
+
+    hipLaunchKernelGGL(
+        step_2_kernel, dim3(grid_size), dim3(block_size), 0, 0,
+        p->get_size()[0], p->get_size()[1], p->get_stride(), x->get_stride(),
+        as_hip_type(x->get_values()), as_hip_type(r->get_values()),
+        as_hip_type(t->get_values()), as_hip_type(p->get_const_values()),
+        as_hip_type(q->get_const_values()),
+        as_hip_type(beta->get_const_values()),
+        as_hip_type(rho->get_const_values()),
+        as_hip_type(stop_status->get_const_data()));
+}
 
 GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE(GKO_DECLARE_FCG_STEP_2_KERNEL);
 
