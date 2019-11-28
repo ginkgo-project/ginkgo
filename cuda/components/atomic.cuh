6c803a0dc902fb54f10fc8f06db25aa672e49076
Tobias Ribizel <ribizel@kit.edu>
2019-11-28 11:15:05 +0100

    review update
    rename a few variables

--- cuda/components/atomic.cuh
+++ cuda/components/atomic.cuh
@@ -50,10 +50,10 @@ namespace cuda {
 __forceinline__ __device__ thrust::complex<float> atomic_add(
     thrust::complex<float> *__restrict__ address, thrust::complex<float> val)
 {
-    cuComplex *cuaddr = reinterpret_cast<cuComplex *>(address);
+    cuComplex *addr = reinterpret_cast<cuComplex *>(address);
     // Separate to real part and imag part
-    auto real = atomic_add(&(cuaddr->x), val.real());
-    auto imag = atomic_add(&(cuaddr->y), val.imag());
+    auto real = atomic_add(&(addr->x), val.real());
+    auto imag = atomic_add(&(addr->y), val.imag());
     return {real, imag};
 }
 
@@ -66,10 +66,10 @@ __forceinline__ __device__ thrust::complex<float> atomic_add(
 __forceinline__ __device__ thrust::complex<double> atomic_add(
     thrust::complex<double> *__restrict__ address, thrust::complex<double> val)
 {
-    cuDoubleComplex *cuaddr = reinterpret_cast<cuDoubleComplex *>(address);
+    cuDoubleComplex *addr = reinterpret_cast<cuDoubleComplex *>(address);
     // Separate to real part and imag part
-    auto real = atomic_add(&(cuaddr->x), val.real());
-    auto imag = atomic_add(&(cuaddr->y), val.imag());
+    auto real = atomic_add(&(addr->x), val.real());
+    auto imag = atomic_add(&(addr->y), val.imag());
     return {real, imag};
 }
 
