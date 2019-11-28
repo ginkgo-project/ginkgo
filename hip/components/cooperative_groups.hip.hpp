26fc3192380697cf63acd2bee856ec215f73023b
Yuhsiang M. Tsai <yhmtsai@gmail.com>
2019-11-28 09:16:33 +0100

    works in amd and move max_block_stride to parameters

--- hip/components/cooperative_groups.hip.hpp
+++ hip/components/cooperative_groups.hip.hpp
@@ -205,18 +205,22 @@ public:
     GKO_BIND_SHFL(shfl, int32, int32);
     GKO_BIND_SHFL(shfl, float, int32);
     GKO_BIND_SHFL(shfl, uint32, int32);
+    GKO_BIND_SHFL(shfl, double, int32);
 
     GKO_BIND_SHFL(shfl_up, int32, uint32);
     GKO_BIND_SHFL(shfl_up, uint32, uint32);
     GKO_BIND_SHFL(shfl_up, float, uint32);
+    GKO_BIND_SHFL(shfl_up, double, uint32);
 
     GKO_BIND_SHFL(shfl_down, int32, uint32);
     GKO_BIND_SHFL(shfl_down, uint32, uint32);
     GKO_BIND_SHFL(shfl_down, float, uint32);
+    GKO_BIND_SHFL(shfl_down, double, uint32);
 
     GKO_BIND_SHFL(shfl_xor, int32, int32);
     GKO_BIND_SHFL(shfl_xor, float, int32);
     GKO_BIND_SHFL(shfl_xor, uint32, int32);
+    GKO_BIND_SHFL(shfl_xor, double, int32);
 
     __device__ __forceinline__ int any(int predicate) const noexcept
     {
