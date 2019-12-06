180bc6a85e0b8e248541e8425650552bb9110395
Tobias Ribizel <ribizel@kit.edu>
2019-12-06 10:33:45 +0100

    fix SPARSITY assertion tests

--- core/test/utils/assertions_test.cpp
+++ core/test/utils/assertions_test.cpp
@@ -47,24 +47,48 @@ class MatricesNear : public ::testing::Test {
 protected:
     using Mtx = gko::matrix::Dense<>;
     using Sparse = gko::matrix::Csr<>;
+
+    template <typename Type, std::size_t size>
+    gko::Array<Type> make_view(std::array<Type, size> &array)
+    {
+        return gko::Array<Type>::view(exec, size, array.data());
+    }
+
     MatricesNear()
         : exec(gko::ReferenceExecutor::create()),
           mtx1(gko::initialize<Mtx>({{1.0, 2.0, 3.0}, {0.0, 4.0, 0.0}}, exec)),
           mtx2(gko::initialize<Mtx>({{1.0, 2.0, 3.0}, {4.0, 0.0, 4.0}}, exec)),
           mtx3(gko::initialize<Mtx>({{1.0, 2.0, 3.0}, {0.0, 4.1, 0.0}}, exec)),
-          mtx1_sp(gko::initialize<Sparse>({{1.0, 2.0, 3.0}, {0.0, 4.0, 0.0}},
-                                          exec)),
-          mtx2_sp(gko::initialize<Sparse>({{1.0, 2.0, 3.0}, {4.0, 0.0, 4.0}},
-                                          exec)),
-          mtx3_sp(
-              gko::initialize<Sparse>({{1.0, 2.0, 3.0}, {0.0, 4.1, 0.0}}, exec))
-
-    {}
+          mtx13_row_ptrs{0, 3, 4},
+          mtx2_row_ptrs{0, 3, 5},
+          mtx13_col_idxs{0, 1, 2, 1},
+          mtx2_col_idxs{0, 1, 2, 0, 2},
+          mtx1_vals{1.0, 2.0, 3.0, 4.0},
+          mtx2_vals{1.0, 2.0, 3.0, 4.0, 4.0},
+          mtx3_vals{1.0, 2.0, 3.0, 4.1}
+    {
+        mtx1_sp = Sparse::create(exec, mtx1->get_size(), make_view(mtx1_vals),
+                                 make_view(mtx13_col_idxs),
+                                 make_view(mtx13_row_ptrs));
+        mtx2_sp =
+            Sparse::create(exec, mtx2->get_size(), make_view(mtx2_vals),
+                           make_view(mtx2_col_idxs), make_view(mtx2_row_ptrs));
+        mtx3_sp = Sparse::create(exec, mtx3->get_size(), make_view(mtx3_vals),
+                                 make_view(mtx13_col_idxs),
+                                 make_view(mtx13_row_ptrs));
+    }
 
     std::shared_ptr<const gko::Executor> exec;
     std::unique_ptr<Mtx> mtx1;
     std::unique_ptr<Mtx> mtx2;
     std::unique_ptr<Mtx> mtx3;
+    std::array<Sparse::index_type, 3> mtx13_row_ptrs;
+    std::array<Sparse::index_type, 3> mtx2_row_ptrs;
+    std::array<Sparse::index_type, 4> mtx13_col_idxs;
+    std::array<Sparse::index_type, 5> mtx2_col_idxs;
+    std::array<Sparse::value_type, 4> mtx1_vals;
+    std::array<Sparse::value_type, 5> mtx2_vals;
+    std::array<Sparse::value_type, 4> mtx3_vals;
     std::unique_ptr<Sparse> mtx1_sp;
     std::unique_ptr<Sparse> mtx2_sp;
     std::unique_ptr<Sparse> mtx3_sp;
@@ -107,7 +131,7 @@ TEST_F(MatricesNear, CanUseShortNotation)
     GKO_EXPECT_MTX_NEAR(mtx1, mtx1, 0.0);
     GKO_ASSERT_MTX_NEAR(mtx1, mtx3, 0.1);
     GKO_EXPECT_MTX_NEAR_SPARSITY(mtx1_sp, mtx1_sp, 0.0);
-    GKO_ASSERT_MTX_NEAR_SPARSITY(mtx1_sp, mtx2_sp, 0.0);
+    GKO_ASSERT_MTX_NEAR_SPARSITY(mtx1_sp, mtx1_sp, 0.0);
 }
 
 
