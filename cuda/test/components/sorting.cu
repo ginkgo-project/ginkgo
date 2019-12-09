d89bcb3efb155dca91dfe8076641860981e6a10e
Tobias Ribizel <ribizel@kit.edu>
2019-12-09 16:20:11 +0100

    review update
    * add static_asserts to catch infinite recursion
    * reorder headers in sorting tests

--- cuda/test/components/sorting.cu
+++ cuda/test/components/sorting.cu
@@ -30,6 +30,9 @@ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ******************************<GINKGO LICENSE>*******************************/
 
+#include "cuda/components/sorting.cuh"
+
+
 #include <memory>
 #include <random>
 
@@ -39,9 +42,6 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include <ginkgo/core/base/array.hpp>
 #include <ginkgo/core/base/executor.hpp>
-
-
-#include "cuda/components/sorting.cuh"
 
 
 namespace {
