811b68ca988f3df6f8d72ba9e0edd363d40bf0d5
flipflapflop <florent.lopez@stfc.ac.uk>
2019-12-13 17:44:22 -0500

    formatting

--- include/ginkgo/core/solver/lower_trs_isai.hpp
+++ include/ginkgo/core/solver/lower_trs_isai.hpp
@@ -50,118 +50,115 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <ginkgo/core/matrix/csr.hpp>
 #include <ginkgo/core/matrix/identity.hpp>
 
-
 namespace gko {
 namespace solver {
 
-   /**
-    * LowerTrsIsai is the triangular solver which solves the system L x = b, when L is
-    * a lower triangular matrix. It works best when passing in a matrix in CSR
-    * format. If the matrix is not in CSR, then the generate step converts it into
-    * a CSR matrix. The generation fails if the matrix is not convertible to CSR.
-    *
-    * @note As the constructor uses the copy and convert functionality, it is not
-    *       possible to create a empty solver or a solver with a matrix in any
-    *       other format other than CSR, if none of the executor modules are being
-    *       compiled with.
-    *
-    * @tparam ValueType  precision of matrix elements
-    * @tparam IndexType  precision of matrix indices
-    *
-    * @ingroup solvers
-    * @ingroup LinOp
-    */
-   template <typename ValueType = default_precision, typename IndexType = int32>
-   class LowerTrsIsai : public EnableLinOp<LowerTrsIsai<ValueType, IndexType>> {
-      friend class EnableLinOp<LowerTrsIsai>;
-      friend class EnablePolymorphicObject<LowerTrsIsai, LinOp>;
+/**
+ * LowerTrsIsai is the triangular solver which solves the system L x = b, when L
+ * is a lower triangular matrix. It works best when passing in a matrix in CSR
+ * format. If the matrix is not in CSR, then the generate step converts it into
+ * a CSR matrix. The generation fails if the matrix is not convertible to CSR.
+ *
+ * @note As the constructor uses the copy and convert functionality, it is not
+ *       possible to create a empty solver or a solver with a matrix in any
+ *       other format other than CSR, if none of the executor modules are being
+ *       compiled with.
+ *
+ * @tparam ValueType  precision of matrix elements
+ * @tparam IndexType  precision of matrix indices
+ *
+ * @ingroup solvers
+ * @ingroup LinOp
+ */
+template <typename ValueType = default_precision, typename IndexType = int32>
+class LowerTrsIsai : public EnableLinOp<LowerTrsIsai<ValueType, IndexType>> {
+    friend class EnableLinOp<LowerTrsIsai>;
+    friend class EnablePolymorphicObject<LowerTrsIsai, LinOp>;
 
-   public:
-      using value_type = ValueType;
-      using index_type = IndexType;
+public:
+    using value_type = ValueType;
+    using index_type = IndexType;
 
-      /**
-       * Gets the system operator (CSR matrix) of the linear system.
-       *
-       * @return the system operator (CSR matrix)
-       */
-      std::shared_ptr<const matrix::Csr<ValueType, IndexType>> get_system_matrix()
-         const
-      {
-         return system_matrix_;
-      }
-      
-      GKO_CREATE_FACTORY_PARAMETERS(parameters, Factory)
-      {
+    /**
+     * Gets the system operator (CSR matrix) of the linear system.
+     *
+     * @return the system operator (CSR matrix)
+     */
+    std::shared_ptr<const matrix::Csr<ValueType, IndexType>> get_system_matrix()
+        const
+    {
+        return system_matrix_;
+    }
 
-         /**
-          * Preconditioner factory.
-          */
-         // std::shared_ptr<const LinOpFactory> GKO_FACTORY_PARAMETER(
-         //       preconditioner, nullptr);
+    GKO_CREATE_FACTORY_PARAMETERS(parameters, Factory)
+    {
+        /**
+         * Preconditioner factory.
+         */
+        // std::shared_ptr<const LinOpFactory> GKO_FACTORY_PARAMETER(
+        //       preconditioner, nullptr);
 
-         /**
-          * Number of relaxation steps.
-          */
-         gko::size_type GKO_FACTORY_PARAMETER(niter, 0u);
+        /**
+         * Number of relaxation steps.
+         */
+        gko::size_type GKO_FACTORY_PARAMETER(niter, 0u);
 
-         /**
-          * Number of right hand sides.
-          *
-          * @note This value is currently a dummy value which is not used by the
-          *       analysis step. It is possible that future algorithms (cusparse
-          *       csrsm2) make use of the number of right hand sides for a more
-          *       sophisticated implementation. Hence this parameter is left
-          *       here. But currently, there is no need to use it.
-          */
-         gko::size_type GKO_FACTORY_PARAMETER(num_rhs, 1u);
+        /**
+         * Number of right hand sides.
+         *
+         * @note This value is currently a dummy value which is not used by the
+         *       analysis step. It is possible that future algorithms (cusparse
+         *       csrsm2) make use of the number of right hand sides for a more
+         *       sophisticated implementation. Hence this parameter is left
+         *       here. But currently, there is no need to use it.
+         */
+        gko::size_type GKO_FACTORY_PARAMETER(num_rhs, 1u);
+    };
+    GKO_ENABLE_LIN_OP_FACTORY(LowerTrsIsai, parameters, Factory);
+    GKO_ENABLE_BUILD_METHOD(Factory);
 
-      };
-      GKO_ENABLE_LIN_OP_FACTORY(LowerTrsIsai, parameters, Factory);
-      GKO_ENABLE_BUILD_METHOD(Factory);
+protected:
+    void build_isai();
 
-   protected:
-      void build_isai();
+    void apply_impl(const LinOp *b, LinOp *x) const override;
 
-      void apply_impl(const LinOp *b, LinOp *x) const override;
+    void apply_impl(const LinOp *alpha, const LinOp *b, const LinOp *beta,
+                    LinOp *x) const override;
 
-      void apply_impl(const LinOp *alpha, const LinOp *b, const LinOp *beta,
-                      LinOp *x) const override;
+    explicit LowerTrsIsai(std::shared_ptr<const Executor> exec)
+        : EnableLinOp<LowerTrsIsai>(std::move(exec))
+    {}
 
-      explicit LowerTrsIsai(std::shared_ptr<const Executor> exec)
-         : EnableLinOp<LowerTrsIsai>(std::move(exec))
-      {}
+    explicit LowerTrsIsai(const Factory *factory,
+                          std::shared_ptr<const LinOp> system_matrix)
+        : EnableLinOp<LowerTrsIsai>(factory->get_executor(),
+                                    transpose(system_matrix->get_size())),
+          parameters_{factory->get_parameters()},
+          system_matrix_{}
+    {
+        using CsrMatrix = matrix::Csr<ValueType, IndexType>;
 
-      explicit LowerTrsIsai(const Factory *factory,
-                            std::shared_ptr<const LinOp> system_matrix)
-         : EnableLinOp<LowerTrsIsai>(factory->get_executor(),
-                                     transpose(system_matrix->get_size())),
-           parameters_{factory->get_parameters()},
-           system_matrix_{}
-      {
-         using CsrMatrix = matrix::Csr<ValueType, IndexType>;
-
-         GKO_ASSERT_IS_SQUARE_MATRIX(system_matrix);
-         // This is needed because it does not make sense to call the copy and
-         // convert if the existing matrix is empty.
-         const auto exec = this->get_executor();
-         if (!system_matrix->get_size()) {
+        GKO_ASSERT_IS_SQUARE_MATRIX(system_matrix);
+        // This is needed because it does not make sense to call the copy and
+        // convert if the existing matrix is empty.
+        const auto exec = this->get_executor();
+        if (!system_matrix->get_size()) {
             system_matrix_ = CsrMatrix::create(exec);
-         } else {
+        } else {
             system_matrix_ =
-               copy_and_convert_to<CsrMatrix>(exec, system_matrix);
-         }
+                copy_and_convert_to<CsrMatrix>(exec, system_matrix);
+        }
 
-         isai_ = CsrMatrix::create(exec);
-         
-         // Build incomplete sparse aproximate inverse
-         this->build_isai();
-      }
+        isai_ = CsrMatrix::create(exec);
 
-   private:
-      std::shared_ptr<const matrix::Csr<ValueType, IndexType>> system_matrix_{};
-      std::shared_ptr<matrix::Csr<ValueType, IndexType>> isai_{};
-   };
+        // Build incomplete sparse aproximate inverse
+        this->build_isai();
+    }
+
+private:
+    std::shared_ptr<const matrix::Csr<ValueType, IndexType>> system_matrix_{};
+    std::shared_ptr<matrix::Csr<ValueType, IndexType>> isai_{};
+};
 
 
 }  // namespace solver
