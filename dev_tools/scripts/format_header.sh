e0dcca739d11a9a97ab7e9fb8fd0ed3e3a159dcc
Yuhsiang M. Tsai <yhmtsai@gmail.com>
2020-01-13 00:03:50 +0100

    keep the same empty lines and fix header def

--- dev_tools/scripts/format_header.sh
+++ dev_tools/scripts/format_header.sh
@@ -69,13 +69,16 @@ RECORD_HEADER=0
 NAMESPACE="^namespace"
 MAIN_PART_MATCH=$(dev_tools/scripts/temp.sh $1)
 HEADER_DEF=$(get_header_def $1)
-IFNDEF="#ifndef $HEADER_DEF"
-DEFINE="#define $HEADER_DEF"
+IFNDEF=""
+DEFINE=""
+IFNDEF_REGEX="^#ifndef GKO_"
+DEFINE_REGEX="^#define GKO_"
 HEADER_REGEX="\.(hpp|cuh)"
 SKIP="true"
 IF_REX="^#if"
 ENDIF_REX="^#endif"
 IN_IF="false"
+KEEP_LINES=0
 while IFS='' read -r line || [ -n "$line" ]; do
     if [ "${DURING_CONTENT}" = "true" ]; then
         echo "${line}" >> "${CONTENT}"
@@ -87,23 +90,27 @@ while IFS='' read -r line || [ -n "$line" ]; do
             DURING_LICENSE="false"
         fi
     elif [ -z "${line}" ] && [ "${SKIP}" = "true" ]; then
+    # Ignore all empty lines beteen LICENSE and Header
         :
     elif [[ ! "${line}" =~ ${NAMESPACE} ]]; then
-        if [[ $1 =~ ${HEADER_REGEX} ]] && [ "${line}" = "${IFNDEF}" ]; then
-            echo "${line}" >> ${BEFORE}
-        elif [[ $1 =~ ${HEADER_REGEX} ]] && [ "${line}" = "${DEFINE}" ]; then
-            echo "${line}" >> ${BEFORE}
+        if [ -z "$line" ]; then
+            KEEP_LINES=$((KEEP_LINES+1))
+        else
+            KEEP_LINES=0
+        fi
+        if [[ $1 =~ ${HEADER_REGEX} ]] && [[ "${line}" =~ ${IFNDEF_REGEX} ]] && [ "${SKIP}" = "true" ] && [ -z "${DEFINE}" ]; then
+            IFNDEF="${line}"
+        elif [[ $1 =~ ${HEADER_REGEX} ]] && [[ "${line}" =~ ${DEFINE_REGEX} ]] && [ "${SKIP}" = "true" ]; then
+            DEFINE="${line}"
         elif [[ "${line}" =~ $IF_REX ]] || [ "$IN_IF" = "true" ]; then
+            # make sure that the header in #if is not extracted
             echo "${line}" >> "${HEADER}"
             IN_IF="true"
             if [[ "${line}" =~ $ENDIF_REX ]]; then
                 IN_IF="false"
             fi
+            SKIP="false"
         elif [[ "${line}" =~ ${MAIN_PART_MATCH} ]]; then
-            if [ -f ${BEFORE} ] && [[ -z "${MAIN_INCLUDE}" ]]; then
-                echo "" >> ${BEFORE}
-                echo "" >> ${BEFORE}
-            fi
             line="$(convert_header ${line})"
             if [ ! "${line}" = "${MAIN_INCLUDE}" ]; then
                 if [ ! -z "${MAIN_INCLUDE}" ]; then
@@ -118,6 +125,7 @@ while IFS='' read -r line || [ -n "$line" ]; do
             fi
             echo "${line}" >> "${HEADER}"
             SKIP="false"
+            # echo "${line}"
         fi
     else
         DURING_CONTENT="true"
@@ -137,6 +145,22 @@ echo "/*${GINKGO_LICENSE_BEACON}" > $1
 cat LICENSE >> $1
 echo "${GINKGO_LICENSE_BEACON}*/" >> $1
 echo "" >> $1
+if [ ! -z "${IFNDEF}" ] && [ ! -z "${DEFINE}" ]; then
+    IFNDEF="#ifndef ${HEADER_DEF}"
+    DEFINE="#define ${HEADER_DEF}"
+elif [ -z "${IFNDEF}" ] && [ -z "${DEFINE}" ]; then
+    :
+else
+    echo "Warning: only #ifndef GKO_ or #define GKO_ is in the header"
+fi
+if [ ! -z "${IFNDEF}" ]; then
+    echo "${IFNDEF}" >> $1
+fi
+if [ ! -z "${DEFINE}" ]; then
+    echo "${DEFINE}" >> $1
+    echo "" >> $1
+    echo "" >> $1
+fi
 if [ -f "${BEFORE}" ]; then
     cat ${BEFORE} >> $1
     if [ -f "${HEADER}" ]; then
@@ -149,29 +173,52 @@ if [ -f "${BEFORE}" ]; then
     rm ${BEFORE}
 fi
 PREV_INC=0
+IN_IF="false"
 if [ -f "${HEADER}" ]; then
     COMMAND="${FORMAT_COMMAND} ${HEADER}"
     eval "${COMMAND}"
     while IFS='' read -r line; do
-        if [[ ${line} =~ ${INCLUDE_REGEX} ]]; then
-            if [[ ${PREV_INC} == 1 ]]; then
-                echo "" >> $1
+        if [[ "${line}" =~ $IF_REX ]] || [ "$IN_IF" = "true" ]; then
+            # make sure that the header in #if is not extracted
+            echo "${line}" >> $1
+            IN_IF="true"
+            if [[ "${line}" =~ $ENDIF_REX ]]; then
+                IN_IF="false"
+                # To keep the original lines
+                PREV_INC=-3
             fi
-            PREV_INC=0
         else
-            if [ -z "${line}" ]; then
-                PREV_INC=$((PREV_INC+1))
+            if [[ ${line} =~ ${INCLUDE_REGEX} ]]; then
+                if [[ ${PREV_INC} == 1 ]]; then
+                    echo "" >> $1
+                fi
+                PREV_INC=0
+            else
+                if [ -z "${line}" ]; then
+                    PREV_INC=$((PREV_INC+1))
+                else
+                    # To keep the original lines
+                    PREV_INC=-3
+                fi
             fi
+            echo "${line}" >> $1
         fi
-        echo "${line}" >> $1
+        
     done < ${HEADER}
     if [ -f "${CONTENT}" ]; then
-        echo "" >> $1
-        echo "" >> $1
+        for (( i=0; i < ${KEEP_LINES}; i++ ))
+        do
+            echo "" >> $1
+        done
     fi
     rm "${HEADER}"
 fi
 if [ -f "${CONTENT}" ]; then
-    cat ${CONTENT} >> $1
+    if [ ! -z "${IFNDEF}" ] && [ ! -z "${DEFINE}" ]; then
+        head -n -1 ${CONTENT} >> $1
+        echo "#endif  // $HEADER_DEF" >> $1
+    else
+        cat ${CONTENT} >> $1
+    fi
     rm "${CONTENT}"
 fi
