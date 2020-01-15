0794e9ecc0d508d71916c45dd901074ede8ee919
Thomas Grützmacher <thomas.gruetzmacher@kit.edu>
2020-01-15 15:23:35 +0100

    Add CUDA header dependency to Lower and UpperTrs solver tests

--- cmake/create_test.cmake
+++ cmake/create_test.cmake
@@ -16,6 +16,44 @@ function(ginkgo_create_test test_name)
     add_test(NAME ${REL_BINARY_DIR}/${test_name} COMMAND ${TEST_TARGET_NAME})
 endfunction(ginkgo_create_test)
 
+function(ginkgo_create_test_cpp_cuda_header test_name)
+    file(RELATIVE_PATH REL_BINARY_DIR
+         ${PROJECT_BINARY_DIR} ${CMAKE_CURRENT_BINARY_DIR})
+    string(REPLACE "/" "_" TEST_TARGET_NAME "${REL_BINARY_DIR}/${test_name}")
+    add_executable(${TEST_TARGET_NAME} ${test_name}.cpp)
+    target_include_directories("${TEST_TARGET_NAME}"
+        PRIVATE
+            "$<BUILD_INTERFACE:${Ginkgo_BINARY_DIR}>"
+            "${CUDA_INCLUDE_DIRS}"
+        )
+    set_target_properties(${TEST_TARGET_NAME} PROPERTIES
+        OUTPUT_NAME ${test_name})
+    if (GINKGO_CHECK_CIRCULAR_DEPS)
+        target_link_libraries(${TEST_TARGET_NAME} PRIVATE "${GINKGO_CIRCULAR_DEPS_FLAGS}")
+    endif()
+    target_link_libraries(${TEST_TARGET_NAME} PRIVATE ginkgo GTest::Main GTest::GTest ${ARGN})
+    add_test(NAME ${REL_BINARY_DIR}/${test_name} COMMAND ${TEST_TARGET_NAME})
+endfunction(ginkgo_create_test_cpp_cuda_header)
+
+function(ginkgo_create_cuda_test test_name)
+    file(RELATIVE_PATH REL_BINARY_DIR
+         ${PROJECT_BINARY_DIR} ${CMAKE_CURRENT_BINARY_DIR})
+    string(REPLACE "/" "_" TEST_TARGET_NAME "${REL_BINARY_DIR}/${test_name}")
+    add_executable(${TEST_TARGET_NAME} ${test_name}.cu)
+    target_include_directories("${TEST_TARGET_NAME}"
+        PRIVATE
+            "$<BUILD_INTERFACE:${Ginkgo_BINARY_DIR}>"
+        )
+    set_target_properties(${TEST_TARGET_NAME} PROPERTIES
+        OUTPUT_NAME ${test_name})
+
+    if (GINKGO_CHECK_CIRCULAR_DEPS)
+        target_link_libraries(${TEST_TARGET_NAME} PRIVATE "${GINKGO_CIRCULAR_DEPS_FLAGS}")
+    endif()
+    target_link_libraries(${TEST_TARGET_NAME} PRIVATE ginkgo GTest::Main GTest::GTest ${ARGN})
+    add_test(NAME ${REL_BINARY_DIR}/${test_name} COMMAND ${TEST_TARGET_NAME})
+endfunction(ginkgo_create_cuda_test)
+
 function(ginkgo_create_hip_test_special_linkage test_name)
     # use gcc to compile but use hip to link
     file(RELATIVE_PATH REL_BINARY_DIR
@@ -38,25 +76,6 @@ function(ginkgo_create_hip_test_special_linkage test_name)
     target_link_libraries(${TEST_TARGET_NAME} PRIVATE ginkgo GTest::Main GTest::GTest ${ARGN})
     add_test(NAME ${REL_BINARY_DIR}/${test_name} COMMAND ${TEST_TARGET_NAME})
 endfunction(ginkgo_create_hip_test_special_linkage)
-
-function(ginkgo_create_cuda_test test_name)
-    file(RELATIVE_PATH REL_BINARY_DIR
-         ${PROJECT_BINARY_DIR} ${CMAKE_CURRENT_BINARY_DIR})
-    string(REPLACE "/" "_" TEST_TARGET_NAME "${REL_BINARY_DIR}/${test_name}")
-    add_executable(${TEST_TARGET_NAME} ${test_name}.cu)
-    target_include_directories("${TEST_TARGET_NAME}"
-        PRIVATE
-            "$<BUILD_INTERFACE:${Ginkgo_BINARY_DIR}>"
-        )
-    set_target_properties(${TEST_TARGET_NAME} PROPERTIES
-        OUTPUT_NAME ${test_name})
-
-    if (GINKGO_CHECK_CIRCULAR_DEPS)
-        target_link_libraries(${TEST_TARGET_NAME} PRIVATE "${GINKGO_CIRCULAR_DEPS_FLAGS}")
-    endif()
-    target_link_libraries(${TEST_TARGET_NAME} PRIVATE ginkgo GTest::Main GTest::GTest ${ARGN})
-    add_test(NAME ${REL_BINARY_DIR}/${test_name} COMMAND ${TEST_TARGET_NAME})
-endfunction(ginkgo_create_cuda_test)
 
 function(ginkgo_create_hip_test test_name)
     file(RELATIVE_PATH REL_BINARY_DIR
