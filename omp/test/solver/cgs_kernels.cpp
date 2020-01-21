2effc4f054ca85ca674e3c1cdd4cb45edf3e7fc1
Eoli-an <dennis.groetzinger@web.de>
2020-01-21 10:46:26 +0100

    swap around the core header files in all omp tests

--- omp/test/solver/cgs_kernels.cpp
+++ omp/test/solver/cgs_kernels.cpp
@@ -39,14 +39,16 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <gtest/gtest.h>
 
 
-#include <core/solver/cgs_kernels.hpp>
-#include <core/test/utils.hpp>
 #include <ginkgo/core/base/exception.hpp>
 #include <ginkgo/core/base/executor.hpp>
 #include <ginkgo/core/matrix/dense.hpp>
 #include <ginkgo/core/stop/combined.hpp>
 #include <ginkgo/core/stop/iteration.hpp>
 #include <ginkgo/core/stop/residual_norm_reduction.hpp>
+
+
+#include "core/solver/cgs_kernels.hpp"
+#include "core/test/utils.hpp"
 
 
 namespace {
--- omp/test/solver/ir_kernels.cpp
+++ omp/test/solver/ir_kernels.cpp
@@ -39,13 +39,15 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <gtest/gtest.h>
 
 
-#include <core/solver/ir_kernels.hpp>
-#include <core/test/utils.hpp>
 #include <ginkgo/core/base/exception.hpp>
 #include <ginkgo/core/base/executor.hpp>
 #include <ginkgo/core/matrix/dense.hpp>
 #include <ginkgo/core/stop/combined.hpp>
 #include <ginkgo/core/stop/iteration.hpp>
+
+
+#include "core/solver/ir_kernels.hpp"
+#include "core/test/utils.hpp"
 
 
 namespace {
