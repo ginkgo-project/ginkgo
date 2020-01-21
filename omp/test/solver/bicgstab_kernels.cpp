98e6eab56542517f3f81442167ce9f7d69a06292
Eoli-an <dennis.groetzinger@web.de>
2020-01-21 10:46:26 +0100

    swap around the core header files in all omp tests

--- omp/test/solver/bicgstab_kernels.cpp
+++ omp/test/solver/bicgstab_kernels.cpp
@@ -39,8 +39,6 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <gtest/gtest.h>
 
 
-#include <core/solver/bicgstab_kernels.hpp>
-#include <core/test/utils.hpp>
 #include <ginkgo/core/base/array.hpp>
 #include <ginkgo/core/base/exception.hpp>
 #include <ginkgo/core/base/executor.hpp>
@@ -48,6 +46,10 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <ginkgo/core/stop/combined.hpp>
 #include <ginkgo/core/stop/iteration.hpp>
 #include <ginkgo/core/stop/residual_norm_reduction.hpp>
+
+
+#include "core/solver/bicgstab_kernels.hpp"
+#include "core/test/utils.hpp"
 
 
 namespace {
