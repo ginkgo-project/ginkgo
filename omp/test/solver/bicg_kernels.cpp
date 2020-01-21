c436f698544fb1341426899d50881f653362a159
Eoli-an <dennis.groetzinger@web.de>
2020-01-21 10:29:53 +0100

    create the code for Omp and add the tests

--- omp/test/solver/bicg_kernels.cpp
+++ omp/test/solver/bicg_kernels.cpp
@@ -86,6 +86,10 @@ protected:
         z = gen_mtx(m, n);
         p = gen_mtx(m, n);
         q = gen_mtx(m, n);
+        r2 = gen_mtx(m, n);
+        z2 = gen_mtx(m, n);
+        p2 = gen_mtx(m, n);
+        q2 = gen_mtx(m, n);
         x = gen_mtx(m, n);
         beta = gen_mtx(1, n);
         prev_rho = gen_mtx(1, n);
@@ -106,6 +110,14 @@ protected:
         d_p->copy_from(p.get());
         d_q = Mtx::create(omp);
         d_q->copy_from(q.get());
+        d_r2 = Mtx::create(omp);
+        d_r2->copy_from(r2.get());
+        d_z2 = Mtx::create(omp);
+        d_z2->copy_from(z2.get());
+        d_p2 = Mtx::create(omp);
+        d_p2->copy_from(p2.get());
+        d_q2 = Mtx::create(omp);
+        d_q2->copy_from(q2.get());
         d_x = Mtx::create(omp);
         d_x->copy_from(x.get());
         d_beta = Mtx::create(omp);
@@ -156,6 +168,10 @@ protected:
     std::unique_ptr<Mtx> z;
     std::unique_ptr<Mtx> p;
     std::unique_ptr<Mtx> q;
+    std::unique_ptr<Mtx> r2;
+    std::unique_ptr<Mtx> z2;
+    std::unique_ptr<Mtx> p2;
+    std::unique_ptr<Mtx> q2;
     std::unique_ptr<Mtx> x;
     std::unique_ptr<Mtx> beta;
     std::unique_ptr<Mtx> prev_rho;
@@ -167,6 +183,10 @@ protected:
     std::unique_ptr<Mtx> d_z;
     std::unique_ptr<Mtx> d_p;
     std::unique_ptr<Mtx> d_q;
+    std::unique_ptr<Mtx> d_r2;
+    std::unique_ptr<Mtx> d_z2;
+    std::unique_ptr<Mtx> d_p2;
+    std::unique_ptr<Mtx> d_q2;
     std::unique_ptr<Mtx> d_x;
     std::unique_ptr<Mtx> d_beta;
     std::unique_ptr<Mtx> d_prev_rho;
@@ -176,102 +196,105 @@ protected:
 
 
 TEST_F(Bicg, OmpBicgInitializeIsEquivalentToRef)
-GKO_NOT_IMPLEMENTED;
-//{
-// TODO (script): change the code imported from solver/cg if needed
-//    initialize_data();
-//
-//    gko::kernels::reference::bicg::initialize(ref, b.get(), r.get(), z.get(),
-//                                            p.get(), q.get(), prev_rho.get(),
-//                                            rho.get(), stop_status.get());
-//    gko::kernels::omp::bicg::initialize(omp, d_b.get(), d_r.get(), d_z.get(),
-//                                      d_p.get(), d_q.get(), d_prev_rho.get(),
-//                                      d_rho.get(), d_stop_status.get());
-//
-//    GKO_ASSERT_MTX_NEAR(d_r, r, 1e-14);
-//    GKO_ASSERT_MTX_NEAR(d_z, z, 1e-14);
-//    GKO_ASSERT_MTX_NEAR(d_p, p, 1e-14);
-//    GKO_ASSERT_MTX_NEAR(d_q, q, 1e-14);
-//    GKO_ASSERT_MTX_NEAR(d_prev_rho, prev_rho, 1e-14);
-//    GKO_ASSERT_MTX_NEAR(d_rho, rho, 1e-14);
-//    GKO_ASSERT_ARRAY_EQ(d_stop_status, stop_status);
-//}
+{
+    initialize_data();
+
+    gko::kernels::reference::bicg::initialize(
+        ref, b.get(), r.get(), z.get(), p.get(), q.get(), prev_rho.get(),
+        rho.get(), r2.get(), z2.get(), p2.get(), q2.get(), stop_status.get());
+    gko::kernels::omp::bicg::initialize(
+        omp, d_b.get(), d_r.get(), d_z.get(), d_p.get(), d_q.get(),
+        d_prev_rho.get(), d_rho.get(), d_r2.get(), d_z2.get(), d_p2.get(),
+        d_q2.get(), d_stop_status.get());
+
+    GKO_ASSERT_MTX_NEAR(d_r, r, 1e-14);
+    GKO_ASSERT_MTX_NEAR(d_z, z, 1e-14);
+    GKO_ASSERT_MTX_NEAR(d_p, p, 1e-14);
+    GKO_ASSERT_MTX_NEAR(d_q, q, 1e-14);
+    GKO_ASSERT_MTX_NEAR(d_r2, r2, 1e-14);
+    GKO_ASSERT_MTX_NEAR(d_z2, z2, 1e-14);
+    GKO_ASSERT_MTX_NEAR(d_p2, p2, 1e-14);
+    GKO_ASSERT_MTX_NEAR(d_q2, q2, 1e-14);
+    GKO_ASSERT_MTX_NEAR(d_prev_rho, prev_rho, 1e-14);
+    GKO_ASSERT_MTX_NEAR(d_rho, rho, 1e-14);
+    GKO_ASSERT_ARRAY_EQ(d_stop_status, stop_status);
+}
 
 
 TEST_F(Bicg, OmpBicgStep1IsEquivalentToRef)
-GKO_NOT_IMPLEMENTED;
-//{
-// TODO (script): change the code imported from solver/cg if needed
-//    initialize_data();
-//
-//    gko::kernels::reference::bicg::step_1(ref, p.get(), z.get(), rho.get(),
-//                                        prev_rho.get(), stop_status.get());
-//    gko::kernels::omp::bicg::step_1(omp, d_p.get(), d_z.get(), d_rho.get(),
-//                                  d_prev_rho.get(), d_stop_status.get());
-//
-//    GKO_ASSERT_MTX_NEAR(d_p, p, 1e-14);
-//    GKO_ASSERT_MTX_NEAR(d_z, z, 1e-14);
-//}
+{
+    initialize_data();
+
+    gko::kernels::reference::bicg::step_1(ref, p.get(), z.get(), p2.get(),
+                                          z2.get(), rho.get(), prev_rho.get(),
+                                          stop_status.get());
+    gko::kernels::omp::bicg::step_1(omp, d_p.get(), d_z.get(), d_p2.get(),
+                                    d_z2.get(), d_rho.get(), d_prev_rho.get(),
+                                    d_stop_status.get());
+
+    GKO_ASSERT_MTX_NEAR(d_p, p, 1e-14);
+    GKO_ASSERT_MTX_NEAR(d_z, z, 1e-14);
+    GKO_ASSERT_MTX_NEAR(d_p2, p2, 1e-14);
+    GKO_ASSERT_MTX_NEAR(d_z2, z2, 1e-14);
+}
 
 
 TEST_F(Bicg, OmpBicgStep2IsEquivalentToRef)
-GKO_NOT_IMPLEMENTED;
-//{
-// TODO (script): change the code imported from solver/cg if needed
-//    initialize_data();
-//    gko::kernels::reference::bicg::step_2(ref, x.get(), r.get(), p.get(),
-//    q.get(),
-//                                        beta.get(), rho.get(),
-//                                        stop_status.get());
-//    gko::kernels::omp::bicg::step_2(omp, d_x.get(), d_r.get(), d_p.get(),
-//                                  d_q.get(), d_beta.get(), d_rho.get(),
-//                                  d_stop_status.get());
-//
-//    GKO_ASSERT_MTX_NEAR(d_x, x, 1e-14);
-//    GKO_ASSERT_MTX_NEAR(d_r, r, 1e-14);
-//    GKO_ASSERT_MTX_NEAR(d_p, p, 1e-14);
-//    GKO_ASSERT_MTX_NEAR(d_q, q, 1e-14);
-//}
+{
+    initialize_data();
+
+    gko::kernels::reference::bicg::step_2(
+        ref, x.get(), r.get(), r2.get(), p.get(), q.get(), q2.get(), beta.get(),
+        rho.get(), stop_status.get());
+    gko::kernels::omp::bicg::step_2(
+        omp, d_x.get(), d_r.get(), d_r2.get(), d_p.get(), d_q.get(), d_q2.get(),
+        d_beta.get(), d_rho.get(), d_stop_status.get());
+
+    GKO_ASSERT_MTX_NEAR(d_x, x, 1e-14);
+    GKO_ASSERT_MTX_NEAR(d_r, r, 1e-14);
+    GKO_ASSERT_MTX_NEAR(d_r2, r2, 1e-14);
+    GKO_ASSERT_MTX_NEAR(d_p, p, 1e-14);
+    GKO_ASSERT_MTX_NEAR(d_q, q, 1e-14);
+    GKO_ASSERT_MTX_NEAR(d_q2, q2, 1e-14);
+}
 
 
 TEST_F(Bicg, ApplyIsEquivalentToRef)
-GKO_NOT_IMPLEMENTED;
-//{
-// TODO (script): change the code imported from solver/cg if needed
-//    auto mtx = gen_mtx(50, 50);
-//    make_spd(mtx.get());
-//    auto x = gen_mtx(50, 3);
-//    auto b = gen_mtx(50, 3);
-//    auto d_mtx = Mtx::create(omp);
-//    d_mtx->copy_from(mtx.get());
-//    auto d_x = Mtx::create(omp);
-//    d_x->copy_from(x.get());
-//    auto d_b = Mtx::create(omp);
-//    d_b->copy_from(b.get());
-//    auto bicg_factory =
-//        gko::solver::Bicg<>::build()
-//            .with_criteria(
-//                gko::stop::Iteration::build().with_max_iters(50u).on(ref),
-//                gko::stop::ResidualNormReduction<>::build()
-//                    .with_reduction_factor(1e-14)
-//                    .on(ref))
-//            .on(ref);
-//    auto d_bicg_factory =
-//        gko::solver::Bicg<>::build()
-//            .with_criteria(
-//                gko::stop::Iteration::build().with_max_iters(50u).on(omp),
-//                gko::stop::ResidualNormReduction<>::build()
-//                    .with_reduction_factor(1e-14)
-//                    .on(omp))
-//            .on(omp);
-//    auto solver = bicg_factory->generate(std::move(mtx));
-//    auto d_solver = d_bicg_factory->generate(std::move(d_mtx));
-//
-//    solver->apply(b.get(), x.get());
-//    d_solver->apply(d_b.get(), d_x.get());
-//
-//    GKO_ASSERT_MTX_NEAR(d_x, x, 1e-14);
-//}
+{
+    auto mtx = gen_mtx(50, 50);
+    make_spd(mtx.get());
+    auto x = gen_mtx(50, 3);
+    auto b = gen_mtx(50, 3);
+    auto d_mtx = Mtx::create(omp);
+    d_mtx->copy_from(mtx.get());
+    auto d_x = Mtx::create(omp);
+    d_x->copy_from(x.get());
+    auto d_b = Mtx::create(omp);
+    d_b->copy_from(b.get());
+    auto bicg_factory =
+        gko::solver::Bicg<>::build()
+            .with_criteria(
+                gko::stop::Iteration::build().with_max_iters(50u).on(ref),
+                gko::stop::ResidualNormReduction<>::build()
+                    .with_reduction_factor(1e-14)
+                    .on(ref))
+            .on(ref);
+    auto d_bicg_factory =
+        gko::solver::Bicg<>::build()
+            .with_criteria(
+                gko::stop::Iteration::build().with_max_iters(50u).on(omp),
+                gko::stop::ResidualNormReduction<>::build()
+                    .with_reduction_factor(1e-14)
+                    .on(omp))
+            .on(omp);
+    auto solver = bicg_factory->generate(std::move(mtx));
+    auto d_solver = d_bicg_factory->generate(std::move(d_mtx));
+
+    solver->apply(b.get(), x.get());
+    d_solver->apply(d_b.get(), d_x.get());
+
+    GKO_ASSERT_MTX_NEAR(d_x, x, 1e-14);
+}
 
 
 }  // namespace
