a3ea5e1dd4b8568d4c2eec49a4c9c271d936dc5f
Tobias Ribizel <ribizel@kit.edu>
2020-05-21 14:23:47 +0200

    optimize composition intermediate storage
    only store the minimal amount of memory necessary
    this is either the maximum sum of input and output dimension
    for all but the last intermediate operator, or the output dimension
    of the last intermediate operator if there is only one operator.

--- reference/test/base/composition.cpp
+++ reference/test/base/composition.cpp
@@ -55,13 +55,25 @@ protected:
 
     Composition()
         : exec{gko::ReferenceExecutor::create()},
-          operators{gko::initialize<Mtx>(I<T>({2.0, 1.0}), exec),
-                    gko::initialize<Mtx>({I<T>({3.0, 2.0})}, exec)}
+          operators{
+              gko::initialize<Mtx>(I<T>({2.0, 1.0}), exec),
+              gko::initialize<Mtx>({I<T>({3.0, 2.0})}, exec),
+              gko::initialize<Mtx>({I<T>{-1.0, 1.0, 2.0}, I<T>{5.0, -3.0, 0.0}},
+                                   exec),
+              gko::initialize<Mtx>(
+                  {I<T>{9.0, 4.0}, I<T>{6.0, -2.0}, I<T>{-3.0, 2.0}}, exec),
+              gko::initialize<Mtx>({I<T>{1.0, 0.0}, I<T>{0.0, 1.0}}, exec)},
+          identity{
+              gko::initialize<Mtx>({I<T>{1.0, 0.0}, I<T>{0.0, 1.0}}, exec)},
+          product{
+              gko::initialize<Mtx>({I<T>{-9.0, -2.0}, I<T>{27.0, 26.0}}, exec)}
     {}
 
     std::shared_ptr<const gko::Executor> exec;
     std::vector<std::shared_ptr<gko::LinOp>> coefficients;
     std::vector<std::shared_ptr<gko::LinOp>> operators;
+    std::shared_ptr<Mtx> identity;
+    std::shared_ptr<Mtx> product;
 };
 
 TYPED_TEST_CASE(Composition, gko::test::ValueTypes);
@@ -102,6 +114,126 @@ TYPED_TEST(Composition, AppliesLinearCombinationToVector)
     cmp->apply(lend(alpha), lend(x), lend(beta), lend(res));
 
     GKO_ASSERT_MTX_NEAR(res, l({41.0, 19.0}), r<TypeParam>::value);
+}
+
+
+TYPED_TEST(Composition, AppliesLongerToVector)
+{
+    /*
+        cmp = [ 2 ] * [ 3 2 ] * [ -9  -2 ]
+              [ 1 ]             [ 27  26 ]
+    */
+    using Mtx = typename TestFixture::Mtx;
+    auto cmp = gko::Composition<TypeParam>::create(
+        this->operators[0], this->operators[1], this->product);
+    auto x = gko::initialize<Mtx>({1.0, 2.0}, this->exec);
+    auto res = clone(x);
+
+    cmp->apply(lend(x), lend(res));
+
+    GKO_ASSERT_MTX_NEAR(res, l({238.0, 119.0}), r<TypeParam>::value);
+}
+
+
+TYPED_TEST(Composition, AppliesLongerLinearCombinationToVector)
+{
+    /*
+        cmp = [ 2 ] * [ 3 2 ] * [ -9  -2 ]
+              [ 1 ]             [ 27  26 ]
+    */
+    using Mtx = typename TestFixture::Mtx;
+    auto cmp = gko::Composition<TypeParam>::create(
+        this->operators[0], this->operators[1], this->product);
+    auto alpha = gko::initialize<Mtx>({3.0}, this->exec);
+    auto beta = gko::initialize<Mtx>({-1.0}, this->exec);
+    auto x = gko::initialize<Mtx>({1.0, 2.0}, this->exec);
+    auto res = clone(x);
+
+    cmp->apply(lend(alpha), lend(x), lend(beta), lend(res));
+
+    GKO_ASSERT_MTX_NEAR(res, l({713.0, 355.0}), r<TypeParam>::value);
+}
+
+
+TYPED_TEST(Composition, AppliesLongestToVector)
+{
+    /*
+        cmp = [ 2 ] * [ 3 2 ] * [ -1  1  2 ] * [  9  4 ] * [ 1 0 ]
+              [ 1 ]             [  5 -3  0 ]   [  6 -2 ]   [ 0 1 ]
+                                               [ -3  2 ]
+    */
+    using Mtx = typename TestFixture::Mtx;
+    auto cmp = gko::Composition<TypeParam>::create(this->operators.begin(),
+                                                   this->operators.end());
+    auto x = gko::initialize<Mtx>({1.0, 2.0}, this->exec);
+    auto res = clone(x);
+
+    cmp->apply(lend(x), lend(res));
+
+    GKO_ASSERT_MTX_NEAR(res, l({238.0, 119.0}), r<TypeParam>::value);
+}
+
+
+TYPED_TEST(Composition, AppliesLongestLinearCombinationToVector)
+{
+    /*
+        cmp = [ 2 ] * [ 3 2 ] * [ -1  1  2 ] * [  9  4 ] * [ 1 0 ]
+              [ 1 ]             [  5 -3  0 ]   [  6 -2 ]   [ 0 1 ]
+                                               [ -3  2 ]
+    */
+    using Mtx = typename TestFixture::Mtx;
+    auto cmp = gko::Composition<TypeParam>::create(this->operators.begin(),
+                                                   this->operators.end());
+    auto alpha = gko::initialize<Mtx>({3.0}, this->exec);
+    auto beta = gko::initialize<Mtx>({-1.0}, this->exec);
+    auto x = gko::initialize<Mtx>({1.0, 2.0}, this->exec);
+    auto res = clone(x);
+
+    cmp->apply(lend(alpha), lend(x), lend(beta), lend(res));
+
+    GKO_ASSERT_MTX_NEAR(res, l({713.0, 355.0}), r<TypeParam>::value);
+}
+
+
+TYPED_TEST(Composition, AppliesLongestToVectorMultipleRhs)
+{
+    /*
+        cmp = [ 2 ] * [ 3 2 ] * [ -1  1  2 ] * [  9  4 ] * [ 1 0 ]
+              [ 1 ]             [  5 -3  0 ]   [  6 -2 ]   [ 0 1 ]
+                                               [ -3  2 ]
+    */
+    using Mtx = typename TestFixture::Mtx;
+    auto cmp = gko::Composition<TypeParam>::create(this->operators.begin(),
+                                                   this->operators.end());
+    auto x = clone(this->identity);
+    auto res = clone(x);
+
+    cmp->apply(lend(x), lend(res));
+
+    GKO_ASSERT_MTX_NEAR(res, l({{54.0, 92.0}, {27.0, 46.0}}),
+                        r<TypeParam>::value);
+}
+
+
+TYPED_TEST(Composition, AppliesLongestLinearCombinationToVectorMultipleRhs)
+{
+    /*
+        cmp = [ 2 ] * [ 3 2 ] * [ -1  1  2 ] * [  9  4 ] * [ 1 0 ]
+              [ 1 ]             [  5 -3  0 ]   [  6 -2 ]   [ 0 1 ]
+                                               [ -3  2 ]
+    */
+    using Mtx = typename TestFixture::Mtx;
+    auto cmp = gko::Composition<TypeParam>::create(this->operators.begin(),
+                                                   this->operators.end());
+    auto alpha = gko::initialize<Mtx>({3.0}, this->exec);
+    auto beta = gko::initialize<Mtx>({-1.0}, this->exec);
+    auto x = clone(this->identity);
+    auto res = clone(x);
+
+    cmp->apply(lend(alpha), lend(x), lend(beta), lend(res));
+
+    GKO_ASSERT_MTX_NEAR(res, l({{161.0, 276.0}, {81.0, 137.0}}),
+                        r<TypeParam>::value);
 }
 
 
