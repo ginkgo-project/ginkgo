e702ca6d05f6263b3dd28d2f296b8a2c32305359
Fritz Goebel <goebel.fritz@googlemail.com>
2020-07-17 17:11:20 +0200

    add tests for dimension mismatch

--- cuda/matrix/diagonal_kernels.cu
+++ cuda/matrix/diagonal_kernels.cu
@@ -33,9 +33,6 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "core/matrix/diagonal_kernels.hpp"
 
 
-#include <algorithm>
-
-
 #include <ginkgo/core/matrix/csr.hpp>
 #include <ginkgo/core/matrix/dense.hpp>
 
