3c963a72446144e108be87e80362389a3024cda7
Yuhsiang M. Tsai <yhmtsai@gmail.com>
2020-07-21 17:54:54 +0200

    hip implementation

--- hip/multigrid/amgx_pgm_kernels.hip.cpp
+++ hip/multigrid/amgx_pgm_kernels.hip.cpp
@@ -45,10 +45,16 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <ginkgo/core/multigrid/amgx_pgm.hpp>
 
 
+#include "core/components/fill_array.hpp"
+#include "core/components/prefix_sum.hpp"
+#include "core/matrix/csr_builder.hpp"
+#include "core/matrix/csr_kernels.hpp"
 #include "hip/base/hipsparse_bindings.hip.hpp"
 #include "hip/base/math.hip.hpp"
 #include "hip/base/types.hip.hpp"
-#include "hip/solver/common_trs_kernels.hip.hpp"
+#include "hip/components/atomic.hip.hpp"
+#include "hip/components/reduction.hip.hpp"
+#include "hip/components/thread_ids.hip.hpp"
 
 
 namespace gko {
@@ -62,25 +68,62 @@ namespace hip {
 namespace amgx_pgm {
 
 
+constexpr int default_block_size = 512;
+
+
+#include "common/multigrid/amgx_pgm_kernels.hpp.inc"
+
+
 template <typename IndexType>
 void match_edge(std::shared_ptr<const HipExecutor> exec,
                 const Array<IndexType> &strongest_neighbor,
-                Array<IndexType> &agg) GKO_NOT_IMPLEMENTED;
+                Array<IndexType> &agg)
+{
+    const auto num = agg.get_num_elems();
+    const dim3 grid(ceildiv(num, default_block_size));
+    hipLaunchKernelGGL(kernel::match_edge_kernel, dim3(grid),
+                       dim3(default_block_size), 0, 0, num,
+                       strongest_neighbor.get_const_data(), agg.get_data());
+}
 
 GKO_INSTANTIATE_FOR_EACH_INDEX_TYPE(GKO_DECLARE_AMGX_PGM_MATCH_EDGE_KERNEL);
 
 
 template <typename IndexType>
 void count_unagg(std::shared_ptr<const HipExecutor> exec,
-                 const Array<IndexType> &agg,
-                 IndexType *num_unagg) GKO_NOT_IMPLEMENTED;
+                 const Array<IndexType> &agg, IndexType *num_unagg)
+{
+    Array<IndexType> active_agg(exec, agg.get_num_elems());
+    const dim3 grid(ceildiv(active_agg.get_num_elems(), default_block_size));
+    hipLaunchKernelGGL(kernel::activate_kernel, dim3(grid),
+                       dim3(default_block_size), 0, 0,
+                       active_agg.get_num_elems(), agg.get_const_data(),
+                       active_agg.get_data());
+    *num_unagg = reduce_add_array(exec, active_agg.get_num_elems(),
+                                  active_agg.get_const_data());
+}
 
 GKO_INSTANTIATE_FOR_EACH_INDEX_TYPE(GKO_DECLARE_AMGX_PGM_COUNT_UNAGG_KERNEL);
 
 
 template <typename IndexType>
 void renumber(std::shared_ptr<const HipExecutor> exec, Array<IndexType> &agg,
-              IndexType *num_agg) GKO_NOT_IMPLEMENTED;
+              IndexType *num_agg)
+{
+    const auto num = agg.get_num_elems();
+    Array<IndexType> agg_map(exec, num + 1);
+    components::fill_array(exec, agg_map.get_data(), agg_map.get_num_elems(),
+                           zero<IndexType>());
+    const dim3 grid(ceildiv(num, default_block_size));
+    hipLaunchKernelGGL(kernel::fill_agg_kernel, dim3(grid),
+                       dim3(default_block_size), 0, 0, num,
+                       agg.get_const_data(), agg_map.get_data());
+    components::prefix_sum(exec, agg_map.get_data(), agg_map.get_num_elems());
+    hipLaunchKernelGGL(kernel::renumber_kernel, dim3(grid),
+                       dim3(default_block_size), 0, 0, num,
+                       agg_map.get_const_data(), agg.get_data());
+    *num_agg = exec->copy_val_to_host(agg_map.get_const_data() + num);
+}
 
 GKO_INSTANTIATE_FOR_EACH_INDEX_TYPE(GKO_DECLARE_AMGX_PGM_RENUMBER_KERNEL);
 
@@ -90,18 +133,45 @@ void find_strongest_neighbor(
     std::shared_ptr<const HipExecutor> exec,
     const matrix::Csr<ValueType, IndexType> *weight_mtx,
     const matrix::Diagonal<ValueType> *diag, Array<IndexType> &agg,
-    Array<IndexType> &strongest_neighbor) GKO_NOT_IMPLEMENTED;
+    Array<IndexType> &strongest_neighbor)
+{
+    const auto num = agg.get_num_elems();
+    const dim3 grid(ceildiv(num, default_block_size));
+    hipLaunchKernelGGL(
+        kernel::find_strongest_neighbor_kernel, dim3(grid),
+        dim3(default_block_size), 0, 0, num, weight_mtx->get_const_row_ptrs(),
+        weight_mtx->get_const_col_idxs(), weight_mtx->get_const_values(),
+        diag->get_const_values(), diag->get_stride(), agg.get_data(),
+        strongest_neighbor.get_data());
+}
 
 GKO_INSTANTIATE_FOR_EACH_NON_COMPLEX_VALUE_AND_INDEX_TYPE(
     GKO_DECLARE_AMGX_PGM_FIND_STRONGEST_NEIGHBOR);
 
 
 template <typename ValueType, typename IndexType>
-void assign_to_exist_agg(
-    std::shared_ptr<const HipExecutor> exec,
-    const matrix::Csr<ValueType, IndexType> *weight_mtx,
-    const matrix::Diagonal<ValueType> *diag, Array<IndexType> &agg,
-    Array<IndexType> &intermediate_agg) GKO_NOT_IMPLEMENTED;
+void assign_to_exist_agg(std::shared_ptr<const HipExecutor> exec,
+                         const matrix::Csr<ValueType, IndexType> *weight_mtx,
+                         const matrix::Diagonal<ValueType> *diag,
+                         Array<IndexType> &agg,
+                         Array<IndexType> &intermediate_agg)
+{
+    auto agg_val = (intermediate_agg.get_num_elems() > 0)
+                       ? intermediate_agg.get_data()
+                       : agg.get_data();
+    const auto num = agg.get_num_elems();
+    const dim3 grid(ceildiv(num, default_block_size));
+    hipLaunchKernelGGL(kernel::assign_to_exist_agg_kernel, dim3(grid),
+                       dim3(default_block_size), 0, 0, num,
+                       weight_mtx->get_const_row_ptrs(),
+                       weight_mtx->get_const_col_idxs(),
+                       weight_mtx->get_const_values(), diag->get_const_values(),
+                       diag->get_stride(), agg.get_const_data(), agg_val);
+    if (intermediate_agg.get_num_elems() > 0) {
+        // Copy the intermediate_agg to agg
+        agg = intermediate_agg;
+    }
+}
 
 GKO_INSTANTIATE_FOR_EACH_NON_COMPLEX_VALUE_AND_INDEX_TYPE(
     GKO_DECLARE_AMGX_PGM_ASSIGN_TO_EXIST_AGG);
@@ -113,7 +183,57 @@ void amgx_pgm_generate(std::shared_ptr<const HipExecutor> exec,
                        const Array<IndexType> &agg,
                        matrix::Csr<ValueType, IndexType> *coarse,
                        matrix::Csr<ValueType, IndexType> *temp)
-    GKO_NOT_IMPLEMENTED;
+{
+    const auto source_nrows = source->get_size()[0];
+    const auto source_nnz = source->get_num_stored_elements();
+    const auto coarse_nrows = coarse->get_size()[0];
+    Array<IndexType> row_map(exec, source_nrows);
+    // fill coarse row pointer as zero
+    components::fill_array(exec, temp->get_row_ptrs(), coarse_nrows + 1,
+                           zero<IndexType>());
+    // compute each source row should be moved and also change column index
+    dim3 grid(ceildiv(source_nrows, default_block_size));
+    // agg source_row (for row size) coarse row source map
+    hipLaunchKernelGGL(kernel::get_source_row_map_kernel, dim3(grid),
+                       dim3(default_block_size), 0, 0, source_nrows,
+                       agg.get_const_data(), source->get_const_row_ptrs(),
+                       temp->get_row_ptrs(), row_map.get_data());
+    // prefix sum of temp_row_ptrs
+    components::prefix_sum(exec, temp->get_row_ptrs(), coarse_nrows + 1);
+    // copy source -> to coarse and change column index
+    hipLaunchKernelGGL(
+        kernel::move_row_kernel, dim3(grid), dim3(default_block_size), 0, 0,
+        source_nrows, agg.get_const_data(), row_map.get_const_data(),
+        source->get_const_row_ptrs(), source->get_const_col_idxs(),
+        as_hip_type(source->get_const_values()), temp->get_const_row_ptrs(),
+        temp->get_col_idxs(), as_hip_type(temp->get_values()));
+    // sort csr
+    csr::sort_by_column_index(exec, temp);
+    // summation of the elements with same position
+    grid = ceildiv(coarse_nrows, default_block_size);
+    hipLaunchKernelGGL(kernel::merge_col_kernel, dim3(grid),
+                       dim3(default_block_size), 0, 0, coarse_nrows,
+                       temp->get_const_row_ptrs(), temp->get_col_idxs(),
+                       as_hip_type(temp->get_values()), coarse->get_row_ptrs());
+    // build the coarse matrix
+    components::prefix_sum(exec, coarse->get_row_ptrs(), coarse_nrows + 1);
+    // prefix sum of coarse->get_row_ptrs
+    const auto coarse_nnz =
+        exec->copy_val_to_host(coarse->get_row_ptrs() + coarse_nrows);
+    // reallocate size of column and values
+    matrix::CsrBuilder<ValueType, IndexType> coarse_builder{coarse};
+    auto &coarse_col_idxs_array = coarse_builder.get_col_idx_array();
+    auto &coarse_vals_array = coarse_builder.get_value_array();
+    coarse_col_idxs_array.resize_and_reset(coarse_nnz);
+    coarse_vals_array.resize_and_reset(coarse_nnz);
+    // copy the result
+    hipLaunchKernelGGL(
+        kernel::copy_to_coarse_kernel, dim3(grid), dim3(default_block_size), 0,
+        0, coarse_nrows, temp->get_const_row_ptrs(), temp->get_const_col_idxs(),
+        as_hip_type(temp->get_const_values()), coarse->get_const_row_ptrs(),
+        coarse_col_idxs_array.get_data(),
+        as_hip_type(coarse_vals_array.get_data()));
+}
 
 GKO_INSTANTIATE_FOR_EACH_VALUE_AND_INDEX_TYPE(GKO_DECLARE_AMGX_PGM_GENERATE);
 
---
+++ hip/test/multigrid/amgx_pgm_kernels.hip.cpp
@@ -0,0 +1,362 @@
+/*******************************<GINKGO LICENSE>******************************
+Copyright (c) 2017-2020, the Ginkgo authors
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+1. Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+contributors may be used to endorse or promote products derived from
+this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+******************************<GINKGO LICENSE>*******************************/
+
+#include <ginkgo/core/multigrid/amgx_pgm.hpp>
+
+
+#include <fstream>
+#include <random>
+#include <string>
+
+
+#include <gtest/gtest.h>
+
+
+#include <ginkgo/core/base/exception.hpp>
+#include <ginkgo/core/base/executor.hpp>
+#include <ginkgo/core/matrix/dense.hpp>
+#include <ginkgo/core/stop/combined.hpp>
+#include <ginkgo/core/stop/iteration.hpp>
+#include <ginkgo/core/stop/residual_norm.hpp>
+
+
+#include "core/multigrid/amgx_pgm_kernels.hpp"
+#include "core/test/utils/matrix_generator.hpp"
+#include "hip/test/utils.hip.hpp"
+
+
+namespace {
+
+
+template <typename Array, typename ValueDistribution, typename Engine>
+Array generate_random_array(gko::size_type num, ValueDistribution &&value_dist,
+                            Engine &&engine,
+                            std::shared_ptr<const gko::Executor> exec)
+{
+    using value_type = typename Array::value_type;
+    Array array_host(exec->get_master(), num);
+    auto val = array_host.get_data();
+    for (int i = 0; i < num; i++) {
+        val[i] =
+            gko::test::detail::get_rand_value<value_type>(value_dist, engine);
+    }
+    Array array(exec);
+    array = array_host;
+    return array;
+}
+
+
+class AmgxPgm : public ::testing::Test {
+protected:
+    using value_type = gko::default_precision;
+    using index_type = gko::int32;
+    using Mtx = gko::matrix::Dense<>;
+    using Csr = gko::matrix::Csr<value_type, index_type>;
+    AmgxPgm() : rand_engine(30) {}
+
+    void SetUp()
+    {
+        ASSERT_GT(gko::HipExecutor::get_num_devices(), 0);
+        ref = gko::ReferenceExecutor::create();
+        hip = gko::HipExecutor::create(0, ref);
+    }
+
+    void TearDown()
+    {
+        if (hip != nullptr) {
+            ASSERT_NO_THROW(hip->synchronize());
+        }
+    }
+
+    std::unique_ptr<Mtx> gen_mtx(int num_rows, int num_cols)
+    {
+        return gko::test::generate_random_matrix<Mtx>(
+            num_rows, num_cols,
+            std::uniform_int_distribution<>(num_cols, num_cols),
+            std::normal_distribution<>(-1.0, 1.0), rand_engine, ref);
+    }
+
+    gko::Array<index_type> gen_array(gko::size_type num, index_type min_val,
+                                     index_type max_val)
+    {
+        return generate_random_array<gko::Array<index_type>>(
+            num, std::uniform_int_distribution<>(min_val, max_val), rand_engine,
+            ref);
+    }
+
+    void initialize_data()
+    {
+        int m = 597;
+        n = 300;
+        int nrhs = 3;
+
+        agg = gen_array(m, 0, n - 1);
+        unfinished_agg = gen_array(m, -1, n - 1);
+        strongest_neighbor = gen_array(m, 0, n - 1);
+        coarse_vector = gen_mtx(n, nrhs);
+        fine_vector = gen_mtx(m, nrhs);
+        auto weight = gen_mtx(m, m);
+        make_weight(weight.get());
+        weight_csr = Csr::create(ref);
+        weight->convert_to(weight_csr.get());
+        weight_diag = weight_csr->extract_diagonal();
+
+        d_agg.set_executor(hip);
+        d_unfinished_agg.set_executor(hip);
+        d_strongest_neighbor.set_executor(hip);
+        d_coarse_vector = Mtx::create(hip);
+        d_fine_vector = Mtx::create(hip);
+        d_weight_csr = Csr::create(hip);
+        d_weight_diag = Mtx::create(hip);
+        d_agg = agg;
+        d_unfinished_agg = unfinished_agg;
+        d_strongest_neighbor = strongest_neighbor;
+        d_coarse_vector->copy_from(coarse_vector.get());
+        d_fine_vector->copy_from(fine_vector.get());
+        d_weight_csr->copy_from(weight_csr.get());
+        d_weight_diag->copy_from(weight_diag.get());
+    }
+
+    void make_symetric(Mtx *mtx)
+    {
+        for (int i = 0; i < mtx->get_size()[0]; ++i) {
+            for (int j = i + 1; j < mtx->get_size()[1]; ++j) {
+                mtx->at(i, j) = mtx->at(j, i);
+            }
+        }
+    }
+
+    // only for real value
+    void make_absoulte(Mtx *mtx)
+    {
+        for (int i = 0; i < mtx->get_size()[0]; ++i) {
+            for (int j = 0; j < mtx->get_size()[1]; ++j) {
+                mtx->at(i, j) = abs(mtx->at(i, j));
+            }
+        }
+    }
+
+    void make_diag_dominant(Mtx *mtx)
+    {
+        using std::abs;
+        for (int i = 0; i < mtx->get_size()[0]; ++i) {
+            auto sum = gko::zero<Mtx::value_type>();
+            for (int j = 0; j < mtx->get_size()[1]; ++j) {
+                sum += abs(mtx->at(i, j));
+            }
+            mtx->at(i, i) = sum;
+        }
+    }
+
+    void make_spd(Mtx *mtx)
+    {
+        make_symetric(mtx);
+        make_diag_dominant(mtx);
+    }
+
+    void make_weight(Mtx *mtx)
+    {
+        make_symetric(mtx);
+        make_absoulte(mtx);
+        make_diag_dominant(mtx);
+    }
+
+    std::shared_ptr<gko::ReferenceExecutor> ref;
+    std::shared_ptr<const gko::HipExecutor> hip;
+
+    std::ranlux48 rand_engine;
+
+    gko::Array<index_type> agg;
+    gko::Array<index_type> unfinished_agg;
+    gko::Array<index_type> strongest_neighbor;
+
+    gko::Array<index_type> d_agg;
+    gko::Array<index_type> d_unfinished_agg;
+    gko::Array<index_type> d_strongest_neighbor;
+
+    std::unique_ptr<Mtx> coarse_vector;
+    std::unique_ptr<Mtx> fine_vector;
+    std::unique_ptr<Mtx> weight_diag;
+    std::unique_ptr<Csr> weight_csr;
+
+    std::unique_ptr<Mtx> d_coarse_vector;
+    std::unique_ptr<Mtx> d_fine_vector;
+    std::unique_ptr<Mtx> d_weight_diag;
+    std::unique_ptr<Csr> d_weight_csr;
+
+    gko::size_type n;
+};
+
+
+TEST_F(AmgxPgm, RestrictApplyIsEquivalentToRef)
+{
+    initialize_data();
+    // fine->coarse
+    auto x = Mtx::create_with_config_of(gko::lend(coarse_vector));
+    auto d_x = Mtx::create_with_config_of(gko::lend(d_coarse_vector));
+
+    gko::kernels::reference::amgx_pgm::restrict_apply(
+        ref, agg, fine_vector.get(), x.get());
+    gko::kernels::hip::amgx_pgm::restrict_apply(hip, d_agg, d_fine_vector.get(),
+                                                d_x.get());
+
+    GKO_ASSERT_MTX_NEAR(d_x, x, 1e-14);
+}
+
+
+TEST_F(AmgxPgm, ProlongApplyaddIsEquivalentToRef)
+{
+    initialize_data();
+    // coarse->fine
+    auto x = fine_vector->clone();
+    auto d_x = d_fine_vector->clone();
+
+    gko::kernels::reference::amgx_pgm::prolong_applyadd(
+        ref, agg, coarse_vector.get(), x.get());
+    gko::kernels::hip::amgx_pgm::prolong_applyadd(
+        hip, d_agg, d_coarse_vector.get(), d_x.get());
+
+    GKO_ASSERT_MTX_NEAR(d_x, x, 1e-14);
+}
+
+
+TEST_F(AmgxPgm, MatchEdgeIsEquivalentToRef)
+{
+    initialize_data();
+    auto x = unfinished_agg;
+    auto d_x = d_unfinished_agg;
+
+    gko::kernels::reference::amgx_pgm::match_edge(ref, strongest_neighbor, x);
+    gko::kernels::hip::amgx_pgm::match_edge(hip, d_strongest_neighbor, d_x);
+
+    GKO_ASSERT_ARRAY_EQ(d_x, x);
+}
+
+
+TEST_F(AmgxPgm, CountUnaggIsEquivalentToRef)
+{
+    initialize_data();
+    gko::size_type num_unagg;
+    gko::size_type d_num_unagg;
+
+    gko::kernels::reference::amgx_pgm::count_unagg(ref, agg, &num_unagg);
+    gko::kernels::hip::amgx_pgm::count_unagg(hip, d_agg, &d_num_unagg);
+
+    ASSERT_EQ(d_num_unagg, num_unagg);
+}
+
+
+TEST_F(AmgxPgm, RenumberIsEquivalentToRef)
+{
+    initialize_data();
+    auto x = unfinished_agg;
+    auto d_x = d_unfinished_agg;
+    gko::size_type num_agg;
+    gko::size_type d_num_agg;
+
+    gko::kernels::reference::amgx_pgm::renumber(ref, agg, &num_agg);
+    gko::kernels::hip::amgx_pgm::renumber(hip, d_agg, &d_num_agg);
+
+    ASSERT_EQ(d_num_agg, num_agg);
+    GKO_ASSERT_ARRAY_EQ(d_agg, agg);
+    ASSERT_LE(num_agg, 300);
+}
+
+
+TEST_F(AmgxPgm, FindStrongestNeighborIsEquivalentToRef)
+{
+    initialize_data();
+    auto snb = strongest_neighbor;
+    auto d_snb = d_strongest_neighbor;
+
+    gko::kernels::reference::amgx_pgm::find_strongest_neighbor(
+        ref, weight_csr.get(), weight_diag.get(), agg, snb);
+    gko::kernels::hip::amgx_pgm::find_strongest_neighbor(
+        hip, d_weight_csr.get(), d_weight_diag.get(), d_agg, d_snb);
+
+    GKO_ASSERT_ARRAY_EQ(d_snb, snb);
+}
+
+
+TEST_F(AmgxPgm, AssignToExistAggIsEquivalentToRef)
+{
+    initialize_data();
+    auto x = unfinished_agg;
+    auto d_x = d_unfinished_agg;
+    auto intermediate_agg = x;
+    auto d_intermediate_agg = d_x;
+
+    gko::kernels::reference::amgx_pgm::assign_to_exist_agg(
+        ref, weight_csr.get(), weight_diag.get(), x, intermediate_agg);
+    gko::kernels::hip::amgx_pgm::assign_to_exist_agg(
+        hip, d_weight_csr.get(), d_weight_diag.get(), d_x, d_intermediate_agg);
+
+    GKO_ASSERT_ARRAY_EQ(d_x, x);
+}
+
+
+TEST_F(AmgxPgm, AssignToExistAggUnderteminsticIsEquivalentToRef)
+{
+    initialize_data();
+    auto d_x = d_unfinished_agg;
+    auto d_intermediate_agg = gko::Array<index_type>(hip, 0);
+    gko::size_type d_num_unagg;
+
+    gko::kernels::hip::amgx_pgm::assign_to_exist_agg(
+        hip, d_weight_csr.get(), d_weight_diag.get(), d_x, d_intermediate_agg);
+    gko::kernels::hip::amgx_pgm::count_unagg(hip, d_agg, &d_num_unagg);
+
+    // only test whether all elements are aggregated.
+    GKO_ASSERT_EQ(d_num_unagg, 0);
+}
+
+
+TEST_F(AmgxPgm, GenerateMtxIsEquivalentToRef)
+{
+    initialize_data();
+    auto csr_coarse = Csr::create(ref, gko::dim<2>{n, n}, 0);
+    auto d_csr_coarse = Csr::create(hip, gko::dim<2>{n, n}, 0);
+    auto csr_temp = Csr::create(ref, gko::dim<2>{n, n},
+                                weight_csr->get_num_stored_elements());
+    auto d_csr_temp = Csr::create(hip, gko::dim<2>{n, n},
+                                  d_weight_csr->get_num_stored_elements());
+
+    gko::kernels::hip::amgx_pgm::amgx_pgm_generate(
+        hip, d_weight_csr.get(), d_agg, d_csr_coarse.get(), d_csr_temp.get());
+    gko::kernels::reference::amgx_pgm::amgx_pgm_generate(
+        ref, weight_csr.get(), agg, csr_coarse.get(), csr_temp.get());
+
+    GKO_ASSERT_MTX_NEAR(d_csr_coarse, csr_coarse, 1e-14);
+}
+
+
+}  // namespace
