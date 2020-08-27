050fd9f3262c780a170f83b3e8115772127570fc
Lukas Riemer <lksriemer@gmail.com>
2020-08-27 13:51:29 +0200

    Implement standard tests for Rcm

--- core/test/reorder/rcm.cpp
+++ core/test/reorder/rcm.cpp
@@ -50,23 +50,116 @@ class Rcm : public ::testing::Test {
 protected:
     using v_type = double;
     using i_type = int;
-    using Mtx = gko::matrix::Dense<v_type>;
-    using reorder_factory_type = gko::reorder::Rcm<v_type, i_type>;
+    using reorder_type = gko::reorder::Rcm<v_type, i_type>;
 
     Rcm()
         : exec(gko::ReferenceExecutor::create()),
-          rcm_factory(reorder_factory_type::build().on(exec))
+          rcm_factory(reorder_type::build().on(exec))
     {}
 
     std::shared_ptr<const gko::Executor> exec;
-    std::unique_ptr<reorder_factory_type::Factory> rcm_factory;
+    std::unique_ptr<reorder_type::Factory> rcm_factory;
 };
-
 
 TEST_F(Rcm, RcmFactoryKnowsItsExecutor)
 {
-    ASSERT_EQ(rcm_factory->get_executor(), exec);
+    ASSERT_EQ(rcm_factory->get_executor(), this->exec);
 }
 
+TEST_F(Rcm, CanBeCopied)
+{
+    auto rcm =
+        rcm_factory->generate(gko::initialize<gko::matrix::Dense<v_type>>(
+            3, {{1.0, 0.0, 0.0}, {0.0, 1.0, 0.0}, {0.0, 0.0, 1.0}},
+            this->exec));
+    auto rcm_copy =
+        rcm_factory->generate(gko::initialize<gko::matrix::Dense<v_type>>(
+            3, {{1.0, 0.0, 1.0}, {0.0, 1.0, 0.0}, {1.0, 0.0, 1.0}},
+            this->exec));
+
+    rcm_copy->copy_from(rcm.get());
+
+    ASSERT_EQ(rcm_copy->get_permutation()->get_const_permutation()[0], 2);
+    ASSERT_EQ(rcm_copy->get_permutation()->get_const_permutation()[1], 1);
+    ASSERT_EQ(rcm_copy->get_permutation()->get_const_permutation()[2], 0);
+    ASSERT_NE(rcm->get_adjacency_matrix(), rcm_copy->get_adjacency_matrix());
+}
+
+
+TEST_F(Rcm, CanBeMoved)
+{
+    auto rcm =
+        rcm_factory->generate(gko::initialize<gko::matrix::Dense<v_type>>(
+            3, {{1.0, 0.0, 0.0}, {0.0, 1.0, 0.0}, {0.0, 0.0, 1.0}},
+            this->exec));
+    auto rcm_move =
+        rcm_factory->generate(gko::initialize<gko::matrix::Dense<v_type>>(
+            3, {{1.0, 0.0, 1.0}, {0.0, 1.0, 0.0}, {1.0, 0.0, 1.0}},
+            this->exec));
+
+    rcm->move_to(rcm_move.get());
+
+    ASSERT_EQ(rcm_move->get_permutation()->get_const_permutation()[0], 2);
+    ASSERT_EQ(rcm_move->get_permutation()->get_const_permutation()[1], 1);
+    ASSERT_EQ(rcm_move->get_permutation()->get_const_permutation()[2], 0);
+}
+
+
+TEST_F(Rcm, CanBeCloned)
+{
+    auto rcm =
+        rcm_factory->generate(gko::initialize<gko::matrix::Dense<v_type>>(
+            3, {{1.0, 0.0, 0.0}, {0.0, 1.0, 0.0}, {0.0, 0.0, 1.0}},
+            this->exec));
+
+    auto rcm_clone = rcm->clone();
+
+    ASSERT_EQ(rcm_clone->get_permutation()->get_const_permutation()[0], 2);
+    ASSERT_EQ(rcm_clone->get_permutation()->get_const_permutation()[1], 1);
+    ASSERT_EQ(rcm_clone->get_permutation()->get_const_permutation()[2], 0);
+    ASSERT_NE(rcm->get_adjacency_matrix(), rcm_clone->get_adjacency_matrix());
+}
+
+TEST_F(Rcm, HasSensibleDefaults)
+{
+    auto rcm = reorder_type::build()
+                   .on(this->exec)
+                   ->generate(gko::initialize<gko::matrix::Dense<v_type>>(
+                       3, {{1.0, 0.0, 0.0}, {0.0, 1.0, 0.0}, {0.0, 0.0, 1.0}},
+                       this->exec));
+
+    ASSERT_EQ(rcm->get_parameters().construct_inverse_permutation, false);
+    ASSERT_EQ(rcm->get_parameters().strategy,
+              gko::reorder::starting_strategy::pseudo_peripheral);
+}
+
+TEST_F(Rcm, CanBeCreatedWithStartingStrategy)
+{
+    auto rcm =
+        reorder_type::build()
+            .with_strategy(gko::reorder::starting_strategy::minimum_degree)
+            .on(this->exec)
+            ->generate(gko::initialize<gko::matrix::Dense<v_type>>(
+                3, {{1.0, 0.0, 0.0}, {0.0, 1.0, 0.0}, {0.0, 0.0, 1.0}},
+                this->exec));
+
+    ASSERT_EQ(rcm->get_parameters().strategy,
+              gko::reorder::starting_strategy::minimum_degree);
+}
+
+TEST_F(Rcm, CanBeCreatedWithConstructInversePermutation)
+{
+    auto rcm = reorder_type::build()
+                   .with_construct_inverse_permutation(true)
+                   .on(this->exec)
+                   ->generate(gko::initialize<gko::matrix::Dense<v_type>>(
+                       3, {{1.0, 0.0, 0.0}, {0.0, 1.0, 0.0}, {0.0, 0.0, 1.0}},
+                       this->exec));
+
+    ASSERT_EQ(rcm->get_inverse_permutation()->get_const_permutation()[0], 2);
+    ASSERT_EQ(rcm->get_inverse_permutation()->get_const_permutation()[1], 1);
+    ASSERT_EQ(rcm->get_inverse_permutation()->get_const_permutation()[2], 0);
+    ASSERT_EQ(rcm->get_parameters().construct_inverse_permutation, true);
+}
 
 }  // namespace
