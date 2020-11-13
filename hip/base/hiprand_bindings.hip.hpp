a726de61c6b421a3d71c90c79beddadce704798a
Fritz Goebel <goebel.fritz@googlemail.com>
2020-11-13 12:41:06 +0100

    Add math header to curand and hiprand bindings, use hiprand image for subdir_build

--- hip/base/hiprand_bindings.hip.hpp
+++ hip/base/hiprand_bindings.hip.hpp
@@ -40,6 +40,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <ginkgo/core/base/exception_helpers.hpp>
 
 
+#include "hip/base/math.hip.hpp"
 #include "hip/base/types.hip.hpp"
 
 
