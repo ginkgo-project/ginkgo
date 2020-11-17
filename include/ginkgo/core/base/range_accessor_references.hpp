52ee142b18663c3c3b994aefcf1f3abc49411ffb
Thomas Grützmacher <thomas.gruetzmacher@kit.edu>
2020-11-17 04:19:33 +0100

    Add missing include in range_accessor_references.hpp

--- include/ginkgo/core/base/range_accessor_references.hpp
+++ include/ginkgo/core/base/range_accessor_references.hpp
@@ -39,6 +39,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
 #include <ginkgo/core/base/std_extensions.hpp>
+#include <ginkgo/core/base/types.hpp>
 
 
 // CUDA TOOLKIT < 11 does not support constexpr in combination with
