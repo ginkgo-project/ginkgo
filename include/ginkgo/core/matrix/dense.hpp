5661669166c21cc6c4c80a314b19f8604bb807b5
Tobias Ribizel <ribizel@kit.edu>
2020-11-19 08:45:32 +0100

    wip

--- include/ginkgo/core/matrix/dense.hpp
+++ include/ginkgo/core/matrix/dense.hpp
@@ -622,23 +622,13 @@ public:
             stride);
     }
 
-    gko::dim<2> get_local_size() const { return local_size_; }
-
     /*
      * Create a local view from the original matrix.
      *
      * @param rows     row span
      * @param columns  column span
      */
-    std::unique_ptr<Dense> create_local_view()
-    {
-        auto sub_exec = this->get_executor() /*->get_subexecutor()*/;
-        return Dense::create(
-            sub_exec, this->get_local_size(),
-            Array<ValueType>::view(sub_exec, this->values_.get_num_elems(),
-                                   this->get_values()),
-            this->get_stride());
-    }
+    std::unique_ptr<Dense> create_local_view();
 
     /*
      * Create a submatrix from the original matrix.
@@ -646,16 +636,7 @@ public:
      * @param rows     row span
      * @param columns  column span
      */
-    std::unique_ptr<const Dense> create_local_view() const
-    {
-        auto sub_exec = this->get_executor() /*->get_subexecutor()*/;
-        return Dense::create(
-            sub_exec, this->get_local_size(),
-            Array<ValueType>::view(
-                sub_exec, this->values_.get_num_elems(),
-                const_cast<ValueType *>(this->get_const_values())),
-            this->get_stride());
-    }
+    std::unique_ptr<const Dense> create_local_view() const;
 
 protected:
     /**
@@ -821,7 +802,6 @@ protected:
 private:
     Array<value_type> values_;
     size_type stride_;
-    gko::dim<2> local_size_;
 };
 
 
