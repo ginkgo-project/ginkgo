65849807a7971a35ca2278f169dd39f888a7aff7
Aditya Kashi <aditya.kashi@kit.edu>
2020-12-01 12:22:30 +0100

    fbcsr complex sample class now uses correct index types

--- include/ginkgo/core/matrix/fbcsr.hpp
+++ include/ginkgo/core/matrix/fbcsr.hpp
@@ -30,8 +30,8 @@ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ******************************<GINKGO LICENSE>*******************************/
 
-#ifndef GKO_CORE_MATRIX_FBCSR_HPP_
-#define GKO_CORE_MATRIX_FBCSR_HPP_
+#ifndef GKO_PUBLIC_CORE_MATRIX_FBCSR_HPP_
+#define GKO_PUBLIC_CORE_MATRIX_FBCSR_HPP_
 
 
 #include <ginkgo/core/base/array.hpp>
@@ -468,4 +468,4 @@ private:
 }  // namespace gko
 
 
-#endif  // GKO_CORE_MATRIX_FBCSR_HPP_
+#endif  // GKO_PUBLIC_CORE_MATRIX_FBCSR_HPP_
