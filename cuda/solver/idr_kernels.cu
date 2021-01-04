1f3cfa0f3f081e52141edaf02248d11793494e75
Yuhsiang M. Tsai <yhmtsai@gmail.com>
2021-01-04 17:44:48 +0100

    bump copyright year to 2021

--- cuda/solver/idr_kernels.cu
+++ cuda/solver/idr_kernels.cu
@@ -1,5 +1,5 @@
 /*******************************<GINKGO LICENSE>******************************
-Copyright (c) 2017-2020, the Ginkgo authors
+Copyright (c) 2017-2021, the Ginkgo authors
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
@@ -33,8 +33,8 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "core/solver/idr_kernels.hpp"
 
 
-#include <random>
 #include <time.h>
+#include <random>
 
 
 #include <ginkgo/core/base/exception_helpers.hpp>
