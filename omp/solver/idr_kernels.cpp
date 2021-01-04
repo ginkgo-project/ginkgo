a659a1be19e52c77244b26d3b4b6c9089411e2ba
Yuhsiang M. Tsai <yhmtsai@gmail.com>
2021-01-04 17:44:48 +0100

    bump copyright year to 2021

--- omp/solver/idr_kernels.cpp
+++ omp/solver/idr_kernels.cpp
@@ -1,5 +1,5 @@
 /*******************************<GINKGO LICENSE>******************************
-Copyright (c) 2017-2020, the Ginkgo authors
+Copyright (c) 2017-2021, the Ginkgo authors
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
@@ -33,10 +33,10 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "core/solver/idr_kernels.hpp"
 
 
+#include <time.h>
 #include <algorithm>
 #include <iostream>
 #include <random>
-#include <time.h>
 
 
 #include <omp.h>
