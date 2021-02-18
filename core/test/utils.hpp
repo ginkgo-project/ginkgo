c65cabcc16b31f084dd3691acce9c0cec34aad91
Pratik Nayak <pratikvn@protonmail.com>
2021-02-19 00:32:45 +0100

    Add BatchDense initialize methods and tests.

--- core/test/utils.hpp
+++ core/test/utils.hpp
@@ -69,6 +69,8 @@ using ValueTypes =
     ::testing::Types<float, double, std::complex<float>, std::complex<double>>;
 #endif
 
+using RealValueTypes = ::testing::Types<float, double>;
+
 using ComplexValueTypes =
 #if GINKGO_DPCPP_SINGLE_MODE
     ::testing::Types<std::complex<float>>;
