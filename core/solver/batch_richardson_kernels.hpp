625241ed51dacc2898eada38a6cb2934107f81e7
Aditya Kashi <aditya.kashi@kit.edu>
2021-03-22 11:03:56 +0100

    Preliminary implementation of batch Richardson solver
    For now, intermediate vectors in the solver use static shared memory.
    This will have to be revisited.
    
    Adds
    - public interface for batch richardson solver
    - core module for batch richardson
    - reference and cuda implementations of batch richardson
    - batch Jacobi preconditioner for reference and cuda
    - one batch stopping criterion for reference and cuda
    Batch logger is not yet implemented.

---
+++ core/solver/batch_richardson_kernels.hpp
@@ -0,0 +1,126 @@
+/*******************************<GINKGO LICENSE>******************************
+Copyright (c) 2017-2021, the Ginkgo authors
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+1. Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+contributors may be used to endorse or promote products derived from
+this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+******************************<GINKGO LICENSE>*******************************/
+
+#ifndef GKO_CORE_SOLVER_BATCH_RICHARDSON_KERNELS_HPP_
+#define GKO_CORE_SOLVER_BATCH_RICHARDSON_KERNELS_HPP_
+
+
+#include <ginkgo/core/matrix/batch_csr.hpp>
+#include <ginkgo/core/matrix/batch_dense.hpp>
+
+
+namespace gko {
+namespace kernels {
+namespace batch_rich {
+
+
+/**
+ * Options controlling the batch Richardson solver.
+ */
+template <typename RealType>
+struct BatchRichardsonOptions {
+    std::string preconditioner;
+    int max_its;
+    RealType rel_residual_tol;
+    RealType relax_factor;
+};
+
+
+#define GKO_DECLARE_BATCH_RICHARDSON_APPLY_KERNEL(_type)                       \
+    void apply(std::shared_ptr<const DefaultExecutor> exec,                    \
+               const gko::kernels::batch_rich::BatchRichardsonOptions<         \
+                   remove_complex<_type>> &options,                            \
+               const LinOp *const a, const matrix::BatchDense<_type> *const b, \
+               matrix::BatchDense<_type> *const x)
+
+
+#define GKO_DECLARE_ALL_AS_TEMPLATES \
+    template <typename ValueType>    \
+    GKO_DECLARE_BATCH_RICHARDSON_APPLY_KERNEL(ValueType)
+
+
+}  // namespace batch_rich
+
+
+namespace omp {
+namespace batch_rich {
+
+GKO_DECLARE_ALL_AS_TEMPLATES;
+
+}  // namespace batch_rich
+}  // namespace omp
+
+
+namespace cuda {
+namespace batch_rich {
+
+GKO_DECLARE_ALL_AS_TEMPLATES;
+
+}  // namespace batch_rich
+}  // namespace cuda
+
+
+namespace reference {
+namespace batch_rich {
+
+GKO_DECLARE_ALL_AS_TEMPLATES;
+
+}  // namespace batch_rich
+}  // namespace reference
+
+
+namespace hip {
+namespace batch_rich {
+
+GKO_DECLARE_ALL_AS_TEMPLATES;
+
+}  // namespace batch_rich
+}  // namespace hip
+
+
+namespace dpcpp {
+namespace batch_rich {
+
+GKO_DECLARE_ALL_AS_TEMPLATES;
+
+}  // namespace batch_rich
+}  // namespace dpcpp
+
+
+#undef GKO_DECLARE_ALL_AS_TEMPLATES
+
+
+}  // namespace kernels
+}  // namespace gko
+
+
+#endif
