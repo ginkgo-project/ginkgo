f8da2d1aba79f7835d0c85442f3af8bd490e58b8
Aditya Kashi <aditya.kashi@kit.edu>
2021-03-22 11:03:56 +0100

    Preliminary implementation of batch Richardson solver
    For now, intermediate vectors in the solver use static shared memory.
    This will have to be revisited.
    
    Adds
    - public interface for batch richardson solver
    - core module for batch richardson
    - reference and cuda implementations of batch richardson
    - batch Jacobi preconditioner for reference and cuda
    - one batch stopping criterion for reference and cuda
    Batch logger is not yet implemented.

---
+++ core/solver/batch_richardson.cpp
@@ -0,0 +1,110 @@
+/*******************************<GINKGO LICENSE>******************************
+Copyright (c) 2017-2021, the Ginkgo authors
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+1. Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+contributors may be used to endorse or promote products derived from
+this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+******************************<GINKGO LICENSE>*******************************/
+
+#include <ginkgo/core/solver/batch_richardson.hpp>
+
+
+#include <ginkgo/core/matrix/batch_dense.hpp>
+#include <ginkgo/core/preconditioner/batch_preconditioner_strings.hpp>
+
+
+#include "core/solver/batch_richardson_kernels.hpp"
+
+
+namespace gko {
+namespace solver {
+namespace batch_rich {
+
+
+GKO_REGISTER_OPERATION(apply, batch_rich::apply);
+
+
+}  // namespace batch_rich
+
+
+template <typename ValueType>
+std::unique_ptr<LinOp> BatchRichardson<ValueType>::transpose() const
+{
+    return build()
+        .with_relaxation_factor(parameters_.relaxation_factor)
+        .on(this->get_executor())
+        ->generate(
+            share(as<Transposable>(this->get_system_matrix())->transpose()));
+}
+
+
+template <typename ValueType>
+std::unique_ptr<LinOp> BatchRichardson<ValueType>::conj_transpose() const
+{
+    return build()
+        .with_relaxation_factor(conj(parameters_.relaxation_factor))
+        .on(this->get_executor())
+        ->generate(share(
+            as<Transposable>(this->get_system_matrix())->conj_transpose()));
+}
+
+
+template <typename ValueType>
+void BatchRichardson<ValueType>::apply_impl(const LinOp *b, LinOp *x) const
+{
+    using Vector = matrix::BatchDense<ValueType>;
+    auto exec = this->get_executor();
+    auto dense_b = as<const Vector>(b);
+    auto dense_x = as<Vector>(x);
+    const kernels::batch_rich::BatchRichardsonOptions<remove_complex<ValueType>>
+        opts{parameters_.preconditioner, parameters_.max_iterations,
+             parameters_.rel_residual_tol, parameters_.relaxation_factor};
+
+    exec->run(
+        batch_rich::make_apply(opts, system_matrix_.get(), dense_b, dense_x));
+}
+
+
+template <typename ValueType>
+void BatchRichardson<ValueType>::apply_impl(const LinOp *alpha, const LinOp *b,
+                                            const LinOp *beta, LinOp *x) const
+{
+    auto dense_x = as<matrix::BatchDense<ValueType>>(x);
+
+    auto x_clone = dense_x->clone();
+    this->apply(b, x_clone.get());
+    dense_x->scale(beta);
+    dense_x->add_scaled(alpha, x_clone.get());
+}
+
+
+#define GKO_DECLARE_BATCH_RICHARDSON(_type) class BatchRichardson<_type>
+GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE(GKO_DECLARE_BATCH_RICHARDSON);
+
+
+}  // namespace solver
+}  // namespace gko
