311c6e02c41f48ef9507753375a64473600a34be
Aditya Kashi <aditya.kashi@kit.edu>
2021-03-22 11:03:56 +0100

    Preliminary implementation of batch Richardson solver
    For now, intermediate vectors in the solver use static shared memory.
    This will have to be revisited.
    
    Adds
    - public interface for batch richardson solver
    - core module for batch richardson
    - reference and cuda implementations of batch richardson
    - batch Jacobi preconditioner for reference and cuda
    - one batch stopping criterion for reference and cuda
    Batch logger is not yet implemented.

---
+++ hip/solver/batch_richardson_kernels.hip.cpp
@@ -0,0 +1,63 @@
+/*******************************<GINKGO LICENSE>******************************
+Copyright (c) 2017-2021, the Ginkgo authors
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+1. Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+contributors may be used to endorse or promote products derived from
+this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+******************************<GINKGO LICENSE>*******************************/
+
+#include "core/solver/batch_richardson_kernels.hpp"
+
+
+namespace gko {
+namespace kernels {
+namespace hip {
+/**
+ * @brief The batch Richardson solver namespace.
+ *
+ * @ingroup batch_rich
+ */
+namespace batch_rich {
+
+
+template <typename T>
+using BatchRichardsonOptions =
+    gko::kernels::batch_rich::BatchRichardsonOptions<T>;
+
+template <typename ValueType>
+void apply(std::shared_ptr<const HipExecutor> exec,
+           const BatchRichardsonOptions<remove_complex<ValueType>> &opts,
+           const LinOp *const a, const matrix::BatchDense<ValueType> *const b,
+           matrix::BatchDense<ValueType> *const x) GKO_NOT_IMPLEMENTED;
+
+GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE(GKO_DECLARE_BATCH_RICHARDSON_APPLY_KERNEL);
+
+
+}  // namespace batch_rich
+}  // namespace hip
+}  // namespace kernels
+}  // namespace gko
