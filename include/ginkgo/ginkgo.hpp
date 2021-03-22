bbe004941676acbed42b7f07ad9764fb7a646d5d
Aditya Kashi <aditya.kashi@kit.edu>
2021-03-22 12:15:50 +0100

    added batch preconditioner strings header

--- include/ginkgo/ginkgo.hpp
+++ include/ginkgo/ginkgo.hpp
@@ -98,6 +98,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <ginkgo/core/multigrid/amgx_pgm.hpp>
 #include <ginkgo/core/multigrid/multigrid_level.hpp>
 
+#include <ginkgo/core/preconditioner/batch_preconditioner_strings.hpp>
 #include <ginkgo/core/preconditioner/ic.hpp>
 #include <ginkgo/core/preconditioner/ilu.hpp>
 #include <ginkgo/core/preconditioner/isai.hpp>
