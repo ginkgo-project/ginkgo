8b1986d99505ba7939043d163bc42a828e9957f0
Aditya Kashi <aditya.kashi@kit.edu>
2021-03-22 11:03:56 +0100

    Preliminary implementation of batch Richardson solver
    For now, intermediate vectors in the solver use static shared memory.
    This will have to be revisited.
    
    Adds
    - public interface for batch richardson solver
    - core module for batch richardson
    - reference and cuda implementations of batch richardson
    - batch Jacobi preconditioner for reference and cuda
    - one batch stopping criterion for reference and cuda
    Batch logger is not yet implemented.

---
+++ cuda/solver/batch_richardson_kernels.cu
@@ -0,0 +1,125 @@
+/*******************************<GINKGO LICENSE>******************************
+Copyright (c) 2017-2021, the Ginkgo authors
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+1. Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+contributors may be used to endorse or promote products derived from
+this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+******************************<GINKGO LICENSE>*******************************/
+
+#include "core/solver/batch_richardson_kernels.hpp"
+
+
+#include <ginkgo/core/base/math.hpp>
+#include <ginkgo/core/preconditioner/batch_preconditioner_strings.hpp>
+
+
+#include "cuda/base/config.hpp"
+#include "cuda/base/types.hpp"
+#include "cuda/components/cooperative_groups.cuh"
+#include "cuda/matrix/batch_struct.hpp"
+
+
+namespace gko {
+namespace kernels {
+namespace cuda {
+
+
+constexpr int default_block_size = 128;
+
+
+/**
+ * @brief The batch Richardson solver namespace.
+ *
+ * @ingroup batch_rich
+ */
+namespace batch_rich {
+
+
+#include "common/matrix/batch_csr_kernels.hpp.inc"
+#include "common/matrix/batch_dense_kernels.hpp.inc"
+#include "common/preconditioner/batch_jacobi.hpp.inc"
+#include "common/stop/batch_criteria.hpp.inc"
+#include "core/matrix/batch_sparse_ops.hpp.inc"
+
+
+#include "common/solver/batch_richardson_kernels.hpp.inc"
+
+
+template <typename T>
+using BatchRichardsonOptions =
+    gko::kernels::batch_rich::BatchRichardsonOptions<T>;
+
+template <typename BatchMatrixType, typename ValueType>
+static void apply_impl(
+    std::shared_ptr<const CudaExecutor> exec,
+    const BatchRichardsonOptions<remove_complex<ValueType>> opts,
+    const BatchMatrixType &a,
+    const gko::batch_dense::UniformBatch<const ValueType> &b,
+    const gko::batch_dense::UniformBatch<ValueType> &x)
+{
+    using real_type = gko::remove_complex<ValueType>;
+    const size_type nbatch = a.num_batch;
+
+    if (opts.preconditioner == gko::preconditioner::batch::jacobi_str) {
+        apply_kernel<BatchJacobi<ValueType>,
+                     stop::RelResidualMaxIter<ValueType>>
+            <<<nbatch, default_block_size>>>(opts.max_its,
+                                             opts.rel_residual_tol,
+                                             opts.relax_factor, a, b, x);
+    } else {
+        GKO_NOT_IMPLEMENTED;
+    }
+}
+
+
+template <typename ValueType>
+void apply(std::shared_ptr<const CudaExecutor> exec,
+           const BatchRichardsonOptions<remove_complex<ValueType>> &opts,
+           const LinOp *const a, const matrix::BatchDense<ValueType> *const b,
+           matrix::BatchDense<ValueType> *const x)
+{
+    using cu_value_type = cuda_type<ValueType>;
+    const gko::batch_dense::UniformBatch<const cu_value_type> b_b =
+        get_batch_struct(b);
+    const gko::batch_dense::UniformBatch<cu_value_type> x_b =
+        get_batch_struct(x);
+    if (auto amat = dynamic_cast<const matrix::BatchCsr<ValueType> *>(a)) {
+        const gko::batch_csr::UniformBatch<const cu_value_type> m_b =
+            get_batch_struct(amat);
+        apply_impl(exec, opts, m_b, b_b, x_b);
+    } else {
+        GKO_NOT_SUPPORTED(a);
+    }
+}
+
+GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE(GKO_DECLARE_BATCH_RICHARDSON_APPLY_KERNEL);
+
+
+}  // namespace batch_rich
+}  // namespace cuda
+}  // namespace kernels
+}  // namespace gko
