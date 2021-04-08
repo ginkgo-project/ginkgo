67c21d81ba58313047316340644c29e9b4894c13
aggarwal2000 <aggarwal2000chd@gmail.com>
2021-04-08 04:23:03 +0200

    Add absolute tol to batch stopping criterion

--- reference/stop/batch_criteria.hpp
+++ reference/stop/batch_criteria.hpp
@@ -35,7 +35,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
 #include <ginkgo/core/base/types.hpp>
-
+#include <ginkgo/core/stop/batch_stop_enum.hpp>
 
 #include "reference/base/config.hpp"
 #include "reference/matrix/batch_dense_kernels.hpp"
@@ -136,6 +136,116 @@ private:
         for (int i = 0; i < nrhs_; i++) {
             if (res_norms[i] / rhs_norms_[i] < rel_tol_) {
                 converged = converged | (1 << i);
+            }
+        }
+    }
+};
+
+
+
+/**
+ * A stopping criterion for batch solvers that comprises a
+ * maximum iteration count as well as relative residual tolerance or absolute
+ * residual tolerance - which one to consider can be chosen.
+ *
+ * At most 32 right-hand-side vectors are supported.
+ */
+template <typename ValueType>
+class AbsAndRelResidualMaxIter {
+public:
+    using real_type = remove_complex<ValueType>;
+    using tolerance = ::gko::stop::batch::ToleranceType;
+    using bitset_type = uint32;
+    static constexpr int max_nrhs = 32;
+
+    /**
+     * Set up the stopping criterion and convergence variable.
+     *
+     * @param num_rhs  The number of right-hand-sides in the linear systems.
+     * @param max_iters  Maximum number of iterations allowed.
+     * @param converged_bitset  A bit-set representing the state of convergence
+     *                          of each RHS: 1 for converged and 0 otherwise.
+     *                          It is initialized appropriately here, and must
+     *                          be passed to the \ref check_converged function.
+     * @param rhs_b_norms  The reference RHS norms.
+     */
+    AbsAndRelResidualMaxIter(const int num_rhs, const int max_iters,
+                             const real_type abs_res_tol,
+                             const real_type rel_res_tol,
+                             const tolerance type_of_tol,
+                             bitset_type &converge_bitset,
+                             const real_type *const rhs_b_norms)
+        : nrhs{num_rhs},
+          rel_tol{rel_res_tol},
+          abs_tol{abs_res_tol},
+          tol_type{type_of_tol},
+          max_its{max_iters},
+          rhs_norms{rhs_b_norms}
+    {
+        if (nrhs > 32) {
+            printf("Batch stopping criterion: Too many right hand sides!\n");
+        }
+        converge_bitset = 0 - (1 << num_rhs);
+    }
+
+    /**
+     * Checks whether the different right hand sides have converged.
+     *
+     * @param iter  The current iteration count.
+     * @param residual_norms  (Optional) current residual norm of each RHS.
+     * @param residual  Current residual vectors. Unused if residual_norms
+     *                  are provided.
+     * @param converged  Bits representing converged (1) or not (0) for each
+     *                   RHS. The 'right-most' bit corresponds to the first RHS.
+     *
+     * @return  True if all RHS have converged, false otherwise.
+     */
+    bool check_converged(
+        const int iter, const real_type *const residual_norms,
+        const gko::batch_dense::BatchEntry<const ValueType> &residual,
+        bitset_type &converged) const
+    {
+        if (iter >= max_its - 1) {
+            return true;
+        }
+
+        if (residual_norms) {
+            check_norms(residual_norms, converged);
+        } else {
+            real_type norms[32];
+            batch_dense::compute_norm2<ValueType>(residual,
+                                                  {norms, 32, 1, nrhs});
+            check_norms(norms, converged);
+        }
+
+        if (converged == all_true) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+private:
+    int nrhs;
+    int max_its;
+    const real_type rel_tol;
+    const real_type abs_tol;
+    const tolerance tol_type;
+    const real_type *const rhs_norms;
+    static constexpr uint32 all_true = detail::pow(2, 32) - 1;
+
+    void check_norms(const real_type *const res_norms,
+                     bitset_type &converged) const
+    {
+        for (int i = 0; i < nrhs; i++) {
+            if (tol_type == tolerance::absolute) {
+                if (res_norms[i] < abs_tol) {
+                    converged = converged | (1 << i);
+                }
+            } else if (tol_type == tolerance::relative) {
+                if (res_norms[i] / rhs_norms[i] < rel_tol) {
+                    converged = converged | (1 << i);
+                }
             }
         }
     }
