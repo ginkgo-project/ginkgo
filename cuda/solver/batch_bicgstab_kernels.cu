adbaf45c1fbc2e09a2b05bcbb7c97b3b17d5f217
aggarwal2000 <aggarwal2000chd@gmail.com>
2021-04-08 04:14:03 +0200

    Add batch bicgstab cuda implementation

--- cuda/solver/batch_bicgstab_kernels.cu
+++ cuda/solver/batch_bicgstab_kernels.cu
@@ -35,7 +35,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include <ginkgo/core/base/math.hpp>
 #include <ginkgo/core/preconditioner/batch_preconditioner_strings.hpp>
-
+#include <ginkgo/core/stop/batch_stop_enum.hpp>
 
 #include "cuda/base/config.hpp"
 #include "cuda/base/types.hpp"
@@ -52,87 +52,81 @@ constexpr int default_block_size = 128;
 
 
 /**
- * @brief The batch Richardson solver namespace.
+ * @brief The batch Bicgstab solver namespace.
  *
- * @ingroup batch_rich
+ * @ingroup batch_bicgstab
  */
-namespace batch_rich {
+namespace batch_bicgstab {
 
 
-#include "common/components/uninitialized_array.hpp.inc"
-
-
-#include "common/log/batch_logger.hpp.inc"
 #include "common/matrix/batch_csr_kernels.hpp.inc"
 #include "common/matrix/batch_dense_kernels.hpp.inc"
+#include "common/preconditioner/batch_identity.hpp.inc"
 #include "common/preconditioner/batch_jacobi.hpp.inc"
 #include "common/stop/batch_criteria.hpp.inc"
 #include "core/matrix/batch_sparse_ops.hpp.inc"
 
 
-template <typename T>
-using BatchRichardsonOptions =
-    gko::kernels::batch_rich::BatchRichardsonOptions<T>;
+#include "common/solver/batch_bicgstab_kernels.hpp.inc"
 
-template <typename BatchMatrixType, typename LogType, typename ValueType>
+
+template <typename T>
+using BatchBicgstabOptions =
+    gko::kernels::batch_bicgstab::BatchBicgstabOptions<T>;
+
+template <typename BatchMatrixType, typename ValueType>
 static void apply_impl(
     std::shared_ptr<const CudaExecutor> exec,
-    const BatchRichardsonOptions<remove_complex<ValueType>> opts,
-    LogType logger, const BatchMatrixType &a,
+    const BatchBicgstabOptions<remove_complex<ValueType>> opts,
+    const BatchMatrixType &a,
     const gko::batch_dense::UniformBatch<const ValueType> &b,
-    const gko::batch_dense::UniformBatch<ValueType> &x) GKO_NOT_IMPLEMENTED;
-//{
-// TODO (script:batch_bicgstab): change the code imported from
-// solver/batch_richardson if needed
-//    using real_type = gko::remove_complex<ValueType>;
-//    const size_type nbatch = a.num_batch;
-//
-//    if (opts.preconditioner == gko::preconditioner::batch::jacobi_str) {
-//        apply_kernel<BatchJacobi<ValueType>,
-//                     stop::RelResidualMaxIter<ValueType>>
-//            <<<nbatch, default_block_size>>>(
-//                opts.max_its, opts.rel_residual_tol, opts.relax_factor,
-//                logger, a, b, x);
-//    } else {
-//        GKO_NOT_IMPLEMENTED;
-//    }
-//}
+    const gko::batch_dense::UniformBatch<ValueType> &x)
+{
+    using real_type = gko::remove_complex<ValueType>;
+    const size_type nbatch = a.num_batch;
+
+    if (opts.preconditioner == gko::preconditioner::batch::jacobi_str) {
+        apply_kernel<BatchJacobi<ValueType>,
+                     stop::AbsAndRelResidualMaxIter<ValueType>>
+            <<<nbatch, default_block_size>>>(
+                opts.max_its, opts.abs_residual_tol, opts.rel_residual_tol,
+                opts.tol_type, a, b, x);
+    } else if (opts.preconditioner == gko::preconditioner::batch::none_str) {
+        apply_kernel<BatchIdentity<ValueType>,
+                     stop::AbsAndRelResidualMaxIter<ValueType>>
+            <<<nbatch, default_block_size>>>(
+                opts.max_its, opts.abs_residual_tol, opts.rel_residual_tol,
+                opts.tol_type, a, b, x);
+    } else {
+        GKO_NOT_IMPLEMENTED;
+    }
+}
 
 
 template <typename ValueType>
 void apply(std::shared_ptr<const CudaExecutor> exec,
-           const BatchRichardsonOptions<remove_complex<ValueType>> &opts,
-           const BatchLinOp *const a,
-           const matrix::BatchDense<ValueType> *const b,
-           matrix::BatchDense<ValueType> *const x,
-           log::BatchLogData<ValueType> &logdata) GKO_NOT_IMPLEMENTED;
-//{
-// TODO (script:batch_bicgstab): change the code imported from
-// solver/batch_richardson if needed
-//    using cu_value_type = cuda_type<ValueType>;
-//
-//    // For now, FinalLogger is the only one available
-//    batch_log::FinalLogger<remove_complex<ValueType>> logger(
-//        static_cast<int>(b->get_size().at(0)[1]), opts.max_its,
-//        logdata.res_norms->get_values(), logdata.iter_counts.get_data());
-//
-//    const gko::batch_dense::UniformBatch<const cu_value_type> b_b =
-//        get_batch_struct(b);
-//    const gko::batch_dense::UniformBatch<cu_value_type> x_b =
-//        get_batch_struct(x);
-//    if (auto amat = dynamic_cast<const matrix::BatchCsr<ValueType> *>(a)) {
-//        const gko::batch_csr::UniformBatch<const cu_value_type> m_b =
-//            get_batch_struct(amat);
-//        apply_impl(exec, opts, logger, m_b, b_b, x_b);
-//    } else {
-//        GKO_NOT_SUPPORTED(a);
-//    }
-//}
+           const BatchBicgstabOptions<remove_complex<ValueType>> &opts,
+           const LinOp *const a, const matrix::BatchDense<ValueType> *const b,
+           matrix::BatchDense<ValueType> *const x)
+{
+    using cu_value_type = cuda_type<ValueType>;
+    const gko::batch_dense::UniformBatch<const cu_value_type> b_b =
+        get_batch_struct(b);
+    const gko::batch_dense::UniformBatch<cu_value_type> x_b =
+        get_batch_struct(x);
+    if (auto amat = dynamic_cast<const matrix::BatchCsr<ValueType> *>(a)) {
+        const gko::batch_csr::UniformBatch<const cu_value_type> m_b =
+            get_batch_struct(amat);
+        apply_impl(exec, opts, m_b, b_b, x_b);
+    } else {
+        GKO_NOT_SUPPORTED(a);
+    }
+}
 
 GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE(GKO_DECLARE_BATCH_BICGSTAB_APPLY_KERNEL);
 
 
-}  // namespace batch_rich
+}  // namespace batch_bicgstab
 }  // namespace cuda
 }  // namespace kernels
 }  // namespace gko
