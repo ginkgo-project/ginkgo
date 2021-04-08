01134d536de47c1eb22874c4d1133625a624d72b
aggarwal2000 <aggarwal2000chd@gmail.com>
2021-04-08 03:56:14 +0200

    Resolve conflicts while adding batch bicgstab public interface and core implementation

---
+++ reference/solver/batch_bicgstab_kernels.cpp
@@ -0,0 +1,238 @@
+/*******************************<GINKGO LICENSE>******************************
+Copyright (c) 2017-2021, the Ginkgo authors
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+1. Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+contributors may be used to endorse or promote products derived from
+this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+******************************<GINKGO LICENSE>*******************************/
+
+#include "core/solver/batch_bicgstab_kernels.hpp"
+
+
+#include <ginkgo/core/preconditioner/batch_preconditioner_strings.hpp>
+
+
+#include "reference/base/config.hpp"
+// include device kernels for every matrix and preconditioner type
+#include "reference/log/batch_logger.hpp"
+#include "reference/matrix/batch_csr_kernels.hpp"
+#include "reference/matrix/batch_dense_kernels.hpp"
+#include "reference/matrix/batch_struct.hpp"
+#include "reference/preconditioner/batch_jacobi.hpp"
+#include "reference/stop/batch_criteria.hpp"
+
+
+namespace gko {
+namespace kernels {
+namespace reference {
+
+
+#include "core/matrix/batch_sparse_ops.hpp.inc"
+
+
+/**
+ * @brief The batch Richardson solver namespace.
+ *
+ * @ingroup batch_rich
+ */
+namespace batch_rich {
+
+
+template <typename T>
+using BatchRichardsonOptions =
+    gko::kernels::batch_rich::BatchRichardsonOptions<T>;
+
+template <typename PrecType, typename StopType, typename LogType,
+          typename BatchMatrixType, typename ValueType>
+static void apply_impl(
+    std::shared_ptr<const ReferenceExecutor> exec,
+    const BatchRichardsonOptions<remove_complex<ValueType>> &opts,
+    LogType logger, const BatchMatrixType *const a,
+    const gko::batch_dense::UniformBatch<const ValueType> *const b,
+    const gko::batch_dense::UniformBatch<ValueType> *const x)
+    GKO_NOT_IMPLEMENTED;
+//{
+// TODO (script:batch_bicgstab): change the code imported from
+// solver/batch_richardson if needed
+//    using real_type = typename gko::remove_complex<ValueType>;
+//    const size_type nbatch = a->num_batch;
+//    const auto nrows = a->num_rows;
+//    const auto nrhs = b->num_rhs;
+//    constexpr int max_nrhs = batch_config<ValueType>::max_num_rhs;
+//    const auto stride = b->stride;
+//
+//    GKO_ASSERT((nrhs == x->num_rhs));
+//    GKO_ASSERT((batch_config<ValueType>::max_num_rows >= nrows * nrhs));
+//
+//    for (size_type ibatch = 0; ibatch < nbatch; ibatch++) {
+//        ValueType residual[batch_config<ValueType>::max_num_rows];
+//        ValueType delta_x[batch_config<ValueType>::max_num_rows];
+//        ValueType prec_work[PrecType::work_size];
+//        uint32 converged = 0;
+//
+//        const typename BatchMatrixType::entry_type a_b =
+//            gko::batch::batch_entry(*a, ibatch);
+//        const gko::batch_dense::BatchEntry<const ValueType> b_b =
+//            gko::batch::batch_entry(*b, ibatch);
+//        const gko::batch_dense::BatchEntry<ValueType> x_b =
+//            gko::batch::batch_entry(*x, ibatch);
+//        const gko::batch_dense::BatchEntry<ValueType> r_b{
+//            residual, static_cast<size_type>(nrhs), nrows, nrhs};
+//        const gko::batch_dense::BatchEntry<ValueType> dx_b{
+//            delta_x, static_cast<size_type>(nrhs), nrows, nrhs};
+//
+//        const ValueType one[] = {1.0};
+//        const gko::batch_dense::BatchEntry<const ValueType> one_b{one, 1, 1,
+//        1}; real_type norms[max_nrhs]; for (int j = 0; j < max_nrhs; j++) {
+//            norms[j] = 0;
+//        }
+//        const gko::batch_dense::BatchEntry<real_type> norms_b{norms, max_nrhs,
+//                                                              1, nrhs};
+//
+//        PrecType prec(a_b, prec_work);
+//
+//        // initial residual
+//        for (int iz = 0; iz < nrows * nrhs; iz++) {
+//            const int i = iz / nrhs;
+//            const int j = iz % nrhs;
+//            r_b.values[i * r_b.stride + j] = b_b.values[i * b_b.stride + j];
+//        }
+//        batch_dense::compute_norm2<ValueType>(gko::batch_dense::to_const(r_b),
+//                                              norms_b);
+//
+//        real_type init_rel_res_norm[max_nrhs];
+//        for (int j = 0; j < nrhs; j++) {
+//            init_rel_res_norm[j] = sqrt(norms_b.values[j]);
+//        }
+//
+//        StopType stop(nrhs, opts.max_its, opts.rel_residual_tol, converged,
+//                      init_rel_res_norm);
+//
+//        int iter = 0;
+//        while (1) {
+//            // r <- r - Adx  This causes instability!
+//            // batch_adv_spmv_single(static_cast<ValueType>(-1.0), a_b,
+//            //
+//            gko::batch_dense::to_const(dx_b),
+//            // static_cast<ValueType>(1.0), r_b);
+//
+//            // r <- b - Ax
+//            for (int iz = 0; iz < nrows * nrhs; iz++) {
+//                const int i = iz / nrhs;
+//                const int j = iz % nrhs;
+//                r_b.values[i * r_b.stride + j] = b_b.values[i * b_b.stride +
+//                j];
+//            }
+//            batch_adv_spmv_single(static_cast<ValueType>(-1.0), a_b,
+//                                  gko::batch_dense::to_const(x_b),
+//                                  static_cast<ValueType>(1.0), r_b);
+//
+//            batch_dense::compute_norm2<ValueType>(
+//                gko::batch_dense::to_const(r_b), norms_b);
+//            for (int j = 0; j < nrhs; j++) {
+//                norms_b.values[j] = sqrt(norms_b.values[j]);
+//            }
+//
+//            const bool all_converged =
+//                stop.check_converged(iter, norms, {NULL, 0, 0, 0}, converged);
+//            logger.log_iteration(ibatch, iter, norms, converged);
+//            if (all_converged) {
+//                break;
+//            }
+//
+//            prec.apply(gko::batch_dense::to_const(r_b), dx_b);
+//
+//            // zero out dx for rhs's which do not need to be updated,
+//            //  though this is unnecessary for this solver.
+//            for (int j = 0; j < nrhs; j++) {
+//                const uint32 conv = converged & (1 << j);
+//                if (conv) {
+//                    for (int i = 0; i < nrows; i++) {
+//                        dx_b.values[i * dx_b.stride + j] = 0.0;
+//                    }
+//                }
+//            }
+//
+//            batch_dense::add_scaled(one_b, gko::batch_dense::to_const(dx_b),
+//                                    x_b);
+//            iter++;
+//        }
+//    }
+//}
+
+template <typename BatchType, typename LoggerType, typename ValueType>
+void apply_select_prec(
+    std::shared_ptr<const ReferenceExecutor> exec,
+    const BatchRichardsonOptions<remove_complex<ValueType>> &opts,
+    const LoggerType logger, const BatchType *const a,
+    const gko::batch_dense::UniformBatch<const ValueType> *const b,
+    const gko::batch_dense::UniformBatch<ValueType> *const x)
+    GKO_NOT_IMPLEMENTED;
+//{
+// TODO (script:batch_bicgstab): change the code imported from
+// solver/batch_richardson if needed
+//    if (opts.preconditioner == gko::preconditioner::batch::jacobi_str) {
+//        apply_impl<BatchJacobi<ValueType>,
+//        stop::RelResidualMaxIter<ValueType>>(
+//            exec, opts, logger, a, b, x);
+//    } else {
+//        GKO_NOT_IMPLEMENTED;
+//    }
+//}
+
+template <typename ValueType>
+void apply(std::shared_ptr<const ReferenceExecutor> exec,
+           const BatchRichardsonOptions<remove_complex<ValueType>> &opts,
+           const BatchLinOp *const a,
+           const matrix::BatchDense<ValueType> *const b,
+           matrix::BatchDense<ValueType> *const x,
+           gko::log::BatchLogData<ValueType> &logdata) GKO_NOT_IMPLEMENTED;
+//{
+// TODO (script:batch_bicgstab): change the code imported from
+// solver/batch_richardson if needed
+//    batch_log::FinalLogger<remove_complex<ValueType>> logger(
+//        b->get_size().at(0)[1], opts.max_its, logdata.res_norms->get_values(),
+//        logdata.iter_counts.get_data());
+//    const gko::batch_dense::UniformBatch<const ValueType> b_b =
+//        get_batch_struct(b);
+//    const gko::batch_dense::UniformBatch<ValueType> x_b = get_batch_struct(x);
+//    if (auto a_mat = dynamic_cast<const matrix::BatchCsr<ValueType> *>(a)) {
+//        const gko::batch_csr::UniformBatch<const ValueType> a_b =
+//            get_batch_struct(a_mat);
+//        apply_select_prec(exec, opts, logger, &a_b, &b_b, &x_b);
+//    } else {
+//        GKO_NOT_IMPLEMENTED;
+//    }
+//}
+
+GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE(GKO_DECLARE_BATCH_BICGSTAB_APPLY_KERNEL);
+
+
+}  // namespace batch_rich
+}  // namespace reference
+}  // namespace kernels
+}  // namespace gko
---
+++ reference/test/solver/batch_bicgstab_kernels.cpp
@@ -0,0 +1,285 @@
+/*******************************<GINKGO LICENSE>******************************
+Copyright (c) 2017-2021, the Ginkgo authors
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+1. Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+contributors may be used to endorse or promote products derived from
+this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+******************************<GINKGO LICENSE>*******************************/
+
+#include "core/solver/batch_bicgstab_kernels.hpp"
+
+
+#include <gtest/gtest.h>
+
+
+#include <ginkgo/core/base/exception.hpp>
+#include <ginkgo/core/base/executor.hpp>
+
+
+#include "core/test/utils.hpp"
+#include "core/test/utils/batch.hpp"
+
+
+namespace {
+
+
+template <typename T>
+class BatchRich : public ::testing::Test {
+protected:
+    using value_type = T;
+    using real_type = gko::remove_complex<value_type>;
+    using Mtx = gko::matrix::BatchCsr<value_type, int>;
+    using BDense = gko::matrix::BatchDense<value_type>;
+    using RBDense = gko::matrix::BatchDense<real_type>;
+
+    BatchRich() : exec(gko::ReferenceExecutor::create())
+    {
+        solve_poisson_uniform_1();
+        solve_poisson_uniform_mult();
+    }
+
+    std::shared_ptr<const gko::ReferenceExecutor> exec;
+
+    const size_t nbatch = 2;
+    std::shared_ptr<BDense> x_1;
+    std::shared_ptr<BDense> xex_1;
+    std::shared_ptr<RBDense> resnorm_1;
+    std::shared_ptr<RBDense> bnorm_1;
+    gko::log::BatchLogData<value_type> logdata_1;
+    const gko::kernels::batch_rich::BatchRichardsonOptions<real_type> opts_1{
+        "jacobi", 500, 1e-6, 1.0};
+
+    const int nrhs = 2;
+    std::shared_ptr<BDense> x_m;
+    std::shared_ptr<BDense> xex_m;
+    std::shared_ptr<RBDense> resnorm_m;
+    std::shared_ptr<RBDense> bnorm_m;
+    gko::log::BatchLogData<value_type> logdata_m;
+    const gko::kernels::batch_rich::BatchRichardsonOptions<real_type> opts_m{
+        "jacobi", 100, 1e-6, 1.0};
+
+    void solve_poisson_uniform_1()
+    {
+        const int nrows = 3;
+        const int nrhs_1 = 1;
+        auto mtx = gko::test::create_poisson1d_batch<value_type>(this->exec,
+                                                                 nrows, nbatch);
+        auto b =
+            gko::batch_initialize<BDense>(nbatch, {-1.0, 3.0, 1.0}, this->exec);
+        x_1 =
+            gko::batch_initialize<BDense>(nbatch, {0.0, 0.0, 0.0}, this->exec);
+        xex_1 =
+            gko::batch_initialize<BDense>(nbatch, {1.0, 3.0, 2.0}, this->exec);
+
+        std::vector<gko::dim<2>> sizes(nbatch, gko::dim<2>(1, nrhs_1));
+        logdata_1.res_norms =
+            gko::matrix::BatchDense<real_type>::create(this->exec, sizes);
+        logdata_1.iter_counts.set_executor(this->exec);
+        logdata_1.iter_counts.resize_and_reset(nrhs_1 * nbatch);
+
+        gko::kernels::reference::batch_rich::apply<value_type>(
+            this->exec, opts_1, mtx.get(), b.get(), x_1.get(), logdata_1);
+
+        std::unique_ptr<BDense> res = b->clone();
+        bnorm_1 = gko::batch_initialize<gko::matrix::BatchDense<real_type>>(
+            nbatch, {0.0}, this->exec);
+        b->compute_norm2(bnorm_1.get());
+        resnorm_1 = gko::batch_initialize<gko::matrix::BatchDense<real_type>>(
+            nbatch, {0.0}, this->exec);
+        auto alpha = gko::batch_initialize<BDense>(nbatch, {-1.0}, this->exec);
+        auto beta = gko::batch_initialize<BDense>(nbatch, {1.0}, this->exec);
+        mtx->apply(alpha.get(), x_1.get(), beta.get(), res.get());
+        res->compute_norm2(resnorm_1.get());
+    }
+
+    int single_iters_regression() const
+    {
+        if (std::is_same<real_type, float>::value) {
+            return 50;
+        } else if (std::is_same<real_type, double>::value) {
+            return 80;
+        } else {
+            return -1;
+        }
+    }
+
+    void solve_poisson_uniform_mult()
+    {
+        const int nrows = 3;
+        auto mtx = gko::test::create_poisson1d_batch<value_type>(this->exec,
+                                                                 nrows, nbatch);
+        auto b = gko::batch_initialize<BDense>(
+            nbatch,
+            std::initializer_list<std::initializer_list<value_type>>{
+                {-1.0, 2.0}, {3.0, -1.0}, {1.0, 0.0}},
+            this->exec);
+        x_m = gko::batch_initialize<BDense>(
+            nbatch,
+            std::initializer_list<std::initializer_list<value_type>>{
+                {0.0, 0.0}, {0.0, 0.0}, {0.0, 0.0}},
+            this->exec);
+        xex_m = gko::batch_initialize<BDense>(
+            nbatch,
+            std::initializer_list<std::initializer_list<value_type>>{
+                {1.0, 1.0}, {3.0, 0.0}, {2.0, 0.0}},
+            this->exec);
+
+        std::vector<gko::dim<2>> sizes(nbatch, gko::dim<2>(1, nrhs));
+        logdata_m.res_norms =
+            gko::matrix::BatchDense<real_type>::create(this->exec, sizes);
+        logdata_m.iter_counts.set_executor(this->exec);
+        logdata_m.iter_counts.resize_and_reset(nrhs * nbatch);
+
+        gko::kernels::reference::batch_rich::apply<value_type>(
+            this->exec, opts_m, mtx.get(), b.get(), x_m.get(), logdata_m);
+
+        std::unique_ptr<BDense> res = b->clone();
+        bnorm_m = gko::batch_initialize<gko::matrix::BatchDense<real_type>>(
+            nbatch, {{0.0, 0.0}}, this->exec);
+        b->compute_norm2(bnorm_m.get());
+        resnorm_m = gko::batch_initialize<gko::matrix::BatchDense<real_type>>(
+            nbatch, {{0.0, 0.0}}, this->exec);
+        auto alpha = gko::batch_initialize<BDense>(nbatch, {-1.0}, this->exec);
+        auto beta = gko::batch_initialize<BDense>(nbatch, {1.0}, this->exec);
+        mtx->apply(alpha.get(), x_m.get(), beta.get(), res.get());
+        res->compute_norm2(resnorm_m.get());
+    }
+
+    std::vector<int> multiple_iters_regression() const
+    {
+        std::vector<int> iters(2);
+        if (std::is_same<real_type, float>::value) {
+            iters[0] = 50;
+            iters[1] = 63;
+        } else if (std::is_same<real_type, double>::value) {
+            iters[0] = 80;
+            iters[1] = 79;
+        } else {
+            iters[0] = -1;
+            iters[1] = -1;
+        }
+        return iters;
+    }
+};
+
+TYPED_TEST_SUITE(BatchRich, gko::test::ValueTypes);
+
+
+TYPED_TEST(BatchRich, SolvesStencilSystemJacobi)
+GKO_NOT_IMPLEMENTED;
+//{
+// TODO (script:batch_bicgstab): change the code imported from
+// solver/batch_richardson if needed
+//    for (size_t i = 0; i < this->nbatch; i++) {
+//        ASSERT_LE(this->resnorm_1->get_const_values()[i] /
+//                      this->bnorm_1->get_const_values()[i],
+//                  this->opts_1.rel_residual_tol);
+//    }
+//    GKO_ASSERT_BATCH_MTX_NEAR(this->x_1, this->xex_1,
+//                              1e-6 /*r<value_type>::value*/);
+//}
+
+TYPED_TEST(BatchRich, StencilSystemJacobiLoggerIsCorrect)
+GKO_NOT_IMPLEMENTED;
+//{
+// TODO (script:batch_bicgstab): change the code imported from
+// solver/batch_richardson if needed
+//    using value_type = typename TestFixture::value_type;
+//    using real_type = gko::remove_complex<value_type>;
+//
+//    const int ref_iters = this->single_iters_regression();
+//
+//    const int *const iter_array =
+//    this->logdata_1.iter_counts.get_const_data(); const real_type *const
+//    res_log_array =
+//        this->logdata_1.res_norms->get_const_values();
+//    for (size_t i = 0; i < this->nbatch; i++) {
+//        // test logger
+//        ASSERT_EQ(iter_array[i], ref_iters);
+//        ASSERT_LE(res_log_array[i] / this->bnorm_1->get_const_values()[i],
+//                  this->opts_1.rel_residual_tol);
+//        // The following is satisfied for float but not for double - why?
+//        // ASSERT_NEAR(res_log_array[i]/bnorm->get_const_values()[i],
+//        //
+//        rnorm->get_const_values()[i]/bnorm->get_const_values()[i],
+//        // 10*r<value_type>::value);
+//    }
+//}
+
+
+TYPED_TEST(BatchRich, SolvesStencilMultipleSystemJacobi)
+GKO_NOT_IMPLEMENTED;
+//{
+// TODO (script:batch_bicgstab): change the code imported from
+// solver/batch_richardson if needed
+//    for (size_t i = 0; i < this->nbatch; i++) {
+//        for (size_t j = 0; j < this->nrhs; j++) {
+//            ASSERT_LE(this->resnorm_m->get_const_values()[i * this->nrhs + j]
+//            /
+//                          this->bnorm_m->get_const_values()[i * this->nrhs +
+//                          j],
+//                      this->opts_m.rel_residual_tol);
+//        }
+//    }
+//    GKO_ASSERT_BATCH_MTX_NEAR(this->x_m, this->xex_m,
+//                              1e-6 /*r<value_type>::value*/);
+//}
+
+
+TYPED_TEST(BatchRich, StencilMultipleSystemJacobiLoggerIsCorrect)
+GKO_NOT_IMPLEMENTED;
+//{
+// TODO (script:batch_bicgstab): change the code imported from
+// solver/batch_richardson if needed
+//    using value_type = typename TestFixture::value_type;
+//    using real_type = gko::remove_complex<value_type>;
+//
+//    const std::vector<int> ref_iters = this->multiple_iters_regression();
+//
+//    const int *const iter_array =
+//    this->logdata_m.iter_counts.get_const_data(); const real_type *const
+//    res_log_array =
+//        this->logdata_m.res_norms->get_const_values();
+//    for (size_t i = 0; i < this->nbatch; i++) {
+//        // test logger
+//        for (size_t j = 0; j < this->nrhs; j++) {
+//            ASSERT_EQ(iter_array[i * this->nrhs + j], ref_iters[j]);
+//            ASSERT_LE(res_log_array[i * this->nrhs + j] /
+//                          this->bnorm_m->get_const_values()[i * this->nrhs +
+//                          j],
+//                      this->opts_m.rel_residual_tol);
+//            // The following is satisfied for float but not for double - why?
+//            // ASSERT_NEAR(res_log_array[i]/bnorm->get_const_values()[i],
+//            //
+//            rnorm->get_const_values()[i]/bnorm->get_const_values()[i],
+//            // 10*r<value_type>::value);
+//        }
+//    }
+//}
+
+
+}  // namespace
