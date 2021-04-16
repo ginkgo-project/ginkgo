dfe86684cbabb8c4e5730c71d5d50e4fe9200de0
aggarwal2000 <aggarwal2000chd@gmail.com>
2021-04-16 06:31:29 +0200

    Add batch bicgstab hip implementation

--- hip/base/config.hip.hpp
+++ hip/base/config.hip.hpp
@@ -89,6 +89,19 @@ struct config {
 };
 
 
+template <typename ValueType>
+struct batch_config {
+    /**
+     * Max number of rows per matrix in a batch of (small) sparse matrices
+     */
+    static constexpr int max_num_rows = 55;
+
+    /**
+     * Max number of RHS vectors in a linear system
+     */
+    static constexpr int max_num_rhs = 4;
+};
+
 }  // namespace hip
 }  // namespace kernels
 }  // namespace gko
