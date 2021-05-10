c6d0a7fb3bfbcd7e92c81656c7704a86cb9d6486
Aditya Kashi <aditya.kashi@kit.edu>
2021-05-10 10:01:53 +0200

    Fixed bugs and inconsistencies from review
    - norm computation error in batch Richardson
    - consistent usage of size constants from config in batch Richardson

--- cuda/base/config.hpp
+++ cuda/base/config.hpp
@@ -79,7 +79,7 @@ struct batch_config {
     /**
      * Max number of rows per matrix in a batch of (small) sparse matrices
      */
-    static constexpr int max_num_rows = 200;
+    static constexpr int max_num_rows = 128;
 
     /**
      * Max number of RHS vectors in a linear system
