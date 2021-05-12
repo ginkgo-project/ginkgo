b2067ea08ce92fa12f5199b4f8d9235ecfe61793
Aditya Kashi <aditya.kashi@kit.edu>
2021-05-12 18:30:57 +0200

    moved shared memory bank configuration to separate header
    This was done to separate code that requires the Cuda runtime to compile
    from code that does not.

--- cuda/solver/batch_richardson_kernels.cu
+++ cuda/solver/batch_richardson_kernels.cu
@@ -37,6 +37,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
 #include "cuda/base/config.hpp"
+#include "cuda/base/kernel_config.cuh"
 #include "cuda/base/types.hpp"
 #include "cuda/components/cooperative_groups.cuh"
 #include "cuda/components/thread_ids.cuh"
