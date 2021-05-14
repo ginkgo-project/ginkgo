392b0cfed88e726ed3187e8d8ec6d3a0d5d785d2
aggarwal2000 <aggarwal2000chd@gmail.com>
2021-05-14 09:53:17 +0200

    Add batch gmres public interface, core, reference and cuda implementations and tests

--- hip/solver/batch_gmres_kernels.hip.cpp
+++ hip/solver/batch_gmres_kernels.hip.cpp
@@ -30,71 +30,67 @@ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ******************************<GINKGO LICENSE>*******************************/
 
-#ifndef GKO_CUDA_BASE_CONFIG_HPP_
-#define GKO_CUDA_BASE_CONFIG_HPP_
+#include "core/solver/batch_gmres_kernels.hpp"
+
+#include <hip/hip_runtime.h>
 
 
-#include <ginkgo/core/base/types.hpp>
+#include <ginkgo/core/base/math.hpp>
+#include <ginkgo/core/preconditioner/batch_preconditioner_strings.hpp>
+#include <ginkgo/core/stop/batch_stop_enum.hpp>
 
-
-#include "cuda/base/math.hpp"
+#include "hip/base/config.hip.hpp"
+#include "hip/base/math.hip.hpp"
+#include "hip/base/types.hip.hpp"
+#include "hip/components/cooperative_groups.hip.hpp"
+#include "hip/matrix/batch_struct.hip.hpp"
 
 
 namespace gko {
 namespace kernels {
-namespace cuda {
+namespace hip {
 
 
-struct config {
-    /**
-     * The type containing a bitmask over all lanes of a warp.
-     */
-    using lane_mask_type = uint32;
+constexpr int default_block_size = 128;
+constexpr int sm_multiplier = 4;
 
-    /**
-     * The number of threads within a CUDA warp.
-     */
-    static constexpr uint32 warp_size = 32;
+/**
+ * @brief The batch Gmres solver namespace.
+ *
+ * @ingroup batch_gmres
+ */
+namespace batch_gmres {
 
-    /**
-     * The bitmask of the entire warp.
-     */
-    static constexpr auto full_lane_mask = ~zero<lane_mask_type>();
+#include "common/components/uninitialized_array.hpp.inc"
 
-    /**
-     * The maximal number of threads allowed in a CUDA warp.
-     */
-    static constexpr uint32 max_block_size = 1024;
 
-    /**
-     * The minimal amount of warps that need to be scheduled for each block
-     * to maximize GPU occupancy.
-     */
-    static constexpr uint32 min_warps_per_block = 4;
-};
+#include "common/log/batch_logger.hpp.inc"
+#include "common/matrix/batch_csr_kernels.hpp.inc"
+#include "common/matrix/batch_dense_kernels.hpp.inc"
+#include "common/preconditioner/batch_identity.hpp.inc"
+#include "common/preconditioner/batch_jacobi.hpp.inc"
+#include "common/stop/batch_criteria.hpp.inc"
+
+
+template <typename T>
+using BatchGmresOptions = gko::kernels::batch_gmres::BatchGmresOptions<T>;
 
 
 template <typename ValueType>
-struct batch_config {
-    /**
-     * Max number of rows per matrix in a batch of (small) sparse matrices.
-     */
-    static constexpr int max_num_rows = 35;
-
-    /**
-     * Max number of RHS vectors in a linear system. This can be at most
-     * `config::warp_size`.
-     */
-    static constexpr int max_num_rhs =
-        2;  // NOTE: max_num_rhs has to be an even number to avoid cuda
-            // misaligned address issues while using complex datatypes(in
-            // reference to the shared memory in solver kernels)
-};
+void apply(std::shared_ptr<const HipExecutor> exec,
+           const BatchGmresOptions<remove_complex<ValueType>> &opts,
+           const BatchLinOp *const a,
+           const matrix::BatchDense<ValueType> *const left_scale,
+           const matrix::BatchDense<ValueType> *const right_scale,
+           const matrix::BatchDense<ValueType> *const b,
+           matrix::BatchDense<ValueType> *const x,
+           log::BatchLogData<ValueType> &logdata) GKO_NOT_IMPLEMENTED;
 
 
-}  // namespace cuda
+GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE(GKO_DECLARE_BATCH_GMRES_APPLY_KERNEL);
+
+
+}  // namespace batch_gmres
+}  // namespace hip
 }  // namespace kernels
 }  // namespace gko
-
-
-#endif  // GKO_CUDA_BASE_CONFIG_HPP_
