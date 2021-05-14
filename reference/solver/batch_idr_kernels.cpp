ed87ec68360214abcc63c112b21e4fa3fa15bd18
aggarwal2000 <aggarwal2000chd@gmail.com>
2021-05-14 14:06:19 +0200

    Add batch-idr public interface and core implementation (Yet to update the core tests)

---
+++ reference/solver/batch_idr_kernels.cpp
@@ -0,0 +1,83 @@
+/*******************************<GINKGO LICENSE>******************************
+Copyright (c) 2017-2021, the Ginkgo authors
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+1. Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+contributors may be used to endorse or promote products derived from
+this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+******************************<GINKGO LICENSE>*******************************/
+
+#include "core/solver/batch_idr_kernels.hpp"
+
+
+#include <ginkgo/core/preconditioner/batch_preconditioner_strings.hpp>
+#include <ginkgo/core/stop/batch_stop_enum.hpp>
+
+#include "reference/base/config.hpp"
+// include device kernels for every matrix and preconditioner type
+#include "reference/log/batch_logger.hpp"
+#include "reference/matrix/batch_csr_kernels.hpp"
+#include "reference/matrix/batch_dense_kernels.hpp"
+#include "reference/matrix/batch_struct.hpp"
+#include "reference/preconditioner/batch_identity.hpp"
+#include "reference/preconditioner/batch_jacobi.hpp"
+#include "reference/stop/batch_criteria.hpp"
+
+
+namespace gko {
+namespace kernels {
+namespace reference {
+
+
+/**
+ * @brief The batch Idr solver namespace.
+ *
+ * @ingroup batch_idr
+ */
+namespace batch_idr {
+
+
+template <typename T>
+using BatchIdrOptions = gko::kernels::batch_idr::BatchIdrOptions<T>;
+
+
+template <typename ValueType>
+void apply(std::shared_ptr<const ReferenceExecutor> exec,
+           const BatchIdrOptions<remove_complex<ValueType>> &opts,
+           const BatchLinOp *const a,
+           const matrix::BatchDense<ValueType> *const left_scale,
+           const matrix::BatchDense<ValueType> *const right_scale,
+           const matrix::BatchDense<ValueType> *const b,
+           matrix::BatchDense<ValueType> *const x,
+           gko::log::BatchLogData<ValueType> &logdata) GKO_NOT_IMPLEMENTED;
+
+GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE(GKO_DECLARE_BATCH_IDR_APPLY_KERNEL);
+
+
+}  // namespace batch_idr
+}  // namespace reference
+}  // namespace kernels
+}  // namespace gko
