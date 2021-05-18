ffd1fcc48d7ed0a51eebcbda2edc0b310964e739
Gregor Olenik <gregor.olenik@web.de>
2021-05-18 12:47:52 +0200

    Add symmetry check for Csr, Coo, and Ell matrices

--- core/test/components/validation_helpers.cpp
+++ core/test/components/validation_helpers.cpp
@@ -40,51 +40,81 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 namespace {
 
-template <typename ValueIndexType>
-class IsSymmetric : public ::testing::Test {
-protected:
-    using value_type =
-        typename std::tuple_element<0, decltype(ValueIndexType())>::type;
-    using index_type =
-        typename std::tuple_element<1, decltype(ValueIndexType())>::type;
-    using Mtx = gko::matrix::Coo<value_type, index_type>;
-
-    IsSymmetric()
-        : exec(gko::ReferenceExecutor::create()),
-          mtx(gko::matrix::Coo<value_type, index_type>::create(
-              exec, gko::dim<2>{3, 3}, 9))
-    {
-        value_type *v = mtx->get_values();
-        index_type *c = mtx->get_col_idxs();
-        index_type *r = mtx->get_row_idxs();
-
-        // clang-format off
-        r[0] = 0; r[3] = 1; r[6] = 2;
-        r[1] = 0; r[4] = 1; r[7] = 2;
-        r[2] = 0; r[5] = 1; r[8] = 2;
-
-        c[0] = 0; c[3] = 0; c[6] = 0;
-        c[1] = 1; c[4] = 1; c[7] = 1;
-        c[2] = 2; c[5] = 2; c[8] = 2;
-
-        v[0] = 1; v[3] = 2; v[6] = 3;
-        v[1] = 2; v[4] = 1; v[7] = 4;
-        v[2] = 3; v[5] = 4; v[8] = 1;
-        // clang-format on
+#define GKO_DEFINE_ISSYMMETRIC(MATRIX_TYPE)                                   \
+    template <typename ValueIndexType>                                        \
+    class IsSymmetric##MATRIX_TYPE : public ::testing::Test {                 \
+    protected:                                                                \
+        using value_type =                                                    \
+            typename std::tuple_element<0, decltype(ValueIndexType())>::type; \
+        using index_type =                                                    \
+            typename std::tuple_element<1, decltype(ValueIndexType())>::type; \
+        using Mtx = gko::matrix::Coo<value_type, index_type>;                 \
+                                                                              \
+        IsSymmetric##MATRIX_TYPE()                                            \
+            : exec(gko::ReferenceExecutor::create()),                         \
+              mtx(gko::matrix::Coo<value_type, index_type>::create(           \
+                  exec, gko::dim<2>{3, 3}, 9))                                \
+        {                                                                     \
+            value_type *v = mtx->get_values();                                \
+                                                                              \
+            index_type *c = mtx->get_col_idxs();                              \
+            index_type *r = mtx->get_row_idxs();                              \
+                                                                              \
+            r[0] = 0;                                                         \
+            r[3] = 1;                                                         \
+            r[6] = 2;                                                         \
+            r[1] = 0;                                                         \
+            r[4] = 1;                                                         \
+            r[7] = 2;                                                         \
+            r[2] = 0;                                                         \
+            r[5] = 1;                                                         \
+            r[8] = 2;                                                         \
+                                                                              \
+            c[0] = 0;                                                         \
+            c[3] = 0;                                                         \
+            c[6] = 0;                                                         \
+            c[1] = 1;                                                         \
+            c[4] = 1;                                                         \
+            c[7] = 1;                                                         \
+            c[2] = 2;                                                         \
+            c[5] = 2;                                                         \
+            c[8] = 2;                                                         \
+                                                                              \
+            v[0] = 1;                                                         \
+            v[3] = 2;                                                         \
+            v[6] = 3;                                                         \
+            v[1] = 2;                                                         \
+            v[4] = 1;                                                         \
+            v[7] = 4;                                                         \
+            v[2] = 3;                                                         \
+            v[5] = 4;                                                         \
+            v[8] = 1;                                                         \
+        }                                                                     \
+                                                                              \
+        std::shared_ptr<const gko::Executor> exec;                            \
+        std::unique_ptr<Mtx> mtx;                                             \
     }
 
-    std::shared_ptr<const gko::Executor> exec;
-    std::unique_ptr<Mtx> mtx;
-};
+#define GKO_TYPED_TEST_SUITE_FOR_MATRIX_TYPE(MATRIX_TYPE)                     \
+    GKO_DEFINE_ISSYMMETRIC(MATRIX_TYPE);                                      \
+                                                                              \
+    TYPED_TEST_SUITE(IsSymmetric##MATRIX_TYPE, gko::test::ValueIndexTypes);   \
+                                                                              \
+    TYPED_TEST(IsSymmetric##MATRIX_TYPE, ReturnsTrueOnSymmetric)              \
+    {                                                                         \
+        ASSERT_EQ(gko::validate::is_symmetric(this->mtx.get(), 1e-32), true); \
+    }                                                                         \
+                                                                              \
+    TYPED_TEST(IsSymmetric##MATRIX_TYPE, ReturnsFalseOnAsymmetric)            \
+    {                                                                         \
+        auto asym_mtx = this->mtx->clone();                                   \
+        asym_mtx->get_values()[2] = 0;                                        \
+        ASSERT_EQ(gko::validate::is_symmetric(asym_mtx.get(), 1e-32), false); \
+    }
 
-TYPED_TEST_SUITE(IsSymmetric, gko::test::ValueIndexTypes);
-
-// TODO expand to other matrix formats
-TYPED_TEST(IsSymmetric, ReturnsTrueOnSymmetric)
-{
-    ASSERT_EQ(gko::validate::is_symmetric(this->mtx.get(), 1e-32), true);
-}
-
+GKO_TYPED_TEST_SUITE_FOR_MATRIX_TYPE(Coo)
+GKO_TYPED_TEST_SUITE_FOR_MATRIX_TYPE(Csr)
+GKO_TYPED_TEST_SUITE_FOR_MATRIX_TYPE(Ell)
 
 template <typename T>
 class IsRowOrdered : public ::testing::Test {
