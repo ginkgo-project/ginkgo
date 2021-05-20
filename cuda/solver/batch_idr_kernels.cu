a7c74210f6c30a220f9ac0d3b43aaaba8e521a9c
aggarwal2000 <aggarwal2000chd@gmail.com>
2021-05-20 09:27:56 +0200

    Add batch idr cuda implementation (Note: This does not work for larger systems)

--- cuda/solver/batch_idr_kernels.cu
+++ cuda/solver/batch_idr_kernels.cu
@@ -32,8 +32,11 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include "core/solver/batch_idr_kernels.hpp"
 
+#include <random>
+#include "curand_kernel.h"
 
 #include <ginkgo/core/base/math.hpp>
+#include <ginkgo/core/matrix/dense.hpp>
 #include <ginkgo/core/preconditioner/batch_preconditioner_strings.hpp>
 #include <ginkgo/core/stop/batch_stop_enum.hpp>
 
@@ -68,9 +71,62 @@ namespace batch_idr {
 #include "common/preconditioner/batch_jacobi.hpp.inc"
 #include "common/stop/batch_criteria.hpp.inc"
 
+#include "common/solver/batch_idr_kernels.hpp.inc"
 
 template <typename T>
 using BatchIdrOptions = gko::kernels::batch_idr::BatchIdrOptions<T>;
+
+template <typename BatchMatrixType, typename LogType, typename ValueType>
+static void apply_impl(
+    std::shared_ptr<const CudaExecutor> exec,
+    const BatchIdrOptions<remove_complex<ValueType>> opts, LogType logger,
+    const BatchMatrixType &a,
+    const gko::batch_dense::UniformBatch<const ValueType> &left,
+    const gko::batch_dense::UniformBatch<const ValueType> &right,
+    const gko::batch_dense::UniformBatch<ValueType> &b,
+    const gko::batch_dense::UniformBatch<ValueType> &x,
+    const gko::batch_dense::BatchEntry<const ValueType> &Subspace_vectors_entry)
+{
+    using real_type = gko::remove_complex<ValueType>;
+    const size_type nbatch = a.num_batch;
+
+    if (opts.preconditioner == gko::preconditioner::batch::jacobi_str) {
+        apply_kernel<BatchJacobi<ValueType>,
+                     stop::AbsAndRelResidualMaxIter<ValueType>>
+            <<<nbatch, default_block_size>>>(
+                opts.max_its, opts.abs_residual_tol, opts.rel_residual_tol,
+                static_cast<int>(opts.subspace_dim_val), opts.kappa_val,
+                opts.to_use_smoothing, opts.deterministic_gen, opts.tol_type,
+                logger, a, left, right, b, x, Subspace_vectors_entry);
+    } else if (opts.preconditioner == gko::preconditioner::batch::none_str) {
+        apply_kernel<BatchIdentity<ValueType>,
+                     stop::AbsAndRelResidualMaxIter<ValueType>>
+            <<<nbatch, default_block_size>>>(
+                opts.max_its, opts.abs_residual_tol, opts.rel_residual_tol,
+                opts.subspace_dim_val, opts.kappa_val, opts.to_use_smoothing,
+                opts.deterministic_gen, opts.tol_type, logger, a, left, right,
+                b, x, Subspace_vectors_entry);
+    } else {
+        GKO_NOT_IMPLEMENTED;
+    }
+}
+
+
+template <typename ValueType, typename Distribution, typename Generator>
+typename std::enable_if<!is_complex_s<ValueType>::value, ValueType>::type
+get_rand_value(Distribution &&dist, Generator &&gen)
+{
+    return dist(gen);
+}
+
+
+template <typename ValueType, typename Distribution, typename Generator>
+typename std::enable_if<is_complex_s<ValueType>::value, ValueType>::type
+get_rand_value(Distribution &&dist, Generator &&gen)
+{
+    return ValueType(dist(gen), dist(gen));
+}
+
 
 template <typename ValueType>
 void apply(std::shared_ptr<const CudaExecutor> exec,
@@ -80,7 +136,84 @@ void apply(std::shared_ptr<const CudaExecutor> exec,
            const matrix::BatchDense<ValueType> *const right_scale,
            const matrix::BatchDense<ValueType> *const b,
            matrix::BatchDense<ValueType> *const x,
-           log::BatchLogData<ValueType> &logdata) GKO_NOT_IMPLEMENTED;
+           log::BatchLogData<ValueType> &logdata)
+{
+    using cu_value_type = cuda_type<ValueType>;
+
+    if (opts.is_complex_subspace == true &&
+        !is_complex<ValueType>())  // Currently, the option of having complex
+                                   // subspace for real matrices is not
+                                   // supported.
+    {
+        GKO_NOT_IMPLEMENTED;
+    }
+
+    // For now, FinalLogger is the only one available
+    batch_log::FinalLogger<remove_complex<ValueType>> logger(
+        static_cast<int>(b->get_size().at(0)[1]), opts.max_its,
+        logdata.res_norms->get_values(), logdata.iter_counts.get_data());
+
+    const gko::batch_dense::UniformBatch<const cu_value_type> left_sb =
+        maybe_null_batch_struct(left_scale);
+    const gko::batch_dense::UniformBatch<const cu_value_type> right_sb =
+        maybe_null_batch_struct(right_scale);
+    const auto to_scale = left_sb.values || right_sb.values;
+    if (to_scale) {
+        if (!left_sb.values || !right_sb.values) {
+            // one-sided scaling not implemented
+            GKO_NOT_IMPLEMENTED;
+        }
+    }
+
+
+    const gko::batch_dense::UniformBatch<cu_value_type> x_b =
+        get_batch_struct(x);
+
+    // TODO: Use this only if smoothing is ON, else pass a {nullptr,0,0,0}
+    std::unique_ptr<gko::matrix::Dense<ValueType>> Subspace_vectors_cpu =
+        gko::matrix::Dense<ValueType>::create(
+            exec->get_master(),
+            gko::dim<2>{x_b.num_rows * opts.subspace_dim_val, 1}, 1);
+
+    auto dist = std::normal_distribution<remove_complex<ValueType>>(0.0, 1.0);
+    auto seed = 15;
+    auto gen = std::ranlux48(seed);
+
+    for (int vec_index = 0; vec_index < opts.subspace_dim_val; vec_index++) {
+        for (int row_index = 0; row_index < x_b.num_rows; row_index++) {
+            ValueType val = get_rand_value<ValueType>(dist, gen);
+
+            Subspace_vectors_cpu->at(vec_index * x_b.num_rows + row_index, 0) =
+                val;
+        }
+    }
+
+    std::unique_ptr<gko::matrix::Dense<ValueType>> Subspace_vectors{
+        gko::clone(exec, Subspace_vectors_cpu)};
+
+
+    const gko::batch_dense::BatchEntry<const cu_value_type>
+        Subspace_vectors_entry = {
+            as_cuda_type(Subspace_vectors->get_const_values()),
+            Subspace_vectors->get_stride(),
+            static_cast<int>(Subspace_vectors->get_size()[0]),
+            static_cast<int>(Subspace_vectors->get_size()[1])};
+
+    // const gko::batch_dense::BatchEntry< const cu_value_type >
+    // Subspace_vectors_entry = {nullptr , 0,0,0};
+    if (auto amat = dynamic_cast<const matrix::BatchCsr<ValueType> *>(a)) {
+        const gko::batch_csr::UniformBatch<cu_value_type> m_b =
+            get_batch_struct(const_cast<matrix::BatchCsr<ValueType> *>(amat));
+
+        const gko::batch_dense::UniformBatch<cu_value_type> b_b =
+            get_batch_struct(const_cast<matrix::BatchDense<ValueType> *>(b));
+
+        apply_impl(exec, opts, logger, m_b, left_sb, right_sb, b_b, x_b,
+                   Subspace_vectors_entry);
+    } else {
+        GKO_NOT_SUPPORTED(a);
+    }
+}
 
 GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE(GKO_DECLARE_BATCH_IDR_APPLY_KERNEL);
 
