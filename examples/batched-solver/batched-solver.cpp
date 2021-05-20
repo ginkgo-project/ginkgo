37d53c9f283ff1a6dcc50c13550f0f148046a29e
Pratik Nayak <pratikvn@protonmail.com>
2021-05-20 19:49:01 +0200

    Add batch-solver example.

---
+++ examples/batched-solver/batched-solver.cpp
@@ -0,0 +1,192 @@
+/*******************************<GINKGO LICENSE>******************************
+Copyright (c) 2017-2021, the Ginkgo authors
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+1. Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+contributors may be used to endorse or promote products derived from
+this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+******************************<GINKGO LICENSE>*******************************/
+
+// @sect3{Include files}
+
+// This is the main ginkgo header file.
+#include <ginkgo/ginkgo.hpp>
+
+// Add the fstream header to read from data from files.
+#include <fstream>
+// Add the C++ iostream header to output information to the console.
+#include <iostream>
+// Add the STL map header for the executor selection
+#include <map>
+// Add the string manipulation header to handle strings.
+#include <string>
+
+
+int main(int argc, char *argv[])
+{
+    // Use some shortcuts. In Ginkgo, vectors are seen as a gko::matrix::Dense
+    // with one column/one row. The advantage of this concept is that using
+    // multiple vectors is a now a natural extension of adding columns/rows are
+    // necessary.
+    using ValueType = double;
+    using RealValueType = gko::remove_complex<ValueType>;
+    using IndexType = int;
+    using size_type = gko::size_type;
+    using vec = gko::matrix::Dense<ValueType>;
+    using bvec = gko::matrix::BatchDense<ValueType>;
+    using real_vec = gko::matrix::Dense<RealValueType>;
+    using real_bvec = gko::matrix::BatchDense<RealValueType>;
+    // The gko::matrix::Csr class is used here, but any other matrix class such
+    // as gko::matrix::Coo, gko::matrix::Hybrid, gko::matrix::Ell or
+    // gko::matrix::Sellp could also be used.
+    using mtx = gko::matrix::Csr<ValueType, IndexType>;
+    using bmtx = gko::matrix::BatchCsr<ValueType, IndexType>;
+    // The gko::solver::Cg is used here, but any other solver class can also be
+    // used.
+    using cg = gko::solver::Cg<ValueType>;
+    using rich = gko::solver::BatchRichardson<ValueType>;
+
+    // Print the ginkgo version information.
+    std::cout << gko::version_info::get() << std::endl;
+
+    if (argc == 2 && (std::string(argv[1]) == "--help")) {
+        std::cerr << "Usage: " << argv[0] << " [executor] " << std::endl;
+        std::exit(-1);
+    }
+
+    // @sect3{Where do you want to run your solver ?}
+    // The gko::Executor class is one of the cornerstones of Ginkgo. Currently,
+    // we have support for
+    // an gko::OmpExecutor, which uses OpenMP multi-threading in most of its
+    // kernels, a gko::ReferenceExecutor, a single threaded specialization of
+    // the OpenMP executor and a gko::CudaExecutor which runs the code on a
+    // NVIDIA GPU if available.
+    // @note With the help of C++, you see that you only ever need to change the
+    // executor and all the other functions/ routines within Ginkgo should
+    // automatically work and run on the executor with any other changes.
+    const auto executor_string = argc >= 2 ? argv[1] : "reference";
+    const size_type nbatch = argc >= 3 ? std::atoi(argv[2]) : 2;
+    std::map<std::string, std::function<std::shared_ptr<gko::Executor>()>>
+        exec_map{
+            {"omp", [] { return gko::OmpExecutor::create(); }},
+            {"cuda",
+             [] {
+                 return gko::CudaExecutor::create(0, gko::OmpExecutor::create(),
+                                                  true);
+             }},
+            {"hip",
+             [] {
+                 return gko::HipExecutor::create(0, gko::OmpExecutor::create(),
+                                                 true);
+             }},
+            {"dpcpp",
+             [] {
+                 return gko::DpcppExecutor::create(0,
+                                                   gko::OmpExecutor::create());
+             }},
+            {"reference", [] { return gko::ReferenceExecutor::create(); }}};
+
+    // executor where Ginkgo will perform the computation
+    const auto exec = exec_map.at(executor_string)();  // throws if not valid
+
+    // @sect3{Reading your data and transfer to the proper device.}
+    // Read the matrix, right hand side and the initial solution using the @ref
+    // read function.
+    // @note Ginkgo uses C++ smart pointers to automatically manage memory. To
+    // this end, we use our own object ownership transfer functions that under
+    // the hood call the required smart pointer functions to manage object
+    // ownership. The gko::share , gko::give and gko::lend are the functions
+    // that you would need to use.
+    auto A = share(gko::read<mtx>(std::ifstream("data/A.mtx"), exec));
+    auto b = gko::read<vec>(std::ifstream("data/b.mtx"), exec);
+    auto x = gko::read<vec>(std::ifstream("data/x0.mtx"), exec);
+
+    auto bA = share(bmtx::create(exec, nbatch, A.get()));
+    auto bb = bvec::create(exec, nbatch, b.get());
+    auto bx = bvec::create(exec, nbatch, x.get());
+    // @sect3{Creating the solver}
+    // Generate the gko::solver factory. Ginkgo uses the concept of Factories to
+    // build solvers with certain
+    // properties. Observe the Fluent interface used here. Here a cg solver is
+    // generated with a stopping criteria of maximum iterations of 20 and a
+    // residual norm reduction of 1e-7. You also observe that the stopping
+    // criteria(gko::stop) are also generated from factories using their build
+    // methods. You need to specify the executors which each of the object needs
+    // to be built on.
+    const RealValueType reduction_factor{1e-7};
+    // auto solver_gen =
+    //     cg::build()
+    //         .with_criteria(
+    //             gko::stop::Iteration::build().with_max_iters(20u).on(exec),
+    //             gko::stop::ResidualNorm<ValueType>::build()
+    //                 .with_reduction_factor(reduction_factor)
+    //                 .on(exec))
+    //         .on(exec);
+    auto solver_gen = rich::build()
+                          .with_max_iterations(500)
+                          .with_rel_residual_tol(reduction_factor)
+                          .with_relaxation_factor(RealValueType{0.95})
+                          .on(exec);
+    // Generate the solver from the matrix. The solver factory built in the
+    // previous step takes a "matrix"(a gko::LinOp to be more general) as an
+    // input. In this case we provide it with a full matrix that we previously
+    // read, but as the solver only effectively uses the apply() method within
+    // the provided "matrix" object, you can effectively create a gko::LinOp
+    // class with your own apply implementation to accomplish more tasks. We
+    // will see an example of how this can be done in the custom-matrix-format
+    // example
+    // auto solver = solver_gen->generate(A);
+    auto solver = solver_gen->generate(bA);
+
+    // Finally, solve the system. The solver, being a gko::LinOp, can be applied
+    // to a right hand side, b to
+    // obtain the solution, x.
+    solver->apply(lend(bb), lend(bx));
+
+    // Print the solution to the command line.
+    // std::cout << "Solution (x):\n";
+    // write(std::cout, lend(x));
+
+    // To measure if your solution has actually converged, you can measure the
+    // error of the solution.
+    // one, neg_one are objects that represent the numbers which allow for a
+    // uniform interface when computing on any device. To compute the residual,
+    // all you need to do is call the apply method, which in this case is an
+    // spmv and equivalent to the LAPACK z_spmv routine. Finally, you compute
+    // the euclidean 2-norm with the compute_norm2 function.
+    auto one = gko::batch_initialize<bvec>(nbatch, {1.0}, exec);
+    auto neg_one = gko::batch_initialize<bvec>(nbatch, {-1.0}, exec);
+    auto res = gko::batch_initialize<real_bvec>(nbatch, {0.0}, exec);
+    bA->apply(lend(one), lend(bx), lend(neg_one), lend(bb));
+    bb->compute_norm2(lend(res));
+
+    std::cout << "Residual norm sqrt(r^T r):\n";
+    for (int i = 0; i < res->get_num_batch_entries(); ++i) {
+        std::cout << res->get_values()[i] << ", "
+                  << "\n";
+    }
+    std::cout << std::endl;
+}
