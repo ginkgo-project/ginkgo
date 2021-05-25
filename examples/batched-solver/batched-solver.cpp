89ffe8592616a0fb9e87c4ba8d7cbe644f2247e4
Pratik Nayak <pratikvn@protonmail.com>
2021-05-25 14:07:42 +0200

    Update example with review

--- examples/batched-solver/batched-solver.cpp
+++ examples/batched-solver/batched-solver.cpp
@@ -59,14 +59,8 @@ int main(int argc, char *argv[])
     using bvec = gko::matrix::BatchDense<ValueType>;
     using real_vec = gko::matrix::Dense<RealValueType>;
     using real_bvec = gko::matrix::BatchDense<RealValueType>;
-    // The gko::matrix::Csr class is used here, but any other matrix class such
-    // as gko::matrix::Coo, gko::matrix::Hybrid, gko::matrix::Ell or
-    // gko::matrix::Sellp could also be used.
     using mtx = gko::matrix::Csr<ValueType, IndexType>;
     using bmtx = gko::matrix::BatchCsr<ValueType, IndexType>;
-    // The gko::solver::Cg is used here, but any other solver class can also be
-    // used.
-    using cg = gko::solver::Cg<ValueType>;
     using rich = gko::solver::BatchRichardson<ValueType>;
 
     // Print the ginkgo version information.
@@ -89,6 +83,8 @@ int main(int argc, char *argv[])
     // automatically work and run on the executor with any other changes.
     const auto executor_string = argc >= 2 ? argv[1] : "reference";
     const size_type nbatch = argc >= 3 ? std::atoi(argv[2]) : 2;
+    const gko::remove_complex<ValueType> relax_factor =
+        argc >= 4 ? std::atof(argv[3]) : 0.95;
     std::map<std::string, std::function<std::shared_ptr<gko::Executor>()>>
         exec_map{
             {"omp", [] { return gko::OmpExecutor::create(); }},
@@ -112,71 +108,37 @@ int main(int argc, char *argv[])
     // executor where Ginkgo will perform the computation
     const auto exec = exec_map.at(executor_string)();  // throws if not valid
 
-    // @sect3{Reading your data and transfer to the proper device.}
-    // Read the matrix, right hand side and the initial solution using the @ref
-    // read function.
-    // @note Ginkgo uses C++ smart pointers to automatically manage memory. To
-    // this end, we use our own object ownership transfer functions that under
-    // the hood call the required smart pointer functions to manage object
-    // ownership. The gko::share , gko::give and gko::lend are the functions
-    // that you would need to use.
     auto A = share(gko::read<mtx>(std::ifstream("data/A.mtx"), exec));
-    auto b = gko::read<vec>(std::ifstream("data/b.mtx"), exec);
-    auto x = gko::read<vec>(std::ifstream("data/x0.mtx"), exec);
+    // Create RHS and initial guess as 1
+    gko::size_type size = A->get_size()[0];
+    auto host_x = vec::create(exec->get_master(), gko::dim<2>(size, 1));
+    for (auto i = 0; i < size; i++) {
+        host_x->at(i, 0) = 1.;
+    }
+    auto x = vec::create(exec);
+    auto b = vec::create(exec);
+    x->copy_from(host_x.get());
+    b->copy_from(host_x.get());
 
+    // Create batched matrices from the input matrix and generated vectors.
     auto bA = share(bmtx::create(exec, nbatch, A.get()));
     auto bb = bvec::create(exec, nbatch, b.get());
     auto bx = bvec::create(exec, nbatch, x.get());
-    // @sect3{Creating the solver}
-    // Generate the gko::solver factory. Ginkgo uses the concept of Factories to
-    // build solvers with certain
-    // properties. Observe the Fluent interface used here. Here a cg solver is
-    // generated with a stopping criteria of maximum iterations of 20 and a
-    // residual norm reduction of 1e-7. You also observe that the stopping
-    // criteria(gko::stop) are also generated from factories using their build
-    // methods. You need to specify the executors which each of the object needs
-    // to be built on.
     const RealValueType reduction_factor{1e-7};
-    // auto solver_gen =
-    //     cg::build()
-    //         .with_criteria(
-    //             gko::stop::Iteration::build().with_max_iters(20u).on(exec),
-    //             gko::stop::ResidualNorm<ValueType>::build()
-    //                 .with_reduction_factor(reduction_factor)
-    //                 .on(exec))
-    //         .on(exec);
+
+    // Create the batch solver factory
     auto solver_gen = rich::build()
                           .with_max_iterations(500)
                           .with_rel_residual_tol(reduction_factor)
-                          .with_relaxation_factor(RealValueType{0.95})
+                          .with_relaxation_factor(relax_factor)
                           .on(exec);
-    // Generate the solver from the matrix. The solver factory built in the
-    // previous step takes a "matrix"(a gko::LinOp to be more general) as an
-    // input. In this case we provide it with a full matrix that we previously
-    // read, but as the solver only effectively uses the apply() method within
-    // the provided "matrix" object, you can effectively create a gko::LinOp
-    // class with your own apply implementation to accomplish more tasks. We
-    // will see an example of how this can be done in the custom-matrix-format
-    // example
-    // auto solver = solver_gen->generate(A);
+
+    // Generate the batch solver from the batch matrix
     auto solver = solver_gen->generate(bA);
 
-    // Finally, solve the system. The solver, being a gko::LinOp, can be applied
-    // to a right hand side, b to
-    // obtain the solution, x.
+    // Solve the batch system
     solver->apply(lend(bb), lend(bx));
 
-    // Print the solution to the command line.
-    // std::cout << "Solution (x):\n";
-    // write(std::cout, lend(x));
-
-    // To measure if your solution has actually converged, you can measure the
-    // error of the solution.
-    // one, neg_one are objects that represent the numbers which allow for a
-    // uniform interface when computing on any device. To compute the residual,
-    // all you need to do is call the apply method, which in this case is an
-    // spmv and equivalent to the LAPACK z_spmv routine. Finally, you compute
-    // the euclidean 2-norm with the compute_norm2 function.
     auto one = gko::batch_initialize<bvec>(nbatch, {1.0}, exec);
     auto neg_one = gko::batch_initialize<bvec>(nbatch, {-1.0}, exec);
     auto res = gko::batch_initialize<real_bvec>(nbatch, {0.0}, exec);
@@ -184,9 +146,8 @@ int main(int argc, char *argv[])
     bb->compute_norm2(lend(res));
 
     std::cout << "Residual norm sqrt(r^T r):\n";
+    auto unb_res = res->unbatch();
     for (int i = 0; i < res->get_num_batch_entries(); ++i) {
-        std::cout << res->get_values()[i] << ", "
-                  << "\n";
+        write(std::cout, lend(unb_res[i]));
     }
-    std::cout << std::endl;
 }
