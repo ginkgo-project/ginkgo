5b28bef9291f4a88f50ec11668ad5b1b2ace95c9
aggarwal2000 <aggarwal2000chd@gmail.com>
2021-05-26 07:47:16 +0200

    Adapt to new batch preconditioner interface

--- reference/solver/batch_bicgstab_kernels.cpp
+++ reference/solver/batch_bicgstab_kernels.cpp
@@ -345,12 +345,12 @@ template <typename T>
 using BatchBicgstabOptions =
     gko::kernels::batch_bicgstab::BatchBicgstabOptions<T>;
 
-template <typename PrecType, typename StopType, typename LogType,
+template <typename StopType, typename PrecType, typename LogType,
           typename BatchMatrixType, typename ValueType>
 static void apply_impl(
     std::shared_ptr<const ReferenceExecutor> exec,
     const BatchBicgstabOptions<remove_complex<ValueType>> &opts, LogType logger,
-    const BatchMatrixType &a,
+    PrecType prec, const BatchMatrixType &a,
     const gko::batch_dense::UniformBatch<const ValueType> &left,
     const gko::batch_dense::UniformBatch<const ValueType> &right,
     const gko::batch_dense::UniformBatch<ValueType> &b,
@@ -480,7 +480,7 @@ static void apply_impl(
             norms_res_temp, static_cast<size_type>(nrhs), 1, nrhs};
 
         // generate preconditioner
-        PrecType prec(A_entry, prec_work);
+        prec.generate(A_entry, prec_work);
 
         // initialization
         // rho_old = 1, omega_old = 1, alpha = 1
@@ -634,18 +634,16 @@ void apply_select_prec(
     const gko::batch_dense::UniformBatch<ValueType> &x)
 {
     if (opts.preconditioner == gko::preconditioner::batch::type::none) {
-        apply_impl<BatchIdentity<ValueType>,
-                   stop::AbsAndRelResidualMaxIter<ValueType>>(
-            exec, opts, logger, a, left, right, b, x);
+        BatchIdentity<ValueType> prec;
+        apply_impl<stop::AbsAndRelResidualMaxIter<ValueType>>(
+            exec, opts, logger, prec, a, left, right, b, x);
 
-    }
-    // else if (opts.preconditioner == gko::preconditioner::batch::type::jacobi)
-    // {
-    //     apply_impl<BatchJacobi<ValueType>,
-    //                stop::AbsAndRelResidualMaxIter<ValueType>>(
-    //         exec, opts, logger, a, left, right, b, x);
-    // }
-    else {
+    } else if (opts.preconditioner ==
+               gko::preconditioner::batch::type::jacobi) {
+        BatchJacobi<ValueType> prec;
+        apply_impl<stop::AbsAndRelResidualMaxIter<ValueType>>(
+            exec, opts, logger, prec, a, left, right, b, x);
+    } else {
         GKO_NOT_IMPLEMENTED;
     }
 }
