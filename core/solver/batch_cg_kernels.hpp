41a75efab10fc104471212d5c7d33c10e0ccfb44
aggarwal2000 <aggarwal2000chd@gmail.com>
2021-05-26 13:29:57 +0200

    Use dynamic shared memory and copy functions in batch cg

--- core/solver/batch_cg_kernels.hpp
+++ core/solver/batch_cg_kernels.hpp
@@ -59,6 +59,32 @@ struct BatchCgOptions {
 };
 
 
+/**
+ * Calculates the amount of in-solver storage needed by batch-Bicgstab.
+ *
+ * The calculation includes multivectors for
+ * - r
+ * - z
+ * - p
+ * - Ap
+ * - x
+ * and small arrays for
+ * - rho_old
+ * - rho_new
+ * - alpha
+ * - beta
+ * - rhs_norms
+ * - res_norms
+ * - res_temp_norms
+ */
+template <typename ValueType>
+inline int local_memory_requirement(const int num_rows, const int num_rhs)
+{
+    return (5 * num_rows * num_rhs + 4 * num_rhs) * sizeof(ValueType) +
+           3 * num_rhs * sizeof(typename gko::remove_complex<ValueType>);
+}
+
+
 #define GKO_DECLARE_BATCH_CG_APPLY_KERNEL(_type)                            \
     void apply(                                                             \
         std::shared_ptr<const DefaultExecutor> exec,                        \
