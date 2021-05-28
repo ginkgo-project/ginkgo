a8957d141291231501e8e5eddd3ed32914675385
aggarwal2000 <aggarwal2000chd@gmail.com>
2021-05-28 03:58:31 +0200

    Add a test for ref. batch bicgstab, cg, idr

--- examples/temp-example/temp-example.cpp
+++ examples/temp-example/temp-example.cpp
@@ -378,7 +378,7 @@ int main(int argc, char *argv[])
 
     // generate the solver factory object
     auto solver_fac =
-        gko::solver::BatchGmres<ValueType>::build()
+        gko::solver::BatchIdr<ValueType>::build()
             .with_abs_residual_tol(1e-11)
             .with_tolerance_type(gko::stop::batch::ToleranceType::absolute)
             .with_max_iterations(100)
