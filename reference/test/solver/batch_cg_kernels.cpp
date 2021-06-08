e0e35976eb596b206a510027e6b745839bc8cbc9
aggarwal2000 <aggarwal2000chd@gmail.com>
2021-06-08 13:56:25 +0200

    Re-add the option of separate abs and rel tols to the solvers' public interfaces

--- reference/test/solver/batch_cg_kernels.cpp
+++ reference/test/solver/batch_cg_kernels.cpp
@@ -336,7 +336,7 @@ TEST(BatchCg, CanSolveWithoutScaling)
     auto batchcg_factory =
         Solver::build()
             .with_max_iterations(10000)
-            .with_residual_tol(tol)
+            .with_rel_residual_tol(tol)
             .with_tolerance_type(gko::stop::batch::ToleranceType::relative)
             .with_preconditioner(gko::preconditioner::batch::type::jacobi)
             .on(exec);
