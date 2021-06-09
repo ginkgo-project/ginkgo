71be11c6450c5acef5a9133559e213b13369460b
Aditya Kashi <aditya.kashi@kit.edu>
2021-06-10 00:20:19 +0200

    All reductions in batch solvers are now parallel
    - Masked versions of norm and dot are parallel
    - Improved efficiency of row reducion in CSR SpMV

--- cuda/test/stop/batch_criteria.cu
+++ cuda/test/stop/batch_criteria.cu
@@ -56,8 +56,9 @@ namespace cuda {
 constexpr int default_block_size = 128;
 constexpr int sm_multiplier = 4;
 
-#include "common/components/reduction.hpp.inc"
 #include "common/components/uninitialized_array.hpp.inc"
+
+#include "common/components/reduction.hpp.inc"
 #include "common/matrix/batch_dense_kernels.hpp.inc"
 #include "common/stop/batch_criteria.hpp.inc"
 
