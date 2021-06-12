4f3b22a2aaf1d5d8722174256d27f0c5185f9f8e
Yuhsiang M. Tsai <yhmtsai@gmail.com>
2021-06-13 00:17:51 +0200

    initial design on ResourceManager - Executor/LinOp/LinOpFactory/CriterionFactory - Executor/Dense

---
+++ extension/resource_manager/include/resource_manager/base/macro_helper.hpp
@@ -0,0 +1,113 @@
+/*******************************<GINKGO LICENSE>******************************
+Copyright (c) 2017-2021, the Ginkgo authors
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+1. Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+contributors may be used to endorse or promote products derived from
+this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+******************************<GINKGO LICENSE>*******************************/
+
+#ifndef GKOEXT_RESOURCE_MANAGER_BASE_MACRO_HELPER_HPP_
+#define GKOEXT_RESOURCE_MANAGER_BASE_MACRO_HELPER_HPP_
+
+
+#define ENUM_VALUE_(_name) _name
+#define ENUM_VALUE_ASSIGN_(_name, _assign) _name = _assign
+#define MACRO_OVERLOAD_(_1, _2, _NAME, ...) _NAME
+#define ENUM_VALUE(...)                                                   \
+    MACRO_OVERLOAD_(__VA_ARGS__, ENUM_VALUE_ASSIGN_, ENUM_VALUE_, UNUSED) \
+    (__VA_ARGS__)
+
+
+// clang-format off
+#define ENUM_LAMBDA_(_name)                                            \
+    {                                                                  \
+        #_name, [&](arg_type item) {                                   \
+            return this->build_item<enum_type, enum_type::_name>(item); \
+        }                                                              \
+    }
+// clang-format on
+
+#define ENUM_LAMBDA_ASSIGN_(_name, _assign) ENUM_LAMBDA_(_name)
+
+#define ENUM_LAMBDA(...)                                                    \
+    MACRO_OVERLOAD_(__VA_ARGS__, ENUM_LAMBDA_ASSIGN_, ENUM_LAMBDA_, UNUSED) \
+    (__VA_ARGS__)
+
+#define ENUM_CLASS(_enum_type, _type, _list) \
+    enum class _enum_type : _type { _list(ENUM_VALUE) }
+
+#define ENUM_MAP(_name, _enum_type, _return_type, _arg_type, _list, _keyword) \
+    using enum_type = _enum_type;                                             \
+    using arg_type = _arg_type;                                               \
+    _keyword std::map<std::string, std::function<_return_type(arg_type)>>     \
+        _name                                                                 \
+    {                                                                         \
+        _list(ENUM_LAMBDA)                                                    \
+    }
+
+
+#define DECLARE_BASE_BUILD_ITEM(_base_type, _enum_type)                    \
+    std::shared_ptr<_base_type> build_item_impl(_enum_type, std::string &, \
+                                                rapidjson::Value &)
+
+#define IMPLEMENT_BASE_BUILD_ITEM_IMPL(_base_type, _enum_type, _list)         \
+    std::shared_ptr<_base_type> ResourceManager::build_item_impl(             \
+        _enum_type, std::string &base, rapidjson::Value &item)                \
+    {                                                                         \
+        ENUM_MAP(_base_type##Select, _enum_type, std::shared_ptr<_base_type>, \
+                 rapidjson::Value &, _list, static);                          \
+        auto it = _base_type##Select.find(base);                              \
+        if (it == _base_type##Select.end()) {                                 \
+            return nullptr;                                                   \
+        } else {                                                              \
+            return it->second(item);                                          \
+        }                                                                     \
+    }
+
+#define IMPLEMENT_BASE_BUILD_ITEM(_base_type, _enum_base_item)           \
+    template <>                                                          \
+    std::shared_ptr<_base_type> ResourceManager::build_item<_base_type>( \
+        std::string & base, rapidjson::Value & item)                     \
+    {                                                                    \
+        return this->build_item_impl(_enum_base_item, base, item);       \
+    }
+
+
+#define IMPLEMENT_BRIDGE(_enum_type, _enum_item, _impl_type)              \
+    template <>                                                           \
+    std::shared_ptr<typename gkobase<_enum_type>::type>                   \
+        ResourceManager::build_item<_enum_type, _enum_type::_enum_item,   \
+                                    typename gkobase<_enum_type>::type>(  \
+            rapidjson::Value & item)                                      \
+    {                                                                     \
+        return this                                                       \
+            ->build_item<_enum_type, _enum_type::_enum_item, _impl_type>( \
+                item);                                                    \
+    }
+
+
+#endif  // GKOEXT_RESOURCE_MANAGER_BASE_MACRO_HELPER_HPP_
