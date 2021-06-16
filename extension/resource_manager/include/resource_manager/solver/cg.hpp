6bd653e4eb85b297e6f15a22abe90e2f3cd6d7ca
Yuhsiang M. Tsai <yhmtsai@gmail.com>
2021-06-16 14:21:47 +0200

    change the operation to free function

--- extension/resource_manager/include/resource_manager/solver/cg.hpp
+++ extension/resource_manager/include/resource_manager/solver/cg.hpp
@@ -47,17 +47,19 @@ namespace resource_manager {
 namespace {
 template <typename T>
 std::shared_ptr<typename gko::solver::Cg<T>::Factory> build_cg_factory(
-    ResourceManager *rm, rapidjson::Value &item)
+    rapidjson::Value &item, std::shared_ptr<const Executor> exec,
+    std::shared_ptr<const LinOp> linop, ResourceManager *manager)
 {
-    auto exec_ptr = get_pointer<Executor>(rm, item["exec"]);
+    auto exec_ptr = get_pointer<Executor>(manager, item["exec"], exec, linop);
     // auto ptr =
     //     gko::solver::Cg<T>::build()
     //         .with_criteria(
     //             gko::stop::Iteration::build().with_max_iters(10u).on(exec_ptr))
     //         .with_generated_preconditioner(
-    //             get_pointer<LinOp>(rm, item, "generated_preconditioner"))
+    //             get_pointer<LinOp>(manager, item,
+    //             "generated_preconditioner"))
     //         .on(exec_ptr);
-    auto ptr = BUILD_FACTORY(gko::solver::Cg<T>, rm, item)
+    auto ptr = BUILD_FACTORY(gko::solver::Cg<T>, manager, item, exec, linop)
         WITH_POINTER(LinOp, generated_preconditioner)
             WITH_POINTER(LinOpFactory, preconditioner)
                 WITH_POINTER_ARRAY(CriterionFactory, criteria) ON_EXECUTOR;
@@ -66,31 +68,37 @@ std::shared_ptr<typename gko::solver::Cg<T>::Factory> build_cg_factory(
 
 
 template <typename T, typename U = gko::solver::Cg<T>>
-std::shared_ptr<U> build_cg(ResourceManager *rm, rapidjson::Value &item)
+std::shared_ptr<U> build_cg(rapidjson::Value &item,
+                            std::shared_ptr<const Executor> exec,
+                            std::shared_ptr<const LinOp> linop,
+                            ResourceManager *manager)
 {
-    auto factory = get_pointer<typename U::Factory>(rm, item["factory"]);
-    auto mtx = get_pointer<LinOp>(rm, item["generate"]);
+    auto factory =
+        get_pointer<typename U::Factory>(manager, item["factory"], exec, linop);
+    auto mtx = get_pointer<LinOp>(manager, item["generate"], exec, linop);
     auto ptr = factory->generate(mtx);
     return std::move(ptr);
 }
 
 }  // namespace
 
-#define CONNECT_CG(base, T, func)                                       \
-    template <>                                                         \
-    std::shared_ptr<base<T>> ResourceManager::build_item_impl<base<T>>( \
-        rapidjson::Value & item)                                        \
-    {                                                                   \
-        return func<T>(this, item);                                     \
+#define CONNECT_CG(base, T, func)                                      \
+    template <>                                                        \
+    std::shared_ptr<base<T>> create_from_config<base<T>>(              \
+        rapidjson::Value & item, std::shared_ptr<const Executor> exec, \
+        std::shared_ptr<const LinOp> linop, ResourceManager * manager) \
+    {                                                                  \
+        return func<T>(item, exec, linop, manager);                    \
     }
 
-#define CONNECT_FACTORY(base, T, func)                               \
-    template <>                                                      \
-    std::shared_ptr<typename base<T>::Factory>                       \
-        ResourceManager::build_item_impl<typename base<T>::Factory>( \
-            rapidjson::Value & item)                                 \
-    {                                                                \
-        return func<T>(this, item);                                  \
+#define CONNECT_FACTORY(base, T, func)                                 \
+    template <>                                                        \
+    std::shared_ptr<typename base<T>::Factory>                         \
+    create_from_config<typename base<T>::Factory>(                     \
+        rapidjson::Value & item, std::shared_ptr<const Executor> exec, \
+        std::shared_ptr<const LinOp> linop, ResourceManager * manager) \
+    {                                                                  \
+        return func<T>(item, exec, linop, manager);                    \
     }
 
 
@@ -101,9 +109,10 @@ CONNECT_CG(gko::solver::Cg, float, build_cg);
 
 
 template <>
-std::shared_ptr<gko::LinOpFactory>
-ResourceManager::build_item<RM_LinOpFactory, RM_LinOpFactory::CgFactory,
-                            gko::LinOpFactory>(rapidjson::Value &item)
+std::shared_ptr<gko::LinOpFactory> create_from_config<
+    RM_LinOpFactory, RM_LinOpFactory::CgFactory, gko::LinOpFactory>(
+    rapidjson::Value &item, std::shared_ptr<const Executor> exec,
+    std::shared_ptr<const LinOp> linop, ResourceManager *manager)
 {
     std::cout << "build_cg_factory" << std::endl;
     // go though the type
@@ -113,17 +122,30 @@ ResourceManager::build_item<RM_LinOpFactory, RM_LinOpFactory::CgFactory,
     }
     if (vt == std::string{"double"}) {
         using type = double;
-        return this->build_item<gko::solver::Cg<type>::Factory>(item);
+        if (manager == nullptr) {
+            return create_from_config<typename gko::solver::Cg<type>::Factory>(
+                item, exec, linop, manager);
+        } else {
+            return manager->build_item<typename gko::solver::Cg<type>::Factory>(
+                item);
+        }
     } else {
         using type = float;
-        return this->build_item<gko::solver::Cg<type>::Factory>(item);
+        if (manager == nullptr) {
+            return create_from_config<typename gko::solver::Cg<type>::Factory>(
+                item, exec, linop, manager);
+        } else {
+            return manager->build_item<typename gko::solver::Cg<type>::Factory>(
+                item);
+        }
     }
 }
 
 template <>
 std::shared_ptr<gko::LinOp>
-ResourceManager::build_item<RM_LinOp, RM_LinOp::Cg, gko::LinOp>(
-    rapidjson::Value &item)
+create_from_config<RM_LinOp, RM_LinOp::Cg, gko::LinOp>(
+    rapidjson::Value &item, std::shared_ptr<const Executor> exec,
+    std::shared_ptr<const LinOp> linop, ResourceManager *manager)
 {
     std::cout << "build_cg" << std::endl;
     // go though the type
@@ -133,10 +155,20 @@ ResourceManager::build_item<RM_LinOp, RM_LinOp::Cg, gko::LinOp>(
     }
     if (vt == std::string{"double"}) {
         using type = double;
-        return this->build_item<gko::solver::Cg<type>>(item);
+        if (manager == nullptr) {
+            return create_from_config<gko::solver::Cg<type>>(item, exec, linop,
+                                                             manager);
+        } else {
+            return manager->build_item<gko::solver::Cg<type>>(item);
+        }
     } else {
         using type = float;
-        return this->build_item<gko::solver::Cg<type>>(item);
+        if (manager == nullptr) {
+            return create_from_config<gko::solver::Cg<type>>(item, exec, linop,
+                                                             manager);
+        } else {
+            return manager->build_item<gko::solver::Cg<type>>(item);
+        }
     }
 }
 
