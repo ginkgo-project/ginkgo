c5e563a4f0a08cec5a3a24b7dc9353bd30bb923a
Aditya Kashi <aditya.kashi@kit.edu>
2021-06-23 18:07:05 -0400

    separated scaling into separate kernel for batch bicgstab

--- cuda/solver/batch_bicgstab_kernels.cu
+++ cuda/solver/batch_bicgstab_kernels.cu
@@ -80,31 +80,28 @@ using BatchBicgstabOptions =
         <<<nbatch, default_block_size, shared_size>>>(                       \
             opts.max_its, opts.residual_tol, logger, _prectype<ValueType>(), \
             nbatch, nrows, nnz, avalues, col_idxs, row_ptrs, b.stride,       \
-            b.num_rhs, left.values, right.values, b.values, x.values)
+            b.num_rhs, b.values, x.values)
 
 template <typename BatchMatrixType, typename LogType, typename ValueType>
 static void apply_impl(
     std::shared_ptr<const CudaExecutor> exec,
     const BatchBicgstabOptions<remove_complex<ValueType>> opts, LogType logger,
     const BatchMatrixType &a,
-    const gko::batch_dense::UniformBatch<const ValueType> &left,
-    const gko::batch_dense::UniformBatch<const ValueType> &right,
-    const gko::batch_dense::UniformBatch<ValueType> &b,
+    const gko::batch_dense::UniformBatch<const ValueType> &b,
     const gko::batch_dense::UniformBatch<ValueType> &x)
 {
     using real_type = gko::remove_complex<ValueType>;
     const size_type nbatch = a.num_batch;
     const int nrows = a.num_rows;
     const int nnz = a.num_nnz;
-    ValueType *const __restrict__ avalues = a.values;
+    const ValueType *const __restrict__ avalues = a.values;
     const int *const __restrict__ col_idxs = a.col_idxs;
     const int *const __restrict__ row_ptrs = a.row_ptrs;
 
     int shared_size =
 #if GKO_CUDA_BATCH_USE_DYNAMIC_SHARED_MEM
         gko::kernels::batch_bicgstab::local_memory_requirement<ValueType>(
-            a.num_rows, b.num_rhs) +
-        nnz;
+            a.num_rows, b.num_rhs);
 #else
         0;
 #endif
@@ -147,8 +144,6 @@ template <typename ValueType>
 void apply(std::shared_ptr<const CudaExecutor> exec,
            const BatchBicgstabOptions<remove_complex<ValueType>> &opts,
            const BatchLinOp *const a,
-           const matrix::BatchDense<ValueType> *const left_scale,
-           const matrix::BatchDense<ValueType> *const right_scale,
            const matrix::BatchDense<ValueType> *const b,
            matrix::BatchDense<ValueType> *const x,
            log::BatchLogData<ValueType> &logdata)
@@ -160,30 +155,13 @@ void apply(std::shared_ptr<const CudaExecutor> exec,
         static_cast<int>(b->get_size().at(0)[1]), opts.max_its,
         logdata.res_norms->get_values(), logdata.iter_counts.get_data());
 
-    const gko::batch_dense::UniformBatch<const cu_value_type> left_sb =
-        maybe_null_batch_struct(left_scale);
-    const gko::batch_dense::UniformBatch<const cu_value_type> right_sb =
-        maybe_null_batch_struct(right_scale);
-    const auto to_scale = left_sb.values || right_sb.values;
-    if (to_scale) {
-        if (!left_sb.values || !right_sb.values) {
-            // one-sided scaling not implemented
-            GKO_NOT_IMPLEMENTED;
-        }
-    }
-
-
     const gko::batch_dense::UniformBatch<cu_value_type> x_b =
         get_batch_struct(x);
 
     if (auto amat = dynamic_cast<const matrix::BatchCsr<ValueType> *>(a)) {
-        const gko::batch_csr::UniformBatch<cu_value_type> m_b =
-            get_batch_struct(const_cast<matrix::BatchCsr<ValueType> *>(amat));
-
-        const gko::batch_dense::UniformBatch<cu_value_type> b_b =
-            get_batch_struct(const_cast<matrix::BatchDense<ValueType> *>(b));
-
-        apply_impl(exec, opts, logger, m_b, left_sb, right_sb, b_b, x_b);
+        auto m_b = get_batch_struct(amat);
+        auto b_b = get_batch_struct(b);
+        apply_impl(exec, opts, logger, m_b, b_b, x_b);
     } else {
         GKO_NOT_SUPPORTED(a);
     }
