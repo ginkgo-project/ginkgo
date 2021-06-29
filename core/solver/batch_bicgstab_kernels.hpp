770afc7b1fc4e13a31fec2b773e814e3a8d07125
Aditya Kashi <aditya.kashi@kit.edu>
2021-06-29 17:28:50 -0400

    reintroduced logger, criteria; changed default block size, other optimizations

--- core/solver/batch_bicgstab_kernels.hpp
+++ core/solver/batch_bicgstab_kernels.hpp
@@ -71,7 +71,7 @@ struct BatchBicgstabOptions {
  * - s_hat
  * - t
  * - x
- * and small arrays for
+ * Note: small arrays for
  * - rho_old
  * - rho_new
  * - omega
@@ -79,12 +79,13 @@ struct BatchBicgstabOptions {
  * - temp
  * - rhs_norms
  * - res_norms
+ * are currently not accounted for as they are in static shared memory.
  */
 template <typename ValueType>
 inline int local_memory_requirement(const int num_rows, const int num_rhs)
 {
-    return (9 * num_rows * num_rhs + 5 * num_rhs) * sizeof(ValueType) +
-           2 * num_rhs * sizeof(typename gko::remove_complex<ValueType>);
+    return (9 * num_rows * num_rhs) * sizeof(ValueType);
+    //+ 2 * num_rhs * sizeof(typename gko::remove_complex<ValueType>);
 }
 
 
