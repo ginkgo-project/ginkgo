b71156039075039b3f7bca5853457845ba1b13d5
Aditya Kashi <aditya.kashi@kit.edu>
2021-07-19 23:24:26 +0200

    started adding batch direct solver

---
+++ reference/solver/batch_direct_kernels.cpp
@@ -0,0 +1,70 @@
+/*******************************<GINKGO LICENSE>******************************
+Copyright (c) 2017-2021, the Ginkgo authors
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+1. Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+contributors may be used to endorse or promote products derived from
+this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+******************************<GINKGO LICENSE>*******************************/
+
+
+#include "core/solver/batch_direct_kernels.hpp"
+
+
+#include "reference/solver/batch_direct_kernels.hpp"
+
+
+namespace gko {
+namespace kernels {
+namespace reference {
+namespace batch_direct {
+
+
+template <typename ValueType>
+void apply(std::shared_ptr<const ReferenceExecutor> exec,
+           matrix::BatchDense<ValueType> *const a,
+           matrix::BatchDense<ValueType> *const b,
+           gko::log::BatchLogData<ValueType> &logdata) GKO_NOT_IMPLEMENTED;
+
+GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE(GKO_DECLARE_BATCH_DIRECT_APPLY_KERNEL);
+
+
+template <typename ValueType>
+void scale_and_copy(std::shared_ptr<const ReferenceExecutor> exec,
+                    const matrix::BatchDense<ValueType> *const scaling_vec,
+                    const matrix::BatchDense<ValueType> *const orig,
+                    matrix::BatchDense<ValueType> *const scaled)
+{
+    GKO_NOT_IMPLEMENTED;
+}
+
+GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE(GKO_DECLARE_BATCH_DIRECT_SCALE_AND_COPY);
+
+
+}  // namespace batch_direct
+}  // namespace reference
+}  // namespace kernels
+}  // namespace gko
