9725ef029e070bec1dbe3b4b5ffce9cd8279e941
Aditya Kashi <aditya.kashi@kit.edu>
2021-07-26 12:30:00 +0200

    added tests for simplified cuda stopping criterion and logger

--- cuda/test/stop/batch_criteria.cu
+++ cuda/test/stop/batch_criteria.cu
@@ -70,6 +70,7 @@ constexpr int sm_multiplier = 4;
 
 namespace {
 
+
 template <typename T>
 __global__ void conv_check(const int nrhs, const int nrows,
                            const gko::remove_complex<T> *const bnorms,
@@ -213,6 +214,98 @@ TYPED_TEST(RelResMaxIter, DetectsConvergencesWithResidualVector)
     const std::vector<int> conv_col{1, 2};
     this->check_helper(conv_col, false, true);
 }
+
+
+template <typename T>
+__global__ void simple_rel_conv_check(
+    const int nrows, const gko::remove_complex<T> *const bnorms,
+    const gko::remove_complex<T> *const res_norms, bool *const all_conv)
+{
+    using BatchStop = gko::kernels::cuda::stop::SimpleRelResidual<T>;
+    const gko::remove_complex<T> tol = 1e-5;
+    BatchStop bstop(tol, bnorms);
+    *all_conv = bstop.check_converged(res_norms);
+}
+
+
+template <typename T>
+__global__ void simple_abs_conv_check(
+    const int nrows, const gko::remove_complex<T> *const res_norms,
+    bool *const all_conv)
+{
+    using BatchStop = gko::kernels::cuda::stop::SimpleAbsResidual<T>;
+    const gko::remove_complex<T> tol = 1e-5;
+    BatchStop bstop(tol, nullptr);
+    *all_conv = bstop.check_converged(res_norms);
+}
+
+
+class SimpleRes : public ::testing::Test {
+protected:
+    using value_type = double;
+    using real_type = gko::remove_complex<value_type>;
+
+    SimpleRes()
+        : exec(gko::ReferenceExecutor::create()),
+          cuexec(gko::CudaExecutor::create(0, exec))
+    {}
+
+    std::shared_ptr<gko::ReferenceExecutor> exec;
+    std::shared_ptr<const gko::CudaExecutor> cuexec;
+    const int nrows = 100;
+    const real_type tol = 1e-5;
+
+    void check_helper(const bool relative, const bool check_converged)
+    {
+        const auto dbs = gko::kernels::cuda::default_block_size;
+        gko::Array<real_type> h_resnorms(this->exec, 1);
+        gko::Array<real_type> h_bnorms(this->exec, 1);
+        if (check_converged) {
+            h_bnorms.get_data()[0] = 1.0e6;
+            if (relative) {
+                h_resnorms.get_data()[0] = h_bnorms.get_data()[0] / (tol * 10);
+            } else {
+                h_resnorms.get_data()[0] = tol / 10.0;
+            }
+        } else {
+            h_bnorms.get_data()[0] = 1.0e-6;
+            if (relative) {
+                h_resnorms.get_data()[0] = 5 * tol * h_bnorms.get_data()[0];
+            } else {
+                h_resnorms.get_data()[0] = 5 * tol;
+            }
+        }
+
+        const gko::Array<real_type> resnorms(this->cuexec, h_resnorms);
+        const gko::Array<real_type> bnorms(this->cuexec, h_bnorms);
+        gko::Array<bool> all_conv(this->cuexec, 1);
+
+        if (relative) {
+            simple_rel_conv_check<value_type>
+                <<<1, dbs>>>(nrows, bnorms.get_const_data(),
+                             resnorms.get_const_data(), all_conv.get_data());
+        } else {
+            simple_abs_conv_check<value_type><<<1, dbs>>>(
+                nrows, resnorms.get_const_data(), all_conv.get_data());
+        }
+
+        gko::Array<bool> h_all_conv(this->exec, all_conv);
+        if (check_converged) {
+            ASSERT_TRUE(h_all_conv.get_const_data()[0]);
+        } else {
+            ASSERT_FALSE(h_all_conv.get_const_data()[0]);
+        }
+    }
+};
+
+
+TEST_F(SimpleRes, RelDetectsConvergence) { check_helper(true, true); }
+
+TEST_F(SimpleRes, RelDetectsDivergence) { check_helper(true, false); }
+
+TEST_F(SimpleRes, AbsDetectsConvergence) { check_helper(false, true); }
+
+TEST_F(SimpleRes, AbsDetectsDivergence) { check_helper(false, false); }
 
 
 }  // namespace
