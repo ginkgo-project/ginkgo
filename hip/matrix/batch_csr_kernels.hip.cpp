db58ca7a8aadbfdfd2c60cd31404f28096ed58af
Aditya Kashi <aditya.kashi@kit.edu>
2021-09-09 07:59:46 -0400

    batch csr kernels and tests work

--- hip/matrix/batch_csr_kernels.hip.cpp
+++ hip/matrix/batch_csr_kernels.hip.cpp
@@ -1,3 +1,4 @@
+#include "hip/hip_runtime.h"
 /*******************************<GINKGO LICENSE>******************************
 Copyright (c) 2017-2021, the Ginkgo authors
 All rights reserved.
@@ -36,9 +37,6 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <algorithm>
 
 
-#include <hip/hip_runtime.h>
-
-
 #include <ginkgo/core/base/array.hpp>
 #include <ginkgo/core/base/exception_helpers.hpp>
 #include <ginkgo/core/base/math.hpp>
@@ -47,9 +45,9 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include "core/components/fill_array.hpp"
 #include "core/components/prefix_sum.hpp"
-#include "core/matrix/dense_kernels.hpp"
+#include "core/matrix/batch_struct.hpp"
 #include "core/synthesizer/implementation_selection.hpp"
-//#include "hip/base/config.hip.hpp"
+#include "hip/base/config.hip.hpp"
 #include "hip/base/hipsparse_bindings.hip.hpp"
 #include "hip/base/math.hip.hpp"
 #include "hip/base/pointer_mode_guard.hip.hpp"
@@ -62,7 +60,8 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "hip/components/segment_scan.hip.hpp"
 #include "hip/components/thread_ids.hip.hpp"
 #include "hip/components/uninitialized_array.hip.hpp"
-//#include "hip/matrix/batch_struct.hip.hpp"
+#include "hip/matrix/batch_struct.hip.hpp"
+
 
 namespace gko {
 namespace kernels {
@@ -77,41 +76,24 @@ namespace batch_csr {
 
 constexpr int default_block_size = 512;
 constexpr int sm_multiplier = 4;
-// constexpr int warps_in_block = 4;
-// constexpr int spmv_block_size = warps_in_block * config::warp_size;
-// constexpr int wsize = config::warp_size;
-// constexpr int classical_overweight = 32;
 
 
-/**
- * A compile-time list of the number items per threads for which spmv kernel
- * should be compiled.
- */
-using compiled_kernels = syn::value_list<int, 3, 4, 6, 7, 8, 12, 14>;
+#include "common/cuda_hip/matrix/batch_csr_kernels.hpp.inc"
 
-using classical_kernels =
-    syn::value_list<int, config::warp_size, 32, 16, 8, 4, 2, 1>;
-
-using spgeam_kernels =
-    syn::value_list<int, 1, 2, 4, 8, 16, 32, config::warp_size>;
-
-
-//#include "common/matrix/batch_csr_kernels.hpp.inc"
 
 template <typename ValueType, typename IndexType>
 void spmv(std::shared_ptr<const HipExecutor> exec,
-          const matrix::BatchCsr<ValueType, IndexType> *const a,
-          const matrix::BatchDense<ValueType> *const b,
-          matrix::BatchDense<ValueType> *const c) GKO_NOT_IMPLEMENTED;
-// {
-//     const auto num_blocks = exec->get_num_multiprocessor() * sm_multiplier;
-//     const auto a_ub = get_batch_struct(a);
-//     const auto b_ub = get_batch_struct(b);
-//     const auto c_ub = get_batch_struct(c);
-//     hipLaunchKernelGGL(HIP_KERNEL_NAME(spmv), dim3(num_blocks),
-//                        dim3(default_block_size), 0, 0, a_ub, b_ub, c_ub);
-// }
-
+          const matrix::BatchCsr<ValueType, IndexType>* const a,
+          const matrix::BatchDense<ValueType>* const b,
+          matrix::BatchDense<ValueType>* const c)
+{
+    const auto num_blocks = exec->get_num_multiprocessor() * sm_multiplier;
+    const auto a_ub = get_batch_struct(a);
+    const auto b_ub = get_batch_struct(b);
+    const auto c_ub = get_batch_struct(c);
+    hipLaunchKernelGGL(spmv, dim3(num_blocks), dim3(default_block_size), 0, 0,
+                       a_ub, b_ub, c_ub);
+}
 
 GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE_AND_INT32_INDEX(
     GKO_DECLARE_BATCH_CSR_SPMV_KERNEL);
@@ -119,23 +101,22 @@ GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE_AND_INT32_INDEX(
 
 template <typename ValueType, typename IndexType>
 void advanced_spmv(std::shared_ptr<const HipExecutor> exec,
-                   const matrix::BatchDense<ValueType> *const alpha,
-                   const matrix::BatchCsr<ValueType, IndexType> *const a,
-                   const matrix::BatchDense<ValueType> *const b,
-                   const matrix::BatchDense<ValueType> *const beta,
-                   matrix::BatchDense<ValueType> *const c) GKO_NOT_IMPLEMENTED;
-// {
-//     const auto num_blocks = exec->get_num_multiprocessor() * sm_multiplier;
-//     const auto a_ub = get_batch_struct(a);
-//     const auto b_ub = get_batch_struct(b);
-//     const auto c_ub = get_batch_struct(c);
-//     const auto alpha_ub = get_batch_struct(alpha);
-//     const auto beta_ub = get_batch_struct(beta);
-//     hipLaunchKernelGGL(HIP_KERNEL_NAME(advanced_spmv), dim3(num_blocks),
-//                        dim3(default_block_size), 0, 0, alpha_ub, a_ub, b_ub,
-//                        beta_ub, c_ub);
-// }
-
+                   const matrix::BatchDense<ValueType>* const alpha,
+                   const matrix::BatchCsr<ValueType, IndexType>* const a,
+                   const matrix::BatchDense<ValueType>* const b,
+                   const matrix::BatchDense<ValueType>* const beta,
+                   matrix::BatchDense<ValueType>* const c)
+{
+    const auto num_blocks = exec->get_num_multiprocessor() * sm_multiplier;
+    const auto a_ub = get_batch_struct(a);
+    const auto b_ub = get_batch_struct(b);
+    const auto c_ub = get_batch_struct(c);
+    const auto alpha_ub = get_batch_struct(alpha);
+    const auto beta_ub = get_batch_struct(beta);
+    hipLaunchKernelGGL(advanced_spmv, dim3(num_blocks),
+                       dim3(default_block_size), 0, 0, alpha_ub, a_ub, b_ub,
+                       beta_ub, c_ub);
+}
 
 GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE_AND_INT32_INDEX(
     GKO_DECLARE_BATCH_CSR_ADVANCED_SPMV_KERNEL);
@@ -143,16 +124,15 @@ GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE_AND_INT32_INDEX(
 
 template <typename IndexType>
 void convert_row_ptrs_to_idxs(std::shared_ptr<const HipExecutor> exec,
-                              const IndexType *ptrs, size_type num_rows,
-                              IndexType *idxs) GKO_NOT_IMPLEMENTED;
+                              const IndexType* ptrs, size_type num_rows,
+                              IndexType* idxs) GKO_NOT_IMPLEMENTED;
 
 
 template <typename ValueType, typename IndexType>
 void convert_to_dense(std::shared_ptr<const HipExecutor> exec,
-                      const matrix::BatchCsr<ValueType, IndexType> *source,
-                      matrix::BatchDense<ValueType> *result)
+                      const matrix::BatchCsr<ValueType, IndexType>* source,
+                      matrix::BatchDense<ValueType>* result)
     GKO_NOT_IMPLEMENTED;
-
 
 GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE_AND_INT32_INDEX(
     GKO_DECLARE_BATCH_CSR_CONVERT_TO_DENSE_KERNEL);
@@ -160,10 +140,9 @@ GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE_AND_INT32_INDEX(
 
 template <typename ValueType, typename IndexType>
 void calculate_total_cols(std::shared_ptr<const HipExecutor> exec,
-                          const matrix::BatchCsr<ValueType, IndexType> *source,
-                          size_type *result, size_type stride_factor,
+                          const matrix::BatchCsr<ValueType, IndexType>* source,
+                          size_type* result, size_type stride_factor,
                           size_type slice_size) GKO_NOT_IMPLEMENTED;
-
 
 GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE_AND_INT32_INDEX(
     GKO_DECLARE_BATCH_CSR_CALCULATE_TOTAL_COLS_KERNEL);
@@ -171,10 +150,9 @@ GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE_AND_INT32_INDEX(
 
 template <typename ValueType, typename IndexType>
 void transpose(std::shared_ptr<const HipExecutor> exec,
-               const matrix::BatchCsr<ValueType, IndexType> *orig,
-               matrix::BatchCsr<ValueType, IndexType> *trans)
+               const matrix::BatchCsr<ValueType, IndexType>* orig,
+               matrix::BatchCsr<ValueType, IndexType>* trans)
     GKO_NOT_IMPLEMENTED;
-
 
 GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE_AND_INT32_INDEX(
     GKO_DECLARE_BATCH_CSR_TRANSPOSE_KERNEL);
@@ -182,10 +160,9 @@ GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE_AND_INT32_INDEX(
 
 template <typename ValueType, typename IndexType>
 void conj_transpose(std::shared_ptr<const HipExecutor> exec,
-                    const matrix::BatchCsr<ValueType, IndexType> *orig,
-                    matrix::BatchCsr<ValueType, IndexType> *trans)
+                    const matrix::BatchCsr<ValueType, IndexType>* orig,
+                    matrix::BatchCsr<ValueType, IndexType>* trans)
     GKO_NOT_IMPLEMENTED;
-
 
 GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE_AND_INT32_INDEX(
     GKO_DECLARE_BATCH_CSR_CONJ_TRANSPOSE_KERNEL);
@@ -194,9 +171,8 @@ GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE_AND_INT32_INDEX(
 template <typename ValueType, typename IndexType>
 void calculate_max_nnz_per_row(
     std::shared_ptr<const HipExecutor> exec,
-    const matrix::BatchCsr<ValueType, IndexType> *source,
-    size_type *result) GKO_NOT_IMPLEMENTED;
-
+    const matrix::BatchCsr<ValueType, IndexType>* source,
+    size_type* result) GKO_NOT_IMPLEMENTED;
 
 GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE_AND_INT32_INDEX(
     GKO_DECLARE_BATCH_CSR_CALCULATE_MAX_NNZ_PER_ROW_KERNEL);
@@ -205,9 +181,8 @@ GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE_AND_INT32_INDEX(
 template <typename ValueType, typename IndexType>
 void calculate_nonzeros_per_row(
     std::shared_ptr<const HipExecutor> exec,
-    const matrix::BatchCsr<ValueType, IndexType> *source,
-    Array<size_type> *result) GKO_NOT_IMPLEMENTED;
-
+    const matrix::BatchCsr<ValueType, IndexType>* source,
+    Array<size_type>* result) GKO_NOT_IMPLEMENTED;
 
 GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE_AND_INT32_INDEX(
     GKO_DECLARE_BATCH_CSR_CALCULATE_NONZEROS_PER_ROW_KERNEL);
@@ -215,9 +190,8 @@ GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE_AND_INT32_INDEX(
 
 template <typename ValueType, typename IndexType>
 void sort_by_column_index(std::shared_ptr<const HipExecutor> exec,
-                          matrix::BatchCsr<ValueType, IndexType> *to_sort)
+                          matrix::BatchCsr<ValueType, IndexType>* to_sort)
     GKO_NOT_IMPLEMENTED;
-
 
 GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE_AND_INT32_INDEX(
     GKO_DECLARE_BATCH_CSR_SORT_BY_COLUMN_INDEX);
@@ -226,9 +200,8 @@ GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE_AND_INT32_INDEX(
 template <typename ValueType, typename IndexType>
 void is_sorted_by_column_index(
     std::shared_ptr<const HipExecutor> exec,
-    const matrix::BatchCsr<ValueType, IndexType> *to_check,
-    bool *is_sorted) GKO_NOT_IMPLEMENTED;
-
+    const matrix::BatchCsr<ValueType, IndexType>* to_check,
+    bool* is_sorted) GKO_NOT_IMPLEMENTED;
 
 GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE_AND_INT32_INDEX(
     GKO_DECLARE_BATCH_CSR_IS_SORTED_BY_COLUMN_INDEX);
@@ -236,25 +209,22 @@ GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE_AND_INT32_INDEX(
 
 template <typename ValueType, typename IndexType>
 void batch_scale(std::shared_ptr<const HipExecutor> exec,
-                 const matrix::BatchDense<ValueType> *const left_scale,
-                 const matrix::BatchDense<ValueType> *const right_scale,
-                 matrix::BatchCsr<ValueType, IndexType> *const mat)
-    GKO_NOT_IMPLEMENTED;
-// {
-//     if (!left_scale->get_size().stores_equal_sizes()) GKO_NOT_IMPLEMENTED;
-//     if (!right_scale->get_size().stores_equal_sizes()) GKO_NOT_IMPLEMENTED;
+                 const matrix::BatchDense<ValueType>* const left_scale,
+                 const matrix::BatchDense<ValueType>* const right_scale,
+                 matrix::BatchCsr<ValueType, IndexType>* const mat)
+{
+    if (!left_scale->get_size().stores_equal_sizes()) GKO_NOT_IMPLEMENTED;
+    if (!right_scale->get_size().stores_equal_sizes()) GKO_NOT_IMPLEMENTED;
 
-//     const auto m_ub = get_batch_struct(mat);
-//     const auto left_ub = get_batch_struct(left_scale);
-//     const auto right_ub = get_batch_struct(right_scale);
+    const auto m_ub = get_batch_struct(mat);
 
-//     constexpr int block_size = 512;
-//     const int num_blocks = exec->get_num_multiprocessor();
-//     hipLaunchKernelGGL(HIP_KERNEL_NAME(uniform_batch_scale),
-//     dim3(num_blocks),
-//                        dim3(block_size), 0, 0, left_ub, right_ub, m_ub);
-// }
-
+    const int num_blocks = mat->get_num_batch_entries();
+    hipLaunchKernelGGL(uniform_batch_scale, dim3(num_blocks),
+                       dim3(default_block_size), 0, 0,
+                       as_hip_type(left_scale->get_const_values()),
+                       as_hip_type(right_scale->get_const_values()), m_ub,
+                       mat->get_size().at()[1]);
+}
 
 GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE_AND_INT32_INDEX(
     GKO_DECLARE_BATCH_CSR_SCALE);
@@ -263,10 +233,24 @@ GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE_AND_INT32_INDEX(
 template <typename ValueType, typename IndexType>
 void pre_diag_scale_system(
     std::shared_ptr<const HipExecutor> exec,
-    const matrix::BatchDense<ValueType> *const left_scale,
-    const matrix::BatchDense<ValueType> *const right_scale,
-    matrix::BatchCsr<ValueType, IndexType> *const a,
-    matrix::BatchDense<ValueType> *const b) GKO_NOT_IMPLEMENTED;
+    const matrix::BatchDense<ValueType>* const left_scale,
+    const matrix::BatchDense<ValueType>* const right_scale,
+    matrix::BatchCsr<ValueType, IndexType>* const a,
+    matrix::BatchDense<ValueType>* const b)
+{
+    const size_type nbatch = a->get_num_batch_entries();
+    const int nrows = static_cast<int>(a->get_size().at()[0]);
+    const size_type nnz = a->get_num_stored_elements() / nbatch;
+    const int nrhs = static_cast<int>(b->get_size().at()[1]);
+    const size_type b_stride = b->get_stride().at();
+    hipLaunchKernelGGL(pre_diag_scale_system, dim3(nbatch),
+                       dim3(default_block_size), 0, 0, nbatch, nrows, nnz,
+                       as_hip_type(a->get_values()), a->get_const_col_idxs(),
+                       a->get_const_row_ptrs(), nrhs, b_stride,
+                       as_hip_type(b->get_values()),
+                       as_hip_type(left_scale->get_const_values()),
+                       as_hip_type(right_scale->get_const_values()));
+}
 
 GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE_AND_INT32_INDEX(
     GKO_DECLARE_BATCH_CSR_PRE_DIAG_SCALE_SYSTEM);
@@ -275,8 +259,21 @@ GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE_AND_INT32_INDEX(
 template <typename ValueType, typename IndexType>
 void convert_to_batch_dense(
     std::shared_ptr<const HipExecutor> exec,
-    const matrix::BatchCsr<ValueType, IndexType> *const src,
-    matrix::BatchDense<ValueType> *const dest) GKO_NOT_IMPLEMENTED;
+    const matrix::BatchCsr<ValueType, IndexType>* const src,
+    matrix::BatchDense<ValueType>* const dest)
+{
+    const size_type nbatches = src->get_num_batch_entries();
+    const int nrows = src->get_size().at()[0];
+    const int ncols = src->get_size().at()[1];
+    const int nnz = static_cast<int>(src->get_num_stored_elements() / nbatches);
+    const size_type dstride = dest->get_stride().at();
+    hipLaunchKernelGGL(uniform_convert_to_batch_dense, dim3(nbatches),
+                       dim3(default_block_size), 0, 0, nbatches, nrows, ncols,
+                       nnz, src->get_const_row_ptrs(),
+                       src->get_const_col_idxs(),
+                       as_hip_type(src->get_const_values()), dstride,
+                       as_hip_type(dest->get_values()));
+}
 
 GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE_AND_INT32_INDEX(
     GKO_DECLARE_BATCH_CSR_CONVERT_TO_BATCH_DENSE);
