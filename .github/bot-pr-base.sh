02cfbce7f0dd5a7e2d5501ed5e3cb8e84f2ac5b5
Tobias Ribizel <ribizel@kit.edu>
2021-09-10 18:12:18 +0200

    add formatting rebase bot command

--- .github/bot-pr-base.sh
+++ .github/bot-pr-base.sh
@@ -2,6 +2,11 @@
 
 source .github/bot-base.sh
 
+EXTENSION_REGEX='\.(cuh?|hpp|hpp\.inc?|cpp)$'
+FORMAT_HEADER_REGEX='^(benchmark|core|cuda|hip|include/ginkgo/core|omp|reference|dpcpp|common/unified)/'
+FORMAT_REGEX='^(common|examples|test_install)/'
+CLANG_FORMAT=clang-format-9
+
 echo -n "Collecting information on triggering PR"
 PR_URL=$(jq -r .pull_request.url "$GITHUB_EVENT_PATH")
 if [[ "$PR_URL" == "null" ]]; then
