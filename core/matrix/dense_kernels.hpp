54dc115840c680c94c9d11e02c8757687d7d2af0
Gregor Olenik <gregor.olenik@web.de>
2021-09-18 08:58:18 +0200

    Add COMPUTE_NORM1 declare macro

--- core/matrix/dense_kernels.hpp
+++ core/matrix/dense_kernels.hpp
@@ -121,6 +121,11 @@ namespace kernels {
                        const matrix::Dense<_type>* x,               \
                        matrix::Dense<remove_complex<_type>>* result)
 
+#define GKO_DECLARE_DENSE_COMPUTE_NORM1_KERNEL(_type)               \
+    void compute_norm1(std::shared_ptr<const DefaultExecutor> exec, \
+                       const matrix::Dense<_type> *x,               \
+                       matrix::Dense<remove_complex<_type>> *result)
+
 #define GKO_DECLARE_DENSE_CONVERT_TO_COO_KERNEL(_type, _prec)        \
     void convert_to_coo(std::shared_ptr<const DefaultExecutor> exec, \
                         const matrix::Dense<_type>* source,          \
