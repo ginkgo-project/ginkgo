133c60176244d96233b03fea93db5ac427f831ff
Gregor Olenik <go@hpsim.de>
2021-09-18 22:47:29 +0200

    Implement dpcpp norm1

--- dpcpp/test/matrix/dense_kernels.cpp
+++ dpcpp/test/matrix/dense_kernels.cpp
@@ -243,6 +243,20 @@ TEST_F(Dense, DpcppComputeNorm2IsEquivalentToRef)
 }
 
 
+TEST_F(Dense, DpcppComputeNorm1IsEquivalentToRef)
+{
+    set_up_vector_data(20);
+    auto norm_size = gko::dim<2>{1, x->get_size()[1]};
+    auto norm_expected = NormVector::create(this->ref, norm_size);
+    auto dnorm = NormVector::create(this->dpcpp, norm_size);
+
+    x->compute_norm1(norm_expected.get());
+    dx->compute_norm1(dnorm.get());
+
+    GKO_ASSERT_MTX_NEAR(norm_expected, dnorm, r<vtype>::value);
+}
+
+
 TEST_F(Dense, SimpleApplyIsEquivalentToRef)
 {
     set_up_apply_data();
