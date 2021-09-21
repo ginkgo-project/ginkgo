655a7f9fc1b859e14ec59af6d1cdf381ef9cf6d2
Aditya Kashi <aditya.kashi@kit.edu>
2021-09-21 12:34:29 -0400

    completed batch cg hip port

--- hip/solver/batch_cg_kernels.hip.cpp
+++ hip/solver/batch_cg_kernels.hip.cpp
@@ -33,22 +33,21 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "core/solver/batch_cg_kernels.hpp"
 
 
-#include <hip/hip_runtime.h>
-
-
 #include <ginkgo/core/base/math.hpp>
 
 
-#include "hip/base/math.hip.hpp"
+#include "hip/base/config.hip.hpp"
+#include "hip/base/exception.hip.hpp"
 #include "hip/base/types.hip.hpp"
 #include "hip/components/cooperative_groups.hip.hpp"
+#include "hip/components/thread_ids.hip.hpp"
+#include "hip/matrix/batch_struct.hip.hpp"
 
 
 namespace gko {
 namespace kernels {
 namespace hip {
 
-#define GKO_CUDA_BATCH_USE_DYNAMIC_SHARED_MEM 1
 constexpr int default_block_size = 256;
 constexpr int sm_multiplier = 4;
 
@@ -59,31 +58,94 @@ constexpr int sm_multiplier = 4;
  */
 namespace batch_cg {
 
-// #include "common/components/uninitialized_array.hpp.inc"
 
+#include "common/cuda_hip/components/uninitialized_array.hpp.inc"
+// include all depedencies (note: do not remove this comment)
+#include "common/cuda_hip/components/reduction.hpp.inc"
+#include "common/cuda_hip/log/batch_logger.hpp.inc"
+#include "common/cuda_hip/matrix/batch_csr_kernels.hpp.inc"
+#include "common/cuda_hip/matrix/batch_vector_kernels.hpp.inc"
+#include "common/cuda_hip/preconditioner/batch_identity.hpp.inc"
+#include "common/cuda_hip/preconditioner/batch_jacobi.hpp.inc"
+#include "common/cuda_hip/solver/batch_cg_kernels.hpp.inc"
+#include "common/cuda_hip/stop/batch_criteria.hpp.inc"
 
-// #include "common/log/batch_logger.hpp.inc"
-// #include "common/matrix/batch_csr_kernels.hpp.inc"
-// #include "common/matrix/batch_dense_kernels.hpp.inc"
-// #include "common/preconditioner/batch_identity.hpp.inc"
-// #include "common/preconditioner/batch_jacobi.hpp.inc"
-// #include "common/stop/batch_criteria.hpp.inc"
-
-
-// #include "common/solver/batch_cg_kernels.hpp.inc"
 
 template <typename T>
 using BatchCgOptions = gko::kernels::batch_cg::BatchCgOptions<T>;
 
+#define BATCH_CG_KERNEL_LAUNCH(_stoppertype, _prectype)                        \
+    hipLaunchKernelGGL(apply_kernel<stop::_stoppertype<ValueType>>,            \
+                       dim3(nbatch), dim3(default_block_size), shared_size, 0, \
+                       opts.max_its, opts.residual_tol, logger,                \
+                       _prectype<ValueType>(), a, b.stride, b.num_rhs,         \
+                       b.values, x.values)
+
+template <typename BatchMatrixType, typename LogType, typename ValueType>
+static void apply_impl(std::shared_ptr<const HipExecutor> exec,
+                       const BatchCgOptions<remove_complex<ValueType>> opts,
+                       LogType logger, const BatchMatrixType& a,
+                       const gko::batch_dense::UniformBatch<const ValueType>& b,
+                       const gko::batch_dense::UniformBatch<ValueType>& x)
+{
+    using real_type = gko::remove_complex<ValueType>;
+    const size_type nbatch = a.num_batch;
+
+    int shared_size =
+        gko::kernels::batch_cg::local_memory_requirement<ValueType>(a.num_rows,
+                                                                    b.num_rhs);
+
+    if (opts.preconditioner == gko::preconditioner::batch::type::none) {
+        shared_size +=
+            BatchIdentity<ValueType>::dynamic_work_size(a.num_rows, a.num_nnz) *
+            sizeof(ValueType);
+        if (opts.tol_type == gko::stop::batch::ToleranceType::absolute) {
+            BATCH_CG_KERNEL_LAUNCH(SimpleAbsResidual, BatchIdentity);
+        } else {
+            BATCH_CG_KERNEL_LAUNCH(SimpleRelResidual, BatchIdentity);
+        }
+
+    } else if (opts.preconditioner ==
+               gko::preconditioner::batch::type::jacobi) {
+        shared_size +=
+            BatchJacobi<ValueType>::dynamic_work_size(a.num_rows, a.num_nnz) *
+            sizeof(ValueType);
+        if (opts.tol_type == gko::stop::batch::ToleranceType::absolute) {
+            BATCH_CG_KERNEL_LAUNCH(SimpleAbsResidual, BatchJacobi);
+        } else {
+            BATCH_CG_KERNEL_LAUNCH(SimpleRelResidual, BatchJacobi);
+        }
+    } else {
+        GKO_NOT_IMPLEMENTED;
+    }
+    GKO_HIP_LAST_IF_ERROR_THROW;
+}
+
 
 template <typename ValueType>
 void apply(std::shared_ptr<const HipExecutor> exec,
-           const BatchCgOptions<remove_complex<ValueType>> &opts,
-           const BatchLinOp *const a,
-           const matrix::BatchDense<ValueType> *const b,
-           matrix::BatchDense<ValueType> *const x,
-           log::BatchLogData<ValueType> &logdata) GKO_NOT_IMPLEMENTED;
+           const BatchCgOptions<remove_complex<ValueType>>& opts,
+           const BatchLinOp* const a,
+           const matrix::BatchDense<ValueType>* const b,
+           matrix::BatchDense<ValueType>* const x,
+           log::BatchLogData<ValueType>& logdata)
+{
+    using cu_value_type = hip_type<ValueType>;
 
+    batch_log::SimpleFinalLogger<remove_complex<ValueType>> logger(
+        logdata.res_norms->get_values(), logdata.iter_counts.get_data());
+
+    const gko::batch_dense::UniformBatch<cu_value_type> x_b =
+        get_batch_struct(x);
+
+    if (auto amat = dynamic_cast<const matrix::BatchCsr<ValueType>*>(a)) {
+        const auto m_b = get_batch_struct(amat);
+        const auto b_b = get_batch_struct(b);
+        apply_impl(exec, opts, logger, m_b, b_b, x_b);
+    } else {
+        GKO_NOT_SUPPORTED(a);
+    }
+}
 
 GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE(GKO_DECLARE_BATCH_CG_APPLY_KERNEL);
 
