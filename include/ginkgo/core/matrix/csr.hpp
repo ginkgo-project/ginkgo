3dad106a9836dbe15330d1abf90539d22927f9f9
Aditya Kashi <aditya.kashi@kit.edu>
2021-09-30 18:21:44 +0200

    added function and test for concatenating CSR matrices into one block-diagonal matrix

--- include/ginkgo/core/matrix/csr.hpp
+++ include/ginkgo/core/matrix/csr.hpp
@@ -1305,6 +1305,23 @@ private:
 };
 
 
+/**
+ * Generates a single large block-diagonal CSR matrix from the given CSR
+ * matrices.
+ *
+ * @param exec  Executor on which both the input and output reside
+ * @param matrices  List of matrices to be concatenated in to one
+ *   block-diagonal matrix.
+ *
+ * @warning Not for use in performance-critical code! The operation currently
+ *   happens on the host and copies are performed.
+ */
+template <typename ValueType, typename IndexType>
+std::unique_ptr<Csr<ValueType, IndexType>> block_diagonal_csr_matrix(
+    std::shared_ptr<const Executor> exec,
+    const std::vector<std::unique_ptr<Csr<ValueType, IndexType>>>& matrices);
+
+
 namespace detail {
 
 
