45eadb5ae49c0fe932684ba453036ae2aa785e34
Marcel Koch <marcel.koch@kit.edu>
2021-10-01 17:46:52 +0200

    start implementing core for constrained_system

--- include/ginkgo/ginkgo.hpp
+++ include/ginkgo/ginkgo.hpp
@@ -69,6 +69,8 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <ginkgo/core/base/utils_helper.hpp>
 #include <ginkgo/core/base/version.hpp>
 
+#include <ginkgo/core/constraints/constrained_system.hpp>
+
 #include <ginkgo/core/distributed/partition.hpp>
 
 #include <ginkgo/core/factorization/ic.hpp>
