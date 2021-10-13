13aac7350cad11e2465ee0b63c2313958fef5824
Tobias Ribizel <ribizel@kit.edu>
2021-10-13 14:56:40 +0200

    fix CUDA compilation implicit include paths

--- cuda/test/preconditioner/isai_kernels.cpp
+++ cuda/test/preconditioner/isai_kernels.cpp
@@ -48,7 +48,6 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
 #include "core/preconditioner/isai_kernels.hpp"
-#include "cuda/base/config.hpp"
 #include "cuda/test/utils.hpp"
 #include "matrices/config.hpp"
 
