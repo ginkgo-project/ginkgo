f7537236297c9f3cb836c30e16d357e9d7655ccf
Yuhsiang M. Tsai <yhmtsai@gmail.com>
2021-10-15 11:23:47 +0200

    apply residual cacheable

--- core/solver/multigrid.cpp
+++ core/solver/multigrid.cpp
@@ -271,11 +271,17 @@ struct MultigridState {
                        mid_case == multigrid::mid_smooth_type::both ||
                        mid_case == multigrid::mid_smooth_type::pre_smoother;
         if (use_pre && pre_smoother) {
-            pre_smoother->apply(b, x);
-            // additional residual computation after pre_smoother if it already
-            // contained the residual.
-            r->copy_from(b);  // n * b
-            matrix->apply(neg_one, x, one, r.get());
+            // avoid additional residual calculation
+            if (auto residual_cacheable =
+                    std::dynamic_pointer_cast<const ResidualCacheable>(
+                        pre_smoother)) {
+                residual_cacheable->set_residual_cache(r);
+                pre_smoother->apply(b, x);
+            } else {
+                pre_smoother->apply(b, x);
+                r->copy_from(b);  // n * b
+                matrix->apply(neg_one, x, one, r.get());
+            }
         } else if (level != 0) {
             // move the residual computation at level 0 to out-of-cycle if there
             // is no pre-smoother at level 0
