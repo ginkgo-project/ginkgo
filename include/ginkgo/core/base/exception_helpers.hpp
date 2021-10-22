e8c76a8e54781bf3ad04992f76eca71ea1a2f5fb
Pratik Nayak <pratikvn@protonmail.com>
2021-10-22 14:01:32 +0200

    Add exception handling and helpers

--- include/ginkgo/core/base/exception_helpers.hpp
+++ include/ginkgo/core/base/exception_helpers.hpp
@@ -299,6 +299,39 @@ inline dim<2> get_size(const dim<2>& size) { return size; }
 
 
 /**
+ * Instantiates a MpiError.
+ *
+ * @param errcode  The error code returned from the MPI routine.
+ */
+#define GKO_MPI_ERROR(_errcode) \
+    ::gko::MpiError(__FILE__, __LINE__, __func__, _errcode)
+
+/**
+ * Throws when MPI has already been initialized.
+ *
+ */
+#define GKO_MPI_INITIALIZED                                                  \
+    {                                                                        \
+        throw ::gko::MpiInitialized(__FILE__, __LINE__, __func__);           \
+    }                                                                        \
+    static_assert(true,                                                      \
+                  "This assert is used to counter the false positive extra " \
+                  "semi-colon warnings")
+
+/**
+ * Throws when MPI has already been finalized.
+ *
+ */
+#define GKO_MPI_FINALIZED                                                    \
+    {                                                                        \
+        throw ::gko::MpiFinalized(__FILE__, __LINE__, __func__);             \
+    }                                                                        \
+    static_assert(true,                                                      \
+                  "This assert is used to counter the false positive extra " \
+                  "semi-colon warnings")
+
+
+/**
  * Instantiates a CudaError.
  *
  * @param errcode  The error code returned from a CUDA runtime API routine.
@@ -525,6 +558,20 @@ inline dim<2> get_size(const dim<2>& size) { return size; }
         if (_errcode != HIPFFT_SUCCESS) {         \
             throw GKO_HIPFFT_ERROR(_errcode);     \
         }                                         \
+    } while (false)
+
+
+/**
+ * Asserts that a MPI library call completed without errors.
+ *
+ * @param _mpi_call  a library call expression
+ */
+#define GKO_ASSERT_NO_MPI_ERRORS(_mpi_call) \
+    do {                                    \
+        auto _errcode = _mpi_call;          \
+        if (_errcode != MPI_SUCCESS) {      \
+            throw GKO_MPI_ERROR(_errcode);  \
+        }                                   \
     } while (false)
 
 
