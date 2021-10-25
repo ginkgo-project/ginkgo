01cc0642851d28a4997e7f73d54b9facf07ccd3d
Pratik Nayak <pratikvn@protonmail.com>
2021-10-25 17:26:42 +0200

    Review update, more tests

--- mpi/base/exception.cpp
+++ mpi/base/exception.cpp
@@ -33,6 +33,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include <mpi.h>
 
+#include <array>
 #include <string>
 
 
@@ -44,12 +45,13 @@ namespace gko {
 
 std::string MpiError::get_error(int64 error_code)
 {
-    int len = MPI_MAX_ERROR_STRING;
-    char* error_string = new char[len];
-    MPI_Error_string(error_code, error_string, &len);
-    std::string message = "MPI Error: " + std::string(error_string);
-    delete[] error_string;
+    int len{};
+    std::array<char, MPI_MAX_ERROR_STRING> error_buf;
+    MPI_Error_string(error_code, &error_buf[0], &len);
+    std::string message = "MPI Error: " + std::string(&error_buf[0], len);
+
     return message;
 }
+
 
 }  // namespace gko
