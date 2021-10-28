5876f0637091667375de1a0b4f6313a117689910
Aditya Kashi <aditya.kashi@kit.edu>
2021-10-28 18:36:16 +0200

    Batch richardson reference now only supports one RHS

--- reference/solver/batch_richardson_kernels.cpp
+++ reference/solver/batch_richardson_kernels.cpp
@@ -58,6 +58,19 @@ namespace reference {
 namespace batch_rich {
 
 
+namespace {
+
+
+using gko::kernels::reference::batch_csr::advanced_spmv_kernel;
+using gko::kernels::reference::batch_csr::spmv_kernel;
+
+
+#include "reference/solver/batch_richardson_kernels.hpp.inc"
+
+
+}  // unnamed namespace
+
+
 template <typename T>
 using BatchRichardsonOptions =
     gko::kernels::batch_rich::BatchRichardsonOptions<T>;
@@ -66,10 +79,10 @@ template <typename StopType, typename PrecType, typename LogType,
           typename BatchMatrixType, typename ValueType>
 static void apply_impl(
     std::shared_ptr<const ReferenceExecutor> exec,
-    const BatchRichardsonOptions<remove_complex<ValueType>> &opts,
-    LogType logger, PrecType prec, const BatchMatrixType &a,
-    const gko::batch_dense::UniformBatch<const ValueType> &b,
-    const gko::batch_dense::UniformBatch<ValueType> &x)
+    const BatchRichardsonOptions<remove_complex<ValueType>>& opts,
+    LogType logger, PrecType prec, const BatchMatrixType& a,
+    const gko::batch_dense::UniformBatch<const ValueType>& b,
+    const gko::batch_dense::UniformBatch<ValueType>& x)
 {
     using real_type = typename gko::remove_complex<ValueType>;
     const size_type nbatch = a.num_batch;
@@ -86,110 +99,37 @@ static void apply_impl(
     Array<byte> local_space(exec, local_size_bytes);
 
     for (size_type ibatch = 0; ibatch < nbatch; ibatch++) {
-        byte *const shared_space = local_space.get_data();
-        const auto residual = reinterpret_cast<ValueType *>(shared_space);
-        ValueType *const delta_x = residual + nrows * nrhs;
-        ValueType *const prec_work = delta_x + nrows * nrhs;
-        real_type norms[batch_config<ValueType>::max_num_rhs];
-        real_type init_res_norm[batch_config<ValueType>::max_num_rhs];
-
-        uint32 converged = 0;
-
-        const auto a_b =
-            gko::batch::batch_entry(gko::batch::to_const(a), ibatch);
-        const auto b_b =
-            gko::batch::batch_entry(gko::batch::to_const(b), ibatch);
-        const auto x_b = gko::batch::batch_entry(x, ibatch);
-        const gko::batch_dense::BatchEntry<ValueType> r_b{
-            residual, static_cast<size_type>(nrhs), nrows, nrhs};
-        const gko::batch_dense::BatchEntry<ValueType> dx_b{
-            delta_x, static_cast<size_type>(nrhs), nrows, nrhs};
-
-        const auto relax = static_cast<ValueType>(opts.relax_factor);
-        const gko::batch_dense::BatchEntry<const ValueType> relax_b{&relax, 1,
-                                                                    1, 1};
-        const gko::batch_dense::BatchEntry<real_type> norms_b{
-            norms, static_cast<size_type>(nrhs), 1, nrhs};
-        const gko::batch_dense::BatchEntry<real_type> init_res_norm_b{
-            init_res_norm, static_cast<size_type>(nrhs), 1, nrhs};
-
-        prec.generate(a_b, prec_work);
-
-        // initial residual
-        batch_dense::compute_norm2<ValueType>(gko::batch::to_const(b_b),
-                                              norms_b);
-        batch_dense::copy(gko::batch::to_const(norms_b), init_res_norm_b);
-
-        StopType stop(nrhs, opts.max_its, opts.residual_tol,
-                      init_res_norm_b.values, converged);
-
-        int iter = 0;
-        while (true) {
-            // r <- r - Adx  This causes instability!
-            // adv_spmv_ker(static_cast<ValueType>(-1.0), a_b,
-            //              gko::batch::to_const(dx_b),
-            // static_cast<ValueType>(1.0), r_b);
-
-            // r <- b - Ax
-            batch_dense::copy(b_b, r_b);
-            batch_csr::advanced_spmv_kernel(static_cast<ValueType>(-1.0), a_b,
-                                            gko::batch::to_const(x_b),
-                                            static_cast<ValueType>(1.0), r_b);
-
-            batch_dense::compute_norm2(gko::batch::to_const(r_b), norms_b);
-
-            const bool all_converged = stop.check_converged(
-                iter, norms, {nullptr, 0, 0, 0}, converged);
-            logger.log_iteration(ibatch, iter, norms, converged);
-            if (all_converged) {
-                break;
-            }
-
-            prec.apply(gko::batch::to_const(r_b), dx_b);
-
-            // zero out dx for rhs's which do not need to be updated,
-            //  though this is unnecessary for this solver.
-            for (int j = 0; j < nrhs; j++) {
-                const uint32 conv = converged & (1 << j);
-                if (conv) {
-                    for (int i = 0; i < nrows; i++) {
-                        dx_b.values[i * dx_b.stride + j] = 0.0;
-                    }
-                }
-            }
-
-            batch_dense::add_scaled(relax_b, gko::batch::to_const(dx_b), x_b);
-            iter++;
-        }
+        batch_entry_richardson_impl<StopType>(opts, logger, prec, a, b, x,
+                                              ibatch, local_space);
     }
 }
 
 template <typename BatchType, typename LoggerType, typename ValueType>
 void apply_select_prec(
     std::shared_ptr<const ReferenceExecutor> exec,
-    const BatchRichardsonOptions<remove_complex<ValueType>> &opts,
-    const LoggerType logger, const BatchType &a,
-    const gko::batch_dense::UniformBatch<const ValueType> &b,
-    const gko::batch_dense::UniformBatch<ValueType> &x)
+    const BatchRichardsonOptions<remove_complex<ValueType>>& opts,
+    const LoggerType logger, const BatchType& a,
+    const gko::batch_dense::UniformBatch<const ValueType>& b,
+    const gko::batch_dense::UniformBatch<ValueType>& x)
 {
     if (opts.preconditioner == gko::preconditioner::batch::type::none) {
         BatchIdentity<ValueType> prec;
         if (opts.tol_type == gko::stop::batch::ToleranceType::absolute) {
-            apply_impl<stop::AbsResidualMaxIter<ValueType>>(exec, opts, logger,
-                                                            prec, a, b, x);
+            apply_impl<stop::SimpleAbsResidual<ValueType>>(exec, opts, logger,
+                                                           prec, a, b, x);
         } else {
-            apply_impl<stop::RelResidualMaxIter<ValueType>>(exec, opts, logger,
-                                                            prec, a, b, x);
+            apply_impl<stop::SimpleRelResidual<ValueType>>(exec, opts, logger,
+                                                           prec, a, b, x);
         }
     } else if (opts.preconditioner ==
                gko::preconditioner::batch::type::jacobi) {
         BatchJacobi<ValueType> prec;
         if (opts.tol_type == gko::stop::batch::ToleranceType::absolute) {
-            apply_impl<stop::AbsResidualMaxIter<ValueType>>(exec, opts, logger,
-                                                            prec, a, b, x);
+            apply_impl<stop::SimpleAbsResidual<ValueType>>(exec, opts, logger,
+                                                           prec, a, b, x);
         } else {
-            apply_impl<stop::RelResidualMaxIter<ValueType>>(exec, opts, logger,
-                                                            prec, a, b, x);
+            apply_impl<stop::SimpleRelResidual<ValueType>>(exec, opts, logger,
+                                                           prec, a, b, x);
         }
     } else {
         GKO_NOT_IMPLEMENTED;
@@ -198,19 +138,18 @@ void apply_select_prec(
 
 template <typename ValueType>
 void apply(std::shared_ptr<const ReferenceExecutor> exec,
-           const BatchRichardsonOptions<remove_complex<ValueType>> &opts,
-           const BatchLinOp *const a,
-           const matrix::BatchDense<ValueType> *const b,
-           matrix::BatchDense<ValueType> *const x,
-           gko::log::BatchLogData<ValueType> &logdata)
+           const BatchRichardsonOptions<remove_complex<ValueType>>& opts,
+           const BatchLinOp* const a,
+           const matrix::BatchDense<ValueType>* const b,
+           matrix::BatchDense<ValueType>* const x,
+           gko::log::BatchLogData<ValueType>& logdata)
 {
-    batch_log::FinalLogger<remove_complex<ValueType>> logger(
-        static_cast<int>(b->get_size().at(0)[1]), opts.max_its,
+    batch_log::SimpleFinalLogger<remove_complex<ValueType>> logger(
         logdata.res_norms->get_values(), logdata.iter_counts.get_data());
 
     const gko::batch_dense::UniformBatch<ValueType> x_b =
         host::get_batch_struct(x);
-    if (auto a_mat = dynamic_cast<const matrix::BatchCsr<ValueType> *>(a)) {
+    if (auto a_mat = dynamic_cast<const matrix::BatchCsr<ValueType>*>(a)) {
         const auto a_b = host::get_batch_struct(a_mat);
         const auto b_b = host::get_batch_struct(b);
         apply_select_prec(exec, opts, logger, a_b, b_b, x_b);
