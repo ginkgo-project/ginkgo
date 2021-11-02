045bed3867663bfa26c95d4c737a9f4cbd0b0260
Aditya Kashi <aditya.kashi@kit.edu>
2021-11-02 14:50:35 +0100

    removed multi-RHS logger and stopping criteria

--- reference/test/log/batch_logger.cpp
+++ reference/test/log/batch_logger.cpp
@@ -52,15 +52,14 @@ protected:
     using value_type = T;
     using real_type = gko::remove_complex<value_type>;
     using BDense = gko::matrix::BatchDense<value_type>;
-    using BatchLog = gko::kernels::reference::batch_log::FinalLogger<real_type>;
+    using BatchLog =
+        gko::kernels::reference::batch_log::SimpleFinalLogger<real_type>;
 
     BatchFinalLogger() : exec(gko::ReferenceExecutor::create()) {}
 
     std::shared_ptr<const gko::ReferenceExecutor> exec;
     const int nrows = 100;
-    const int nrhs = 4;
     const size_t nbatch = 3;
-    static constexpr int max_nrhs = 6;
 };
 
 TYPED_TEST_SUITE(BatchFinalLogger, gko::test::ValueTypes);
@@ -70,281 +69,25 @@ TYPED_TEST(BatchFinalLogger, LogsOneRhsConvergedOneIteration)
 {
     using real_type = typename TestFixture::real_type;
     using BatchLog = typename TestFixture::BatchLog;
-    constexpr int max_nrhs = TestFixture::max_nrhs;
-    gko::Array<real_type> res_norms_log(this->exec, this->nbatch * this->nrhs);
-    gko::Array<int> iters_log(this->exec, this->nbatch * this->nrhs);
-    for (int i = 0; i < this->nbatch * this->nrhs; i++) {
+    gko::Array<real_type> res_norms_log(this->exec, this->nbatch);
+    gko::Array<int> iters_log(this->exec, this->nbatch);
+    for (int i = 0; i < this->nbatch; i++) {
         res_norms_log.get_data()[i] = 0.0;
         iters_log.get_data()[i] = -1;
     }
     const int maxits = 10;
     const int iter = 5;
-    BatchLog blog_h(this->nrhs, maxits, res_norms_log.get_data(),
-                    iters_log.get_data());
+    BatchLog blog_h(res_norms_log.get_data(), iters_log.get_data());
     for (size_t ib = 0; ib < this->nbatch; ib++) {
         BatchLog blog = blog_h;
-        real_type resnv[max_nrhs];
-        for (int i = 0; i < this->nrhs; i++) {
-            resnv[i] = i + 1.0;
-        }
-        // Initially, no RHS has converged.
-        const uint32_t converged = 0xfffffff4;  //< suppose 3rd RHS converged
-        blog.log_iteration(ib, iter, resnv, converged);
+        real_type resnv = 1.0;
+        blog.log_iteration(ib, iter, resnv);
     }
 
     for (size_t i = 0; i < this->nbatch; i++) {
-        for (int j = 0; j < this->nrhs; j++) {
-            ASSERT_EQ(res_norms_log.get_const_data()[i * this->nrhs + j],
-                      j + 1.0);
-            if (j == 2) {
-                ASSERT_EQ(iters_log.get_const_data()[i * this->nrhs + j], iter);
-            } else {
-                ASSERT_EQ(iters_log.get_const_data()[i * this->nrhs + j], -1);
-            }
-        }
+        ASSERT_EQ(res_norms_log.get_const_data()[i], 1.0);
+        ASSERT_EQ(iters_log.get_const_data()[i], iter);
     }
-}
-
-TYPED_TEST(BatchFinalLogger, LogsNothingWhenNothingChanges)
-{
-    using real_type = typename TestFixture::real_type;
-    using BatchLog = typename TestFixture::BatchLog;
-    constexpr int max_nrhs = TestFixture::max_nrhs;
-    gko::Array<real_type> res_norms_log(this->exec, this->nbatch * this->nrhs);
-    gko::Array<int> iters_log(this->exec, this->nbatch * this->nrhs);
-    for (int i = 0; i < this->nbatch * this->nrhs; i++) {
-        res_norms_log.get_data()[i] = 0.0;
-        iters_log.get_data()[i] = -1;
-    }
-    const int maxits = 10;
-    const int iter = 5;
-    BatchLog blog_h(this->nrhs, maxits, res_norms_log.get_data(),
-                    iters_log.get_data());
-    for (size_t ib = 0; ib < this->nbatch; ib++) {
-        BatchLog blog = blog_h;
-        real_type resnv[max_nrhs];
-        for (int i = 0; i < this->nrhs; i++) {
-            resnv[i] = i + 1.0;
-        }
-        // Initially, no RHS has converged.
-        const uint32_t converged = 0xfffffff0;
-        blog.log_iteration(ib, iter, resnv, converged);
-    }
-
-    for (size_t i = 0; i < this->nbatch; i++) {
-        for (int j = 0; j < this->nrhs; j++) {
-            ASSERT_EQ(res_norms_log.get_const_data()[i * this->nrhs + j], 0.0);
-            ASSERT_EQ(iters_log.get_const_data()[i * this->nrhs + j], -1);
-        }
-    }
-}
-
-TYPED_TEST(BatchFinalLogger, LogsTwoRhsConvergedOneIteration)
-{
-    using real_type = typename TestFixture::real_type;
-    using BatchLog = typename TestFixture::BatchLog;
-    constexpr int max_nrhs = TestFixture::max_nrhs;
-    gko::Array<real_type> res_norms_log(this->exec, this->nbatch * this->nrhs);
-    gko::Array<int> iters_log(this->exec, this->nbatch * this->nrhs);
-    for (int i = 0; i < this->nbatch * this->nrhs; i++) {
-        res_norms_log.get_data()[i] = 0.0;
-        iters_log.get_data()[i] = -1;
-    }
-    const int maxits = 10;
-    const int iter = 5;
-    BatchLog blog_h(this->nrhs, maxits, res_norms_log.get_data(),
-                    iters_log.get_data());
-    for (size_t ib = 0; ib < this->nbatch; ib++) {
-        BatchLog blog = blog_h;
-        real_type resnv[max_nrhs];
-        for (int i = 0; i < this->nrhs; i++) {
-            resnv[i] = i + 1.0;
-        }
-        // Initially, no RHS has converged.
-        // suppose 2rd and 4th RHS converged
-        const uint32_t converged = 0xfffffffa;
-        blog.log_iteration(ib, iter, resnv, converged);
-    }
-
-    for (size_t i = 0; i < this->nbatch; i++) {
-        for (int j = 0; j < this->nrhs; j++) {
-            ASSERT_EQ(res_norms_log.get_const_data()[i * this->nrhs + j],
-                      j + 1.0);
-            if (j == 1 || j == 3) {
-                ASSERT_EQ(iters_log.get_const_data()[i * this->nrhs + j], iter);
-            } else {
-                ASSERT_EQ(iters_log.get_const_data()[i * this->nrhs + j], -1);
-            }
-        }
-    }
-}
-
-TYPED_TEST(BatchFinalLogger, LogsLastIterationCorrectly)
-{
-    using real_type = typename TestFixture::real_type;
-    using BatchLog = typename TestFixture::BatchLog;
-    constexpr int max_nrhs = TestFixture::max_nrhs;
-    gko::Array<real_type> res_norms_log(this->exec, this->nbatch * this->nrhs);
-    gko::Array<int> iters_log(this->exec, this->nbatch * this->nrhs);
-    for (int i = 0; i < this->nbatch * this->nrhs; i++) {
-        res_norms_log.get_data()[i] = 0.0;
-        iters_log.get_data()[i] = -1;
-    }
-    const int maxits = 10;
-    BatchLog blog_h(this->nrhs, maxits, res_norms_log.get_data(),
-                    iters_log.get_data());
-    for (size_t ib = 0; ib < this->nbatch; ib++) {
-        BatchLog blog = blog_h;
-        real_type resnv[max_nrhs];
-        for (int i = 0; i < this->nrhs; i++) {
-            resnv[i] = i + 1.0;
-        }
-        // no RHS has converged.
-        int iter = 7;
-        uint32_t converged = 0xfffffff2;
-        blog.log_iteration(ib, iter, resnv, converged);
-        iter = 9;
-        for (int i = 0; i < this->nrhs; i++) {
-            resnv[i] = i + 10.0;
-        }
-        blog.log_iteration(ib, iter, resnv, converged);
-    }
-
-    for (size_t i = 0; i < this->nbatch; i++) {
-        for (int j = 0; j < this->nrhs; j++) {
-            ASSERT_EQ(res_norms_log.get_const_data()[i * this->nrhs + j],
-                      j + 10.0);
-            if (j == 1) {
-                ASSERT_EQ(iters_log.get_const_data()[i * this->nrhs + j], 7);
-            } else {
-                ASSERT_EQ(iters_log.get_const_data()[i * this->nrhs + j], -1);
-            }
-        }
-    }
-}
-
-TYPED_TEST(BatchFinalLogger, LogsConvergenceTwoIterations)
-{
-    using real_type = typename TestFixture::real_type;
-    using BatchLog = typename TestFixture::BatchLog;
-    constexpr int max_nrhs = TestFixture::max_nrhs;
-    gko::Array<real_type> res_norms_log(this->exec, this->nbatch * this->nrhs);
-    gko::Array<int> iters_log(this->exec, this->nbatch * this->nrhs);
-    for (int i = 0; i < this->nbatch * this->nrhs; i++) {
-        res_norms_log.get_data()[i] = 0.0;
-        iters_log.get_data()[i] = -1;
-    }
-    const int maxits = 20;
-    BatchLog blog_h(this->nrhs, maxits, res_norms_log.get_data(),
-                    iters_log.get_data());
-    for (size_t ib = 0; ib < this->nbatch; ib++) {
-        BatchLog blog = blog_h;
-        int iter = 5;
-        real_type resnv[max_nrhs];
-        for (int i = 0; i < this->nrhs; i++) {
-            resnv[i] = i + 1.0;
-        }
-
-        // First suppose 1st RHS has converged
-        uint32_t converged = 0xfffffff1;
-        blog.log_iteration(ib, iter, resnv, converged);
-        for (int i = 0; i < this->nrhs; i++) {
-            resnv[i] = i + ib + 10.0;
-        }
-
-        // Then other RHS converged for different small systems
-        if (ib == 0) {
-            iter = 8;
-            converged = 0xfffffff5;
-            blog.log_iteration(ib, iter, resnv, converged);
-        } else {
-            iter = 10;
-            converged = 0xfffffff9;
-            blog.log_iteration(ib, iter, resnv, converged);
-        }
-    }
-
-    // the latest residual norms are logged
-    for (size_t i = 0; i < this->nbatch; i++) {
-        for (int j = 0; j < this->nrhs; j++) {
-            ASSERT_EQ(res_norms_log.get_const_data()[i * this->nrhs + j],
-                      i + j + 10.0);
-        }
-    }
-    // The iterations at which the convergence of each RHS were flagged
-    //  are logged.
-    ASSERT_EQ(iters_log.get_const_data()[0 * this->nrhs + 0], 5);
-    ASSERT_EQ(iters_log.get_const_data()[1 * this->nrhs + 0], 5);
-    ASSERT_EQ(iters_log.get_const_data()[0 * this->nrhs + 1], -1);
-    ASSERT_EQ(iters_log.get_const_data()[1 * this->nrhs + 1], -1);
-    ASSERT_EQ(iters_log.get_const_data()[0 * this->nrhs + 2], 8);
-    ASSERT_EQ(iters_log.get_const_data()[1 * this->nrhs + 2], -1);
-    ASSERT_EQ(iters_log.get_const_data()[0 * this->nrhs + 3], -1);
-    ASSERT_EQ(iters_log.get_const_data()[1 * this->nrhs + 3], 10);
-}
-
-TYPED_TEST(BatchFinalLogger, FirstIterationResetsLogger)
-{
-    using real_type = typename TestFixture::real_type;
-    using BatchLog = typename TestFixture::BatchLog;
-    constexpr int max_nrhs = TestFixture::max_nrhs;
-    gko::Array<real_type> res_norms_log(this->exec, this->nbatch * this->nrhs);
-    gko::Array<int> iters_log(this->exec, this->nbatch * this->nrhs);
-    for (int i = 0; i < this->nbatch * this->nrhs; i++) {
-        res_norms_log.get_data()[i] = 0.0;
-        iters_log.get_data()[i] = -1;
-    }
-    const int maxits = 20;
-    BatchLog blog_h(this->nrhs, maxits, res_norms_log.get_data(),
-                    iters_log.get_data());
-    for (size_t ib = 0; ib < this->nbatch; ib++) {
-        real_type resnv[max_nrhs];
-        for (int i = 0; i < this->nrhs; i++) {
-            resnv[i] = i + 1.0;
-        }
-
-        // First suppose 1st RHS has converged
-        int iter = 5;
-        uint32_t converged = 0xfffffff1;
-        blog_h.log_iteration(ib, iter, resnv, converged);
-
-        // suppose we move to the next linear system and
-        // run the first iteration when nothing has converged
-        blog_h.log_iteration(ib, 0, resnv, 0 - (1 << this->nrhs));
-
-        for (int i = 0; i < this->nrhs; i++) {
-            resnv[i] = i + ib + 10.0;
-        }
-
-        // Then other RHS converged for different small systems
-        if (ib == 0) {
-            iter = 8;
-            converged = 0xfffffff5;
-            blog_h.log_iteration(ib, iter, resnv, converged);
-        } else {
-            iter = 10;
-            converged = 0xfffffff9;
-            blog_h.log_iteration(ib, iter, resnv, converged);
-        }
-    }
-
-    // the latest residual norms are logged
-    for (size_t i = 0; i < this->nbatch; i++) {
-        for (int j = 0; j < this->nrhs; j++) {
-            ASSERT_EQ(res_norms_log.get_const_data()[i * this->nrhs + j],
-                      i + j + 10.0);
-        }
-    }
-    // The iterations at which the convergence of each RHS were flagged
-    //  are logged.
-    ASSERT_EQ(iters_log.get_const_data()[0 * this->nrhs + 0], 8);
-    ASSERT_EQ(iters_log.get_const_data()[1 * this->nrhs + 0], 10);
-    ASSERT_EQ(iters_log.get_const_data()[0 * this->nrhs + 1], maxits - 1);
-    ASSERT_EQ(iters_log.get_const_data()[1 * this->nrhs + 1], maxits - 1);
-    ASSERT_EQ(iters_log.get_const_data()[0 * this->nrhs + 2], 8);
-    ASSERT_EQ(iters_log.get_const_data()[1 * this->nrhs + 2], maxits - 1);
-    ASSERT_EQ(iters_log.get_const_data()[0 * this->nrhs + 3], maxits - 1);
-    ASSERT_EQ(iters_log.get_const_data()[1 * this->nrhs + 3], 10);
 }
 
 
