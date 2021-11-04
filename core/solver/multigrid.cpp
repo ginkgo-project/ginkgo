58cd94192f6120c1047468d3d1adf4cd7e0d9f07
Yuhsiang M. Tsai <yhmtsai@gmail.com>
2021-11-04 12:38:55 +0800

    Comment nvtx, use zero_guess parameter

--- core/solver/multigrid.cpp
+++ core/solver/multigrid.cpp
@@ -52,7 +52,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "core/components/fill_array_kernels.hpp"
 #include "core/solver/ir_kernels.hpp"
 #include "core/solver/multigrid_kernels.hpp"
-#include "nvToolsExt.h"
+// #include "nvToolsExt.h"
 
 namespace gko {
 namespace solver {
@@ -583,7 +583,7 @@ void Multigrid::generate()
 
 void Multigrid::apply_impl(const LinOp* b, LinOp* x) const
 {
-    nvtxRangePushA("Multigrid apply");
+    // nvtxRangePushA("Multigrid apply");
     auto lambda = [this](auto mg_level, auto b, auto x) {
         using value_type = typename std::decay_t<
             detail::pointee<decltype(mg_level)>>::value_type;
@@ -636,7 +636,7 @@ void Multigrid::apply_impl(const LinOp* b, LinOp* x) const
             }
 
             state.run_cycle(this->get_parameters().cycle, 0, system_matrix_, b,
-                            x);
+                            x, parameters_.zero_guess);
             // r->copy_from(b);
             // system_matrix_->apply(lend(neg_one_op), x, lend(one_op),
             // r.get());
@@ -648,7 +648,7 @@ void Multigrid::apply_impl(const LinOp* b, LinOp* x) const
     run<gko::multigrid::EnableMultigridLevel, float, double,
         std::complex<float>, std::complex<double>>(first_mg_level, lambda, b,
                                                    x);
-    nvtxRangePop();
+    // nvtxRangePop();
 }
 
 
