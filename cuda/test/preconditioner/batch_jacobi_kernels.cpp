b6a93a9ace15ac85277221383e06bb1ef97e4eff
Aditya Kashi <aditya.kashi@kit.edu>
2021-11-05 16:34:04 +0100

    removed unused code in cuda_hip batch preconditioners

--- cuda/test/preconditioner/batch_jacobi_kernels.cpp
+++ cuda/test/preconditioner/batch_jacobi_kernels.cpp
@@ -81,9 +81,9 @@ protected:
           cu_mtx(Mtx::create(cuexec))
     {
         // make diagonal larger
-        const int *const row_ptrs = ref_mtx->get_const_row_ptrs();
-        const int *const col_idxs = ref_mtx->get_const_col_idxs();
-        value_type *const vals = ref_mtx->get_values();
+        const int* const row_ptrs = ref_mtx->get_const_row_ptrs();
+        const int* const col_idxs = ref_mtx->get_const_col_idxs();
+        value_type* const vals = ref_mtx->get_values();
         const int nnz = row_ptrs[nrows];
         for (int irow = 0; irow < nrows; irow++) {
             for (int iz = row_ptrs[irow]; iz < row_ptrs[irow + 1]; iz++) {
@@ -145,12 +145,6 @@ TYPED_TEST_SUITE(BatchJacobi, gko::test::ValueTypes);
 TYPED_TEST(BatchJacobi, ApplySingleIsEquivalentToReference)
 {
     this->check_jacobi(1);
-}
-
-
-TYPED_TEST(BatchJacobi, ApplyMultipleIsEquivalentToReference)
-{
-    this->check_jacobi(2);
 }
 
 
