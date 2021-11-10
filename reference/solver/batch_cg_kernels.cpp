1068af5c1e609bd8bacfdc787417342cef97ca58
Aditya Kashi <aditya.kashi@kit.edu>
2021-11-10 17:50:43 +0100

    Batched dispatcher now stores the kernel caller by value, and ref/omp use std vector instead of Array
    Clang-tidy flags null pointer dereference issues when gko Array is used for allocating the
    'dynamic shared memory' for batched solvers in reference/OpenMP. This is not the case when
    std vectors are used instead.

--- reference/solver/batch_cg_kernels.cpp
+++ reference/solver/batch_cg_kernels.cpp
@@ -88,14 +88,13 @@ public:
             gko::kernels::batch_cg::local_memory_requirement<ValueType>(nrows,
                                                                         nrhs) +
             PrecType::dynamic_work_size(nrows, a.num_nnz) * sizeof(ValueType);
-        using byte = unsigned char;
-
-        Array<byte> local_space(exec_, local_size_bytes);
+        // Array<unsigned char> local_space(exec_, local_size_bytes);
+        std::vector<unsigned char> local_space(local_size_bytes);
 
         for (size_type ibatch = 0; ibatch < nbatch; ibatch++) {
             batch_entry_cg_impl<StopType, PrecType, LogType, BatchMatrixType,
                                 ValueType>(opts_, logger, PrecType(), a, b, x,
-                                           ibatch, local_space.get_data());
+                                           ibatch, local_space.data());
         }
     }
 
