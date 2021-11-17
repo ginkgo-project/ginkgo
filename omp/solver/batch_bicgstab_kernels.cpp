723ba9e903ac25edd3c2635e5021887762cc6d32
Aditya Kashi <aditya.kashi@kit.edu>
2021-11-17 12:55:30 -0500

    batch solvers now compile with batch dense as matrix type

--- omp/solver/batch_bicgstab_kernels.cpp
+++ omp/solver/batch_bicgstab_kernels.cpp
@@ -35,7 +35,6 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include "core/solver/batch_dispatch.hpp"
 #include "omp/base/config.hpp"
-#include "reference/matrix/batch_dense_kernels.hpp"
 
 
 namespace gko {
@@ -49,10 +48,10 @@ namespace omp {
 namespace batch_bicgstab {
 
 
-namespace batch_dense = gko::kernels::reference::batch_dense;
 constexpr int max_num_rhs = 1;
 
 #include "reference/matrix/batch_csr_kernels.hpp.inc"
+#include "reference/matrix/batch_dense_kernels.hpp.inc"
 #include "reference/matrix/batch_ell_kernels.hpp.inc"
 #include "reference/solver/batch_bicgstab_kernels.hpp.inc"
 
