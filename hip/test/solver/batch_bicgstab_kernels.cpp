85f359fbde4e0d688c8cb15d0ed3afef25d0f9a3
Aditya Kashi <aditya.kashi@kit.edu>
2021-11-22 18:06:16 +0100

    batch bicgstab is now tested on all eligible matrix formats

--- hip/test/solver/batch_bicgstab_kernels.cpp
+++ hip/test/solver/batch_bicgstab_kernels.cpp
@@ -262,4 +262,62 @@ TEST(BatchBicgstab, SolvesLargeSystemEquivalentToReference)
         d_exec, r_sys, r_factory.get(), iter_tol, res_tol, sol_tol);
 }
 
+
+TEST(BatchBicgstab, SolvesLargeEllSystemEquivalentToReference)
+{
+    using value_type = double;
+    using real_type = double;
+    using mtx_type = gko::matrix::BatchEll<value_type, int>;
+    using solver_type = gko::solver::BatchBicgstab<value_type>;
+    std::shared_ptr<gko::ReferenceExecutor> refexec =
+        gko::ReferenceExecutor::create();
+    std::shared_ptr<const gko::HipExecutor> d_exec =
+        gko::HipExecutor::create(0, refexec);
+    const float solver_restol = 1e-4;
+    auto r_sys = gko::test::generate_solvable_batch_system<mtx_type>(
+        refexec, 2, 95, 1, false);
+    auto r_factory =
+        solver_type::build()
+            .with_max_iterations(500)
+            .with_residual_tol(solver_restol)
+            .with_tolerance_type(gko::stop::batch::ToleranceType::relative)
+            .with_preconditioner(gko::preconditioner::batch::type::jacobi)
+            .on(refexec);
+    const double iter_tol = 0.01;
+    const double res_tol = 1e-9;
+    const double sol_tol = 10 * solver_restol;
+
+    gko::test::compare_with_reference<value_type, solver_type>(
+        d_exec, r_sys, r_factory.get(), iter_tol, res_tol, sol_tol);
+}
+
+
+TEST(BatchBicgstab, SolvesLargeDenseSystemEquivalentToReference)
+{
+    using value_type = double;
+    using real_type = double;
+    using mtx_type = gko::matrix::BatchDense<value_type>;
+    using solver_type = gko::solver::BatchBicgstab<value_type>;
+    std::shared_ptr<gko::ReferenceExecutor> refexec =
+        gko::ReferenceExecutor::create();
+    std::shared_ptr<const gko::HipExecutor> d_exec =
+        gko::HipExecutor::create(0, refexec);
+    const float solver_restol = 1e-4;
+    auto r_sys = gko::test::generate_solvable_batch_system<mtx_type>(
+        refexec, 2, 37, 1, false);
+    auto r_factory =
+        solver_type::build()
+            .with_max_iterations(500)
+            .with_residual_tol(solver_restol)
+            .with_tolerance_type(gko::stop::batch::ToleranceType::relative)
+            .with_preconditioner(gko::preconditioner::batch::type::jacobi)
+            .on(refexec);
+    const double iter_tol = 0.01;
+    const double res_tol = 1e-9;
+    const double sol_tol = 10 * solver_restol;
+
+    gko::test::compare_with_reference<value_type, solver_type>(
+        d_exec, r_sys, r_factory.get(), iter_tol, res_tol, sol_tol);
+}
+
 }  // namespace
