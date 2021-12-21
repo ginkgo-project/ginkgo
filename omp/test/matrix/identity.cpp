689d17aa33aff5b2b6d662292373ba2ff92566ed
Aditya Kashi <aditya.kashi@kit.edu>
2021-12-21 16:43:50 +0100

    review comments on tolerances
    Co-authored-by: Pratik Nayak <pratikvn@protonmail.com>

--- omp/test/matrix/identity.cpp
+++ omp/test/matrix/identity.cpp
@@ -126,7 +126,7 @@ TEST_F(Identity, ScaleDenseAddIdentity)
     id->apply(alpha.get(), id.get(), beta.get(), x.get());
     did->apply(dalpha.get(), did.get(), dbeta.get(), dx.get());
 
-    GKO_ASSERT_MTX_NEAR(x, dx, std::numeric_limits<vtype>::epsilon());
+    GKO_ASSERT_MTX_NEAR(x, dx, r<vtype>::value);
 }
 
 
@@ -140,7 +140,7 @@ TEST_F(Identity, ScaleCsrAddIdentity)
     id->apply(alpha.get(), id.get(), beta.get(), mtx.get());
     did->apply(dalpha.get(), did.get(), dbeta.get(), dmtx.get());
 
-    GKO_ASSERT_MTX_NEAR(mtx, dmtx, std::numeric_limits<vtype>::epsilon());
+    GKO_ASSERT_MTX_NEAR(mtx, dmtx, r<vtype>::value);
 }
 
 
