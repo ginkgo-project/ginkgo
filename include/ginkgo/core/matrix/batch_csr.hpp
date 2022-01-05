a16de4f496a53c04e6b22a9026309853f9ad0480
Aditya Kashi <aditya.kashi@kit.edu>
2022-01-05 13:39:18 +0100

    batch matrix formats no longer derive from EnableBatchScaling

--- include/ginkgo/core/matrix/batch_csr.hpp
+++ include/ginkgo/core/matrix/batch_csr.hpp
@@ -74,8 +74,7 @@ class BatchCsr
       public ConvertibleTo<BatchDense<ValueType>>,
       public BatchReadableFromMatrixData<ValueType, IndexType>,
       public BatchWritableToMatrixData<ValueType, IndexType>,
-      public BatchTransposable,
-      public EnableBatchScaling {
+      public BatchTransposable {
     friend class EnableCreateMethod<BatchCsr>;
     friend class EnablePolymorphicObject<BatchCsr, BatchLinOp>;
     friend class BatchCsr<to_complex<ValueType>, IndexType>;
@@ -408,9 +407,6 @@ private:
     Array<value_type> values_;
     Array<index_type> col_idxs_;
     Array<index_type> row_ptrs_;
-
-    void batch_scale_impl(const BatchLinOp* left_scale,
-                          const BatchLinOp* right_scale) override;
 };
 
 
