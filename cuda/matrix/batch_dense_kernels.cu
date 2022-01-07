cd4d9c86ec7ab14e7ca556fe67aca18da41da706
Aditya Kashi <aditya.kashi@kit.edu>
2022-01-07 17:26:39 +0100

    fixes after rebase onto develop

--- cuda/matrix/batch_dense_kernels.cu
+++ cuda/matrix/batch_dense_kernels.cu
@@ -37,7 +37,6 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <ginkgo/core/base/range_accessors.hpp>
 
 
-#include "core/components/prefix_sum.hpp"
 #include "core/matrix/batch_struct.hpp"
 #include "cuda/base/config.hpp"
 #include "cuda/base/cublas_bindings.hpp"
