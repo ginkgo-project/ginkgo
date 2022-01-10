6c150fda88fdc2d8e8dba4d452b652811d4db533
Aditya Kashi <aditya.kashi@kit.edu>
2022-01-10 18:02:08 +0100

    fixes after rebase

--- reference/test/matrix/batch_diagonal_kernels.cpp
+++ reference/test/matrix/batch_diagonal_kernels.cpp
@@ -46,7 +46,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <ginkgo/core/matrix/diagonal.hpp>
 
 
-#include "core/components/fill_array.hpp"
+#include "core/components/fill_array_kernels.hpp"
 #include "core/matrix/batch_diagonal_kernels.hpp"
 #include "core/test/utils.hpp"
 
