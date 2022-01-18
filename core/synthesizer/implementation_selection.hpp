999bccd44e7e01cbc33d27e4433e770010f23aa2
Yuhsiang M. Tsai <yhmtsai@gmail.com>
2022-01-18 17:57:26 +0800

    two level draft

--- core/synthesizer/implementation_selection.hpp
+++ core/synthesizer/implementation_selection.hpp
@@ -106,6 +106,91 @@ namespace syn {
     }
 
 
+#define GKO_ENABLE_IMPLEMENTATION_TWO_SELECTION_KERNEL(_name, _callable)      \
+    template <typename Predicate, bool... BoolArgs, int... IntArgs,           \
+              gko::size_type... SizeTArgs, typename... TArgs,                 \
+              std::uint32_t cfg, typename... InferredArgs>                    \
+    inline void _name(::gko::syn::value_list<int>, Predicate,                 \
+                      ::gko::syn::value_list<std::uint32_t, cfg>,             \
+                      ::gko::syn::value_list<bool, BoolArgs...>,              \
+                      ::gko::syn::value_list<int, IntArgs...>,                \
+                      ::gko::syn::value_list<gko::size_type, SizeTArgs...>,   \
+                      ::gko::syn::type_list<TArgs...>, InferredArgs...)       \
+        GKO_KERNEL_NOT_FOUND;                                                 \
+                                                                              \
+    template <int K, int... Rest, typename Predicate, bool... BoolArgs,       \
+              int... IntArgs, gko::size_type... SizeTArgs, typename... TArgs, \
+              std::uint32_t cfg, typename... InferredArgs>                    \
+    inline void _name(                                                        \
+        ::gko::syn::value_list<int, K, Rest...>, Predicate is_eligible,       \
+        ::gko::syn::value_list<std::uint32_t, cfg> device_args,               \
+        ::gko::syn::value_list<bool, BoolArgs...> bool_args,                  \
+        ::gko::syn::value_list<int, IntArgs...> int_args,                     \
+        ::gko::syn::value_list<gko::size_type, SizeTArgs...> size_args,       \
+        ::gko::syn::type_list<TArgs...> type_args, InferredArgs... args)      \
+    {                                                                         \
+        if (is_eligible(K)) {                                                 \
+            _callable<BoolArgs..., IntArgs..., SizeTArgs..., TArgs..., K,     \
+                      cfg>(::gko::syn::value_list<int, K>(),                  \
+                           std::forward<InferredArgs>(args)...);              \
+        } else {                                                              \
+            _name(::gko::syn::value_list<int, Rest...>(), is_eligible,        \
+                  device_args, bool_args, int_args, size_args, type_args,     \
+                  std::forward<InferredArgs>(args)...);                       \
+        }                                                                     \
+    }                                                                         \
+    static_assert(true,                                                       \
+                  "This assert is used to counter the false positive extra "  \
+                  "semi-colon warnings")
+
+#define GKO_ENABLE_IMPLEMENTATION_TWO_SELECTION_CONFIG(_name, _callable)       \
+    template <typename Predicate, int... KernelArgs, typename KernelPredicate, \
+              bool... BoolArgs, int... IntArgs, gko::size_type... SizeTArgs,   \
+              typename... TArgs, typename... InferredArgs>                     \
+    inline void _name(::gko::syn::value_list<std::uint32_t>, Predicate,        \
+                      ::gko::syn::value_list<int, KernelArgs...>,              \
+                      KernelPredicate,                                         \
+                      ::gko::syn::value_list<bool, BoolArgs...>,               \
+                      ::gko::syn::value_list<int, IntArgs...>,                 \
+                      ::gko::syn::value_list<gko::size_type, SizeTArgs...>,    \
+                      ::gko::syn::type_list<TArgs...>, InferredArgs...)        \
+        GKO_KERNEL_NOT_FOUND;                                                  \
+                                                                               \
+    template <std::uint32_t K, std::uint32_t... Rest, typename Predicate,      \
+              int... KernelArgs, typename KernelPredicate, bool... BoolArgs,   \
+              int... IntArgs, gko::size_type... SizeTArgs, typename... TArgs,  \
+              typename... InferredArgs>                                        \
+    inline void _name(                                                         \
+        ::gko::syn::value_list<std::uint32_t, K, Rest...>,                     \
+        Predicate is_eligible,                                                 \
+        ::gko::syn::value_list<int, KernelArgs...> kernel_args,                \
+        KernelPredicate kernel_is_eligible,                                    \
+        ::gko::syn::value_list<bool, BoolArgs...> bool_args,                   \
+        ::gko::syn::value_list<int, IntArgs...> int_args,                      \
+        ::gko::syn::value_list<gko::size_type, SizeTArgs...> size_args,        \
+        ::gko::syn::type_list<TArgs...> type_args, InferredArgs... args)       \
+    {                                                                          \
+        if (is_eligible(K)) {                                                  \
+            _callable(kernel_args, kernel_is_eligible,                         \
+                      ::gko::syn::value_list<std::uint32_t, K>(), bool_args,   \
+                      int_args, size_args, type_args,                          \
+                      std::forward<InferredArgs>(args)...);                    \
+        } else {                                                               \
+            _name(::gko::syn::value_list<std::uint32_t, Rest...>(),            \
+                  is_eligible, kernel_args, kernel_is_eligible, bool_args,     \
+                  int_args, size_args, type_args,                              \
+                  std::forward<InferredArgs>(args)...);                        \
+        }                                                                      \
+    }                                                                          \
+    static_assert(true,                                                        \
+                  "This assert is used to counter the false positive extra "   \
+                  "semi-colon warnings")
+
+#define GKO_ENABLE_IMPLEMENTATION_TWO_SELECTION(_name, _callable)              \
+    GKO_ENABLE_IMPLEMENTATION_TWO_SELECTION_KERNEL(_name##_kernel, _callable); \
+    GKO_ENABLE_IMPLEMENTATION_TWO_SELECTION_CONFIG(_name, _name##_kernel)
+
+
 }  // namespace syn
 }  // namespace gko
 
