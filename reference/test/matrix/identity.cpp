b4a1fd19425b1c8f6948969c3b9bf25d39d76847
Aditya Kashi <aditya.kashi@kit.edu>
2022-01-21 11:55:59 +0100

    removed unnecessary includes and completed omp tests

--- reference/test/matrix/identity.cpp
+++ reference/test/matrix/identity.cpp
@@ -36,7 +36,6 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <gtest/gtest.h>
 
 
-#include <ginkgo/core/matrix/csr.hpp>
 #include <ginkgo/core/matrix/dense.hpp>
 
 
