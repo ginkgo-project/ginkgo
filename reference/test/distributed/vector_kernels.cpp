724276fcced3a8033a7e9686439afbc7d02ea2e8
Marcel Koch <marcel.koch@kit.edu>
2022-01-24 16:01:24 +0100

    fix formatting

--- reference/test/distributed/vector_kernels.cpp
+++ reference/test/distributed/vector_kernels.cpp
@@ -1,5 +1,5 @@
 /*******************************<GINKGO LICENSE>******************************
-Copyright (c) 2017-2021, the Ginkgo authors
+Copyright (c) 2017-2022, the Ginkgo authors
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
@@ -30,9 +30,6 @@ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ******************************<GINKGO LICENSE>*******************************/
 
-#include "core/distributed/vector_kernels.hpp"
-
-
 #include <algorithm>
 #include <memory>
 #include <vector>
@@ -46,6 +43,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <ginkgo/core/base/matrix_data.hpp>
 
 
+#include "core/distributed/vector_kernels.hpp"
 #include "core/test/utils.hpp"
 
 
