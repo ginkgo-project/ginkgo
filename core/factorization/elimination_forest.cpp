62ff6ba7d2f7006a647e91ac1620bb895ed04460
Tobias Ribizel <ribizel@kit.edu>
2022-01-27 00:42:49 +0100

    add elimination forest computation

---
+++ core/factorization/elimination_forest.cpp
@@ -0,0 +1,187 @@
+/*******************************<GINKGO LICENSE>******************************
+Copyright (c) 2017-2022, the Ginkgo authors
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+1. Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+contributors may be used to endorse or promote products derived from
+this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+******************************<GINKGO LICENSE>*******************************/
+
+#include "core/factorization/elimination_forest.hpp"
+#include "ginkgo/core/base/types.hpp"
+
+
+namespace gko {
+namespace factorization {
+namespace {
+
+
+template <typename IndexType>
+void compute_elim_forest_parent_impl(std::shared_ptr<const Executor> host_exec,
+                                     const IndexType* row_ptrs,
+                                     const IndexType* cols, IndexType num_rows,
+                                     IndexType* parent)
+{
+    disjoint_sets<IndexType> subtrees{host_exec, num_rows};
+    Array<IndexType> subtree_root_array{host_exec,
+                                        static_cast<size_type>(num_rows)};
+    // pseudo-root one past the last row to deal with disconnected matrices
+    const auto unattached = num_rows;
+    auto subtree_root = subtree_root_array.get_data();
+    for (IndexType row = 0; row < num_rows; row++) {
+        // so far the row is an unattached singleton subtree
+        subtree_root[row] = row;
+        parent[row] = unattached;
+        auto row_rep = row;
+        for (auto nz = row_ptrs[row]; nz < row_ptrs[row + 1]; nz++) {
+            const auto col = cols[nz];
+            // for each lower diagonal entry
+            if (col < row) {
+                // find the subtree it is contained in
+                const auto col_rep = subtrees.find(col);
+                const auto col_root = subtree_root[col_rep];
+                // if it is not yet attached, put it below row
+                // and make row its new root
+                if (parent[col_root] == unattached && col_root != row) {
+                    parent[col_root] = row;
+                    row_rep = subtrees.join(row_rep, col_rep);
+                    subtree_root[row_rep] = row;
+                }
+            }
+        }
+    }
+}
+
+
+template <typename IndexType>
+void compute_elim_forest_children_impl(const IndexType* parent, IndexType size,
+                                       IndexType* child_ptr, IndexType* child)
+{
+    // count how many times each parent occurs, excluding pseudo-root at
+    // parent == size
+    std::fill_n(child_ptr, size + 2, IndexType{});
+    for (IndexType i = 0; i < size; i++) {
+        const auto p = parent[i];
+        if (p < size) {
+            child_ptr[p + 2]++;
+        }
+    }
+    // shift by 2 leads to exclusive prefix sum with 0 padding
+    std::partial_sum(child_ptr, child_ptr + size + 2, child_ptr);
+    // we count the same again, this time shifted by 1 => exclusive prefix sum
+    for (IndexType i = 0; i < size; i++) {
+        const auto p = parent[i];
+        child[child_ptr[p + 1]++] = i;
+    }
+}
+
+
+template <typename IndexType>
+void compute_elim_forest_postorder_lvl_impl(
+    std::shared_ptr<const Executor> host_exec, const IndexType* parent,
+    const IndexType* child_ptr, const IndexType* child, IndexType size,
+    IndexType* postorder, IndexType* inv_postorder, IndexType* level)
+{
+    Array<IndexType> current_child_array{host_exec,
+                                         static_cast<size_type>(size + 1)};
+    current_child_array.fill(0);
+    auto current_child = current_child_array.get_data();
+    IndexType postorder_idx{};
+    // for each tree in the elimination forest
+    for (auto tree = child_ptr[size]; tree < child_ptr[size + 1]; tree++) {
+        // start from the root
+        const auto root = child[tree];
+        auto cur_node = root;
+        IndexType level_idx{};
+        // traverse until we moved to the pseudo-root
+        while (cur_node < size) {
+            const auto first_child = child_ptr[cur_node];
+            const auto num_children = child_ptr[cur_node + 1] - first_child;
+            if (current_child[cur_node] >= num_children) {
+                // if this node is completed, output it
+                postorder[postorder_idx] = cur_node;
+                inv_postorder[cur_node] = postorder_idx;
+                level[postorder_idx] = level_idx;
+                cur_node = parent[cur_node];
+                level_idx--;
+                postorder_idx++;
+            } else {
+                // otherwise go to the next child node
+                cur_node = child[first_child + current_child[cur_node]++];
+                level_idx++;
+            }
+        }
+    }
+}
+
+
+}  // namespace
+
+
+template <typename IndexType>
+elimination_forest<IndexType>::elimination_forest(
+    std::shared_ptr<const Executor> host_exec, IndexType size)
+    : parents{host_exec, static_cast<size_type>(size)},
+      child_ptrs{host_exec, static_cast<size_type>(size + 2)},
+      children{host_exec, static_cast<size_type>(size)},
+      postorder{host_exec, static_cast<size_type>(size)},
+      inv_postorder{host_exec, static_cast<size_type>(size)},
+      levels{host_exec, static_cast<size_type>(size)}
+{}
+
+
+template <typename ValueType, typename IndexType>
+elimination_forest<IndexType> compute_elim_forest(
+    const matrix::Csr<ValueType, IndexType>* mtx)
+{
+    const auto host_exec = mtx->get_executor()->get_master();
+    const auto host_mtx = make_temporary_clone(host_exec, mtx);
+    const auto num_rows = static_cast<IndexType>(host_mtx->get_size()[0]);
+    elimination_forest<IndexType> forest{host_exec, num_rows};
+    compute_elim_forest_parent_impl(host_exec, host_mtx->get_const_row_ptrs(),
+                                    host_mtx->get_const_col_idxs(), num_rows,
+                                    forest.parents.get_data());
+    compute_elim_forest_children_impl(forest.parents.get_const_data(), num_rows,
+                                      forest.child_ptrs.get_data(),
+                                      forest.children.get_data());
+    compute_elim_forest_postorder_lvl_impl(
+        host_exec, forest.parents.get_const_data(),
+        forest.child_ptrs.get_const_data(), forest.children.get_const_data(),
+        num_rows, forest.postorder.get_data(), forest.inv_postorder.get_data(),
+        forest.levels.get_data());
+    return forest;
+}
+
+
+#define GKO_DECLARE_COMPUTE_ELIM_FOREST(ValueType, IndexType) \
+    elimination_forest<IndexType> compute_elim_forest(        \
+        const matrix::Csr<ValueType, IndexType>* mtx)
+
+GKO_INSTANTIATE_FOR_EACH_VALUE_AND_INDEX_TYPE(GKO_DECLARE_COMPUTE_ELIM_FOREST);
+
+
+}  // namespace factorization
+}  // namespace gko
---
+++ core/test/factorization/elimination_forest.cpp
@@ -0,0 +1,169 @@
+/*******************************<GINKGO LICENSE>******************************
+Copyright (c) 2017-2022, the Ginkgo authors
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+1. Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+contributors may be used to endorse or promote products derived from
+this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+******************************<GINKGO LICENSE>*******************************/
+
+#include "core/factorization/elimination_forest.cpp"
+
+
+#include <algorithm>
+#include <numeric>
+
+
+#include <gtest/gtest.h>
+
+
+#include <ginkgo/core/base/executor.hpp>
+
+
+#include "core/factorization/elimination_forest.hpp"
+#include "core/test/utils.hpp"
+#include "matrices/config.hpp"
+
+
+namespace {
+
+
+template <typename ValueIndexType>
+class EliminationForest : public ::testing::Test {
+public:
+    using value_type =
+        typename std::tuple_element<0, decltype(ValueIndexType())>::type;
+    using index_type =
+        typename std::tuple_element<1, decltype(ValueIndexType())>::type;
+    using matrix_type = gko::matrix::Csr<value_type, index_type>;
+
+protected:
+    EliminationForest() : ref(gko::ReferenceExecutor::create()) {}
+
+    std::shared_ptr<const gko::ReferenceExecutor> ref;
+};
+
+TYPED_TEST_SUITE(EliminationForest, gko::test::ValueIndexTypes,
+                 PairTypenameNameGenerator);
+
+
+TYPED_TEST(EliminationForest, WorksForExample)
+{
+    using matrix_type = typename TestFixture::matrix_type;
+    using index_type = typename TestFixture::index_type;
+    auto mtx = gko::initialize<typename TestFixture::matrix_type>(
+        {{1, 0, 1, 0, 0, 0, 0, 1, 0, 0},
+         {0, 1, 0, 1, 0, 0, 0, 0, 0, 1},
+         {1, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+         {0, 0, 0, 1, 0, 0, 0, 0, 1, 1},
+         {0, 1, 0, 0, 1, 0, 0, 0, 1, 1},
+         {0, 0, 0, 0, 0, 1, 0, 1, 0, 0},
+         {0, 0, 1, 0, 0, 1, 1, 0, 0, 0},
+         {1, 0, 0, 0, 0, 1, 0, 1, 1, 1},
+         {0, 0, 0, 1, 1, 0, 0, 1, 1, 0},
+         {0, 1, 0, 1, 1, 0, 0, 1, 0, 1}},
+        this->ref);
+
+    auto forest = gko::factorization::compute_elim_forest(mtx.get());
+
+    GKO_ASSERT_ARRAY_EQ(forest.parents,
+                        I<index_type>({2, 4, 6, 8, 8, 6, 7, 8, 9, 10}));
+    GKO_ASSERT_ARRAY_EQ(forest.child_ptrs,
+                        I<index_type>({0, 0, 0, 1, 1, 2, 2, 4, 5, 8, 9, 10}));
+    GKO_ASSERT_ARRAY_EQ(forest.children,
+                        I<index_type>({0, 1, 2, 5, 6, 3, 4, 7, 8, 9}));
+    GKO_ASSERT_ARRAY_EQ(forest.postorder,
+                        I<index_type>({3, 1, 4, 0, 2, 5, 6, 7, 8, 9}));
+    GKO_ASSERT_ARRAY_EQ(forest.inv_postorder,
+                        I<index_type>({3, 1, 4, 0, 2, 5, 6, 7, 8, 9}));
+    GKO_ASSERT_ARRAY_EQ(forest.levels,
+                        I<index_type>({2, 3, 2, 5, 4, 4, 3, 2, 1, 0}));
+}
+
+
+TYPED_TEST(EliminationForest, WorksForSeparable)
+{
+    using matrix_type = typename TestFixture::matrix_type;
+    using index_type = typename TestFixture::index_type;
+    auto mtx = gko::initialize<typename TestFixture::matrix_type>(
+        {
+            {1, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+            {0, 1, 1, 0, 0, 0, 0, 0, 0, 0},
+            {1, 1, 1, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 1, 1, 0, 0, 0, 0, 0},
+            {0, 0, 0, 1, 1, 1, 0, 0, 0, 1},
+            {0, 0, 0, 0, 1, 1, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 1, 0, 0, 1},
+            {0, 0, 0, 0, 0, 0, 1, 1, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 1, 1},
+            {0, 0, 0, 0, 1, 0, 1, 0, 1, 1},
+        },
+        this->ref);
+
+    auto forest = gko::factorization::compute_elim_forest(mtx.get());
+
+    GKO_ASSERT_ARRAY_EQ(forest.parents,
+                        I<index_type>({2, 2, 10, 4, 5, 9, 7, 9, 9, 10}));
+    GKO_ASSERT_ARRAY_EQ(forest.child_ptrs,
+                        I<index_type>({0, 0, 0, 2, 2, 3, 4, 4, 5, 5, 8, 10}));
+    GKO_ASSERT_ARRAY_EQ(forest.children,
+                        I<index_type>({0, 1, 3, 4, 6, 5, 7, 8, 2, 9}));
+    GKO_ASSERT_ARRAY_EQ(forest.postorder,
+                        I<index_type>({0, 1, 2, 3, 4, 5, 6, 7, 8, 9}));
+    GKO_ASSERT_ARRAY_EQ(forest.inv_postorder,
+                        I<index_type>({0, 1, 2, 3, 4, 5, 6, 7, 8, 9}));
+    GKO_ASSERT_ARRAY_EQ(forest.levels,
+                        I<index_type>({1, 1, 0, 3, 2, 1, 2, 1, 1, 0}));
+}
+
+
+TYPED_TEST(EliminationForest, WorksForAni1)
+{
+    using matrix_type = typename TestFixture::matrix_type;
+    using index_type = typename TestFixture::index_type;
+    std::ifstream stream{gko::matrices::location_ani1_mtx};
+    auto mtx = gko::read<matrix_type>(stream, this->ref);
+
+    auto forest = gko::factorization::compute_elim_forest(mtx.get());
+
+    // the elimination tree is a path
+    gko::Array<index_type> iota_arr{this->ref, 36};
+    gko::Array<index_type> iota_arr2{this->ref, 38};
+    std::iota(iota_arr.get_data(), iota_arr.get_data() + 36, 1);
+    std::iota(iota_arr2.get_data() + 1, iota_arr2.get_data() + 38, 0);
+    iota_arr2.get_data()[0] = 0;
+    GKO_ASSERT_ARRAY_EQ(forest.parents, iota_arr);
+    GKO_ASSERT_ARRAY_EQ(forest.child_ptrs, iota_arr2);
+    std::iota(iota_arr.get_data(), iota_arr.get_data() + 36, 0);
+    GKO_ASSERT_ARRAY_EQ(forest.children, iota_arr);
+    GKO_ASSERT_ARRAY_EQ(forest.postorder, iota_arr);
+    GKO_ASSERT_ARRAY_EQ(forest.inv_postorder, iota_arr);
+    std::reverse(iota_arr.get_data(), iota_arr.get_data() + 36);
+    GKO_ASSERT_ARRAY_EQ(forest.levels, iota_arr);
+}
+
+
+}  // namespace
