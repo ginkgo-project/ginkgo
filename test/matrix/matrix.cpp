afcedd91787f9d1db3e7e31ce1780370afe8ff44
Yuhsiang M. Tsai <yhmtsai@gmail.com>
2022-02-11 07:00:40 +0100

    change missing ranlux to default_random_engine
    Co-authored-by: Tobias Ribizel <ribizel@kit.edu>

--- test/matrix/matrix.cpp
+++ test/matrix/matrix.cpp
@@ -613,7 +613,7 @@ protected:
     std::shared_ptr<gko::ReferenceExecutor> ref;
     std::shared_ptr<gko::EXEC_TYPE> exec;
 
-    std::ranlux48 rand_engine;
+    std::default_random_engine rand_engine;
 };
 
 using MatrixTypes = ::testing::Types<
