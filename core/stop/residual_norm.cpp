20a5c0ceadafbd59faed5a33c9bf256a95dd3b44
Marcel Koch <marcel.koch@kit.edu>
2022-02-23 12:40:51 +0100

    add distributed dispatch to residual norm criteria

--- core/stop/residual_norm.cpp
+++ core/stop/residual_norm.cpp
@@ -30,6 +30,7 @@ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ******************************<GINKGO LICENSE>*******************************/
 
+#include <ginkgo/core/base/precision_dispatch.hpp>
 #include <ginkgo/core/stop/residual_norm.hpp>
 
 
@@ -62,6 +63,123 @@ GKO_REGISTER_OPERATION(implicit_residual_norm,
 }  // namespace implicit_residual_norm
 
 
+#if GINKGO_BUILD_MPI
+
+
+template <typename Arg>
+bool use_distributed(Arg* linop)
+{
+    return dynamic_cast<const distributed::DistributedBase*>(linop);
+}
+
+
+template <typename Arg, typename... Rest>
+bool use_distributed(Arg* linop, Rest*... rest)
+{
+    bool is_distributed =
+        dynamic_cast<const distributed::DistributedBase*>(linop);
+    GKO_ASSERT(is_distributed == use_distributed(rest...));
+    return is_distributed;
+}
+
+
+template <typename ValueType, typename Function, typename... LinOps>
+void norm_dispatch(Function&& fn, LinOps*... linops)
+{
+    if (use_distributed(linops...)) {
+        precision_dispatch_distributed<ValueType>(std::forward<Function>(fn),
+                                                  linops...);
+    } else {
+        precision_dispatch<ValueType>(std::forward<Function>(fn), linops...);
+    }
+}
+
+
+#else
+
+
+template <typename ValueType, typename Function, typename... LinOps>
+void norm_dispatch(Function&& fn, LinOps*... linops)
+{
+    precision_dispatch<ValueType>(std::forward<Function>(fn), linops...);
+}
+
+
+#endif
+
+
+template <typename ValueType>
+ResidualNormBase<ValueType>::ResidualNormBase(
+    std::shared_ptr<const gko::Executor> exec, const CriterionArgs& args,
+    remove_complex<ValueType> reduction_factor, mode baseline)
+    : EnablePolymorphicObject<ResidualNormBase, Criterion>(exec),
+      device_storage_{exec, 2},
+      reduction_factor_{reduction_factor},
+      baseline_{baseline},
+      system_matrix_{args.system_matrix},
+      b_{args.b},
+      one_{gko::initialize<Vector>({1}, exec)},
+      neg_one_{gko::initialize<Vector>({-1}, exec)}
+{
+    switch (baseline_) {
+    case mode::initial_resnorm: {
+        if (args.initial_residual == nullptr) {
+            if (args.system_matrix == nullptr || args.b == nullptr ||
+                args.x == nullptr) {
+                GKO_NOT_SUPPORTED(nullptr);
+            } else {
+                this->starting_tau_ =
+                    NormVector::create(exec, dim<2>{1, args.b->get_size()[1]});
+                auto b_clone = share(args.b->clone());
+                args.system_matrix->apply(neg_one_.get(), args.x, one_.get(),
+                                          b_clone.get());
+                norm_dispatch<ValueType>(
+                    [&](auto dense_r) {
+                        dense_r->compute_norm2(this->starting_tau_.get());
+                    },
+                    b_clone.get());
+            }
+        } else {
+            this->starting_tau_ = NormVector::create(
+                exec, dim<2>{1, args.initial_residual->get_size()[1]});
+            norm_dispatch<ValueType>(
+                [&](auto dense_r) {
+                    dense_r->compute_norm2(this->starting_tau_.get());
+                },
+                args.initial_residual);
+        }
+        break;
+    }
+    case mode::rhs_norm: {
+        if (args.b == nullptr) {
+            GKO_NOT_SUPPORTED(nullptr);
+        }
+        this->starting_tau_ =
+            NormVector::create(exec, dim<2>{1, args.b->get_size()[1]});
+        norm_dispatch<ValueType>(
+            [&](auto dense_r) {
+                dense_r->compute_norm2(this->starting_tau_.get());
+            },
+            args.b.get());
+        break;
+    }
+    case mode::absolute: {
+        if (args.b == nullptr) {
+            GKO_NOT_SUPPORTED(nullptr);
+        }
+        this->starting_tau_ =
+            NormVector::create(exec, dim<2>{1, args.b->get_size()[1]});
+        this->starting_tau_->fill(gko::one<remove_complex<ValueType>>());
+        break;
+    }
+    default:
+        GKO_NOT_SUPPORTED(nullptr);
+    }
+    this->u_dense_tau_ =
+        NormVector::create_with_config_of(this->starting_tau_.get());
+}
+
+
 template <typename ValueType>
 bool ResidualNormBase<ValueType>::check_impl(
     uint8 stopping_id, bool set_finalized, array<stopping_status>* stop_status,
@@ -71,33 +189,21 @@ bool ResidualNormBase<ValueType>::check_impl(
     if (updater.residual_norm_ != nullptr) {
         dense_tau = as<NormVector>(updater.residual_norm_);
     } else if (updater.residual_ != nullptr) {
-        if (dynamic_cast<const ComplexVector*>(updater.residual_)) {
-            auto* dense_r = as<ComplexVector>(updater.residual_);
-            dense_r->compute_norm2(u_dense_tau_.get());
-        } else {
-            auto* dense_r = as<Vector>(updater.residual_);
-            dense_r->compute_norm2(u_dense_tau_.get());
-        }
+        norm_dispatch<ValueType>(
+            [&](auto dense_r) { dense_r->compute_norm2(u_dense_tau_.get()); },
+            updater.residual_);
         dense_tau = u_dense_tau_.get();
     } else if (updater.solution_ != nullptr && system_matrix_ != nullptr &&
                b_ != nullptr) {
         auto exec = this->get_executor();
-        // when LinOp is real but rhs is complex, we use real view on complex,
-        // so it still uses the same type of scalar in apply.
-        if (auto vec_b = std::dynamic_pointer_cast<const Vector>(b_)) {
-            auto dense_r = vec_b->clone();
-            system_matrix_->apply(neg_one_.get(), updater.solution_, one_.get(),
-                                  dense_r.get());
-            dense_r->compute_norm2(u_dense_tau_.get());
-        } else if (auto vec_b =
-                       std::dynamic_pointer_cast<const ComplexVector>(b_)) {
-            auto dense_r = vec_b->clone();
-            system_matrix_->apply(neg_one_.get(), updater.solution_, one_.get(),
-                                  dense_r.get());
-            dense_r->compute_norm2(u_dense_tau_.get());
-        } else {
-            GKO_NOT_SUPPORTED(nullptr);
-        }
+        norm_dispatch<ValueType>(
+            [&](auto dense_b, auto dense_x) {
+                auto dense_r = dense_b->clone();
+                system_matrix_->apply(neg_one_.get(), dense_x, one_.get(),
+                                      dense_r.get());
+                dense_r->compute_norm2(u_dense_tau_.get());
+            },
+            b_.get(), updater.solution_);
         dense_tau = u_dense_tau_.get();
     } else {
         GKO_NOT_SUPPORTED(nullptr);
