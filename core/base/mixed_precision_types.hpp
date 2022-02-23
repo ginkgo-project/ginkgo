f32e5c44cd17c470f40a6112f3ac1c06c67e63a1
Yuhsiang M. Tsai <yhmtsai@gmail.com>
2022-02-23 17:03:32 +0100

    fix the mixed on single mode and use adapt macro

--- core/base/mixed_precision_types.hpp
+++ core/base/mixed_precision_types.hpp
@@ -39,39 +39,39 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
 #ifdef GINKGO_MIXED_PRECISION
-#define GKO_INSTANTIATE_FOR_EACH_MIXED_VALUE_TYPE(_macro, ...)  \
-    template _macro(float, float, float, __VA_ARGS__);          \
-    template _macro(float, float, double, __VA_ARGS__);         \
-    template _macro(float, double, float, __VA_ARGS__);         \
-    template _macro(float, double, double, __VA_ARGS__);        \
-    template _macro(double, float, float, __VA_ARGS__);         \
-    template _macro(double, float, double, __VA_ARGS__);        \
-    template _macro(double, double, float, __VA_ARGS__);        \
-    template _macro(double, double, double, __VA_ARGS__);       \
-    template _macro(std::complex<float>, std::complex<float>,   \
-                    std::complex<float>, __VA_ARGS__);          \
-    template _macro(std::complex<float>, std::complex<float>,   \
-                    std::complex<double>, __VA_ARGS__);         \
-    template _macro(std::complex<float>, std::complex<double>,  \
-                    std::complex<float>, __VA_ARGS__);          \
-    template _macro(std::complex<float>, std::complex<double>,  \
-                    std::complex<double>, __VA_ARGS__);         \
-    template _macro(std::complex<double>, std::complex<float>,  \
-                    std::complex<float>, __VA_ARGS__);          \
-    template _macro(std::complex<double>, std::complex<float>,  \
-                    std::complex<double>, __VA_ARGS__);         \
-    template _macro(std::complex<double>, std::complex<double>, \
-                    std::complex<float>, __VA_ARGS__);          \
-    template _macro(std::complex<double>, std::complex<double>, \
-                    std::complex<double>, __VA_ARGS__)
+#define GKO_INSTANTIATE_FOR_EACH_MIXED_VALUE_TYPE(_macro, ...)          \
+    template _macro(float, float, float, __VA_ARGS__);                  \
+    GKO_ADAPT_SINGLE(_macro(float, float, double, __VA_ARGS__));        \
+    GKO_ADAPT_SINGLE(_macro(float, double, float, __VA_ARGS__));        \
+    GKO_ADAPT_SINGLE(_macro(float, double, double, __VA_ARGS__));       \
+    GKO_ADAPT_SINGLE(_macro(double, float, float, __VA_ARGS__));        \
+    GKO_ADAPT_SINGLE(_macro(double, float, double, __VA_ARGS__));       \
+    GKO_ADAPT_SINGLE(_macro(double, double, float, __VA_ARGS__));       \
+    GKO_ADAPT_SINGLE(_macro(double, double, double, __VA_ARGS__));      \
+    template _macro(std::complex<float>, std::complex<float>,           \
+                    std::complex<float>, __VA_ARGS__);                  \
+    GKO_ADAPT_SINGLE(_macro(std::complex<float>, std::complex<float>,   \
+                            std::complex<double>, __VA_ARGS__));        \
+    GKO_ADAPT_SINGLE(_macro(std::complex<float>, std::complex<double>,  \
+                            std::complex<float>, __VA_ARGS__));         \
+    GKO_ADAPT_SINGLE(_macro(std::complex<float>, std::complex<double>,  \
+                            std::complex<double>, __VA_ARGS__));        \
+    GKO_ADAPT_SINGLE(_macro(std::complex<double>, std::complex<float>,  \
+                            std::complex<float>, __VA_ARGS__));         \
+    GKO_ADAPT_SINGLE(_macro(std::complex<double>, std::complex<float>,  \
+                            std::complex<double>, __VA_ARGS__));        \
+    GKO_ADAPT_SINGLE(_macro(std::complex<double>, std::complex<double>, \
+                            std::complex<float>, __VA_ARGS__));         \
+    GKO_ADAPT_SINGLE(_macro(std::complex<double>, std::complex<double>, \
+                            std::complex<double>, __VA_ARGS__))
 #else
-#define GKO_INSTANTIATE_FOR_EACH_MIXED_VALUE_TYPE(_macro, ...)  \
-    template _macro(float, float, float, __VA_ARGS__);          \
-    template _macro(double, double, double, __VA_ARGS__);       \
-    template _macro(std::complex<float>, std::complex<float>,   \
-                    std::complex<float>, __VA_ARGS__);          \
-    template _macro(std::complex<double>, std::complex<double>, \
-                    std::complex<double>, __VA_ARGS__)
+#define GKO_INSTANTIATE_FOR_EACH_MIXED_VALUE_TYPE(_macro, ...)          \
+    template _macro(float, float, float, __VA_ARGS__);                  \
+    GKO_ADAPT_SINGLE(_macro(double, double, double, __VA_ARGS__));      \
+    template _macro(std::complex<float>, std::complex<float>,           \
+                    std::complex<float>, __VA_ARGS__);                  \
+    GKO_ADAPT_SINGLE(_macro(std::complex<double>, std::complex<double>, \
+                            std::complex<double>, __VA_ARGS__))
 #endif
 
 
@@ -81,21 +81,25 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
 #ifdef GINKGO_MIXED_PRECISION
-#define GKO_INSTANTIATE_FOR_EACH_MIXED_VALUE_TYPE_2(_macro, ...)             \
-    template _macro(float, float, __VA_ARGS__);                              \
-    template _macro(float, double, __VA_ARGS__);                             \
-    template _macro(double, float, __VA_ARGS__);                             \
-    template _macro(double, double, __VA_ARGS__);                            \
-    template _macro(std::complex<float>, std::complex<float>, __VA_ARGS__);  \
-    template _macro(std::complex<float>, std::complex<double>, __VA_ARGS__); \
-    template _macro(std::complex<double>, std::complex<float>, __VA_ARGS__); \
-    template _macro(std::complex<double>, std::complex<double>, __VA_ARGS__)
+#define GKO_INSTANTIATE_FOR_EACH_MIXED_VALUE_TYPE_2(_macro, ...)            \
+    template _macro(float, float, __VA_ARGS__);                             \
+    GKO_ADAPT_SINGLE(_macro(float, double, __VA_ARGS__));                   \
+    GKO_ADAPT_SINGLE(_macro(double, float, __VA_ARGS__));                   \
+    GKO_ADAPT_SINGLE(_macro(double, double, __VA_ARGS__));                  \
+    template _macro(std::complex<float>, std::complex<float>, __VA_ARGS__); \
+    GKO_ADAPT_SINGLE(                                                       \
+        _macro(std::complex<float>, std::complex<double>, __VA_ARGS__));    \
+    GKO_ADAPT_SINGLE(                                                       \
+        _macro(std::complex<double>, std::complex<float>, __VA_ARGS__));    \
+    GKO_ADAPT_SINGLE(                                                       \
+        _macro(std::complex<double>, std::complex<double>, __VA_ARGS__))
 #else
 #define GKO_INSTANTIATE_FOR_EACH_MIXED_VALUE_TYPE_2(_macro, ...)            \
     template _macro(float, float, __VA_ARGS__);                             \
-    template _macro(double, double, __VA_ARGS__);                           \
+    GKO_ADAPT_SINGLE(_macro(double, double, __VA_ARGS__));                  \
     template _macro(std::complex<float>, std::complex<float>, __VA_ARGS__); \
-    template _macro(std::complex<double>, std::complex<double>, __VA_ARGS__)
+    GKO_ADAPT_SINGLE(                                                       \
+        _macro(std::complex<double>, std::complex<double>, __VA_ARGS__))
 #endif
 
 
