9e1d62751f8e88d9977e2212a53b8fd9d270aa95
Marcel Koch <marcel.koch@kit.edu>
2022-02-24 16:40:37 +0100

    add generic distributed solver tests
    the changes to matrix_utils.hpp should be merged with https://github.com/ginkgo-project/ginkgo/pull/973

--- core/test/utils/matrix_utils.hpp
+++ core/test/utils/matrix_utils.hpp
@@ -153,47 +153,185 @@ void make_hpd(matrix::Dense<ValueType>* mtx,
 
 
 /**
- * Changes the diagonal entry in the requested row, shrinking the
- * matrix by 1 nonzero entry.
+ * Removes all diagonal entries from the given matrix data.
  *
- * @param mtx  The matrix to remove a diagonal entry from.
- * @param row_to_process  The row from which to remove the diagonal entry.
+ * @param data  the matrix data
+ *
+ * @tparam ValueType  the value type
+ * @tparam IndexType  the index type
  */
-template <typename MtxType>
-void remove_diagonal_entry_from_row(
-    MtxType* const mtx, const typename MtxType::index_type row_to_process)
+template <typename ValueType, typename IndexType>
+void make_remove_diagonal(matrix_data<ValueType, IndexType>& data)
 {
-    using value_type = typename MtxType::value_type;
-    using index_type = typename MtxType::index_type;
-    matrix_data<value_type, index_type> mdata;
-    mtx->write(mdata);
-    auto it = std::remove_if(mdata.nonzeros.begin(), mdata.nonzeros.end(),
-                             [&](auto entry) {
-                                 return entry.row == row_to_process &&
-                                        entry.column == row_to_process;
-                             });
-    mdata.nonzeros.erase(it, mdata.nonzeros.end());
-    mtx->read(mdata);
+    data.nonzeros.erase(
+        std::remove_if(data.nonzeros.begin(), data.nonzeros.end(),
+                       [](auto entry) { return entry.row == entry.column; }),
+        data.nonzeros.end());
 }
 
 
 /**
- * Ensures each row has a diagonal entry.
+ * Sets all diagonal entries for the given matrix data to one.
+ *
+ * @param data  the matrix data
+ *
+ * @tparam ValueType  the value type
+ * @tparam IndexType  the index type
  */
-template <typename MtxType>
-void ensure_all_diagonal_entries(MtxType* const mtx)
+template <typename ValueType, typename IndexType>
+void make_unit_diagonal(matrix_data<ValueType, IndexType>& data)
 {
-    using value_type = typename MtxType::value_type;
-    using index_type = typename MtxType::index_type;
-    matrix_data<value_type, index_type> mdata;
-    mtx->write(mdata);
-    const auto nrows = static_cast<index_type>(mtx->get_size()[0]);
-    mdata.nonzeros.reserve(mtx->get_num_stored_elements() + nrows);
-    for (index_type i = 0; i < nrows; i++) {
-        mdata.nonzeros.push_back({i, i, zero<value_type>()});
+    make_remove_diagonal(data);
+    auto num_diags = std::min(data.size[0], data.size[1]);
+    for (gko::int64 i = 0; i < num_diags; i++) {
+        data.nonzeros.emplace_back(i, i, 1.0);
     }
-    mdata.sum_duplicates();
-    mtx->read(mdata);
+    data.ensure_row_major_order();
+}
+
+
+/**
+ * Replace A by (A + op(A^T)) / 2 in the matrix data
+ *
+ * @param data  the matrix data
+ * @param op  the operator to apply to the transposed value
+ *
+ * @tparam ValueType  the value type
+ * @tparam IndexType  the index type
+ */
+template <typename ValueType, typename IndexType, typename Op>
+void make_symmetric_generic(matrix_data<ValueType, IndexType>& data, Op op)
+{
+    GKO_ASSERT_IS_SQUARE_MATRIX(data.size);
+    // compute (A + op(A^T)) / 2
+    const auto nnz = data.nonzeros.size();
+    for (std::size_t i = 0; i < nnz; i++) {
+        data.nonzeros[i].value /= 2.0;
+        auto entry = data.nonzeros[i];
+        data.nonzeros.emplace_back(entry.column, entry.row, op(entry.value));
+    }
+    // combine duplicates
+    data.sum_duplicates();
+}
+
+
+/**
+ * Replace A by (A + A^T) / 2 in the matrix data
+ *
+ * @param data  the matrix data
+ *
+ * @tparam ValueType  the value type
+ * @tparam IndexType  the index type
+ */
+template <typename ValueType, typename IndexType>
+void make_symmetric(matrix_data<ValueType, IndexType>& data)
+{
+    make_symmetric_generic(data, [](auto val) { return val; });
+}
+
+
+/**
+ * Replace A by (A + conj(A^T)) / 2 in the matrix data
+ *
+ * @param data  the matrix data
+ *
+ * @tparam ValueType  the value type
+ * @tparam IndexType  the index type
+ */
+template <typename ValueType, typename IndexType>
+void make_hermitian(matrix_data<ValueType, IndexType>& data)
+{
+    make_symmetric_generic(data, [](auto val) { return gko::conj(val); });
+}
+
+
+/**
+ * Scales the matrix data such that its diagonal is at least ratio times as
+ * large as the sum of offdiagonal magnitudes. Inserts diagonals if missing.
+ *
+ * @param data  the matrix data
+ * @param ratio  how much bigger should the diagonal entries be compared to the
+ *               offdiagonal entry magnitude sum?
+ *
+ * @tparam ValueType  the value type
+ * @tparam IndexType  the index type
+ */
+template <typename ValueType, typename IndexType>
+void make_diag_dominant(matrix_data<ValueType, IndexType>& data,
+                        remove_complex<ValueType> ratio = 1.0)
+{
+    GKO_ASSERT_EQ(ratio >= 1.0, true);
+    GKO_ASSERT_IS_SQUARE_MATRIX(data.size);
+    std::vector<remove_complex<ValueType>> norms(data.size[0]);
+    std::vector<gko::int64> diag_positions(data.size[0], -1);
+    gko::int64 i{};
+    for (auto entry : data.nonzeros) {
+        if (entry.row == entry.column) {
+            diag_positions[entry.row] = i;
+        } else {
+            norms[entry.row] += gko::abs(entry.value);
+        }
+        i++;
+    }
+    for (i = 0; i < data.size[0]; i++) {
+        if (diag_positions[i] < 0) {
+            data.nonzeros.emplace_back(i, i, norms[i] * ratio);
+        } else {
+            auto& diag_value = data.nonzeros[diag_positions[i]].value;
+            const auto diag_magnitude = gko::abs(diag_value);
+            const auto offdiag_magnitude = norms[i];
+            if (diag_magnitude < offdiag_magnitude * ratio) {
+                const auto scaled_value =
+                    diag_value * (offdiag_magnitude * ratio / diag_magnitude);
+                if (gko::is_finite(scaled_value)) {
+                    diag_value = scaled_value;
+                } else {
+                    diag_value = offdiag_magnitude * ratio;
+                }
+            }
+        }
+    }
+    data.ensure_row_major_order();
+}
+
+
+/**
+ * Makes the matrix symmetric and diagonally dominant by the given ratio.
+ *
+ * @param data  the matrix data
+ * @param ratio  how much bigger should the diagonal entries be compared to the
+ *               offdiagonal entry magnitude sum?
+ *
+ * @tparam ValueType  the value type
+ * @tparam IndexType  the index type
+ */
+template <typename ValueType, typename IndexType>
+void make_spd(matrix_data<ValueType, IndexType>& data,
+              remove_complex<ValueType> ratio = 1.001)
+{
+    GKO_ASSERT_EQ(ratio > 1.0, true);
+    make_symmetric(data);
+    make_diag_dominant(data, ratio);
+}
+
+
+/**
+ * Makes the matrix hermitian and diagonally dominant by the given ratio.
+ *
+ * @param data  the matrix data
+ * @param ratio  how much bigger should the diagonal entries be compared to the
+ *               offdiagonal entry magnitude sum?
+ *
+ * @tparam ValueType  the value type
+ * @tparam IndexType  the index type
+ */
+template <typename ValueType, typename IndexType>
+void make_hpd(matrix_data<ValueType, IndexType>& data,
+              remove_complex<ValueType> ratio = 1.001)
+{
+    GKO_ASSERT_EQ(ratio > 1.0, true);
+    make_hermitian(data);
+    make_diag_dominant(data, ratio);
 }
 
 
