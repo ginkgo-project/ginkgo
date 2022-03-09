ab90306fec24635c92e9a6d60c98f4bfd231bf7d
Yuhsiang M. Tsai <yhmtsai@gmail.com>
2022-03-09 19:12:16 +0100

    finished

--- extension/resource_manager/include/resource_manager/solver/cg.hpp
+++ extension/resource_manager/include/resource_manager/solver/cg.hpp
@@ -50,15 +50,6 @@ namespace extension {
 namespace resource_manager {
 
 
-// TODO: Please add the corresponding to the resource_manager/base/types.hpp
-// Add _expand(CgFactory) to ENUM_LINOPFACTORY
-// Add _expand(Cg) to ENUM_LINOP
-// If need to override the generated enum for RM, use RM_CLASS or
-// RM_CLASS_FACTORY env and rerun the generated script. Or replace the
-// (RM_LinOpFactory::)CgFactory and (RM_LinOp::)Cg and their snake case in
-// IMPLEMENT_BRIDGE, ENABLE_SELECTION, *_select, ...
-
-
 template <typename ValueType>
 struct Generic<typename gko::solver::Cg<ValueType>::Factory,
                gko::solver::Cg<ValueType>> {
