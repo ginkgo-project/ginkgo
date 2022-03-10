080a620bb56d7d4d7d132f47ca2047be5dc6e17f
Yuhsiang M. Tsai <yhmtsai@gmail.com>
2022-03-10 20:56:50 +0100

    generated multigrid

---
+++ extension/resource_manager/include/resource_manager/solver/multigrid.hpp
@@ -0,0 +1,119 @@
+/*******************************<GINKGO LICENSE>******************************
+Copyright (c) 2017-2022, the Ginkgo authors
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+1. Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+contributors may be used to endorse or promote products derived from
+this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+******************************<GINKGO LICENSE>*******************************/
+
+#ifndef GKO_PUBLIC_EXT_RESOURCE_MANAGER_SOLVER_MULTIGRID_HPP_
+#define GKO_PUBLIC_EXT_RESOURCE_MANAGER_SOLVER_MULTIGRID_HPP_
+
+
+#include <type_traits>
+
+
+#include "resource_manager/base/element_types.hpp"
+#include "resource_manager/base/helper.hpp"
+#include "resource_manager/base/macro_helper.hpp"
+#include "resource_manager/base/rapidjson_helper.hpp"
+#include "resource_manager/base/resource_manager.hpp"
+#include "resource_manager/base/type_list.hpp"
+
+
+namespace gko {
+namespace extension {
+namespace resource_manager {
+
+
+// TODO: Please add the corresponding to the resource_manager/base/types.hpp
+// Add _expand(MultigridFactory) to ENUM_LINOPFACTORY
+// Add _expand(Multigrid) to ENUM_LINOP
+// If need to override the generated enum for RM, use RM_CLASS or
+// RM_CLASS_FACTORY env and rerun the generated script. Or replace the
+// (RM_LinOpFactory::)MultigridFactory and (RM_LinOp::)Multigrid and their snake
+// case in IMPLEMENT_BRIDGE, ENABLE_SELECTION, *_select, ...
+
+
+template <>
+struct Generic<typename gko::solver::Multigrid::Factory,
+               gko::solver::Multigrid> {
+    using type = std::shared_ptr<typename gko::solver::Multigrid::Factory>;
+    static type build(rapidjson::Value& item,
+                      std::shared_ptr<const Executor> exec,
+                      std::shared_ptr<const LinOp> linop,
+                      ResourceManager* manager)
+    {
+        auto ptr = [&]() {
+            BUILD_FACTORY(gko::solver::Multigrid, manager, item, exec, linop);
+            SET_POINTER_VECTOR(const stop::CriterionFactory, criteria);
+            SET_POINTER_VECTOR(const gko::LinOpFactory, mg_level);
+            /* TODO: please create a map level_selector_map to handle the
+             * function size_type(const size_type, const LinOp*)*/
+            SET_FUNCTION(
+                std::function<size_type(const size_type, const LinOp*)>,
+                level_selector);
+            SET_POINTER_VECTOR(const LinOpFactory, pre_smoother);
+            SET_POINTER_VECTOR(const LinOpFactory, post_smoother);
+            SET_POINTER_VECTOR(const LinOpFactory, mid_smoother);
+            SET_VALUE(bool, post_uses_pre);
+            SET_VALUE(multigrid::mid_smooth_type, mid_case);
+            SET_VALUE(size_type, max_levels);
+            SET_VALUE(size_type, min_coarse_rows);
+            SET_POINTER_VECTOR(const LinOpFactory, coarsest_solver);
+            /* TODO: please create a map solver_selector_map to handle the
+             * function size_type(const size_type, const LinOp*)*/
+            SET_FUNCTION(
+                std::function<size_type(const size_type, const LinOp*)>,
+                solver_selector);
+            SET_VALUE(multigrid::cycle, cycle);
+            SET_VALUE(size_type, kcycle_base);
+            SET_VALUE(double, kcycle_rel_tol);
+            SET_VALUE(std::complex<double>, smoother_relax);
+            SET_VALUE(size_type, smoother_iters);
+            SET_VALUE(bool, zero_guess);
+            SET_EXECUTOR;
+        }();
+        return std::move(ptr);
+    }
+};
+
+
+SIMPLE_LINOP_WITH_FACTORY_IMPL_BASE(gko::solver::Multigrid);
+
+
+IMPLEMENT_BRIDGE(RM_LinOpFactory, MultigridFactory,
+                 gko::solver::Multigrid::Factory);
+IMPLEMENT_BRIDGE(RM_LinOp, Multigrid, gko::solver::Multigrid);
+
+
+}  // namespace resource_manager
+}  // namespace extension
+}  // namespace gko
+
+
+#endif  // GKO_PUBLIC_EXT_RESOURCE_MANAGER_SOLVER_MULTIGRID_HPP_
