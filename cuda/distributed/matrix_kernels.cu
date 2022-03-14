aa9eea50b2e700cc767743b7e70169a0d3202bc8
Marcel Koch <marcel.koch@kit.edu>
2022-03-14 16:28:54 +0100

    allows specifying different partition for rows and cols
    this also allows to create non-squared distributed matrices. as a downside, the user must now be more careful when applying vectors to a distributed matrix. the input vector must be build with the column partition, while the output vector must be build with the row partition.

--- cuda/distributed/matrix_kernels.cu
+++ cuda/distributed/matrix_kernels.cu
@@ -46,7 +46,10 @@ template <typename ValueType, typename LocalIndexType, typename GlobalIndexType>
 void build_diag_offdiag(
     std::shared_ptr<const DefaultExecutor> exec,
     const device_matrix_data<ValueType, GlobalIndexType>& input,
-    const distributed::Partition<LocalIndexType, GlobalIndexType>* partition,
+    const distributed::Partition<LocalIndexType, GlobalIndexType>*
+        row_partition,
+    const distributed::Partition<LocalIndexType, GlobalIndexType>*
+        col_partition,
     comm_index_type local_part,
     device_matrix_data<ValueType, LocalIndexType>& diag_data,
     device_matrix_data<ValueType, LocalIndexType>& offdiag_data,
--- omp/distributed/matrix_kernels.cpp
+++ omp/distributed/matrix_kernels.cpp
@@ -54,7 +54,10 @@ template <typename ValueType, typename LocalIndexType, typename GlobalIndexType>
 void build_diag_offdiag(
     std::shared_ptr<const DefaultExecutor> exec,
     const device_matrix_data<ValueType, GlobalIndexType>& input,
-    const distributed::Partition<LocalIndexType, GlobalIndexType>* partition,
+    const distributed::Partition<LocalIndexType, GlobalIndexType>*
+        row_partition,
+    const distributed::Partition<LocalIndexType, GlobalIndexType>*
+        col_partition,
     comm_index_type local_part,
     device_matrix_data<ValueType, LocalIndexType>& diag_data,
     device_matrix_data<ValueType, LocalIndexType>& offdiag_data,
@@ -67,17 +70,17 @@ void build_diag_offdiag(
     using range_index_type = GlobalIndexType;
     using global_nonzero = matrix_data_entry<ValueType, GlobalIndexType>;
     using local_nonzero = matrix_data_entry<ValueType, LocalIndexType>;
-    auto range_bounds = partition->get_range_bounds();
-    auto part_ids = partition->get_part_ids();
-    auto range_starting_indices = partition->get_range_starting_indices();
-    auto num_parts = partition->get_num_parts();
-    auto num_ranges = partition->get_num_ranges();
+    auto row_part_ids = row_partition->get_part_ids();
+    auto col_part_ids = col_partition->get_part_ids();
+    auto num_parts = row_partition->get_num_parts();
     size_type row_range_id_hint = 0;
     size_type col_range_id_hint = 0;
     // zero recv_offsets values
     std::fill_n(recv_offsets, num_parts + 1, comm_index_type{});
 
-    auto find_range = [&](GlobalIndexType idx, size_type hint) {
+    auto find_row_range = [&](GlobalIndexType idx, size_type hint) {
+        auto range_bounds = row_partition->get_range_bounds();
+        auto num_ranges = row_partition->get_num_ranges();
         if (range_bounds[hint] <= idx && idx < range_bounds[hint + 1]) {
             return hint;
         } else {
@@ -86,8 +89,30 @@ void build_diag_offdiag(
             return static_cast<size_type>(std::distance(range_bounds + 1, it));
         }
     };
-    auto map_to_local = [&](GlobalIndexType idx,
-                            size_type range_id) -> LocalIndexType {
+    auto map_to_local_row = [&](GlobalIndexType idx,
+                                size_type range_id) -> LocalIndexType {
+        auto range_bounds = row_partition->get_range_bounds();
+        auto range_starting_indices =
+            row_partition->get_range_starting_indices();
+        return static_cast<LocalIndexType>(idx - range_bounds[range_id]) +
+               range_starting_indices[range_id];
+    };
+    auto find_col_range = [&](GlobalIndexType idx, size_type hint) {
+        auto range_bounds = col_partition->get_range_bounds();
+        auto num_ranges = col_partition->get_num_ranges();
+        if (range_bounds[hint] <= idx && idx < range_bounds[hint + 1]) {
+            return hint;
+        } else {
+            auto it = std::upper_bound(range_bounds + 1,
+                                       range_bounds + num_ranges + 1, idx);
+            return static_cast<size_type>(std::distance(range_bounds + 1, it));
+        }
+    };
+    auto map_to_local_col = [&](GlobalIndexType idx,
+                                size_type range_id) -> LocalIndexType {
+        auto range_bounds = col_partition->get_range_bounds();
+        auto range_starting_indices =
+            col_partition->get_range_starting_indices();
         return static_cast<LocalIndexType>(idx - range_bounds[range_id]) +
                range_starting_indices[range_id];
     };
@@ -119,18 +144,19 @@ void build_diag_offdiag(
             const auto global_row = input_row_idxs[i];
             const auto global_col = input_col_idxs[i];
             const auto value = input_vals[i];
-            auto row_range_id = find_range(global_row, row_range_id_hint);
+            auto row_range_id = find_row_range(global_row, row_range_id_hint);
             row_range_id_hint = row_range_id;
             // skip non-local rows
-            if (part_ids[row_range_id] == local_part) {
+            if (row_part_ids[row_range_id] == local_part) {
                 // map to part-local indices
-                auto local_row = map_to_local(global_row, row_range_id);
+                auto local_row = map_to_local_row(global_row, row_range_id);
 
-                auto col_range_id = find_range(global_col, col_range_id_hint);
+                auto col_range_id =
+                    find_col_range(global_col, col_range_id_hint);
                 col_range_id_hint = col_range_id;
-                if (part_ids[col_range_id] == local_part) {
+                if (col_part_ids[col_range_id] == local_part) {
                     // store diagonal entry
-                    auto local_col = map_to_local(global_col, col_range_id);
+                    auto local_col = map_to_local_col(global_col, col_range_id);
                     thread_diag_entries.emplace_back(local_row, local_col,
                                                      value);
                 } else {
@@ -169,20 +195,22 @@ void build_diag_offdiag(
         // write back the local data to the output ranges
         auto diag = diag_entry_offsets[thread_id];
         auto offdiag = offdiag_entry_offsets[thread_id];
-        for (auto& entry : thread_diag_entries) {
+        for (const auto& entry : thread_diag_entries) {
             diag_entries[diag] = entry;
             diag++;
         }
-        for (auto& entry : thread_offdiag_entries) {
+        for (const auto& entry : thread_offdiag_entries) {
             global_offdiag_entries[offdiag] = entry;
             offdiag++;
         }
     }
     // store diagonal data to output
-    const auto diag_size =
-        static_cast<size_type>(partition->get_part_size(local_part));
+    const auto num_diag_rows =
+        static_cast<size_type>(row_partition->get_part_size(local_part));
+    const auto num_diag_cols =
+        static_cast<size_type>(col_partition->get_part_size(local_part));
     diag_data = std::move(device_matrix_data<ValueType, LocalIndexType>(
-        exec, gko::dim<2>{diag_size, diag_size},
+        exec, gko::dim<2>{num_diag_rows, num_diag_cols},
         static_cast<size_type>(diag_entries.size())));
 #pragma omp parallel for
     for (size_type i = 0; i < diag_entries.size(); ++i) {
@@ -193,27 +221,27 @@ void build_diag_offdiag(
     }
 
     // count off-diagonal columns per part
-    for (auto entry : offdiag_cols) {
+    for (const auto& entry : offdiag_cols) {
         auto col_range_id = entry.second;
-        recv_offsets[part_ids[col_range_id]]++;
+        recv_offsets[col_part_ids[col_range_id]]++;
     }
     components::prefix_sum(exec, recv_offsets, num_parts + 1);
+
+    // collect and renumber offdiagonal columns
     const auto num_ghost_elems =
         static_cast<size_type>(recv_offsets[num_parts]);
     local_gather_idxs.resize_and_reset(num_ghost_elems);
     std::unordered_map<GlobalIndexType, LocalIndexType> offdiag_global_to_local;
-    // collect and renumber offdiagonal columns
-    for (auto entry : offdiag_cols) {
+    for (const auto& entry : offdiag_cols) {
         auto range = entry.second;
-        auto range_begin = range_bounds[range];
-        auto starting_index = range_starting_indices[range];
-        auto part = part_ids[range];
+        auto part = col_part_ids[range];
         auto idx = recv_offsets[part];
-        local_gather_idxs.get_data()[idx] = static_cast<comm_index_type>(
-            entry.first - range_begin + starting_index);
+        local_gather_idxs.get_data()[idx] =
+            map_to_local_col(entry.first, entry.second);
         offdiag_global_to_local[entry.first] = idx;
         ++recv_offsets[part];
     }
+
     // build local-to-global map for offdiag columns
     local_to_global_ghost.resize_and_reset(num_ghost_elems);
     std::fill_n(local_to_global_ghost.get_data(),
@@ -224,14 +252,16 @@ void build_diag_offdiag(
         const auto local_idx = key_value.second;
         local_to_global_ghost.get_data()[local_idx] = global_idx;
     }
+
     // shift recv_offsets to the back, insert 0 in front again
     LocalIndexType local_prev{};
     for (size_type i = 0; i <= num_parts; i++) {
         recv_offsets[i] = std::exchange(local_prev, recv_offsets[i]);
     }
+
     // map off-diag values to local column indices
     offdiag_data = std::move(device_matrix_data<ValueType, LocalIndexType>(
-        exec, gko::dim<2>{diag_size, num_ghost_elems},
+        exec, gko::dim<2>{num_diag_rows, num_ghost_elems},
         static_cast<size_type>(global_offdiag_entries.size())));
 #pragma omp parallel for
     for (size_type i = 0; i < global_offdiag_entries.size(); i++) {
