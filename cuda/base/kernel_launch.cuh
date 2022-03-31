4aa7d02c161b9c41ab18fda4dca335c263ae87c5
Tobias Ribizel <ribizel@kit.edu>
2022-03-31 12:59:46 +0200

    unify HIP/CUDA common kernels

--- cuda/base/kernel_launch.cuh
+++ cuda/base/kernel_launch.cuh
@@ -54,56 +54,7 @@ namespace device_std = thrust;
 constexpr int default_block_size = 512;
 
 
-template <typename KernelFunction, typename... KernelArgs>
-__global__ __launch_bounds__(default_block_size) void generic_kernel_1d(
-    int64 size, KernelFunction fn, KernelArgs... args)
-{
-    auto tidx = thread::get_thread_id_flat<int64>();
-    if (tidx >= size) {
-        return;
-    }
-    fn(tidx, args...);
-}
-
-
-template <typename KernelFunction, typename... KernelArgs>
-__global__ __launch_bounds__(default_block_size) void generic_kernel_2d(
-    int64 rows, int64 cols, KernelFunction fn, KernelArgs... args)
-{
-    auto tidx = thread::get_thread_id_flat<int64>();
-    auto col = tidx % cols;
-    auto row = tidx / cols;
-    if (row >= rows) {
-        return;
-    }
-    fn(row, col, args...);
-}
-
-
-template <typename KernelFunction, typename... KernelArgs>
-void run_kernel(std::shared_ptr<const CudaExecutor> exec, KernelFunction fn,
-                size_type size, KernelArgs&&... args)
-{
-    if (size > 0) {
-        constexpr auto block_size = default_block_size;
-        auto num_blocks = ceildiv(size, block_size);
-        generic_kernel_1d<<<num_blocks, block_size>>>(
-            static_cast<int64>(size), fn, map_to_device(args)...);
-    }
-}
-
-template <typename KernelFunction, typename... KernelArgs>
-void run_kernel(std::shared_ptr<const CudaExecutor> exec, KernelFunction fn,
-                dim<2> size, KernelArgs&&... args)
-{
-    if (size[0] > 0 && size[1] > 0) {
-        constexpr auto block_size = default_block_size;
-        auto num_blocks = ceildiv(size[0] * size[1], block_size);
-        generic_kernel_2d<<<num_blocks, block_size>>>(
-            static_cast<int64>(size[0]), static_cast<int64>(size[1]), fn,
-            map_to_device(args)...);
-    }
-}
+#include "common/cuda_hip/base/kernel_launch.hpp.inc"
 
 
 }  // namespace cuda
