fa3d04e6daad74ffbeb5df00cac236adad884be3
Aditya Kashi <aditya.kashi@kit.edu>
2022-04-06 17:31:41 +0200

    added missing omp batch solver kernels and fixed tests

--- hip/solver/batch_cg_kernels.hip.cpp
+++ hip/solver/batch_cg_kernels.hip.cpp
@@ -115,15 +115,15 @@ private:
 template <typename ValueType>
 void apply(std::shared_ptr<const HipExecutor> exec,
            const BatchCgOptions<remove_complex<ValueType>>& opts,
-           const BatchLinOp* const a,
+           const BatchLinOp* const a, const BatchLinOp* const precon,
            const matrix::BatchDense<ValueType>* const b,
            matrix::BatchDense<ValueType>* const x,
            log::BatchLogData<ValueType>& logdata)
 {
     using d_value_type = hip_type<ValueType>;
     auto dispatcher = batch_solver::create_dispatcher<ValueType>(
-        KernelCaller<d_value_type>(exec, opts), opts);
-    dispatcher.apply(a, b, x, logdata);
+        KernelCaller<d_value_type>(exec, opts), opts, a, precon);
+    dispatcher.apply(b, x, logdata);
 }
 
 GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE(GKO_DECLARE_BATCH_CG_APPLY_KERNEL);
