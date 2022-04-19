8312ccbe192f2e26449ce40f3d0a7da2bd5008a9
Claudius Holeksa <mail@keldu.de>
2022-04-19 12:42:09 +0200

    simple word splitting globbing changes

--- dev_tools/scripts/mkfavicon.sh
+++ dev_tools/scripts/mkfavicon.sh
@@ -3,4 +3,4 @@
 INPUT_IMAGE=$1
 SIZES=256,128,64,48,32,16
 
-convert ${INPUT_IMAGE} -define icon:auto-resize=$SIZES favicon.ico
+convert "${INPUT_IMAGE}" -define icon:auto-resize="$SIZES" favicon.ico
