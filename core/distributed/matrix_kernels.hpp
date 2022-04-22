0ed4cfccdf40971f8c45623bbe685459009a104d
Marcel Koch <marcel.koch@kit.edu>
2022-04-22 10:13:21 +0200

    use arrays instead of device_matrix_data in read_distributed kernels

--- core/distributed/matrix_kernels.hpp
+++ core/distributed/matrix_kernels.hpp
@@ -48,20 +48,22 @@ namespace gko {
 namespace kernels {
 
 
-#define GKO_DECLARE_BUILD_DIAG_OFFDIAG(ValueType, LocalIndexType,      \
-                                       GlobalIndexType)                \
-    void build_diag_offdiag(                                           \
-        std::shared_ptr<const DefaultExecutor> exec,                   \
-        const device_matrix_data<ValueType, GlobalIndexType>& input,   \
-        const distributed::Partition<LocalIndexType, GlobalIndexType>* \
-            row_partition,                                             \
-        const distributed::Partition<LocalIndexType, GlobalIndexType>* \
-            col_partition,                                             \
-        comm_index_type local_part,                                    \
-        device_matrix_data<ValueType, LocalIndexType>& diag_data,      \
-        device_matrix_data<ValueType, LocalIndexType>& offdiag_data,   \
-        Array<LocalIndexType>& local_gather_idxs,                      \
-        comm_index_type* recv_offsets,                                 \
+#define GKO_DECLARE_BUILD_DIAG_OFFDIAG(ValueType, LocalIndexType,            \
+                                       GlobalIndexType)                      \
+    void build_diag_offdiag(                                                 \
+        std::shared_ptr<const DefaultExecutor> exec,                         \
+        const device_matrix_data<ValueType, GlobalIndexType>& input,         \
+        const distributed::Partition<LocalIndexType, GlobalIndexType>*       \
+            row_partition,                                                   \
+        const distributed::Partition<LocalIndexType, GlobalIndexType>*       \
+            col_partition,                                                   \
+        comm_index_type local_part, Array<LocalIndexType>& diag_row_idxs,    \
+        Array<LocalIndexType>& diag_col_idxs, Array<ValueType>& diag_values, \
+        Array<LocalIndexType>& offdiag_row_idxs,                             \
+        Array<LocalIndexType>& offdiag_col_idxs,                             \
+        Array<ValueType>& offdiag_values,                                    \
+        Array<LocalIndexType>& local_gather_idxs,                            \
+        comm_index_type* recv_offsets,                                       \
         Array<GlobalIndexType>& local_to_global_ghost)
 
 
