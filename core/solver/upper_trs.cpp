dd7676ccd21d5c97b3c84bec115310b7d3296f61
Fritz Goebel <fritz.goebel@kit.edu>
2022-05-12 17:08:36 +0200

    Add mixed precision dispatch for advanced apply

--- core/solver/upper_trs.cpp
+++ core/solver/upper_trs.cpp
@@ -192,14 +192,17 @@ void UpperTrs<ValueType, IndexType>::apply_impl(const LinOp* alpha,
     if (!this->get_system_matrix()) {
         return;
     }
-    precision_dispatch_real_complex<ValueType>(
-        [this](auto dense_alpha, auto dense_b, auto dense_beta, auto dense_x) {
+    mixed_precision_dispatch_real_complex<ValueType>(
+        [this, alpha, beta](auto dense_b, auto dense_x) {
+            auto dense_alpha = make_temporary_conversion<ValueType>(alpha);
+            auto dense_beta = make_temporary_conversion<
+                typename std::decay_t<decltype(*dense_x)>::value_type>(beta);
             auto x_clone = dense_x->clone();
             this->apply_impl(dense_b, x_clone.get());
-            dense_x->scale(dense_beta);
-            dense_x->add_scaled(dense_alpha, x_clone.get());
+            dense_x->scale(dense_beta.get());
+            dense_x->add_scaled(dense_alpha.get(), x_clone.get());
         },
-        alpha, b, beta, x);
+        b, x);
 }
 
 
