8891a49920a16ddab21e31e115683ea502044b46
Gregor Olenik <go@hpsim.de>
2022-05-24 16:30:36 +0200

    fixup

--- core/distributed/vector.cpp
+++ core/distributed/vector.cpp
@@ -456,7 +456,7 @@ void Vector<ValueType>::compute_average_unsafe(LinOp* result) const
 
     local_sum.fill(ValueType{0});
 
-    auto local_view = array_const_cast(Array<ValueType>::const_view(
+    const auto local_view = array_const_cast(Array<ValueType>::const_view(
         exec, num_local_rows, this->get_const_local_values()));
 
     reduce_add(local_view, local_sum);
