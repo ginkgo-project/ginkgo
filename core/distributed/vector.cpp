bb0c3250a3433e1defcfe9fb03c20024aaa8c776
Marcel Koch <marcel.koch@kit.edu>
2022-06-23 09:10:25 +0200

    add runtime mpi buffer switch to vector

--- core/distributed/vector.cpp
+++ core/distributed/vector.cpp
@@ -326,7 +326,7 @@ void Vector<ValueType>::compute_dot(const LinOp* b, LinOp* result,
     exec->synchronize();
     auto use_host_buffer =
         exec->get_master() != exec && !gko::mpi::is_gpu_aware();
-    if (use_host_buffer) {
+    if (use_host_buffer || comm.force_host_buffer()) {
         host_reduction_buffer_.init(exec->get_master(), dense_res->get_size());
         host_reduction_buffer_->copy_from(dense_res.get());
         comm.all_reduce(exec->get_master(),
@@ -362,7 +362,7 @@ void Vector<ValueType>::compute_conj_dot(const LinOp* b, LinOp* result,
     exec->synchronize();
     auto use_host_buffer =
         exec->get_master() != exec && !gko::mpi::is_gpu_aware();
-    if (use_host_buffer) {
+    if (use_host_buffer || comm.force_host_buffer()) {
         host_reduction_buffer_.init(exec->get_master(), dense_res->get_size());
         host_reduction_buffer_->copy_from(dense_res.get());
         comm.all_reduce(exec->get_master(),
@@ -397,7 +397,7 @@ void Vector<ValueType>::compute_norm2(LinOp* result, array<char>& tmp) const
     exec->synchronize();
     auto use_host_buffer =
         exec->get_master() != exec && !gko::mpi::is_gpu_aware();
-    if (use_host_buffer) {
+    if (use_host_buffer || comm.force_host_buffer()) {
         host_norm_buffer_.init(exec->get_master(), dense_res->get_size());
         host_norm_buffer_->copy_from(dense_res.get());
         comm.all_reduce(exec->get_master(), host_norm_buffer_->get_values(),
@@ -431,7 +431,7 @@ void Vector<ValueType>::compute_norm1(LinOp* result, array<char>& tmp) const
     exec->synchronize();
     auto use_host_buffer =
         exec->get_master() != exec && !gko::mpi::is_gpu_aware();
-    if (use_host_buffer) {
+    if (use_host_buffer || comm.force_host_buffer()) {
         host_norm_buffer_.init(exec->get_master(), dense_res->get_size());
         host_norm_buffer_->copy_from(dense_res.get());
         comm.all_reduce(exec->get_master(), host_norm_buffer_->get_values(),
