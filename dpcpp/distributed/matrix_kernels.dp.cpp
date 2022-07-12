a021c8775d79fa13db1b6f9a2c99935c2cb35b3c
Marcel Koch <marcel.koch@kit.edu>
2022-07-12 10:55:18 +0200

    review updates:
    - renaming
    - remove unnecessary includes
    
    Co-authored-by: Terry Cojean <terry.cojean@kit.edu>

--- dpcpp/distributed/matrix_kernels.dp.cpp
+++ dpcpp/distributed/matrix_kernels.dp.cpp
@@ -33,15 +33,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "core/distributed/matrix_kernels.hpp"
 
 
-#include <oneapi/dpl/algorithm>
-#include <oneapi/dpl/execution>
-#include <oneapi/dpl/iterator>
-
-
 #include <ginkgo/core/base/exception_helpers.hpp>
-
-
-#include "dpcpp/components/atomic.dp.hpp"
 
 
 namespace gko {
