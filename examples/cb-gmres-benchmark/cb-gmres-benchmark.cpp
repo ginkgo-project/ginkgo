a8f2859fcdbe2fa6388a39f7757121c16de07527
Thomas Grützmacher <thomas.gruetzmacher@kit.edu>
2022-08-17 23:34:21 +0200

    Extend example to make it extendable
    It is now possible to use a matrix file and to run even more
    solver variation without much additional code.

--- examples/cb-gmres-benchmark/cb-gmres-benchmark.cpp
+++ examples/cb-gmres-benchmark/cb-gmres-benchmark.cpp
@@ -41,17 +41,59 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <string>
 
 
+struct solver_settings {
+    unsigned krylov_dim;
+    unsigned stop_iter;
+    double stop_rel_res;
+    gko::solver::cb_gmres::storage_precision storage_prec;
+};
+
+struct solver_result {
+    unsigned iters;
+    double time_s;
+    double res_norm;
+};
+
+
 // Helper function which measures the time of `solver->apply(b, x)` in seconds
 // To get an accurate result, the solve is repeated multiple times (while
 // ensuring the initial guess is always the same). The result of the solve will
 // be written to x.
-double measure_solve_time_in_s(const gko::Executor* exec, gko::LinOp* solver,
-                               const gko::LinOp* b, gko::LinOp* x)
+template <typename ValueType>
+solver_result benchmark_solver(
+    std::shared_ptr<const gko::Executor> exec, solver_settings s_s,
+    std::shared_ptr<gko::matrix::Csr<ValueType, int>> A,
+    const gko::matrix::Dense<ValueType>* b,
+    const gko::matrix::Dense<ValueType>* x)
 {
+    using RealValueType = gko::remove_complex<ValueType>;
+    using vec = gko::matrix::Dense<ValueType>;
+    using real_vec = gko::matrix::Dense<RealValueType>;
     constexpr int repeats{1};
     double duration{0};
     // Make a copy of x, so we can re-use the same initial guess multiple times
-    auto x_copy = clone(x);
+    auto x_copy = x->clone();
+
+    auto iter_stop = gko::share(
+        gko::stop::Iteration::build().with_max_iters(s_s.stop_iter).on(exec));
+    auto tol_stop = gko::share(gko::stop::ResidualNorm<ValueType>::build()
+                                   .with_reduction_factor(s_s.stop_rel_res)
+                                   .on(exec));
+    std::shared_ptr<const gko::log::Convergence<ValueType>> logger =
+        gko::log::Convergence<ValueType>::create();
+    iter_stop->add_logger(logger);
+    tol_stop->add_logger(logger);
+
+    // Create solver:
+    auto solver_gen = gko::solver::CbGmres<ValueType>::build()
+                          .with_criteria(iter_stop, tol_stop)
+                          .with_krylov_dim(s_s.krylov_dim)
+                          .with_storage_precision(s_s.storage_prec)
+                          .on(exec);
+
+    // Generate the actual solver from the factory and the matrix.
+    auto solver = solver_gen->generate(A);
+
     for (int i = 0; i < repeats; ++i) {
         // No need to copy it in the first iteration
         if (i != 0) {
@@ -68,8 +110,28 @@ double measure_solve_time_in_s(const gko::Executor* exec, gko::LinOp* solver,
         duration += std::chrono::duration<double>(tac - tic).count();
     }
     // Copy the solution back to x, so the caller has the result
-    x->copy_from(x_copy);
-    return duration / static_cast<double>(repeats);
+    // x->copy_from(x_copy);
+
+    // To measure if your solution has actually converged, the error of the
+    // solution is measured.
+    // one, neg_one are objects that represent the numbers which allow for a
+    // uniform interface when computing on any device. To compute the residual,
+    // the (advanced) apply method is used.
+    auto one = gko::initialize<vec>({1.0}, exec);
+    auto neg_one = gko::initialize<vec>({-1.0}, exec);
+
+    auto res_norm = gko::initialize<real_vec>({0.0}, exec);
+    auto tmp = gko::clone(b);
+
+    // tmp = Ax - tmp
+    A->apply(one, x_copy, neg_one, tmp);
+    tmp->compute_norm2(res_norm);
+
+    solver_result result{};
+    result.iters = logger->get_num_iterations();
+    result.time_s = duration / static_cast<double>(repeats);
+    result.res_norm = exec->copy_val_to_host(tmp->get_const_values());
+    return result;
 }
 
 
@@ -101,8 +163,8 @@ int main(int argc, char* argv[])
     }
 
     // Map which generates the appropriate executor
-    const auto executor_string = argc >= 2 ? argv[1] : "reference";
-    const auto matrix_string = "data/A.mtx";
+    const auto executor_string = "omp";  // argc >= 2 ? argv[1] : "omp";
+    const auto matrix_string = argc >= 2 ? argv[1] : "data/A.mtx";
     std::map<std::string, std::function<std::shared_ptr<gko::Executor>()>>
         exec_map{
             {"omp", [] { return gko::OmpExecutor::create(); }},
@@ -144,96 +206,31 @@ int main(int argc, char* argv[])
     auto b = clone(exec, b_host);
 
     // As an initial guess, use the right-hand side
-    auto x_keep = clone(b);
-    auto x_reduce = clone(x_keep);
+    auto x = clone(b);
 
-    const RealValueType reduction_factor{1e-6};
-    const unsigned int num_iters{20000u};
-
-    auto iter_stop_keep = gko::share(
-        gko::stop::Iteration::build().with_max_iters(num_iters).on(exec));
-    auto tol_stop_keep = gko::share(gko::stop::ResidualNorm<ValueType>::build()
-                                        .with_reduction_factor(reduction_factor)
-                                        .on(exec));
-    std::shared_ptr<const gko::log::Convergence<ValueType>> logger_keep =
-        gko::log::Convergence<ValueType>::create();
-    iter_stop_keep->add_logger(logger_keep);
-    tol_stop_keep->add_logger(logger_keep);
-
-    auto iter_stop_reduce = gko::share(
-        gko::stop::Iteration::build().with_max_iters(num_iters).on(exec));
-    auto tol_stop_reduce =
-        gko::share(gko::stop::ResidualNorm<ValueType>::build()
-                       .with_reduction_factor(reduction_factor)
-                       .on(exec));
-    std::shared_ptr<const gko::log::Convergence<ValueType>> logger_reduce =
-        gko::log::Convergence<ValueType>::create();
-    iter_stop_reduce->add_logger(logger_reduce);
-    tol_stop_reduce->add_logger(logger_reduce);
-
-    // Generate two solver factories: `_keep` uses the same precision for the
-    // krylov basis as the matrix, and `_reduce` uses one precision below it.
-    // If `ValueType` is double, then `_reduce` uses float as the krylov basis
-    // storage type
-    auto solver_gen_keep =
-        cb_gmres::build()
-            .with_criteria(iter_stop_keep, tol_stop_keep)
-            .with_krylov_dim(100u)
-            .with_storage_precision(
-                gko::solver::cb_gmres::storage_precision::keep)
-            .on(exec);
-
-    auto solver_gen_reduce =
-        cb_gmres::build()
-            .with_criteria(iter_stop_reduce, tol_stop_reduce)
-            .with_krylov_dim(100u)
-            .with_storage_precision(
-                gko::solver::cb_gmres::storage_precision::use_sz)  // reduce1)
-            .on(exec);
-    // Generate the actual solver from the factory and the matrix.
-    auto solver_keep = solver_gen_keep->generate(A);
-    auto solver_reduce = solver_gen_reduce->generate(A);
+    solver_settings s_s{};
+    s_s.stop_iter = 20000u;
+    s_s.stop_rel_res = 1e-6;
+    s_s.krylov_dim = 100u;
+    s_s.storage_prec = gko::solver::cb_gmres::storage_precision::keep;
 
     // Solve both system and measure the time for each.
-    auto time_keep = measure_solve_time_in_s(exec.get(), solver_keep.get(),
-                                             b.get(), x_keep.get());
-    auto time_reduce = measure_solve_time_in_s(exec.get(), solver_reduce.get(),
-                                               b.get(), x_reduce.get());
+    auto keep = benchmark_solver(exec, s_s, A, b.get(), x.get());
+    s_s.storage_prec = gko::solver::cb_gmres::storage_precision::use_sz;
+    auto sz = benchmark_solver(exec, s_s, A, b.get(), x.get());
+    s_s.storage_prec = gko::solver::cb_gmres::storage_precision::reduce1;
+    auto reduce = benchmark_solver(exec, s_s, A, b.get(), x.get());
 
     // Make sure the output is in scientific notation for easier comparison
     std::cout << std::scientific;
     // Note: The time might not be significantly different since the matrix is
     //       quite small
-    std::cout << "Solve time without compression: " << time_keep << " s\n"
-              << "Solve time with compression:    " << time_reduce << " s\n";
-    std::cout << "Number iterations without compression: "
-              << logger_keep->get_num_iterations() << '\n'
-              << "Number iterations with compression:    "
-              << logger_reduce->get_num_iterations() << '\n';
+    std::cout << "Solve time without compression: " << keep.time_s << " s\n"
+              << "Solve time with compression:    " << sz.time_s << " s\n";
+    std::cout << "Number iterations without compression: " << keep.iters << '\n'
+              << "Number iterations with compression:    " << sz.iters << '\n';
 
-    // To measure if your solution has actually converged, the error of the
-    // solution is measured.
-    // one, neg_one are objects that represent the numbers which allow for a
-    // uniform interface when computing on any device. To compute the residual,
-    // the (advanced) apply method is used.
-    auto one = gko::initialize<vec>({1.0}, exec);
-    auto neg_one = gko::initialize<vec>({-1.0}, exec);
+    std::cout << "\nResidual norm without compression: " << keep.res_norm;
 
-    auto res_norm_keep = gko::initialize<real_vec>({0.0}, exec);
-    auto res_norm_reduce = gko::initialize<real_vec>({0.0}, exec);
-    auto tmp = gko::clone(b);
-
-    // tmp = Ax - tmp
-    A->apply(one, x_keep, neg_one, tmp);
-    tmp->compute_norm2(res_norm_keep);
-
-    std::cout << "\nResidual norm without compression:\n";
-    write(std::cout, res_norm_keep);
-
-    tmp->copy_from(b);
-    A->apply(one, x_reduce, neg_one, tmp);
-    tmp->compute_norm2(res_norm_reduce);
-
-    std::cout << "\nResidual norm with compression:\n";
-    write(std::cout, res_norm_reduce);
+    std::cout << "\nResidual norm with compression:    " << sz.res_norm << '\n';
 }
