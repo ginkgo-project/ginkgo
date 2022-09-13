4f64b1d7bdac12a64e49bd8f12a2b1bf00a21de9
aggarwal2000 <aggarwal2000chd@gmail.com>
2022-09-13 11:06:15 +0200

    Add cuda kernels for batch parilu external generation

--- cuda/preconditioner/batch_par_ilu_kernels.cu
+++ cuda/preconditioner/batch_par_ilu_kernels.cu
@@ -52,6 +52,8 @@ namespace {
 
 constexpr size_type default_block_size = 256;
 
+#include "common/cuda_hip/matrix/batch_vector_kernels.hpp.inc"
+#include "common/cuda_hip/preconditioner/batch_par_ilu_kernels.hpp.inc"
 //#include "common/cuda_hip/preconditioner/batch_par_ilu.hpp.inc"
 
 }  // namespace
@@ -62,8 +64,21 @@ void generate_common_pattern_to_fill_l_and_u(
     std::shared_ptr<const DefaultExecutor> exec,
     const matrix::Csr<ValueType, IndexType>* const first_sys_mat,
     const IndexType* const l_row_ptrs, const IndexType* const u_row_ptrs,
-    IndexType* const l_col_holders,
-    IndexType* const u_col_holders) GKO_NOT_IMPLEMENTED;
+    IndexType* const l_col_holders, IndexType* const u_col_holders)
+{
+    const size_type num_rows = first_sys_mat->get_size()[0];
+    const size_type num_warps = num_rows;
+    const size_type num_blocks =
+        ceildiv(num_warps, ceildiv(default_block_size, config::warp_size));
+
+    generate_common_pattern_to_fill_L_and_U<<<num_blocks, default_block_size>>>(
+        static_cast<int>(num_rows), first_sys_mat->get_const_row_ptrs(),
+        first_sys_mat->get_const_col_idxs(), l_row_ptrs, u_row_ptrs,
+        l_col_holders, u_col_holders);
+
+    GKO_CUDA_LAST_IF_ERROR_THROW;
+}
+
 
 GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE_AND_INT32_INDEX(
     GKO_DECLARE_BATCH_PAR_ILU_GENERATE_COMMON_PATTERN_KERNEL);
@@ -75,8 +90,29 @@ void initialize_batch_l_and_batch_u(
     const matrix::BatchCsr<ValueType, IndexType>* const sys_mat,
     matrix::BatchCsr<ValueType, IndexType>* const l_factor,
     matrix::BatchCsr<ValueType, IndexType>* const u_factor,
-    const IndexType* const l_col_holders,
-    const IndexType* const u_col_holders) GKO_NOT_IMPLEMENTED;
+    const IndexType* const l_col_holders, const IndexType* const u_col_holders)
+{
+    const auto num_rows = static_cast<int>(sys_mat->get_size().at(0)[0]);
+    const auto nbatch = sys_mat->get_num_batch_entries();
+    const auto nnz =
+        static_cast<int>(sys_mat->get_num_stored_elements() / nbatch);
+    const auto l_nnz =
+        static_cast<int>(l_factor->get_num_stored_elements() / nbatch);
+    const auto u_nnz =
+        static_cast<int>(u_factor->get_num_stored_elements() / nbatch);
+    const int greater_nnz = l_nnz > u_nnz ? l_nnz : u_nnz;
+    const size_type grid_fill_LU =
+        ceildiv(greater_nnz * nbatch, default_block_size);
+
+    fill_L_and_U<<<grid_fill_LU, default_block_size>>>(
+        nbatch, num_rows, nnz, sys_mat->get_const_col_idxs(),
+        as_cuda_type(sys_mat->get_const_values()), l_nnz,
+        l_factor->get_col_idxs(), as_cuda_type(l_factor->get_values()),
+        l_col_holders, u_nnz, u_factor->get_col_idxs(),
+        as_cuda_type(u_factor->get_values()), u_col_holders);
+
+    GKO_CUDA_LAST_IF_ERROR_THROW;
+}
 
 GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE_AND_INT32_INDEX(
     GKO_DECLARE_BATCH_PAR_ILU_INITIALIZE_BATCH_L_AND_BATCH_U);
@@ -88,7 +124,30 @@ void compute_par_ilu0(
     matrix::BatchCsr<ValueType, IndexType>* const l_factor,
     matrix::BatchCsr<ValueType, IndexType>* const u_factor,
     const int num_sweeps, const IndexType* const dependencies,
-    const IndexType* const nz_ptrs) GKO_NOT_IMPLEMENTED;
+    const IndexType* const nz_ptrs)
+{
+    const auto num_rows = static_cast<int>(sys_mat->get_size().at(0)[0]);
+    const auto nbatch = sys_mat->get_num_batch_entries();
+    const auto nnz =
+        static_cast<int>(sys_mat->get_num_stored_elements() / nbatch);
+    const auto l_nnz =
+        static_cast<int>(l_factor->get_num_stored_elements() / nbatch);
+    const auto u_nnz =
+        static_cast<int>(u_factor->get_num_stored_elements() / nbatch);
+
+    const size_type dynamic_shared_mem_bytes =
+        (l_nnz + u_nnz) * sizeof(ValueType);
+
+    compute_parilu0_kernel<<<nbatch, default_block_size,
+                             dynamic_shared_mem_bytes>>>(
+        nbatch, num_rows, nnz, as_cuda_type(sys_mat->get_const_values()), l_nnz,
+        as_cuda_type(l_factor->get_values()), u_nnz,
+        as_cuda_type(u_factor->get_values()), num_sweeps, dependencies,
+        nz_ptrs);
+
+    GKO_CUDA_LAST_IF_ERROR_THROW;
+}
+
 
 GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE_AND_INT32_INDEX(
     GKO_DECLARE_BATCH_PAR_ILU_COMPUTE_PARILU0_KERNEL);
