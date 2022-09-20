f1f63d8250041b903bc38fafa7ab7b1213777e95
aggarwal2000 <aggarwal2000chd@gmail.com>
2022-09-20 11:21:35 +0200

    Add batch exact and parilu hip kernels

--- hip/test/preconditioner/batch_exact_ilu_kernels.cpp
+++ hip/test/preconditioner/batch_exact_ilu_kernels.cpp
@@ -39,9 +39,100 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <gtest/gtest.h>
 
 
+#include <ginkgo/core/factorization/ilu.hpp>
+#include <ginkgo/core/preconditioner/ilu.hpp>
+#include <ginkgo/core/solver/upper_trs.hpp>
+
+
+#include "core/factorization/factorization_kernels.hpp"
+#include "core/factorization/ilu_kernels.hpp"
 #include "core/preconditioner/batch_exact_ilu_kernels.hpp"
 #include "core/test/utils.hpp"
 #include "core/test/utils/batch.hpp"
 
 
-namespace {}  // namespace
+namespace {
+
+
+class BatchExactIlu : public ::testing::Test {
+protected:
+    using value_type = double;
+    using index_type = int;
+    using real_type = gko::remove_complex<value_type>;
+    using Mtx = gko::matrix::BatchCsr<value_type>;
+    using BDense = gko::matrix::BatchDense<value_type>;
+    using RBDense = gko::matrix::BatchDense<real_type>;
+    using prec_type = gko::preconditioner::BatchExactIlu<value_type>;
+
+    BatchExactIlu()
+        : ref(gko::ReferenceExecutor::create()),
+          d_exec(gko::HipExecutor::create(0, ref)),
+          mtx(gko::share(gko::test::generate_uniform_batch_random_matrix<Mtx>(
+              nbatch, nrows, nrows,
+              std::uniform_int_distribution<>(min_nnz_row, nrows),
+              std::normal_distribution<real_type>(0.0, 1.0), rand_engine, true,
+              ref)))
+    {}
+
+    std::shared_ptr<gko::ReferenceExecutor> ref;
+    std::shared_ptr<const gko::HipExecutor> d_exec;
+    std::ranlux48 rand_engine;
+
+    const size_t nbatch = 9;
+    const index_type nrows = 15;
+    const int min_nnz_row = 5;
+    std::shared_ptr<const Mtx> mtx;
+};
+
+
+TEST_F(BatchExactIlu, GenerateIsEquivalentToReference)
+{
+    auto d_mtx = gko::share(gko::clone(d_exec, mtx.get()));
+    auto prec_fact = prec_type::build().with_skip_sorting(true).on(ref);
+    auto d_prec_fact = prec_type::build().with_skip_sorting(true).on(d_exec);
+
+    auto prec = prec_fact->generate(mtx);
+    auto d_prec = d_prec_fact->generate(d_mtx);
+
+    const auto factorized_mat = prec->get_const_factorized_matrix();
+    const auto d_factorized_mat = d_prec->get_const_factorized_matrix();
+    const auto tol = 5000 * r<value_type>::value;
+    GKO_ASSERT_BATCH_MTX_NEAR(factorized_mat, d_factorized_mat, tol);
+}
+
+
+TEST_F(BatchExactIlu, ApplyIsEquivalentToReference)
+{
+    using BDense = gko::matrix::BatchDense<value_type>;
+
+    auto prec_fact = prec_type::build().with_skip_sorting(true).on(ref);
+    auto prec = prec_fact->generate(mtx);
+
+    const auto factorized_mat = prec->get_const_factorized_matrix();
+    const auto diag_locs = prec->get_const_diag_locations();
+
+    auto d_mtx = gko::share(gko::clone(d_exec, mtx.get()));
+    auto d_prec_fact = prec_type::build().with_skip_sorting(true).on(d_exec);
+    auto d_prec = d_prec_fact->generate(d_mtx);
+
+    const auto d_factorized_mat = d_prec->get_const_factorized_matrix();
+    const auto d_diag_locs = d_prec->get_const_diag_locations();
+
+    auto rv = gko::test::generate_uniform_batch_random_matrix<BDense>(
+        nbatch, nrows, 1, std::uniform_int_distribution<>(1, 1),
+        std::normal_distribution<real_type>(0.0, 1.0), rand_engine, false, ref);
+    auto z = BDense::create(ref, rv->get_size());
+    auto d_rv = gko::as<BDense>(gko::clone(d_exec, rv));
+    auto d_z = BDense::create(d_exec, rv->get_size());
+
+    gko::kernels::reference::batch_exact_ilu::apply_exact_ilu(
+        ref, factorized_mat, diag_locs, rv.get(), z.get());
+    gko::kernels::hip::batch_exact_ilu::apply_exact_ilu(
+        d_exec, d_factorized_mat, d_diag_locs, d_rv.get(), d_z.get());
+
+    const auto tol = 5000 * r<value_type>::value;
+    GKO_ASSERT_BATCH_MTX_NEAR(z, d_z, tol);
+}
+
+
+}  // namespace
--- hip/test/preconditioner/batch_par_ilu_kernels.cpp
+++ hip/test/preconditioner/batch_par_ilu_kernels.cpp
@@ -39,9 +39,109 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <gtest/gtest.h>
 
 
+#include <ginkgo/core/factorization/ilu.hpp>
+#include <ginkgo/core/preconditioner/ilu.hpp>
+#include <ginkgo/core/solver/upper_trs.hpp>
+
+
+#include "core/factorization/factorization_kernels.hpp"
+#include "core/factorization/ilu_kernels.hpp"
 #include "core/preconditioner/batch_par_ilu_kernels.hpp"
 #include "core/test/utils.hpp"
 #include "core/test/utils/batch.hpp"
 
 
-namespace {}  // namespace
+namespace {
+
+
+class BatchParIlu : public ::testing::Test {
+protected:
+    using value_type = double;
+    using index_type = int;
+    using real_type = gko::remove_complex<value_type>;
+    using Mtx = gko::matrix::BatchCsr<value_type>;
+    using BDense = gko::matrix::BatchDense<value_type>;
+    using RBDense = gko::matrix::BatchDense<real_type>;
+    using prec_type = gko::preconditioner::BatchParIlu<value_type>;
+
+    BatchParIlu()
+        : ref(gko::ReferenceExecutor::create()),
+          d_exec(gko::HipExecutor::create(0, ref)),
+          mtx(gko::share(gko::test::generate_uniform_batch_random_matrix<Mtx>(
+              nbatch, nrows, nrows,
+              std::uniform_int_distribution<>(min_nnz_row, nrows),
+              std::normal_distribution<real_type>(0.0, 1.0), rand_engine, true,
+              ref)))
+    {}
+
+    std::shared_ptr<gko::ReferenceExecutor> ref;
+    std::shared_ptr<const gko::HipExecutor> d_exec;
+    std::ranlux48 rand_engine;
+
+    const size_t nbatch = 2;
+    const index_type nrows = 7;
+    const int min_nnz_row = 3;
+    std::shared_ptr<const Mtx> mtx;
+};
+
+
+TEST_F(BatchParIlu, GenerateIsEquivalentToReference)
+{
+    auto d_mtx = gko::share(gko::clone(d_exec, mtx.get()));
+    auto prec_fact =
+        prec_type::build().with_skip_sorting(true).with_num_sweeps(1).on(ref);
+    auto d_prec_fact =
+        prec_type::build().with_skip_sorting(true).with_num_sweeps(50).on(
+            d_exec);
+
+    auto prec = prec_fact->generate(mtx);
+    auto d_prec = d_prec_fact->generate(d_mtx);
+
+    const auto lower_factor = prec->get_const_l_factor();
+    const auto upper_factor = prec->get_const_u_factor();
+    const auto d_lower_factor = d_prec->get_const_l_factor();
+    const auto d_upper_factor = d_prec->get_const_u_factor();
+    const auto tol = 1000 * r<value_type>::value;
+    GKO_ASSERT_BATCH_MTX_NEAR(lower_factor, d_lower_factor, tol);
+    GKO_ASSERT_BATCH_MTX_NEAR(upper_factor, d_upper_factor, tol);
+}
+
+
+TEST_F(BatchParIlu, ApplyIsEquivalentToReference)
+{
+    using BDense = gko::matrix::BatchDense<value_type>;
+
+    auto prec_fact =
+        prec_type::build().with_skip_sorting(true).with_num_sweeps(1).on(ref);
+    auto prec = prec_fact->generate(mtx);
+
+    const auto l_factor = prec->get_const_l_factor();
+    const auto u_factor = prec->get_const_u_factor();
+
+    auto d_mtx = gko::share(gko::clone(d_exec, mtx.get()));
+    auto d_prec_fact =
+        prec_type::build().with_skip_sorting(true).with_num_sweeps(20).on(
+            d_exec);
+    auto d_prec = d_prec_fact->generate(d_mtx);
+
+    const auto d_l_factor = d_prec->get_const_l_factor();
+    const auto d_u_factor = d_prec->get_const_u_factor();
+
+    auto rv = gko::test::generate_uniform_batch_random_matrix<BDense>(
+        nbatch, nrows, 1, std::uniform_int_distribution<>(1, 1),
+        std::normal_distribution<real_type>(0.0, 1.0), rand_engine, false, ref);
+    auto z = BDense::create(ref, rv->get_size());
+    auto d_rv = gko::as<BDense>(gko::clone(d_exec, rv));
+    auto d_z = BDense::create(d_exec, rv->get_size());
+
+    gko::kernels::reference::batch_par_ilu::apply_par_ilu0(
+        ref, l_factor, u_factor, rv.get(), z.get());
+    gko::kernels::hip::batch_par_ilu::apply_par_ilu0(
+        d_exec, d_l_factor, d_u_factor, d_rv.get(), d_z.get());
+
+    const auto tol = 5000 * r<value_type>::value;
+    GKO_ASSERT_BATCH_MTX_NEAR(z, d_z, tol);
+}
+
+
+}  // namespace
