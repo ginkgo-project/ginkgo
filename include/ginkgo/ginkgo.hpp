061b3a706fe75abf61cdb5d3e3e7cb089f7a6af5
aggarwal2000 <aggarwal2000chd@gmail.com>
2022-09-21 12:34:54 +0200

    Remove the previous batch ilu precond

--- include/ginkgo/ginkgo.hpp
+++ include/ginkgo/ginkgo.hpp
@@ -115,7 +115,6 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <ginkgo/core/multigrid/multigrid_level.hpp>
 
 #include <ginkgo/core/preconditioner/batch_exact_ilu.hpp>
-#include <ginkgo/core/preconditioner/batch_ilu.hpp>
 #include <ginkgo/core/preconditioner/batch_isai.hpp>
 #include <ginkgo/core/preconditioner/batch_jacobi.hpp>
 #include <ginkgo/core/preconditioner/batch_par_ilu.hpp>
