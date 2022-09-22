81cc030dd0a839add6f10922bd044bd88bbbd392
Marcel Koch <marcel.koch@kit.edu>
2022-09-22 18:02:15 +0200

    changes run signature T -> T*
    this makes checking for constness way easier

--- core/matrix/row_gatherer.cpp
+++ core/matrix/row_gatherer.cpp
@@ -46,8 +46,8 @@ namespace matrix {
 template <typename IndexType>
 void RowGatherer<IndexType>::apply_impl(const LinOp* in, LinOp* out) const
 {
-    run<const Dense<float>*, const Dense<double>*,
-        const Dense<std::complex<float>>*, const Dense<std::complex<double>>*>(
+    run<const Dense<float>, const Dense<double>,
+        const Dense<std::complex<float>>, const Dense<std::complex<double>>>(
         in, [&](auto gather) { gather->row_gather(&row_idxs_, out); });
 }
 
@@ -55,8 +55,8 @@ template <typename IndexType>
 void RowGatherer<IndexType>::apply_impl(const LinOp* alpha, const LinOp* in,
                                         const LinOp* beta, LinOp* out) const
 {
-    run<const Dense<float>*, const Dense<double>*,
-        const Dense<std::complex<float>>*, const Dense<std::complex<double>>*>(
+    run<const Dense<float>, const Dense<double>,
+        const Dense<std::complex<float>>, const Dense<std::complex<double>>>(
         in,
         [&](auto gather) { gather->row_gather(alpha, &row_idxs_, beta, out); });
 }
