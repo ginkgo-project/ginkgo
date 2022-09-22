c61266f77dc3475eaf321acf96ed15f6b1410a78
Marcel Koch <marcel.koch@kit.edu>
2022-09-22 18:05:45 +0200

    adds dispatch for distributed matrix instantiations

--- core/distributed/helpers.hpp
+++ core/distributed/helpers.hpp
@@ -34,6 +34,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
 #include <ginkgo/config.hpp>
+#include <ginkgo/core/distributed/matrix.hpp>
 #include <ginkgo/core/distributed/vector.hpp>
 #include <ginkgo/core/matrix/dense.hpp>
 
@@ -169,6 +170,31 @@ LinOpType* get_local(LinOpType* op)
             op, [&](auto vector_op) { local = get_local(vector_op); });
     }
     return local;
+}
+
+
+/**
+ * Extracts the correct Matrix instantiation for a given linop and calls a
+ * function with it.
+ *
+ * @note internally this uses run(T*, func, Args...)
+ */
+template <
+    typename T, typename func, typename... Args,
+    typename = std::enable_if<std::is_same<LinOp, std::decay_t<T>>::value>>
+void dispatch_distributed_matrix(T* obj, func f, Args... args)
+{
+    using namespace gko::distributed;
+    gko::run<Matrix<float, int32, int32>, Matrix<float, int32, int64>,
+             Matrix<float, int64, int64>, Matrix<double, int32, int32>,
+             Matrix<double, int32, int64>, Matrix<double, int64, int64>,
+             Matrix<std::complex<float>, int32, int32>,
+             Matrix<std::complex<float>, int32, int64>,
+             Matrix<std::complex<float>, int64, int64>,
+             Matrix<std::complex<double>, int32, int32>,
+             Matrix<std::complex<double>, int32, int64>,
+             Matrix<std::complex<double>, int64, int64>>(
+        obj, f, std::forward<Args>(args)...);
 }
 
 
