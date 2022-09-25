0bea147fb5341ceadaec0daee3706dd2649ce82e
aggarwal2000 <aggarwal2000chd@gmail.com>
2022-09-26 00:38:12 +0200

    Remove factor splitting from parilu0 and combine classes for exact and parilu

--- hip/preconditioner/batch_ilu_kernels.hip.cpp
+++ hip/preconditioner/batch_ilu_kernels.hip.cpp
@@ -30,7 +30,7 @@ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ******************************<GINKGO LICENSE>*******************************/
 
-#include "core/preconditioner/batch_exact_ilu_kernels.hpp"
+#include "core/preconditioner/batch_ilu_kernels.hpp"
 
 
 #include <ginkgo/core/matrix/batch_csr.hpp>
@@ -46,21 +46,22 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 namespace gko {
 namespace kernels {
 namespace hip {
-namespace batch_exact_ilu {
+namespace batch_ilu {
 namespace {
 
 
 constexpr size_type default_block_size = 256;
 
 
-#include "common/cuda_hip/preconditioner/batch_exact_ilu.hpp.inc"
-#include "common/cuda_hip/preconditioner/batch_exact_ilu_kernels.hpp.inc"
+#include "common/cuda_hip/matrix/batch_vector_kernels.hpp.inc"
+#include "common/cuda_hip/preconditioner/batch_ilu.hpp.inc"
+#include "common/cuda_hip/preconditioner/batch_ilu_kernels.hpp.inc"
 
 }  // namespace
 
 
 template <typename ValueType, typename IndexType>
-void compute_factorization(
+void compute_ilu0_factorization(
     std::shared_ptr<const DefaultExecutor> exec,
     const IndexType* const diag_locs,
     matrix::BatchCsr<ValueType, IndexType>* const mat_fact)
@@ -86,7 +87,35 @@ GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE_AND_INT32_INDEX(
 
 
 template <typename ValueType, typename IndexType>
-void apply_exact_ilu(
+void compute_parilu0_factorization(
+    std::shared_ptr<const DefaultExecutor> exec,
+    const matrix::BatchCsr<ValueType, IndexType>* const sys_mat,
+    matrix::BatchCsr<ValueType, IndexType>* const mat_fact,
+    const int parilu_num_sweeps, const IndexType* const dependencies,
+    const IndexType* const nz_ptrs)
+{
+    const auto num_rows = static_cast<int>(sys_mat->get_size().at(0)[0]);
+    const auto nbatch = sys_mat->get_num_batch_entries();
+    const auto nnz =
+        static_cast<int>(sys_mat->get_num_stored_elements() / nbatch);
+
+    const int dynamic_shared_mem_bytes = nnz * sizeof(ValueType);
+
+    hipLaunchKernelGGL(generate_parilu0_kernel, nbatch, default_block_size,
+                       dynamic_shared_mem_bytes, 0, nbatch, num_rows, nnz,
+                       dependencies, nz_ptrs, parilu_num_sweeps,
+                       as_hip_type(sys_mat->get_const_values()),
+                       as_hip_type(mat_fact->get_values()));
+
+    GKO_HIP_LAST_IF_ERROR_THROW;
+}
+
+GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE_AND_INT32_INDEX(
+    GKO_DECLARE_BATCH_PARILU_COMPUTE_FACTORIZATION_KERNEL);
+
+
+template <typename ValueType, typename IndexType>
+void apply_ilu(
     std::shared_ptr<const DefaultExecutor> exec,
     const matrix::BatchCsr<ValueType, IndexType>* const factored_matrix,
     const IndexType* const diag_locs,
@@ -98,13 +127,12 @@ void apply_exact_ilu(
     const auto nbatch = factored_matrix->get_num_batch_entries();
     const auto factored_matrix_batch = get_batch_struct(factored_matrix);
     using d_value_type = hip_type<ValueType>;
-    using prec_type = batch_exact_ilu<d_value_type>;
-    bool is_fallback_required = true;
+    using prec_type = batch_ilu<d_value_type>;
 
-    prec_type prec(factored_matrix_batch, diag_locs, is_fallback_required);
+    prec_type prec(factored_matrix_batch, diag_locs);
 
     hipLaunchKernelGGL(
-        batch_exact_ilu_apply, nbatch, default_block_size,
+        batch_ilu_apply, nbatch, default_block_size,
         prec_type::dynamic_work_size(num_rows, 0) * sizeof(ValueType), 0, prec,
         nbatch, num_rows, as_hip_type(r->get_const_values()),
         as_hip_type(z->get_values()));
@@ -113,9 +141,9 @@ void apply_exact_ilu(
 }
 
 GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE_AND_INT32_INDEX(
-    GKO_DECLARE_BATCH_EXACT_ILU_APPLY_KERNEL);
+    GKO_DECLARE_BATCH_ILU_APPLY_KERNEL);
 
-}  // namespace batch_exact_ilu
+}  // namespace batch_ilu
 }  // namespace hip
 }  // namespace kernels
 }  // namespace gko
