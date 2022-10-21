8c8f455af566a75f4b6f3bd0cf585cf004691aa4
Marcel Koch <marcel.koch@kit.edu>
2022-10-21 09:11:41 +0200

    uses json input format for scaling benchmark

--- benchmark/distributed/scaling.cpp
+++ benchmark/distributed/scaling.cpp
@@ -37,44 +37,140 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <set>
 #include <string>
 
+#include "benchmark/utils/formats.hpp"
 #include "benchmark/utils/general.hpp"
+#include "benchmark/utils/loggers.hpp"
 #include "benchmark/utils/stencil_matrix.hpp"
 #include "benchmark/utils/timer.hpp"
 #include "benchmark/utils/types.hpp"
 
 
-DEFINE_int64(
-    target_rows, 100,
-    "Target number of rows, either in total (strong_scaling == true) or per "
-    "process (strong_scaling == false).");
-DEFINE_int32(dim, 2, "Dimension of stencil, either 2D or 3D");
-DEFINE_bool(restrict, false,
-            "If true creates 5/7pt stencil, if false creates 9/27pt stencil.");
-DEFINE_string(comm_pattern, "stencil",
-              "Choose the communication pattern for the matrix, "
-              "possible values are: stencil, optimal");
-DEFINE_bool(
-    strong_scaling, false,
-    "If set to true will treat target_rows as the total number of rows.");
+DEFINE_uint32(nrhs, 1, "The number of right hand sides");
+
+
+template <typename ValueType>
+using dist_vec = gko::experimental::distributed::Vector<ValueType>;
+template <typename ValueType, typename LocalIndexType, typename GlobalIndexType>
+using dist_mtx =
+    gko::experimental::distributed::Matrix<ValueType, LocalIndexType,
+                                           GlobalIndexType>;
+
+
+std::string example_config = R"(
+  [
+    {"size": 100, "stencil": "7pt", "comm_pattern": "optimal",
+     "format" : {"local": "csr", "non_local": "coo"}},
+    {"size": 100, "stencil": "9pt", "comm_pattern": "stencil",
+     "format" : {"local": "ell"}}
+  ]
+)";
+
+
+// input validation
+[[noreturn]] void print_config_error_and_exit()
+{
+    std::cerr << "Input has to be a JSON array of matrix configurations:\n"
+              << example_config << std::endl;
+    std::exit(1);
+}
+
+
+void validate_option_object(const rapidjson::Value& value)
+{
+    if (!value.IsObject() || !value.HasMember("size") ||
+        !value.HasMember("stencil") || !value.HasMember("comm_pattern")) {
+        print_config_error_and_exit();
+    }
+}
+
+
+std::unique_ptr<dist_mtx<etype, itype, gko::int64>> create_distributed_matrix(
+    std::shared_ptr<gko::Executor> exec, gko::mpi::communicator comm,
+    const std::string& format_local, const std::string& format_non_local,
+    const gko::matrix_data<etype, gko::int64>& data,
+    const gko::distributed::Partition<itype, gko::int64>* part,
+    rapidjson::Value& spmv_case, rapidjson::MemoryPoolAllocator<>& allocator)
+{
+    auto local_mat = formats::matrix_type_factory.at(format_local)(exec);
+    auto non_local_mat =
+        formats::matrix_type_factory.at(format_non_local)(exec);
+
+    auto storage_logger = std::make_shared<StorageLogger>();
+    exec->add_logger(storage_logger);
+
+    auto dist_mat = dist_mtx<etype, itype, gko::int64>::create(
+        exec, comm, local_mat.get(), non_local_mat.get());
+    dist_mat->read_distributed(data, part);
+
+    exec->remove_logger(gko::lend(storage_logger));
+    storage_logger->write_data(comm, spmv_case, allocator);
+
+    return dist_mat;
+}
+
+
+// This function supposes that management of `FLAGS_overwrite` is done before
+// calling it
+void apply_spmv(std::shared_ptr<gko::Executor> exec,
+                const dist_mtx<etype, itype, gko::int64>* system_matrix,
+                const dist_vec<etype>* b, const dist_vec<etype>* x,
+                rapidjson::Value& test_case,
+                rapidjson::MemoryPoolAllocator<>& allocator)
+{
+    auto& spmv_case = test_case["spmv"];
+    try {
+        auto comm = system_matrix->get_communicator();
+        IterationControl ic{get_timer(exec, FLAGS_gpu_timer)};
+        // warm run
+        for (auto _ : ic.warmup_run()) {
+            auto x_clone = clone(x);
+            exec->synchronize();
+            comm.synchronize();
+            system_matrix->apply(lend(b), lend(x_clone));
+            exec->synchronize();
+        }
+
+        // timed run
+        auto x_clone = clone(x);
+        for (auto _ : ic.run()) {
+            comm.synchronize();
+            system_matrix->apply(lend(b), lend(x_clone));
+        }
+        add_or_set_member(spmv_case, "time", ic.compute_average_time(),
+                          allocator);
+        add_or_set_member(spmv_case, "repetitions", ic.get_num_repetitions(),
+                          allocator);
+
+        // compute and write benchmark data
+        add_or_set_member(spmv_case, "completed", true, allocator);
+    } catch (const std::exception& e) {
+        add_or_set_member(spmv_case, "completed", false, allocator);
+        if (FLAGS_keep_errors) {
+            rapidjson::Value msg_value;
+            msg_value.SetString(e.what(), allocator);
+            add_or_set_member(spmv_case, "error", msg_value, allocator);
+        }
+        std::cerr << "Error when processing test case " << test_case << "\n"
+                  << "what(): " << e.what() << std::endl;
+    }
+}
 
 
 int main(int argc, char* argv[])
 {
     gko::mpi::environment mpi_env{argc, argv};
 
-    using ValueType = etype;
-    using GlobalIndexType = gko::int64;
-    using LocalIndexType = GlobalIndexType;
-    using dist_mtx =
-        gko::experimental::distributed::Matrix<ValueType, LocalIndexType,
-                                               GlobalIndexType>;
-    using dist_vec = gko::experimental::distributed::Vector<ValueType>;
-    using vec = gko::matrix::Dense<ValueType>;
-
     std::string header =
         "A benchmark for measuring the strong or weak scaling of Ginkgo's "
         "distributed SpMV\n";
-    std::string format = "";
+    std::string format = example_config + R"(
+  The size parameter denotes the size per process. It might be adjusted to
+  fit the dimensionality of the stencil more easily.
+  Possible values for "stencil" are:  5pt (2D), 7pt (3D), 9pt (2D), 27pt (3D).
+  Possible values for "comm_pattern" are: stencil, optimal.
+  Possible values for "local" and "non_local" are any of the recognized spmv
+  formats.
+)";
     initialize_argument_parsing(&argc, &argv, header, format);
 
     const auto comm = gko::mpi::communicator(MPI_COMM_WORLD);
@@ -93,89 +189,107 @@ int main(int argc, char* argv[])
         print_general_information(extra_information);
     }
 
-    const auto num_target_rows = FLAGS_target_rows;
-    const auto dim = FLAGS_dim;
-    const bool restricted = FLAGS_restrict;
-
-    // Build global matrix from local matrix data.
+    std::string json_input;
     if (rank == 0) {
-        std::clog << "Generating stencil matrix ";
-    }
-    auto h_A = dist_mtx::create(exec->get_master(), comm);
-    gko::matrix_data<ValueType, GlobalIndexType> A_data;
-    // Generate matrix data on each rank
-    if (FLAGS_comm_pattern == "stencil") {
-        if (rank == 0) {
-            std::clog << "using stencil communication pattern..." << std::endl;
+        std::string line;
+        while (std::cin >> line) {
+            json_input += line;
         }
-        A_data = dim == 2 ? generate_2d_stencil<ValueType, GlobalIndexType>(
-                                comm, num_target_rows, restricted)
-                          : generate_3d_stencil<ValueType, GlobalIndexType>(
-                                comm, num_target_rows, restricted);
-    } else if (FLAGS_comm_pattern == "optimal") {
-        if (rank == 0) {
-            std::clog << "using optimal communication pattern..." << std::endl;
+    }
+    auto input_size = json_input.size();
+    comm.broadcast(exec->get_master(), &input_size, 1, 0);
+    json_input.resize(input_size);
+    comm.broadcast(exec->get_master(), &json_input[0],
+                   static_cast<int>(input_size), 0);
+
+    rapidjson::Document test_cases;
+    test_cases.Parse(json_input.c_str());
+    if (!test_cases.IsArray()) {
+        print_config_error_and_exit();
+    }
+
+    auto engine = get_engine();
+    auto& allocator = test_cases.GetAllocator();
+
+    for (auto& test_case : test_cases.GetArray()) {
+        try {
+            // set up benchmark
+            validate_option_object(test_case);
+            if (!test_case.HasMember("spmv")) {
+                test_case.AddMember("spmv",
+                                    rapidjson::Value(rapidjson::kObjectType),
+                                    allocator);
+            }
+            if (!test_case.HasMember("format")) {
+                test_case.AddMember("format",
+                                    rapidjson::Value(rapidjson::kObjectType),
+                                    allocator);
+            }
+            if (!test_case["format"].HasMember("local")) {
+                add_or_set_member(test_case["format"], "local", "csr",
+                                  allocator);
+            }
+            if (!test_case["format"].HasMember("non_local")) {
+                add_or_set_member(test_case["format"], "non_local", "csr",
+                                  allocator);
+            }
+            auto& spmv_case = test_case["spmv"];
+            if (rank == 0) {
+                std::clog << "Running test case: " << test_case << std::endl;
+            }
+
+            auto data = generate_stencil<etype, gko::int64>(
+                test_case["stencil"].GetString(), comm,
+                test_case["size"].GetInt64(),
+                test_case["comm_pattern"].GetString() ==
+                    std::string("optimal"));
+            auto part = gko::distributed::Partition<itype, gko::int64>::
+                build_from_global_size_uniform(
+                    exec, comm.size(), static_cast<gko::int64>(data.size[0]));
+            const auto global_size = part->get_size();
+            const auto local_size =
+                static_cast<gko::size_type>(part->get_part_size(rank));
+
+            auto system_matrix = create_distributed_matrix(
+                exec, comm, test_case["format"]["local"].GetString(),
+                test_case["format"]["non_local"].GetString(), data, part.get(),
+                spmv_case, allocator);
+
+            using Vec = dist_vec<etype>;
+            auto nrhs = FLAGS_nrhs;
+            auto b =
+                Vec::create(exec, comm, gko::dim<2>{global_size, nrhs},
+                            create_matrix<etype>(
+                                exec, gko::dim<2>{local_size, nrhs}, engine)
+                                .get());
+            auto x = Vec::create(
+                exec, comm, gko::dim<2>{global_size, nrhs},
+                create_matrix<etype>(exec, gko::dim<2>{local_size, nrhs}, 1)
+                    .get());
+
+            if (rank == 0) {
+                std::clog << "Running on " << comm.size() << " processes."
+                          << std::endl;
+                std::clog << "Matrix is of size ("
+                          << system_matrix->get_size()[0] << ", "
+                          << system_matrix->get_size()[1] << ")." << std::endl;
+            }
+            add_or_set_member(spmv_case, "num_procs", comm.size(), allocator);
+            add_or_set_member(spmv_case, "global_size", global_size, allocator);
+            add_or_set_member(spmv_case, "local_size", local_size, allocator);
+            add_or_set_member(spmv_case, "num_rhs", FLAGS_nrhs, allocator);
+            apply_spmv(exec, system_matrix.get(), b.get(), x.get(), test_case,
+                       allocator);
+            if (rank == 0) {
+                backup_results(test_cases);
+            }
+        } catch (const std::exception& e) {
+            std::cerr << "Error setting up spmv, what(): " << e.what()
+                      << std::endl;
         }
-        A_data = dim == 2
-                     ? generate_2d_stencil_with_optimal_comm<ValueType,
-                                                             GlobalIndexType>(
-                           num_target_rows, comm, FLAGS_restrict,
-                           FLAGS_strong_scaling)
-                     : generate_3d_stencil_with_optimal_comm<ValueType,
-                                                             GlobalIndexType>(
-                           num_target_rows, comm, FLAGS_restrict,
-                           FLAGS_strong_scaling);
-    } else {
-        throw std::runtime_error("Communication pattern " + FLAGS_comm_pattern +
-                                 " not implemented");
-    }
-    auto part = gko::distributed::Partition<LocalIndexType, GlobalIndexType>::
-        build_from_global_size_uniform(
-            exec, comm.size(), static_cast<GlobalIndexType>(A_data.size[0]));
-    h_A->read_distributed(A_data, part.get(), part.get());
-
-    auto A = dist_mtx::create(exec, comm);
-    A->copy_from(h_A.get());
-
-    // Set up global vectors for the distributed SpMV
-    if (rank == 0) {
-        std::clog << "Setting up vectors..." << std::endl;
-    }
-    const auto global_size = part->get_size();
-    const auto local_size =
-        static_cast<gko::size_type>(part->get_part_size(comm.rank()));
-    auto x = dist_vec::create(exec, comm, gko::dim<2>{global_size, 1},
-                              gko::dim<2>{local_size, 1});
-    x->fill(gko::one<ValueType>());
-    auto b = dist_vec::create(exec, comm, gko::dim<2>{global_size, 1},
-                              gko::dim<2>{local_size, 1});
-    b->fill(gko::one<ValueType>());
-
-    auto timer = get_timer(exec, FLAGS_gpu_timer);
-    IterationControl ic{timer};
-
-    // Do a warmup run
-    if (rank == 0) {
-        std::clog << "Warming up..." << std::endl;
-    }
-    comm.synchronize();
-    for (auto _ : ic.warmup_run()) {
-        A->apply(gko::lend(x), gko::lend(b));
-    }
-
-    // Do and time the actual benchmark runs
-    if (rank == 0) {
-        std::clog << "Running benchmark..." << std::endl;
-    }
-    comm.synchronize();
-    for (auto _ : ic.run()) {
-        A->apply(gko::lend(x), gko::lend(b));
     }
 
     if (rank == 0) {
-        std::clog << "SIZE: " << part->get_size() << std::endl;
-        std::clog << "DURATION: " << ic.compute_average_time() << "s"
-                  << std::endl;
-        std::clog << "ITERATIONS: " << ic.get_num_repetitions() << std::endl;
+        std::cout << test_cases << std::endl;
     }
 }
