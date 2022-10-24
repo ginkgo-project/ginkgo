8ecb50c1fffda18f69f6983f8e454f36fd40836f
Marcel Koch <marcel.koch@kit.edu>
2022-10-24 16:59:03 +0200

    adds distributed solver benchmark

--- benchmark/utils/generator.hpp
+++ benchmark/utils/generator.hpp
@@ -37,6 +37,9 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "benchmark/utils/formats.hpp"
 #include "benchmark/utils/general.hpp"
 #include "benchmark/utils/loggers.hpp"
+#if GINKGO_BUILD_MPI
+#include "benchmark/utils/distributed_helpers.hpp"
+#endif
 
 
 struct DefaultSystemGenerator {
@@ -89,4 +92,79 @@ struct DefaultSystemGenerator {
     std::default_random_engine engine = get_engine();
 };
 
+
+#if GINKGO_BUILD_MPI
+
+
+struct DistributedDefaultSystemGenerator {
+    using Mtx = dist_mtx<etype, itype, gko::int64>;
+    using Vec = dist_vec<etype>;
+
+    std::shared_ptr<gko::LinOp> generate_matrix(
+        std::shared_ptr<const gko::Executor> exec,
+        rapidjson::Value& config) const
+    {
+        auto data = ::generate_matrix_data<etype, gko::int64>(config, comm);
+        return generate_matrix(std::move(exec), config, data);
+    }
+
+    std::shared_ptr<gko::LinOp> generate_matrix(
+        std::shared_ptr<const gko::Executor> exec, rapidjson::Value& config,
+        const gko::matrix_data<etype, gko::int64>& data) const
+    {
+        auto part = gko::distributed::Partition<itype, gko::int64>::
+            build_from_global_size_uniform(
+                exec, comm.size(), static_cast<gko::int64>(data.size[0]));
+        return ::create_distributed_matrix(
+            exec, comm, config["optimal"]["spmv"]["local"].GetString(),
+            config["optimal"]["spmv"]["non-local"].GetString(), data,
+            part.get());
+    }
+
+    std::unique_ptr<Vec> generate_rhs(std::shared_ptr<const gko::Executor> exec,
+                                      const gko::LinOp* system_matrix,
+                                      rapidjson::Value& config) const
+    {
+        return Vec::create(
+            exec, comm, gko::dim<2>{system_matrix->get_size()[0], FLAGS_nrhs},
+            gko::as<DefaultSystemGenerator::Vec>(
+                local_generator.generate_rhs(
+                    exec, gko::as<Mtx>(system_matrix)->get_local_matrix().get(),
+                    config))
+                .get());
+    }
+
+    std::unique_ptr<Vec> generate_initial_guess(
+        std::shared_ptr<const gko::Executor> exec,
+        const gko::LinOp* system_matrix, const Vec* rhs) const
+    {
+        return Vec::create(
+            exec, comm, gko::dim<2>{rhs->get_size()[0], FLAGS_nrhs},
+            gko::as<DefaultSystemGenerator::Vec>(
+                local_generator.generate_initial_guess(
+                    exec, gko::as<Mtx>(system_matrix)->get_local_matrix().get(),
+                    rhs->get_local_vector()))
+                .get());
+    }
+
+    std::unique_ptr<Vec> initialize(
+        std::initializer_list<etype> il,
+        std::shared_ptr<const gko::Executor> exec) const
+    {
+        auto local = gko::initialize<DefaultSystemGenerator::Vec>(
+            std::move(il), std::move(exec));
+        auto global_rows = local->get_size()[0];
+        comm.all_reduce(gko::ReferenceExecutor::create(), &global_rows, 1,
+                        MPI_SUM);
+        return Vec::create(exec, comm,
+                           gko::dim<2>{global_rows, local->get_size()[1]},
+                           local.get());
+    }
+
+    gko::mpi::communicator comm;
+    DefaultSystemGenerator local_generator{};
+};
+
+
+#endif
 #endif  // GINKGO_BENCHMARK_UTILS_GENERATOR_HPP
