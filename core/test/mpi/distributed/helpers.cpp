1bbe5f51591b47cd39a09af0deea30a41493820f
Marcel Koch <marcel.koch@kit.edu>
2022-10-26 16:53:57 +0200

    review updates:
    - formatting
    - fixes tests
    - test for nullptr/wrong type
    
    Co-authored-by: Yuhsiang M. Tsai <yhmtsai@gmail.com>
    Co-authored-by: Terry Cojean <terry.cojean@kit.edu>

--- core/test/mpi/distributed/helpers.cpp
+++ core/test/mpi/distributed/helpers.cpp
@@ -37,17 +37,17 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "core/distributed/helpers.hpp"
 
 
+#include <ginkgo/core/matrix/csr.hpp>
+
+
 #include "core/test/utils.hpp"
 
 
 int run_function(gko::experimental::distributed::Vector<>*) { return 1; }
 
-
 int run_function(const gko::experimental::distributed::Vector<>*) { return 2; }
 
-
 int run_function(gko::matrix::Dense<>*) { return 3; }
-
 
 int run_function(const gko::matrix::Dense<>*) { return 4; }
 
@@ -61,47 +61,53 @@ public:
 
 TEST_F(RunVector, PicksDistributedVectorCorrectly)
 {
-    auto dist_vector =
+    std::unique_ptr<gko::LinOp> dist_vector =
         gko::experimental::distributed::Vector<>::create(exec, MPI_COMM_WORLD);
     int result;
 
     gko::detail::vector_dispatch<double>(
         dist_vector.get(), [&](auto* dense) { result = run_function(dense); });
 
-    ASSERT_EQ(result, run_function(dist_vector.get()));
+    ASSERT_EQ(result,
+              run_function(gko::as<gko::experimental::distributed::Vector<>>(
+                  dist_vector.get())));
 }
 
 
 TEST_F(RunVector, PicksConstDistributedVectorCorrectly)
 {
-    std::unique_ptr<const gko::experimental::distributed::Vector<>>
-        const_dist_vector = gko::experimental::distributed::Vector<>::create(
-            exec, MPI_COMM_WORLD);
+    std::unique_ptr<const gko::LinOp> const_dist_vector =
+        gko::experimental::distributed::Vector<>::create(exec, MPI_COMM_WORLD);
     int result;
 
     gko::detail::vector_dispatch<double>(
         const_dist_vector.get(),
         [&](auto* dense) { result = run_function(dense); });
 
-    ASSERT_EQ(result, run_function(const_dist_vector.get()));
+    ASSERT_EQ(
+        result,
+        run_function(gko::as<const gko::experimental::distributed::Vector<>>(
+            const_dist_vector.get())));
 }
 
 
 TEST_F(RunVector, PicksDenseVectorCorrectly)
 {
-    auto dense_vector = gko::matrix::Dense<>::create(exec);
+    std::unique_ptr<gko::LinOp> dense_vector =
+        gko::matrix::Dense<>::create(exec);
     int result;
 
     gko::detail::vector_dispatch<double>(
         dense_vector.get(), [&](auto* dense) { result = run_function(dense); });
 
-    ASSERT_EQ(result, run_function(dense_vector.get()));
+    ASSERT_EQ(result,
+              run_function(gko::as<gko::matrix::Dense<>>(dense_vector.get())));
 }
 
 
 TEST_F(RunVector, PicksConstDenseVectorCorrectly)
 {
-    std::unique_ptr<const gko::matrix::Dense<>> const_dense_vector =
+    std::unique_ptr<const gko::LinOp> const_dense_vector =
         gko::matrix::Dense<>::create(exec);
     int result;
 
@@ -109,5 +115,23 @@ TEST_F(RunVector, PicksConstDenseVectorCorrectly)
         const_dense_vector.get(),
         [&](auto* dense) { result = run_function(dense); });
 
-    ASSERT_EQ(result, run_function(const_dense_vector.get()));
+    ASSERT_EQ(result, run_function(gko::as<const gko::matrix::Dense<>>(
+                          const_dense_vector.get())));
+}
+
+TEST_F(RunVector, ThrowsIfWrongType)
+{
+    std::unique_ptr<gko::LinOp> csr = gko::matrix::Csr<>::create(exec);
+
+    ASSERT_THROW(
+        gko::detail::vector_dispatch<double>(csr.get(), [&](auto* dense) {}),
+        gko::NotSupported);
+}
+
+
+TEST_F(RunVector, ThrowsIfNullptr)
+{
+    ASSERT_THROW(gko::detail::vector_dispatch<double>(
+                     static_cast<gko::LinOp*>(nullptr), [&](auto* dense) {}),
+                 gko::NotSupported);
 }
