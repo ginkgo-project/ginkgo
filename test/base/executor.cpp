edd7f9b3163aaa6517d64d4634fc43dedf6b3de7
Marcel Koch <marcel.koch@kit.edu>
2022-12-12 15:41:06 +0100

    enable executor run tests only if corresponding module is build

---
+++ test/base/executor.cpp
@@ -0,0 +1,89 @@
+#include <ginkgo/core/base/executor.hpp>
+#include <map>
+
+
+#include <gtest/gtest.h>
+
+
+#include "core/test/utils/assertions.hpp"
+
+
+namespace reference {
+int value = 5;
+}
+
+namespace omp {
+int value = 1;
+}
+
+namespace cuda {
+int value = 2;
+}
+
+namespace hip {
+int value = 3;
+}
+
+namespace dpcpp {
+int value = 4;
+}
+
+
+class ExampleOperation : public gko::Operation {
+public:
+    explicit ExampleOperation(int& val) : value(val) {}
+    void run(std::shared_ptr<const gko::OmpExecutor>) const override
+    {
+        value = omp::value;
+    }
+    void run(std::shared_ptr<const gko::CudaExecutor>) const override
+    {
+        value = cuda::value;
+    }
+    void run(std::shared_ptr<const gko::HipExecutor>) const override
+    {
+        value = hip::value;
+    }
+    void run(std::shared_ptr<const gko::DpcppExecutor>) const override
+    {
+        value = dpcpp::value;
+    }
+    void run(std::shared_ptr<const gko::ReferenceExecutor>) const override
+    {
+        value = reference::value;
+    }
+
+    int& value;
+};
+
+
+TEST(Executor, RunsCorrectOperation)
+{
+    int value = 0;
+    auto exec = gko::EXEC_TYPE::create();
+
+    exec->run(ExampleOperation(value));
+
+    ASSERT_EQ(EXEC_NAMESPACE::value, value);
+}
+
+
+#ifndef GKO_COMPILING_REFERENCE
+
+
+TEST(Executor, RunsCorrectLambdaOperation)
+{
+    int value = 0;
+    auto omp_lambda = [&value]() { value = omp::value; };
+    auto cuda_lambda = [&value]() { value = cuda::value; };
+    auto hip_lambda = [&value]() { value = hip::value; };
+    auto dpcpp_lambda = [&value]() { value = dpcpp::value; };
+    auto exec = gko::EXEC_TYPE::create();
+
+    exec->run(omp_lambda, cuda_lambda, hip_lambda, dpcpp_lambda);
+
+    ASSERT_EQ(EXEC_NAMESPACE::value, value);
+}
+
+
+#endif  // GKO_COMPILING_REFERENCE
