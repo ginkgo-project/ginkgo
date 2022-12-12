b782563f378b72fbf50a5f1328e216da2ab7ae9d
Marcel Koch <marcel.koch@kit.edu>
2022-12-12 17:41:57 +0100

    adds MPI tests

--- test/mpi/partition_helpers.cpp
+++ test/mpi/partition_helpers.cpp
@@ -6,6 +6,9 @@
 #include "test/utils/mpi/executor.hpp"
 
 
+using comm_index_type = gko::experimental::distributed::comm_index_type;
+
+
 template <typename IndexType>
 class PartitionHelpers : public CommonMpiTestFixture {
 protected:
@@ -19,7 +22,9 @@ TYPED_TEST(PartitionHelpers, CanBuildFromLocalRanges)
 {
     using itype = typename TestFixture::index_type;
     gko::span local_range[] = {{0u, 4u}, {4u, 9u}, {9u, 11u}};
-    gko::array<itype> expects{this->exec, {0, 4, 9, 11}};
+    gko::array<itype> expects_ranges{this->exec, {0, 4, 9, 11}};
+    gko::array<comm_index_type> expects_pid{this->exec, {0, 1, 2}};
+
 
     auto part =
         gko::experimental::distributed::build_partition_from_local_range<
@@ -27,6 +32,48 @@ TYPED_TEST(PartitionHelpers, CanBuildFromLocalRanges)
                                this->comm);
 
     GKO_ASSERT_ARRAY_EQ(
-        expects, gko::make_const_array_view(this->exec, expects.get_num_elems(),
-                                            part->get_range_bounds()));
+        expects_ranges,
+        gko::make_const_array_view(this->exec, expects_ranges.get_num_elems(),
+                                   part->get_range_bounds()));
+    GKO_ASSERT_ARRAY_EQ(
+        expects_pid,
+        gko::make_const_array_view(this->exec, expects_pid.get_num_elems(),
+                                   part->get_part_ids()));
+}
+
+TYPED_TEST(PartitionHelpers, CanBuildFromLocalRangesUnsorted)
+{
+    using itype = typename TestFixture::index_type;
+    gko::span local_range[] = {{4u, 9u}, {9u, 11u}, {0u, 4u}};
+    gko::array<itype> expects_ranges{this->exec, {0, 4, 9, 11}};
+    gko::array<comm_index_type> expects_pid{this->exec, {2, 0, 1}};
+
+    auto part =
+        gko::experimental::distributed::build_partition_from_local_range<
+            gko::int32, itype>(this->exec, local_range[this->comm.rank()],
+                               this->comm);
+
+    GKO_ASSERT_ARRAY_EQ(
+        expects_ranges,
+        gko::make_const_array_view(this->exec, expects_ranges.get_num_elems(),
+                                   part->get_range_bounds()));
+    GKO_ASSERT_ARRAY_EQ(
+        expects_pid,
+        gko::make_const_array_view(this->exec, expects_pid.get_num_elems(),
+                                   part->get_part_ids()));
+}
+
+
+TYPED_TEST(PartitionHelpers, CanBuildFromLocalRangesThrowsOnGap)
+{
+    using itype = typename TestFixture::index_type;
+    gko::span local_range[] = {{4u, 6u}, {9u, 11u}, {0u, 4u}};
+    auto build_from_local_ranges = [](auto... args) {
+        return gko::experimental::distributed::build_partition_from_local_range<
+            gko::int32, itype>(args...);
+    };
+
+    ASSERT_THROW(build_from_local_ranges(
+                     this->exec, local_range[this->comm.rank()], this->comm),
+                 gko::Error);
 }
