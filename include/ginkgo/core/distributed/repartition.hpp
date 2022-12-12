11e38162984df8637b3e8a3b391eed78de41ce62
Gregor Olenik <gregor.olenik@web.de>
2022-12-12 10:30:04 +0100

    Fixup add experimental namespace

--- include/ginkgo/core/distributed/repartition.hpp
+++ include/ginkgo/core/distributed/repartition.hpp
@@ -48,6 +48,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
 namespace gko {
+namespace experimental {
 /**
  * @brief The distributed namespace.
  *
@@ -120,6 +121,7 @@ private:
 
 
 }  // namespace distributed
+}  // namespace experimental
 }  // namespace gko
 
 #endif
