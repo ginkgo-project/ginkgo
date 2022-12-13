75211713640a398c32499300cde07d254e777533
Yu-Hsiang M. Tsai <yhmtsai@gmail.com>
2022-12-13 09:09:08 -0600

    change 1:ST:skip-changelog-wiki-check to 1:ST:no-changelog-entry

--- .github/check-wiki-changelog.sh
+++ .github/check-wiki-changelog.sh
@@ -2,11 +2,11 @@
 
 source .github/bot-pr-base.sh
 
-echo "Checking whether the PR contains the 1:ST:skip-changelog-wiki-check label"
-SKIP_CHECK=$(api_get "$PR_URL" | jq -r 'any( [.labels | .[] | .name ] | .[] ; . == "1:ST:skip-changelog-wiki-check")')
+echo "Checking whether the PR contains the 1:ST:no-changelog-entry label"
+SKIP_CHECK=$(api_get "$PR_URL" | jq -r 'any( [.labels | .[] | .name ] | .[] ; . == "1:ST:no-changelog-entry")')
 
 if [[ "$SKIP_CHECK" == "true" ]]; then
-  echo "The PR contains the 1:ST:skip-changelog-wiki-check label. Skipping the wiki check."
+  echo "The PR contains the 1:ST:no-changelog-entry label. Skipping the wiki check."
 elif [[ "$HEAD_BRANCH" =~ ^release-.* ]]; then
   echo "The PR branch name starts with release. Skipping the wiki check."
 else
@@ -16,7 +16,7 @@ else
   if [[ "${HAS_WIKI}" == "false" ]]; then
     echo "This PR does not create the corresponding entry in wiki/Changelog"
     echo "Please add [#${PR_NUMBER}](https://github.com/ginkgo-project/ginkgo/pull/${PR_NUMBER}) in the wiki/Changelog page"
-    echo "Alternatively, use the label 1:ST:skip-changelog-wiki-check to skip this check."
+    echo "Alternatively, use the label 1:ST:no-changelog-entry to skip this check."
     exit 1
   else
     echo "wiki/Changelog already has this PR entry."
