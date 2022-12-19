610ef0f267f4aab62e4fb7be8b5bc9bbedd2d4d5
Marcel Koch <marcel.koch@kit.edu>
2022-12-19 17:14:17 +0100

    adds device consecutive ranges check

--- common/unified/distributed/partition_helpers_kernels.cpp
+++ common/unified/distributed/partition_helpers_kernels.cpp
@@ -3,6 +3,7 @@
 
 
 #include "common/unified/base/kernel_launch.hpp"
+#include "common/unified/base/kernel_launch_reduction.hpp"
 
 
 namespace gko {
@@ -24,7 +25,24 @@ GKO_INSTANTIATE_FOR_EACH_INDEX_TYPE(
 template <typename GlobalIndexType>
 void check_consecutive_ranges(std::shared_ptr<const DefaultExecutor> exec,
                               array<GlobalIndexType>& range_start_ends,
-                              bool* result) GKO_NOT_IMPLEMENTED;
+                              bool* result)
+{
+    array<uint32> result_uint32{exec, 1};
+    auto num_ranges = range_start_ends.get_num_elems() / 2;
+    run_kernel_reduction(
+        exec,
+        [] GKO_KERNEL(const auto i, const auto* starts, const auto* ends) {
+            return starts[i + 1] == ends[i];
+        },
+        [] GKO_KERNEL(const auto a, const auto b) {
+            return static_cast<uint32>(a && b);
+        },
+        [] GKO_KERNEL(auto x) { return x; }, static_cast<uint32>(true),
+        result_uint32.get_data(), num_ranges - 1, range_start_ends.get_data(),
+        range_start_ends.get_data() + num_ranges);
+    *result =
+        static_cast<bool>(exec->copy_val_to_host(result_uint32.get_data()));
+}
 
 GKO_INSTANTIATE_FOR_EACH_INDEX_TYPE(
     GKO_DECLARE_PARTITION_HELPERS_CHECK_CONSECUTIVE_RANGES);
