03a02b8757b5814eff6035263d7077ffd905cb07
aggarwal2000 <aggarwal2000chd@gmail.com>
2022-12-20 18:54:05 +0100

    Interface change in batch precond

--- include/ginkgo/core/preconditioner/batch_ilu.hpp
+++ include/ginkgo/core/preconditioner/batch_ilu.hpp
@@ -167,16 +167,14 @@ protected:
           system_matrix_{system_matrix}
     {
         GKO_ASSERT_BATCH_HAS_SQUARE_MATRICES(system_matrix);
-        this->generate_precond(lend(system_matrix));
+        this->generate_precond();
     }
 
     /**
      * Generates the preconditoner.
      *
-     * @param system_matrix  the source matrix used to generate the
-     *                       preconditioner
      */
-    void generate_precond(const BatchLinOp* system_matrix);
+    void generate_precond();
 
     // Since there is no guarantee that the complete generation of the
     // preconditioner would occur outside the solver kernel, that is in the
@@ -196,6 +194,8 @@ protected:
 
 
 private:
+    // Note: Storing the system matrix is necessary because it is being used in
+    // the transpose and conjugate transpose.
     std::shared_ptr<const BatchLinOp> system_matrix_;
     std::shared_ptr<matrix::BatchCsr<ValueType, IndexType>> mat_factored_;
     array<IndexType> diag_locations_;
