4213d363b6fdd8632cd7827c794caf9fea72d958
Marcel Koch <marcel.koch@kit.edu>
2022-12-20 11:58:29 +0100

    review updates
    Co-authored-by: Tobias Ribizel <ribizel@kit.edu>

--- test/mpi/partition_helpers.cpp
+++ test/mpi/partition_helpers.cpp
@@ -47,7 +47,8 @@ protected:
     using index_type = IndexType;
 };
 
-TYPED_TEST_SUITE(PartitionHelpers, gko::test::IndexTypes);
+TYPED_TEST_SUITE(PartitionHelpers, gko::test::IndexTypes,
+                 TypenameNameGenerator);
 
 
 TYPED_TEST(PartitionHelpers, CanBuildFromLocalRanges)
