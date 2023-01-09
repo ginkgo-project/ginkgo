ecfe740231e84501368e05add418f9c66f4e4d52
Pratik Nayak <pratikvn@protonmail.com>
2023-01-09 13:39:01 +0100

    Update copyright year for batched files

--- core/preconditioner/batch_ilu_isai.cpp
+++ core/preconditioner/batch_ilu_isai.cpp
@@ -1,5 +1,5 @@
 /*******************************<GINKGO LICENSE>******************************
-Copyright (c) 2017-2022, the Ginkgo authors
+Copyright (c) 2017-2023, the Ginkgo authors
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
