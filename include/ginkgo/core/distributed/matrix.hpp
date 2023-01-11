adf542470498c16f9fa29f92dc51cf8045606e1e
Yu-Hsiang M. Tsai <yhmtsai@gmail.com>
2023-01-11 16:35:58 -0600

    compile for cuda/sycl/test/mpi (hip needs trick)

--- include/ginkgo/core/distributed/matrix.hpp
+++ include/ginkgo/core/distributed/matrix.hpp
@@ -249,10 +249,14 @@ class Matrix
           Matrix<ValueType, LocalIndexType, GlobalIndexType>>,
       public ConvertibleTo<
           Matrix<next_precision<ValueType>, LocalIndexType, GlobalIndexType>>,
+      public ConvertibleTo<Matrix<next_precision<next_precision<ValueType>>,
+                                  LocalIndexType, GlobalIndexType>>,
       public DistributedBase {
     friend class EnableDistributedPolymorphicObject<Matrix, LinOp>;
-    friend class Matrix<next_precision<ValueType>, LocalIndexType,
+    friend class Matrix<previous_precision<ValueType>, LocalIndexType,
                         GlobalIndexType>;
+    friend class Matrix<previous_precision<previous_precision<ValueType>>,
+                        LocalIndexType, GlobalIndexType>;
     friend class multigrid::Pgm<ValueType, LocalIndexType>;
 
 public:
@@ -276,6 +280,13 @@ public:
 
     void move_to(Matrix<next_precision<value_type>, local_index_type,
                         global_index_type>* result) override;
+
+    void convert_to(
+        Matrix<next_precision<next_precision<value_type>>, local_index_type,
+               global_index_type>* result) const override;
+
+    void move_to(Matrix<next_precision<next_precision<value_type>>,
+                        local_index_type, global_index_type>* result) override;
 
     /**
      * Reads a square matrix from the device_matrix_data structure and a global
