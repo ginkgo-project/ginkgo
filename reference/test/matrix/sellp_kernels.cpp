7a59a38e30348e8a73ec8dc17c72fc81995bf9de
Yu-Hsiang M. Tsai <yhmtsai@gmail.com>
2023-01-11 20:56:21 -0600

    fix the narrow issue and atomic support

--- reference/test/matrix/sellp_kernels.cpp
+++ reference/test/matrix/sellp_kernels.cpp
@@ -200,7 +200,9 @@ TYPED_TEST(Sellp, ConvertsToPrecision)
     // If OtherType is more precise: 0, otherwise r
     auto residual = r<OtherType>::value < r<ValueType>::value
                         ? gko::remove_complex<ValueType>{0}
-                        : gko::remove_complex<ValueType>{r<OtherType>::value};
+                        : gko::remove_complex<ValueType>{
+                              static_cast<gko::remove_complex<ValueType>>(
+                                  r<OtherType>::value)};
 
     this->mtx1->convert_to(tmp);
     tmp->convert_to(res);
@@ -221,7 +223,9 @@ TYPED_TEST(Sellp, MovesToPrecision)
     // If OtherType is more precise: 0, otherwise r
     auto residual = r<OtherType>::value < r<ValueType>::value
                         ? gko::remove_complex<ValueType>{0}
-                        : gko::remove_complex<ValueType>{r<OtherType>::value};
+                        : gko::remove_complex<ValueType>{
+                              static_cast<gko::remove_complex<ValueType>>(
+                                  r<OtherType>::value)};
 
     this->mtx1->move_to(tmp);
     tmp->move_to(res);
