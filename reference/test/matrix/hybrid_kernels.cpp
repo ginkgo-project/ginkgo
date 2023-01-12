ce073f0b0fe9d94fd065b66d04e4615c963ab8f7
Yu-Hsiang M. Tsai <yhmtsai@gmail.com>
2023-01-11 20:56:21 -0600

    fix the narrow issue and atomic support

--- reference/test/matrix/hybrid_kernels.cpp
+++ reference/test/matrix/hybrid_kernels.cpp
@@ -241,7 +241,9 @@ TYPED_TEST(Hybrid, ConvertsToPrecision)
     // If OtherType is more precise: 0, otherwise r
     auto residual = r<OtherType>::value < r<ValueType>::value
                         ? gko::remove_complex<ValueType>{0}
-                        : gko::remove_complex<ValueType>{r<OtherType>::value};
+                        : gko::remove_complex<ValueType>{
+                              static_cast<gko::remove_complex<ValueType>>(
+                                  r<OtherType>::value)};
 
     this->mtx1->convert_to(tmp);
     tmp->convert_to(res);
@@ -262,7 +264,9 @@ TYPED_TEST(Hybrid, MovesToPrecision)
     // If OtherType is more precise: 0, otherwise r
     auto residual = r<OtherType>::value < r<ValueType>::value
                         ? gko::remove_complex<ValueType>{0}
-                        : gko::remove_complex<ValueType>{r<OtherType>::value};
+                        : gko::remove_complex<ValueType>{
+                              static_cast<gko::remove_complex<ValueType>>(
+                                  r<OtherType>::value)};
 
     this->mtx1->move_to(tmp);
     tmp->move_to(res);
