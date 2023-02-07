23f41faa3918947a0d21401593b41a37caac6513
Yuhsiang M. Tsai <yhmtsai@gmail.com>
2023-02-07 15:36:55 +0100

    limit the next precision in test and benchmark
    next_precision<double> = float not half

--- reference/test/matrix/sparsity_csr_kernels.cpp
+++ reference/test/matrix/sparsity_csr_kernels.cpp
@@ -149,7 +149,7 @@ TYPED_TEST(SparsityCsr, AppliesToDenseVector)
 
 TYPED_TEST(SparsityCsr, AppliesToMixedDenseVector)
 {
-    using T = gko::next_precision<typename TestFixture::value_type>;
+    using T = next_precision<typename TestFixture::value_type>;
     using Vec = gko::matrix::Dense<T>;
     auto x = gko::initialize<Vec>({2.0, 1.0, 4.0}, this->exec);
     auto y = Vec::create(this->exec, gko::dim<2>{2, 1});
@@ -196,7 +196,7 @@ TYPED_TEST(SparsityCsr, AppliesLinearCombinationToDenseVector)
 
 TYPED_TEST(SparsityCsr, AppliesLinearCombinationToMixedDenseVector)
 {
-    using T = gko::next_precision<typename TestFixture::value_type>;
+    using T = next_precision<typename TestFixture::value_type>;
     using Vec = gko::matrix::Dense<T>;
     auto alpha = gko::initialize<Vec>({-1.0}, this->exec);
     auto beta = gko::initialize<Vec>({2.0}, this->exec);
@@ -247,8 +247,7 @@ TYPED_TEST(SparsityCsr, AppliesToComplex)
 
 TYPED_TEST(SparsityCsr, AppliesToMixedComplex)
 {
-    using T =
-        gko::next_precision<gko::to_complex<typename TestFixture::value_type>>;
+    using T = next_precision<gko::to_complex<typename TestFixture::value_type>>;
     using Vec = gko::matrix::Dense<T>;
     auto x = gko::initialize<Vec>({T{2.0, 4.0}, T{1.0, 2.0}, T{4.0, 8.0}},
                                   this->exec);
@@ -282,8 +281,8 @@ TYPED_TEST(SparsityCsr, AppliesLinearCombinationToComplex)
 
 TYPED_TEST(SparsityCsr, AppliesLinearCombinationToMixedComplex)
 {
-    using Vec = gko::matrix::Dense<
-        gko::next_precision<typename TestFixture::value_type>>;
+    using Vec =
+        gko::matrix::Dense<next_precision<typename TestFixture::value_type>>;
     using ComplexVec = gko::to_complex<Vec>;
     using T = typename ComplexVec::value_type;
     auto alpha = gko::initialize<Vec>({-1.0}, this->exec);
