313773b70d98f1b35ea31eb94c714eb27e278308
aggarwal2000 <aggarwal2000chd@gmail.com>
2023-02-09 11:43:16 +0530

    Fix bug in batch dense convert to csr function

--- reference/test/solver/batch_bicgstab_kernels.cpp
+++ reference/test/solver/batch_bicgstab_kernels.cpp
@@ -309,9 +309,8 @@ TEST(BatchBicgstab, CoreCanSolveDenseWithoutScaling)
             .with_default_max_iterations(maxits)
             .with_default_residual_tol(tol)
             .with_tolerance_type(gko::stop::batch::ToleranceType::relative)
-            .with_preconditioner(gko::preconditioner::BatchJacobi<T>::build()
-                                     .with_max_block_size(1u)
-                                     .on(exec))
+            .with_preconditioner(
+                gko::preconditioner::BatchJacobi<T>::build().on(exec))
             .on(exec);
     const int nrows = 13;
     const size_t nbatch = 3;
