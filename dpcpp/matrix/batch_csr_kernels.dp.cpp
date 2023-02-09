e096ed0cd079a6850c133804c7b93208e9a20fc3
Pratik Nayak <pratikvn@protonmail.com>
2023-02-09 09:21:42 +0100

    Instantiate for int32 only

--- dpcpp/matrix/batch_csr_kernels.dp.cpp
+++ dpcpp/matrix/batch_csr_kernels.dp.cpp
@@ -385,7 +385,8 @@ void batch_scale(std::shared_ptr<const DpcppExecutor> exec,
     });
 }
 
-GKO_INSTANTIATE_FOR_EACH_VALUE_AND_INDEX_TYPE(GKO_DECLARE_BATCH_CSR_SCALE);
+GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE_AND_INT32_INDEX(
+    GKO_DECLARE_BATCH_CSR_SCALE);
 
 #if 1
 template <typename ValueType, typename IndexType>
