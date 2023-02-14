b750f333bf6b2c0bb9397ac295ebb39cc8a0608a
Phuong Nguyen <bichphuong1204@gmail.com>
2023-02-14 16:35:10 +0000

    implemented batch diagonal kernels

--- dpcpp/matrix/batch_diagonal_kernels.dp.cpp
+++ dpcpp/matrix/batch_diagonal_kernels.dp.cpp
@@ -64,6 +64,29 @@ namespace dpcpp {
  */
 namespace batch_diagonal {
 
+template <typename ValueType>
+inline void apply_kernel(sycl::nd_item<3>& item_ct1, const int nrows,
+                         const int ncols, const ValueType* const diag,
+                         const int nrhs, const size_type b_stride,
+                         const ValueType* const b, const size_type x_stride,
+                         ValueType* const x)
+{
+    const int local_id = item_ct1.get_local_linear_id();
+    const int local_range = item_ct1.get_local_range().size();
+
+    const int mindim = min(nrows, ncols);
+    for (int iz = local_id; iz < mindim * nrhs; iz += local_range) {
+        const int row = iz / nrhs;
+        const int col = iz % nrhs;
+        x[row * x_stride + col] = diag[row] * b[row * b_stride + col];
+    }
+    for (int iz = local_id + mindim * nrhs; iz < nrows * nrhs;
+         iz += local_range) {
+        const int row = iz / nrhs;
+        const int col = iz % nrhs;
+        x[row * x_stride + col] = zero<ValueType>();
+    }
+}
 
 template <typename ValueType>
 void apply(std::shared_ptr<const DpcppExecutor> exec,
@@ -71,18 +94,94 @@ void apply(std::shared_ptr<const DpcppExecutor> exec,
            const matrix::BatchDense<ValueType>* const b,
            matrix::BatchDense<ValueType>* const x)
 {
-    GKO_NOT_IMPLEMENTED;
+    if (!b->get_size().stores_equal_sizes()) GKO_NOT_IMPLEMENTED;
+    const auto b_stride = b->get_stride().at();
+    const auto x_stride = x->get_stride().at();
+    const auto num_rows = static_cast<int>(diag->get_size().at()[0]);
+    const auto num_cols = static_cast<int>(diag->get_size().at()[1]);
+    const auto num_rhs = static_cast<int>(x->get_size().at()[1]);
+    const int mindim = min(num_rows, num_cols);
+
+    const auto diag_values = diag->get_const_values();
+    const auto b_values = b->get_const_values();
+    auto x_values = x->get_values();
+
+    const auto num_batches = b->get_num_batch_entries();
+    auto device = exec->get_queue()->get_device();
+    auto group_size =
+        device.get_info<sycl::info::device::max_work_group_size>();
+
+    const dim3 block(group_size);
+    const dim3 grid(num_batches);
+
+    (exec->get_queue())->submit([&](sycl::handler& cgh) {
+        cgh.parallel_for(
+            sycl_nd_range(grid, block), [=](sycl::nd_item<3> item_ct1) {
+                auto group = item_ct1.get_group();
+                auto batch_id = group.get_group_linear_id();
+                const auto b_ptr = gko::batch::batch_entry_ptr(
+                    b_values, b_stride, num_cols, batch_id);
+                const auto x_ptr = gko::batch::batch_entry_ptr(
+                    x_values, x_stride, num_rows, batch_id);
+                const auto d_ptr = gko::batch::batch_entry_ptr(
+                    diag_values, 1, mindim, batch_id);
+                apply_kernel(item_ct1, num_rows, num_cols, d_ptr, num_rhs,
+                             b_stride, b_ptr, x_stride, x_ptr);
+            });
+    });
 }
 
 GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE(GKO_DECLARE_BATCH_DIAGONAL_APPLY_KERNEL);
 
-
+template <typename ValueType>
+inline void apply_in_place_kernel(sycl::nd_item<3>& item_ct1,
+                                  const int num_rows, const size_type stride,
+                                  const int num_rhs,
+                                  const ValueType* const diag_vec,
+                                  ValueType* const a)
+{
+    for (int iz = item_ct1.get_local_linear_id(); iz < num_rows * num_rhs;
+         iz += item_ct1.get_local_range().size()) {
+        const int row = iz / num_rhs;
+        const int col = iz % num_rhs;
+        a[row * stride + col] *= diag_vec[row];
+    }
+}
 template <typename ValueType>
 void apply_in_place(std::shared_ptr<const DpcppExecutor> exec,
                     const matrix::BatchDiagonal<ValueType>* const diag,
                     matrix::BatchDense<ValueType>* const b)
 {
-    GKO_NOT_IMPLEMENTED;
+    if (!diag->get_size().stores_equal_sizes()) GKO_NOT_IMPLEMENTED;
+
+    const auto stride = b->get_stride().at();
+    const auto num_rows = b->get_size().at()[0];
+    const auto num_rhs = b->get_size().at()[1];
+
+    const auto diag_values = diag->get_const_values();
+    auto b_values = b->get_values();
+
+    const auto num_batches = b->get_num_batch_entries();
+    auto device = exec->get_queue()->get_device();
+    auto group_size =
+        device.get_info<sycl::info::device::max_work_group_size>();
+
+    const dim3 block(group_size);
+    const dim3 grid(num_batches);
+
+    (exec->get_queue())->submit([&](sycl::handler& cgh) {
+        cgh.parallel_for(sycl_nd_range(grid, block),
+                         [=](sycl::nd_item<3> item_ct1) {
+                             auto group = item_ct1.get_group();
+                             auto batch_id = group.get_group_linear_id();
+                             const auto b_ptr = gko::batch::batch_entry_ptr(
+                                 b_values, stride, num_rows, batch_id);
+                             const auto d_ptr = gko::batch::batch_entry_ptr(
+                                 diag_values, 1, num_rows, batch_id);
+                             apply_in_place_kernel(item_ct1, num_rows, stride,
+                                                   num_rhs, d_ptr, b_ptr);
+                         });
+    });
 }
 
 GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE(
