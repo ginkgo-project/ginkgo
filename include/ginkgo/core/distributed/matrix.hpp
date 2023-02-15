8fbefe409db515b450fcf49425f9038f5b88e2f3
Marcel Koch <marcel.koch@kit.edu>
2023-02-15 14:05:27 +0100

    enable add scale identity for distributed
    need to use csr for non local mtx

--- include/ginkgo/core/distributed/matrix.hpp
+++ include/ginkgo/core/distributed/matrix.hpp
@@ -247,6 +247,7 @@ template <typename ValueType = default_precision,
 class Matrix
     : public EnableDistributedLinOp<
           Matrix<ValueType, LocalIndexType, GlobalIndexType>>,
+      public ScaledIdentityAddable,
       public ConvertibleTo<
           Matrix<next_precision<ValueType>, LocalIndexType, GlobalIndexType>>,
 #if GINKGO_ENABLE_HALF
@@ -630,6 +631,8 @@ protected:
 
     void apply_impl(const LinOp* alpha, const LinOp* b, const LinOp* beta,
                     LinOp* x) const override;
+
+    void add_scaled_identity_impl(const LinOp* a, const LinOp* b) override;
 
 private:
     std::vector<comm_index_type> send_offsets_;
