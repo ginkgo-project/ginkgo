862e021321a9ae9a0a28c6f720c8b00e8e348f48
Marcel Koch <marcel.koch@kit.edu>
2023-02-21 12:05:35 +0100

    fixing dpcpp

--- dpcpp/distributed/partition_helpers_kernels.dp.cpp
+++ dpcpp/distributed/partition_helpers_kernels.dp.cpp
@@ -45,6 +45,20 @@ namespace kernels {
 namespace dpcpp {
 namespace partition_helpers {
 
+struct stride {
+    // Some version requires [] while some requires (), so I added both
+    template <typename Index>
+    Index operator[](const Index& i) const
+    {
+        return i * 2;
+    }
+
+    template <typename Index>
+    Index operator()(const Index& i) const
+    {
+        return operator[](i);
+    }
+};
 
 template <typename GlobalIndexType>
 void sort_by_range_start(
@@ -55,10 +69,11 @@ void sort_by_range_start(
     auto policy =
         oneapi::dpl::execution::make_device_policy(*exec->get_queue());
     auto num_ranges = range_start_ends.get_num_elems() / 2;
+
     auto start_it = oneapi::dpl::make_permutation_iterator(
-        range_start_ends.get_data(), [](auto i) { return 2 * i; });
+        range_start_ends.get_data(), stride{});
     auto end_it = oneapi::dpl::make_permutation_iterator(
-        range_start_ends.get_data(), [](auto i) { return 2 * i + 1; });
+        range_start_ends.get_data() + 1, stride{});
     auto zip_it =
         oneapi::dpl::make_zip_iterator(start_it, end_it, part_ids.get_data());
     std::stable_sort(policy, zip_it, zip_it + num_ranges, [](auto a, auto b) {
