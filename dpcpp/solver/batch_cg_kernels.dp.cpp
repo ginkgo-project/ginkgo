c29913700817f0a3c3bd3335b1832eedb58229e4
Phuong Nguyen <bichphuong1204@gmail.com>
2023-02-22 03:02:04 +0000

    implemented batch-cg, did not test them yet

--- dpcpp/solver/batch_cg_kernels.dp.cpp
+++ dpcpp/solver/batch_cg_kernels.dp.cpp
@@ -74,34 +74,63 @@ public:
                      const gko::batch_dense::UniformBatch<ValueType>& x) const
     {
         using real_type = typename gko::remove_complex<ValueType>;
-        const size_type num_batch_entries = a.num_batch;
+        const size_type num_batches = a.num_batch;
         const auto nrows = a.num_rows;
         const auto nrhs = b.num_rhs;
         GKO_ASSERT(nrhs == 1);
+
         auto device = exec_->get_queue()->get_device();
         auto group_size =
             device.get_info<sycl::info::device::max_work_group_size>();
-
+        auto subgroup_size =
+            device.get_info<sycl::info::device::sub_group_sizes>()[-1];
         const dim3 block(group_size);
-        const dim3 grid(num_batch_entries);
+        const dim3 grid(num_batches);
 
-        const int workspace_size =
-            gko::kernels::batch_cg::local_memory_requirement<ValueType>(nrows,
-                                                                        nrhs) +
-            PrecType::dynamic_work_size(nrows, a.num_nnz) * sizeof(ValueType);
+        const auto slm_size =
+            device.get_info<sycl::info::device::local_mem_size>();
+        const auto shmem_per_blk =
+            slm_size -
+            5 * sizeof(ValueType);  // reserve 5 for intermediate rho-s
+        const int shared_gap =
+            a.num_rows;             // TODO: check if it is neccessary to align
+        const size_type prec_size =
+            PrecType::dynamic_work_size(shared_gap, a.num_nnz) *
+            sizeof(ValueType);
+        const auto sconf =
+            gko::kernels::batch_cg::compute_shared_storage<PrecType, ValueType>(
+                shmem_per_blk, shared_gap, a.num_nnz, b.num_rhs);
+        const size_t shared_size =
+            sconf.n_shared * shared_gap * sizeof(ValueType) +
+            (sconf.prec_shared ? prec_size : 0);
         auto workspace = gko::Array<ValueType>(
-            exec_, workspace_size * num_batch_entries / sizeof(ValueType));
+            exec_, sconf.gmem_stride_bytes * num_batches / sizeof(ValueType));
+        assert(sconf.gmem_stride_bytes % sizeof(ValueType) == 0);
+
+        /*
+                const int workspace_size =
+                    gko::kernels::batch_cg::local_memory_requirement<ValueType>(nrows,
+                                                                                nrhs) +
+                    PrecType::dynamic_work_size(nrows, a.num_nnz) *
+           sizeof(ValueType); auto workspace = gko::Array<ValueType>( exec_,
+           workspace_size * num_batch_entries / sizeof(ValueType));
+                    */
         ValueType* const workspace_data = workspace.get_data();
         auto max_iters = opts_.max_its;
         auto res_tol = opts_.residual_tol;
 
         (exec_->get_queue())->submit([&](sycl::handler& cgh) {
+            sycl::accessor<ValueType, 1, sycl::access_mode::read_write,
+                           sycl::access::target::local>
+                slm_storage(sycl::range<1>(shmem_per_blk), cgh);
+
             cgh.parallel_for(
                 sycl_nd_range(grid, block), [=](sycl::nd_item<3> item_ct1) {
                     auto group = item_ct1.get_group();
                     auto group_id = group.get_group_linear_id();
-                    apply_kernel<StopType>(item_ct1, max_iters, res_tol, logger,
-                                           prec, a, b.values, x.values);
+                    apply_kernel(sconf, max_iters, res_tol, logger, prec, a,
+                                 b.values, x.values, slm_storage.get_pointer(),
+                                 item_ct1, workspace_data);
                 });
         });
     }
--- dpcpp/solver/batch_cg_kernels.hpp
+++ dpcpp/solver/batch_cg_kernels.hpp
@@ -29,7 +29,7 @@ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ******************************<GINKGO LICENSE>*******************************/
-#ifndef GKO_DPCPP_SOLVER_BATCH_CG_KERNELS_HPP_
+
 #define GKO_DPCPP_SOLVER_BATCH_CG_KERNELS_HPP_
 
 
@@ -40,236 +40,255 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "dpcpp/matrix/batch_dense_kernels.hpp"
 #include "dpcpp/matrix/batch_ell_kernels.hpp"
 #include "dpcpp/matrix/batch_struct.hpp"
+#include "dpcpp/matrix/batch_vector_kernels.hpp"
 
 
 // namespace {
 
 
-// template <typename BatchMatrixType_entry, typename PrecType, typename
-// ValueType>
-// __device__ __forceinline__ void initialize(
-//     group::thread_block_tile<config::warp_size>& warp_grp, const int
-//     num_rows, const BatchMatrixType_entry& A_global_entry, const ValueType*
-//     const b_global_entry, const ValueType* const x_global_entry, ValueType*
-//     const x_shared_entry, ValueType* const r_shared_entry, const PrecType&
-//     prec_shared, ValueType* const z_shared_entry, ValueType&
-//     rho_old_shared_entry, ValueType* const p_shared_entry, typename
-//     gko::remove_complex<ValueType>& rhs_norms_sh)
-// {
-//     // copy x from global to shared memory
-//     // r = b
-//     for (int iz = threadIdx.x; iz < num_rows; iz += blockDim.x) {
-//         x_shared_entry[iz] = x_global_entry[iz];
-//         r_shared_entry[iz] = b_global_entry[iz];
-//     }
-//     __syncthreads();
+template <typename BatchMatrixType_entry, typename PrecType, typename ValueType>
+__dpct_inline__ void initialize(
+    const int num_rows, const BatchMatrixType_entry& A_global_entry,
+    const ValueType* const b_global_entry,
+    const ValueType* const x_global_entry, ValueType* const x_shared_entry,
+    ValueType* const r_shared_entry, const PrecType& prec_shared,
+    ValueType* const z_shared_entry, ValueType& rho_old_shared_entry,
+    ValueType* const p_shared_entry,
+    typename gko::remove_complex<ValueType>& rhs_norms_sh,
+    sycl::nd_item<3> item_ct1)
+{
+    auto sg = item_ct1.get_sub_group();
+    const auto sg_id = sg.get_group_id();
+    const auto tid = item_ct1.get_local_linear_id();
+    const auto group_size = item_ct1.get_local_range().size();
+    // copy x from global to shared memory
+    // r = b
+    for (int iz = tid; iz < num_rows; iz += group_size) {
+        x_shared_entry[iz] = x_global_entry[iz];
+        r_shared_entry[iz] = b_global_entry[iz];
+    }
+    item_ct1.barrier(sycl::access::fence_space::local_space);
 
-//     // r = b - A*x
-//     single_advanced_matvec_kernel(static_cast<ValueType>(-1.0),
-//     A_global_entry,
-//                                   x_shared_entry,
-//                                   static_cast<ValueType>(1.0),
-//                                   r_shared_entry);
-//     __syncthreads();
-
-//     // z = precond * r
-//     prec_shared.apply(num_rows, r_shared_entry, z_shared_entry);
-//     __syncthreads();
-
-//     if (threadIdx.x / config::warp_size == 0) {
-//         // Compute norms of rhs
-//         single_compute_norm2(warp_grp, num_rows, b_global_entry,
-//         rhs_norms_sh);
-//     } else if (threadIdx.x / config::warp_size == 1) {
-//         // rho_old = r' * z
-//         single_compute_dot_product(warp_grp, num_rows, r_shared_entry,
-//                                    z_shared_entry, rho_old_shared_entry);
-//     }
-
-//     // p = z
-//     for (int iz = threadIdx.x; iz < num_rows; iz += blockDim.x) {
-//         p_shared_entry[iz] = z_shared_entry[iz];
-//     }
-// }
+    // r = b - A*x
+    advanced_matvec_kernel(static_cast<ValueType>(-1.0), A_global_entry,
+                           x_shared_entry, static_cast<ValueType>(1.0),
+                           r_shared_entry, item_ct1);
+    item_ct1.barrier(sycl::access::fence_space::local_space);
 
 
-// template <typename ValueType>
-// __device__ __forceinline__ void update_p(const int num_rows,
-//                                          const ValueType&
-//                                          rho_new_shared_entry, const
-//                                          ValueType& rho_old_shared_entry,
-//                                          const ValueType* const
-//                                          z_shared_entry, ValueType* const
-//                                          p_shared_entry)
-// {
-//     for (int li = threadIdx.x; li < num_rows; li += blockDim.x) {
-//         const ValueType beta = rho_new_shared_entry / rho_old_shared_entry;
-//         p_shared_entry[li] = z_shared_entry[li] + beta * p_shared_entry[li];
-//     }
-// }
+    // z = precond * r
+    prec_shared.apply(num_rows, r_shared_entry, z_shared_entry, item_ct1);
+    item_ct1.barrier(sycl::access::fence_space::local_space);
+
+    if (sg_id == 0) {
+        // Compute norms of rhs
+        compute_norm2_sg_kernel(num_rows, b_global_entry, rhs_norms_sh,
+                                item_ct1);
+    } else if (sg_id == 1) {
+        // rho_old = r' * z
+        compute_dot_product_sg_kernel(num_rows, r_shared_entry, z_shared_entry,
+                                      rho_old_shared_entry, item_ct1);
+    }
+
+    // p = z
+    for (int iz = tid; iz < num_rows; iz += group_size) {
+        p_shared_entry[iz] = z_shared_entry[iz];
+    }
+    item_ct1.barrier(sycl::access::fence_space::local_space);
+}
 
 
-// template <typename ValueType>
-// __device__ __forceinline__ void update_x_and_r(
-//     group::thread_block_tile<config::warp_size>& warp_grp, const int
-//     num_rows, const ValueType& rho_old_shared_entry, const ValueType* const
-//     p_shared_entry, const ValueType* const Ap_shared_entry, ValueType&
-//     alpha_shared_entry, ValueType* const x_shared_entry, ValueType* const
-//     r_shared_entry)
-// {
-//     if (threadIdx.x / config::warp_size == 0) {
-//         single_compute_dot_product(warp_grp, num_rows, p_shared_entry,
-//                                    Ap_shared_entry, alpha_shared_entry);
-//     }
-//     __syncthreads();
+template <typename ValueType>
+__dpct_inline__ void update_p(const int num_rows,
+                              const ValueType& rho_new_shared_entry,
+                              const ValueType& rho_old_shared_entry,
+                              const ValueType* const z_shared_entry,
+                              ValueType* const p_shared_entry,
+                              sycl::nd_item<3> item_ct1)
+{
+    for (int li = item_ct1.get_local_linear_id(); li < num_rows;
+         li += item_ct1.get_local_range().size()) {
+        const ValueType beta = rho_new_shared_entry / rho_old_shared_entry;
+        p_shared_entry[li] = z_shared_entry[li] + beta * p_shared_entry[li];
+    }
+}
 
-//     for (int li = threadIdx.x; li < num_rows; li += blockDim.x) {
-//         const ValueType alpha = rho_old_shared_entry / alpha_shared_entry;
-//         x_shared_entry[li] += alpha * p_shared_entry[li];
-//         r_shared_entry[li] -= alpha * Ap_shared_entry[li];
-//     }
-// }
 
-// }  // namespace
+template <typename ValueType>
+__dpct_inline__ void update_x_and_r(const int num_rows,
+                                    const ValueType& rho_old_shared_entry,
+                                    const ValueType* const p_shared_entry,
+                                    const ValueType* const Ap_shared_entry,
+                                    ValueType& alpha_shared_entry,
+                                    ValueType* const x_shared_entry,
+                                    ValueType* const r_shared_entry,
+                                    sycl::nd_item<3> item_ct1)
+{
+    auto sg = item_ct1.get_sub_group();
+    const auto sg_id = sg.get_group_id();
+
+    if (sg_id == 0) {
+        compute_dot_product_sg_kernel(num_rows, p_shared_entry, Ap_shared_entry,
+                                      alpha_shared_entry, item_ct1);
+    }
+    item_ct1.barrier(sycl::access::fence_space::local_space);
+
+    for (int li = item_ct1.get_local_linear_id(); li < num_rows;
+         li += item_ct1.get_local_range().size()) {
+        const ValueType alpha = rho_old_shared_entry / alpha_shared_entry;
+        x_shared_entry[li] += alpha * p_shared_entry[li];
+        r_shared_entry[li] -= alpha * Ap_shared_entry[li];
+    }
+}
+
+//}  // namespace
 
 
 template <typename StopType, typename PrecType, typename LogType,
           typename BatchMatrixType, typename ValueType>
-void apply_kernel(sycl::nd_item<3>& item_ct1, const int max_iter,
-                  const gko::remove_complex<ValueType> tol, LogType logger,
-                  PrecType prec_shared, const BatchMatrixType a,
+void apply_kernel(const gko::kernels::batch_cg::StorageConfig sconf,
+                  const int max_iter, const gko::remove_complex<ValueType> tol,
+                  LogType logger, PrecType prec_shared, const BatchMatrixType a,
                   const ValueType* const __restrict__ b,
                   ValueType* const __restrict__ x,
+                  ValueType* const local_mem_sh, sycl::nd_item<3> item_ct1,
                   ValueType* const __restrict__ workspace = nullptr)
 {
-    // TODO
-    // using real_type = typename gko::remove_complex<ValueType>;
-    // const auto nbatch = a.num_batch;
-    // const auto nrows = a.num_rows;
+    using real_type = typename gko::remove_complex<ValueType>;
+    const auto nbatch = a.num_batch;
+    const auto nrows = a.num_rows;
 
-    // constexpr auto tile_size = config::warp_size;
-    // auto thread_block = group::this_thread_block();
-    // auto warp_grp = group::tiled_partition<tile_size>(thread_block);
+    const auto sg = item_ct1.get_sub_group();
+    const int sg_id = sg.get_group_id();
+    const int sg_size = sg.get_local_range().size();
+    const int num_sg = sg.get_group_range().size();
 
-    // for (size_type ibatch = blockIdx.x; ibatch < nbatch; ibatch += gridDim.x)
-    // {
-    //     const int gmem_offset =
-    //         ibatch * sconf.gmem_stride_bytes / sizeof(ValueType);
-    //     extern __shared__ char local_mem_sh[];
-    //     ValueType* r_sh;
-    //     ValueType* z_sh;
-    //     ValueType* p_sh;
-    //     ValueType* Ap_sh;
-    //     ValueType* x_sh;
-    //     ValueType* prec_work_sh;
-    //     if (sconf.n_shared >= 1) {
-    //         r_sh = reinterpret_cast<ValueType*>(local_mem_sh);
-    //     } else {
-    //         r_sh = workspace + gmem_offset;
-    //     }
-    //     if (sconf.n_shared == 1) {
-    //         z_sh = workspace + gmem_offset;
-    //     } else {
-    //         z_sh = r_sh + sconf.padded_vec_len;
-    //     }
-    //     if (sconf.n_shared == 2) {
-    //         p_sh = workspace + gmem_offset;
-    //     } else {
-    //         p_sh = z_sh + sconf.padded_vec_len;
-    //     }
-    //     if (sconf.n_shared == 3) {
-    //         Ap_sh = workspace + gmem_offset;
-    //     } else {
-    //         Ap_sh = p_sh + sconf.padded_vec_len;
-    //     }
-    //     if (!sconf.prec_shared && sconf.n_shared == 4) {
-    //         prec_work_sh = workspace + gmem_offset;
-    //     } else {
-    //         prec_work_sh = Ap_sh + sconf.padded_vec_len;
-    //     }
-    //     if (sconf.n_shared == 4 && sconf.prec_shared) {
-    //         x_sh = workspace + gmem_offset;
-    //     } else {
-    //         x_sh = prec_work_sh + PrecType::dynamic_work_size(nrows,
-    //         a.num_nnz);
-    //     }
-    //     __shared__ uninitialized_array<ValueType, 1> rho_old_sh;
-    //     __shared__ uninitialized_array<ValueType, 1> rho_new_sh;
-    //     __shared__ uninitialized_array<ValueType, 1> alpha_sh;
-    //     __shared__ real_type norms_rhs_sh[1];
-    //     __shared__ real_type norms_res_sh[1];
+    const auto group = item_ct1.get_group();
+    const auto ibatch = group.get_group_linear_id();
 
-    //     const auto A_global_entry = gko::batch::batch_entry(a, ibatch);
-    //     const ValueType* const b_global_entry =
-    //         gko::batch::batch_entry_ptr(b, 1, nrows, ibatch);
-    //     ValueType* const x_global_entry =
-    //         gko::batch::batch_entry_ptr(x, 1, nrows, ibatch);
+    /*
+       __shared__ uninitialized_array<ValueType, 1> rho_old_sh;
+       __shared__ uninitialized_array<ValueType, 1> rho_new_sh;
+       __shared__ uninitialized_array<ValueType, 1> alpha_sh;
+       __shared__ real_type norms_rhs_sh[1];
+       __shared__ real_type norms_res_sh[1];
+     */
+    auto& rho_old_sh = local_mem_sh[0];
+    auto& rho_new_sh = local_mem_sh[1];
+    auto& alpha_sh = local_mem_sh[2];
+    auto& norms_rhs_sh = local_mem_sh[3];
+    auto& norms_res_sh = local_mem_sh[4];
 
-    //     // generate preconditioner
-    //     prec_shared.generate(ibatch, A_global_entry, prec_work_sh);
+    const int gmem_offset =
+        ibatch * sconf.gmem_stride_bytes / sizeof(ValueType);
+    //        extern __shared__ char local_mem_sh[];
+    ValueType* r_sh;
+    ValueType* z_sh;
+    ValueType* p_sh;
+    ValueType* Ap_sh;
+    ValueType* x_sh;
+    ValueType* prec_work_sh;
+    if (sconf.n_shared >= 1) {
+        // r_sh = reinterpret_cast<ValueType*>(local_mem_sh);
+        r_sh = local_mem_sh[5];
+    } else {
+        r_sh = workspace + gmem_offset;
+    }
+    if (sconf.n_shared == 1) {
+        z_sh = workspace + gmem_offset;
+    } else {
+        z_sh = r_sh + sconf.padded_vec_len;
+    }
+    if (sconf.n_shared == 2) {
+        p_sh = workspace + gmem_offset;
+    } else {
+        p_sh = z_sh + sconf.padded_vec_len;
+    }
+    if (sconf.n_shared == 3) {
+        Ap_sh = workspace + gmem_offset;
+    } else {
+        Ap_sh = p_sh + sconf.padded_vec_len;
+    }
+    if (!sconf.prec_shared && sconf.n_shared == 4) {
+        prec_work_sh = workspace + gmem_offset;
+    } else {
+        prec_work_sh = Ap_sh + sconf.padded_vec_len;
+    }
+    if (sconf.n_shared == 4 && sconf.prec_shared) {
+        x_sh = workspace + gmem_offset;
+    } else {
+        x_sh = prec_work_sh + PrecType::dynamic_work_size(nrows, a.num_nnz);
+    }
 
-    //     // initialization
-    //     // compute b norms
-    //     // r = b - A*x
-    //     // z = precond*r
-    //     // rho_old = r' * z (' is for hermitian transpose)
-    //     // p = z
-    //     initialize(warp_grp, nrows, A_global_entry, b_global_entry,
-    //                x_global_entry, x_sh, r_sh, prec_shared, z_sh,
-    //                rho_old_sh[0], p_sh, norms_rhs_sh[0]);
-    //     __syncthreads();
+    const auto A_global_entry = gko::batch::batch_entry(a, ibatch);
+    const ValueType* const b_global_entry =
+        gko::batch::batch_entry_ptr(b, 1, nrows, ibatch);
+    ValueType* const x_global_entry =
+        gko::batch::batch_entry_ptr(x, 1, nrows, ibatch);
 
-    //     // stopping criterion object
-    //     StopType stop(tol, norms_rhs_sh);
+    // generate preconditioner
+    prec_shared.generate(A_global_entry, prec_work_sh, item_ct1);
 
-    //     int iter = 0;
-    //     for (; iter < max_iter; iter++) {
-    //         norms_res_sh[0] = sqrt(abs(rho_old_sh[0]));
-    //         __syncthreads();
-    //         if (stop.check_converged(norms_res_sh)) {
-    //             break;
-    //         }
+    // initialization
+    // compute b norms
+    // r = b - A*x
+    // z = precond*r
+    // rho_old = r' * z (' is for hermitian transpose)
+    // p = z
+    initialize(nrows, A_global_entry, b_global_entry, x_global_entry, x_sh,
+               r_sh, prec_shared, z_sh, rho_old_sh, p_sh, norms_rhs_sh,
+               item_ct1);
 
-    //         // Ap = A * p
-    //         single_matvec_kernel(A_global_entry, p_sh, Ap_sh);
-    //         __syncthreads();
+    // stopping criterion object
+    StopType stop(tol, norms_rhs_sh);
 
-    //         // alpha = rho_old / (p' * Ap)
-    //         // x = x + alpha * p
-    //         // r = r - alpha * Ap
-    //         update_x_and_r(warp_grp, nrows, rho_old_sh[0], p_sh, Ap_sh,
-    //                        alpha_sh[0], x_sh, r_sh);
-    //         __syncthreads();
+    int iter = 0;
+    for (; iter < max_iter; iter++) {
+        norms_res_sh = sqrt(abs(rho_old_sh));
+        //            __syncthreads();
+        if (stop.check_converged(norms_res_sh)) {
+            break;
+        }
 
-    //         // z = precond * r
-    //         prec_shared.apply(nrows, r_sh, z_sh);
-    //         __syncthreads();
+        // Ap = A * p
+        matvec_kernel(A_global_entry, p_sh, Ap_sh, item_ct1);
+        item_ct1.barrier(sycl::access::fence_space::local_space);
 
-    //         if (threadIdx.x / config::warp_size == 0) {
-    //             // rho_new =  (r)' * (z)
-    //             single_compute_dot_product(warp_grp, nrows, r_sh, z_sh,
-    //                                        rho_new_sh[0]);
-    //         }
-    //         __syncthreads();
+        // alpha = rho_old / (p' * Ap)
+        // x = x + alpha * p
+        // r = r - alpha * Ap
+        update_x_and_r(nrows, rho_old_sh, p_sh, Ap_sh, alpha_sh, x_sh, r_sh,
+                       item_ct1);
+        item_ct1.barrier(sycl::access::fence_space::local_space);
 
-    //         // beta = rho_new / rho_old
-    //         // p = z + beta * p
-    //         update_p(nrows, rho_new_sh[0], rho_old_sh[0], z_sh, p_sh);
-    //         __syncthreads();
+        // z = precond * r
+        prec_shared.apply(nrows, r_sh, z_sh, item_ct1);
+        item_ct1.barrier(sycl::access::fence_space::local_space);
 
-    //         // rho_old = rho_new
-    //         if (threadIdx.x == 0) {
-    //             rho_old_sh[0] = rho_new_sh[0];
-    //         }
-    //         __syncthreads();
-    //     }
+        if (sg_id == 0) {
+            // rho_new =  (r)' * (z)
+            compute_dot_product_sg_kernel(nrows, r_sh, z_sh, rho_new_sh,
+                                          item_ct1);
+        }
+        item_ct1.barrier(sycl::access::fence_space::local_space);
 
-    //     logger.log_iteration(ibatch, iter, norms_res_sh[0]);
+        // beta = rho_new / rho_old
+        // p = z + beta * p
+        update_p(nrows, rho_new_sh, rho_old_sh, z_sh, p_sh, item_ct1);
+        item_ct1.barrier(sycl::access::fence_space::local_space);
 
-    //     // copy x back to global memory
-    //     single_copy(nrows, x_sh, x_global_entry);
-    //     __syncthreads();
-    // }
+        // rho_old = rho_new
+        if (item_ct1.get_local_linear_id() == 0) {
+            rho_old_sh = rho_new_sh;
+        }
+        item_ct1.barrier(sycl::access::fence_space::local_space);
+    }
+
+    logger.log_iteration(ibatch, iter, norms_res_sh[0]);
+
+    // copy x back to global memory
+    single_copy_kernel(nrows, x_sh, x_global_entry, item_ct1);
+    item_ct1.barrier(sycl::access::fence_space::local_space);
 }
 
 #endif
