62e81b743b8e077c92efcc180960b37f67a64996
Yu-Hsiang M. Tsai <yhmtsai@gmail.com>
2023-02-23 18:45:24 +0100

    init property tree

---
+++ extension/property_tree/test/utils.hpp
@@ -0,0 +1,42 @@
+#ifndef GKO_EXT_PROPERTY_TREE_TEST_UTILS_HPP_
+#define GKO_EXT_PROPERTY_TREE_TEST_UTILS_HPP_
+
+
+#include <ostream>
+
+#include "property_tree/property_tree.hpp"
+
+namespace gko {
+namespace extension {
+// For debug usage
+void print(std::ostream& stream, const pnode& tree, int offset = 0,
+           bool is_array = false)
+{
+    std::string offset_str(offset, ' ');
+    stream << offset_str;
+    if (!is_array) {
+        stream << tree.get_name() << ": ";
+    }
+    if (tree.is(pnode::status_t::array)) {
+        stream << "[" << std::endl;
+        for (const auto node : tree.get_child_list()) {
+            print(stream, node, offset + 2, true);
+        }
+        stream << offset_str << "]" << std::endl;
+    } else if (tree.is(pnode::status_t::object_list)) {
+        stream << "{" << std::endl;
+        for (const auto node : tree.get_child_list()) {
+            print(stream, node, offset + 2);
+        }
+        stream << offset_str << "}" << std::endl;
+    } else if (tree.is(pnode::status_t::object)) {
+        stream << tree.get() << std::endl;
+    } else {
+        stream << "empty_node" << std::endl;
+    }
+}
+
+}  // namespace extension
+}  // namespace gko
+
+#endif  // GKO_EXT_PROPERTY_TREE_TEST_UTILS_HPP_
