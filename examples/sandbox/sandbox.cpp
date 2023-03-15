27dd50f68ba6c65e52facac2c885abfca342084e
Tobias Ribizel <ribizel@kit.edu>
2023-03-15 13:57:29 +0100

    tmp sandbox

---
+++ examples/sandbox/sandbox.cpp
@@ -0,0 +1,79 @@
+#include <ginkgo/ginkgo.hpp>
+
+#include <iostream>
+#include "ginkgo/core/base/executor.hpp"
+#include "ginkgo/core/matrix/csr.hpp"
+
+using gko::size_type;
+using gko::zero;
+
+
+template <typename NonzeroIterator>
+auto get_next_value(NonzeroIterator& it, const NonzeroIterator& end,
+                    size_type next_row, size_type next_col) ->
+    typename std::decay<decltype(it->value)>::type
+{
+    if (it != end && it->row == next_row && it->column == next_col) {
+        return (it++)->value;
+    } else {
+        return zero<typename std::decay<decltype(it->value)>::type>();
+    }
+}
+
+template <typename Ostream, typename MatrixData1, typename MatrixData2>
+void print_sparsity_pattern(Ostream& os, const MatrixData1& first,
+                            const MatrixData2& second)
+{
+    auto first_it = first.nonzeros.begin();
+    auto second_it = second.nonzeros.begin();
+    os << ' ';
+    for (size_type col = 0; col < first.size[1]; col++) {
+        os << (col % 10);
+    }
+    os << '\n';
+    for (size_type row = 0; row < first.size[0]; row++) {
+        os << (row % 10);
+        for (size_type col = 0; col < first.size[1]; col++) {
+            const auto has_first =
+                get_next_value(first_it, end(first.nonzeros), row, col) !=
+                zero<typename MatrixData1::value_type>();
+            const auto has_second =
+                get_next_value(second_it, end(second.nonzeros), row, col) !=
+                zero<typename MatrixData2::value_type>();
+            if (has_first) {
+                if (has_second) {
+                    os << '+';
+                } else {
+                    os << '|';
+                }
+            } else {
+                if (has_second) {
+                    os << '-';
+                } else {
+                    os << ' ';
+                }
+            }
+        }
+        os << (row % 10) << '\n';
+    }
+    os << ' ';
+    for (size_type col = 0; col < first.size[1]; col++) {
+        os << (col % 10);
+    }
+    os << "\n'|' is first, '-' is second, '+' is both, ' ' is none\n";
+}
+
+
+int main(int argc, char* argv[])
+{
+    auto exec = gko::ReferenceExecutor::create();
+    auto mtx = gko::share(gko::read<gko::matrix::Csr<>>(std::cin, exec));
+    auto perm = gko::reorder::Amd<int>::build().on(exec)->generate(mtx);
+    auto arr = gko::make_array_view(exec, perm->get_size()[0],
+                                    perm->get_permutation());
+    gko::matrix_data<> data;
+    gko::as<gko::matrix::Csr<>>(mtx->permute(&arr))->write(data);
+    data.ensure_row_major_order();
+    gko::write_raw(std::cout, data);
+    // print_sparsity_pattern(std::cout, data, data);
+}
