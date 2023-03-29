5c0b01b1e60447f6a305d4826edddfa1e9381496
Phuong Nguyen <bichphuong1204@gmail.com>
2023-03-29 18:18:32 +0000

    batch_block_jacobi passed test for float and double, but not for cmplx

--- test/preconditioner/batch_jacobi_kernels.cpp
+++ test/preconditioner/batch_jacobi_kernels.cpp
@@ -48,6 +48,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "core/test/utils.hpp"
 #include "core/test/utils/batch.hpp"
 #include "test/utils/executor.hpp"
+#include "core/preconditioner/jacobi_kernels.hpp"
 
 
 namespace {
@@ -69,8 +70,9 @@ template <typename T>
 class BatchJacobi : public CommonTestFixture {
 protected:
     using value_type = T;
+    using index_type = int;
     using real_type = gko::remove_complex<value_type>;
-    using Mtx = gko::matrix::BatchCsr<value_type, int>;
+    using Mtx = gko::matrix::BatchCsr<value_type, index_type>;
     using BDense = gko::matrix::BatchDense<value_type>;
     using BJ = gko::preconditioner::BatchJacobi<value_type>;
 
@@ -112,12 +114,12 @@ protected:
                                     ->generate(ref_mtx);
 
         // so that the block pointers are exactly the same for ref and device
-        const int* block_pointers_generated_by_ref =
+        const index_type* block_pointers_generated_by_ref =
             ref_block_jacobi_prec->get_const_block_pointers();
         const auto num_blocks_generated_by_ref =
             ref_block_jacobi_prec->get_num_blocks();
 
-        gko::array<int> block_pointers_for_device(
+        gko::array<index_type> block_pointers_for_device(
             this->exec, block_pointers_generated_by_ref,
             block_pointers_generated_by_ref + num_blocks_generated_by_ref + 1);
 
@@ -144,10 +146,10 @@ protected:
     std::unique_ptr<BJ> d_block_jacobi_prec;
 };
 
-template <typename ValueType>
+template <typename ValueType, typename IndexType>
 void check_device_block_jacobi_equivalent_to_ref(
-    std::unique_ptr<gko::preconditioner::BatchJacobi<ValueType>> ref_prec,
-    std::unique_ptr<gko::preconditioner::BatchJacobi<ValueType>> d_prec)
+    std::unique_ptr<gko::preconditioner::BatchJacobi<ValueType, IndexType>> ref_prec,
+    std::unique_ptr<gko::preconditioner::BatchJacobi<ValueType, IndexType>> d_prec)
 {
     auto ref = ref_prec->get_executor();
     auto exec = d_prec->get_executor();
@@ -160,18 +162,18 @@ void check_device_block_jacobi_equivalent_to_ref(
 
     const auto tol = 100000 * r<ValueType>::value;
 
-    gko::array<int> d_block_pointers_copied_to_ref(ref, num_blocks + 1);
+    gko::array<IndexType> d_block_pointers_copied_to_ref(ref, num_blocks + 1);
     ref->copy_from(exec.get(), num_blocks + 1,
                    d_prec->get_const_block_pointers(),
                    d_block_pointers_copied_to_ref.get_data());
 
-    gko::array<int> d_block_cumul_storage_copied_to_ref(ref, num_blocks + 1);
+    gko::array<IndexType> d_block_cumul_storage_copied_to_ref(ref, num_blocks + 1);
     ref->copy_from(exec.get(), num_blocks + 1,
                    d_prec->get_const_blocks_cumulative_storage(),
                    d_block_cumul_storage_copied_to_ref.get_data());
 
-    for (int batch_id = 0; batch_id < nbatch; batch_id++) {
-        for (int block_id = 0; block_id < num_blocks; block_id++) {
+    for (IndexType batch_id = 0; batch_id < nbatch; batch_id++) {
+        for (IndexType block_id = 0; block_id < num_blocks; block_id++) {
             const auto bsize =
                 block_pointers_ref[block_id + 1] - block_pointers_ref[block_id];
 
@@ -190,8 +192,8 @@ void check_device_block_jacobi_equivalent_to_ref(
             const auto d_stride = d_storage_scheme.get_stride(
                 block_id, d_block_pointers_copied_to_ref.get_const_data());
 
-            for (int r = 0; r < bsize; r++) {
-                for (int c = 0; c < bsize; c++) {
+            for (IndexType r = 0; r < bsize; r++) {
+                for (IndexType c = 0; c < bsize; c++) {
                     const auto ref_val_ptr =
                         ref_dense_block_ptr + r * ref_stride + c;
                     const auto d_val_ptr = d_dense_block_ptr + r * d_stride + c;
@@ -212,13 +214,14 @@ TYPED_TEST_SUITE(BatchJacobi, gko::test::ValueTypes);
 TYPED_TEST(BatchJacobi, BatchScalarJacobiApplyToSingleVectorIsEquivalentToRef)
 {
     using value_type = typename TestFixture::value_type;
+    using index_type = typename TestFixture::index_type;
 
     auto& ref_prec = this->ref_scalar_jacobi_prec;
 
     value_type* blocks_arr_ref = nullptr;
-    int* block_ptr_ref = nullptr;
-    int* row_part_of_which_block_ref = nullptr;
-    int* cumul_block_storage_ref = nullptr;
+    index_type* block_ptr_ref = nullptr;
+    index_type* row_part_of_which_block_ref = nullptr;
+    index_type* cumul_block_storage_ref = nullptr;
 
     gko::kernels::reference::batch_jacobi::batch_jacobi_apply(
         this->ref, this->ref_mtx.get(), ref_prec->get_num_blocks(),
@@ -229,9 +232,9 @@ TYPED_TEST(BatchJacobi, BatchScalarJacobiApplyToSingleVectorIsEquivalentToRef)
     auto& d_prec = this->d_scalar_jacobi_prec;
 
     value_type* blocks_arr_d = nullptr;
-    int* block_ptr_d = nullptr;
-    int* row_part_of_which_block_d = nullptr;
-    int* cumul_block_storage_d = nullptr;
+    index_type* block_ptr_d = nullptr;
+    index_type* row_part_of_which_block_d = nullptr;
+    index_type* cumul_block_storage_d = nullptr;
 
     gko::kernels::EXEC_NAMESPACE::batch_jacobi::batch_jacobi_apply(
         this->exec, this->d_mtx.get(), d_prec->get_num_blocks(),
@@ -257,6 +260,7 @@ TYPED_TEST(BatchJacobi, BatchBlockJacobiGenerationIsEquivalentToRef)
 TYPED_TEST(BatchJacobi, BatchBlockJacobiApplyToSingleVectorIsEquivalentToRef)
 {
     using value_type = typename TestFixture::value_type;
+    using index_type = typename TestFixture::index_type;
 
     auto& ref_prec = this->ref_block_jacobi_prec;
 
@@ -311,5 +315,43 @@ TYPED_TEST(BatchJacobi, BatchBlockJacobiConjugateTransposeIsEquivalentToRef)
                                                 std::move(d_prec_conj_trans));
 }
 
+//TYPED_TEST(BatchJacobi, BatchBlockJacobiFindRowIsPartOfWhichBlock)
+TYPED_TEST(BatchJacobi, BatchBlockJacobiFindRowANDComputeCumulativeBlock)
+{
+    using value_type = typename TestFixture::value_type;
+    using index_type = typename TestFixture::index_type;
+
+    const auto num_batch = this->ref_mtx->get_num_batch_entries();
+    const auto num_rows = this->ref_mtx->get_size().at(0)[0];
+    const auto num_nz = this->ref_mtx->get_num_stored_elements() / num_batch;
+
+    const auto unbatch_size =
+        gko::dim<2>{num_rows, this->ref_mtx->get_size().at(0)[1]};
+    auto sys_rows_view = gko::array<index_type>::const_view(
+            this->ref, num_rows + 1, this->ref_mtx->get_const_row_ptrs());
+    auto sys_cols_view = gko::array<index_type>::const_view(
+            this->ref, num_nz, this->ref_mtx->get_const_col_idxs());
+    auto sys_vals_view = gko::array<value_type>::const_view(this->ref, num_nz, this->ref_mtx->get_const_values());
+    auto first_sys_csr = gko::share(gko::matrix::Csr<value_type, index_type>::create_const( this->ref, unbatch_size, std::move(sys_vals_view), std::move(sys_cols_view), std::move(sys_rows_view)));
+
+    gko::array<index_type> block_pointers(this->ref, first_sys_csr->get_size()[0]+1);
+    gko::size_type num_blocks;
+    gko::kernels::reference::jacobi::find_blocks(this->ref, first_sys_csr.get(), this->max_blk_sz, num_blocks, block_pointers);
+    gko::array<index_type> d_block_pointers(this->exec, block_pointers);
+
+    // Testing function find_row_is_part_of_which_block()
+    gko::array<index_type> row_part_of_which_block_ref(this->ref, first_sys_csr->get_size()[0]);
+    gko::array<index_type> row_part_of_which_block_d(this->exec, row_part_of_which_block_ref);
+    gko::kernels::reference::batch_jacobi::find_row_is_part_of_which_block(this->ref, num_blocks, block_pointers.get_data(), row_part_of_which_block_ref.get_data());
+    gko::kernels::EXEC_NAMESPACE::batch_jacobi::find_row_is_part_of_which_block(this->ref, num_blocks, block_pointers.get_data(), row_part_of_which_block_d.get_data());
+    GKO_ASSERT_ARRAY_EQ(row_part_of_which_block_ref, row_part_of_which_block_d);
+
+    // Testing function compute_cumulative_block_storage()
+    gko::array<index_type> block_cumulative_storage(this->ref, first_sys_csr->get_size()[0]+1);
+    gko::array<index_type> d_block_cumulative_storage(this->exec, block_cumulative_storage);
+    gko::kernels::reference::batch_jacobi::compute_cumulative_block_storage(this->ref, num_blocks, block_pointers.get_data(), block_cumulative_storage.get_data());
+    gko::kernels::EXEC_NAMESPACE::batch_jacobi::compute_cumulative_block_storage(this->exec, num_blocks, d_block_pointers.get_data(), d_block_cumulative_storage.get_data());
+    GKO_ASSERT_ARRAY_EQ(block_cumulative_storage, d_block_cumulative_storage);
+}
 
 }  // namespace
