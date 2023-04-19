e605f2b397ecc17b3e584e0819a7b41ed5ca8be6
Tobias Ribizel <ribizel@kit.edu>
2023-04-19 09:58:22 +0200

    fix benchmark tests for multi-config generators

--- benchmark/test/multi_vector_distributed.py
+++ benchmark/test/multi_vector_distributed.py
@@ -1,29 +1,28 @@
 #!/usr/bin/env python3
 import test_framework
-base_flags = ["blas/distributed/multi_vector_distributed"]
 # check that all input modes work:
 # parameter
-test_framework.compare_output_distributed(base_flags + ["-input", '[{"n": 100}]'],
+test_framework.compare_output_distributed(["-input", '[{"n": 100}]'],
                                           expected_stdout="multi_vector_distributed.simple.stdout",
                                           expected_stderr="multi_vector_distributed.simple.stderr",
                                           num_procs=3)
 
 # stdin
-test_framework.compare_output_distributed(base_flags,
+test_framework.compare_output_distributed([],
                                           expected_stdout="multi_vector_distributed.simple.stdout",
                                           expected_stderr="multi_vector_distributed.simple.stderr",
                                           stdin='[{"n": 100}]',
                                           num_procs=3)
 
 # file
-test_framework.compare_output_distributed(base_flags + ["-input", str(test_framework.sourcepath / "input.blas.json")],
+test_framework.compare_output_distributed(["-input", str(test_framework.sourcepath / "input.blas.json")],
                                           expected_stdout="multi_vector_distributed.simple.stdout",
                                           expected_stderr="multi_vector_distributed.simple.stderr",
                                           stdin='[{"n": 100}]',
                                           num_procs=3)
 
 # profiler annotations
-test_framework.compare_output_distributed(base_flags + ["-input", '[{"n": 100}]', '-profile', '-profiler_hook', 'debug'],
+test_framework.compare_output_distributed(["-input", '[{"n": 100}]', '-profile', '-profiler_hook', 'debug'],
                                           expected_stdout="multi_vector_distributed.profile.stdout",
                                           expected_stderr="multi_vector_distributed.profile.stderr",
                                           stdin='[{"n": 100}]',
--- benchmark/test/preconditioner.py
+++ benchmark/test/preconditioner.py
@@ -2,22 +2,22 @@
 import test_framework
 # check that all input modes work:
 # parameter
-test_framework.compare_output(["preconditioner/preconditioner", "-input", '[{"size": 100, "stencil": "7pt"}]'],
+test_framework.compare_output(["-input", '[{"size": 100, "stencil": "7pt"}]'],
                               expected_stdout="preconditioner.simple.stdout",
                               expected_stderr="preconditioner.simple.stderr")
 
 # stdin
-test_framework.compare_output(["preconditioner/preconditioner"],
+test_framework.compare_output([],
                               expected_stdout="preconditioner.simple.stdout",
                               expected_stderr="preconditioner.simple.stderr",
                               stdin='[{"size": 100, "stencil": "7pt"}]')
 
 # input file
-test_framework.compare_output(["preconditioner/preconditioner", "-input", str(test_framework.sourcepath / "input.mtx.json")],
+test_framework.compare_output(["-input", str(test_framework.sourcepath / "input.mtx.json")],
                               expected_stdout="preconditioner.simple.stdout",
                               expected_stderr="preconditioner.simple.stderr")
 
 # profiler annotations
-test_framework.compare_output(["preconditioner/preconditioner", "-input", '[{"size": 100, "stencil": "7pt"}]', '-profile', '-profiler_hook', 'debug'],
+test_framework.compare_output(["-input", '[{"size": 100, "stencil": "7pt"}]', '-profile', '-profiler_hook', 'debug'],
                               expected_stdout="preconditioner.profile.stdout",
                               expected_stderr="preconditioner.profile.stderr")
