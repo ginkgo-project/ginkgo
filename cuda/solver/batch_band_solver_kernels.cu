b990f794cb3cc98d33343f52e54f5acc2faa5067
aggarwal2000 <aggarwal2000chd@gmail.com>
2023-04-19 17:52:42 +0200

    Add band solver cuda - unblocked LU factorization

--- cuda/solver/batch_band_solver_kernels.cu
+++ cuda/solver/batch_band_solver_kernels.cu
@@ -32,21 +32,15 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include "core/solver/batch_band_solver_kernels.hpp"
 
-#include <chrono>
 #include <ginkgo/config.hpp>
-#include <ginkgo/core/synthesizer/containers.hpp>
+#include <ginkgo/core/base/math.hpp>
 #include "core/matrix/batch_struct.hpp"
-#include "core/synthesizer/implementation_selection.hpp"
 #include "cuda/base/config.hpp"
-#include "cuda/base/cublas_bindings.hpp"
-#include "cuda/base/cusparse_bindings.hpp"
 #include "cuda/base/exception.cuh"
 #include "cuda/base/math.hpp"
 #include "cuda/components/cooperative_groups.cuh"
-#include "cuda/components/load_store.cuh"
 #include "cuda/components/thread_ids.cuh"
 #include "cuda/matrix/batch_struct.hpp"
-#include "cuda/preconditioner/jacobi_common.hpp"
 
 namespace gko {
 namespace kernels {
@@ -57,6 +51,25 @@ namespace {
 
 constexpr int default_block_size = 128;
 
+// Block size for optimal performance - Found out by hit and trial
+int inline get_thread_block_size_unblocked_banded(const int nrows)
+{
+    if (nrows <= 50) {
+        return 64;
+    } else if (nrows <= 100) {
+        return 128;
+    } else if (nrows <= 200) {
+        return 256;
+    } else if (nrows <= 300) {
+        return 512;
+    } else {
+        return 1024;
+    }
+}
+
+// include all depedencies (note: do not remove this comment)
+#include "common/cuda_hip/matrix/batch_vector_kernels.hpp.inc"
+#include "common/cuda_hip/solver/batch_band_solver_kernels.hpp.inc"
 
 }  // namespace
 
@@ -68,7 +81,41 @@ void apply(std::shared_ptr<const DefaultExecutor> exec,
            matrix::BatchDense<ValueType>* const x, const int workspace_size,
            ValueType* const workspace_ptr,
            const enum gko::solver::batch_band_solve_approach approach,
-           const int blocked_solve_panel_size) GKO_NOT_IMPLEMENTED;
+           const int blocked_solve_panel_size)
+{
+    const auto nbatch = band_mat->get_num_batch_entries();
+    const auto nrows = static_cast<int>(band_mat->get_size().at(0)[0]);
+    const auto nrhs = static_cast<int>(b->get_size().at(0)[1]);
+    assert(nrhs == 1);
+    const auto KL = static_cast<int>(band_mat->get_num_subdiagonals().at(0));
+    const auto KU = static_cast<int>(band_mat->get_num_superdiagonals().at(0));
+
+    const int shared_size =
+        gko::kernels::batch_band_solver::local_memory_requirement<ValueType>(
+            nrows, nrhs, approach);
+
+    assert(workspace_size >= band_mat->get_num_stored_elements());
+    ValueType* const band_arr = workspace_ptr;
+    exec->copy(band_mat->get_num_stored_elements(),
+               band_mat->get_const_band_array(), band_arr);
+
+    if (approach == gko::solver::batch_band_solve_approach::unblocked) {
+        dim3 block(get_thread_block_size_unblocked_banded(nrows));
+        dim3 grid(nbatch);
+
+        band_solver_unblocked_kernel<config::warp_size>
+            <<<grid, block, shared_size>>>(nbatch, nrows, KL, KU,
+                                           as_cuda_type(band_arr),
+                                           as_cuda_type(b->get_const_values()),
+                                           as_cuda_type(x->get_values()));
+    } else if (approach == gko::solver::batch_band_solve_approach::blocked) {
+        GKO_NOT_IMPLEMENTED;
+    } else {
+        GKO_NOT_IMPLEMENTED;
+    }
+
+    GKO_CUDA_LAST_IF_ERROR_THROW;
+}
 
 GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE(GKO_DECLARE_BATCH_BAND_SOLVER_APPLY_KERNEL);
 
