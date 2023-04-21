2a641469a3bfe479017e35767edf64dd7b657a8f
Marcel Koch <marcel.koch@kit.edu>
2023-04-21 10:56:22 +0200

    correctly initializes stopping status

--- common/unified/solver/gcr_kernels.cpp
+++ common/unified/solver/gcr_kernels.cpp
@@ -56,16 +56,23 @@ void initialize(std::shared_ptr<const DefaultExecutor> exec,
                 matrix::Dense<ValueType>* residual,
                 stopping_status* stop_status)
 {
-    run_kernel_solver(
-        exec,
-        [] GKO_KERNEL(auto row, auto col, auto b, auto residual, auto stop) {
-            if (row == 0) {
-                stop[col].reset();
-            }
-            residual(row, col) = b(row, col);
-        },
-        b->get_size(), b->get_stride(), default_stride(b),
-        default_stride(residual), stop_status);
+    if (b->get_size()) {
+        run_kernel_solver(
+            exec,
+            [] GKO_KERNEL(auto row, auto col, auto b, auto residual,
+                          auto stop) {
+                if (row == 0) {
+                    stop[col].reset();
+                }
+                residual(row, col) = b(row, col);
+            },
+            b->get_size(), b->get_stride(), default_stride(b),
+            default_stride(residual), stop_status);
+    } else {
+        run_kernel(
+            exec, [] GKO_KERNEL(auto col, auto stop) { stop[col].reset(); },
+            b->get_size()[1], stop_status);
+    }
 }
 
 GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE(GKO_DECLARE_GCR_INITIALIZE_KERNEL);
@@ -78,18 +85,28 @@ void restart(std::shared_ptr<const DefaultExecutor> exec,
              matrix::Dense<ValueType>* p_bases,
              matrix::Dense<ValueType>* Ap_bases, size_type* final_iter_nums)
 {
-    run_kernel_solver(
-        exec,
-        [] GKO_KERNEL(auto row, auto col, auto residual, auto A_residual,
-                      auto p_bases, auto Ap_bases, auto final_iter_nums) {
-            if (row == 0) {
+    if (residual->get_size()) {
+        run_kernel_solver(
+            exec,
+            [] GKO_KERNEL(auto row, auto col, auto residual, auto A_residual,
+                          auto p_bases, auto Ap_bases, auto final_iter_nums) {
+                if (row == 0) {
+                    final_iter_nums[col] = 0;
+                }
+                p_bases(row, col) = residual(row, col);
+                Ap_bases(row, col) = A_residual(row, col);
+            },
+            residual->get_size(), residual->get_stride(),
+            default_stride(residual), default_stride(A_residual), p_bases,
+            Ap_bases, final_iter_nums);
+    } else {
+        run_kernel(
+            exec,
+            [] GKO_KERNEL(auto col, auto final_iter_nums) {
                 final_iter_nums[col] = 0;
-            }
-            p_bases(row, col) = residual(row, col);
-            Ap_bases(row, col) = A_residual(row, col);
-        },
-        residual->get_size(), residual->get_stride(), default_stride(residual),
-        default_stride(A_residual), p_bases, Ap_bases, final_iter_nums);
+            },
+            residual->get_size()[1], final_iter_nums);
+    }
 }
 
 GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE(GKO_DECLARE_GCR_RESTART_KERNEL);
