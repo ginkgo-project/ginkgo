2cf83e1ebc1d63c97b76ff7302e0135ffa438552
Tobias Ribizel <ribizel@kit.edu>
2023-05-05 11:30:58 +0200

    shut up ABI diff bot

--- .github/abidiff.sh
+++ .github/abidiff.sh
@@ -6,4 +6,4 @@ popd
 
 bot_delete_comments_matching "Note: This PR changes the Ginkgo ABI"
 
-abidiff build-old/lib/libginkgod.so build-new/lib/libginkgod.so &> abi.diff || (bot_comment "Note: This PR changes the Ginkgo ABI:\n\`\`\`\n$(head -n2 abi.diff | tr '\n' ';' | sed 's/;/\\n/g')\`\`\`\nFor details check the full ABI diff under **Artifacts** [here]($JOB_URL)")
+abidiff build-old/lib/libginkgod.so build-new/lib/libginkgod.so &> abi.diff || true # (bot_comment "Note: This PR changes the Ginkgo ABI:\n\`\`\`\n$(head -n2 abi.diff | tr '\n' ';' | sed 's/;/\\n/g')\`\`\`\nFor details check the full ABI diff under **Artifacts** [here]($JOB_URL)")
