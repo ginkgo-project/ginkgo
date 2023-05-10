41ed0aae9eccfc7a21a7640f8e43a75fe2d948bc
aggarwal2000 <aggarwal2000chd@gmail.com>
2023-05-10 19:46:48 +0530

    Add hip batch band kernels

--- hip/solver/batch_band_solver_kernels.hip.cpp
+++ hip/solver/batch_band_solver_kernels.hip.cpp
@@ -34,9 +34,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include <chrono>
 #include "core/matrix/batch_struct.hpp"
-#include "core/synthesizer/implementation_selection.hpp"
 #include "hip/base/exception.hip.hpp"
-#include "hip/base/hipsparse_bindings.hip.hpp"
 #include "hip/base/math.hip.hpp"
 #include "hip/components/cooperative_groups.hip.hpp"
 #include "hip/components/load_store.hip.hpp"
@@ -48,12 +46,62 @@ namespace kernels {
 namespace hip {
 namespace batch_band_solver {
 
+
 namespace {
+
+template <typename ValueType>
+__host__ __device__ bool is_matrix_in_shared_mem(const int N, const int KL,
+                                                 const int KU)
+{
+    const int band_nrows = 2 * KL + KU + 1;
+    const size_type storage_in_bytes = band_nrows * N * sizeof(ValueType);
+    if (storage_in_bytes <=
+        20000) {  // TODO: Find out experimentally an optimal value
+        return true;
+    } else {
+        return false;
+    }
+}
 
 constexpr int default_block_size = 128;
 
+// TODO: Tune for optimal performance
+int inline get_thread_block_size_unblocked_banded(const int nrows)
+{
+    if (nrows <= 50) {
+        return 64;
+    } else if (nrows <= 100) {
+        return 128;
+    } else if (nrows <= 200) {
+        return 256;
+    } else if (nrows <= 300) {
+        return 512;
+    } else {
+        return 512;  // 1024 // otherwise too many reources requested on launch
+                     // error
+    }
+}
+
+int inline get_thread_block_size_blocked_banded(const int nrows)
+{
+    if (nrows <= 100) {
+        return 64;
+    } else if (nrows <= 200) {
+        return 128;
+    } else if (nrows <= 400) {
+        return 256;
+    } else {
+        return 512;  // otherwise too many reources requested on launch error
+    }
+}
+
+
+// include all depedencies (note: do not remove this comment)
+#include "common/cuda_hip/matrix/batch_vector_kernels.hpp.inc"
+#include "common/cuda_hip/solver/batch_band_solver_kernels.hpp.inc"
 
 }  // namespace
+
 
 template <typename ValueType>
 void apply(std::shared_ptr<const DefaultExecutor> exec,
@@ -62,7 +110,75 @@ void apply(std::shared_ptr<const DefaultExecutor> exec,
            matrix::BatchDense<ValueType>* const x, const int workspace_size,
            ValueType* const workspace_ptr,
            const enum gko::solver::batch_band_solve_approach approach,
-           const int blocked_solve_panel_size) GKO_NOT_IMPLEMENTED;
+           const int blocked_solve_panel_size)
+{
+    const auto nbatch = band_mat->get_num_batch_entries();
+    const auto nrows = static_cast<int>(band_mat->get_size().at(0)[0]);
+    const auto nrhs = static_cast<int>(b->get_size().at(0)[1]);
+    assert(nrhs == 1);
+    const auto KL = static_cast<int>(band_mat->get_num_subdiagonals().at(0));
+    const auto KU = static_cast<int>(band_mat->get_num_superdiagonals().at(0));
+
+    assert(workspace_size >= band_mat->get_num_stored_elements());
+
+    if (workspace_size < band_mat->get_num_stored_elements()) {
+        std::cout << " file: " << __FILE__ << " line: " << __LINE__
+                  << " workspace size is not enough" << std::endl;
+        exit(0);
+    }
+
+    ValueType* const band_arr = workspace_ptr;
+    exec->copy(band_mat->get_num_stored_elements(),
+               band_mat->get_const_band_array(), band_arr);
+
+    int shared_size = 0;
+    if (is_matrix_in_shared_mem<ValueType>(
+            nrows, KL, KU)) {  // TODO: Avoid extra workspace copy in this case
+        shared_size +=
+            (band_mat->get_num_stored_elements() / nbatch) * sizeof(ValueType);
+    }
+
+    if (approach == gko::solver::batch_band_solve_approach::unblocked ||
+        (approach == gko::solver::batch_band_solve_approach::blocked &&
+         blocked_solve_panel_size > KL)) {
+        shared_size +=
+            gko::kernels::batch_band_solver::local_memory_requirement<
+                ValueType>(nrows, nrhs,
+                           gko::solver::batch_band_solve_approach::unblocked);
+
+        dim3 block(get_thread_block_size_unblocked_banded(nrows));
+        dim3 grid(nbatch);
+
+        hipLaunchKernelGGL(
+            HIP_KERNEL_NAME(band_solver_unblocked_kernel<config::warp_size>),
+            grid, block, shared_size, 0, nbatch, nrows, KL, KU,
+            as_hip_type(band_arr), as_hip_type(b->get_const_values()),
+            as_hip_type(x->get_values()));
+
+    } else if (approach == gko::solver::batch_band_solve_approach::blocked) {
+        shared_size +=
+            gko::kernels::batch_band_solver::local_memory_requirement<
+                ValueType>(nrows, nrhs,
+                           gko::solver::batch_band_solve_approach::blocked,
+                           blocked_solve_panel_size);
+
+        dim3 block(get_thread_block_size_blocked_banded(nrows));
+        dim3 grid(nbatch);
+
+        const int subwarp_size = 8;
+
+        hipLaunchKernelGGL(
+            HIP_KERNEL_NAME(band_solver_blocked_kernel<subwarp_size>), grid,
+            block, shared_size, 0, nbatch, nrows, KL, KU,
+            blocked_solve_panel_size, as_hip_type(band_arr),
+            as_hip_type(b->get_const_values()), as_hip_type(x->get_values()));
+
+    } else {
+        GKO_NOT_IMPLEMENTED;
+    }
+
+    GKO_HIP_LAST_IF_ERROR_THROW;
+}
 
 GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE(GKO_DECLARE_BATCH_BAND_SOLVER_APPLY_KERNEL);
 
