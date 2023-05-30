440c21f3373c439784c188167a848c6a33efd29b
Tobias Ribizel <ribizel@kit.edu>
2023-05-30 14:30:00 +0200

    updates

--- dev_tools/container/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-. /root/spack/share/spack/setup-env.sh
-spack env activate ginkgo
-# Intel's compiler packages set these variables, reset them
-export CC=gcc CXX=g++ CUDACXX=`spack location -i cuda`/bin/nvcc
-
-/bin/bash "$@"
