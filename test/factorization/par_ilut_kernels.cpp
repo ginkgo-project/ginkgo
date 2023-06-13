8381bc33ee45b87e79fbf7ff3221775f2db5f1dd
Yu-Hsiang M. Tsai <yhmtsai@gmail.com>
2023-06-13 16:52:41 +0200

    fix/skip half test and fix numeric_limit on device

--- test/factorization/par_ilut_kernels.cpp
+++ test/factorization/par_ilut_kernels.cpp
@@ -365,6 +365,8 @@ TYPED_TEST(ParIlut, KernelAddCandidatesIsEquivalentToRef)
 {
     using Csr = typename TestFixture::Csr;
     using value_type = typename TestFixture::value_type;
+    // there's one value larger than half range
+    SKIP_IF_HALF(value_type);
     auto square_size = this->mtx_square->get_size();
     auto mtx_lu = Csr::create(this->ref, square_size);
     this->mtx_l2->apply(this->mtx_u, mtx_lu);
@@ -393,6 +395,8 @@ TYPED_TEST(ParIlut, KernelComputeLUIsEquivalentToRef)
 {
     using Csr = typename TestFixture::Csr;
     using Coo = typename TestFixture::Coo;
+    using value_type = typename TestFixture::value_type;
+    SKIP_IF_HALF(value_type);
     auto square_size = this->mtx_ani->get_size();
     auto mtx_l_coo = Coo::create(this->ref, square_size);
     auto mtx_u_coo = Coo::create(this->ref, square_size);
