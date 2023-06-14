edb93dfd1edbf0eeeced92724bea339f4a6872fc
josealiaga <aliaga@uji.es>
2023-06-14 17:30:48 +0200

    Renaming in unaligned_access files is done.

--- core/base/unaligned_access.hpp
+++ core/base/unaligned_access.hpp
@@ -47,169 +47,168 @@ namespace bccoo {
 
 
 /**
- * Copies value of type T starting at byte start from ptr.
+ * Copies value of type T starting at byte dspl from ptr.
  *
  * @tparam T     the type of value
  *
  * @param ptr    the starting pointer
- * @param start  the offset
+ * @param dspl   the offset (in bytes)
  * @param value  the value
  */
 template <typename T>
-void set_value_chunk(void* ptr, size_type start, T value)
+void set_value_chunk(void* ptr, size_type dspl, T value)
 {
-    std::memcpy(static_cast<unsigned char*>(ptr) + start, &value, sizeof(T));
+    std::memcpy(static_cast<unsigned char*>(ptr) + dspl, &value, sizeof(T));
 }
 
 
 /**
  * Copies value of type T at pos-th component of array<T> starting
- *        at byte start from ptr.
+ *        at byte dspl from ptr.
  *
  * @tparam T     the type of value
  *
  * @param ptr    the starting pointer
- * @param start  the offset
+ * @param dspl   the offset (in bytes)
  * @param pos    the component position of the vector
  * @param value  the value
  */
 template <typename T>
-void set_value_chunk(void* ptr, size_type start, size_type pos, T value)
+void set_value_chunk(void* ptr, size_type dspl, size_type pos, T value)
 {
-    set_value_chunk<T>(ptr, start + pos * sizeof(T), value);
+    set_value_chunk<T>(ptr, dspl + pos * sizeof(T), value);
 }
 
 
 /**
- * Copies value of type T starting at byte start from ptr.
- * Also, start is updated by the size of T.
+ * Copies value of type T starting at byte dspl from ptr.
+ * Also, dspl is updated by the size of T.
  *
  * @tparam T     the type of value
  *
  * @param ptr    the starting pointer
- * @param start  the offset
+ * @param dspl   the offset (in bytes)
  * @param value  the value
  */
 template <typename T>
-void set_value_chunk_and_increment(void* ptr, size_type& start, T value)
+void set_value_chunk_and_increment(void* ptr, size_type& dspl, T value)
 {
-    set_value_chunk<T>(ptr, start, value);
-    start += sizeof(T);
+    set_value_chunk<T>(ptr, dspl, value);
+    dspl += sizeof(T);
 }
 
 
 /**
- * Returns the value of type T starting at byte start from ptr.
+ * Returns the value of type T starting at byte dspl from ptr.
  *
  * @tparam T     the type of value
  *
  * @param ptr    the starting pointer
- * @param start  the offset
+ * @param dspl   the offset (in bytes)
  *
- * @return the value of type T starting at byte start from ptr.
+ * @return the value of type T starting at byte dspl from ptr.
  *
  * @note The memory does not need to be aligned to be written.
  */
 template <typename T>
-T get_value_chunk(const void* ptr, size_type start)
+T get_value_chunk(const void* ptr, size_type dspl)
 {
     T val{};
-    std::memcpy(&val, static_cast<const unsigned char*>(ptr) + start,
-                sizeof(T));
+    std::memcpy(&val, static_cast<const unsigned char*>(ptr) + dspl, sizeof(T));
     return val;
 }
 
 
 /**
  * Returns the value of type T at pos-th component of array<T> starting
- *         at byte start from ptr.
+ *         at byte dspl from ptr.
  *
  * @tparam T     the type of value
  *
  * @param ptr    the starting pointer
- * @param start  the offset
+ * @param dspl   the offset (in bytes)
  * @param pos    the component position of the vector
  *
  * @return the value in the m-th byte of ptr, which is adjusting to T class.
  */
 template <typename T>
-T get_value_chunk(const void* ptr, const size_type start, const size_type pos)
+T get_value_chunk(const void* ptr, const size_type dspl, const size_type pos)
 {
-    return get_value_chunk<T>(ptr, start + pos * sizeof(T));
+    return get_value_chunk<T>(ptr, dspl + pos * sizeof(T));
 }
 
 
 /**
- * Returns the value of type T starting at byte start from ptr.
- * Also, start is updated by the size of T.
+ * Returns the value of type T starting at byte dspl from ptr.
+ * Also, dspl is updated by the size of T.
  *
  * @tparam T     the type of value
  *
  * @param ptr    the starting pointer
- * @param start  the offset
+ * @param dspl   the offset (in bytes)
  *
  * @return the value of type T at pos-th component of array<T> starting
- *         at byte start from ptr.
+ *         at byte dspl from ptr.
  */
 template <typename T>
-T get_value_chunk_and_increment(const void* ptr, size_type& start)
+T get_value_chunk_and_increment(const void* ptr, size_type& dspl)
 {
-    T val = get_value_chunk<T>(ptr, start);
-    start += sizeof(T);
+    T val = get_value_chunk<T>(ptr, dspl);
+    dspl += sizeof(T);
     return val;
 }
 
 
 /**
  * Copies num values of type T
- *    from array<T> starting at byte start_src from ptr_src
- *    to array<T> starting at byte start_res from ptr_res.
+ *    from array<T> starting at byte dspl_src from ptr_src
+ *    to array<T> starting at byte dspl_res from ptr_res.
  *
  * @tparam T     the type of value
  *
  * @param ptr_res    the starting pointer of the result
- * @param start_res  the offset of the result
+ * @param dspl_res   the offset of the result (in bytes)
  * @param ptr_src    the starting pointer of the source
- * @param start_src  the offset of the source
+ * @param dspl_src   the offset of the source (in bytes)
  * @param num        the number of values to copy
  *
  * @note The memory does not need to be aligned to be written or read.
  */
 template <typename T>
-void copy_array_chunk(void* ptr_res, size_type start_res, const void* ptr_src,
-                      size_type start_src, size_type num)
+void copy_array_chunk(void* ptr_res, size_type dspl_res, const void* ptr_src,
+                      size_type dspl_src, size_type num)
 {
-    memcpy(static_cast<unsigned char*>(ptr_res) + start_res,
-           static_cast<const unsigned char*>(ptr_src) + start_src,
+    memcpy(static_cast<unsigned char*>(ptr_res) + dspl_res,
+           static_cast<const unsigned char*>(ptr_src) + dspl_src,
            sizeof(T) * num);
 }
 
 
 /**
  * Copies num values of type T
- *    from array<T> starting at byte start_src from ptr_src
- *    to array<T> starting at byte start_res from ptr_res.
- * Also, start_src and start_res are updated by the size of T.
+ *    from array<T> starting at byte dspl_src from ptr_src
+ *    to array<T> starting at byte dspl_res from ptr_res.
+ * Also, dspl_src and dspl_res are updated by the size of T.
  *
  * @tparam T     the type of value
  *
  * @param ptr_res    the starting pointer of the result
- * @param start_res  the offset of the result
+ * @param dspl_res   the offset of the result (in bytes)
  * @param ptr_src    the starting pointer of the source
- * @param start_src  the offset of the source
+ * @param dspl_src   the offset of the source (in bytes)
  * @param num        the number of values to copy
  *
  * @note The memory does not need to be aligned to be written or read.
  */
 template <typename T>
-void copy_array_chunk_and_increment(void* ptr_res, size_type& start_res,
-                                    const void* ptr_src, size_type& start_src,
+void copy_array_chunk_and_increment(void* ptr_res, size_type& dspl_res,
+                                    const void* ptr_src, size_type& dspl_src,
                                     size_type num)
 {
     size_type num_bytes = sizeof(T) * num;
-    copy_array_chunk<T>(ptr_res, start_res, ptr_src, start_src, num);
-    start_src += num_bytes;
-    start_res += num_bytes;
+    copy_array_chunk<T>(ptr_res, dspl_res, ptr_src, dspl_src, num);
+    dspl_src += num_bytes;
+    dspl_res += num_bytes;
 }
 
 
