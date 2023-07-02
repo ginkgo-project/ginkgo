98f08cd55080dda6723feefc0211e7f444dae72a
Tobias Ribizel <mail@ribizel.de>
2023-07-02 12:37:24 +0000

    make nvcc visible
    We cannot include nvcc in the spack environment
    because we need to avoid collisions in the `thrust` include dir.
    But we can make CMake find the CUDA toolkit that way.

--- dev_tools/container/unified/entrypoint.sh
+++ dev_tools/container/unified/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 . /spack/share/spack/setup-env.sh
 spack env activate ginkgo
-export CC=gcc CXX=g++ CUDACXX=`spack location -i cuda`/bin/nvcc 
+export CC=gcc CXX=g++ CUDACXX=`spack location -i cuda`/bin/nvcc PATH=$PATH:`spack location -i cuda`/bin
 exec "$@"
