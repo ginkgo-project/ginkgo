85e43e83a73d6ae4f9228766dda9ccefe268685d
Marcel Koch <marcel.koch@kit.edu>
2023-07-05 17:34:12 +0200

    use workaround for old dpcpp version

--- dpcpp/distributed/partition_helpers_kernels.dp.cpp
+++ dpcpp/distributed/partition_helpers_kernels.dp.cpp
@@ -74,11 +74,31 @@ void sort_by_range_start(
         range_start_ends.get_data(), stride{});
     auto end_it = oneapi::dpl::make_permutation_iterator(
         range_start_ends.get_data() + 1, stride{});
+
+    // older versions of oneDPL have a bug when sorting permutation iterators
+#if ONEDPL_VERSION_MAJOR >= 2022 && ONEDPL_VERSION_MINOR >= 1
     auto zip_it =
         oneapi::dpl::make_zip_iterator(start_it, end_it, part_ids.get_data());
     std::stable_sort(policy, zip_it, zip_it + num_ranges, [](auto a, auto b) {
         return std::get<0>(a) < std::get<0>(b);
     });
+#else
+    array<GlobalIndexType> starts(exec, num_ranges);
+    array<GlobalIndexType> ends(exec, num_ranges);
+
+    std::copy(policy, start_it, start_it + num_ranges, starts.get_data());
+    std::copy(policy, end_it, end_it + num_ranges, ends.get_data());
+
+    auto zip_it = oneapi::dpl::make_zip_iterator(
+        starts.get_data(), ends.get_data(), part_ids.get_data());
+    std::stable_sort(policy, zip_it, zip_it + num_ranges, [](auto a, auto b) {
+        return std::get<0>(a) < std::get<0>(b);
+    });
+
+    std::copy(policy, starts.get_data(), starts.get_data() + num_ranges,
+              start_it);
+    std::copy(policy, ends.get_data(), ends.get_data() + num_ranges, end_it);
+#endif
 }
 
 GKO_INSTANTIATE_FOR_EACH_INDEX_TYPE(
