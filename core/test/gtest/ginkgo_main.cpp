af2ab0cf676051fb871870ad324bb63a5cb71457
Marcel Koch <marcel.koch@kit.edu>
2023-07-12 17:15:45 +0200

    use resources in tests

--- core/test/gtest/ginkgo_main.cpp
+++ core/test/gtest/ginkgo_main.cpp
@@ -3,12 +3,19 @@
 
 #include "core/test/gtest/environments.hpp"
 
+resource ResourceEnvironment::rs = {};
 
 int main(int argc, char** argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
+
+    auto resources = get_ctest_resources();
+
+    ::testing::AddGlobalTestEnvironment(
+        new ResourceEnvironment(resources.front()));
     ::testing::AddGlobalTestEnvironment(new CudaEnvironment);
     ::testing::AddGlobalTestEnvironment(new HipEnvironment);
+    ::testing::AddGlobalTestEnvironment(new OmpEnvironment);
     int result = RUN_ALL_TESTS();
     return result;
 }
