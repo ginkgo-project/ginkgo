9f0282050beec93013db612ae6f1cd7cf924e8dd
Pratik Nayak <pratikvn@protonmail.com>
2023-07-24 14:14:45 +0200

    Update dpcpp kernels and fix for 2022-1
    Cannot use sycl::reduce_over_group for older DPCPP versions.

--- dpcpp/base/batch_multi_vector_kernels.dp.cpp
+++ dpcpp/base/batch_multi_vector_kernels.dp.cpp
@@ -48,6 +48,10 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "dpcpp/base/dim3.dp.hpp"
 #include "dpcpp/base/dpct.hpp"
 #include "dpcpp/base/helper.hpp"
+#include "dpcpp/components/cooperative_groups.dp.hpp"
+#include "dpcpp/components/intrinsics.dp.hpp"
+#include "dpcpp/components/reduction.dp.hpp"
+#include "dpcpp/components/thread_ids.dp.hpp"
 
 
 namespace gko {
@@ -81,16 +85,31 @@ void scale(std::shared_ptr<const DefaultExecutor> exec,
     const dim3 grid(num_batches);
 
     // Launch a kernel that has nbatches blocks, each block has max group size
-    (exec->get_queue())->submit([&](sycl::handler& cgh) {
-        cgh.parallel_for(
-            sycl_nd_range(grid, block), [=](sycl::nd_item<3> item_ct1) {
-                auto group = item_ct1.get_group();
-                auto group_id = group.get_group_linear_id();
-                const auto alpha_b = batch::batch_entry(alpha_ub, group_id);
-                const auto x_b = batch::batch_entry(x_ub, group_id);
-                scale_kernel(alpha_b, x_b, item_ct1);
-            });
-    });
+    if (alpha->get_common_size()[1] == 1) {
+        (exec->get_queue())->submit([&](sycl::handler& cgh) {
+            cgh.parallel_for(
+                sycl_nd_range(grid, block), [=](sycl::nd_item<3> item_ct1) {
+                    auto group = item_ct1.get_group();
+                    auto group_id = group.get_group_linear_id();
+                    const auto alpha_b = batch::batch_entry(alpha_ub, group_id);
+                    const auto x_b = batch::batch_entry(x_ub, group_id);
+                    scale_kernel(alpha_b, x_b, item_ct1,
+                                 [](int col) { return 0; });
+                });
+        });
+    } else {
+        (exec->get_queue())->submit([&](sycl::handler& cgh) {
+            cgh.parallel_for(
+                sycl_nd_range(grid, block), [=](sycl::nd_item<3> item_ct1) {
+                    auto group = item_ct1.get_group();
+                    auto group_id = group.get_group_linear_id();
+                    const auto alpha_b = batch::batch_entry(alpha_ub, group_id);
+                    const auto x_b = batch::batch_entry(x_ub, group_id);
+                    scale_kernel(alpha_b, x_b, item_ct1,
+                                 [](int col) { return col; });
+                });
+        });
+    }
 }
 
 GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE(
@@ -116,17 +135,33 @@ void add_scaled(std::shared_ptr<const DefaultExecutor> exec,
     const auto alpha_ub = get_batch_struct(alpha);
     const auto x_ub = get_batch_struct(x);
     const auto y_ub = get_batch_struct(y);
-    (exec->get_queue())->submit([&](sycl::handler& cgh) {
-        cgh.parallel_for(
-            sycl_nd_range(grid, block), [=](sycl::nd_item<3> item_ct1) {
-                auto group = item_ct1.get_group();
-                auto group_id = group.get_group_linear_id();
-                const auto alpha_b = batch::batch_entry(alpha_ub, group_id);
-                const auto x_b = batch::batch_entry(x_ub, group_id);
-                const auto y_b = batch::batch_entry(y_ub, group_id);
-                add_scaled_kernel(alpha_b, x_b, y_b, item_ct1);
-            });
-    });
+    if (alpha->get_common_size()[1] == 1) {
+        (exec->get_queue())->submit([&](sycl::handler& cgh) {
+            cgh.parallel_for(
+                sycl_nd_range(grid, block), [=](sycl::nd_item<3> item_ct1) {
+                    auto group = item_ct1.get_group();
+                    auto group_id = group.get_group_linear_id();
+                    const auto alpha_b = batch::batch_entry(alpha_ub, group_id);
+                    const auto x_b = batch::batch_entry(x_ub, group_id);
+                    const auto y_b = batch::batch_entry(y_ub, group_id);
+                    add_scaled_kernel(alpha_b, x_b, y_b, item_ct1,
+                                      [](auto col) { return 0; });
+                });
+        });
+    } else {
+        (exec->get_queue())->submit([&](sycl::handler& cgh) {
+            cgh.parallel_for(
+                sycl_nd_range(grid, block), [=](sycl::nd_item<3> item_ct1) {
+                    auto group = item_ct1.get_group();
+                    auto group_id = group.get_group_linear_id();
+                    const auto alpha_b = batch::batch_entry(alpha_ub, group_id);
+                    const auto x_b = batch::batch_entry(x_ub, group_id);
+                    const auto y_b = batch::batch_entry(y_ub, group_id);
+                    add_scaled_kernel(alpha_b, x_b, y_b, item_ct1,
+                                      [](auto col) { return col; });
+                });
+        });
+    }
 }
 
 GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE(
@@ -159,7 +194,8 @@ void compute_dot(std::shared_ptr<const DefaultExecutor> exec,
                 const auto x_b = batch::batch_entry(x_ub, group_id);
                 const auto y_b = batch::batch_entry(y_ub, group_id);
                 const auto res_b = batch::batch_entry(res_ub, group_id);
-                compute_dot_product_kernel(x_b, y_b, res_b, item_ct1);
+                compute_gen_dot_product_kernel(x_b, y_b, res_b, item_ct1,
+                                               [](auto val) { return val; });
             });
     });
 }
@@ -194,7 +230,9 @@ void compute_conj_dot(std::shared_ptr<const DefaultExecutor> exec,
                 const auto x_b = batch::batch_entry(x_ub, group_id);
                 const auto y_b = batch::batch_entry(y_ub, group_id);
                 const auto res_b = batch::batch_entry(res_ub, group_id);
-                compute_conj_dot_product_kernel(x_b, y_b, res_b, item_ct1);
+                compute_gen_dot_product_kernel(
+                    x_b, y_b, res_b, item_ct1,
+                    [](auto val) { return conj(val); });
             });
     });
 }
