3f2513a01713c5459349b7f3c4a3ac0d4f7f1b87
Pratik Nayak <pratikvn@protonmail.com>
2023-07-27 17:19:55 +0200

    Update batched-tridiag example

--- examples/batched-tridiagonal-solver/batched-tridiagonal-solver.cpp
+++ examples/batched-tridiagonal-solver/batched-tridiagonal-solver.cpp
@@ -57,6 +57,40 @@ using mtx_type = gko::matrix::BatchTridiagonal<value_type>;
 using batch_tridiag_solver = gko::solver::BatchTridiagonalSolver<value_type>;
 
 
+// @sect3{'Application' structures and functions}
+// Structure to simulate application data related to the linear systems
+// to be solved.
+//
+// We use raw pointers below to demonstrate how to handle the situation when
+// the application only gives us raw pointers. Ideally, one should use
+// Ginkgo's gko::Array class here.
+struct ApplSysData {
+    // Number of small systems in the batch.
+    size_type num_systems;
+    // Number of rows in each system.
+    int num_rows;
+    // Number of non-zeros in each system matrix.
+    int nnz;
+    const value_type* sup_diag;
+    const value_type* main_diag;
+    const value_type* sub_diag;
+    const value_type* all_rhs;
+};
+
+
+// Generates a batch of tridiagonal systems.
+//
+// @param num_rows  Number of rows in each system.
+// @param num_systems  Number of systems in the batch.
+// @param exec  The device executor to use for the solver.
+//   Normally, the application may not deal with Ginkgo executors, nor do we
+//   need it to. Here, we use the executor for backend-independent device
+//   memory allocation. The application, for example, might assume Hip
+//   (for AMD GPUs) and use `hipMalloc` directly.
+ApplSysData appl_generate_system(const int num_rows,
+                                 const size_type num_systems,
+                                 std::shared_ptr<gko::Executor> exec);
+
 int main(int argc, char* argv[])
 {
     // Print the ginkgo version information.
@@ -106,74 +140,53 @@ int main(int argc, char* argv[])
     // executor where Ginkgo will perform the computation
     const auto exec = exec_map.at(executor_string)();  // throws if not valid
 
-    // @sect3{Other optional command-line arguments}
-    const std::string dir_name = argc >= 3 ? argv[2] : "data/";
+    const size_type num_systems = argc >= 3 ? std::atoi(argv[2]) : 2;
+    const int num_rows = argc >= 4 ? std::atoi(argv[3]) : 32;  // per system
+    const std::string in_strat = argc >= 5 ? argv[4] : "recursive_app1";
+    const bool print_time =
+        argc >= 6 ? (std::string(argv[5]) == "time") : false;
+    const bool print_residuals =
+        argc >= 7 ? (std::string(argv[6]) == "residuals") : false;
+    const int num_reps = argc >= 8 ? std::atoi(argv[7]) : 20;
 
-    // Name of the problem
-    const std::string problem_name = argc >= 4 ? argv[3] : "gallery_lesp_100";
-
-    // Number of linear systems to read from files.
-    const size_type num_systems = argc >= 5 ? std::atoi(argv[4]) : 2;
-
-    // Number of times to duplicate whatever systems are read from files.
-    const size_type num_duplications = argc >= 6 ? std::atoi(argv[5]) : 10;
-
-    // Number of WM steps
-    const int number_recursive_steps = argc >= 7 ? std::atoi(argv[6]) : 2;
-
-    // wm_pge subwarp size
-    const int subwarp_size = argc >= 8 ? std::atoi(argv[7]) : 16;
+    const int number_recursive_steps = argc >= 9 ? std::atoi(argv[8]) : 2;
+    const int subwarp_size = argc >= 10 ? std::atoi(argv[9]) : 16;
 
     // Approach
     enum gko::solver::batch_tridiag_solve_approach approach;
-    const std::string approach_str = argc >= 9 ? argv[8] : "recursive_app1";
-    if (approach_str == std::string("recursive_app1")) {
+    if (in_strat == std::string("recursive_app1")) {
         approach = gko::solver::batch_tridiag_solve_approach::recursive_app1;
-    } else if (approach_str == std::string("recursive_app2")) {
+    } else if (in_strat == std::string("recursive_app2")) {
         approach = gko::solver::batch_tridiag_solve_approach::recursive_app2;
-    } else if (approach_str == std::string("vendor_provided")) {
+    } else if (in_strat == std::string("vendor_provided")) {
         approach = gko::solver::batch_tridiag_solve_approach::vendor_provided;
     }
 
-    const char* log_file = argc >= 10 ? argv[9] : "timings_file.txt";
-
     // @sect3{Read batch from files}
-    auto data = std::vector<gko::matrix_data<value_type>>(num_systems);
+    auto appl_sys = appl_generate_system(num_rows, num_systems, exec);
+    // Create batch_dim object to describe the dimensions of the batch matrix.
+    auto batch_mat_size =
+        gko::batch_dim<>(num_systems, gko::dim<2>(num_rows, num_rows));
+    auto batch_vec_size =
+        gko::batch_dim<>(num_systems, gko::dim<2>(num_rows, 1));
 
-    for (size_type i = 0; i < data.size(); ++i) {
-        const std::string mat_str = "A.mtx";
-        const std::string fbase =
-            dir_name + problem_name + "/" + std::to_string(i) + "/";
-        std::string fname = fbase + mat_str;
-        std::cout << "\nReading file: " << fname << std::endl;
-        std::ifstream mtx_fd(fname);
-        data[i] = gko::read_raw<value_type>(mtx_fd);
-    }
-
-    auto single_batch = mtx_type::create(exec);
-    single_batch->read(data);
-
-    // We can duplicate the batch a few times if we wish.
-    std::shared_ptr<mtx_type> A =
-        mtx_type::create(exec, num_duplications, single_batch.get());
+    auto sub_view = gko::array<value_type>::const_view(
+        exec, num_systems * appl_sys.num_rows, appl_sys.sub_diag);
+    auto main_view = gko::array<value_type>::const_view(
+        exec, num_systems * appl_sys.num_rows, appl_sys.main_diag);
+    auto sup_view = gko::array<value_type>::const_view(
+        exec, num_systems * appl_sys.num_rows, appl_sys.sup_diag);
+    auto A = gko::share(
+        mtx_type::create_const(exec, batch_mat_size, std::move(sub_view),
+                               std::move(main_view), std::move(sup_view)));
 
     // Create RHS
-    const auto nrows = A->get_size().at(0)[0];
-
-    const size_type num_total_systems = num_systems * num_duplications;
-
-    auto host_b = vec_type::create(
-        exec->get_master(),
-        gko::batch_dim<2>(num_total_systems, gko::dim<2>(nrows, 1)));
-
-    for (size_type isys = 0; isys < num_total_systems; isys++) {
-        for (int irow = 0; irow < nrows; irow++) {
-            host_b->at(isys, irow, 0) = static_cast<value_type>(rand());
-        }
-    }
-
-    auto b = vec_type::create(exec);
-    b->copy_from(host_b.get());
+    auto batch_vec_stride = gko::batch_stride(num_systems, 1);
+    // Create RHS, again reusing application allocation
+    auto b_view = gko::array<value_type>::const_view(
+        exec, num_systems * num_rows, appl_sys.all_rhs);
+    auto b = vec_type::create_const(exec, batch_vec_size, std::move(b_view),
+                                    batch_vec_stride);
 
     auto x = vec_type::create(exec, b->get_size());
 
@@ -189,7 +202,6 @@ int main(int argc, char* argv[])
     // Generate the batch solver from the batch matrix
     auto solver = solver_gen->generate(A);
 
-    const int num_rounds = 50;
     const int leave_first = 5;
 
     for (int i = 0; i < leave_first; i++) {
@@ -201,7 +213,7 @@ int main(int argc, char* argv[])
     solver->initialize_preprocess_time(0.0);
     auto start = std::chrono::high_resolution_clock::now();
 
-    for (int i = 0; i < num_rounds; i++) {
+    for (int i = 0; i < num_reps; i++) {
         // Solve the batch system
         solver->apply(lend(b), lend(x));
     }
@@ -210,36 +222,125 @@ int main(int argc, char* argv[])
     auto stop = std::chrono::high_resolution_clock::now();
 
     auto duration =
-        std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+        std::chrono::duration_cast<std::chrono::duration<double>>(stop - start);
+    auto apply_time = duration.count();
 
-    double total_time_millisec =
-        (double)(std::chrono::duration_cast<std::chrono::microseconds>(stop -
-                                                                       start))
-            .count() /
-        (double)1000;
+    auto b_norm = gko::batch_initialize<real_vec_type>(num_systems, {0.0},
+                                                       exec->get_master());
+    b->compute_norm2(lend(b_norm));
+    // we need constants on the device
+    auto one = gko::batch_initialize<vec_type>(num_systems, {1.0}, exec);
+    auto neg_one = gko::batch_initialize<vec_type>(num_systems, {-1.0}, exec);
+    // allocate and compute the residual
+    auto res = vec_type::create(exec, batch_vec_size);
+    res->copy_from(lend(b));
 
-    if (approach ==
-        gko::solver::batch_tridiag_solve_approach::vendor_provided) {
-        std::cout << "\n\nThe total time before subtraction: "
-                  << total_time_millisec << " millisec " << std::endl;
-
-        total_time_millisec -= solver->get_preprocess_time();
-        std::cout << "\n\nThe total time subtracted (time taken up by buffer "
-                     "size calculation + allocations): "
-                  << solver->get_preprocess_time()
-                  << " and now finally time is: " << total_time_millisec
-                  << " millisec " << std::endl;
+    A->apply(lend(one), lend(x), lend(neg_one), lend(res));
+    // allocate and compute residual norm
+    auto res_norm = gko::batch_initialize<real_vec_type>(num_systems, {0.0},
+                                                         exec->get_master());
+    res->compute_norm2(lend(res_norm));
+    if (print_residuals) {
+        std::cout << "Residual norm sqrt(r^T r):\n";
+        // "unbatch" converts a batch object into a vector of objects of the
+        //   corresponding single type, eg. BatchDense --> vector<Dense>.
+        auto unb_res = res_norm->unbatch();
+        auto unb_bnorm = b_norm->unbatch();
+        for (size_type i = 0; i < num_systems; ++i) {
+            std::cout << " System no. " << i
+                      << ": residual norm = " << unb_res[i]->at(0, 0)
+                      << std::endl;
+            const real_type relresnorm =
+                unb_res[i]->at(0, 0) / unb_bnorm[i]->at(0, 0);
+            if (!(relresnorm <= 1e-6)) {
+                std::cout << "System " << i << " converged only to "
+                          << relresnorm << " relative residual." << std::endl;
+            }
+        }
+    }
+    if (print_time) {
+        std::cout << apply_time / num_reps << std::endl;
+    } else {
+        std::cout << "Solver type: " << in_strat
+                  << "\nMatrix size: " << A->get_size().at(0)
+                  << "\nNum batch entries: " << A->get_num_batch_entries()
+                  << "\nEntire solve took: " << apply_time / num_reps
+                  << " seconds." << std::endl;
+        if (approach ==
+            gko::solver::batch_tridiag_solve_approach::vendor_provided) {
+            apply_time -= (solver->get_preprocess_time() * 1000);
+            std::cout << "Preprocess time"
+                      << solver->get_preprocess_time() * 1000
+                      << "secs\nThe solve time (without pre-processing)"
+                      << apply_time / num_reps << " secs " << std::endl;
+        }
     }
 
-    double av_time_millisec = total_time_millisec / num_rounds;
-
-    std::cout << "\nThe entire solve took " << av_time_millisec
-              << " milliseconds." << std::endl;
-
-    std::ofstream timings_file;
-    timings_file.open(log_file, std::ofstream::app);
-    timings_file << num_total_systems << "  " << av_time_millisec << " \n";
-    timings_file.close();
-
     return 0;
+}
+
+
+ApplSysData appl_generate_system(const int num_rows,
+                                 const size_type num_systems,
+                                 std::shared_ptr<gko::Executor> exec)
+{
+    int nnz = num_rows * 3;
+    std::ranlux48 rgen(15);
+    std::normal_distribution<real_type> distb(0.5, 0.1);
+    std::vector<value_type> sup_diag_values(num_rows * num_systems);
+    std::vector<value_type> main_diag_values(num_rows * num_systems);
+    std::vector<value_type> sub_diag_values(num_rows * num_systems);
+    for (size_type sys = 0; sys < num_systems; ++sys) {
+        for (size_type row = 0; row < num_rows; ++row) {
+            if (row == 0) {
+                sup_diag_values[sys * num_rows + row] = value_type{-2};
+                main_diag_values[sys * num_rows + row] = value_type{4};
+                sub_diag_values[sys * num_rows + row] = value_type{0};
+            } else if (row == num_rows - 1) {
+                sup_diag_values[sys * num_rows + row] = value_type{0};
+                main_diag_values[sys * num_rows + row] = value_type{4};
+                sub_diag_values[sys * num_rows + row] = value_type{-1};
+            } else {
+                sup_diag_values[sys * num_rows + row] = value_type{-2};
+                main_diag_values[sys * num_rows + row] = value_type{4};
+                sub_diag_values[sys * num_rows + row] = value_type{-1};
+            }
+        }
+    }
+    std::vector<value_type> allb(num_rows * num_systems);
+    for (size_type isys = 0; isys < num_systems; isys++) {
+        const value_type bval = distb(rgen);
+        std::fill(allb.begin() + isys * num_rows,
+                  allb.begin() + (isys + 1) * num_rows, bval);
+    }
+
+    value_type* const sup_diag =
+        exec->alloc<value_type>(num_rows * num_systems);
+    exec->copy_from(exec->get_master().get(),
+                    static_cast<size_type>(num_rows * num_systems),
+                    sup_diag_values.data(), sup_diag);
+    value_type* const main_diag =
+        exec->alloc<value_type>(num_rows * num_systems);
+    exec->copy_from(exec->get_master().get(),
+                    static_cast<size_type>(num_rows * num_systems),
+                    main_diag_values.data(), main_diag);
+    value_type* const sub_diag =
+        exec->alloc<value_type>(num_rows * num_systems);
+    exec->copy_from(exec->get_master().get(),
+                    static_cast<size_type>(num_rows * num_systems),
+                    sub_diag_values.data(), sub_diag);
+    value_type* const all_b = exec->alloc<value_type>(num_systems * num_rows);
+    exec->copy_from(exec->get_master().get(), num_systems * num_rows,
+                    allb.data(), all_b);
+    return {num_systems, num_rows, nnz, sup_diag, main_diag, sub_diag, all_b};
+}
+
+void appl_clean_up(ApplSysData& appl_data, std::shared_ptr<gko::Executor> exec)
+{
+    // In general, the application would control non-const pointers;
+    //  the const casts below would not be needed.
+    exec->free(const_cast<value_type*>(appl_data.sub_diag));
+    exec->free(const_cast<value_type*>(appl_data.sup_diag));
+    exec->free(const_cast<value_type*>(appl_data.main_diag));
+    exec->free(const_cast<value_type*>(appl_data.all_rhs));
 }
