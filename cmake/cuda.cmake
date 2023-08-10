ee9aead534950c36b47d7f8893f026b0112d6998
Tobias Ribizel <ribizel@kit.edu>
2023-08-10 13:57:44 +0200

    review updates
    - Only detect available GPUs if requested
    - Remove unnecessary include paths
    - Remove unnecessary config variables
    
    Co-authored-by: Marcel Koch <marcel.koch@kit.edu>
    Co-authored-by: Terry Cojean <terry.cojean@kit.edu>

--- cmake/cuda.cmake
+++ cmake/cuda.cmake
@@ -4,10 +4,9 @@ find_package(CUDAToolkit REQUIRED)
 
 include(cmake/Modules/CudaArchitectureSelector.cmake)
 
-# Detect the CUDA architecture and propagate to all the project
-cas_variable_cmake_cuda_architectures(cuda_detected_archs ${GINKGO_CUDA_ARCHITECTURES})
 if(NOT CMAKE_CUDA_ARCHITECTURES)
-    set(CMAKE_CUDA_ARCHITECTURES "${cuda_detected_archs}")
+    # Detect the CUDA architecture and propagate it to the entire project
+    cas_variable_cmake_cuda_architectures(CMAKE_CUDA_ARCHITECTURES ${GINKGO_CUDA_ARCHITECTURES})
 endif()
 
 find_package(NVTX REQUIRED)
