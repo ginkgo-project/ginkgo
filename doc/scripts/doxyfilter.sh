c520ce5f1d6c52c92e0225074a8e93e86f6c641a
Marcel Koch <marcel.koch@kit.edu>
2023-08-15 15:54:17 +0200

    fixup! automatic license update

--- doc/scripts/doxyfilter.sh
+++ doc/scripts/doxyfilter.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+
+# SPDX-FileCopyrightText: 2017-2023 The Ginkgo authors
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
 sed -e :a -e '/\\$/N;s/\\\n */ /;ta' $1 | sed '/#/d;/^\w*$/d' | sort
