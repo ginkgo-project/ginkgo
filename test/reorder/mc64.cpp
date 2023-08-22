ffdf8204835060dc9738f36204be52d36b291aec
Tobias Ribizel <ribizel@kit.edu>
2023-08-22 08:07:51 +0200

    make MC64 core functionality

--- test/reorder/mc64.cpp
+++ test/reorder/mc64.cpp
@@ -36,48 +36,62 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <gtest/gtest.h>
 
 
+#include <ginkgo/core/base/composition.hpp>
+
+
 #include "core/test/utils/assertions.hpp"
+#include "test/utils/executor.hpp"
 
 
 namespace {
 
 
-class Mc64 : public ::testing::Test {
+class Mc64 : public CommonTestFixture {
 protected:
     using v_type = double;
     using i_type = int;
     using CsrMtx = gko::matrix::Csr<v_type, i_type>;
-    using reorder_type = gko::reorder::Mc64<v_type, i_type>;
-    using perm_type = gko::matrix::Permutation<i_type>;
-
+    using reorder_type = gko::experimental::reorder::Mc64<v_type, i_type>;
+    using result_type = gko::Composition<v_type>;
+    using perm_type = gko::matrix::ScaledPermutation<v_type, i_type>;
 
     Mc64()
-        : exec(gko::OmpExecutor::create()),
-          // clang-format off
-          p_mtx(gko::initialize<CsrMtx>({{1.0, 2.0, 0.0, -1.3, 2.1},
-                                         {2.0, 5.0, 1.5, 0.0, 0.0},
-                                         {0.0, 1.5, 1.5, 1.1, 0.0},
-                                         {-1.3, 0.0, 1.1, 2.0, 0.0},
-                                         {2.1, 0.0, 0.0, 0.0, 1.0}},
-                                        exec)),
-          // clang-format on
-          mc64_factory(reorder_type::build().on(exec)),
-          reorder_op(mc64_factory->generate(p_mtx))
+        : mtx(gko::initialize<CsrMtx>({{1.0, 2.0, 0.0, -1.3, 2.1},
+                                       {2.0, 5.0, 1.5, 0.0, 0.0},
+                                       {0.0, 1.5, 1.5, 1.1, 0.0},
+                                       {-1.3, 0.0, 1.1, 2.0, 0.0},
+                                       {2.1, 0.0, 0.0, 0.0, 1.0}},
+                                      ref)),
+          dmtx(mtx->clone(exec)),
+          mc64_factory(reorder_type::build().on(ref)),
+          dmc64_factory(reorder_type::build().on(exec))
     {}
 
-    std::shared_ptr<const gko::Executor> exec;
-    std::unique_ptr<reorder_type::Factory> mc64_factory;
-    std::shared_ptr<CsrMtx> p_mtx;
-    std::unique_ptr<reorder_type> reorder_op;
+    std::pair<std::shared_ptr<const perm_type>,
+              std::shared_ptr<const perm_type>>
+    unpack(const result_type* result)
+    {
+        GKO_ASSERT_EQ(result->get_operators().size(), 2);
+        return std::make_pair(gko::as<perm_type>(result->get_operators()[0]),
+                              gko::as<perm_type>(result->get_operators()[1]));
+    }
+
+    std::unique_ptr<reorder_type> mc64_factory;
+    std::unique_ptr<reorder_type> dmc64_factory;
+    std::shared_ptr<CsrMtx> mtx;
+    std::shared_ptr<CsrMtx> dmtx;
 };
 
 
-TEST_F(Mc64, IsExecutedOnReferenceExecutor)
+TEST_F(Mc64, IsEquivalentToReference)
 {
-    // This only executes successfully if computed on reference executor.
-    auto p = reorder_op->get_permutation();
+    auto perm = mc64_factory->generate(mtx);
+    auto dperm = dmc64_factory->generate(dmtx);
 
-    ASSERT_TRUE(true);
+    auto ops = unpack(perm.get());
+    auto dops = unpack(dperm.get());
+    GKO_ASSERT_MTX_NEAR(ops.first, dops.first, 0.0);
+    GKO_ASSERT_MTX_NEAR(ops.second, dops.second, 0.0);
 }
 
 
