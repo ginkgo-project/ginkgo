6939480449699308002cc4283a5d177b9198bd9d
Yu-Hsiang M. Tsai <yhmtsai@gmail.com>
2023-10-05 16:53:12 +0200

    update script

--- examples/mixed-multigrid-experiment/collect_case.sh
+++ examples/mixed-multigrid-experiment/collect_case.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 AMGX_LISTS="
 2cubes_sphere
@@ -22,22 +22,22 @@ for num_levels in 10; do
         for mg_mode in ${MG_MODE}; do
             for sm_mode in ${SM_MODE}; do
                 output=${RESULT_FOLDER}/collect_case_${num_levels}_${cycle}_${mg_mode}_${sm_mode}.csv
-                echo "matrix, final residual, iteration count, generation time[ms], total execution time[ms], executation time per iteration[ms]" > ${output}
+                echo "matrix, final residual, iteration count, generation time[ms], total execution time[ms], executation time per iteration[ms], total execution median time[ms]" > ${output}
                 for list in AMGX_LISTS MFEM_LISTS; do
                     for matrix in ${!list}; do
                         echo "matrix ${matrix} in ${mtx}"
                         line=""
-                        for mixed_mode in 0 1 2 3 -11 -12 -13 -21 -22 -23; do
+                        for mixed_mode in 0 1 2 3 7 8 -11 -12 -13 -21 -22 -23 -32 -33 -42 -43; do
                             echo "|_ mixed_mode ${mixed_mode}"
                             # if [[ "${mixed_mode}" != "0" ]]; then
                             #     line="${line}, "
                             # fi
                             label="${matrix}_mixed${mixed_mode}"
                             suffix="_level${num_levels}_cycle${cycle}_mode${mg_mode}_${sm_mode}"
-                            file="${RESULT_FOLDER}/${label}${suffix}.txt"
+                            file="${RESULT_FOLDER}/${label}${suffix}_scale0.txt"
                             # delete the line with CRAYBLAS_WARNING from frontier
-                            residual_norm=$(sed '/CRAYBLAS_WARNING/d' ${file} | tail -n 5 | head -n 1)
-                            info=$(sed '/CRAYBLAS_WARNING/d' ${file} | tail -n 4 | sed -E 's/[^0-9\.]//g' | tr '\n' ',' | sed -E 's/,$/\n/g')
+                            residual_norm=$(sed '/CRAYBLAS_WARNING/d' ${file} | tail -n 7 | head -n 1)
+                            info=$(sed '/CRAYBLAS_WARNING/d' ${file} | tail -n 6 | head -n 5 | sed -E 's/[^0-9\.]//g' | tr '\n' ',' | sed -E 's/,$/\n/g')
                             # line="${line}${label}, ${residual_norm}, ${info}"
                             echo "${label}, ${residual_norm}, ${info}" >> "${output}"
                         done
--- examples/mixed-multigrid-experiment/gen_table.sh
+++ examples/mixed-multigrid-experiment/gen_table.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 AMGX_LISTS="
 2cubes_sphere
@@ -13,32 +13,33 @@ beam-pw-sv0.1-o-3-l-3
 l-shape-const-o-3-l-7
 "
 RESULT_FOLDER=$1
+smoother=jacobi
 for num_levels in 10; do
-    for cycle in v w f; do
-        for mg_mode in solver cg; do
+    for cycle in v; do
+        for mg_mode in cg; do
             output=${RESULT_FOLDER}/latex_${num_levels}_${cycle}_${mg_mode}.tex
             echo "\begin{table*}[t]" > ${output}
             echo "\centering" >> ${output}
             echo "\scriptsize" >> ${output}
-            echo "\begin{tabular}{c | r r r | r r r | r r r | r r r }" >> ${output}
+            echo "\begin{tabular}{c | r r r | r r r | r r r | r r r | r r r | r r r}" >> ${output}
             echo "\toprule" >> ${output}
-            echo " & \multicolumn{3}{c|}{\gko's AMG (DP)} & \multicolumn{3}{c|}{\gko's AMG (DP SP)} & \multicolumn{3}{c|}{\gko's AMG (DP FP HP)} & \multicolumn{3}{c}{\gko's AMG (DP HP)} \\\\" >> ${output}
-            echo "problem & res. norm & \#iter & time[ms] & res. norm & \#iter & time[ms] & res. norm & \#iter & time[ms] & res. norm & \#iter & time[ms] \\\\" >> ${output}
+            echo " & \multicolumn{3}{c|}{\gko's AMG (DP)} & \multicolumn{3}{c|}{\gko's AMG (DP SP)} & \multicolumn{3}{c|}{\gko's AMG (DP FP HP)} & \multicolumn{3}{c|}{\gko's AMG (DP HP)} & \multicolumn{3}{c|}{\gko's AMG (DP SP BF)}  & \multicolumn{3}{c}{\gko's AMG (DP BF)} \\\\" >> ${output}
+            echo "problem & res. norm & \#iter & time[ms] & res. norm & \#iter & time[ms] & res. norm & \#iter & time[ms] & res. norm & \#iter & time[ms] & res. norm & \#iter & time[ms] & res. norm & \#iter & time[ms] \\\\" >> ${output}
             echo "\midrule" >> ${output}
             for list in AMGX_LISTS MFEM_LISTS; do
                 for matrix in ${!list}; do
                     echo "matrix ${matrix} in ${mtx}"
                     line="${matrix}"
-                    for mixed_mode in 0 1 2 3; do
+                    for mixed_mode in 0 1 2 3 7 8; do
                         echo "|_ mixed_mode ${mixed_mode}"
                         label="${matrix}_mixed${mixed_mode}_level${num_levels}"
-                        suffix="_cycle${cycle}_mode${mg_mode}"
+                        suffix="_cycle${cycle}_mode${mg_mode}_${smoother}_scale0"
                         file="${RESULT_FOLDER}/${label}${suffix}.txt"
-                        residual_norm=$(tail -n 5 ${file} | head -n 1)
-                        info=$(tail -n 4 ${file} | sed -E 's/[^0-9\.]//g' | tr '\n' ',' | sed -E 's/,$/\n/g')
-                        # iteration count, generation time[ms], total execution time[ms], executation time per iteration[ms]
+                        residual_norm=$(tail -n 7 ${file} | head -n 1)
+                        info=$(tail -n 6 ${file} | head -n 5 | sed -E 's/[^0-9\.]//g' | tr '\n' ',' | sed -E 's/,$/\n/g')
+                        # iteration count, generation time[ms], total execution time[ms], executation time per iteration[ms], total execution median time[ms]
                         IFS=',' read -r -a array <<< "$info"
-                        line="${line} & ${residual_norm} & ${array[0]} & ${array[2]}"
+                        line="${line} & ${residual_norm} & ${array[0]} & ${array[4]}"
                     done
                     echo "${line} \\\\" >> "${output}"
                 done
