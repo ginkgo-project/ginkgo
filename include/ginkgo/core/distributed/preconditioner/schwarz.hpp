f93da9ec843a1b9864fcb455ac70bf89a1eae994
Pratik Nayak <pratikvn@protonmail.com>
2023-10-08 23:26:29 +0200

    multilevel schwarz WIP

--- include/ginkgo/core/distributed/preconditioner/schwarz.hpp
+++ include/ginkgo/core/distributed/preconditioner/schwarz.hpp
@@ -19,6 +19,7 @@
 #include <ginkgo/core/config/type_descriptor.hpp>
 #include <ginkgo/core/distributed/matrix.hpp>
 #include <ginkgo/core/distributed/vector.hpp>
+#include <ginkgo/core/multigrid/multigrid_level.hpp>
 
 
 namespace gko {
@@ -78,6 +79,12 @@ public:
          */
         std::shared_ptr<const LinOp> GKO_FACTORY_PARAMETER_SCALAR(
             generated_local_solver, nullptr);
+
+        /**
+         * Coarse solver factory.
+         */
+        std::shared_ptr<const LinOpFactory> GKO_FACTORY_PARAMETER_SCALAR(
+            coarse_solver_factory, nullptr);
     };
     GKO_ENABLE_LIN_OP_FACTORY(Schwarz, parameters, Factory);
     GKO_ENABLE_BUILD_METHOD(Factory);
@@ -110,6 +117,7 @@ protected:
      */
     explicit Schwarz(std::shared_ptr<const Executor> exec)
         : EnableLinOp<Schwarz>(std::move(exec))
+
     {}
 
     /**
@@ -150,6 +158,7 @@ private:
     void set_solver(std::shared_ptr<const LinOp> new_solver);
 
     std::shared_ptr<const LinOp> local_solver_;
+    std::shared_ptr<const multigrid::MultigridLevel> coarse_solver_;
 };
 
 
