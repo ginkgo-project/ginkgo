6ce4a8c9bf401d23ebfcb8f400254b3194fa3b45
Hartwig Anzt <hanzt@icl.utk.edu>
2023-10-11 01:36:50 +0000

    change to Jacobi style updates

--- examples/cuda-kernel-integration/cuda-kernel.cu
+++ examples/cuda-kernel-integration/cuda-kernel.cu
@@ -13,21 +13,26 @@ __global__ void parsinv_kernel(
     int *Srowptr, // row pointer S
     int *Srowidx, // row index S 
     int *Scolidx, //col index S 
-    double *Sval // val array S 
+    double *Sval, // val array S
+    double *tval 
     ){
     
     int threadidx = blockIdx.x * blockDim.x + threadIdx.x;
     
     if (threadidx < Snnz) {
         int i, j, il, is, jl, js;
-        double Lii, s, sp;
+        double Lii, s=0.0, sp;
 
         // handle element S(threadidx) = S(i,j)
         i = Srowidx[ threadidx ];
         j = Scolidx[ threadidx ];
 
+
+	// we are working on a symmetric matrix S
+        // if we notice j>i, we compute S(j,i) instead of S(i,j)
+        // maybe later
         if( i>j ){
-            // swap indices - there might be a more efficient way, though
+	    // swap indices - there might be a more efficient way, though
             int t = i;
             i = j;
             j = t;
@@ -35,13 +40,6 @@ __global__ void parsinv_kernel(
 
         // retrieve L(i,i), easy as these are the first element in each row
         Lii = Lval[ Lrowptr[ i ] ];
-
-
-        // we are working on a symmetric matrix S
-        // if we notice j>i, we compute S(j,i) instead of S(i,j)
-        // maybe later
-
-
 
         // compute L(i,:).* S(j,:)
         // il and is are iterating over the nonzero entries in the respective rows
@@ -53,17 +51,17 @@ __global__ void parsinv_kernel(
             jl = Lcolidx[ il ];
             js = Scolidx[ is ];
             sp = (jl == js) ? Lval[ il ] * Sval[ is ] : sp;
-            s = (jl == js) ? s-sp : s;
+            s = (jl == js) ? s+sp : s;
             il = (jl <= js) ? il+1 : il;
             is = (jl >= js) ? is+1 : is;
         }
-        s += sp;  // undo the last operation (it must be the last)    
+        s -= sp;  // undo the last operation (it must be the last)    
         s = 1. / Lii * s; // scaling
         
         if (i == j) // diagonal element
-            Sval[ threadidx ] = 1. / ( Lii * Lii) - s;
+            tval[ threadidx ] = 1. / ( Lii * Lii) - s;
         else  
-            Sval[ threadidx ] = - s;
+            tval[ threadidx ] = - s;
 
 
 
@@ -84,9 +82,13 @@ void parsinv(
     int *Scolidx, //col index S
     double *Sval // val array S
     ){
-
     unsigned int grid_dim = (Snnz + default_block_size - 1) / default_block_size;
+    double *tval;
+    cudaMalloc(&tval, sizeof(double)*Snnz);
+
     for( int i=0; i<10; i++){
-    parsinv_kernel<<<dim3(grid_dim), dim3(default_block_size)>>>(n, Lnnz, Lrowptr, Lcolidx, Lval, Snnz, Srowptr, Srowidx, Scolidx, Sval);
+    parsinv_kernel<<<dim3(grid_dim), dim3(default_block_size)>>>(n, Lnnz, Lrowptr, Lcolidx, Lval, Snnz, Srowptr, Srowidx, Scolidx, Sval, tval);
+    cudaMemcpy(Sval, tval, sizeof(double)*Snnz, cudaMemcpyDeviceToDevice);
     }
+    cudaFree(tval);
 }
