b08c49048920c2a83c9b4465bf3a55843ae515b0
Yuhsiang M. Tsai <yhmtsai@gmail.com>
2023-10-13 10:09:53 +0200

    do not delete deprecated var from CMake, keep doc
    Co-authored-by: Tobias Ribizel <ribizel@kit.edu>

--- cmake/rename.cmake
+++ cmake/rename.cmake
@@ -1,5 +1,5 @@
 # Only for CACHE variable (option)
-macro(gko_rename_cache deprecated actual type)
+macro(gko_rename_cache deprecated actual type doc_string)
     if(DEFINED ${deprecated})
         if(DEFINED ${actual})
             message("actual ${actual} and deprecated ${deprecated}")
@@ -8,18 +8,13 @@ macro(gko_rename_cache deprecated actual type)
                 message(WARNING "${deprecated} was deprecated, please only use ${actual} instead.")
             else()
                 # They are different
-                set(${deprecated}_copy ${${deprecated}})
-                unset(${deprecated} CACHE)
-                message(FATAL_ERROR "Both ${deprecated} and ${actual} were specified, please use ${actual} instead.  "
-                    "We remove ${deprecated}:${${deprecated}_copy} and keep ${actual}:${${actual}}")
+                message(FATAL_ERROR "Both ${deprecated} and ${actual} were specified differently, please only use ${actual} instead.")
             endif()
         else()
             # Only set `deprecated`, move it to `actual`.
             message(WARNING "${deprecated} was deprecated, please use ${actual} instead.  "
-                "We copy ${${deprecated}} to ${actual} and unset ${deprecated}.")
-            set(${actual} ${${deprecated}} CACHE ${type} "")
+                "We copy ${${deprecated}} to ${actual}")
+            set(${actual} ${${deprecated}} CACHE ${type} "${doc_string}")
         endif()
-        # We always unset the deprecated for easier next setup
-        unset(${deprecated} CACHE)
     endif()
 endmacro()
