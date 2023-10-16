7a036c7ac58345246bba99cecf91de0aec260370
Tobias Ribizel <mail@ribizel.de>
2023-10-16 13:48:16 +0200

    use proper atomics for ParILU(T) and ParIC(T)

--- cuda/factorization/par_ic_kernels.cu
+++ cuda/factorization/par_ic_kernels.cu
@@ -40,6 +40,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include "cuda/base/math.hpp"
 #include "cuda/base/types.hpp"
+#include "cuda/components/memory.cuh"
 #include "cuda/components/thread_ids.cuh"
 
 
