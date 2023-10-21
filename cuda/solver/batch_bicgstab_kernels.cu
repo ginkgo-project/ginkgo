68041304ed6509421291d39fe6c3f92e1e44822d
Pratik Nayak <pratikvn@protonmail.com>
2023-10-21 20:40:57 +0200

    Add ref and omp kernels and placeholders
    Co-authored-by: Aditya Kashi <kashia@ornl.gov>

---
+++ cuda/solver/batch_bicgstab_kernels.cu
@@ -0,0 +1,87 @@
+/*******************************<GINKGO LICENSE>******************************
+Copyright (c) 2017-2023, the Ginkgo authors
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+1. Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+contributors may be used to endorse or promote products derived from
+this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+******************************<GINKGO LICENSE>*******************************/
+
+#include "core/solver/batch_bicgstab_kernels.hpp"
+
+
+#include <ginkgo/core/base/exception_helpers.hpp>
+#include <ginkgo/core/base/math.hpp>
+
+
+#include "core/solver/batch_dispatch.hpp"
+#include "cuda/base/config.hpp"
+#include "cuda/base/exception.cuh"
+#include "cuda/base/kernel_config.cuh"
+#include "cuda/base/types.hpp"
+#include "cuda/components/cooperative_groups.cuh"
+#include "cuda/components/thread_ids.cuh"
+#include "cuda/matrix/batch_struct.hpp"
+
+
+namespace gko {
+namespace kernels {
+namespace cuda {
+
+
+// NOTE: this default block size is not used for the main solver kernel.
+constexpr int default_block_size = 256;
+constexpr int sm_oversubscription = 4;
+
+/**
+ * @brief The batch Bicgstab solver namespace.
+ *
+ * @ingroup batch_bicgstab
+ */
+namespace batch_bicgstab {
+
+
+template <typename T>
+using BatchBicgstabOptions =
+    gko::kernels::batch_bicgstab::BatchBicgstabOptions<T>;
+
+
+template <typename ValueType>
+void apply(std::shared_ptr<const DefaultExecutor> exec,
+           const BatchBicgstabOptions<remove_complex<ValueType>>& opts,
+           const batch::BatchLinOp* const a,
+           const batch::BatchLinOp* const precon,
+           const batch::MultiVector<ValueType>* const b,
+           batch::MultiVector<ValueType>* const x,
+           batch::log::BatchLogData<double>& logdata) GKO_NOT_IMPLEMENTED;
+
+GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE(GKO_DECLARE_BATCH_BICGSTAB_APPLY_KERNEL);
+
+
+}  // namespace batch_bicgstab
+}  // namespace cuda
+}  // namespace kernels
+}  // namespace gko
---
+++ reference/solver/batch_bicgstab_kernels.cpp
@@ -0,0 +1,135 @@
+/*******************************<GINKGO LICENSE>******************************
+Copyright (c) 2017-2023, the Ginkgo authors
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+1. Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+contributors may be used to endorse or promote products derived from
+this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+******************************<GINKGO LICENSE>*******************************/
+
+#include "core/solver/batch_bicgstab_kernels.hpp"
+
+
+#include "core/solver/batch_dispatch.hpp"
+
+
+namespace gko {
+namespace kernels {
+namespace reference {
+
+
+/**
+ * @brief The batch Bicgstab solver namespace.
+ *
+ * @ingroup batch_bicgstab
+ */
+namespace batch_bicgstab {
+
+
+namespace {
+
+
+constexpr int max_num_rhs = 1;
+
+
+#include "reference/base/batch_multi_vector_kernels.hpp.inc"
+#include "reference/matrix/batch_dense_kernels.hpp.inc"
+#include "reference/matrix/batch_ell_kernels.hpp.inc"
+#include "reference/solver/batch_bicgstab_kernels.hpp.inc"
+
+
+}  // unnamed namespace
+
+
+template <typename T>
+using BatchBicgstabOptions =
+    gko::kernels::batch_bicgstab::BatchBicgstabOptions<T>;
+
+template <typename ValueType>
+class KernelCaller {
+public:
+    KernelCaller(std::shared_ptr<const DefaultExecutor> exec,
+                 const BatchBicgstabOptions<remove_complex<ValueType>> opts)
+        : exec_{std::move(exec)}, opts_{opts}
+    {}
+
+    template <typename BatchMatrixType, typename PrecType, typename StopType,
+              typename LogType>
+    void call_kernel(
+        const LogType& logger, const BatchMatrixType& a, PrecType prec,
+        const gko::batch::multi_vector::uniform_batch<const ValueType>& b,
+        const gko::batch::multi_vector::uniform_batch<ValueType>& x) const
+    {
+        using real_type = typename gko::remove_complex<ValueType>;
+        const size_type nbatch = a.num_batch_items;
+        const auto nrows = a.num_rows;
+        const auto nrhs = b.num_rhs;
+        if (nrhs > 1) {
+            GKO_NOT_IMPLEMENTED;
+        }
+
+        const int local_size_bytes =
+            gko::kernels::batch_bicgstab::local_memory_requirement<ValueType>(
+                nrows, nrhs) +
+            PrecType::dynamic_work_size(nrows, a.get_num_nnz()) *
+                sizeof(ValueType);
+        // array<unsigned char> local_space(exec_, local_size_bytes);
+        std::vector<unsigned char> local_space(local_size_bytes);
+
+        for (size_type ibatch = 0; ibatch < nbatch; ibatch++) {
+            batch_entry_bicgstab_impl<StopType, PrecType, LogType,
+                                      BatchMatrixType, ValueType>(
+                opts_, logger, prec, a, b, x, ibatch, local_space.data());
+        }
+    }
+
+private:
+    const std::shared_ptr<const DefaultExecutor> exec_;
+    const BatchBicgstabOptions<remove_complex<ValueType>> opts_;
+};
+
+
+template <typename ValueType>
+void apply(std::shared_ptr<const DefaultExecutor> exec,
+           const BatchBicgstabOptions<remove_complex<ValueType>>& opts,
+           const batch::BatchLinOp* const a,
+           const batch::BatchLinOp* const precon,
+           const batch::MultiVector<ValueType>* const b,
+           batch::MultiVector<ValueType>* const x,
+           batch::log::BatchLogData<double>& logdata)
+{
+    auto dispatcher = batch::solver::create_dispatcher<ValueType>(
+        KernelCaller<ValueType>(exec, opts), opts, a, precon);
+    dispatcher.apply(b, x, logdata);
+}
+
+GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE(GKO_DECLARE_BATCH_BICGSTAB_APPLY_KERNEL);
+
+
+}  // namespace batch_bicgstab
+}  // namespace reference
+}  // namespace kernels
+}  // namespace gko
