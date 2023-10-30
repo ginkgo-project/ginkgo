0a57b5a43b01a0f0e94b16f9d296f9fc37939aeb
Tobias Ribizel <mail@ribizel.de>
2023-10-30 18:51:26 -0400

    add OpenMP tests

--- test/factorization/lu_kernels.cpp
+++ test/factorization/lu_kernels.cpp
@@ -53,6 +53,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "core/components/prefix_sum_kernels.hpp"
 #include "core/factorization/cholesky_kernels.hpp"
 #include "core/factorization/elimination_forest.hpp"
+#include "core/factorization/symbolic.hpp"
 #include "core/matrix/csr_kernels.hpp"
 #include "core/matrix/csr_lookup.hpp"
 #include "core/test/utils.hpp"
@@ -234,6 +235,48 @@ TYPED_TEST(Lu, KernelFactorizeIsEquivalentToRef)
             ddiag_idxs.get_const_data(), this->dmtx_lu.get(), dtmp);
 
         GKO_ASSERT_MTX_NEAR(this->mtx_lu, this->dmtx_lu, r<value_type>::value);
+    });
+}
+
+
+TYPED_TEST(Lu, SymbolicCholeskyWorks)
+{
+    using value_type = typename TestFixture::value_type;
+    using index_type = typename TestFixture::index_type;
+    this->forall_matrices([this] {
+        std::unique_ptr<gko::matrix::Csr<value_type, index_type>> dlu;
+        std::unique_ptr<gko::factorization::elimination_forest<index_type>>
+            forest;
+        gko::factorization::symbolic_cholesky(this->dmtx.get(), true, dlu,
+                                              forest);
+
+        GKO_ASSERT_MTX_EQ_SPARSITY(dlu, this->dmtx_lu);
+    });
+}
+
+
+TYPED_TEST(Lu, SymbolicLUWorks)
+{
+    using value_type = typename TestFixture::value_type;
+    using index_type = typename TestFixture::index_type;
+    this->forall_matrices([this] {
+        std::unique_ptr<gko::matrix::Csr<value_type, index_type>> dlu;
+        gko::factorization::symbolic_lu(this->dmtx.get(), dlu);
+
+        GKO_ASSERT_MTX_EQ_SPARSITY(dlu, this->dmtx_lu);
+    });
+}
+
+
+TYPED_TEST(Lu, SymbolicLUNearSymmWorks)
+{
+    using value_type = typename TestFixture::value_type;
+    using index_type = typename TestFixture::index_type;
+    this->forall_matrices([this] {
+        std::unique_ptr<gko::matrix::Csr<value_type, index_type>> dlu;
+        gko::factorization::symbolic_lu_near_symm(this->dmtx.get(), dlu);
+
+        GKO_ASSERT_MTX_EQ_SPARSITY(dlu, this->dmtx_lu);
     });
 }
 
