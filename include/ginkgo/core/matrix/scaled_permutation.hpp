6cb61ecdc023c2202e433a0c1600d3d4d8da6135
Tobias Ribizel <mail@ribizel.de>
2023-10-31 20:03:17 -0400

    fix LaTeX in documentation

--- include/ginkgo/core/matrix/scaled_permutation.hpp
+++ include/ginkgo/core/matrix/scaled_permutation.hpp
@@ -52,7 +52,7 @@ namespace matrix {
 /**
  * ScaledPermutation is a matrix combining a permutation with scaling factors.
  * It is a combination of Diagonal and Permutation, and can be read as
- * $SP = P \pdot S$, i.e. the scaling gets applied before the permutation.
+ * $SP = P \cdot S$, i.e. the scaling gets applied before the permutation.
  *
  * @tparam IndexType  index type of permutation indices
  * @tparam ValueType  value type of the scaling factors
