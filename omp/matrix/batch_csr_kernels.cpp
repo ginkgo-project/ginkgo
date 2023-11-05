f4565b56029f9767dd7ea1bac499b435f58cee74
Pratik Nayak <pratikvn@protonmail.com>
2023-11-05 15:17:14 +0100

    Add omp kernels, dev kernel framework and tests
    Co-authored-by: Aditya Kashi <kashia@ornl.gov>

--- omp/matrix/batch_csr_kernels.cpp
+++ omp/matrix/batch_csr_kernels.cpp
@@ -1,15 +1,43 @@
-// SPDX-FileCopyrightText: 2017-2023 The Ginkgo authors
-//
-// SPDX-License-Identifier: BSD-3-Clause
+/*******************************<GINKGO LICENSE>******************************
+Copyright (c) 2017-2023, the Ginkgo authors
+All rights reserved.
 
-#include "core/matrix/batch_ell_kernels.hpp"
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+1. Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+contributors may be used to endorse or promote products derived from
+this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+******************************<GINKGO LICENSE>*******************************/
+
+#include "core/matrix/batch_csr_kernels.hpp"
 
 
 #include <algorithm>
 
 
 #include <ginkgo/core/base/batch_multi_vector.hpp>
-#include <ginkgo/core/matrix/batch_ell.hpp>
+#include <ginkgo/core/matrix/batch_csr.hpp>
 
 
 #include "core/base/batch_struct.hpp"
@@ -22,19 +50,19 @@ namespace gko {
 namespace kernels {
 namespace omp {
 /**
- * @brief The Ell matrix format namespace.
- * @ref Ell
- * @ingroup batch_ell
+ * @brief The Csr matrix format namespace.
+ * @ref Csr
+ * @ingroup batch_csr
  */
-namespace batch_ell {
+namespace batch_csr {
 
 
-#include "reference/matrix/batch_ell_kernels.hpp.inc"
+#include "reference/matrix/batch_csr_kernels.hpp.inc"
 
 
 template <typename ValueType, typename IndexType>
 void simple_apply(std::shared_ptr<const DefaultExecutor> exec,
-                  const batch::matrix::Ell<ValueType, IndexType>* mat,
+                  const batch::matrix::Csr<ValueType, IndexType>* mat,
                   const batch::MultiVector<ValueType>* b,
                   batch::MultiVector<ValueType>* x)
 {
@@ -51,13 +79,13 @@ void simple_apply(std::shared_ptr<const DefaultExecutor> exec,
 }
 
 GKO_INSTANTIATE_FOR_EACH_VALUE_AND_INT32_TYPE(
-    GKO_DECLARE_BATCH_ELL_SIMPLE_APPLY_KERNEL);
+    GKO_DECLARE_BATCH_CSR_SIMPLE_APPLY_KERNEL);
 
 
 template <typename ValueType, typename IndexType>
 void advanced_apply(std::shared_ptr<const DefaultExecutor> exec,
                     const batch::MultiVector<ValueType>* alpha,
-                    const batch::matrix::Ell<ValueType, IndexType>* mat,
+                    const batch::matrix::Csr<ValueType, IndexType>* mat,
                     const batch::MultiVector<ValueType>* b,
                     const batch::MultiVector<ValueType>* beta,
                     batch::MultiVector<ValueType>* x)
@@ -80,10 +108,10 @@ void advanced_apply(std::shared_ptr<const DefaultExecutor> exec,
 }
 
 GKO_INSTANTIATE_FOR_EACH_VALUE_AND_INT32_TYPE(
-    GKO_DECLARE_BATCH_ELL_ADVANCED_APPLY_KERNEL);
+    GKO_DECLARE_BATCH_CSR_ADVANCED_APPLY_KERNEL);
 
 
-}  // namespace batch_ell
+}  // namespace batch_csr
 }  // namespace omp
 }  // namespace kernels
 }  // namespace gko
