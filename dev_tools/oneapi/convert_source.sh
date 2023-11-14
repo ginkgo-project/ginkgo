35a3ab11e52526fc12da536b26a19b05abc433bf
Marcel Koch <marcel.koch@kit.edu>
2023-11-14 08:23:28 +0100

    review updates:
    - fix script
    - always use `The` in copyright
    
    Co-authored-by: Yu-Hsiang M. Tsai <yhmtsai@gmail.com>

--- dev_tools/oneapi/convert_source.sh
+++ dev_tools/oneapi/convert_source.sh
@@ -168,7 +168,7 @@ rm "${OUTPUT_FILE}"
 echo "#define GET_QUEUE 0" >> "${OUTPUT_FILE}"
 # add empty ginkgo license such that format_header recognize some header before header def macro
 CURRENT_YEAR=$(date +%Y)
-echo "${GINKGO_LICENSE_BEGIN} 2017-${CURRENT_YEAR} The Ginkgo authors" > "${OUTPUT_FILE}"
+echo "${GINKGO_LICENSE_BEGIN} 2017-${CURRENT_YEAR} The Ginkgo authors" >> "${OUTPUT_FILE}"
 echo "//" >> "${OUTPUT_FILE}"
 echo "${GINKGO_LICENSE_END} BSD-3-Clause" >> "${OUTPUT_FILE}"
 rm "${GLOBAL_FILE}"
