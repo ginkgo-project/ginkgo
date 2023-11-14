84652dde9759c70180c68ea4261445e2a9f71452
Marcel Koch <marcel.koch@kit.edu>
2023-11-14 13:01:51 +0100

    uses pre-commit in CI

--- .github/format.sh
+++ .github/format.sh
@@ -3,11 +3,29 @@
 cp .github/bot-pr-format-base.sh /tmp
 source /tmp/bot-pr-format-base.sh
 
+echo "Retrieving PR file list"
+PR_FILES=$(bot_get_all_changed_files ${PR_URL})
+NUM=$(echo "${PR_FILES}" | wc -l)
+echo "PR has ${NUM} changed files"
+
+TO_FORMAT="$(echo "$PR_FILES" | grep -E $EXTENSION_REGEX || true)"
+
+# format files
+dev_tools/scripts/add_license.sh
+dev_tools/scripts/update_ginkgo_header.sh
+for f in $(echo "$TO_FORMAT" | grep -E $FORMAT_HEADER_REGEX); do dev_tools/scripts/format_header.sh "$f"; done
+pipx run pre-commit run --files $TO_FORMAT || true
+
+# restore formatting scripts so they don't appear in the diff
+git checkout -- dev_tools/scripts/*.sh
+
 # check for changed files, replace newlines by \n
-LIST_FILES=$(git diff --name-only | sed '$!s/$/\\n/' | tr -d '\n')
+CHANGES=$(git diff --name-only | sed '$!s/$/\\n/' | tr -d '\n')
+
+echo "$CHANGES"
 
 # commit changes if necessary
-if [[ "$LIST_FILES" != "" ]]; then
+if [[ "$CHANGES" != "" ]]; then
   git commit -a -m "Format files
 
 Co-authored-by: $USER_COMBINED"
