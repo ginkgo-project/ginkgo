a16ca6694d9a8e11294d92502fdee51acc7c7444
Marcel Koch <marcel.koch@kit.edu>
2023-11-16 15:37:09 +0000

    remove format_header.sh

--- .github/bot-pr-format-base.sh
+++ .github/bot-pr-format-base.sh
@@ -14,7 +14,7 @@ git config user.name "ginkgo-bot"
 
 # save scripts from develop
 pushd dev_tools/scripts || exit 1
-cp add_license.sh format_header.sh update_ginkgo_header.sh /tmp
+cp add_license.sh update_ginkgo_header.sh /tmp
 popd || exit 1
 
 # checkout current PR head
@@ -23,5 +23,4 @@ git checkout -b $LOCAL_BRANCH fork/$HEAD_BRANCH
 
 # restore files from develop
 cp /tmp/add_license.sh dev_tools/scripts/
-cp /tmp/format_header.sh dev_tools/scripts/
 cp /tmp/update_ginkgo_header.sh dev_tools/scripts/
