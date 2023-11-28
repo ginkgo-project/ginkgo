d55042eaa7f5adb87cb71aecc1caffa394feda0d
Yu-Hsiang M. Tsai <yhmtsai@gmail.com>
2023-11-28 16:28:37 +0100

    update format

--- include/ginkgo/ginkgo.hpp
+++ include/ginkgo/ginkgo.hpp
@@ -51,8 +51,10 @@
 #include <ginkgo/core/base/utils_helper.hpp>
 #include <ginkgo/core/base/version.hpp>
 
+#include <ginkgo/core/config/config.hpp>
 #include <ginkgo/core/config/data.hpp>
 #include <ginkgo/core/config/property_tree.hpp>
+#include <ginkgo/core/config/registry.hpp>
 
 #include <ginkgo/core/distributed/base.hpp>
 #include <ginkgo/core/distributed/lin_op.hpp>
