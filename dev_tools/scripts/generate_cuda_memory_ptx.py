bf4268809cd562fa12d3d804ad4e2786adbf8c42
Tobias Ribizel <mail@ribizel.de>
2023-12-11 14:58:52 +0100

    add load_*_local loads for workgroup-coherent global memory atomics

--- dev_tools/scripts/generate_cuda_memory_ptx.py
+++ dev_tools/scripts/generate_cuda_memory_ptx.py
@@ -30,6 +30,8 @@ class type_desc:
 memory_spaces = [
     space(ptx_space_suffix=".shared", ptx_scope_suffix=".cta", fn_suffix="_shared",
           ptr_expr="convert_generic_ptr_to_smem_ptr({ptr})", ptr_constraint="r"),
+    space(ptx_space_suffix="", ptx_scope_suffix=".cta", fn_suffix="_local",
+          ptr_expr="{ptr}", ptr_constraint="l"),
     space(ptx_space_suffix="", ptx_scope_suffix=".gpu", fn_suffix="", ptr_expr="{ptr}", ptr_constraint="l")]
 memory_orderings = [
     ordering(ptx_load_suffix=".relaxed", fn_load_suffix="_relaxed",
