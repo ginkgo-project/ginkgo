b8fd28d56fe03ebe9244df3c21e9e79c3c896f50
Tobias Ribizel <mail@ribizel.de>
2023-12-12 10:42:09 +0100

    review updates
    Co-authored-by: Pratik Nayak <pratik.nayak@kit.edu>

--- core/base/array_access.hpp
+++ core/base/array_access.hpp
@@ -20,6 +20,7 @@ namespace gko {
  * device-to-host copy, so it is not suitable for accessing many elements
  * in performance-critical code.
  *
+ * @param array  the array to get the element from.
  * @param index  the array element index.
  * @tparam ValueType  the value type of the array.
  * @return the value at index.
@@ -28,9 +29,7 @@ template <typename ValueType>
 ValueType get_element(const array<ValueType>& array, size_type index)
 {
     // TODO2.0 add bounds check for negative indices
-    if (index >= array.get_size()) {
-        throw OutOfBoundsError{__FILE__, __LINE__, index, array.get_size()};
-    }
+    GKO_ENSURE_IN_BOUNDS(index, array.get_size());
     return array.get_executor()->copy_val_to_host(array.get_const_data() +
                                                   index);
 }
@@ -43,6 +42,7 @@ ValueType get_element(const array<ValueType>& array, size_type index)
  * host-to-device copy, so it is not suitable for accessing many elements
  * in performance-critical code.
  *
+ * @param array  the array to set the element in.
  * @param index  the array element index.
  * @param value  the new value.
  * @tparam ValueType  the value type of the array.
@@ -51,9 +51,7 @@ template <typename ValueType>
 void set_element(array<ValueType>& array, size_type index, ValueType value)
 {
     // TODO2.0 add bounds check for negative indices
-    if (index >= array.get_size()) {
-        throw OutOfBoundsError{__FILE__, __LINE__, index, array.get_size()};
-    }
+    GKO_ENSURE_IN_BOUNDS(index, array.get_size());
     auto exec = array.get_executor();
     exec->copy_from(exec->get_master(), 1, &value, array.get_data() + index);
 }
