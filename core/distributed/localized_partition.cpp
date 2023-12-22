a372ca445b9b494e6971c6605ac83f5caf5e14b5
Marcel Koch <marcel.koch@kit.edu>
2023-12-22 18:18:03 +0100

    use dummy info object
    Cray MPI seems to not work with MPI_INFO_NULL.

--- core/distributed/localized_partition.cpp
+++ core/distributed/localized_partition.cpp
@@ -1,38 +1,11 @@
-/*******************************<GINKGO LICENSE>******************************
-Copyright (c) 2017-2023, the Ginkgo authors
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions
-are met:
-
-1. Redistributions of source code must retain the above copyright
-notice, this list of conditions and the following disclaimer.
-
-2. Redistributions in binary form must reproduce the above copyright
-notice, this list of conditions and the following disclaimer in the
-documentation and/or other materials provided with the distribution.
-
-3. Neither the name of the copyright holder nor the names of its
-contributors may be used to endorse or promote products derived from
-this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
-IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
-TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
-PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-******************************<GINKGO LICENSE>*******************************/
+// SPDX-FileCopyrightText: 2017-2023 The Ginkgo authors
+//
+// SPDX-License-Identifier: BSD-3-Clause
 
 #include <ginkgo/core/distributed/localized_partition.hpp>
 
 
+// Copyright (c) 2017-2023, the Ginkgo authors
 #include "core/components/max_array_kernels.hpp"
 
 
@@ -228,10 +201,13 @@ array<LocalIndexType> communicate_send_gather_idxs(
     const array<comm_index_type>& send_sizes)
 {
     MPI_Comm sparse_comm;
+    MPI_Info info;
+    MPI_Info_create(&info);
     MPI_Dist_graph_create_adjacent(
         comm.get(), send_ids.get_num_elems(), send_ids.get_const_data(),
         MPI_UNWEIGHTED, recv_ids.get_num_elems(), recv_ids.get_const_data(),
-        MPI_UNWEIGHTED, MPI_Info{}, false, &sparse_comm);
+        MPI_UNWEIGHTED, info, false, &sparse_comm);
+    MPI_Info_free(&info);
 
     std::vector<comm_index_type> recv_offsets(recv_sizes.get_num_elems() + 1);
     std::vector<comm_index_type> send_offsets(send_sizes.get_num_elems() + 1);
