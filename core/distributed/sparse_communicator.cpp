54128e70f13f7090b3e9ca1d1f0b7709bfdedb9a
Marcel Koch <marcel.koch@kit.edu>
2023-12-22 18:03:09 +0100

    use dummy info object
    Cray MPI seems to not work with MPI_INFO_NULL.

--- core/distributed/sparse_communicator.cpp
+++ core/distributed/sparse_communicator.cpp
@@ -1,37 +1,11 @@
-/*******************************<GINKGO LICENSE>******************************
-Copyright (c) 2017-2023, the Ginkgo authors
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions
-are met:
-
-1. Redistributions of source code must retain the above copyright
-notice, this list of conditions and the following disclaimer.
-
-2. Redistributions in binary form must reproduce the above copyright
-notice, this list of conditions and the following disclaimer in the
-documentation and/or other materials provided with the distribution.
-
-3. Neither the name of the copyright holder nor the names of its
-contributors may be used to endorse or promote products derived from
-this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
-IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
-TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
-PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-******************************<GINKGO LICENSE>*******************************/
+// SPDX-FileCopyrightText: 2017-2023 The Ginkgo authors
+//
+// SPDX-License-Identifier: BSD-3-Clause
 
 #include <ginkgo/core/distributed/sparse_communicator.hpp>
 
+
+// Copyright (c) 2017-2023, the Ginkgo authors
 namespace gko {
 namespace experimental {
 namespace distributed {
@@ -75,10 +49,13 @@ mpi::communicator create_neighborhood_comm(
     // adjacent constructor guarantees that querying sources/destinations
     // will result in the array having the same order as defined here
     MPI_Comm new_comm;
+    MPI_Info info;
+    MPI_Info_create(&info);
     MPI_Dist_graph_create_adjacent(
         base.get(), in_degree, sources_host->get_const_data(), MPI_UNWEIGHTED,
-        out_degree, destinations_host->get_const_data(), MPI_UNWEIGHTED,
-        MPI_INFO_NULL, false, &new_comm);
+        out_degree, destinations_host->get_const_data(), MPI_UNWEIGHTED, info,
+        false, &new_comm);
+    MPI_Info_free(&info);
     mpi::communicator neighbor_comm{new_comm};  // need to make this owning
 
     return neighbor_comm;
