7c566bb69c233261cc461cd51edd440710871bc9
Pratik Nayak <pratikvn@protonmail.com>
2024-01-09 10:18:06 +0100

    Fix reduce_over_group for new Intel SYCL versions

--- dpcpp/components/cooperative_groups.dp.hpp
+++ dpcpp/components/cooperative_groups.dp.hpp
@@ -212,7 +212,8 @@ public:
     {
         // todo: change it when OneAPI update the mask related api
         return sycl::reduce_over_group(
-            *this, (predicate != 0) ? mask_type(1) << data_.rank : mask_type(0),
+            static_cast<sycl::sub_group>(*this),
+            (predicate != 0) ? mask_type(1) << data_.rank : mask_type(0),
             sycl::plus<mask_type>());
     }
 
