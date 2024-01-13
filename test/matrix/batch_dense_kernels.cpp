ec9d170d3bd6151a7a679dc6cf5a119c70cdf418
Pratik Nayak <pratikvn@protonmail.com>
2024-01-13 17:58:56 +0100

    Add kernel tests

--- test/matrix/batch_dense_kernels.cpp
+++ test/matrix/batch_dense_kernels.cpp
@@ -20,6 +20,7 @@
 
 #include "core/base/batch_utilities.hpp"
 #include "core/test/utils.hpp"
+#include "core/test/utils/array_generator.hpp"
 #include "core/test/utils/assertions.hpp"
 #include "core/test/utils/batch_helpers.hpp"
 #include "test/utils/executor.hpp"
@@ -43,17 +44,28 @@ protected:
             std::normal_distribution<>(-1.0, 1.0), rand_engine, ref);
     }
 
-    void set_up_apply_data(gko::size_type num_rows, gko::size_type num_vecs = 1)
+    void set_up_apply_data(gko::size_type num_rows,
+                           gko::size_type num_cols = 32,
+                           gko::size_type num_vecs = 1)
     {
-        const gko::size_type num_cols = 32;
         mat = gen_mtx<BMtx>(batch_size, num_rows, num_cols);
+        mat2 = gen_mtx<BMtx>(batch_size, num_rows, num_cols);
         y = gen_mtx<BMVec>(batch_size, num_cols, num_vecs);
         alpha = gen_mtx<BMVec>(batch_size, 1, 1);
         beta = gen_mtx<BMVec>(batch_size, 1, 1);
         dmat = gko::clone(exec, mat);
+        dmat2 = gko::clone(exec, mat2);
         dy = gko::clone(exec, y);
         dalpha = gko::clone(exec, alpha);
         dbeta = gko::clone(exec, beta);
+        row_scale = gko::test::generate_random_array<value_type>(
+            num_rows * batch_size, std::normal_distribution<>(2.0, 0.5),
+            rand_engine, ref);
+        col_scale = gko::test::generate_random_array<value_type>(
+            num_cols * batch_size, std::normal_distribution<>(4.0, 0.5),
+            rand_engine, ref);
+        drow_scale = gko::array<value_type>(exec, row_scale);
+        dcol_scale = gko::array<value_type>(exec, col_scale);
         expected = BMVec::create(
             ref,
             gko::batch_dim<2>(batch_size, gko::dim<2>{num_rows, num_vecs}));
@@ -64,16 +76,22 @@ protected:
     std::default_random_engine rand_engine;
 
     const gko::size_type batch_size = 11;
-    std::unique_ptr<BMtx> mat;
+    std::shared_ptr<BMtx> mat;
+    std::shared_ptr<BMtx> mat2;
     std::unique_ptr<BMVec> y;
     std::unique_ptr<BMVec> alpha;
     std::unique_ptr<BMVec> beta;
     std::unique_ptr<BMVec> expected;
     std::unique_ptr<BMVec> dresult;
-    std::unique_ptr<BMtx> dmat;
+    std::shared_ptr<BMtx> dmat;
+    std::shared_ptr<BMtx> dmat2;
     std::unique_ptr<BMVec> dy;
     std::unique_ptr<BMVec> dalpha;
     std::unique_ptr<BMVec> dbeta;
+    gko::array<value_type> row_scale;
+    gko::array<value_type> col_scale;
+    gko::array<value_type> drow_scale;
+    gko::array<value_type> dcol_scale;
 };
 
 
@@ -107,4 +125,15 @@ TEST_F(Dense, SingleVectorAdvancedApplyIsEquivalentToRef)
     dmat->apply(dalpha.get(), dy.get(), dbeta.get(), dresult.get());
 
     GKO_ASSERT_BATCH_MTX_NEAR(dresult, expected, r<value_type>::value);
+}
+
+
+TEST_F(Dense, TwoSidedScaleIsEquivalentToRef)
+{
+    set_up_apply_data(257);
+
+    gko::batch::matrix::two_sided_scale(col_scale, row_scale, mat.get());
+    gko::batch::matrix::two_sided_scale(dcol_scale, drow_scale, dmat.get());
+
+    GKO_ASSERT_BATCH_MTX_NEAR(dmat, mat, r<value_type>::value);
 }
