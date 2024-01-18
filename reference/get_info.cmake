9d1a7b7f3a1a37340d68c7088c2267d869dd86d5
Tobias Ribizel <mail@ribizel.de>
2024-01-18 21:59:14 +0100

    use native CMake variables for compiler flags

--- reference/get_info.cmake
+++ reference/get_info.cmake
@@ -1,4 +1,3 @@
-ginkgo_print_module_header(${detailed_log} "Reference")
-ginkgo_print_variable(${detailed_log} "GINKGO_BUILD_REFERENCE")
-ginkgo_print_variable(${detailed_log} "GINKGO_COMPILER_FLAGS")
-ginkgo_print_module_footer(${detailed_log} "")
+#ginkgo_print_module_header(${detailed_log} "Reference")
+#ginkgo_print_variable(${detailed_log} "GINKGO_BUILD_REFERENCE")
+#ginkgo_print_module_footer(${detailed_log} "")
