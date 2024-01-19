b75ed2d20097def5ec75e4571cc5e47d0b886725
Marcel Koch <marcel.koch@kit.edu>
2024-01-19 13:08:06 +0000

    remove unused cmake function

--- cmake/build_helpers.cmake
+++ cmake/build_helpers.cmake
@@ -115,26 +115,6 @@ macro(ginkgo_modify_flags name)
     string(REPLACE "\"" "\\\"" ${name}_MODIFY "${${name}}")
 endmacro()
 
-# Extract the clang version from a clang executable path
-function(ginkgo_extract_clang_version CLANG_COMPILER GINKGO_CLANG_VERSION)
-    set(CLANG_VERSION_PROG "#include <cstdio>\n"
-        "int main() {printf(\"%d.%d.%d\", __clang_major__, __clang_minor__, __clang_patchlevel__)\;"
-        "return 0\;}")
-    file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/extract_clang_ver.cpp" ${CLANG_VERSION_PROG})
-    execute_process(COMMAND ${CLANG_COMPILER} ${CMAKE_CURRENT_BINARY_DIR}/extract_clang_ver.cpp
-        -o ${CMAKE_CURRENT_BINARY_DIR}/extract_clang_ver
-        ERROR_VARIABLE CLANG_EXTRACT_VER_ERROR)
-    execute_process(COMMAND ${CMAKE_CURRENT_BINARY_DIR}/extract_clang_ver
-        OUTPUT_VARIABLE FOUND_CLANG_VERSION
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-        ERROR_STRIP_TRAILING_WHITESPACE
-        )
-
-    set (${GINKGO_CLANG_VERSION} "${FOUND_CLANG_VERSION}" PARENT_SCOPE)
-    file(REMOVE ${CMAKE_CURRENT_BINARY_DIR}/extract_clang_ver.cpp)
-    file(REMOVE ${CMAKE_CURRENT_BINARY_DIR}/extract_clang_ver)
-endfunction()
-
 # Extract the DPC++ version
 function(ginkgo_extract_dpcpp_version DPCPP_COMPILER GINKGO_DPCPP_VERSION MACRO_VAR)
     set(DPCPP_VERSION_PROG "#include <CL/sycl.hpp>\n#include <iostream>\n"
