ebd3c5d41cb869549a5aa675e890bff65ff083a3
Pratik Nayak <pratikvn@protonmail.com>
2024-02-13 16:45:02 +0100

    Add omp kernels and unified tests
    Co-authored-by: Isha Aggarwal<isha.aggarwal2@kit.edu>
    Co-authored-by: Aditya Kashi<kashia@ornl.gov>

--- omp/preconditioner/batch_jacobi_kernels.cpp
+++ omp/preconditioner/batch_jacobi_kernels.cpp
@@ -5,96 +5,101 @@
 #include "core/preconditioner/batch_jacobi_kernels.hpp"
 
 
-#include <ginkgo/core/matrix/batch_csr.hpp>
-
-
+#include "core/base/batch_struct.hpp"
 #include "core/components/prefix_sum_kernels.hpp"
 #include "core/matrix/batch_struct.hpp"
+#include "core/solver/batch_dispatch.hpp"
+#include "reference/base/batch_struct.hpp"
 #include "reference/matrix/batch_struct.hpp"
 #include "reference/preconditioner/batch_block_jacobi.hpp"
 #include "reference/preconditioner/batch_scalar_jacobi.hpp"
 
+
 namespace gko {
 namespace kernels {
 namespace omp {
-
 namespace batch_jacobi {
+
 
 namespace {
 
 
 template <typename BatchMatrixType, typename PrecType, typename ValueType>
-void apply_jacobi(const BatchMatrixType& sys_mat_batch, PrecType prec,
-                  const gko::batch_dense::UniformBatch<const ValueType>& rub,
-                  const gko::batch_dense::UniformBatch<ValueType>& zub)
+void apply_jacobi(
+    const BatchMatrixType& sys_mat_batch, PrecType& prec,
+    const gko::batch::multi_vector::uniform_batch<const ValueType>& rub,
+    const gko::batch::multi_vector::uniform_batch<ValueType>& zub)
 {
 #pragma omp parallel for firstprivate(prec)
-    for (size_type batch_id = 0; batch_id < sys_mat_batch.num_batch;
+    for (size_type batch_id = 0; batch_id < sys_mat_batch.num_batch_items;
          batch_id++) {
         const auto sys_mat_entry =
-            gko::batch::batch_entry(sys_mat_batch, batch_id);
-        const auto r_b = gko::batch::batch_entry(rub, batch_id);
-        const auto z_b = gko::batch::batch_entry(zub, batch_id);
+            gko::batch::matrix::extract_batch_item(sys_mat_batch, batch_id);
+        const auto r_b = gko::batch::extract_batch_item(rub, batch_id);
+        const auto z_b = gko::batch::extract_batch_item(zub, batch_id);
 
         const auto work_arr_size = PrecType::dynamic_work_size(
-            sys_mat_batch.num_rows, sys_mat_batch.num_nnz);
+            sys_mat_batch.num_rows, sys_mat_batch.get_single_item_num_nnz());
         std::vector<ValueType> work(work_arr_size);
 
         prec.generate(batch_id, sys_mat_entry, work.data());
         prec.apply(r_b, z_b);
     }
 }
-// Note: Do not change the ordering
 
+
+// Note: Do not change the ordering
 #include "reference/preconditioner/batch_jacobi_kernels.hpp.inc"
+
 
 }  // unnamed namespace
 
 
 template <typename ValueType, typename IndexType>
 void batch_jacobi_apply(
-    std::shared_ptr<const gko::OmpExecutor> exec,
-    const matrix::BatchCsr<ValueType, IndexType>* const sys_mat,
+    std::shared_ptr<const DefaultExecutor> exec,
+    const batch::matrix::Csr<ValueType, IndexType>* const sys_mat,
     const size_type num_blocks, const uint32 max_block_size,
-    const gko::preconditioner::batched_jacobi_blocks_storage_scheme<IndexType>&
-        storage_scheme,
+    const gko::batch::preconditioner::batched_jacobi_blocks_storage_scheme<
+        IndexType>& storage_scheme,
     const IndexType* const cumulative_block_storage,
     const ValueType* const blocks_array, const IndexType* const block_ptrs,
     const IndexType* const row_part_of_which_block_info,
-    const matrix::BatchDense<ValueType>* const r,
-    matrix::BatchDense<ValueType>* const z)
+    const batch::MultiVector<ValueType>* const r,
+    batch::MultiVector<ValueType>* const z)
 {
-    const auto sys_mat_batch = gko::kernels::host::get_batch_struct(sys_mat);
+    const auto sys_mat_batch = host::get_batch_struct(sys_mat);
     batch_jacobi_apply_helper(sys_mat_batch, num_blocks, max_block_size,
                               storage_scheme, cumulative_block_storage,
                               blocks_array, block_ptrs,
                               row_part_of_which_block_info, r, z);
 }
 
-GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE_AND_INT32_INDEX(
+GKO_INSTANTIATE_FOR_EACH_VALUE_AND_INT32_TYPE(
     GKO_DECLARE_BATCH_JACOBI_APPLY_KERNEL);
+
 
 template <typename ValueType, typename IndexType>
 void batch_jacobi_apply(
-    std::shared_ptr<const gko::OmpExecutor> exec,
-    const matrix::BatchEll<ValueType, IndexType>* const sys_mat,
+    std::shared_ptr<const DefaultExecutor> exec,
+    const batch::matrix::Ell<ValueType, IndexType>* const sys_mat,
     const size_type num_blocks, const uint32 max_block_size,
-    const gko::preconditioner::batched_jacobi_blocks_storage_scheme<IndexType>&
-        storage_scheme,
+    const gko::batch::preconditioner::batched_jacobi_blocks_storage_scheme<
+        IndexType>& storage_scheme,
     const IndexType* const cumulative_block_storage,
     const ValueType* const blocks_array, const IndexType* const block_ptrs,
     const IndexType* const row_part_of_which_block_info,
-    const matrix::BatchDense<ValueType>* const r,
-    matrix::BatchDense<ValueType>* const z)
+    const batch::MultiVector<ValueType>* const r,
+    batch::MultiVector<ValueType>* const z)
 {
-    const auto sys_mat_batch = gko::kernels::host::get_batch_struct(sys_mat);
+    const auto sys_mat_batch = host::get_batch_struct(sys_mat);
     batch_jacobi_apply_helper(sys_mat_batch, num_blocks, max_block_size,
                               storage_scheme, cumulative_block_storage,
                               blocks_array, block_ptrs,
                               row_part_of_which_block_info, r, z);
 }
 
-GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE_AND_INT32_INDEX(
+GKO_INSTANTIATE_FOR_EACH_VALUE_AND_INT32_TYPE(
     GKO_DECLARE_BATCH_JACOBI_ELL_APPLY_KERNEL);
 
 
@@ -143,10 +148,10 @@ template void find_row_is_part_of_which_block<int>(
 template <typename ValueType, typename IndexType>
 void extract_common_blocks_pattern(
     std::shared_ptr<const DefaultExecutor> exec,
-    const matrix::Csr<ValueType, IndexType>* const first_sys_csr,
+    const gko::matrix::Csr<ValueType, IndexType>* const first_sys_csr,
     const size_type num_blocks,
-    const preconditioner::batched_jacobi_blocks_storage_scheme<IndexType>&
-        storage_scheme,
+    const batch::preconditioner::batched_jacobi_blocks_storage_scheme<
+        IndexType>& storage_scheme,
     const IndexType* const cumulative_block_storage,
     const IndexType* const block_pointers, const IndexType* const,
     IndexType* const blocks_pattern)
@@ -159,22 +164,22 @@ void extract_common_blocks_pattern(
     }
 }
 
-GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE_AND_INT32_INDEX(
+GKO_INSTANTIATE_FOR_EACH_VALUE_AND_INT32_TYPE(
     GKO_DECLARE_BATCH_BLOCK_JACOBI_EXTRACT_PATTERN_KERNEL);
 
 
 template <typename ValueType, typename IndexType>
 void compute_block_jacobi(
     std::shared_ptr<const DefaultExecutor> exec,
-    const matrix::BatchCsr<ValueType, IndexType>* const sys_csr, const uint32,
+    const batch::matrix::Csr<ValueType, IndexType>* const sys_csr, const uint32,
     const size_type num_blocks,
-    const preconditioner::batched_jacobi_blocks_storage_scheme<IndexType>&
-        storage_scheme,
+    const batch::preconditioner::batched_jacobi_blocks_storage_scheme<
+        IndexType>& storage_scheme,
     const IndexType* const cumulative_block_storage,
     const IndexType* const block_pointers,
     const IndexType* const blocks_pattern, ValueType* const blocks)
 {
-    const auto nbatch = sys_csr->get_num_batch_entries();
+    const auto nbatch = sys_csr->get_num_batch_items();
     const auto A_batch = host::get_batch_struct(sys_csr);
 
 #pragma omp parallel for
@@ -182,41 +187,16 @@ void compute_block_jacobi(
         size_type block_idx = i % num_blocks;
         size_type batch_idx = i / num_blocks;
 
-        const auto A_entry = gko::batch::batch_entry(A_batch, batch_idx);
+        const auto A_entry =
+            gko::batch::matrix::extract_batch_item(A_batch, batch_idx);
         compute_block_jacobi_impl(batch_idx, block_idx, A_entry, num_blocks,
                                   storage_scheme, cumulative_block_storage,
                                   block_pointers, blocks_pattern, blocks);
     }
 }
 
-GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE_AND_INT32_INDEX(
+GKO_INSTANTIATE_FOR_EACH_VALUE_AND_INT32_TYPE(
     GKO_DECLARE_BATCH_BLOCK_JACOBI_COMPUTE_KERNEL);
-
-
-template <typename ValueType, typename IndexType>
-void transpose_block_jacobi(
-    std::shared_ptr<const DefaultExecutor> exec, const size_type nbatch,
-    const size_type, const size_type num_blocks, const uint32,
-    const IndexType* const block_pointers, const ValueType* const blocks_array,
-    const gko::preconditioner::batched_jacobi_blocks_storage_scheme<IndexType>&
-        storage_scheme,
-    const IndexType* const cumulative_block_storage, const IndexType* const,
-    ValueType* const out_blocks_array, const bool to_conjugate)
-{
-#pragma omp parallel for
-    for (size_type i = 0; i < nbatch * num_blocks; i++) {
-        size_type block_idx = i % num_blocks;
-        size_type batch_idx = i / num_blocks;
-
-        tranpose_dense_block_impl(batch_idx, block_idx, num_blocks,
-                                  block_pointers, blocks_array, storage_scheme,
-                                  cumulative_block_storage, out_blocks_array,
-                                  to_conjugate);
-    }
-}
-
-GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE_AND_INT32_INDEX(
-    GKO_DECLARE_BATCH_BLOCK_JACOBI_TRANSPOSE_KERNEL);
 
 
 }  // namespace batch_jacobi
