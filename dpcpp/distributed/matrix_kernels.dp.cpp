a562ab19a7970de4fa3d912623e1661faaddab04
Marcel Koch <marcel.koch@kit.edu>
2024-02-14 16:38:50 +0000

    [dist-mat] introduce kernel to separate local and non-local matrix data

--- dpcpp/distributed/matrix_kernels.dp.cpp
+++ dpcpp/distributed/matrix_kernels.dp.cpp
@@ -15,7 +15,7 @@ namespace distributed_matrix {
 
 
 template <typename ValueType, typename LocalIndexType, typename GlobalIndexType>
-void build_local_nonlocal(
+void separate_local_nonlocal(
     std::shared_ptr<const DefaultExecutor> exec,
     const device_matrix_data<ValueType, GlobalIndexType>& input,
     const experimental::distributed::Partition<LocalIndexType, GlobalIndexType>*
@@ -25,14 +25,11 @@ void build_local_nonlocal(
     comm_index_type local_part, array<LocalIndexType>& local_row_idxs,
     array<LocalIndexType>& local_col_idxs, array<ValueType>& local_values,
     array<LocalIndexType>& non_local_row_idxs,
-    array<LocalIndexType>& non_local_col_idxs,
-    array<ValueType>& non_local_values,
-    array<LocalIndexType>& local_gather_idxs,
-    array<comm_index_type>& recv_sizes,
-    array<GlobalIndexType>& non_local_to_global) GKO_NOT_IMPLEMENTED;
+    array<GlobalIndexType>& non_local_col_idxs,
+    array<ValueType>& non_local_values) GKO_NOT_IMPLEMENTED;
 
 GKO_INSTANTIATE_FOR_EACH_VALUE_AND_LOCAL_GLOBAL_INDEX_TYPE(
-    GKO_DECLARE_BUILD_LOCAL_NONLOCAL);
+    GKO_DECLARE_SEPARATE_LOCAL_NONLOCAL);
 
 
 }  // namespace distributed_matrix
