defc8c663abdd63d42b294fbe45143dd72ec569c
Marcel Koch <marcel.koch@kit.edu>
2024-04-03 10:52:49 +0200

    [row-gatherer] adds any dense cache

--- core/base/dense_cache.cpp
+++ core/base/dense_cache.cpp
@@ -37,5 +37,62 @@ void DenseCache<ValueType>::init_from(
 GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE(GKO_DECLARE_DENSE_CACHE);
 
 
+template <typename ValueType>
+void AnyDenseCache::init_from(
+    const matrix::Dense<ValueType>* template_vec) const
+{
+    if (!vec || vec->get_size() != template_vec->get_size() ||
+        vec->get_executor() != template_vec->get_executor() ||
+        !this->get<ValueType>()) {
+        vec = matrix::Dense<ValueType>::create_with_config_of(template_vec);
+    }
+}
+
+#define GKO_DECLARE_ANYDENSECACHE_INIT_FROM(_vtype)                          \
+    void AnyDenseCache::init_from(const matrix::Dense<_vtype>* template_vec) \
+        const
+
+GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE(GKO_DECLARE_ANYDENSECACHE_INIT_FROM);
+
+#undef GKO_DECLARE_ANYDENSECACHE_INIT_FROM
+
+
+template <typename ValueType>
+void AnyDenseCache::init(std::shared_ptr<const Executor> exec,
+                         dim<2> size) const
+{
+    if (!vec || vec->get_size() != size || vec->get_executor() != exec ||
+        !this->get<ValueType>()) {
+        vec = matrix::Dense<ValueType>::create(exec, size);
+    }
+}
+
+#define GKO_DECLARE_ANYDENSECACHE_INIT(_vtype)                             \
+    void AnyDenseCache::init<_vtype>(std::shared_ptr<const Executor> exec, \
+                                     dim<2> size) const
+
+GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE(GKO_DECLARE_ANYDENSECACHE_INIT);
+
+#undef GKO_DECLARE_ANYDENSECACHE_INIT
+
+
+template <typename ValueType>
+matrix::Dense<ValueType>* AnyDenseCache::get() const
+{
+    if (auto p = dynamic_cast<matrix::Dense<ValueType>*>(vec.get())) {
+        return p;
+    }
+
+    return nullptr;
+}
+
+#define GKO_DECLARE_ANYDENSECACHE_GET(_vtype) \
+    matrix::Dense<_vtype>* AnyDenseCache::get() const
+
+GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE(GKO_DECLARE_ANYDENSECACHE_GET);
+
+#undef GKO_DECLARE_ANYDENSECACHE_GET
+
+
 }  // namespace detail
 }  // namespace gko
--- include/ginkgo/core/base/dense_cache.hpp
+++ include/ginkgo/core/base/dense_cache.hpp
@@ -13,6 +13,11 @@
 
 
 namespace gko {
+
+
+class LinOp;
+
+
 namespace matrix {
 
 
@@ -90,6 +95,60 @@ struct DenseCache {
     matrix::Dense<ValueType>* get() const { return vec.get(); }
 };
 
+/**
+ * @copydoc DenseCache
+ *
+ * @note Compared to DenseCache, this can store vectors of different value type,
+ *       similar to std::any. Access to the stored vector is only possible, if
+ *       the stored value type is also known. Otherwise, the access will result
+ *       in a nullptr.
+ */
+struct AnyDenseCache {
+    AnyDenseCache() = default;
+    ~AnyDenseCache() = default;
+    AnyDenseCache(const AnyDenseCache&) {}
+    AnyDenseCache(AnyDenseCache&&) noexcept {}
+    AnyDenseCache& operator=(const AnyDenseCache&) { return *this; }
+    AnyDenseCache& operator=(AnyDenseCache&&) noexcept { return *this; }
+    mutable std::unique_ptr<LinOp> vec{};
+
+
+    /**
+     * Initializes the buffered vector with the same configuration as the
+     * template vector, if
+     * - the current vector is null,
+     * - the sizes of the buffered and template vector differ,
+     * - the executor of the buffered and template vector differ.
+     *
+     * @note This does not copy any data from the template vector.
+     *
+     * @param template_vec  Defines the configuration (executor, size, stride)
+     *                      of the buffered vector.
+     */
+    template <typename ValueType>
+    void init_from(const matrix::Dense<ValueType>* template_vec) const;
+
+    /**
+     * Initializes the buffered vector, if
+     * - the current vector is null,
+     * - the sizes differ,
+     * - the executor differs.
+     *
+     * @param exec  Executor of the buffered vector.
+     * @param size  Size of the buffered vector.
+     */
+    template <typename ValueType>
+    void init(std::shared_ptr<const Executor> exec, dim<2> size) const;
+
+    /**
+     * Pointer access to the underlying vector.
+     * @tparam ValueType  the desired value type of the vector
+     * @return  Pointer to the stored vector. Returns nullptr if the stored
+     *          vector has a different value type than ValueType
+     */
+    template <typename ValueType>
+    matrix::Dense<ValueType>* get() const;
+};
 
 }  // namespace detail
 }  // namespace gko
