8dc1297616a6c575625815f1bb896f52d0aa8c25
Marcel Koch <marcel.koch@kit.edu>
2024-04-04 10:05:03 +0000

    [coll-comm] adds interface for collective communicator
    Co-authored-by: Pratik Nayak <pratik.nayak@kit.edu>
    Signed-off-by: Marcel Koch <marcel.koch@kit.edu>

---
+++ core/distributed/collective_communicator.cpp
@@ -0,0 +1,26 @@
+// SPDX-FileCopyrightText: 2024 - 2025 The Ginkgo authors
+//
+// SPDX-License-Identifier: BSD-3-Clause
+
+#include "ginkgo/core/distributed/collective_communicator.hpp"
+
+
+namespace gko {
+namespace experimental {
+namespace mpi {
+
+
+CollectiveCommunicator::CollectiveCommunicator(communicator base)
+    : base_(std::move(base))
+{}
+
+
+const communicator& CollectiveCommunicator::get_base_communicator() const
+{
+    return base_;
+}
+
+
+}  // namespace mpi
+}  // namespace experimental
+}  // namespace gko
