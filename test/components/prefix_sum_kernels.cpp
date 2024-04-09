7cc9054916dfda015bdd28efa11f4d97c1357dc6
Thomas Grützmacher <thomas.gruetzmacher@kit.edu>
2024-04-09 15:07:37 +0200

    Use new type aliases in relevant tests
    Additionally, introduce `IntegerTypes`, which is IndexTypes combined
    with gko::size_type (used for the prefix sum tests).

--- test/components/prefix_sum_kernels.cpp
+++ test/components/prefix_sum_kernels.cpp
@@ -39,10 +39,7 @@ protected:
     gko::array<index_type> dvals;
 };
 
-using PrefixSumIndexTypes =
-    ::testing::Types<gko::int32, gko::int64, gko::size_type>;
-
-TYPED_TEST_SUITE(PrefixSum, PrefixSumIndexTypes, TypenameNameGenerator);
+TYPED_TEST_SUITE(PrefixSum, gko::test::IntegerTypes, TypenameNameGenerator);
 
 
 TYPED_TEST(PrefixSum, EqualsReference)
