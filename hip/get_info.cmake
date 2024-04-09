f2a246c38f66531b89d54798aa07139d995a5be6
Tobias Ribizel <mail@ribizel.de>
2024-04-09 15:13:48 +0200

    review updates
    - fix incorrect CMake version requirement
    - remove explicit RPATH additions
    - clean up configure log
    
    Co-authored-by: Yuhsiang M. Tsai <yhmtsai@gmail.com>
    Co-authored-by: Marcel Koch <marcel.koch@kit.edu>

--- hip/get_info.cmake
+++ hip/get_info.cmake
@@ -1,12 +1,10 @@
 ginkgo_print_module_header(${detailed_log} "HIP")
-ginkgo_print_foreach_variable(${detailed_log}
-    "GINKGO_HIPCONFIG_PATH")
 ginkgo_print_module_footer(${detailed_log} "HIP variables:")
+ginkgo_print_flags(${detailed_log} "CMAKE_HIP_FLAGS")
+ginkgo_print_flags(${detailed_log} "CMAKE_HIP_COMPILER")
 ginkgo_print_foreach_variable(${detailed_log}
     "HIP_VERSION;HIP_PATH;ROCM_PATH"
     "HIP_PLATFORM;HIP_ROOT_DIR;HIP_RUNTIME;HIPBLAS_PATH;HIPSPARSE_PATH"
     "HIPRAND_PATH;ROCRAND_PATH;HIP_CLANG_INCLUDE_PATH"
     "HIP_HIPCONFIG_EXECUTABLE")
-ginkgo_print_flags(${detailed_log} "CMAKE_HIP_FLAGS")
-ginkgo_print_flags(${detailed_log} "CMAKE_HIP_COMPILER")
 ginkgo_print_module_footer(${detailed_log} "")
