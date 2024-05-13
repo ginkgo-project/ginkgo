6e3b31edc765be0c3a33362b61f2d583e9b90c02
Yu-Hsiang M. Tsai <yhmtsai@gmail.com>
2024-05-13 17:00:35 +0200

    reuse in macro

--- core/config/factorization_config.cpp
+++ core/config/factorization_config.cpp
@@ -17,111 +17,22 @@
 
 #include "core/config/config_helper.hpp"
 #include "core/config/dispatch.hpp"
+#include "core/config/parse_macro.hpp"
 
 
 namespace gko {
 namespace config {
 
 
-template <>
-deferred_factory_parameter<gko::LinOpFactory>
-parse<LinOpFactoryType::Factorization_Ic>(const pnode& config,
-                                          const registry& context,
-                                          const type_descriptor& td)
-{
-    auto updated = update_type(config, td);
-    return dispatch<gko::LinOpFactory, gko::factorization::Ic>(
-        config, context, updated,
-        make_type_selector(updated.get_value_typestr(), value_type_list()),
-        make_type_selector(updated.get_index_typestr(), index_type_list()));
-}
-
-
-template <>
-deferred_factory_parameter<gko::LinOpFactory>
-parse<LinOpFactoryType::Factorization_Ilu>(const pnode& config,
-                                           const registry& context,
-                                           const type_descriptor& td)
-{
-    auto updated = update_type(config, td);
-    return dispatch<gko::LinOpFactory, gko::factorization::Ilu>(
-        config, context, updated,
-        make_type_selector(updated.get_value_typestr(), value_type_list()),
-        make_type_selector(updated.get_index_typestr(), index_type_list()));
-}
-
-
-template <>
-deferred_factory_parameter<gko::LinOpFactory> parse<LinOpFactoryType::Cholesky>(
-    const pnode& config, const registry& context, const type_descriptor& td)
-{
-    auto updated = update_type(config, td);
-    return dispatch<gko::LinOpFactory,
-                    gko::experimental::factorization::Cholesky>(
-        config, context, updated,
-        make_type_selector(updated.get_value_typestr(), value_type_list()),
-        make_type_selector(updated.get_index_typestr(), index_type_list()));
-}
-
-
-template <>
-deferred_factory_parameter<gko::LinOpFactory> parse<LinOpFactoryType::Lu>(
-    const pnode& config, const registry& context, const type_descriptor& td)
-{
-    auto updated = update_type(config, td);
-    return dispatch<gko::LinOpFactory, gko::experimental::factorization::Lu>(
-        config, context, updated,
-        make_type_selector(updated.get_value_typestr(), value_type_list()),
-        make_type_selector(updated.get_index_typestr(), index_type_list()));
-}
-
-
-template <>
-deferred_factory_parameter<gko::LinOpFactory> parse<LinOpFactoryType::ParIlu>(
-    const pnode& config, const registry& context, const type_descriptor& td)
-{
-    auto updated = update_type(config, td);
-    return dispatch<gko::LinOpFactory, gko::factorization::ParIlu>(
-        config, context, updated,
-        make_type_selector(updated.get_value_typestr(), value_type_list()),
-        make_type_selector(updated.get_index_typestr(), index_type_list()));
-}
-
-
-template <>
-deferred_factory_parameter<gko::LinOpFactory> parse<LinOpFactoryType::ParIlut>(
-    const pnode& config, const registry& context, const type_descriptor& td)
-{
-    auto updated = update_type(config, td);
-    return dispatch<gko::LinOpFactory, gko::factorization::ParIlut>(
-        config, context, updated,
-        make_type_selector(updated.get_value_typestr(), value_type_list()),
-        make_type_selector(updated.get_index_typestr(), index_type_list()));
-}
-
-
-template <>
-deferred_factory_parameter<gko::LinOpFactory> parse<LinOpFactoryType::ParIc>(
-    const pnode& config, const registry& context, const type_descriptor& td)
-{
-    auto updated = update_type(config, td);
-    return dispatch<gko::LinOpFactory, gko::factorization::ParIc>(
-        config, context, updated,
-        make_type_selector(updated.get_value_typestr(), value_type_list()),
-        make_type_selector(updated.get_index_typestr(), index_type_list()));
-}
-
-
-template <>
-deferred_factory_parameter<gko::LinOpFactory> parse<LinOpFactoryType::ParIct>(
-    const pnode& config, const registry& context, const type_descriptor& td)
-{
-    auto updated = update_type(config, td);
-    return dispatch<gko::LinOpFactory, gko::factorization::ParIct>(
-        config, context, updated,
-        make_type_selector(updated.get_value_typestr(), value_type_list()),
-        make_type_selector(updated.get_index_typestr(), index_type_list()));
-}
+GKO_PARSE_VALUE_AND_INDEX_TYPE(Factorization_Ic, gko::factorization::Ic);
+GKO_PARSE_VALUE_AND_INDEX_TYPE(Factorization_Ilu, gko::factorization::Ilu);
+GKO_PARSE_VALUE_AND_INDEX_TYPE(Cholesky,
+                               gko::experimental::factorization::Cholesky);
+GKO_PARSE_VALUE_AND_INDEX_TYPE(Lu, gko::experimental::factorization::Lu);
+GKO_PARSE_VALUE_AND_INDEX_TYPE(ParIlu, gko::factorization::ParIlu);
+GKO_PARSE_VALUE_AND_INDEX_TYPE(ParIlut, gko::factorization::ParIlut);
+GKO_PARSE_VALUE_AND_INDEX_TYPE(ParIc, gko::factorization::ParIc);
+GKO_PARSE_VALUE_AND_INDEX_TYPE(ParIct, gko::factorization::ParIct);
 
 
 }  // namespace config
