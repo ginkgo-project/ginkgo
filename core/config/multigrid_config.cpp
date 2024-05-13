e0f79bac0422f1d9c8c4bc924d92d6c9357e7282
Yu-Hsiang M. Tsai <yhmtsai@gmail.com>
2024-05-13 16:41:42 +0200

    use macro and move to proper place

--- core/config/multigrid_config.cpp
+++ core/config/multigrid_config.cpp
@@ -7,11 +7,11 @@
 #include <ginkgo/core/config/registry.hpp>
 #include <ginkgo/core/multigrid/fixed_coarsening.hpp>
 #include <ginkgo/core/multigrid/pgm.hpp>
-#include <ginkgo/core/solver/multigrid.hpp>
 
 
 #include "core/config/config_helper.hpp"
 #include "core/config/dispatch.hpp"
+#include "core/config/parse_macro.hpp"
 #include "core/config/type_descriptor_helper.hpp"
 
 
@@ -19,39 +19,7 @@ namespace gko {
 namespace config {
 
 
-template <>
-deferred_factory_parameter<gko::LinOpFactory> parse<LinOpFactoryType::Pgm>(
-    const pnode& config, const registry& context,
-    const gko::config::type_descriptor& td)
-{
-    auto updated = update_type(config, td);
-    return dispatch<gko::LinOpFactory, multigrid::Pgm>(
-        config, context, updated,
-        make_type_selector(updated.get_value_typestr(), value_type_list()),
-        make_type_selector(updated.get_index_typestr(), index_type_list()));
-}
-
-template <>
-deferred_factory_parameter<gko::LinOpFactory>
-parse<LinOpFactoryType::FixedCoarsening>(const pnode& config,
-                                         const registry& context,
-                                         const gko::config::type_descriptor& td)
-{
-    auto updated = update_type(config, td);
-    return dispatch<gko::LinOpFactory, multigrid::FixedCoarsening>(
-        config, context, updated,
-        make_type_selector(updated.get_value_typestr(), value_type_list()),
-        make_type_selector(updated.get_index_typestr(), index_type_list()));
-}
-
-template <>
-deferred_factory_parameter<gko::LinOpFactory>
-parse<LinOpFactoryType::Multigrid>(const pnode& config, const registry& context,
-                                   const gko::config::type_descriptor& td)
-{
-    auto updated = update_type(config, td);
-    return solver::Multigrid::parse(config, context, updated);
-}
+GKO_PARSE_VALUE_AND_INDEX_TYPE(Pgm, gko::multigrid::Pgm);
 
 
 }  // namespace config
