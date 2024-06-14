91ccf49ebf0c03ec8047cee109eff60e4ad53447
Marcel Koch <marcel.koch@kit.edu>
2024-06-14 14:48:37 +0200

    [test] fix whitespace bug in profiler_hook test

--- core/test/log/profiler_hook.cpp
+++ core/test/log/profiler_hook.cpp
@@ -387,8 +387,9 @@ TEST(ProfilerHookTableSummaryWriter, SummaryWorks)
     entries.push_back({"medium", 1ms, 500us, 4});  // check division by count
     entries.push_back({"long", 120s, 60s, 1});
     entries.push_back({"eternal", 24h, 24h, 1});
+    // clang-format off
     const auto expected = R"(Test header
-Overhead estimate 1.0 s 
+Overhead estimate 1.0 ns
 |   name   | total  | total (self) | count |   avg    | avg (self) |
 |----------|-------:|-------------:|------:|---------:|-----------:|
 | eternal  | 1.0 d  |       1.0 d  |     1 |   1.0 d  |     1.0 d  |
@@ -398,8 +399,9 @@ Overhead estimate 1.0 s
 | short    | 1.0 ns |       0.0 ns |     1 |   1.0 ns |     0.0 ns |
 | empty    | 0.0 ns |       0.0 ns |     0 |   0.0 ns |     0.0 ns |
 )";
+    // clang-format on
 
-    writer.write(entries, 1s);
+    writer.write(entries, 1ns);
 
     ASSERT_EQ(ss.str(), expected);
 }
@@ -422,6 +424,7 @@ TEST(ProfilerHookTableSummaryWriter, NestedSummaryWorks)
              2,
              {ProfilerHook::nested_summary_entry{"child", 100ns, 2, {}}}},
          ProfilerHook::nested_summary_entry{"baz", 1ns, 2, {}}}};
+    // clang-format off
     const auto expected = R"(Test header
 Overhead estimate 1.0 ns
 |    name    |  total   | fraction | count |   avg    |
@@ -434,6 +437,7 @@ Overhead estimate 1.0 ns
 |   foo      | 100.0 ns |    5.0 % |     5 |  20.0 ns |
 |   baz      |   1.0 ns |    0.1 % |     2 |   0.0 ns |
 )";
+    // clang-format on
 
     writer.write_nested(entry, 1ns);
 
