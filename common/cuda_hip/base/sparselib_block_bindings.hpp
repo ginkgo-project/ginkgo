885b21371f2e2c9c58e88491b6840136c22a1740
Tobias Ribizel <mail@ribizel.de>
2024-06-29 18:29:33 +0200

    fix includes for thrust and sparselib block

---
+++ common/cuda_hip/base/sparselib_block_bindings.hpp
@@ -0,0 +1,18 @@
+// SPDX-FileCopyrightText: 2017 - 2024 The Ginkgo authors
+//
+// SPDX-License-Identifier: BSD-3-Clause
+
+#ifndef GKO_COMMON_CUDA_HIP_BASE_SPARSELIB_BLOCK_BINDINGS_HPP_
+#define GKO_COMMON_CUDA_HIP_BASE_SPARSELIB_BLOCK_BINDINGS_HPP_
+
+
+#if defined(GKO_COMPILING_CUDA)
+#include "cuda/base/cusparse_block_bindings.hpp"
+#elif defined(GKO_COMPILING_HIP)
+#include "hip/base/hipsparse_block_bindings.hip.hpp"
+#else
+#error "Executor definition missing"
+#endif
+
+
+#endif  // GKO_COMMON_CUDA_HIP_BASE_SPARSELIB_BLOCK_BINDINGS_HPP_
