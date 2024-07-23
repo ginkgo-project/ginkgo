2500766e50e490b74f2769630f6acf0b1399b2dc
Marcel Koch <marcel.koch@kit.edu>
2024-07-23 15:34:25 +0200

    [config] temporarily reduce preconditioner variations

---
+++ core/config/jacobi_config.cpp
@@ -0,0 +1,25 @@
+// SPDX-FileCopyrightText: 2017 - 2024 The Ginkgo authors
+//
+// SPDX-License-Identifier: BSD-3-Clause
+
+#include <ginkgo/core/base/exception_helpers.hpp>
+#include <ginkgo/core/config/config.hpp>
+#include <ginkgo/core/config/registry.hpp>
+#include <ginkgo/core/preconditioner/jacobi.hpp>
+
+
+#include "core/config/config_helper.hpp"
+#include "core/config/dispatch.hpp"
+#include "core/config/parse_macro.hpp"
+#include "core/config/type_descriptor_helper.hpp"
+
+
+namespace gko {
+namespace config {
+
+
+GKO_PARSE_VALUE_AND_INDEX_TYPE(Jacobi, gko::preconditioner::Jacobi);
+
+
+}  // namespace config
+}  // namespace gko
