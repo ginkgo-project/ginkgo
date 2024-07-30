8020da04895c722cf0870ab0f2231e88ad56077e
Yu-Hsiang M. Tsai <yhmtsai@gmail.com>
2024-07-31 00:21:27 +0200

    only set the global index via type descriptor

--- core/config/type_descriptor.cpp
+++ core/config/type_descriptor.cpp
@@ -17,6 +17,7 @@ type_descriptor update_type(const pnode& config, const type_descriptor& td)
 {
     auto value_typestr = td.get_value_typestr();
     auto index_typestr = td.get_index_typestr();
+    auto global_index_typestr = td.get_global_index_typestr();
 
     if (auto& obj = config.get("value_type")) {
         value_typestr = obj.get_string();
@@ -26,37 +27,93 @@ type_descriptor update_type(const pnode& config, const type_descriptor& td)
             "Setting index_type in the config is not allowed. Please set the "
             "proper index_type through type_descriptor of parse");
     }
-    return type_descriptor{value_typestr, index_typestr};
+    if (auto& obj = config.get("global_index_type")) {
+        GKO_INVALID_STATE(
+            "Setting global_index_type in the config is not allowed. Please "
+            "set the proper global_index_type through type_descriptor of "
+            "parse");
+    }
+    return type_descriptor{value_typestr, index_typestr, global_index_typestr};
 }
 
 
-template <typename ValueType, typename IndexType>
+template <typename ValueType, typename IndexType, typename GlobalIndexType>
 type_descriptor make_type_descriptor()
 {
     return type_descriptor{type_string<ValueType>::str(),
-                           type_string<IndexType>::str()};
+                           type_string<IndexType>::str(),
+                           type_string<GlobalIndexType>::str()};
 }
 
-template type_descriptor make_type_descriptor<void, void>();
-template type_descriptor make_type_descriptor<float, void>();
-template type_descriptor make_type_descriptor<double, void>();
-template type_descriptor make_type_descriptor<std::complex<float>, void>();
-template type_descriptor make_type_descriptor<std::complex<double>, void>();
-template type_descriptor make_type_descriptor<void, int32>();
-template type_descriptor make_type_descriptor<float, int32>();
-template type_descriptor make_type_descriptor<double, int32>();
-template type_descriptor make_type_descriptor<std::complex<float>, int32>();
-template type_descriptor make_type_descriptor<std::complex<double>, int32>();
-template type_descriptor make_type_descriptor<void, int64>();
-template type_descriptor make_type_descriptor<float, int64>();
-template type_descriptor make_type_descriptor<double, int64>();
-template type_descriptor make_type_descriptor<std::complex<float>, int64>();
-template type_descriptor make_type_descriptor<std::complex<double>, int64>();
+// global_index: void
+template type_descriptor make_type_descriptor<void, void, void>();
+template type_descriptor make_type_descriptor<float, void, void>();
+template type_descriptor make_type_descriptor<double, void, void>();
+template type_descriptor
+make_type_descriptor<std::complex<float>, void, void>();
+template type_descriptor
+make_type_descriptor<std::complex<double>, void, void>();
+template type_descriptor make_type_descriptor<void, int32, void>();
+template type_descriptor make_type_descriptor<float, int32, void>();
+template type_descriptor make_type_descriptor<double, int32, void>();
+template type_descriptor
+make_type_descriptor<std::complex<float>, int32, void>();
+template type_descriptor
+make_type_descriptor<std::complex<double>, int32, void>();
+template type_descriptor make_type_descriptor<void, int64, void>();
+template type_descriptor make_type_descriptor<float, int64, void>();
+template type_descriptor make_type_descriptor<double, int64, void>();
+template type_descriptor
+make_type_descriptor<std::complex<float>, int64, void>();
+template type_descriptor
+make_type_descriptor<std::complex<double>, int64, void>();
+
+// global_index int32
+template type_descriptor make_type_descriptor<void, void, int32>();
+template type_descriptor make_type_descriptor<float, void, int32>();
+template type_descriptor make_type_descriptor<double, void, int32>();
+template type_descriptor
+make_type_descriptor<std::complex<float>, void, int32>();
+template type_descriptor
+make_type_descriptor<std::complex<double>, void, int32>();
+template type_descriptor make_type_descriptor<void, int32, int32>();
+template type_descriptor make_type_descriptor<float, int32, int32>();
+template type_descriptor make_type_descriptor<double, int32, int32>();
+template type_descriptor
+make_type_descriptor<std::complex<float>, int32, int32>();
+template type_descriptor
+make_type_descriptor<std::complex<double>, int32, int32>();
+
+// global_index_type int64
+template type_descriptor make_type_descriptor<void, void, int64>();
+template type_descriptor make_type_descriptor<float, void, int64>();
+template type_descriptor make_type_descriptor<double, void, int64>();
+template type_descriptor
+make_type_descriptor<std::complex<float>, void, int64>();
+template type_descriptor
+make_type_descriptor<std::complex<double>, void, int64>();
+template type_descriptor make_type_descriptor<void, int32, int64>();
+template type_descriptor make_type_descriptor<float, int32, int64>();
+template type_descriptor make_type_descriptor<double, int32, int64>();
+template type_descriptor
+make_type_descriptor<std::complex<float>, int32, int64>();
+template type_descriptor
+make_type_descriptor<std::complex<double>, int32, int64>();
+template type_descriptor make_type_descriptor<void, int64, int64>();
+template type_descriptor make_type_descriptor<float, int64, int64>();
+template type_descriptor make_type_descriptor<double, int64, int64>();
+template type_descriptor
+make_type_descriptor<std::complex<float>, int64, int64>();
+template type_descriptor
+make_type_descriptor<std::complex<double>, int64, int64>();
 
 
 type_descriptor::type_descriptor(std::string value_typestr,
-                                 std::string index_typestr)
-    : value_typestr_(value_typestr), index_typestr_(index_typestr)
+                                 std::string index_typestr,
+                                 std::string global_index_typestr)
+    : value_typestr_(value_typestr),
+      index_typestr_(index_typestr),
+      global_index_typestr_(global_index_typestr)
 {}
 
 const std::string& type_descriptor::get_value_typestr() const
@@ -69,6 +126,10 @@ const std::string& type_descriptor::get_index_typestr() const
     return index_typestr_;
 }
 
+const std::string& type_descriptor::get_global_index_typestr() const
+{
+    return global_index_typestr_;
+}
 
 }  // namespace config
 }  // namespace gko
