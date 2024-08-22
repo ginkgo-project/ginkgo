58b184b45d65a05fba70aae7b57112c690d7e3e5
Pratik Nayak <pratikvn@protonmail.com>
2024-08-22 17:57:03 +0200

    [dpcpp] unify dpcpp kernels

--- dpcpp/matrix/batch_csr_kernels.hpp
+++ dpcpp/matrix/batch_csr_kernels.hpp
@@ -2,8 +2,32 @@
 //
 // SPDX-License-Identifier: BSD-3-Clause
 
+#include <memory>
+
+#include <CL/sycl.hpp>
+
+#include "core/base/batch_struct.hpp"
+#include "core/matrix/batch_struct.hpp"
+#include "dpcpp/base/batch_struct.hpp"
+#include "dpcpp/base/config.hpp"
+#include "dpcpp/base/dim3.dp.hpp"
+#include "dpcpp/base/dpct.hpp"
+#include "dpcpp/base/helper.hpp"
+#include "dpcpp/components/cooperative_groups.dp.hpp"
+#include "dpcpp/components/intrinsics.dp.hpp"
+#include "dpcpp/components/reduction.dp.hpp"
+#include "dpcpp/components/thread_ids.dp.hpp"
+#include "dpcpp/matrix/batch_struct.hpp"
+
+
+namespace gko {
+namespace kernels {
+namespace GKO_DEVICE_NAMESPACE {
+namespace batch_single_kernels {
+
+
 template <typename ValueType, typename IndexType>
-__dpct_inline__ void simple_apply_kernel(
+__dpct_inline__ void simple_apply(
     const gko::batch::matrix::csr::batch_item<const ValueType, IndexType>& mat,
     const ValueType* b, ValueType* x, sycl::nd_item<3>& item_ct1)
 {
@@ -23,7 +47,7 @@ __dpct_inline__ void simple_apply_kernel(
 
 
 template <typename ValueType, typename IndexType>
-__dpct_inline__ void advanced_apply_kernel(
+__dpct_inline__ void advanced_apply(
     const ValueType alpha,
     const gko::batch::matrix::csr::batch_item<const ValueType, IndexType>& mat,
     const ValueType* b, const ValueType beta, ValueType* x,
@@ -45,13 +69,11 @@ __dpct_inline__ void advanced_apply_kernel(
 
 
 template <typename ValueType, typename IndexType>
-__dpct_inline__ void scale_kernel(const int num_rows,
-                                  const ValueType* const col_scale,
-                                  const ValueType* const row_scale,
-                                  const IndexType* const col_idxs,
-                                  const IndexType* const row_ptrs,
-                                  ValueType* const values,
-                                  sycl::nd_item<3>& item_ct1)
+__dpct_inline__ void scale(const int num_rows, const ValueType* const col_scale,
+                           const ValueType* const row_scale,
+                           const IndexType* const col_idxs,
+                           const IndexType* const row_ptrs,
+                           ValueType* const values, sycl::nd_item<3>& item_ct1)
 {
     for (int row = item_ct1.get_local_linear_id(); row < num_rows;
          row += item_ct1.get_local_range().size()) {
@@ -64,7 +86,7 @@ __dpct_inline__ void scale_kernel(const int num_rows,
 
 
 template <typename ValueType, typename IndexType>
-__dpct_inline__ void add_scaled_identity_kernel(
+__dpct_inline__ void add_scaled_identity(
     const ValueType alpha, const ValueType beta,
     const gko::batch::matrix::csr::batch_item<ValueType, IndexType>& mat,
     sycl::nd_item<3>& item_ct1)
@@ -80,3 +102,9 @@ __dpct_inline__ void add_scaled_identity_kernel(
         }
     }
 }
+
+
+}  // namespace batch_single_kernels
+}  // namespace GKO_DEVICE_NAMESPACE
+}  // namespace kernels
+}  // namespace gko
