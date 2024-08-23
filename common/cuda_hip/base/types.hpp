3ffba6a713eefcc61068bb642ab75345e31d7a4b
Pratik Nayak <pratikvn@protonmail.com>
2024-08-23 14:47:10 +0200

    [cuda, hip] rem anon namespace, type defs

--- common/cuda_hip/base/types.hpp
+++ common/cuda_hip/base/types.hpp
@@ -8,12 +8,8 @@
 
 #if defined(GKO_COMPILING_CUDA)
 #include "cuda/base/types.hpp"
-#define device_type cuda_type
-#define as_device_type as_cuda_type
 #elif defined(GKO_COMPILING_HIP)
 #include "hip/base/types.hip.hpp"
-#define device_type hip_type
-#define as_device_type as_hip_type
 #else
 #error "Executor definition missing"
 #endif
