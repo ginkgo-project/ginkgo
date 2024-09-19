3c4601bc7307cc24f660b6d63ac0168f214cdd0c
Marcel Koch <marcel.koch@kit.edu>
2024-09-19 13:32:25 +0200

    [doc] use doxysphinx to integrate API docs into sphinx
    Signed-off-by: Marcel Koch <marcel.koch@kit.edu>

--- doc/_doxygen/helpers.cmake
+++ doc/_doxygen/helpers.cmake
@@ -1,10 +1,8 @@
 # generates the API documentation
 function(ginkgo_doc_gen)
     set(GINKGO_DOXYGEN_DIR "${CMAKE_CURRENT_SOURCE_DIR}")
-    set(DIR_OUT "${CMAKE_CURRENT_BINARY_DIR}/../html/_doxygen/html")
+    set(GINKGO_DOXYGEN_BUILD_DIR ${CMAKE_CURRENT_SOURCE_DIR}/../_build_doxygen)
     configure_file(${CMAKE_CURRENT_SOURCE_DIR}/Doxyfile.in ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile)
-    set(doxyfile "${CMAKE_CURRENT_BINARY_DIR}/Doxyfile")
-    set(layout "${GINKGO_DOXYGEN_DIR}/DoxygenLayout.xml")
     file(GLOB doxygen_depend
         ${GINKGO_DOXYGEN_DIR}/headers/*.hpp
         ${Ginkgo_SOURCE_DIR}/include/ginkgo/**/*.hpp
@@ -20,29 +18,27 @@ function(ginkgo_doc_gen)
         ${Ginkgo_SOURCE_DIR}/examples/*.hpp
         )
     LIST(APPEND doxygen_depend ${_ginkgo_examples})
-    file(MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/../html/_doxygen)
     add_custom_target(doxygen ALL
-        COMMAND "${DOXYGEN_EXECUTABLE}" ${doxyfile}
-        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/../html/_doxygen
+        COMMAND "${DOXYGEN_EXECUTABLE}" ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile
+        WORKING_DIRECTORY ${GINKGO_DOXYGEN_BUILD_DIR}
         DEPENDS
         examples
         ${CMAKE_CURRENT_SOURCE_DIR}/mainpage.md
         ${CMAKE_CURRENT_SOURCE_DIR}/Doxyfile.in
-        ${CMAKE_CURRENT_SOURCE_DIR}/header.html
-        ${doxyfile}
-        ${layout}
+        ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile
         ${doxygen_depend}
         COMMENT "Generating API documentation with Doxygen"
         VERBATIM
         )
     if(GINKGO_DOC_GENERATE_PDF)
         add_custom_command(TARGET "doxygen" POST_BUILD
-        COMMAND make
-        COMMAND "${CMAKE_COMMAND}" -E copy refman.pdf
-        "${CMAKE_CURRENT_BINARY_DIR}/${name}.pdf"
-        WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/../html/_doxygen/latex"
-        COMMENT "Generating ${name} PDF from LaTeX"
-        VERBATIM
+            COMMAND make
+            COMMAND "${CMAKE_COMMAND}" -E copy refman.pdf
+            "${CMAKE_CURRENT_BINARY_DIR}/${name}.pdf"
+            WORKING_DIRECTORY "${GINKGO_DOXYGEN_BUILD_DIR}/latex"
+            DEPENDS doxygen
+            COMMENT "Generating ${name} PDF from LaTeX"
+            VERBATIM
         )
     endif()
 endfunction()
