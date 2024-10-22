92b208fed7cf5e5f6ca62915ab95acb7135b297e
Marcel Koch <marcel.koch@kit.edu>
2024-10-22 15:37:35 +0000

    [wip] fixup omp

--- examples/batched-matrix-free-templated/batched/backend/omp/batch_cg_kernels.hpp
+++ examples/batched-matrix-free-templated/batched/backend/omp/batch_cg_kernels.hpp
@@ -14,7 +14,6 @@
 #include "../../batch_criteria.hpp"
 #include "../../batch_identity.hpp"
 #include "../../batch_logger.hpp"
-#include "../../batch_multi_vector.hpp"
 
 
 namespace gko {
@@ -28,7 +27,7 @@ template <typename ValueType, typename Op>
 void apply(
     std::shared_ptr<const DefaultExecutor> exec,
     const kernels::batch_cg::settings<remove_complex<ValueType>>& options,
-    const Op* mat, batch::multi_vector::uniform_batch<const ValueType> b,
+    const Op mat, batch::multi_vector::uniform_batch<const ValueType> b,
     batch::multi_vector::uniform_batch<ValueType> x,
     batch::log::detail::log_data<remove_complex<ValueType>>& logdata)
     GKO_NOT_IMPLEMENTED;
