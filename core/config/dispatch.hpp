fb7042cc53b4332dc351d40159b2deffb85a2159
Yu-Hsiang M. Tsai <yhmtsai@gmail.com>
2024-10-25 16:27:44 +0200

    config

--- core/config/dispatch.hpp
+++ core/config/dispatch.hpp
@@ -105,6 +105,14 @@ deferred_factory_parameter<ReturnType> dispatch(
 using value_type_list =
     syn::type_list<double, float, std::complex<double>, std::complex<float>>;
 
+#if GINKGO_ENABLE_HALF
+using value_type_list_with_half =
+    syn::type_list<double, float, half, std::complex<double>,
+                   std::complex<float>, std::complex<half>>;
+#else
+using value_type_list_with_half = value_type_list;
+#endif  // GINKGO_ENABLE_HALF
+
 using index_type_list = syn::type_list<int32, int64>;
 
 }  // namespace config
