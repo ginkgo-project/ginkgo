b0f81dfd1a9ea957b9cf2f08158257193b17a2c2
Marcel Koch <marcel.koch@kit.edu>
2024-11-14 09:50:43 +0100

    [sycl] use non-deprecated MKL call only after v2024
    Somehow the 'correct' version of the MKL calls lead to a segfault if version < 2024, even though they already deprecated the old call in v2023...

--- dpcpp/matrix/csr_kernels.dp.cpp
+++ dpcpp/matrix/csr_kernels.dp.cpp
@@ -1398,7 +1398,10 @@ bool try_general_sparselib_spmv(std::shared_ptr<const DpcppExecutor> exec,
         oneapi::mkl::sparse::matrix_handle_t mat_handle;
         oneapi::mkl::sparse::init_matrix_handle(&mat_handle);
         oneapi::mkl::sparse::set_csr_data(
-            *exec->get_queue(), mat_handle, IndexType(a->get_size()[0]),
+#if INTEL_MKL_VERSION >= 20240000
+            *exec->get_queue(),
+#endif
+            mat_handle, IndexType(a->get_size()[0]),
             IndexType(a->get_size()[1]), oneapi::mkl::index_base::zero,
             const_cast<IndexType*>(a->get_const_row_ptrs()),
             const_cast<IndexType*>(a->get_const_col_idxs()),
@@ -1417,8 +1420,11 @@ bool try_general_sparselib_spmv(std::shared_ptr<const DpcppExecutor> exec,
                 const_cast<ValueType*>(b->get_const_values()), b->get_size()[1],
                 b->get_stride(), host_beta, c->get_values(), c->get_stride());
         }
-        oneapi::mkl::sparse::release_matrix_handle(*exec->get_queue(),
-                                                   &mat_handle);
+        oneapi::mkl::sparse::release_matrix_handle(
+#if INTEL_MKL_VERSION >= 20240000
+            *exec->get_queue(),
+#endif
+            &mat_handle);
     }
     return try_sparselib;
 }
