98e6fbdbc94262822c4437f45964c7aa24a4650e
Yu-Hsiang M. Tsai <yhmtsai@gmail.com>
2024-11-21 15:12:19 +0100

    make the macro stack clear

--- core/base/batch_instantiation.hpp
+++ core/base/batch_instantiation.hpp
@@ -12,37 +12,43 @@
 #include <ginkgo/core/matrix/batch_identity.hpp>
 #include <ginkgo/core/preconditioner/batch_jacobi.hpp>
 
+
 namespace gko {
 namespace batch {
+
+
+// just make the call list more consistent
+#define GKO_CALL(_macro, ...) _macro(__VA_ARGS__)
+
+#define GKO_BATCH_INSTANTIATE_PRECONDITIONER(_next, ...) \
+    _next(__VA_ARGS__, gko::batch::matrix::Identity);    \
+    _next(__VA_ARGS__, gko::batch::preconditioner::Jacobi)
+
+#define GKO_BATCH_INSTANTIATE_MATRIX(_next, ...)   \
+    _next(__VA_ARGS__, gko::batch::matrix::Ell);   \
+    _next(__VA_ARGS__, gko::batch::matrix::Dense); \
+    _next(__VA_ARGS__, gko::batch::matrix::Csr)
 
 /**
  * Instantiates a template for each valid combination of value type, batch
  * matrix type, and batch preconditioner type. This only allows batch matrix
  * type and preconditioner type also uses the same value type.
  *
- * @param _macro  A macro which expands the template instantiation
- *                (not including the leading `template` specifier).
- *                Should take three arguments, where the first is replaced by
- *                the value type, the second by the matrix, and the third by the
- *                preconditioner.
+ * @param args   the first should be a macro which expands the template
+ *               instantiation (not including the leading `template` specifier).
+ *               Should take three arguments, where the first is replaced by the
+ *               value type, the second by the matrix, and the third by the
+ *               preconditioner.
  *
  * @note the second and third arguments only accept the base type.s
  */
-#define GKO_INSTANTIATE_FOR_BATCH_VALUE_MATRIX_PRECONDITIONER(_macro)          \
-    GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE_VARGS(_macro, gko::batch::matrix::Csr, \
-                                              gko::batch::matrix::Identity);   \
-    GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE_VARGS(_macro, gko::batch::matrix::Ell, \
-                                              gko::batch::matrix::Identity);   \
-    GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE_VARGS(                                 \
-        _macro, gko::batch::matrix::Dense, gko::batch::matrix::Identity);      \
-    GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE_VARGS(                                 \
-        _macro, gko::batch::matrix::Csr, gko::batch::preconditioner::Jacobi);  \
-    GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE_VARGS(                                 \
-        _macro, gko::batch::matrix::Ell, gko::batch::preconditioner::Jacobi);  \
-    GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE_VARGS(                                 \
-        _macro, gko::batch::matrix::Dense, gko::batch::preconditioner::Jacobi)
+#define GKO_INSTANTIATE_FOR_BATCH_VALUE_MATRIX_PRECONDITIONER(...) \
+    GKO_CALL(GKO_BATCH_INSTANTIATE_MATRIX,                         \
+             GKO_BATCH_INSTANTIATE_PRECONDITIONER,                 \
+             GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE_VARGS, __VA_ARGS__)
+
 
 }  // namespace batch
 }  // namespace gko
 
-#endif  //
+#endif  // GKO_PUBLIC_CORE_BASE_BATCH_INSTANTIATION_HPP_
