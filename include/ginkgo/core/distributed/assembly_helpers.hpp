6ffc9b308396c62baa716c1d8f66fc2e0b9bc32c
Fritz Goebel <fritz.goebel@tum.de>
2024-11-21 10:08:26 +0100

    Add documentation for the assemble function

--- include/ginkgo/core/distributed/assembly_helpers.hpp
+++ include/ginkgo/core/distributed/assembly_helpers.hpp
@@ -25,6 +25,21 @@ template <typename LocalIndexType, typename GlobalIndexType>
 class Partition;
 
 
+/**
+ * Assembles device_matrix_data entries owned by this MPI rank from other ranks
+ * and communicates entries located on this MPI rank owned by other ranks to
+ * their respective owners. This can be useful e.g. in a finite element code
+ * where each rank assembles a local contribution to a global system matrix and
+ * the global matrix has to be assembled by summing up the local contributions
+ * on rank boundaries.
+ *
+ * @param comm the communicator used to assemble the global matrix.
+ * @param input the device_matrix_data structure.
+ * @param partition the partition used to determine owndership.
+ *
+ * @return the globally assembled device_matrix_data structure for this MPI
+ * rank.
+ */
 template <typename ValueType, typename LocalIndexType, typename GlobalIndexType>
 device_matrix_data<ValueType, GlobalIndexType> assemble(
     mpi::communicator comm,
