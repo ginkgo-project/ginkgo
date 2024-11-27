fca55a6e5f2bbd7740f2e5dff00d50a348166d73
Fritz Goebel <fritz.goebel@tum.de>
2024-11-27 17:22:41 +0100

    Fix multiple definitions in dpcpp

--- dpcpp/distributed/assembly_helpers_kernels.dp.cpp
+++ dpcpp/distributed/assembly_helpers_kernels.dp.cpp
@@ -27,22 +27,6 @@ GKO_INSTANTIATE_FOR_EACH_VALUE_AND_LOCAL_GLOBAL_INDEX_TYPE(
     GKO_DECLARE_COUNT_NON_OWNING_ENTRIES);
 
 
-template <typename ValueType, typename LocalIndexType, typename GlobalIndexType>
-void fill_send_buffers(
-    std::shared_ptr<const DefaultExecutor> exec,
-    const device_matrix_data<ValueType, GlobalIndexType>& input,
-    const experimental::distributed::Partition<LocalIndexType, GlobalIndexType>*
-        row_partition,
-    comm_index_type local_part, const array<GlobalIndexType>& send_positions,
-    const array<GlobalIndexType>& original_positions,
-    array<GlobalIndexType>& send_row_idxs,
-    array<GlobalIndexType>& send_col_idxs,
-    array<ValueType>& send_values) GKO_NOT_IMPLEMENTED;
-
-GKO_INSTANTIATE_FOR_EACH_VALUE_AND_LOCAL_GLOBAL_INDEX_TYPE(
-    GKO_DECLARE_FILL_SEND_BUFFERS);
-
-
 }  // namespace assembly_helpers
 }  // namespace dpcpp
 }  // namespace kernels
