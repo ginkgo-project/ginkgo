b4a3a403e6ef2af4cb1f1d8ad59c8547f094ac3c
Yu-Hsiang M. Tsai <yhmtsai@gmail.com>
2024-12-02 16:32:51 +0100

    manual changes

--- core/config/parse_macro.hpp
+++ core/config/parse_macro.hpp
@@ -16,8 +16,7 @@
 
 
 // for value_type only
-#define GKO_PARSE_VALUE_TYPE_BASE_(_type, _configurator,                     \
-                                   _value_type_list_base)                    \
+#define GKO_PARSE_VALUE_TYPE_(_type, _configurator, _value_type_list)        \
     template <>                                                              \
     deferred_factory_parameter<gko::LinOpFactory>                            \
     parse<gko::config::LinOpFactoryType::_type>(                             \
@@ -29,22 +28,21 @@
         return gko::config::dispatch<gko::LinOpFactory, _configurator>(      \
             config, context, updated,                                        \
             gko::config::make_type_selector(updated.get_value_typestr(),     \
-                                            _value_type_list_base));         \
+                                            _value_type_list));              \
     }                                                                        \
     static_assert(true,                                                      \
                   "This assert is used to counter the false positive extra " \
                   "semi-colon warnings")
 #define GKO_PARSE_VALUE_TYPE_BASE(_type, _configurator) \
-    GKO_PARSE_VALUE_TYPE_BASE_(_type, _configurator,    \
-                               gko::config::value_type_list_base())
+    GKO_PARSE_VALUE_TYPE_(_type, _configurator,         \
+                          gko::config::value_type_list_base())
 
-#define GKO_PARSE_VALUE_TYPE(_type, _configurator)   \
-    GKO_PARSE_VALUE_TYPE_BASE_(_type, _configurator, \
-                               gko::config::value_type_list())
+#define GKO_PARSE_VALUE_TYPE(_type, _configurator) \
+    GKO_PARSE_VALUE_TYPE_(_type, _configurator, gko::config::value_type_list())
 
 // for value_type and index_type
-#define GKO_PARSE_VALUE_AND_INDEX_TYPE_BASE_(_type, _configurator,            \
-                                             _value_type_list_base)           \
+#define GKO_PARSE_VALUE_AND_INDEX_TYPE_(_type, _configurator,                 \
+                                        _value_type_list)                     \
     template <>                                                               \
     deferred_factory_parameter<gko::LinOpFactory>                             \
     parse<gko::config::LinOpFactoryType::_type>(                              \
@@ -56,7 +54,7 @@
         return gko::config::dispatch<gko::LinOpFactory, _configurator>(       \
             config, context, updated,                                         \
             gko::config::make_type_selector(updated.get_value_typestr(),      \
-                                            _value_type_list_base),           \
+                                            _value_type_list),                \
             gko::config::make_type_selector(updated.get_index_typestr(),      \
                                             gko::config::index_type_list())); \
     }                                                                         \
@@ -65,12 +63,12 @@
                   "semi-colon warnings")
 
 #define GKO_PARSE_VALUE_AND_INDEX_TYPE_BASE(_type, _configurator) \
-    GKO_PARSE_VALUE_AND_INDEX_TYPE_BASE_(_type, _configurator,    \
-                                         gko::config::value_type_list_base())
+    GKO_PARSE_VALUE_AND_INDEX_TYPE_(_type, _configurator,         \
+                                    gko::config::value_type_list_base())
 
-#define GKO_PARSE_VALUE_AND_INDEX_TYPE(_type, _configurator)   \
-    GKO_PARSE_VALUE_AND_INDEX_TYPE_BASE_(_type, _configurator, \
-                                         gko::config::value_type_list())
+#define GKO_PARSE_VALUE_AND_INDEX_TYPE(_type, _configurator) \
+    GKO_PARSE_VALUE_AND_INDEX_TYPE_(_type, _configurator,    \
+                                    gko::config::value_type_list())
 
 
 #endif  // GKO_CORE_CONFIG_PARSE_MACRO_HPP_
