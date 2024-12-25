013e5be62be9d8ac66951c5e38cb08373855752a
Tobias Ribizel <mail@ribizel.de>
2024-12-25 20:03:18 +0100

    add some tests

--- core/components/range_minimum_query.hpp
+++ core/components/range_minimum_query.hpp
@@ -14,48 +14,115 @@ namespace gko {
 namespace detail {
 
 
-constexpr int ballot_number(int p, int q)
-{
-    if (p == 0 && q == 0) {
-        return 1;
-    }
-    if (p > q || p < 0 || q <= 0) {
-        return 0;
-    }
-    return ballot_number(p - 1, q) + ballot_number(p, q - 1);
-}
-
-
-constexpr int catalan_number(int s) { return ballot_number(s, s); }
-
-
-template <int max_value>
-struct ballot_number_lookup {
-    constexpr static int size = max_value + 1;
-    constexpr static int size2 = size * size;
-
-    constexpr static int get(int p, int q)
+struct cartesian_tree {
+    constexpr static int ballot_number(int p, int q)
     {
+        if (p == 0 && q == 0) {
+            return 1;
+        }
         if (p > q || p < 0 || q <= 0) {
             return 0;
         }
-        assert(p <= max_value && q <= max_value);
-        return lut[p * size + q];
+        return ballot_number(p - 1, q) + ballot_number(p, q - 1);
     }
 
-    constexpr static std::array<int, size2> compute()
+
+    constexpr static int catalan_number(int s) { return ballot_number(s, s); }
+
+    template <int max_value>
+    struct ballot_number_lookup {
+        constexpr static int size = max_value + 1;
+        constexpr static int size2 = size * size;
+
+        constexpr static int get(int p, int q)
+        {
+            if (p > q || p < 0 || q <= 0) {
+                return 0;
+            }
+            assert(p <= max_value && q <= max_value);
+            return lut[p * size + q];
+        }
+
+        constexpr static std::array<int, size2> compute()
+        {
+            std::array<int, size2> lut{};
+            for (const auto p : irange{size}) {
+                for (const auto q : irange{size}) {
+                    lut[p * size + q] = ballot_number(p, q);
+                }
+            }
+            return lut;
+        }
+
+        constexpr static std::array<int, size2> lut = compute();
+    };
+
+
+    template <std::size_t array_size, typename T>
+    constexpr static int compute_tree_index(std::array<T, array_size> values,
+                                            int num_nodes = array_size)
     {
-        std::array<int, size2> lut{{}};
-        for (const auto p : irange{size}) {
-            for (const auto q : irange{size}) {
-                lut[p * size + q] = ballot_number(p, q);
+        using ballot_lookup = ballot_number_lookup<array_size>;
+        // build cartesian tree left-to-right and traverse ballot number
+        // triangle in parallel
+        std::array<int, array_size + 1> rightmost{};
+        rightmost[0] = std::numeric_limits<T>::lowest();
+        int number = 0;
+        int q = num_nodes;
+        for (int i = 0; i < num_nodes; i++) {
+            while (rightmost[q + i - num_nodes] > values[i]) {
+                number += ballot_lookup::get(num_nodes - (i + 1), q);
+                q--;
+            }
+            rightmost[q + i + 1 - num_nodes] = values[i];
+        }
+        return number;
+    }
+
+
+    template <int num_nodes>
+    constexpr static std::array<std::array<int, num_nodes>,
+                                catalan_number(num_nodes)>
+    compute_tree_representatives()
+    {
+        std::array<
+            std::array<std::array<int, num_nodes>, catalan_number(num_nodes)>,
+            num_nodes + 1>
+            all_representatives{};
+        for (const int cur_num_nodes : irange{1, num_nodes + 1}) {
+            for (const int min_pos : irange{cur_num_nodes}) {
+                const auto left_size = min_pos;
+                const auto right_size = cur_num_nodes - min_pos - 1;
+                for (const int left_idx : irange{catalan_number(left_size)}) {
+                    const auto& left_rep =
+                        all_representatives[left_size][left_idx];
+                    for (const int right_idx :
+                         irange{catalan_number(right_size)}) {
+                        const auto& right_rep =
+                            all_representatives[right_size][right_idx];
+                        std::array<int, num_nodes> local_rep{{}};
+                        local_rep[min_pos] = 0;
+                        for (const auto i : irange{left_size}) {
+                            local_rep[i] = left_rep[i] + 1;
+                        }
+                        for (const auto i : irange{right_size}) {
+                            local_rep[i + min_pos + 1] = right_rep[i] + 1;
+                        }
+                        const auto tree_number =
+                            compute_tree_index(local_rep, cur_num_nodes);
+                        auto& output_rep =
+                            all_representatives[cur_num_nodes][tree_number];
+                        for (const auto i : irange{cur_num_nodes}) {
+                            output_rep[i] = local_rep[i];
+                        }
+                    }
+                }
             }
         }
-        return lut;
+        return all_representatives[num_nodes];
     }
-
-    constexpr static std::array<int, size2> lut = compute();
 };
+
 
 constexpr int ceil_log2(int value)
 {
@@ -66,139 +133,11 @@ constexpr int ceil_log2(int value)
 }
 
 
-template <int array_size, typename T>
-constexpr int compute_tree_number(std::array<T, array_size> values,
-                                  int num_nodes = array_size)
-{
-    using ballot_lookup = ballot_number_lookup<array_size>;
-    // build cartesian tree left-to-right and traverse ballot number
-    // triangle in parallel
-    std::array<int, array_size + 1> rightmost{{0}};
-    rightmost[0] = std::numeric_limits<T>::lowest();
-    int number = 0;
-    int q = num_nodes;
-    for (int i = 0; i < num_nodes; i++) {
-        while (rightmost[q + i - num_nodes] > values[i]) {
-            number += ballot_lookup::get(num_nodes - (i + 1), q);
-            q--;
-        }
-        rightmost[q + i + 1 - num_nodes] = values[i];
-    }
-    return number;
-}
-
-
-template <int num_nodes>
-constexpr std::array<std::array<int, num_nodes>, catalan_number(num_nodes)>
-compute_tree_representatives();
-
-
-template <int num_nodes, int min_pos, int left_tree_idx, int right_tree_idx>
-constexpr void compute_tree_representative(
-    std::array<std::array<int, num_nodes>, catalan_number(num_nodes)>&
-        representatives,
-    const std::array<std::array<int, min_pos>, catalan_number(min_pos)>&
-        left_tree_reps,
-    const std::array<std::array<int, num_nodes - min_pos - 1>,
-                     catalan_number(num_nodes - min_pos - 1)>& right_tree_reps)
-{
-    constexpr auto left_size = min_pos;
-    constexpr auto right_size = num_nodes - min_pos - 1;
-    static_assert(min_pos >= 0);
-    static_assert(min_pos < num_nodes);
-    static_assert(left_tree_idx >= 0);
-    static_assert(left_tree_idx < catalan_number(left_size));
-    static_assert(right_tree_idx >= 0);
-    static_assert(right_tree_idx < catalan_number(right_size));
-    std::array<int, num_nodes> rep{{}};
-    const auto& left_rep = left_tree_reps[left_tree_idx];
-    const auto& right_rep = right_tree_reps[right_tree_idx];
-    rep[min_pos] = 0;
-    for (const auto i : irange{left_size}) {
-        rep[i] = left_rep[i] + 1;
-    }
-    for (const auto i : irange{right_size}) {
-        rep[min_pos + 1 + i] = right_rep[i] + 1;
-    }
-    const auto tree_number = compute_tree_number<num_nodes>(rep);
-    for (const auto i : irange{num_nodes}) {
-        representatives[tree_number][i] = rep[i];
-    }
-}
-
-
-template <int num_nodes, int min_pos, int left_tree_idx, int... right_tree_idx>
-constexpr void compute_tree_representatives_right_loop(
-    std::array<std::array<int, num_nodes>, catalan_number(num_nodes)>&
-        representatives,
-    const std::array<std::array<int, min_pos>, catalan_number(min_pos)>&
-        left_tree_reps,
-    std::integer_sequence<int, right_tree_idx...>)
-{
-    constexpr auto left_size = min_pos;
-    constexpr auto right_size = num_nodes - min_pos - 1;
-    static_assert(min_pos >= 0);
-    static_assert(min_pos < num_nodes);
-    static_assert(left_tree_idx >= 0);
-    static_assert(left_tree_idx < catalan_number(left_size));
-    const auto right_tree_reps = compute_tree_representatives<right_size>();
-    (compute_tree_representative<num_nodes, min_pos, left_tree_idx,
-                                 right_tree_idx>(
-         representatives, left_tree_reps, right_tree_reps),
-     ...);
-}
-
-
-template <int num_nodes, int min_pos, int... left_tree_idx>
-constexpr void compute_tree_representatives_left_loop(
-    std::array<std::array<int, num_nodes>, catalan_number(num_nodes)>&
-        representatives,
-    std::integer_sequence<int, left_tree_idx...>)
-{
-    constexpr auto left_size = min_pos;
-    static_assert(min_pos >= 0);
-    static_assert(min_pos < num_nodes);
-    const auto left_trees = compute_tree_representatives<left_size>();
-    (compute_tree_representatives_right_loop<num_nodes, min_pos, left_tree_idx>(
-         representatives, left_trees,
-         std::make_integer_sequence<int,
-                                    catalan_number(num_nodes - min_pos - 1)>{}),
-     ...);
-}
-
-
-template <int num_nodes, int... min_pos>
-constexpr void compute_tree_representatives_min_pos_loop(
-    std::array<std::array<int, num_nodes>, catalan_number(num_nodes)>&
-        representatives,
-    std::integer_sequence<int, min_pos...>)
-{
-    (compute_tree_representatives_left_loop<num_nodes, min_pos>(
-         representatives,
-         std::make_integer_sequence<int, catalan_number(min_pos)>{}),
-     ...);
-}
-
-
-template <int num_nodes>
-constexpr std::array<std::array<int, num_nodes>, catalan_number(num_nodes)>
-compute_tree_representatives()
-{
-    std::array<std::array<int, num_nodes>, catalan_number(num_nodes)>
-        representatives{{{}}};
-    if constexpr (num_nodes > 1) {
-        compute_tree_representatives_min_pos_loop<num_nodes>(
-            representatives, std::make_integer_sequence<int, num_nodes>{});
-    }
-    return representatives;
-}
-
-
 template <int block_size>
 class block_range_minimum_query_lookup_table {
 public:
     // how many trees does the lookup table (LUT) contain?
-    constexpr static int num_trees = catalan_number(block_size);
+    constexpr static int num_trees = cartesian_tree::catalan_number(block_size);
     // how many bits do we need theoretically for this block?
     constexpr static int num_min_bits = ceil_log2(block_size);
     // for actual bits we use a power of two
@@ -211,21 +150,45 @@ public:
         (block_size * block_size) / values_per_word;
 
 
-    constexpr block_range_minimum_query_lookup_table() {}
-
-    constexpr static std::array<int, block_size> example_values(int tree_number)
-    {}
-
-    constexpr int lookup(int tree, int begin, int end) const
+    constexpr block_range_minimum_query_lookup_table() : lookup_table{}
     {
-        const auto flat_entry = begin + block_size * end;
+        const auto representatives =
+            cartesian_tree::compute_tree_representatives<block_size>();
+        for (const int tree :
+             irange{static_cast<int>(representatives.size())}) {
+            const auto& rep = representatives[tree];
+            for (const int first : irange{block_size}) {
+                for (const int last : irange{block_size}) {
+                    int min_index{};
+                    for (int i = first; i <= last; i++) {
+                        if (rep[i] < rep[min_index]) {
+                            min_index = i;
+                        }
+                    }
+                    const auto [block_id, shift] = lookup_pos(first, last);
+                    lookup_table[tree][block_id] |=
+                        static_cast<uint32>(min_index) << shift;
+                }
+            }
+        }
+    }
+
+    constexpr std::pair<int, int> lookup_pos(int first, int last) const
+    {
+        const auto flat_entry = first + block_size * last;
         const auto block_id = flat_entry / values_per_word;
         const auto shift = (flat_entry % values_per_word) * num_bits;
+        return std::make_pair(block_id, shift);
+    }
+
+    constexpr int lookup(int tree, int first, int last) const
+    {
+        const auto [block_id, shift] = lookup_pos(first, last);
         const auto block = lookup_table[tree][block_id];
         return static_cast<int>((block >> shift) & mask);
     }
 
-private:
+    // private:
     std::array<std::array<uint32, tree_lut_size>, num_trees> lookup_table;
 };
 
--- core/test/components/range_minimum_query.cpp
+++ core/test/components/range_minimum_query.cpp
@@ -14,48 +14,83 @@ namespace {
 
 TEST(RangeMinimumQuery, CatalanNumber)
 {
-    ASSERT_EQ(gko::detail::catalan_number(1), 1);
-    ASSERT_EQ(gko::detail::catalan_number(2), 2);
-    ASSERT_EQ(gko::detail::catalan_number(3), 5);
-    ASSERT_EQ(gko::detail::catalan_number(4), 14);
-    ASSERT_EQ(gko::detail::catalan_number(5), 42);
-    ASSERT_EQ(gko::detail::catalan_number(6), 132);
-    ASSERT_EQ(gko::detail::catalan_number(7), 429);
-    ASSERT_EQ(gko::detail::catalan_number(8), 1430);
+    using gko::detail::cartesian_tree;
+    ASSERT_EQ(cartesian_tree::catalan_number(1), 1);
+    ASSERT_EQ(cartesian_tree::catalan_number(2), 2);
+    ASSERT_EQ(cartesian_tree::catalan_number(3), 5);
+    ASSERT_EQ(cartesian_tree::catalan_number(4), 14);
+    ASSERT_EQ(cartesian_tree::catalan_number(5), 42);
+    ASSERT_EQ(cartesian_tree::catalan_number(6), 132);
+    ASSERT_EQ(cartesian_tree::catalan_number(7), 429);
+    ASSERT_EQ(cartesian_tree::catalan_number(8), 1430);
 }
 
 
 TEST(RangeMinimumQuery, BallotNumber)
 {
-    ASSERT_EQ(gko::detail::ballot_number(0, 0), 1);
-    ASSERT_EQ(gko::detail::ballot_number(0, 3), 1);
-    ASSERT_EQ(gko::detail::ballot_number(0, 2), 1);
-    ASSERT_EQ(gko::detail::ballot_number(1, 3), 3);
+    using gko::detail::cartesian_tree;
+    ASSERT_EQ(cartesian_tree::ballot_number(0, 0), 1);
+    ASSERT_EQ(cartesian_tree::ballot_number(0, 3), 1);
+    ASSERT_EQ(cartesian_tree::ballot_number(0, 2), 1);
+    ASSERT_EQ(cartesian_tree::ballot_number(1, 3), 3);
 }
 
 
 TEST(RangeMinimumQuery, ComputeTreeNumber)
 {
-    ASSERT_EQ((gko::detail::compute_tree_number<3, int>({1, 2, 3})), 0);
-    ASSERT_EQ((gko::detail::compute_tree_number<3, int>({1, 3, 2})), 1);
-    ASSERT_EQ((gko::detail::compute_tree_number<3, int>({2, 3, 1})), 2);
-    ASSERT_EQ((gko::detail::compute_tree_number<3, int>({2, 1, 3})), 3);
-    ASSERT_EQ((gko::detail::compute_tree_number<3, int>({3, 2, 1})), 4);
+    using gko::detail::cartesian_tree;
+    ASSERT_EQ((cartesian_tree::compute_tree_index<3, int>({1, 2, 3})), 0);
+    ASSERT_EQ((cartesian_tree::compute_tree_index<3, int>({1, 3, 2})), 1);
+    ASSERT_EQ((cartesian_tree::compute_tree_index<3, int>({2, 3, 1})), 2);
+    ASSERT_EQ((cartesian_tree::compute_tree_index<3, int>({2, 1, 3})), 3);
+    ASSERT_EQ((cartesian_tree::compute_tree_index<3, int>({3, 2, 1})), 4);
 }
 
 
 TEST(RangeMinimumQuery, Representatives)
 {
+    using gko::detail::cartesian_tree;
     constexpr auto size = 8;
     std::array<int, size> values{{}};
     std::iota(values.begin(), values.end(), 0);
-    constexpr auto reps = gko::detail::compute_tree_representatives<size>();
+    constexpr auto reps = cartesian_tree::compute_tree_representatives<size>();
     do {
-        const auto tree_number = gko::detail::compute_tree_number<size>(values);
+        const auto tree_number =
+            cartesian_tree::compute_tree_index<size>(values);
         const auto rep_tree_number =
-            gko::detail::compute_tree_number<size>(reps[tree_number]);
+            cartesian_tree::compute_tree_index<size>(reps[tree_number]);
         ASSERT_EQ(tree_number, rep_tree_number);
     } while (std::next_permutation(values.begin(), values.end()));
+}
+
+
+TEST(RangeMinimumQuery, Lookup)
+{
+    using namespace gko::detail;
+    constexpr auto size = 8;
+    block_range_minimum_query_lookup_table<size> table;
+    auto reps = cartesian_tree::compute_tree_representatives<size>();
+    for (auto v : table.lookup_table) {
+        for (auto vv : v) {
+            std::cout << std::hex << vv << ' ';
+        }
+        std::cout << '\n';
+    }
+    for (const auto& rep : reps) {
+        const auto tree = cartesian_tree::compute_tree_index(rep);
+        for (const auto first : gko::irange{size}) {
+            for (const auto last : gko::irange{size}) {
+                const auto begin = rep.begin() + first;
+                const auto end = rep.begin() + last + 1;
+                const auto min_pos =
+                    begin > end ? 0
+                                : std::distance(rep.begin(),
+                                                std::min_element(begin, end));
+                ASSERT_EQ(table.lookup(tree, first, last), min_pos)
+                    << tree << ' ' << first << ' ' << last;
+            }
+        }
+    }
 }
 
 
