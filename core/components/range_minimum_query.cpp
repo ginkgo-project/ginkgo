3c8074e9070d551a629f53fe33d1e933e6ccf34f
Tobias Ribizel <mail@ribizel.de>
2025-01-19 22:46:45 +0100

    fix typos

--- core/components/range_minimum_query.cpp
+++ core/components/range_minimum_query.cpp
@@ -28,8 +28,8 @@ device_range_minimum_query<IndexType>::device_range_minimum_query(
     : num_blocks_{static_cast<index_type>(
           ceildiv(static_cast<index_type>(data.get_size()), block_size))},
       lut_{data.get_executor()},
-      block_tree_indexs_{data.get_executor(),
-                         static_cast<size_type>(num_blocks_)},
+      block_tree_indices_{data.get_executor(),
+                          static_cast<size_type>(num_blocks_)},
       block_argmin_storage_{
           data.get_executor(),
           static_cast<size_type>(block_argmin_view_type::storage_size(
@@ -45,7 +45,7 @@ device_range_minimum_query<IndexType>::device_range_minimum_query(
         block_argmin_storage_.get_data(), block_argmin_num_bits, num_blocks_};
     exec->run(make_compute_lookup_small(
         values_.get_const_data(), static_cast<index_type>(values_.get_size()),
-        block_argmin, block_min_.get_data(), block_tree_indexs_.get_data()));
+        block_argmin, block_min_.get_data(), block_tree_indices_.get_data()));
     auto superblocks =
         superblock_view_type{block_min_.get_const_data(),
                              superblock_storage_.get_data(), num_blocks_};
@@ -61,7 +61,7 @@ device_range_minimum_query<IndexType>::get() const
     return range_minimum_query{values_.get_const_data(),
                                block_min_.get_const_data(),
                                block_argmin_storage_.get_const_data(),
-                               block_tree_indexs_.get_const_data(),
+                               block_tree_indices_.get_const_data(),
                                superblock_storage_.get_const_data(),
                                lut_.get(),
                                static_cast<index_type>(values_.get_size())};
