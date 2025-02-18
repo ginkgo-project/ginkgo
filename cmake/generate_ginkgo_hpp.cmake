f37f9754c0d4364d18a38050650fae26352e9358
Pratik Nayak <pratikvn@protonmail.com>
2025-02-18 10:21:26 +0100

    [cmake] format fixes

--- cmake/generate_ginkgo_hpp.cmake
+++ cmake/generate_ginkgo_hpp.cmake
@@ -1,20 +1,34 @@
 function(ginkgo_generate_ginkgo_hpp)
-    file(GLOB_RECURSE headers CONFIGURE_DEPENDS "${Ginkgo_SOURCE_DIR}/include/ginkgo/*.hpp")
+    file(
+        GLOB_RECURSE headers
+        CONFIGURE_DEPENDS
+        "${Ginkgo_SOURCE_DIR}/include/ginkgo/*.hpp"
+    )
     set(GKO_PUBLIC_HEADER_CONTENTS)
     foreach(file IN LISTS headers)
         file(RELATIVE_PATH file "${Ginkgo_SOURCE_DIR}/include" "${file}")
         # just making sure it uses / path separators
         file(TO_CMAKE_PATH file "${file}")
-        if((file MATCHES "^ginkgo/extensions/.*$") OR
-           (file MATCHES "^ginkgo/core/stop/residual_norm_reduction.hpp$") OR
-           (file MATCHES "^ginkgo/core/solver/.*_trs.hpp$"))
-           continue()
+        if(
+            (file MATCHES "^ginkgo/extensions/.*$")
+            OR (file MATCHES "^ginkgo/core/stop/residual_norm_reduction.hpp$")
+            OR (file MATCHES "^ginkgo/core/solver/.*_trs.hpp$")
+        )
+            continue()
         endif()
-        set(GKO_PUBLIC_HEADER_CONTENTS "${GKO_PUBLIC_HEADER_CONTENTS}#include <${file}>\n")
+        set(GKO_PUBLIC_HEADER_CONTENTS
+            "${GKO_PUBLIC_HEADER_CONTENTS}#include <${file}>\n"
+        )
     endforeach()
-    configure_file("${Ginkgo_SOURCE_DIR}/include/ginkgo/ginkgo.hpp.in"
-        "${Ginkgo_BINARY_DIR}/include/ginkgo/ginkgo.hpp" @ONLY)
+    configure_file(
+        "${Ginkgo_SOURCE_DIR}/include/ginkgo/ginkgo.hpp.in"
+        "${Ginkgo_BINARY_DIR}/include/ginkgo/ginkgo.hpp"
+        @ONLY
+    )
     if(EXISTS "${Ginkgo_SOURCE_DIR}/include/ginkgo/ginkgo.hpp")
-        message(FATAL_ERROR "ginkgo.hpp is auto-generated and should not be in include/ginkgo")
+        message(
+            FATAL_ERROR
+            "ginkgo.hpp is auto-generated and should not be in include/ginkgo"
+        )
     endif()
 endfunction()
