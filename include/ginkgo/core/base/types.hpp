887ef7e03897db9454d78f2f6255576417737f09
Pratik Nayak <pratikvn@protonmail.com>
2025-03-06 14:22:32 +0100

    [csl] add CslExecutor class

--- include/ginkgo/core/base/types.hpp
+++ include/ginkgo/core/base/types.hpp
@@ -397,7 +397,8 @@ GKO_ATTRIBUTES constexpr bool operator!=(precision_reduction x,
     _enable_macro(OmpExecutor, omp);                \
     _enable_macro(HipExecutor, hip);                \
     _enable_macro(DpcppExecutor, dpcpp);            \
-    _enable_macro(CudaExecutor, cuda)
+    _enable_macro(CudaExecutor, cuda);              \
+    _enable_macro(PoplarExecutor, poplar)
 
 
 // cuda half operation is supported from arch 5.3
