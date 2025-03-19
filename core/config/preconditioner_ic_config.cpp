70e584cec495be003ce1a165b9787f5cdb99368f
Yu-Hsiang M. Tsai <yhmtsai@gmail.com>
2025-03-19 16:10:02 +0100

    only allow LinOp in IC during parse

--- core/config/preconditioner_ic_config.cpp
+++ core/config/preconditioner_ic_config.cpp
@@ -22,46 +22,20 @@ namespace gko {
 namespace config {
 
 
-// For Ic and Ilu, we use additional ValueType to help Solver type decision
-template <typename Solver>
 class IcSolverHelper {
 public:
-    template <typename ValueType, typename IndexType>
+    template <typename IndexType>
     class Configurator {
     public:
-        static
-            typename gko::preconditioner::Ic<Solver, IndexType>::parameters_type
-            parse(const pnode& config, const registry& context,
-                  const type_descriptor& td_for_child)
+        static typename gko::preconditioner::Ic<gko::LinOp,
+                                                IndexType>::parameters_type
+        parse(const pnode& config, const registry& context,
+              const type_descriptor& td_for_child)
         {
-            return gko::preconditioner::Ic<Solver, IndexType>::parse(
+            return gko::preconditioner::Ic<gko::LinOp, IndexType>::parse(
                 config, context, td_for_child);
         }
     };
-};
-
-
-// Do not use the partial specialization for SolverBase<V> and SolverBase<V, I>
-// because the default template arguments are allowed for a template template
-// argument (detail: CWG 150 after c++17
-// https://en.cppreference.com/w/cpp/language/template_parameters#Template_template_arguments)
-template <template <typename V> class SolverBase>
-class IcHelper1 {
-public:
-    template <typename ValueType, typename IndexType>
-    class Configurator
-        : public IcSolverHelper<SolverBase<ValueType>>::template Configurator<
-              ValueType, IndexType> {};
-};
-
-
-template <template <typename V, typename I> class SolverBase>
-class IcHelper2 {
-public:
-    template <typename ValueType, typename IndexType>
-    class Configurator
-        : public IcSolverHelper<SolverBase<ValueType, IndexType>>::
-              template Configurator<ValueType, IndexType> {};
 };
 
 
@@ -70,42 +44,9 @@ deferred_factory_parameter<gko::LinOpFactory> parse<LinOpFactoryType::Ic>(
     const pnode& config, const registry& context, const type_descriptor& td)
 {
     auto updated = update_type(config, td);
-    std::string str("solver::LowerTrs");
-    if (auto& obj = config.get("l_solver_type")) {
-        str = obj.get_string();
-    }
-    if (str == "solver::LowerTrs") {
-        return dispatch<gko::LinOpFactory,
-                        IcHelper2<solver::LowerTrs>::Configurator>(
-            config, context, updated,
-            make_type_selector(updated.get_value_typestr(), value_type_list()),
-            make_type_selector(updated.get_index_typestr(), index_type_list()));
-    } else if (str == "solver::Ir") {
-        return dispatch<gko::LinOpFactory, IcHelper1<solver::Ir>::Configurator>(
-            config, context, updated,
-            make_type_selector(updated.get_value_typestr(), value_type_list()),
-            make_type_selector(updated.get_index_typestr(), index_type_list()));
-    } else if (str == "preconditioner::LowerIsai") {
-        return dispatch<gko::LinOpFactory,
-                        IcHelper2<preconditioner::LowerIsai>::Configurator>(
-            config, context, updated,
-            make_type_selector(updated.get_value_typestr(), value_type_list()),
-            make_type_selector(updated.get_index_typestr(), index_type_list()));
-    } else if (str == "solver::Gmres") {
-        return dispatch<gko::LinOpFactory,
-                        IcHelper1<solver::Gmres>::Configurator>(
-            config, context, updated,
-            make_type_selector(updated.get_value_typestr(), value_type_list()),
-            make_type_selector(updated.get_index_typestr(), index_type_list()));
-    } else if (str == "LinOp") {
-        return dispatch<gko::LinOpFactory, IcSolverHelper<LinOp>::Configurator>(
-            config, context, updated,
-            // no effect
-            make_type_selector(updated.get_value_typestr(), value_type_list()),
-            make_type_selector(updated.get_index_typestr(), index_type_list()));
-    } else {
-        GKO_INVALID_CONFIG_VALUE("l_solver_type", str);
-    }
+    return dispatch<gko::LinOpFactory, IcSolverHelper::Configurator>(
+        config, context, updated,
+        make_type_selector(updated.get_index_typestr(), index_type_list()));
 }
 
 
