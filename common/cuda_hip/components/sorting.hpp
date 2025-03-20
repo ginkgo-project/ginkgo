8ffc7d78e2f73a987d9724ef741896b101ef56e9
Tobias Ribizel <mail@ribizel.de>
2025-03-20 13:26:25 +0100

    wip

--- common/cuda_hip/components/sorting.hpp
+++ common/cuda_hip/components/sorting.hpp
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2017 - 2024 The Ginkgo authors
+// SPDX-FileCopyrightText: 2017 - 2025 The Ginkgo authors
 //
 // SPDX-License-Identifier: BSD-3-Clause
 
@@ -308,6 +308,59 @@ __forceinline__ __device__ void bitonic_sort(ValueType* local_elements,
             local_elements, false);
     }
 }
+
+/*
+template <int num_buckets, typename Iterator, typename BucketIndexOp>
+std::array<int64, num_buckets + 1> bucket_sort(
+    std::shared_ptr<const DefaultExecutor> exec, Iterator begin, Iterator end,
+    Iterator out_begin, BucketIndexOp bucket_op, array<int64> tmp)
+{
+    using index_type = typename std::iterator_traits<Iterator>::difference_type;
+    const auto size = end - begin;
+
+
+    std::vector<std::array<int64, num_buckets>> sums(
+        num_threads, std::array<int64, num_buckets>{});
+    std::array<int64, num_buckets + 1> global_offsets{};
+#pragma omp parallel
+    {
+        const auto tid = omp_get_thread_num();
+        auto& counts = sums[tid];
+#pragma omp for
+        for (index_type i = 0; i < size; i++) {
+            const auto value = *(begin + i);
+            const auto bucket = bucket_op(value);
+            assert(bucket >= 0);
+            assert(bucket < num_buckets);
+            counts[bucket]++;
+        }
+#pragma omp barrier
+#pragma omp single
+        {
+            std::array<int64, num_buckets> offsets{};
+            for (int tid = 0; tid < num_threads; tid++) {
+                for (int i = 0; i < num_buckets; i++) {
+                    const auto value = sums[tid][i];
+                    sums[tid][i] = offsets[i];
+                    offsets[i] += value;
+                }
+            }
+            std::copy_n(offsets.begin(), num_buckets, global_offsets.begin());
+            std::exclusive_scan(global_offsets.begin(), global_offsets.end(),
+                                global_offsets.begin(), index_type{});
+        }
+#pragma omp for
+        for (index_type i = 0; i < size; i++) {
+            const auto value = *(begin + i);
+            const auto bucket = bucket_op(value);
+            assert(bucket >= 0);
+            assert(bucket < num_buckets);
+            const auto output_pos = counts[bucket]++ + global_offsets[bucket];
+            *(out_begin + output_pos) = value;
+        }
+    }
+    return global_offsets;
+}*/
 
 
 }  // namespace GKO_DEVICE_NAMESPACE
