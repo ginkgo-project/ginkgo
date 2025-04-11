60a1cce23cdc9ea52be5e4364c7c37f053a378ef
Yu-Hsiang M. Tsai <yhmtsai@gmail.com>
2025-04-11 18:33:39 +0200

    fix numeric_limits, reduction, unary- issue on bfloat16 from sycl

--- dpcpp/preconditioner/batch_block_jacobi.hpp
+++ dpcpp/preconditioner/batch_block_jacobi.hpp
@@ -129,9 +129,12 @@ public:
                 sum += block_val * r[dense_block_col + idx_start];
             }
 
-            // reduction (it does not support complex<half/bfloat16>)
+            // reduction (it does not support complex<half/bfloat16> and
+            // bfloat16)
             if constexpr (std::is_same_v<value_type,
-                                         gko::complex<device_type<float16>>>) {
+                                         gko::complex<device_type<float16>>> ||
+                          std::is_same_v<value_type,
+                                         device_type<gko::bfloat16>>) {
                 for (int i = sg_size / 2; i > 0; i /= 2) {
                     sum += sycl::shift_group_left(sg, sum, i);
                 }
