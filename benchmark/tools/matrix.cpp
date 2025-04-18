767d76a7b35c1ea5467fc0a61c42f669c67d766c
Tobias Ribizel <mail@ribizel.de>
2025-04-18 15:06:06 +0200

    add reordering to matrix preprocessing tool

--- benchmark/tools/matrix.cpp
+++ benchmark/tools/matrix.cpp
@@ -10,7 +10,11 @@
 #include <ginkgo/core/base/math.hpp>
 #include <ginkgo/core/base/mtx_io.hpp>
 #include <ginkgo/core/matrix/csr.hpp>
+#include <ginkgo/core/reorder/amd.hpp>
+#include <ginkgo/core/reorder/nested_dissection.hpp>
+#include <ginkgo/core/reorder/rcm.hpp>
 
+#include "core/components/fill_array_kernels.hpp"
 #include "core/factorization/elimination_forest_kernels.hpp"
 #include "core/utils/matrix_utils.hpp"
 
@@ -58,6 +62,7 @@ int main(int argc, char** argv)
     data.sort_row_major();
     for (int argi = binary ? 2 : 1; argi < argc; argi++) {
         std::string arg{argv[argi]};
+        const std::string reorder_str{"reorder:"};
         if (arg == "lower-triangular") {
             gko::utils::make_lower_triangular(data);
         } else if (arg == "upper-triangular") {
@@ -92,6 +97,9 @@ int main(int argc, char** argv)
             gko::kernels::reference::elimination_forest::compute_skeleton_tree(
                 exec, mtx->get_const_row_ptrs(), mtx->get_const_col_idxs(),
                 mtx->get_size()[0], tree->get_row_ptrs(), tree->get_col_idxs());
+            gko::kernels::reference::components::fill_array(
+                exec, mtx->get_values(), mtx->get_num_stored_elements(),
+                gko::one<value_type>());
             tree->write(data);
             std::unique_ptr<gko::factorization::elimination_forest<gko::int64>>
                 etree, etree2;
@@ -103,6 +111,35 @@ int main(int argc, char** argv)
                     etree2->parents.get_const_data())) {
                 std::cerr << "failed\n";
             }
+        } else if (arg.size() > reorder_str.size() &&
+                   std::equal(reorder_str.begin(), reorder_str.end(),
+                              arg.begin())) {
+            std::string op{arg.begin() + reorder_str.size(), arg.end()};
+            std::unique_ptr<gko::matrix::Permutation<gko::int64>> perm;
+            auto exec = gko::ReferenceExecutor::create();
+            auto mtx = gko::share(
+                gko::matrix::Csr<value_type, gko::int64>::create(exec));
+            mtx->read(data);
+            if (op == "amd") {
+                perm = gko::experimental::reorder::Amd<gko::int64>::build()
+                           .on(exec)
+                           ->generate(mtx);
+            } else if (op == "rcm") {
+                perm = gko::experimental::reorder::Rcm<gko::int64>::build()
+                           .on(exec)
+                           ->generate(mtx);
+#if GKO_HAVE_METIS
+            } else if (op == "nd") {
+                perm = gko::experimental::reorder::NestedDissection<
+                           value_type, gko::int64>::build()
+                           .on(exec)
+                           ->generate(mtx);
+#endif
+            } else {
+                std::cerr << "Unknown reordering algorithm " << op << std::endl;
+                return 1;
+            }
+            mtx->permute(perm)->write(data);
         } else {
             std::cerr << "Unknown operation " << arg << std::endl;
             return 1;
