41506bec0113c84d173f174d185cfa6aef98d017
Tobias Ribizel <mail@ribizel.de>
2025-04-19 23:22:37 +0200

    extract combined_workspace into header

---
+++ core/components/combined_workspace.hpp
@@ -0,0 +1,58 @@
+// SPDX-FileCopyrightText: 2017 - 2025 The Ginkgo authors
+//
+// SPDX-License-Identifier: BSD-3-Clause
+
+#ifndef GKO_CORE_COMPONENTS_COMBINED_WORKSPACE_HPP_
+#define GKO_CORE_COMPONENTS_COMBINED_WORKSPACE_HPP_
+
+
+#include <memory>
+#include <numeric>
+#include <vector>
+
+#include <ginkgo/core/base/array.hpp>
+#include <ginkgo/core/base/executor.hpp>
+
+
+namespace gko {
+
+
+template <typename IndexType>
+struct combined_workspace {
+    explicit combined_workspace(std::shared_ptr<const Executor> exec,
+                                std::vector<size_type> sizes)
+        : offsets{std::move(sizes)}, workspace{exec}
+    {
+        offsets.insert(offsets.begin(), 0);
+        std::partial_sum(offsets.begin(), offsets.end(), offsets.begin());
+        workspace.resize_and_reset(offsets.back());
+    }
+
+    static size_type get_total_size(std::vector<size_type> sizes)
+    {
+        return std::accumulate(sizes.begin(), sizes.end(), size_type{});
+    }
+
+    IndexType* get_pointer(int i)
+    {
+        return workspace.get_data() + offsets.at(i);
+    }
+    size_type get_size(int i) const
+    {
+        return offsets.at(i + 1) - offsets.at(i);
+    }
+    array<IndexType> get_view(int i)
+    {
+        return make_array_view(workspace.get_executor(), get_size(i),
+                               get_pointer(i));
+    }
+
+    array<IndexType> workspace;
+    std::vector<size_type> offsets;
+};
+
+
+}  // namespace gko
+
+
+#endif  // GKO_CORE_COMPONENTS_COMBINED_WORKSPACE_HPP_
