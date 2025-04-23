3b9b31b7b1a19eca188cc2c70e52e9ec86e4fba2
Atell Krasnopolski <delta_atell@protonmail.com>
2025-04-23 14:01:15 +0200

    Add PIPECG step kernels

--- common/unified/solver/pipe_cg_kernels.cpp
+++ common/unified/solver/pipe_cg_kernels.cpp
@@ -112,7 +112,28 @@ void step_1(std::shared_ptr<const DefaultExecutor> exec,
             const matrix::Dense<ValueType>* beta,
             const array<stopping_status>* stop_status)
 {
-    GKO_NOT_IMPLEMENTED;
+    // tmp = rho / beta
+    // x = x + tmp * p
+    // r = r - tmp * q
+    // z = z - tmp * f
+    // w = w - tmp * g
+    run_kernel_solver(
+        exec,
+        [] GKO_KERNEL(auto row, auto col, auto x, auto r, auto z, auto w,
+                      auto p, auto q, auto f, auto g, auto rho, auto beta,
+                      auto stop) {
+            if (!stop[col].has_stopped()) {
+                auto tmp = safe_divide(rho[col], beta[col]);
+                x(row, col) += tmp * p(row, col);
+                r(row, col) -= tmp * q(row, col);
+                z(row, col) -= tmp * f(row, col);
+                w(row, col) -= tmp * g(row, col);
+            }
+        },
+        x->get_size(), r->get_stride(), x, default_stride(r), default_stride(z),
+        default_stride(w), default_stride(p), default_stride(q),
+        default_stride(f), default_stride(g), row_vector(rho), row_vector(beta),
+        *stop_status);
 }
 
 GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE(GKO_DECLARE_PIPE_CG_STEP_1_KERNEL);
@@ -131,7 +152,37 @@ void step_2(std::shared_ptr<const DefaultExecutor> exec,
             const matrix::Dense<ValueType>* delta,
             const array<stopping_status>* stop_status)
 {
-    GKO_NOT_IMPLEMENTED;
+    // tmp = rho / prev_rho
+    // beta = delta - |tmp|^2 * beta
+    // p = z + tmp * p
+    // q = w + tmp * q
+    // f = m + tmp * f
+    // g = n + tmp * g
+    run_kernel_solver(
+        exec,
+        [] GKO_KERNEL(auto row, auto col, auto beta, auto p, auto q, auto f,
+                      auto g, auto z, auto w, auto m, auto n, auto prev_rho,
+                      auto rho, auto delta, auto stop) {
+            if (!stop[col].has_stopped()) {
+                auto tmp = safe_divide(rho[col], prev_rho[col]);
+                if (row == 0) {
+                    auto abs_tmp = abs(tmp);
+                    beta[col] = delta[col] - abs_tmp * abs_tmp * beta[col];
+                    if (is_zero(beta[col])) {
+                        beta[col] = delta[col];
+                    }
+                }
+                p(row, col) = z(row, col) + tmp * p(row, col);
+                q(row, col) = w(row, col) + tmp * q(row, col);
+                f(row, col) = m(row, col) + tmp * f(row, col);
+                g(row, col) = n(row, col) + tmp * g(row, col);
+            }
+        },
+        p->get_size(), p->get_stride(), row_vector(beta), default_stride(p),
+        default_stride(q), default_stride(f), default_stride(g),
+        default_stride(z), default_stride(w), default_stride(m),
+        default_stride(n), row_vector(prev_rho), row_vector(rho),
+        row_vector(delta), *stop_status);
 }
 
 GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE(GKO_DECLARE_PIPE_CG_STEP_2_KERNEL);
