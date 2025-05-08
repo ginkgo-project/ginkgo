fc7c03e7a5daf291eaefd5a46be5337b1a1915e4
Yu-Hsiang M. Tsai <yhmtsai@gmail.com>
2025-05-08 13:42:23 +0200

    ekeleton_tree hangs currently

--- dpcpp/factorization/elimination_forest_kernels.dp.cpp
+++ dpcpp/factorization/elimination_forest_kernels.dp.cpp
@@ -16,7 +16,11 @@
 #include "core/components/fill_array_kernels.hpp"
 #include "core/components/format_conversion_kernels.hpp"
 #include "core/factorization/elimination_forest_kernels.hpp"
+#include "dpcpp/base/dpct.hpp"
+#include "dpcpp/base/helper.hpp"
 #include "dpcpp/base/onedpl.hpp"
+#include "dpcpp/components/memory.dp.hpp"
+#include "dpcpp/components/thread_ids.dp.hpp"
 
 
 namespace gko {
@@ -25,11 +29,293 @@ namespace dpcpp {
 namespace elimination_forest {
 
 
+constexpr int default_block_size = 512;
+
+
+namespace kernel {
+
+
+template <typename IndexType>
+void mst_initialize_worklist(const IndexType* __restrict__ rows,
+                             const IndexType* __restrict__ cols, IndexType size,
+                             IndexType* __restrict__ worklist_sources,
+                             IndexType* __restrict__ worklist_targets,
+                             IndexType* __restrict__ worklist_edge_ids,
+                             IndexType* __restrict__ worklist_counter,
+                             sycl::nd_item<3> item_ct1)
+{
+    const auto i = thread::get_thread_id_flat<IndexType>(item_ct1);
+    if (i >= size) {
+        return;
+    }
+    const auto row = rows[i];
+    const auto col = cols[i];
+    if (col < row) {
+        const auto out_i = atomic_add_relaxed(worklist_counter, 1);
+        worklist_sources[out_i] = row;
+        worklist_targets[out_i] = col;
+        worklist_edge_ids[out_i] = i;
+    }
+}
+
+GKO_ENABLE_DEFAULT_HOST(mst_initialize_worklist, mst_initialize_worklist);
+
+
+template <typename IndexType>
+__dpct_inline__ IndexType mst_find(const IndexType* parents, IndexType node)
+{
+    auto parent = parents[node];
+    while (parent != node) {
+        node = parent;
+        parent = parents[node];
+    };
+    return parent;
+}
+
+
+template <typename IndexType>
+__dpct_inline__ IndexType mst_find_relaxed(const IndexType* parents,
+                                           IndexType node)
+{
+    auto parent = load_relaxed_local(parents + node);
+    while (parent != node) {
+        node = parent;
+        parent = load_relaxed_local(parents + node);
+    };
+    return parent;
+}
+
+
+template <typename IndexType>
+__dpct_inline__ void guarded_atomic_min(IndexType* ptr, IndexType value)
+{
+    // only execute the atomic if we know that it might have an effect
+    if (load_relaxed_local(ptr) > value) {
+        atomic_min_relaxed(ptr, value);
+    }
+    sycl::atomic_fence(sycl::memory_order::acq_rel, sycl::memory_scope::device);
+}
+
+
+template <typename IndexType>
+void mst_find_minimum(const IndexType* __restrict__ in_sources,
+                      const IndexType* __restrict__ in_targets,
+                      const IndexType* __restrict__ in_edge_ids, IndexType size,
+                      const IndexType* __restrict__ parents,
+                      IndexType* __restrict__ min_edge,
+                      IndexType* __restrict__ worklist_sources,
+                      IndexType* __restrict__ worklist_targets,
+                      IndexType* __restrict__ worklist_edge_ids,
+                      IndexType* __restrict__ worklist_counter,
+                      sycl::nd_item<3> item_ct1)
+{
+    const auto i = thread::get_thread_id_flat<IndexType>(item_ct1);
+    if (i >= size) {
+        return;
+    }
+    const auto source = in_sources[i];
+    const auto target = in_targets[i];
+    const auto edge_id = in_edge_ids[i];
+    const auto source_rep = mst_find(parents, source);
+    const auto target_rep = mst_find(parents, target);
+    if (source_rep != target_rep) {
+        const auto out_i = atomic_add_relaxed(worklist_counter, 1);
+        worklist_sources[out_i] = source_rep;
+        worklist_targets[out_i] = target_rep;
+        worklist_edge_ids[out_i] = edge_id;
+        guarded_atomic_min(min_edge + source_rep, edge_id);
+        guarded_atomic_min(min_edge + target_rep, edge_id);
+    }
+}
+
+GKO_ENABLE_DEFAULT_HOST(mst_find_minimum, mst_find_minimum);
+
+
+template <typename IndexType>
+void mst_join_edges(const IndexType* __restrict__ in_sources,
+                    const IndexType* __restrict__ in_targets,
+                    const IndexType* __restrict__ in_edge_ids, IndexType size,
+                    IndexType* __restrict__ parents,
+                    const IndexType* __restrict__ min_edge,
+                    const IndexType* __restrict__ edge_sources,
+                    const IndexType* __restrict__ edge_targets,
+                    IndexType* __restrict__ out_sources,
+                    IndexType* __restrict__ out_targets,
+                    IndexType* __restrict__ out_counter,
+                    sycl::nd_item<3> item_ct1)
+{
+    const auto i = thread::get_thread_id_flat<IndexType>(item_ct1);
+    if (i >= size) {
+        return;
+    }
+    const auto source = in_sources[i];
+    const auto target = in_targets[i];
+    const auto edge_id = in_edge_ids[i];
+    if (min_edge[source] == edge_id || min_edge[target] == edge_id) {
+        // join source and sink
+        const auto source_rep = mst_find_relaxed(parents, source);
+        const auto target_rep = mst_find_relaxed(parents, target);
+        assert(source_rep != target_rep);
+        const auto new_rep = min(source_rep, target_rep);
+        auto old_rep = max(source_rep, target_rep);
+        bool repeat = false;
+        do {
+            repeat = false;
+            auto old_parent =
+                atomic_cas_relaxed(parents + old_rep, old_rep, new_rep);
+            // if this fails, the parent of old_rep changed recently, so we need
+            // to try again by updating the parent's parent (hopefully its rep)
+            if (old_parent != old_rep) {
+                old_rep = old_parent;
+                repeat = true;
+            }
+        } while (repeat);
+        const auto out_i = atomic_add_relaxed(out_counter, 1);
+        out_sources[out_i] = edge_sources[edge_id];
+        out_targets[out_i] = edge_targets[edge_id];
+    }
+}
+
+GKO_ENABLE_DEFAULT_HOST(mst_join_edges, mst_join_edges);
+
+
+template <typename IndexType>
+void mst_reset_min_edges(const IndexType* __restrict__ in_sources,
+                         const IndexType* __restrict__ in_targets,
+                         IndexType size, IndexType* __restrict__ min_edge,
+                         sycl::nd_item<3> item_ct1)
+{
+    constexpr auto sentinel = std::numeric_limits<IndexType>::max();
+    const auto i = thread::get_thread_id_flat<IndexType>(item_ct1);
+    if (i >= size) {
+        return;
+    }
+    const auto source = in_sources[i];
+    const auto target = in_targets[i];
+    // we could write the values non-atomically, but this makes race checkers
+    // happier without a performance penalty (hopefully, thanks to _local)
+    store_relaxed_local(min_edge + source, sentinel);
+    store_relaxed_local(min_edge + target, sentinel);
+}
+
+GKO_ENABLE_DEFAULT_HOST(mst_reset_min_edges, mst_reset_min_edges);
+
+
+}  // namespace kernel
+
+
 template <typename IndexType>
 void compute_skeleton_tree(std::shared_ptr<const DefaultExecutor> exec,
                            const IndexType* row_ptrs, const IndexType* cols,
                            size_type size, IndexType* out_row_ptrs,
-                           IndexType* out_cols) GKO_NOT_IMPLEMENTED;
+                           IndexType* out_cols)
+{
+    // This is a minimum spanning tree algorithm implementation based on
+    // A. Fallin, A. Gonzalez, J. Seo, and M. Burtscher,
+    // "A High-Performance MST Implementation for GPUs,”
+    // doi: 10.1145/3581784.3607093
+    // we don't filter heavy edges since the heaviest edges are necessary to
+    // reach the last node and we don't need to sort since the COO format
+    // already sorts by row index.
+    const auto policy = onedpl_policy(exec);
+    const auto nnz = exec->copy_val_to_host(row_ptrs + size);
+    // convert edges to COO representation
+    // the edge list is sorted, since we only consider edges where row > col,
+    // and the row array (= weights) is sorted coming from row_ptrs
+    array<IndexType> row_array{exec, static_cast<size_type>(nnz)};
+    const auto rows = row_array.get_data();
+    components::convert_ptrs_to_idxs(exec, row_ptrs, size, rows);
+    // we assume the matrix is symmetric, so we can remove every second edge
+    // also round up the worklist size for equal cache alignment between fields
+    const auto worklist_size =
+        ceildiv(nnz, config::warp_size * 2) * config::warp_size;
+    // create 2 worklists consisting of (start, end, edge_id)
+    array<IndexType> worklist{exec, static_cast<size_type>(worklist_size * 6)};
+    auto wl1_source = worklist.get_data();
+    auto wl1_target = wl1_source + worklist_size;
+    auto wl1_edge_id = wl1_target + worklist_size;
+    auto wl2_source = wl1_source + 3 * worklist_size;
+    auto wl2_target = wl1_target + 3 * worklist_size;
+    auto wl2_edge_id = wl1_edge_id + 3 * worklist_size;
+    // atomic counters for worklists and output edge list
+    array<IndexType> counters{exec, 3};
+    auto wl1_counter = counters.get_data();
+    auto wl2_counter = wl1_counter + 1;
+    auto output_counter = wl2_counter + 1;
+    components::fill_array(exec, counters.get_data(), counters.get_size(),
+                           IndexType{});
+    // helpers for interacting with worklists
+    const auto clear_wl1 = [&] {
+        IndexType value{};
+        exec->copy_from(exec->get_master(), 1, &value, wl1_counter);
+    };
+    const auto clear_wl2 = [&] {
+        IndexType value{};
+        exec->copy_from(exec->get_master(), 1, &value, wl2_counter);
+    };
+    const auto get_wl1_size = [&] {
+        return exec->copy_val_to_host(wl1_counter);
+    };
+    const auto swap_wl1_wl2 = [&] {
+        std::swap(wl1_source, wl2_source);
+        std::swap(wl1_target, wl2_target);
+        std::swap(wl1_edge_id, wl2_edge_id);
+        std::swap(wl1_counter, wl2_counter);
+    };
+    // initialize every node to a singleton set
+    array<IndexType> parent_array{exec, size};
+    const auto parents = parent_array.get_data();
+    components::fill_seq_array(exec, parents, size);
+    // array storing the minimal edge adjacent to each node
+    array<IndexType> min_edge_array{exec, size};
+    const auto min_edges = min_edge_array.get_data();
+    constexpr auto min_edge_sentinel = std::numeric_limits<IndexType>::max();
+    components::fill_array(exec, min_edges, size, min_edge_sentinel);
+    // output row array, to be used in conjunction with out_cols in COO storage
+    array<IndexType> out_row_array{exec, size};
+    const auto out_rows = out_row_array.get_data();
+    // initialize worklist1 with forward edges
+    {
+        const auto num_blocks = ceildiv(nnz, default_block_size);
+        kernel::mst_initialize_worklist(
+            num_blocks, default_block_size, 0, exec->get_queue(), rows, cols,
+            nnz, wl1_source, wl1_target, wl1_edge_id, wl1_counter);
+    }
+    auto wl1_size = get_wl1_size();
+    while (wl1_size > 0) {
+        clear_wl2();
+        // attach each node to its smallest adjacent non-cycle edge
+        {
+            const auto num_blocks = ceildiv(wl1_size, default_block_size);
+            kernel::mst_find_minimum(
+                num_blocks, default_block_size, 0, exec->get_queue(),
+                wl1_source, wl1_target, wl1_edge_id, wl1_size, parents,
+                min_edges, wl2_source, wl2_target, wl2_edge_id, wl2_counter);
+        }
+        clear_wl1();
+        swap_wl1_wl2();
+        wl1_size = get_wl1_size();
+        if (wl1_size > 0) {
+            // join minimal edges
+            const auto num_blocks = ceildiv(wl1_size, default_block_size);
+            kernel::mst_join_edges(
+                num_blocks, default_block_size, 0, exec->get_queue(),
+                wl1_source, wl1_target, wl1_edge_id, wl1_size, parents,
+                min_edges, rows, cols, out_rows, out_cols, output_counter);
+            kernel::mst_reset_min_edges(num_blocks, default_block_size, 0,
+                                        exec->get_queue(), wl1_source,
+                                        wl1_target, wl1_size, min_edges);
+        }
+    }
+    const auto num_mst_edges = exec->copy_val_to_host(output_counter);
+    // TODO: check the performance of sorting both or twice.
+    oneapi::dpl::sort_by_key(policy, out_cols, out_cols + num_mst_edges,
+                             out_rows);
+    oneapi::dpl::sort_by_key(policy, out_rows, out_rows + num_mst_edges,
+                             out_cols);
+    components::convert_idxs_to_ptrs(exec, out_rows, num_mst_edges, size,
+                                     out_row_ptrs);
+}
 
 GKO_INSTANTIATE_FOR_EACH_INDEX_TYPE(
     GKO_DECLARE_ELIMINATION_FOREST_COMPUTE_SKELETON_TREE);
