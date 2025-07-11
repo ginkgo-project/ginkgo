8b345047c2b4309c7e55164e2b8f55243736204e
Yu-Hsiang M. Tsai <yhmtsai@gmail.com>
2025-07-11 15:56:50 +0200

    move implementation back to header because the backend need them

--- core/base/reused_thread.cpp
+++ core/base/reused_thread.cpp
@@ -5,36 +5,4 @@
 #include <ginkgo/core/base/reused_thread.hpp>
 
 
-namespace gko {
-
-reused_thread::reused_thread()
-    : stop_(false), worker_([&] {
-          while (true) {
-              std::function<void()> task;
-              {
-                  // get job in the queue
-                  std::unique_lock<std::mutex> lock(this->queue_mutex_);
-                  this->condition_.wait(lock, [this] {
-                      return this->stop_ || !this->tasks_.empty();
-                  });
-                  if (this->stop_ && this->tasks_.empty()) {
-                      return;
-                  }
-                  task = std::move(this->tasks_.front());
-                  this->tasks_.pop();
-              }
-              task();
-          }
-      })
-{}
-
-
-reused_thread::~reused_thread()
-{
-    this->stop_ = true;
-    this->condition_.notify_all();
-    this->worker_.join();
-}
-
-
-}  // namespace gko
+namespace gko {}  // namespace gko
--- include/ginkgo/core/base/reused_thread.hpp
+++ include/ginkgo/core/base/reused_thread.hpp
@@ -20,9 +20,33 @@ namespace gko {
 
 class reused_thread {
 public:
-    reused_thread();
+    reused_thread()
+        : stop_(false), worker_([&] {
+              while (true) {
+                  std::function<void()> task;
+                  {
+                      // get job in the queue
+                      std::unique_lock<std::mutex> lock(this->queue_mutex_);
+                      this->condition_.wait(lock, [this] {
+                          return this->stop_ || !this->tasks_.empty();
+                      });
+                      if (this->stop_ && this->tasks_.empty()) {
+                          return;
+                      }
+                      task = std::move(this->tasks_.front());
+                      this->tasks_.pop();
+                  }
+                  task();
+              }
+          })
+    {}
 
-    ~reused_thread();
+    ~reused_thread()
+    {
+        this->stop_ = true;
+        this->condition_.notify_all();
+        this->worker_.join();
+    }
 
 
     template <typename Func, typename... Args>
