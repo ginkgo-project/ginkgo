b8d0758f7401af3d9dc2b5faf3ae12f0c0aae2c2
keshvtuteja <keshvituteja@gmail.com>
2025-08-06 16:32:23 +0200

    Added 1D convolution for stride=1

--- reference/matrix/conv_kernels.cpp
+++ reference/matrix/conv_kernels.cpp
@@ -24,10 +24,25 @@ namespace conv {
 
 template <typename ValueType>
 void conv(std::shared_ptr<const DefaultExecutor> exec,
-          const matrix::Conv<ValueType>* kernel,
-          const matrix::Dense<ValueType>* b, matrix::Dense<ValueType>* x)
+          const array<ValueType>& kernel, const matrix::Dense<ValueType>* b,
+          matrix::Dense<ValueType>* x)
 {
-    GKO_NOT_IMPLEMENTED;
+    const auto b_size = b->get_size();                 // (N, 1)
+    const auto x_size = x->get_size();                 // (N + K - 1, 1)
+    const auto kernel_size = kernel.get_size();        // K
+    const auto* kernel_ptr = kernel.get_const_data();  // pointer to kernel data
+
+    for (int i = 0; i < x_size[0]; ++i) {
+        ValueType sum = zero<ValueType>();
+        for (int j = 0; j < kernel_size; ++j) {
+            int b_idx = i - j;
+            if (b_idx >= 0 && b_idx < b_size[0]) {
+                sum +=
+                    kernel_ptr[j] * b->at(b_idx, 0);  // direct pointer access
+            }
+        }
+        x->at(i, 0) = sum;
+    }
 }
 
 GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE(GKO_DECLARE_CONV_KERNEL);
