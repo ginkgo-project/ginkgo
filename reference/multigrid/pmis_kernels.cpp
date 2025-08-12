ed1a9207a570b6d0c99cce8012c382e912816a05
Yu-Hsiang M. Tsai <yhmtsai@gmail.com>
2025-08-12 11:32:58 +0200

    initialize pmis classify part

---
+++ reference/multigrid/pmis_kernels.cpp
@@ -0,0 +1,90 @@
+// SPDX-FileCopyrightText: 2025 The Ginkgo authors
+//
+// SPDX-License-Identifier: BSD-3-Clause
+
+#include "core/multigrid/pmis_kernels.hpp"
+
+#include <algorithm>
+#include <memory>
+#include <tuple>
+
+#include <ginkgo/core/base/exception_helpers.hpp>
+#include <ginkgo/core/base/math.hpp>
+#include <ginkgo/core/base/types.hpp>
+#include <ginkgo/core/matrix/csr.hpp>
+#include <ginkgo/core/matrix/sparsity_csr.hpp>
+
+
+namespace gko {
+namespace kernels {
+namespace reference {
+/**
+ * @brief The PMIS solver namespace.
+ *
+ * @ingroup pmis
+ */
+namespace pmis {
+
+
+template <typename ValueType, typename IndexType>
+void compute_strong_dep_row(std::shared_ptr<const DefaultExecutor> exec,
+                            const matrix::Csr<ValueType, IndexType>* csr,
+                            remove_complex<ValueType> strength_threshold,
+                            IndexType* sparsity_rows)
+{
+    GKO_NOT_IMPLEMENTED;
+}
+
+GKO_INSTANTIATE_FOR_EACH_VALUE_AND_INDEX_TYPE(
+    GKO_DECLARE_PMIS_COMPUTE_STRONG_DEP_ROW_KERNEL);
+
+
+template <typename ValueType, typename IndexType>
+void compute_strong_dep(std::shared_ptr<const DefaultExecutor> exec,
+                        const matrix::Csr<ValueType, IndexType>* csr,
+                        remove_complex<ValueType> strength_threshold,
+                        matrix::SparsityCsr<ValueType, IndexType>* strong_dep)
+{
+    GKO_NOT_IMPLEMENTED;
+}
+
+GKO_INSTANTIATE_FOR_EACH_VALUE_AND_INDEX_TYPE(
+    GKO_DECLARE_PMIS_COMPUTE_STRONG_DEP_KERNEL);
+
+
+template <typename ValueType, typename IndexType>
+void initialize_weight_and_status(
+    std::shared_ptr<const DefaultExecutor> exec,
+    const matrix::SparsityCsr<ValueType, IndexType>* strong_dep,
+    remove_complex<ValueType>* weight, int* status)
+{
+    GKO_NOT_IMPLEMENTED;
+}
+
+GKO_INSTANTIATE_FOR_EACH_VALUE_AND_INDEX_TYPE(
+    GKO_DECLARE_PMIS_INITIALIZE_WEIGHT_AND_STATUS_KERNEL);
+
+
+template <typename ValueType, typename IndexType>
+void classify(std::shared_ptr<const DefaultExecutor> exec,
+              const remove_complex<ValueType>* weight,
+              const matrix::SparsityCsr<ValueType, IndexType>* strong_dep,
+              int* status)
+{
+    GKO_NOT_IMPLEMENTED;
+}
+
+GKO_INSTANTIATE_FOR_EACH_VALUE_AND_INDEX_TYPE(GKO_DECLARE_PMIS_CLASSIFY_KERNEL);
+
+
+void count(std::shared_ptr<const DefaultExecutor> exec,
+           const array<int>& status, size_type* num)
+{
+    GKO_NOT_IMPLEMENTED;
+}
+
+
+}  // namespace pmis
+}  // namespace reference
+}  // namespace kernels
+}  // namespace gko
