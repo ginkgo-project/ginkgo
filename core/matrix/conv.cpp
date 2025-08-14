63be92717e5516c607b838909e5d7d32bd01c774
keshvtuteja <keshvituteja@gmail.com>
2025-08-14 12:51:04 +0200

    Added stride=1 and padding=2 in conv_kernels.cpp and example

--- core/matrix/conv.cpp
+++ core/matrix/conv.cpp
@@ -57,9 +57,11 @@ void Conv<ValueType>::validate_application_parameters(const LinOp* b,
     const auto kernel_len = kernel_.get_size();
 
     if (x_rows != b_rows + kernel_len - 1) {
-        throw DimensionMismatch(__FILE__, __LINE__, __func__, "x", x_rows, 1,
-                                "b + kernel - 1", b_rows + kernel_len - 1, 1,
-                                "x must have size = b + kernel - 1");
+        throw DimensionMismatch(
+            __FILE__, __LINE__, __func__, "x", x_rows, 1,
+            "(b + 2*padding - kernel)/stride + 1",
+            (b_rows + 2 * 2 - kernel_len) / 1 + 1, 1,
+            "x must have size = (b + 2*padding - kernel)/stride + 1");
     }
 
 
