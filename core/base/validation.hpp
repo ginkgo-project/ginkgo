c95378281756dcd078a6c45c162318b3efbcc207
ge94caf <ge94caf@tum.de>
2025-09-03 16:14:38 +0800

    Resolve some naming convention and utilize existing function. Co-authored-by: Yuhsiang M. Tsai <yhmtsai@gmail.com>

--- core/base/validation.hpp
+++ core/base/validation.hpp
@@ -10,6 +10,7 @@
 #include <unordered_set>
 
 #include <ginkgo/core/base/array.hpp>
+#include <ginkgo/core/base/math.hpp>
 #include <ginkgo/core/base/temporary_clone.hpp>
 
 
@@ -24,30 +25,17 @@ namespace validation {
     }
 
 
-template <typename ValueType>
-bool is_finite_scalar(const ValueType& value)
-{
-    return std::isfinite(static_cast<double>(value));
-}
-
-template <typename ValueType>
-bool is_finite_scalar(const std::complex<ValueType>& value)
-{
-    return is_finite_scalar(value.real()) && is_finite_scalar(value.imag());
-}
-
-
 template <typename IndexType>
-bool is_sorted(const gko::array<IndexType>& row_ptrs)
+bool is_sorted(const gko::array<IndexType>& idxs_array)
 {
-    const auto host_row_ptrs = row_ptrs.copy_to_host();
-    for (size_t i = 0; i + 1 < host_row_ptrs.size(); ++i) {
-        if (host_row_ptrs[i] > host_row_ptrs[i + 1]) {
+    const auto host_idxs_array = idxs_array.copy_to_host();
+    for (size_t i = 0; i + 1 < host_idxs_array.size(); ++i) {
+        if (host_idxs_array[i] > host_idxs_array[i + 1]) {
             std::printf(
                 "row_ptrs not sorted at row %zu : row_ptrs[%zu] = %lld > "
                 "row_ptrs[%zu] = %lld\n",
-                i, i, static_cast<long long>(host_row_ptrs[i]), i + 1,
-                static_cast<long long>(host_row_ptrs[i + 1]));
+                i, i, static_cast<long long>(host_idxs_array[i]), i + 1,
+                static_cast<long long>(host_idxs_array[i + 1]));
             return false;
         }
     }
@@ -56,38 +44,38 @@ bool is_sorted(const gko::array<IndexType>& row_ptrs)
 
 
 template <typename IndexType>
-bool is_within_bounds(const gko::array<IndexType>& col_idxs,
+bool is_within_bounds(const gko::array<IndexType>& idxs_array,
                       const IndexType upper_bound)
 {
-    const auto host_col_idxs = col_idxs.copy_to_host();
+    const auto host_idxs_array = idxs_array.copy_to_host();
     auto min_pos = 0;
     auto max_pos = 0;
 
-    for (size_t i = 1; i < host_col_idxs.size(); ++i) {
-        if (host_col_idxs[i] < host_col_idxs[min_pos]) {
+    for (size_t i = 1; i < host_idxs_array.size(); ++i) {
+        if (host_idxs_array[i] < host_idxs_array[min_pos]) {
             min_pos = i;
         }
-        if (host_col_idxs[i] > host_col_idxs[max_pos]) {
+        if (host_idxs_array[i] > host_idxs_array[max_pos]) {
             max_pos = i;
         }
     }
-    if (host_col_idxs[min_pos] < 0) {
+    if (host_idxs_array[min_pos] < 0) {
         std::printf("position %d is out of bounds with negative value %lld\n",
-                    min_pos, static_cast<long long>(host_col_idxs[min_pos]));
+                    min_pos, static_cast<long long>(host_idxs_array[min_pos]));
         return false;
     }
-    if (host_col_idxs[max_pos] >= upper_bound) {
+    if (host_idxs_array[max_pos] >= upper_bound) {
         std::printf(
             "position %d is out of bounds with value %lld larger than upper "
             "bound %lld\n",
-            max_pos, static_cast<long long>(host_col_idxs[max_pos]),
+            max_pos, static_cast<long long>(host_idxs_array[max_pos]),
             static_cast<long long>(upper_bound));
         return false;
-    } else if (host_col_idxs[max_pos] == upper_bound) {
+    } else if (host_idxs_array[max_pos] == upper_bound) {
         std::printf(
             "position %d is out of bounds with value %lld equal to upper bound "
             "%lld\n",
-            max_pos, static_cast<long long>(host_col_idxs[max_pos]),
+            max_pos, static_cast<long long>(host_idxs_array[max_pos]),
             static_cast<long long>(upper_bound));
         return false;
     }
@@ -97,13 +85,11 @@ bool is_within_bounds(const gko::array<IndexType>& col_idxs,
 
 
 template <typename ValueType>
-bool is_finite(const gko::array<ValueType>& values)
+bool array_is_finite(const gko::array<ValueType>& values)
 {
     const auto host_values = values.copy_to_host();
     for (size_t i = 0; i < host_values.size(); ++i) {
-        if (!is_finite_scalar(host_values[i])) {
-            // throw std::invalid_argument("matrix must contain only finite
-            // values");
+        if (!is_finite(host_values[i])) {
             return false;
         }
     }
@@ -112,8 +98,8 @@ bool is_finite(const gko::array<ValueType>& values)
 
 
 template <typename IndexType>
-bool has_unique_columns(const gko::array<IndexType>& row_ptrs,
-                        const gko::array<IndexType>& col_idxs)
+bool has_unique_idxs(const gko::array<IndexType>& row_ptrs,
+                     const gko::array<IndexType>& col_idxs)
 {
     const auto host_row_ptrs = row_ptrs.copy_to_host();
     const auto host_col_idxs = col_idxs.copy_to_host();
