9ceddb4289217d3ca3687c9369bc039d9cc32139
keshvtuteja <keshvituteja@gmail.com>
2025-09-22 08:38:12 +0200

    Definition for 2D dense convolution for different backends

---
+++ omp/matrix/conv_kernels.cpp
@@ -0,0 +1,31 @@
+// SPDX-FileCopyrightText: 2025 The Ginkgo authors
+//
+// SPDX-License-Identifier: BSD-3-Clause
+
+#include "core/matrix/conv_kernels.hpp"
+
+#include <ginkgo/core/base/exception_helpers.hpp>
+#include <ginkgo/core/base/math.hpp>
+#include <ginkgo/core/matrix/dense.hpp>
+
+#include "core/base/allocator.hpp"
+
+namespace gko {
+namespace kernels {
+namespace omp {
+namespace conv2d {
+
+template <typename ValueType>
+void conv2d(std::shared_ptr<const DefaultExecutor> exec,
+            const matrix::Dense<ValueType>* kernel,
+            const matrix::Dense<ValueType>* b, matrix::Dense<ValueType>* x)
+{
+    GKO_NOT_IMPLEMENTED;
+}
+
+GKO_INSTANTIATE_FOR_EACH_VALUE_TYPE(GKO_DECLARE_CONV2D_KERNEL);
+
+}  // namespace conv2d
+}  // namespace omp
+}  // namespace kernels
+}  // namespace gko
