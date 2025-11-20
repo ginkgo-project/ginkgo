7c8f3acd12abe7168451f8cb88e169a17d3857cb
Tobias Ribizel <mail@ribizel.de>
2025-11-20 13:44:33 +0100

    extract iteration class

--- core/stop/iteration.cpp
+++ core/stop/iteration.cpp
@@ -4,7 +4,7 @@
 
 #include "ginkgo/core/stop/iteration.hpp"
 
-#include <ginkgo/core/base/abstract_factory.hpp>
+#include "core/stop/iteration.hpp"
 
 
 namespace gko {
@@ -28,61 +28,6 @@ deferred_factory_parameter<Iteration::Factory> max_iters(size_type count)
 {
     return Iteration::build().with_max_iters(count);
 }
-
-
-class MinIterationWrapper
-    : public EnablePolymorphicObject<MinIterationWrapper, Criterion> {
-    friend class EnablePolymorphicObject<MinIterationWrapper, Criterion>;
-
-public:
-    GKO_CREATE_FACTORY_PARAMETERS(parameters, Factory)
-    {
-        /**
-         * Minimum number of iterations, after which we check the inner
-         * criterion
-         */
-        size_type min_iters{0};
-
-        parameters_type& with_min_iters(size_type value)
-        {
-            this->min_iters = value;
-            return *this;
-        }
-
-        std::shared_ptr<const CriterionFactory> GKO_DEFERRED_FACTORY_PARAMETER(
-            inner_criterion);
-    };
-    GKO_ENABLE_CRITERION_FACTORY(MinIterationWrapper, parameters, Factory);
-    GKO_ENABLE_BUILD_METHOD(Factory);
-
-protected:
-    bool check_impl(uint8 stopping_id, bool set_finalized,
-                    array<stopping_status>* stop_status, bool* one_changed,
-                    const Updater& updater) override
-    {
-        if (updater.num_iterations_ < this->get_parameters().min_iters) {
-            return false;
-        }
-        return inner_criterion_->check(stopping_id, set_finalized, stop_status,
-                                       one_changed, updater);
-    }
-
-    explicit MinIterationWrapper(std::shared_ptr<const gko::Executor> exec)
-        : EnablePolymorphicObject<MinIterationWrapper, Criterion>(
-              std::move(exec))
-    {}
-
-    explicit MinIterationWrapper(const Factory* factory,
-                                 const CriterionArgs& args)
-        : EnablePolymorphicObject<MinIterationWrapper, Criterion>(
-              factory->get_executor()),
-          parameters_{factory->get_parameters()},
-          inner_criterion_{
-              factory->get_parameters().inner_criterion->generate(args)}
-    {}
-
-    std::shared_ptr<Criterion> inner_criterion_;
-};
 
 
 deferred_factory_parameter<CriterionFactory> min_iters(
