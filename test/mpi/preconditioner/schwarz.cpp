67059cc091f115613efd1d1e9407c551dad79bd2
Atell Krasnopolski <delta_atell@protonmail.com>
2026-01-16 12:33:04 +0100

    remove the multiplier argument in `assert_equal_to_non_distributed_vector`

--- test/mpi/preconditioner/schwarz.cpp
+++ test/mpi/preconditioner/schwarz.cpp
@@ -146,9 +146,7 @@ protected:
 
     void assert_equal_to_non_distributed_vector(
         std::shared_ptr<dist_vec_type> dist_vec,
-        std::shared_ptr<local_vec_type> local_vec,
-        gko::remove_complex<value_type> mult =
-            gko::one<gko::remove_complex<value_type>>())
+        std::shared_ptr<local_vec_type> local_vec)
     {
         auto host_row_part = row_part->clone(ref);
         auto l_dist_vec = dist_vec->get_local_vector();
@@ -159,8 +157,7 @@ protected:
                 local_vec->get_const_values() +
                     host_row_part->get_range_bounds()[comm.rank()]),
             l_dist_vec->get_size()[1]);
-        GKO_ASSERT_MTX_NEAR(l_dist_vec, vec_view.get(),
-                            mult * r<value_type>::value);
+        GKO_ASSERT_MTX_NEAR(l_dist_vec, vec_view.get(), r<value_type>::value);
     }
 };
 
