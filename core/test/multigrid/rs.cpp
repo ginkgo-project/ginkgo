6da7688a0033150b40c664fd4f907c3400fc827e
Atell Krasnopolski <delta_atell@protonmail.com>
2026-02-24 13:02:14 +0100

    comment out the tests for RS

--- core/test/multigrid/rs.cpp
+++ core/test/multigrid/rs.cpp
@@ -15,60 +15,59 @@
 namespace {
 
 
-template <typename ValueIndexType>
-class RsFactory : public ::testing::Test {
-protected:
-    using value_type =
-        typename std::tuple_element<0, decltype(ValueIndexType())>::type;
-    using index_type =
-        typename std::tuple_element<1, decltype(ValueIndexType())>::type;
-    using Mtx = gko::matrix::Csr<value_type, index_type>;
-    using Vec = gko::matrix::Dense<value_type>;
-    using MgLevel = gko::multigrid::Rs<value_type, index_type>;
-    RsFactory()
-        : exec(gko::ReferenceExecutor::create()),
-          rs_factory(MgLevel::build()
-                         .with_coarse_rows(gko::array<index_type>(exec, {2, 3}))
-                         .with_skip_sorting(true)
-                         .on(exec))
-    {}
+// template <typename ValueIndexType>
+// class RsFactory : public ::testing::Test {
+// protected:
+//     using value_type =
+//         typename std::tuple_element<0, decltype(ValueIndexType())>::type;
+//     using index_type =
+//         typename std::tuple_element<1, decltype(ValueIndexType())>::type;
+//     using Mtx = gko::matrix::Csr<value_type, index_type>;
+//     using Vec = gko::matrix::Dense<value_type>;
+//     using MgLevel = gko::multigrid::Rs<value_type, index_type>;
+//     RsFactory()
+//         : exec(gko::ReferenceExecutor::create()),
+//           rs_factory(MgLevel::build()
+//                          .with_coarse_rows(gko::array<index_type>(exec, {2,
+//                          3})) .with_skip_sorting(true) .on(exec))
+//     {}
 
-    std::shared_ptr<const gko::Executor> exec;
-    std::unique_ptr<typename MgLevel::Factory> rs_factory;
-};
+//     std::shared_ptr<const gko::Executor> exec;
+//     std::unique_ptr<typename MgLevel::Factory> rs_factory;
+// };
 
-TYPED_TEST_SUITE(RsFactory, gko::test::ValueIndexTypes,
-                 PairTypenameNameGenerator);
-
-
-TYPED_TEST(RsFactory, FactoryKnowsItsExecutor)
-{
-    ASSERT_EQ(this->rs_factory->get_executor(), this->exec);
-}
+// TYPED_TEST_SUITE(RsFactory, gko::test::ValueIndexTypes,
+//                  PairTypenameNameGenerator);
 
 
-TYPED_TEST(RsFactory, DefaultSetting)
-{
-    using MgLevel = typename TestFixture::MgLevel;
-    auto factory = MgLevel::build().on(this->exec);
-
-    ASSERT_EQ(factory->get_parameters().coarse_rows.get_const_data(), nullptr);
-    ASSERT_EQ(factory->get_parameters().skip_sorting, false);
-}
+// TYPED_TEST(RsFactory, FactoryKnowsItsExecutor)
+// {
+//     ASSERT_EQ(this->rs_factory->get_executor(), this->exec);
+// }
 
 
-TYPED_TEST(RsFactory, SetCoarseRows)
-{
-    using T = typename TestFixture::index_type;
-    GKO_ASSERT_ARRAY_EQ(this->rs_factory->get_parameters().coarse_rows,
-                        gko::array<T>(this->exec, {2, 3}));
-}
+// TYPED_TEST(RsFactory, DefaultSetting)
+// {
+//     using MgLevel = typename TestFixture::MgLevel;
+//     auto factory = MgLevel::build().on(this->exec);
+
+//     ASSERT_EQ(factory->get_parameters().coarse_rows.get_const_data(),
+//     nullptr); ASSERT_EQ(factory->get_parameters().skip_sorting, false);
+// }
 
 
-TYPED_TEST(RsFactory, SetSkipSorting)
-{
-    ASSERT_EQ(this->rs_factory->get_parameters().skip_sorting, true);
-}
+// TYPED_TEST(RsFactory, SetCoarseRows)
+// {
+//     using T = typename TestFixture::index_type;
+//     GKO_ASSERT_ARRAY_EQ(this->rs_factory->get_parameters().coarse_rows,
+//                         gko::array<T>(this->exec, {2, 3}));
+// }
+
+
+// TYPED_TEST(RsFactory, SetSkipSorting)
+// {
+//     ASSERT_EQ(this->rs_factory->get_parameters().skip_sorting, true);
+// }
 
 
 }  // namespace
--- test/multigrid/rs_kernels.cpp
+++ test/multigrid/rs_kernels.cpp
@@ -29,117 +29,118 @@
 #include "test/utils/common_fixture.hpp"
 
 
-class Rs : public CommonTestFixture {
-protected:
-    using Mtx = gko::matrix::Dense<value_type>;
-    using Csr = gko::matrix::Csr<value_type, index_type>;
+// class Rs : public CommonTestFixture {
+// protected:
+//     using Mtx = gko::matrix::Dense<value_type>;
+//     using Csr = gko::matrix::Csr<value_type, index_type>;
 
-    Rs() : rand_engine(30), m{597} {}
+//     Rs() : rand_engine(30), m{597} {}
 
-    gko::array<index_type> gen_coarse_array(gko::size_type num,
-                                            gko::size_type num_rows)
-    {
-        GKO_ASSERT(num <= num_rows);
-        gko::array<index_type> coarse_array(ref, num);
-        std::vector<index_type> base_vec(num_rows, 0);
-        std::iota(base_vec.begin(), base_vec.end(), 0);
-        std::shuffle(base_vec.begin(), base_vec.end(),
-                     std::mt19937{std::random_device{}()});
-        for (gko::size_type i = 0; i < num; i++) {
-            coarse_array.get_data()[i] = base_vec[i + num_rows - num];
-        }
-        return coarse_array;
-    }
+//     gko::array<index_type> gen_coarse_array(gko::size_type num,
+//                                             gko::size_type num_rows)
+//     {
+//         GKO_ASSERT(num <= num_rows);
+//         gko::array<index_type> coarse_array(ref, num);
+//         std::vector<index_type> base_vec(num_rows, 0);
+//         std::iota(base_vec.begin(), base_vec.end(), 0);
+//         std::shuffle(base_vec.begin(), base_vec.end(),
+//                      std::mt19937{std::random_device{}()});
+//         for (gko::size_type i = 0; i < num; i++) {
+//             coarse_array.get_data()[i] = base_vec[i + num_rows - num];
+//         }
+//         return coarse_array;
+//     }
 
-    std::unique_ptr<Mtx> gen_mtx(int num_rows, int num_cols)
-    {
-        return gko::test::generate_random_matrix<Mtx>(
-            num_rows, num_cols,
-            std::uniform_int_distribution<>(num_cols, num_cols),
-            std::normal_distribution<value_type>(-1.0, 1.0), rand_engine, ref);
-    }
+//     std::unique_ptr<Mtx> gen_mtx(int num_rows, int num_cols)
+//     {
+//         return gko::test::generate_random_matrix<Mtx>(
+//             num_rows, num_cols,
+//             std::uniform_int_distribution<>(num_cols, num_cols),
+//             std::normal_distribution<value_type>(-1.0, 1.0), rand_engine,
+//             ref);
+//     }
 
-    void initialize_data(gko::size_type coarse_size = 2)
-    {
-        coarse_rows = gen_coarse_array(coarse_size, m);
-        c_dim = coarse_size;
+//     void initialize_data(gko::size_type coarse_size = 2)
+//     {
+//         coarse_rows = gen_coarse_array(coarse_size, m);
+//         c_dim = coarse_size;
 
-        d_coarse_rows = gko::array<index_type>(exec);
-        d_coarse_rows = coarse_rows;
-        restrict_op = Csr::create(ref, gko::dim<2>(c_dim, m), c_dim);
+//         d_coarse_rows = gko::array<index_type>(exec);
+//         d_coarse_rows = coarse_rows;
+//         restrict_op = Csr::create(ref, gko::dim<2>(c_dim, m), c_dim);
 
-        d_restrict_op = Csr::create(exec);
-        d_restrict_op->copy_from(restrict_op);
-        auto system_dense = gen_mtx(m, m);
-        system_mtx = Csr::create(ref);
-        system_dense->convert_to(system_mtx);
+//         d_restrict_op = Csr::create(exec);
+//         d_restrict_op->copy_from(restrict_op);
+//         auto system_dense = gen_mtx(m, m);
+//         system_mtx = Csr::create(ref);
+//         system_dense->convert_to(system_mtx);
 
-        d_system_mtx = gko::clone(exec, system_mtx);
-    }
+//         d_system_mtx = gko::clone(exec, system_mtx);
+//     }
 
-    std::default_random_engine rand_engine;
+//     std::default_random_engine rand_engine;
 
-    gko::array<index_type> coarse_rows;
-    gko::array<index_type> d_coarse_rows;
+//     gko::array<index_type> coarse_rows;
+//     gko::array<index_type> d_coarse_rows;
 
-    std::shared_ptr<Csr> restrict_op;
-    std::shared_ptr<Csr> d_restrict_op;
+//     std::shared_ptr<Csr> restrict_op;
+//     std::shared_ptr<Csr> d_restrict_op;
 
-    std::shared_ptr<Csr> system_mtx;
-    std::shared_ptr<Csr> d_system_mtx;
+//     std::shared_ptr<Csr> system_mtx;
+//     std::shared_ptr<Csr> d_system_mtx;
 
-    gko::size_type n;
-    gko::size_type m;
-    gko::size_type c_dim;
-};
-
-
-TEST_F(Rs, GenerateMgLevelIsEquivalentToRef)
-{
-    gko::size_type c_size = 34;
-    initialize_data(c_size);
-    auto mg_level_factory = gko::multigrid::Rs<value_type, int>::build()
-                                .with_coarse_rows(coarse_rows)
-                                .with_skip_sorting(true)
-                                .on(ref);
-    auto d_mg_level_factory = gko::multigrid::Rs<value_type, int>::build()
-                                  .with_coarse_rows(d_coarse_rows)
-                                  .with_skip_sorting(true)
-                                  .on(exec);
-
-    auto mg_level = mg_level_factory->generate(system_mtx);
-    auto d_mg_level = d_mg_level_factory->generate(d_system_mtx);
-
-    GKO_ASSERT_MTX_NEAR(gko::as<Csr>(d_mg_level->get_prolong_op()),
-                        gko::as<Csr>(mg_level->get_prolong_op()), 0.0);
-    GKO_ASSERT_MTX_NEAR(gko::as<Csr>(d_mg_level->get_restrict_op()),
-                        gko::as<Csr>(mg_level->get_restrict_op()), 0.0);
-    GKO_ASSERT_MTX_NEAR(gko::as<Csr>(d_mg_level->get_coarse_op()),
-                        gko::as<Csr>(mg_level->get_coarse_op()),
-                        r<value_type>::value);
-}
+//     gko::size_type n;
+//     gko::size_type m;
+//     gko::size_type c_dim;
+// };
 
 
-TEST_F(Rs, GenerateMgLevelIsEquivalentToRefOnUnsortedMatrix)
-{
-    initialize_data(243);
-    gko::test::unsort_matrix(system_mtx, rand_engine);
-    d_system_mtx = gko::clone(exec, system_mtx);
-    auto mg_level_factory = gko::multigrid::Rs<value_type, int>::build()
-                                .with_coarse_rows(coarse_rows)
-                                .on(ref);
-    auto d_mg_level_factory = gko::multigrid::Rs<value_type, int>::build()
-                                  .with_coarse_rows(d_coarse_rows)
-                                  .on(exec);
+// TEST_F(Rs, GenerateMgLevelIsEquivalentToRef)
+// {
+//     gko::size_type c_size = 34;
+//     initialize_data(c_size);
+//     auto mg_level_factory = gko::multigrid::Rs<value_type, int>::build()
+//                                 .with_coarse_rows(coarse_rows)
+//                                 .with_skip_sorting(true)
+//                                 .on(ref);
+//     auto d_mg_level_factory = gko::multigrid::Rs<value_type, int>::build()
+//                                   .with_coarse_rows(d_coarse_rows)
+//                                   .with_skip_sorting(true)
+//                                   .on(exec);
 
-    auto mg_level = mg_level_factory->generate(system_mtx);
-    auto d_mg_level = d_mg_level_factory->generate(d_system_mtx);
+//     auto mg_level = mg_level_factory->generate(system_mtx);
+//     auto d_mg_level = d_mg_level_factory->generate(d_system_mtx);
 
-    GKO_ASSERT_MTX_NEAR(gko::as<Csr>(d_mg_level->get_prolong_op()),
-                        gko::as<Csr>(mg_level->get_prolong_op()), 0.0);
-    GKO_ASSERT_MTX_NEAR(gko::as<Csr>(d_mg_level->get_restrict_op()),
-                        gko::as<Csr>(mg_level->get_restrict_op()), 0.0);
-    GKO_ASSERT_MTX_NEAR(gko::as<Csr>(d_mg_level->get_coarse_op()),
-                        gko::as<Csr>(mg_level->get_coarse_op()),
-                        r<value_type>::value);
-}
+//     GKO_ASSERT_MTX_NEAR(gko::as<Csr>(d_mg_level->get_prolong_op()),
+//                         gko::as<Csr>(mg_level->get_prolong_op()), 0.0);
+//     GKO_ASSERT_MTX_NEAR(gko::as<Csr>(d_mg_level->get_restrict_op()),
+//                         gko::as<Csr>(mg_level->get_restrict_op()), 0.0);
+//     GKO_ASSERT_MTX_NEAR(gko::as<Csr>(d_mg_level->get_coarse_op()),
+//                         gko::as<Csr>(mg_level->get_coarse_op()),
+//                         r<value_type>::value);
+// }
+
+
+// TEST_F(Rs, GenerateMgLevelIsEquivalentToRefOnUnsortedMatrix)
+// {
+//     initialize_data(243);
+//     gko::test::unsort_matrix(system_mtx, rand_engine);
+//     d_system_mtx = gko::clone(exec, system_mtx);
+//     auto mg_level_factory = gko::multigrid::Rs<value_type, int>::build()
+//                                 .with_coarse_rows(coarse_rows)
+//                                 .on(ref);
+//     auto d_mg_level_factory = gko::multigrid::Rs<value_type, int>::build()
+//                                   .with_coarse_rows(d_coarse_rows)
+//                                   .on(exec);
+
+//     auto mg_level = mg_level_factory->generate(system_mtx);
+//     auto d_mg_level = d_mg_level_factory->generate(d_system_mtx);
+
+//     GKO_ASSERT_MTX_NEAR(gko::as<Csr>(d_mg_level->get_prolong_op()),
+//                         gko::as<Csr>(mg_level->get_prolong_op()), 0.0);
+//     GKO_ASSERT_MTX_NEAR(gko::as<Csr>(d_mg_level->get_restrict_op()),
+//                         gko::as<Csr>(mg_level->get_restrict_op()), 0.0);
+//     GKO_ASSERT_MTX_NEAR(gko::as<Csr>(d_mg_level->get_coarse_op()),
+//                         gko::as<Csr>(mg_level->get_coarse_op()),
+//                         r<value_type>::value);
+// }
