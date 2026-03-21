0d41f71e9f456aae88d9c3e999c7cfa9860a8921
Pratik Nayak <pratikvn@protonmail.com>
2026-03-21 22:41:14 +0100

    update solverbaselinop eiwth new workspace node

--- core/solver/workspace_tree.cpp
+++ core/solver/workspace_tree.cpp
@@ -2,12 +2,67 @@
 //
 // SPDX-License-Identifier: BSD-3-Clause
 
+#include <ginkgo/core/solver/solver_base.hpp>
 #include <ginkgo/core/solver/workspace_tree.hpp>
 
 
 namespace gko {
 namespace solver {
 namespace detail {
+
+
+SolverBaseLinOp::SolverBaseLinOp(std::shared_ptr<const Executor> exec)
+{
+    owned_workspace_ = Workspace::create();
+    node_ = owned_workspace_->root();
+    node_->bind_executor(std::move(exec));
+}
+
+SolverBaseLinOp::SolverBaseLinOp(const SolverBaseLinOp& /*other*/)
+{
+    owned_workspace_ = Workspace::create();
+    node_ = owned_workspace_->root();
+}
+
+SolverBaseLinOp::SolverBaseLinOp(SolverBaseLinOp&& other) noexcept
+{
+    owned_workspace_ = std::move(other.owned_workspace_);
+    node_ = owned_workspace_ ? owned_workspace_->root() : nullptr;
+    other.node_ = nullptr;
+}
+
+SolverBaseLinOp& SolverBaseLinOp::operator=(const SolverBaseLinOp& /*other*/)
+{
+    return *this;
+}
+
+SolverBaseLinOp& SolverBaseLinOp::operator=(SolverBaseLinOp&& other) noexcept
+{
+    if (this != &other) {
+        owned_workspace_ = std::move(other.owned_workspace_);
+        node_ = owned_workspace_ ? owned_workspace_->root() : nullptr;
+        other.node_ = nullptr;
+    }
+    return *this;
+}
+
+void SolverBaseLinOp::set_workspace(std::unique_ptr<Workspace> ws)
+{
+    owned_workspace_ = std::move(ws);
+    node_ = owned_workspace_ ? owned_workspace_->root() : nullptr;
+}
+
+void SolverBaseLinOp::set_workspace_node(WorkspaceNode* node)
+{
+    owned_workspace_ = nullptr;
+    node_ = node;
+}
+
+std::unique_ptr<Workspace> SolverBaseLinOp::extract_workspace()
+{
+    node_ = nullptr;
+    return std::move(owned_workspace_);
+}
 
 
 void WorkspaceNode::describe(std::ostream& os, int indent) const
