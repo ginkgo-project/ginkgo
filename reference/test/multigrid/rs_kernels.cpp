c6dd66abf9f7ed67e44b2778df48b9a955d6a517
Atell Krasnopolski <delta_atell@protonmail.com>
2026-04-24 20:06:56 +0200

    Use a 5x5 example in RS-coarsening tests

--- reference/test/multigrid/rs_kernels.cpp
+++ reference/test/multigrid/rs_kernels.cpp
@@ -26,22 +26,24 @@ class Rs : public ::testing::Test {
 protected:
     Rs() : exec(gko::ReferenceExecutor::create()) {}
 
-    //  * A =
-    //  * [  2  -1   0 ]
-    //  * [ -1   2  -1 ]
-    //  * [  0  -1   2 ]
-    // split: 0=C, 1=F, 2=C
-    // interpolation row 1: w_10 = -(-1/2) = 0.5, w_12 = -(-1/2) = 0.5
+    // A = 5x5 Tridiagonal [ -1  2 -1 ]
+    // split: 0=F, 1=C, 2=F, 3=C, 4=F
+    // C-indices: 1, 3. Coarse size: 2.
     void setup_test_data()
     {
-        A = csr::create(exec, gko::dim<2>{3, 3}, 7);
-        A->read({{2.0, -1.0, 0.0}, {-1.0, 2.0, -1.0}, {0.0, -1.0, 2.0}});
+        A = csr::create(exec, gko::dim<2>{5, 5}, 13);
+        A->read({{2.0, -1.0, 0.0, 0.0, 0.0},
+                 {-1.0, 2.0, -1.0, 0.0, 0.0},
+                 {0.0, -1.0, 2.0, -1.0, 0.0},
+                 {0.0, 0.0, -1.0, 2.0, -1.0},
+                 {0.0, 0.0, 0.0, -1.0, 2.0}});
 
-        // mask aligned with A's non-zeros
-        is_strong = gko::array<bool>(
-            exec, {false, true, true, false, true, true, false});
+        // strength mask: all 8 off-diagonals are strong
+        is_strong =
+            gko::array<bool>(exec, {false, true, true, false, true, true, false,
+                                    true, true, false, true, true, false});
 
-        cf = gko::array<index_type>(exec, {1, -1, 1});  // C, F, C
+        cf = gko::array<index_type>(exec, {-1, 1, -1, 1, -1});
     }
 
     std::shared_ptr<gko::ReferenceExecutor> exec;
@@ -53,54 +55,58 @@ protected:
 
 TEST_F(Rs, ComputeSocAndRunRs)
 {
-    auto A = csr::create(exec, gko::dim<2>{3, 3}, 7);
-    A->read({{2.0, -1.0, 0.0}, {-1.0, 2.0, -1.0}, {0.0, -1.0, 2.0}});
+    auto A = csr::create(exec, gko::dim<2>{5, 5}, 13);
+    A->read({{2.0, -1.0, 0.0, 0.0, 0.0},
+             {-1.0, 2.0, -1.0, 0.0, 0.0},
+             {0.0, -1.0, 2.0, -1.0, 0.0},
+             {0.0, 0.0, -1.0, 2.0, -1.0},
+             {0.0, 0.0, 0.0, -1.0, 2.0}});
 
-    gko::array<bool> is_strong(exec, 7);
-    gko::array<index_type> lambda(exec, 3);
-    gko::array<index_type> cf(exec, 3);
+    gko::array<bool> is_strong(exec, 13);
+    gko::array<index_type> lambda(exec, 5);
+    gko::array<index_type> cf(exec, 5);
     index_type coarse{};
 
     gko::kernels::reference::rs::compute_soc_and_run_rs(
         exec, A.get(), 0.5, is_strong, lambda, cf, coarse);
 
-    std::vector<bool> expected{false, true, true, false, true, true, false};
+    // all off-diagonals are strong
+    std::vector<bool> expected_soc{false, true, true,  false, true, true, false,
+                                   true,  true, false, true,  true, false};
 
-    for (int i = 0; i < 7; ++i) {
-        ASSERT_EQ(is_strong.get_const_data()[i], expected[i]);
+    for (int i = 0; i < 13; ++i) {
+        ASSERT_EQ(is_strong.get_const_data()[i], expected_soc[i]);
     }
-    ASSERT_EQ(lambda.get_const_data()[0], 1);
-    ASSERT_EQ(lambda.get_const_data()[1], 2);
-    ASSERT_EQ(lambda.get_const_data()[2], 1);
+    // initial lambda: [1, 2, 2, 2, 1]. After greedy RS:
     ASSERT_EQ(cf.get_const_data()[0], -1);
     ASSERT_EQ(cf.get_const_data()[1], 1);
     ASSERT_EQ(cf.get_const_data()[2], -1);
-    ASSERT_EQ(coarse, 1);
+    ASSERT_EQ(cf.get_const_data()[3], 1);
+    ASSERT_EQ(cf.get_const_data()[4], -1);
+    ASSERT_EQ(coarse, 2);
 }
 
 
 TEST_F(Rs, FillCoarseAndComputeProlongRowPtrs)
 {
     this->setup_test_data();
-    gko::array<index_type> cf(exec, 3);
-    cf.get_data()[0] = -1;
-    cf.get_data()[1] = 1;
-    cf.get_data()[2] = -1;
-    gko::array<index_type> f2c(exec, 3);
-    gko::array<index_type> p_row_ptrs(exec, 4);
-    gko::array<index_type> coarse(exec, 1);
+    gko::array<index_type> f2c(exec, 5);
+    gko::array<index_type> p_row_ptrs(exec, 6);
+    gko::array<index_type> coarse(exec, 2);
 
     gko::kernels::reference::rs::fill_coarse_and_compute_prolong_row_ptrs(
         exec, cf, coarse, f2c, A.get(), is_strong, p_row_ptrs);
 
-    // C-points get sequential IDs, F-points get -1
+    // C-points (1, 3) map to (0, 1)
     ASSERT_EQ(f2c.get_const_data()[0], -1);
     ASSERT_EQ(f2c.get_const_data()[1], 0);
     ASSERT_EQ(f2c.get_const_data()[2], -1);
-    ASSERT_EQ(coarse.get_const_data()[0], 1);
-    std::vector<index_type> expected{0, 1, 2, 3};
-    for (int i = 0; i < 4; ++i) {
-        ASSERT_EQ(p_row_ptrs.get_const_data()[i], expected[i]);
+    ASSERT_EQ(f2c.get_const_data()[3], 1);
+    ASSERT_EQ(f2c.get_const_data()[4], -1);
+
+    std::vector<index_type> expected_ptrs{0, 1, 2, 4, 5, 6};
+    for (int i = 0; i < 6; ++i) {
+        ASSERT_EQ(p_row_ptrs.get_const_data()[i], expected_ptrs[i]);
     }
 }
 
@@ -108,26 +114,34 @@ TEST_F(Rs, FillCoarseAndComputeProlongRowPtrs)
 TEST_F(Rs, ComputeInterpolation)
 {
     this->setup_test_data();
-    gko::array<index_type> f2c(exec, {0, -1, 1});
-    auto P = csr::create(exec, gko::dim<2>{3, 2}, 4);
-    // manual row_ptrs for 0=C, 1=F(0,2), 2=C
-    exec->copy_from(exec, 4, std::vector<index_type>{0, 1, 3, 4}.data(),
+    gko::array<index_type> f2c(exec, {-1, 0, -1, 1, -1});
+    auto P = csr::create(exec, gko::dim<2>{5, 2}, 6);
+    exec->copy_from(exec, 6, std::vector<index_type>{0, 1, 2, 4, 5, 6}.data(),
                     P->get_row_ptrs());
 
     gko::kernels::reference::rs::compute_interpolation(
         exec, A.get(), is_strong.get_const_data(), cf, f2c.get_const_data(),
         P.get());
 
-    ASSERT_EQ(P->get_const_col_idxs()[0], 0);
-    ASSERT_DOUBLE_EQ(P->get_const_values()[0], 1.0);
+    auto p_vals = P->get_const_values();
+    auto p_cols = P->get_const_col_idxs();
 
-    ASSERT_EQ(P->get_const_col_idxs()[1], 0);
-    ASSERT_DOUBLE_EQ(P->get_const_values()[1], 0.5);
-    ASSERT_EQ(P->get_const_col_idxs()[2], 1);
-    ASSERT_DOUBLE_EQ(P->get_const_values()[2], 0.5);
+    ASSERT_EQ(p_cols[0], 0);
+    ASSERT_DOUBLE_EQ(p_vals[0], 0.5);
 
-    ASSERT_EQ(P->get_const_col_idxs()[3], 1);
-    ASSERT_DOUBLE_EQ(P->get_const_values()[3], 1.0);
+    ASSERT_EQ(p_cols[1], 0);
+    ASSERT_DOUBLE_EQ(p_vals[1], 1.0);
+
+    ASSERT_EQ(p_cols[2], 0);
+    ASSERT_DOUBLE_EQ(p_vals[2], 0.5);
+    ASSERT_EQ(p_cols[3], 1);
+    ASSERT_DOUBLE_EQ(p_vals[3], 0.5);
+
+    ASSERT_EQ(p_cols[4], 1);
+    ASSERT_DOUBLE_EQ(p_vals[4], 1.0);
+
+    ASSERT_EQ(p_cols[5], 1);
+    ASSERT_DOUBLE_EQ(p_vals[5], 0.5);
 }
 
 
