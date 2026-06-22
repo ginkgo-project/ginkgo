930d3f1808fe3c70a36ef430512b08b0a5bd1807
Yu-Hsiang M. Tsai <yhmtsai@gmail.com>
2026-06-22 23:06:43 +0200

    add direct interpolation and change the status mapping

--- reference/multigrid/pmis_kernels.cpp
+++ reference/multigrid/pmis_kernels.cpp
@@ -150,7 +150,7 @@ void initialize_weight_and_status(
         }
     }
     for (auto i = 0; i < nrows; ++i) {
-        status[i] = (weight[i] == 0 ? 1 : 0);
+        status[i] = (weight[i] == 0 ? 0 : -1);
         weight[i] += static_cast<rc>(dist(gen));
     }
 }
@@ -171,21 +171,21 @@ void classify(std::shared_ptr<const DefaultExecutor> exec,
     const auto s_col_idxs = strong_dep->get_const_col_idxs();
 
     for (IndexType row = 0; row < nrows; row++) {
-        // 0 is unassigned yet
+        // -1 is unassigned yet
         auto ans = status[row];
-        if (status[row] == 0) {
+        if (status[row] == -1) {
             const auto row_start = s_row_ptrs[row];
             const auto row_end = s_row_ptrs[row + 1];
             bool is_coarse = true;
             for (IndexType j = row_start; j < row_end; ++j) {
                 auto c = s_col_idxs[j];
-                if (status[c] == 0 && weight[c] >= weight[row]) {
+                if (status[c] == -1 && weight[c] >= weight[row]) {
                     is_coarse = false;
                     break;
                 }
             }
             if (is_coarse) {
-                ans = 2;
+                ans = 1;
             }
         }
         new_status[row] = ans;
@@ -193,14 +193,14 @@ void classify(std::shared_ptr<const DefaultExecutor> exec,
     // mark all points strongly influenced by the new coarse points to fine
     // group
     for (IndexType row = 0; row < nrows; row++) {
-        if (new_status[row] == 2 && new_status[row] != status[row]) {
+        if (new_status[row] == 1 && new_status[row] != status[row]) {
             for (auto idx = trans_strong_dep->get_const_row_ptrs()[row];
                  idx < trans_strong_dep->get_const_row_ptrs()[row + 1]; idx++) {
                 // It is correct even if more than one threads might assign the
                 // value
                 auto col = trans_strong_dep->get_const_col_idxs()[idx];
-                if (new_status[col] == 0) {
-                    new_status[col] = 1;
+                if (new_status[col] == -1) {
+                    new_status[col] = 0;
                 }
             }
         }
@@ -215,12 +215,122 @@ void count(std::shared_ptr<const DefaultExecutor> exec, size_type num,
 {
     size_type ans = 0;
     for (size_type i = 0; i < num; i++) {
-        if (status[i] == 0) {
+        if (status[i] == -1) {
             ans++;
         }
     }
     *num_unassigned = ans;
 }
+
+
+template <typename ValueType, typename IndexType>
+void direct_interpolation_row_count(
+    std::shared_ptr<const DefaultExecutor> exec,
+    const matrix::SparsityCsr<ValueType, IndexType>* strong_dep,
+    const int* status, IndexType* prolong_row_ptr)
+{
+    for (size_type i = 0; i < strong_dep->get_size()[0]; i++) {
+        IndexType num = 0;
+        if (status[i] == 1) {
+            prolong_row_ptr[i] = 1;
+            continue;
+        }
+        for (auto idx = strong_dep->get_const_row_ptrs()[i];
+             idx < strong_dep->get_const_row_ptrs()[i + 1]; idx++) {
+            auto col = strong_dep->get_const_col_idxs()[idx];
+            if (status[col] == 1) {
+                num++;
+            }
+        }
+        prolong_row_ptr[i] = num;
+    }
+}
+
+GKO_INSTANTIATE_FOR_EACH_VALUE_AND_INDEX_TYPE(
+    GKO_DECLARE_DIRECT_INTERPOLATION_ROW_COUNT);
+
+
+template <typename ValueType, typename IndexType>
+void direct_interpolation_fill(
+    std::shared_ptr<const DefaultExecutor> exec,
+    const matrix::Csr<ValueType, IndexType>* csr,
+    const remove_complex<ValueType>* row_maxabs,
+    const remove_complex<ValueType> strength_threshold, const int* coarse_map,
+    const IndexType* prolong_row_ptrs, IndexType* prolong_col_idxs,
+    ValueType* prolong_values)
+{
+    auto csr_values = csr->get_const_values();
+    auto csr_col_idxs = csr->get_const_col_idxs();
+    auto csr_row_ptrs = csr->get_const_row_ptrs();
+    for (size_type row = 0; row < csr->get_size()[0]; row++) {
+        if (coarse_map[row] != coarse_map[row + 1]) {
+            auto idx = prolong_row_ptrs[row];
+            prolong_col_idxs[idx] = coarse_map[row];
+            prolong_values[idx] = one<ValueType>();
+            continue;
+        }
+        auto pos = zero<ValueType>();
+        auto pos_divisor = zero<ValueType>();
+        auto neg = zero<ValueType>();
+        auto neg_divisor = zero<ValueType>();
+        auto diag = zero<ValueType>();
+        bool enable_neg = false;
+        bool enable_pos = false;
+        // first compute alpha/beta
+        auto max_abs = row_maxabs[row];
+        for (auto idx = csr_row_ptrs[row]; idx < csr_row_ptrs[row + 1]; idx++) {
+            auto val = csr_values[idx];
+            auto col = csr_col_idxs[idx];
+            if (col == row) {
+                diag = val;
+                continue;
+            }
+            if (real(val) >= 0) {
+                pos += val;
+                if (coarse_map[col] != coarse_map[col + 1] &&
+                    abs(val) >= strength_threshold * max_abs) {
+                    pos_divisor += val;
+                    enable_pos = true;
+                }
+            } else {
+                neg += val;
+                if (coarse_map[col] != coarse_map[col + 1] &&
+                    abs(val) >= strength_threshold * max_abs) {
+                    neg_divisor += val;
+                    enable_neg = true;
+                }
+            }
+        }
+        pos = safe_divide(pos, pos_divisor);
+        neg = safe_divide(neg, neg_divisor);
+        if (!enable_neg && !enable_pos) {
+            continue;
+        }
+        auto start = prolong_row_ptrs[row];
+        for (auto idx = csr_row_ptrs[row]; idx < csr_row_ptrs[row + 1]; idx++) {
+            auto val = csr_values[idx];
+            auto col = csr_col_idxs[idx];
+            if (col == row || abs(val) < strength_threshold * max_abs) {
+                continue;
+            }
+            if (real(val) >= 0 && enable_pos &&
+                coarse_map[col] != coarse_map[col + 1]) {
+                prolong_col_idxs[start] = coarse_map[col];
+                prolong_values[start] = -pos * val / diag;
+                start++;
+            }
+            if (real(val) < 0 && enable_neg &&
+                coarse_map[col] != coarse_map[col + 1]) {
+                prolong_col_idxs[start] = coarse_map[col];
+                prolong_values[start] = -neg * val / diag;
+                start++;
+            }
+        }
+    }
+}
+
+GKO_INSTANTIATE_FOR_EACH_VALUE_AND_INDEX_TYPE(
+    GKO_DECLARE_DIRECT_INTERPOLATION_FILL);
 
 
 }  // namespace pmis
