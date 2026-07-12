945046d329c92ff67babe4fa30a5eb7826140d8a
Timofei Rusanov <timkingteam@gmail.com>
2026-07-12 11:51:57 +0200

    Accessor: Replace Ginkgo-specific macros in cuda_hip_helper.hpp with __CUDACC__ and __HIPCC__

--- accessor/include/accessor/cuda_hip_helper.hpp
+++ accessor/include/accessor/cuda_hip_helper.hpp
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2017 - 2024 The Ginkgo authors
+// SPDX-FileCopyrightText: 2017 - 2026 The Ginkgo authors
 //
 // SPDX-License-Identifier: BSD-3-Clause
 
@@ -9,10 +9,13 @@
 #include <utility>
 
 
-#ifdef GKO_COMPILING_HIP
+#if defined(__HIPCC__)
 #include "accessor/hip_helper.hpp"
-#else  // GKO_COMPILING_CUDA
+#elif defined(__CUDACC__)
 #include "accessor/cuda_helper.hpp"
+#else
+#error \
+    "cuda_hip_helper.hpp requires compilation with a CUDA (__CUDACC__) or HIP (__HIPCC__) compiler"
 #endif
 
 
@@ -23,9 +26,9 @@ namespace acc {
 template <typename AccType>
 GKO_ACC_INLINE auto as_device_range(AccType&& acc)
 {
-#ifdef GKO_COMPILING_HIP
+#if defined(__HIPCC__)
     return as_hip_range(std::forward<AccType>(acc));
-#else  // GKO_COMPILING_CUDA
+#elif defined(__CUDACC__)
     return as_cuda_range(std::forward<AccType>(acc));
 #endif
 }
