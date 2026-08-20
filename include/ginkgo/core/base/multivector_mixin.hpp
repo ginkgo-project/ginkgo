b747dc2275075a1c264274b6db1d5ce77dc42386
Marcel Koch <marcel.koch@kit.edu>
2026-08-20 15:59:45 +0200

    add more mixin documentation

--- include/ginkgo/core/base/multivector_mixin.hpp
+++ include/ginkgo/core/base/multivector_mixin.hpp
@@ -25,6 +25,18 @@ class MultiVector;
 /**
  * Trait class containing type aliases required by EnableMultiVector.
  *
+ * A specialization for a vector type has to define the following aliases:
+ * - `value_type`: the value type of the vector,
+ * - `absolute_value_type`: the real counterpart of `value_type`, used to store
+ *   absolute values and norms,
+ * - `absolute_type`: the vector type storing absolute values,
+ * - `real_type`: the vector type storing real or imaginary parts,
+ * - `complex_value_type`: the complex counterpart of `value_type`,
+ * - `complex_type`: the vector type storing complex values.
+ *
+ * Vector types whose first template parameter is the value type are already
+ * covered by the specialization below.
+ *
  * @tparam ConcreteType  The vector type derived from EnableMultiVector.
  */
 template <typename ConcreteType>
@@ -55,6 +67,11 @@ struct vector_traits<ConcreteType<ValueType, Args...>> {
  * The allowed matrix::MultiVector<T> type to be used in scaling operations,
  * e.g. in scaled_add.
  *
+ * The stored pointer is never null, and it always points to a vector whose
+ * precision is one of the alternatives of variant_type, i.e. no further
+ * precision conversion is necessary. Implementations have to handle every
+ * alternative, for example by using std::visit.
+ *
  * @tparam ValueType The value type of the vector type on which to call the
  *                    scaling operation
  */
@@ -62,6 +79,7 @@ template <typename ValueType>
 struct scaling_param {
     using variant_type = std::variant<const matrix::MultiVector<ValueType>*>;
 
+    /** The scaling factor with one of the allowed precisions. */
     variant_type variant;
 };
 
@@ -75,6 +93,7 @@ struct scaling_param<std::complex<ValueType>> {
         std::variant<const matrix::MultiVector<ValueType>*,
                      const matrix::MultiVector<std::complex<ValueType>>*>;
 
+    /** The scaling factor with one of the allowed precisions. */
     variant_type variant;
 };
 
@@ -111,8 +130,20 @@ struct scaling_param<std::complex<ValueType>> {
  * as AbstractMultiVector::add_scaled_impl(const AbstractMultiVector* alpha,
  * const AbstractMultiVector* b). This function is overridden (with final) here.
  * Classes using this mixin, instead override functions with concretized
- * arguments, such as add_sca(scaling_param<value_type> alpha,
+ * arguments, such as add_scaled_impl(scaling_param<value_type> alpha,
  * const ConcreteType* b).
+ *
+ * If a requested combination of precisions is not supported, for example
+ * scaling a real vector by a complex factor, then the dispatch throws
+ * NotImplemented instead of forwarding to the concretized function.
+ *
+ * Besides the concretized virtual functions, ConcreteType has to provide:
+ * - a specialization of vector_traits,
+ * - a member function template `as_precision<ValueType>()` returning a
+ *   temporary_conversion of a vector with the requested value type, which is
+ *   used to implement AbstractMultiVector::as_precision, and
+ * - copy and move assignment, which are used to implement the conversion to
+ *   and from ConcreteType.
  *
  * @tparam ConcreteType Multi vector type to enable the mixin for.
  */
@@ -120,93 +151,267 @@ template <typename ConcreteType>
 class EnableMultiVector : public AbstractMultiVector,
                           public ConvertibleTo<ConcreteType> {
 public:
+    /** The traits providing the types related to ConcreteType. */
     using traits = vector_traits<ConcreteType>;
+    /** The value type of the stored elements. */
     using value_type = typename traits::value_type;
+    /** The real counterpart of value_type. */
     using absolute_value_type = typename traits::absolute_value_type;
+    /** The vector type holding absolute values of this vector. */
     using absolute_type = typename traits::absolute_type;
+    /** The vector type holding real or imaginary parts of this vector. */
     using real_type = typename traits::real_type;
+    /** The vector type holding a complex version of this vector. */
     using complex_type = typename traits::complex_type;
+    /** The target type of the conversions provided by this mixin. */
     using result_type = ConcreteType;
+    /** The vector type used to store the results of reductions to norms. */
     using norm_type = matrix::MultiVector<absolute_value_type>;
+    /** The mutable device view type of the local data. */
     using device_view = AbstractMultiVector::device_view<value_type>;
+    /** The immutable device view type of the local data. */
     using const_device_view =
         AbstractMultiVector::device_view<const value_type>;
 
     using ConvertibleTo<result_type>::convert_to;
     using ConvertibleTo<result_type>::move_to;
 
+    /**
+     * @copydoc AbstractMultiVector::create_with_config_of(ptr_param<const
+     *          ConcreteType>)
+     *
+     * @note Returns an object with ConcreteType
+     */
     [[nodiscard]] static std::unique_ptr<ConcreteType> create_with_config_of(
         ptr_param<const ConcreteType> other);
 
+    /**
+     * @copydoc AbstractMultiVector::create_with_type_of(ptr_param<const
+     *          ConcreteType>, std::shared_ptr<const Executor>)
+     *
+     * @note Returns an object with ConcreteType
+     */
     [[nodiscard]] static std::unique_ptr<ConcreteType> create_with_type_of(
         ptr_param<const ConcreteType> other,
         std::shared_ptr<const Executor> exec);
 
+    /**
+     * @copydoc AbstractMultiVector::create_with_type_of(ptr_param<const
+     *          ConcreteType>, std::shared_ptr<const Executor>, const dim<2>&,
+     *          const dim<2>&)
+     *
+     * @note Returns an object with ConcreteType
+     */
     [[nodiscard]] static std::unique_ptr<ConcreteType> create_with_type_of(
         ptr_param<const ConcreteType> other,
         std::shared_ptr<const Executor> exec, const dim<2>& global_size,
         const dim<2>& local_size);
 
+    /**
+     * @copydoc AbstractMultiVector::create_with_type_of(ptr_param<const
+     *          ConcreteType>, std::shared_ptr<const Executor>, const dim<2>&,
+     *          const dim<2>&, size_type)
+     *
+     * @note Returns an object with ConcreteType
+     */
     [[nodiscard]] static std::unique_ptr<ConcreteType> create_with_type_of(
         ptr_param<const ConcreteType> other,
         std::shared_ptr<const Executor> exec, const dim<2>& global_size,
         const dim<2>& local_size, size_type stride);
 
+    /**
+     * @copydoc AbstractMultiVector::clone(std::shared_ptr<const Executor>)
+     *
+     * @note Returns an object with ConcreteType
+     */
     [[nodiscard]] std::unique_ptr<ConcreteType> clone(
         std::shared_ptr<const Executor> exec) const;
 
+    /**
+     * @copydoc AbstractMultiVector::clone()
+     *
+     * @note Returns an object with ConcreteType
+     */
     [[nodiscard]] std::unique_ptr<ConcreteType> clone() const;
 
+    /**
+     * @copydoc AbstractMultiVector::copy_from(ptr_param<const ConcreteType>)
+     *
+     * @note Takes an object with ConcreteType as input
+     */
     ConcreteType* copy_from(ptr_param<const ConcreteType> other);
 
+
+    /**
+     * @copydoc AbstractMultiVector::move_from(ptr_param<ConcreteType>)
+     *
+     * @note Takes an object with ConcreteType as input
+     */
     ConcreteType* move_from(ptr_param<ConcreteType> other);
 
+    /**
+     * @copydoc AbstractMultiVector::create_default()
+     *
+     * @note Returns an object with ConcreteType
+     */
     [[nodiscard]] std::unique_ptr<ConcreteType> create_default();
 
+    /**
+     * @copydoc AbstractMultiVector::create_default(ptr_param<const Executor>)
+     *
+     * @note Returns an object with ConcreteType
+     */
     [[nodiscard]] std::unique_ptr<ConcreteType> create_default(
         std::shared_ptr<const Executor> exec);
 
+    /**
+     * @copydoc AbstractMultiVector::create_subview(local_span, local_span)
+     *
+     * @note Returns an object with ConcreteType
+     */
     [[nodiscard]] std::unique_ptr<ConcreteType> create_subview(
         local_span rows, local_span columns);
 
+    /**
+     * @copydoc AbstractMultiVector::create_subview(local_span, local_span)
+     *
+     * @note Returns an object with ConcreteType
+     */
     [[nodiscard]] std::unique_ptr<const ConcreteType> create_subview(
         local_span rows, local_span columns) const;
 
+    /**
+     * @copydoc AbstractMultiVector::create_subview(local_span, local_span,
+     *          dim<2>)
+     *
+     * @note Returns an object with ConcreteType
+     */
     [[nodiscard]] std::unique_ptr<ConcreteType> create_subview(
         local_span rows, local_span columns, dim<2> global_size);
 
+    /**
+     * @copydoc AbstractMultiVector::create_subview(local_span, local_span,
+     *          dim<2>)
+     *
+     * @note Returns an object with ConcreteType
+     */
     [[nodiscard]] std::unique_ptr<const ConcreteType> create_subview(
         local_span rows, local_span columns, dim<2> global_size) const;
 
+    /**
+     * @copydoc AbstractMultiVector::create_real_view() const
+     *
+     * @note Returns an object with real_type
+     */
     [[nodiscard]] std::unique_ptr<const real_type> create_real_view() const;
 
+    /**
+     * @copydoc create_real_view() const
+     *
+     * @note Returns an object with real_type
+     */
     [[nodiscard]] std::unique_ptr<real_type> create_real_view();
 
+    /**
+     * @copydoc AbstractMultiVector::compute_absolute()
+     *
+     * @note Returns an object with absolute_type
+     */
     [[nodiscard]] std::unique_ptr<absolute_type> compute_absolute() const;
 
+    /**
+     * @copydoc
+     * AbstractMultiVector::compute_absolute(ptr_param<AbstractMultiVector>)
+     *
+     * @note Takes an object with absolute_type as output, so the precision is
+     *       checked at compile time
+     */
     void compute_absolute(ptr_param<absolute_type> output) const;
 
+    /**
+     * @copydoc AbstractMultiVector::make_complex()
+     *
+     * @note Returns an object with complex_type
+     */
     [[nodiscard]] std::unique_ptr<complex_type> make_complex() const;
 
+    /**
+     * @copydoc
+     * AbstractMultiVector::make_complex(ptr_param<AbstractMultiVector>)
+     *
+     * @note Takes an object with complex_type as output, so the precision is
+     *       checked at compile time
+     */
     void make_complex(ptr_param<complex_type> output) const;
 
+    /**
+     * @copydoc AbstractMultiVector::get_real()
+     *
+     * @note Returns an object with real_type
+     */
     [[nodiscard]] std::unique_ptr<real_type> get_real() const;
 
+    /**
+     * @copydoc AbstractMultiVector::get_real(ptr_param<AbstractMultiVector>)
+     *
+     * @note Takes an object with real_type as output, so the precision is
+     *       checked at compile time
+     */
     void get_real(ptr_param<real_type> output) const;
 
+    /**
+     * @copydoc AbstractMultiVector::get_imag()
+     *
+     * @note Returns an object with real_type
+     */
     [[nodiscard]] std::unique_ptr<real_type> get_imag() const;
 
+    /**
+     * @copydoc AbstractMultiVector::get_imag(ptr_param<AbstractMultiVector>)
+     *
+     * @note Takes an object with real_type as output, so the precision is
+     *       checked at compile time
+     */
     void get_imag(ptr_param<real_type> output) const;
 
+    /**
+     * @copydoc ConvertibleTo::convert_to(result_type*)
+     *
+     * @note This uses the copy assignment of ConcreteType.
+     */
     void convert_to(result_type* result) const override;
 
+    /**
+     * @copydoc ConvertibleTo::move_to(result_type*)
+     *
+     * @note This uses the move assignment of ConcreteType.
+     */
     void move_to(result_type* result) override;
 
+    /**
+     * Gets a local device view of this vector.
+     *
+     * @note In contrast to AbstractMultiVector::get_local_device_view, the
+     *       value type is already known, so it does not have to be specified
+     *       and no runtime check on the precision is necessary.
+     *
+     * @return A device view of the local data of this vector
+     */
     [[nodiscard]] device_view get_local_device_view();
 
+    /** @copydoc get_local_device_view() */
     [[nodiscard]] const_device_view get_const_local_device_view() const;
 
 protected:
+    /**
+     * Constructs a vector on the given executor with the given size.
+     *
+     * The precision is deduced from value_type, so derived classes don't have
+     * to pass it on.
+     *
+     * @param exec  The executor of this vector
+     * @param size  The size of this vector
+     */
     EnableMultiVector(std::shared_ptr<const Executor> exec, dim<2> size = {})
         : AbstractMultiVector(exec, size, precision_v<value_type>)
     {}
@@ -227,6 +432,12 @@ protected:
         std::shared_ptr<const Executor> exec) const override;
 
     // Concretized virtual function calls
+    //
+    // These are the functions that classes using this mixin have to implement.
+    // All arguments passed to them are already concretized, i.e. they are
+    // stored on the executor of this vector, their dimensions have been
+    // checked, and their precision matches the one required by the operation.
+    // Thus, implementations don't have to handle any conversion themselves.
 
     [[nodiscard]] virtual std::unique_ptr<ConcreteType>
     create_with_same_config_impl() const = 0;
@@ -348,6 +559,13 @@ protected:
     GKO_ENABLE_SELF(ConcreteType);
 
     // Overridden generic functions
+    //
+    // These implement the type-erased interface of AbstractMultiVector by
+    // dispatching to the concretized functions above, see the class
+    // documentation. They are final, so classes using this mixin can't
+    // override them and have to implement the concretized functions instead.
+    // Precision combinations that can't be dispatched, e.g. scaling a real
+    // vector by a complex factor, throw NotImplemented.
 
     [[nodiscard]] std::unique_ptr<AbstractMultiVector>
     create_generic_with_same_config_impl() const final;
@@ -896,6 +1114,13 @@ void EnableMultiVector<ConcreteType>::fill_impl(any_scalar value)
 namespace detail {
 
 
+/**
+ * The value type a scaling factor with AlphaValueType is converted to, before
+ * it is passed to a vector with ValueType.
+ *
+ * A real scaling factor for a complex vector stays real, every other factor is
+ * converted to the vector's value type.
+ */
 template <typename ValueType, typename AlphaValueType>
 struct scaling_factor_target {
     using type = std::conditional_t<is_complex<ValueType>() &&
@@ -920,6 +1145,7 @@ void EnableMultiVector<ConcreteType>::scale_impl(
             using alpha_value_type = std::decay_t<decltype(p)>;
             if constexpr (is_complex<alpha_value_type>() &&
                           !is_complex<value_type>()) {
+                // can't scale a real vector with a complex scalar
                 GKO_NOT_IMPLEMENTED;
             } else {
                 auto alpha_v = as<matrix::MultiVector<alpha_value_type>>(alpha);
@@ -944,6 +1170,7 @@ void EnableMultiVector<ConcreteType>::inv_scale_impl(
             using alpha_value_type = std::decay_t<decltype(p)>;
             if constexpr (is_complex<alpha_value_type>() &&
                           !is_complex<value_type>()) {
+                // can't scale a real vector with a complex scalar
                 GKO_NOT_IMPLEMENTED;
             } else {
                 auto alpha_v = as<matrix::MultiVector<alpha_value_type>>(alpha);
@@ -967,6 +1194,7 @@ void EnableMultiVector<ConcreteType>::add_scaled_impl(
             using alpha_value_type = std::decay_t<decltype(p)>;
             if constexpr (is_complex<alpha_value_type>() &&
                           !is_complex<value_type>()) {
+                // can't scale a real vector with a complex scalar
                 GKO_NOT_IMPLEMENTED;
             } else {
                 auto alpha_v = as<matrix::MultiVector<alpha_value_type>>(alpha);
@@ -994,6 +1222,7 @@ void EnableMultiVector<ConcreteType>::sub_scaled_impl(
             using alpha_value_type = std::decay_t<decltype(p)>;
             if constexpr (is_complex<alpha_value_type>() &&
                           !is_complex<value_type>()) {
+                // can't scale a real vector with a complex scalar
                 GKO_NOT_IMPLEMENTED;
             } else {
                 auto alpha_v = as<matrix::MultiVector<alpha_value_type>>(alpha);
