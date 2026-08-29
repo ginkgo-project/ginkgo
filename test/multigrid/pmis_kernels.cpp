a2ea531a322ff0848c8ddaaf6f4e51a55d4d00c0
Yu-Hsiang M. Tsai <yhmtsai@gmail.com>
2026-08-29 17:14:55 +0200

    collect the reference run in the initialize to avoid duplication in each test

--- test/multigrid/pmis_kernels.cpp
+++ test/multigrid/pmis_kernels.cpp
@@ -36,7 +36,18 @@ protected:
     using SparsityCsr = gko::matrix::SparsityCsr<value_type, index_type>;
     using real_type = gko::remove_complex<value_type>;
 
-    Pmis() : rand_engine(30) {}
+    Pmis()
+        : rand_engine(30),
+          row_ptrs(ref),
+          maxabs(ref),
+          col_idxs(ref),
+          weight(ref),
+          status(ref),
+          new_status(ref),
+          final_status(ref),
+          prolong_row_ptrs(ref),
+          coarse_map(ref)
+    {}
 
     void initialize_data()
     {
@@ -52,14 +63,83 @@ protected:
         gko::utils::make_diag_dominant(system_data);
         system_mtx = Csr::create(ref);
         system_mtx->read(system_data);
-
         d_system_mtx = gko::clone(exec, system_mtx);
+        // the followings run almost whole pmis on reference.
+        auto num = system_mtx->get_size()[0];
+        row_ptrs.resize_and_reset(num + 1);
+        maxabs.resize_and_reset(num);
+        gko::kernels::reference::pmis::compute_row_maxabs(ref, system_mtx.get(),
+                                                          maxabs.get_data());
+        gko::kernels::reference::pmis::compute_strong_dep_row(
+            ref, system_mtx.get(), maxabs.get_const_data(), real_type{0.25},
+            row_ptrs.get_data());
+        gko::kernels::reference::components::prefix_sum_nonnegative(
+            ref, row_ptrs.get_data(), row_ptrs.get_size());
+        col_idxs.resize_and_reset(row_ptrs.get_const_data()[num]);
+        strong_dep = gko::matrix::SparsityCsr<value_type, index_type>::create(
+            ref, system_mtx->get_size(), col_idxs, row_ptrs);
+        gko::kernels::reference::pmis::compute_strong_dep(
+            ref, system_mtx.get(), maxabs.get_const_data(), real_type{0.25},
+            strong_dep.get());
+        trans_strong_dep = gko::as<SparsityCsr>(strong_dep->transpose());
+        weight.resize_and_reset(num);
+        status.resize_and_reset(num);
+        gko::kernels::reference::pmis::initialize_weight_and_status(
+            ref, trans_strong_dep.get(), weight.get_data(), status.get_data());
+        new_status.resize_and_reset(num);
+        auto status_ptr = status.get_data();
+        auto new_status_ptr = new_status.get_data();
+        gko::size_type num_not_assigned = 0;
+        gko::kernels::reference::pmis::count(ref, num, status_ptr,
+                                             &num_not_assigned);
+        while (num_not_assigned != 0) {
+            gko::kernels::reference::pmis::classify(ref, weight.get_data(),
+                                                    strong_dep.get(),
+                                                    status_ptr, new_status_ptr);
+            gko::size_type new_num = 0;
+            gko::kernels::reference::pmis::count(ref, num, new_status_ptr,
+                                                 &new_num);
+            if (new_num == num_not_assigned) {
+                // no progess -> throw error (maybe unneccessary)
+                throw std::runtime_error("no progress in Pmis");
+            }
+            num_not_assigned = new_num;
+            std::swap(new_status_ptr, status_ptr);
+        }
+        if (status_ptr == status.get_data()) {
+            final_status = status;
+        } else {
+            final_status = new_status;
+        }
+
+        prolong_row_ptrs.resize_and_reset(num + 1);
+        gko::kernels::reference::pmis::direct_interpolation_row_count(
+            ref, strong_dep.get(), status_ptr, prolong_row_ptrs.get_data());
+        gko::kernels::reference::components::prefix_sum_nonnegative(
+            ref, prolong_row_ptrs.get_data(), prolong_row_ptrs.get_size());
+        coarse_map.resize_and_reset(num + 1);
+        gko::kernels::reference::components::convert_precision(
+            ref, num, status_ptr, coarse_map.get_data());
+        gko::kernels::reference::components::prefix_sum_nonnegative(
+            ref, coarse_map.get_data(), coarse_map.get_size());
+        auto prolong_nnz = prolong_row_ptrs.get_const_data()[num];
     }
 
     std::default_random_engine rand_engine;
     std::shared_ptr<Csr> system_mtx;
     std::shared_ptr<Csr> d_system_mtx;
     gko::size_type m;
+    gko::array<index_type> row_ptrs;
+    gko::array<real_type> maxabs;
+    gko::array<index_type> col_idxs;
+    std::shared_ptr<SparsityCsr> strong_dep;
+    std::shared_ptr<SparsityCsr> trans_strong_dep;
+    gko::array<real_type> weight;
+    gko::array<int> status;
+    gko::array<int> new_status;
+    gko::array<int> final_status;
+    gko::array<index_type> prolong_row_ptrs;
+    gko::array<index_type> coarse_map;
 };
 
 
@@ -81,9 +161,6 @@ TEST_F(Pmis, ComputeRowMaxAbsIsEquivalentToRef)
 TEST_F(Pmis, ComputeStrongDepRowIsEquivalentToRef)
 {
     initialize_data();
-    gko::array<real_type> maxabs(ref, system_mtx->get_size()[0]);
-    gko::kernels::reference::pmis::compute_row_maxabs(ref, system_mtx.get(),
-                                                      maxabs.get_data());
     gko::array<real_type> d_maxabs(exec, maxabs);
     gko::array<index_type> rows(ref, system_mtx->get_size()[0]);
     gko::array<index_type> d_rows(exec, d_system_mtx->get_size()[0]);
@@ -103,19 +180,10 @@ TEST_F(Pmis, ComputeStrongDepIsEquivalentToRef)
 {
     initialize_data();
     auto num = system_mtx->get_size()[0];
-    gko::array<index_type> row_ptrs(ref, num + 1);
-    gko::array<real_type> maxabs(ref, num);
-    gko::kernels::reference::pmis::compute_row_maxabs(ref, system_mtx.get(),
-                                                      maxabs.get_data());
-    gko::kernels::reference::pmis::compute_strong_dep_row(
-        ref, system_mtx.get(), maxabs.get_const_data(), real_type{0.25},
-        row_ptrs.get_data());
-    gko::kernels::reference::components::prefix_sum_nonnegative(
-        ref, row_ptrs.get_data(), row_ptrs.get_size());
-    gko::array<index_type> col_idxs(ref, row_ptrs.get_const_data()[num]);
+    gko::array<index_type> strong_col_idxs(ref, row_ptrs.get_const_data()[num]);
     auto strong_dep = gko::matrix::SparsityCsr<value_type, index_type>::create(
-        ref, system_mtx->get_size(), std::move(col_idxs), std::move(row_ptrs));
-    gko::array<index_type> d_rows(exec, d_system_mtx->get_size()[0]);
+        ref, system_mtx->get_size(), std::move(strong_col_idxs),
+        std::move(row_ptrs));
     gko::array<real_type> d_maxabs(exec, maxabs);
     auto d_strong_dep = gko::clone(exec, strong_dep);
 
@@ -151,24 +219,8 @@ TEST_F(Pmis, InitializeWeightAndStatusIsEquivalentToRef)
 {
     initialize_data();
     auto num = system_mtx->get_size()[0];
-    gko::array<index_type> row_ptrs(ref, num + 1);
-    gko::array<real_type> maxabs(ref, num);
-    gko::kernels::reference::pmis::compute_row_maxabs(ref, system_mtx.get(),
-                                                      maxabs.get_data());
-    gko::kernels::reference::pmis::compute_strong_dep_row(
-        ref, system_mtx.get(), maxabs.get_const_data(), real_type{0.25},
-        row_ptrs.get_data());
-    gko::kernels::reference::components::prefix_sum_nonnegative(
-        ref, row_ptrs.get_data(), row_ptrs.get_size());
-    gko::array<index_type> col_idxs(ref, row_ptrs.get_const_data()[num]);
-    auto strong_dep = gko::matrix::SparsityCsr<value_type, index_type>::create(
-        ref, system_mtx->get_size(), std::move(col_idxs), std::move(row_ptrs));
-    gko::kernels::reference::pmis::compute_strong_dep(
-        ref, system_mtx.get(), maxabs.get_const_data(), real_type{0.25},
-        strong_dep.get());
     auto trans_strong_dep = gko::as<SparsityCsr>(strong_dep->transpose());
     auto d_trans_strong_dep = gko::clone(exec, trans_strong_dep);
-
     gko::array<real_type> weight(ref, num);
     gko::array<int> status(ref, num);
     gko::array<real_type> d_weight(exec, num);
@@ -193,22 +245,6 @@ TEST_F(Pmis, ClassifyIsEquivalentToRef)
 {
     initialize_data();
     auto num = system_mtx->get_size()[0];
-    gko::array<index_type> row_ptrs(ref, num + 1);
-    gko::array<real_type> maxabs(ref, num);
-    gko::kernels::reference::pmis::compute_row_maxabs(ref, system_mtx.get(),
-                                                      maxabs.get_data());
-    gko::kernels::reference::pmis::compute_strong_dep_row(
-        ref, system_mtx.get(), maxabs.get_const_data(), real_type{0.25},
-        row_ptrs.get_data());
-    gko::kernels::reference::components::prefix_sum_nonnegative(
-        ref, row_ptrs.get_data(), row_ptrs.get_size());
-    gko::array<index_type> col_idxs(ref, row_ptrs.get_const_data()[num]);
-    auto strong_dep = gko::matrix::SparsityCsr<value_type, index_type>::create(
-        ref, system_mtx->get_size(), std::move(col_idxs), std::move(row_ptrs));
-    gko::kernels::reference::pmis::compute_strong_dep(
-        ref, system_mtx.get(), maxabs.get_const_data(), real_type{0.25},
-        strong_dep.get());
-    auto trans_strong_dep = gko::as<SparsityCsr>(strong_dep->transpose());
     gko::array<real_type> weight(ref, num);
     gko::array<int> status(ref, num);
     gko::kernels::reference::pmis::initialize_weight_and_status(
@@ -234,60 +270,16 @@ TEST_F(Pmis, DirectInterpolationRowCountIsEquivalentToRef)
 {
     initialize_data();
     auto num = system_mtx->get_size()[0];
-    gko::array<index_type> row_ptrs(ref, num + 1);
-    gko::array<real_type> maxabs(ref, num);
-    gko::kernels::reference::pmis::compute_row_maxabs(ref, system_mtx.get(),
-                                                      maxabs.get_data());
-    gko::kernels::reference::pmis::compute_strong_dep_row(
-        ref, system_mtx.get(), maxabs.get_const_data(), real_type{0.25},
-        row_ptrs.get_data());
-    gko::kernels::reference::components::prefix_sum_nonnegative(
-        ref, row_ptrs.get_data(), row_ptrs.get_size());
-    gko::array<index_type> col_idxs(ref, row_ptrs.get_const_data()[num]);
-    auto strong_dep = gko::matrix::SparsityCsr<value_type, index_type>::create(
-        ref, system_mtx->get_size(), std::move(col_idxs), std::move(row_ptrs));
-    gko::kernels::reference::pmis::compute_strong_dep(
-        ref, system_mtx.get(), maxabs.get_const_data(), real_type{0.25},
-        strong_dep.get());
-    auto trans_strong_dep = gko::as<SparsityCsr>(strong_dep->transpose());
-    gko::array<real_type> weight(ref, num);
-    gko::array<int> status(ref, num);
-    gko::kernels::reference::pmis::initialize_weight_and_status(
-        ref, trans_strong_dep.get(), weight.get_data(), status.get_data());
-    gko::array<int> new_status(ref, num);
-    auto status_ptr = status.get_data();
-    auto new_status_ptr = new_status.get_data();
-    gko::size_type num_not_assigned = 0;
-    gko::kernels::reference::pmis::count(ref, num, status_ptr,
-                                         &num_not_assigned);
-    while (num_not_assigned != 0) {
-        gko::kernels::reference::pmis::classify(ref, weight.get_data(),
-                                                strong_dep.get(), status_ptr,
-                                                new_status_ptr);
-        gko::size_type new_num = 0;
-        gko::kernels::reference::pmis::count(ref, num, new_status_ptr,
-                                             &new_num);
-        if (new_num == num_not_assigned) {
-            // no progess -> throw error (maybe unneccessary)
-            throw std::runtime_error("no progress in Pmis");
-        }
-        num_not_assigned = new_num;
-        std::swap(new_status_ptr, status_ptr);
-    }
     auto d_strong_dep = gko::clone(exec, strong_dep);
-    gko::array<int> d_status(exec);
-    if (status_ptr == status.get_data()) {
-        d_status = status;
-    } else {
-        d_status = new_status;
-    }
+    gko::array<int> d_final_status(exec, final_status);
     gko::array<index_type> prolong_row_count(ref, num);
     gko::array<index_type> d_prolong_row_count(exec, num);
 
     gko::kernels::reference::pmis::direct_interpolation_row_count(
-        ref, strong_dep.get(), status_ptr, prolong_row_count.get_data());
+        ref, strong_dep.get(), final_status.get_const_data(),
+        prolong_row_count.get_data());
     gko::kernels::GKO_DEVICE_NAMESPACE::pmis::direct_interpolation_row_count(
-        exec, d_strong_dep.get(), d_status.get_const_data(),
+        exec, d_strong_dep.get(), d_final_status.get_const_data(),
         d_prolong_row_count.get_data());
 
     GKO_ASSERT_ARRAY_EQ(d_prolong_row_count, prolong_row_count);
@@ -298,69 +290,12 @@ TEST_F(Pmis, DirectInterpolationFillIsEquivalentToRef)
 {
     initialize_data();
     auto num = system_mtx->get_size()[0];
-    gko::array<index_type> row_ptrs(ref, num + 1);
-    gko::array<real_type> maxabs(ref, num);
-    gko::kernels::reference::pmis::compute_row_maxabs(ref, system_mtx.get(),
-                                                      maxabs.get_data());
-    gko::kernels::reference::pmis::compute_strong_dep_row(
-        ref, system_mtx.get(), maxabs.get_const_data(), real_type{0.25},
-        row_ptrs.get_data());
-    gko::kernels::reference::components::prefix_sum_nonnegative(
-        ref, row_ptrs.get_data(), row_ptrs.get_size());
-    gko::array<index_type> col_idxs(ref, row_ptrs.get_const_data()[num]);
-    auto strong_dep = gko::matrix::SparsityCsr<value_type, index_type>::create(
-        ref, system_mtx->get_size(), std::move(col_idxs), std::move(row_ptrs));
-    gko::kernels::reference::pmis::compute_strong_dep(
-        ref, system_mtx.get(), maxabs.get_const_data(), real_type{0.25},
-        strong_dep.get());
-    auto trans_strong_dep = gko::as<SparsityCsr>(strong_dep->transpose());
-    gko::array<real_type> weight(ref, num);
-    gko::array<int> status(ref, num);
-    gko::kernels::reference::pmis::initialize_weight_and_status(
-        ref, trans_strong_dep.get(), weight.get_data(), status.get_data());
-    gko::array<int> new_status(ref, num);
-    auto status_ptr = status.get_data();
-    auto new_status_ptr = new_status.get_data();
-    gko::size_type num_not_assigned = 0;
-    gko::kernels::reference::pmis::count(ref, num, status_ptr,
-                                         &num_not_assigned);
-    while (num_not_assigned != 0) {
-        gko::kernels::reference::pmis::classify(ref, weight.get_data(),
-                                                strong_dep.get(), status_ptr,
-                                                new_status_ptr);
-        gko::size_type new_num = 0;
-        gko::kernels::reference::pmis::count(ref, num, new_status_ptr,
-                                             &new_num);
-        if (new_num == num_not_assigned) {
-            // no progess -> throw error (maybe unneccessary)
-            throw std::runtime_error("no progress in Pmis");
-        }
-        num_not_assigned = new_num;
-        std::swap(new_status_ptr, status_ptr);
-    }
-    auto d_strong_dep = gko::clone(exec, strong_dep);
-    gko::array<int> d_status(exec);
-    if (status_ptr == status.get_data()) {
-        d_status = status;
-    } else {
-        d_status = new_status;
-    }
-    gko::array<index_type> prolong_row_ptrs(ref, num + 1);
-    gko::kernels::reference::pmis::direct_interpolation_row_count(
-        ref, strong_dep.get(), status_ptr, prolong_row_ptrs.get_data());
-    gko::kernels::reference::components::prefix_sum_nonnegative(
-        ref, prolong_row_ptrs.get_data(), prolong_row_ptrs.get_size());
-    gko::array<index_type> coarse_map(ref, num + 1);
-    gko::kernels::reference::components::convert_precision(
-        ref, num, status_ptr, coarse_map.get_data());
-    gko::kernels::reference::components::prefix_sum_nonnegative(
-        ref, coarse_map.get_data(), coarse_map.get_size());
-    auto prolong_nnz = prolong_row_ptrs.get_const_data()[num];
-    gko::array<index_type> prolong_col_idxs(ref, prolong_nnz);
-    gko::array<value_type> prolong_values(ref, prolong_nnz);
     gko::array<real_type> d_maxabs(exec, maxabs);
     gko::array<index_type> d_coarse_map(exec, coarse_map);
     gko::array<index_type> d_prolong_row_ptrs(exec, prolong_row_ptrs);
+    auto prolong_nnz = prolong_row_ptrs.get_const_data()[num];
+    gko::array<index_type> prolong_col_idxs(ref, prolong_nnz);
+    gko::array<value_type> prolong_values(ref, prolong_nnz);
     gko::array<index_type> d_prolong_col_idxs(exec, prolong_nnz);
     gko::array<value_type> d_prolong_values(exec, prolong_nnz);
 
