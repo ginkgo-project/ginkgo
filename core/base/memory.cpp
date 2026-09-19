b14e508267c38c904c6932385e7605b732c5beb9
Pratik Nayak <pratikvn@protonmail.com>
2026-09-19 04:44:39 +0200

    overalign CpuAllocator to 128B

--- core/base/memory.cpp
+++ core/base/memory.cpp
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2017 - 2024 The Ginkgo authors
+// SPDX-FileCopyrightText: 2017 - 2026 The Ginkgo authors
 //
 // SPDX-License-Identifier: BSD-3-Clause
 
@@ -12,9 +12,21 @@
 namespace gko {
 
 
+// 128B: Intel L2 prefetchers pair cache lines; M1/POWER8 lines are 128B.
+#ifdef __cpp_aligned_new
+constexpr std::align_val_t cpu_allocator_alignment{128};
+#endif
+
+
 void* CpuAllocator::allocate(size_type num_bytes)
 {
+    // Apple Clang disables aligned new for macOS < 10.14.
+#ifdef __cpp_aligned_new
+    auto ptr =
+        ::operator new (num_bytes, cpu_allocator_alignment, std::nothrow_t{});
+#else
     auto ptr = ::operator new (num_bytes, std::nothrow_t{});
+#endif
     GKO_ENSURE_ALLOCATED(ptr, "cpu", num_bytes);
     return ptr;
 }
@@ -22,7 +34,11 @@ void* CpuAllocator::allocate(size_type num_bytes)
 
 void CpuAllocator::deallocate(void* ptr)
 {
+#ifdef __cpp_aligned_new
+    ::operator delete (ptr, cpu_allocator_alignment, std::nothrow_t{});
+#else
     ::operator delete (ptr, std::nothrow_t{});
+#endif
 }
 
 
